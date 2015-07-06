1ddadf9a0710aa4ed70b4b750f354d0577f1a137
gdi2290 <github@gdi2290.com>
2015-07-05 18:54:02 -0700

    chore(update-ng-bundle): copy files from bundle

--- scripts/update-ng-bundle.sh
+++ scripts/update-ng-bundle.sh
@@ -1,13 +1,7 @@
 echo ""
 echo "updating scripts from code.angularjs.org"
 
-curl -o web_modules/angular2.dev.js "https://code.angularjs.org/$1/angular2.dev.js"
-curl -o web_modules/angular2.js "https://code.angularjs.org/$1/angular2.js"
-curl -o web_modules/angular2.min.js "https://code.angularjs.org/$1/angular2.min.js"
-curl -o web_modules/router.dev.js "https://code.angularjs.org/$1/router.dev.js"
-curl -o web_modules/router.dev.js.map "https://code.angularjs.org/$1/router.dev.js.map"
-curl -o web_modules/mock.dev.js "https://code.angularjs.org/$1/mock.dev.js"
-curl -o web_modules/mock.dev.js.map "https://code.angularjs.org/$1/mock.dev.js.map"
+cp ./angular/dist/bundle/. ./web_modules/ -R
 
 curl -o web_modules/system.js  "https://cdn.rawgit.com/systemjs/systemjs/$2/dist/system.js"
 curl -o web_modules/system.js.map  "https://cdn.rawgit.com/systemjs/systemjs/$2/dist/system.js.map"
