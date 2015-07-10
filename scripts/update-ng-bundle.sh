6e4b4f9c1f3181c6080e220993e7e70b1891ff7b
gdi2290 <github@gdi2290.com>
2015-07-10 04:57:46 -0700

    chore(update-ng-bundle): replace files

--- scripts/update-ng-bundle.sh
+++ scripts/update-ng-bundle.sh
@@ -3,7 +3,7 @@ echo ""
 echo "updating scripts from angular/dist/bundle"
 
 mkdir -p ./web_modules
-cp -R ./angular/dist/bundle/. ./web_modules/
+cp -fR ./angular/dist/bundle/. ./web_modules/
 
 echo "done!"
 echo ""
