bf36f864f37157dea9d0dc06ae00bed5f93b0422
gdi2290 <github@gdi2290.com>
2015-07-15 15:34:05 -0700

    style(typings): include missing gulp types

---
+++ custom_typings/gulp-uglify.d.ts
@@ -0,0 +1,11 @@
+// Type definitions for vinyl-source-stream
+// Project: https://github.com/terinjokes/gulp-uglify
+// Definitions by: Jeff Whelpley <https://github.com/jeffwhelpley>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+/// <reference path="../tsd_typings/node/node.d.ts"/>
+
+declare module "gulp-uglify" {
+  function uglify(): NodeJS.ReadWriteStream;
+  export = uglify;
+}
