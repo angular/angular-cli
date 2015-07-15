9b1b78002acd3f607dfbe518944758f3f1b69f5f
gdi2290 <github@gdi2290.com>
2015-07-15 16:19:27 -0700

    test(todo.e2e): select elements using better queries

--- test/todo.e2e.js
+++ test/todo.e2e.js
@@ -14,7 +14,7 @@ describe('Todo', function() {
   });
 
   it('should verify if the main web elements are present in the page', function() {
-    subject = element(by.id('todoapp'));
+    subject = element(by.deepCss('#todoapp')).isPresent();
     result  = true;
 
     expect(subject).toBe(result);
@@ -22,18 +22,24 @@ describe('Todo', function() {
 
 
   it('should be able to add items in the to do list', function() {
-    subject = element(by.css('.view label'));
+    subject = element.all(by.deepCss('.view label')).first().getText();
     result  = 'Universal JavaScript';
 
     expect(subject).toEqual(result);
   });
 
-  it('should be able to clean the to do list', function() {
-    subject = element(by.id('toggle-all'));
-    subject.click();
-    // clearCompletedButton.click();
-    // expect(viewDiv.isPresent()).toBe(false);
+  xit('should be able to clean the to do list', function() {
+    var toggleAll = element(by.deepCss('#toggle-all'));
+    var clearCompletedButton = element(by.deepCss('#clear-completed'));
+    var viewDiv = element(by.deepCss('.view'));
 
+    toggleAll.click();
+    clearCompletedButton.click();
+
+    subject = viewDiv.isPresent();
+    result  = false;
+
+    expect(subject).toBe(result);
   });
 
 
