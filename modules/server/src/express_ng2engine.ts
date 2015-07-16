a0ee14040c019208477149fae0a5baee058a0ebf
gdi2290 <github@gdi2290.com>
2015-07-15 19:41:53 -0700

    refactor(ng2Engine): include options for feature toggling

--- modules/server/src/express_ng2engine.ts
+++ modules/server/src/express_ng2engine.ts
@@ -1,18 +1,38 @@
 /// <reference path="../typings/tsd.d.ts" />
-
+import './server_patch';
 import * as fs from 'fs';
 import {render} from './render';
 
+import {
+  prebootScript,
+  angularScript,
+  bootstrapButton,
+  bootstrapFunction,
+  bootstrapApp,
+  buildClientScripts
+} from './build_scripts';
+
 export function ng2engine(filePath: string, options, done) {
+  options.scripts = options.scripts || {};
+  options.serverInjector = options.serverInjector || [];
   // read file on disk
   try {
     fs.readFile(filePath, (err, content) => {
-      if (err) {
-        return done(err);
+
+      if (err) { return done(err); }
+      var clientHtml = content.toString();
+      if (options.server === false && options.client === false) {
+        return done(null, clientHtml);
+      }
+      if (options.server === false && options.client !== false) {
+        return done(null, buildClientScripts(clientHtml, options));
       }
 
-      render(content, options.Component, options)
-      .then(rendered => done(null, rendered))
+      render(clientHtml, options.Component, options.serverInjector)
+      .then(html => {
+        var rendered = buildClientScripts(html, options);
+        done(null, rendered);
+      })
       .catch(e => done(e));
     });
   } catch (e) {
