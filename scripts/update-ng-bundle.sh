2197b3c85c0a745206ad66a6aae958ee1a2bdb78
gdi2290 <github@gdi2290.com>
2015-07-19 19:48:17 -0700

    chore(update-ng-bundle): correctly build and include benchpress_bundle

--- scripts/update-ng-bundle.sh
+++ scripts/update-ng-bundle.sh
@@ -3,7 +3,9 @@ echo ""
 echo "updating scripts from angular/dist/bundle"
 
 mkdir -p ./web_modules
+mkdir -p ./web_modules/benchpress_bundle
 cp -fR ./angular/dist/bundle/. ./web_modules/
+cp -fR ./angular/dist/build/benchpress_bundle/. ./web_modules/benchpress_bundle
 
 echo "done!"
 echo ""
