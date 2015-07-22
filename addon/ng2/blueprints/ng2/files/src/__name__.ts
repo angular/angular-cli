39813c7bb36369781209e251ff2158d563e5d47c
rodyhaddad <rody@rodyhaddad.com>
2015-07-23 00:27:07 +0300

    fix(ng2 blueprint typing): fix typings/ location to allow TS compilation
    When the src/ directory was being copied to the tmp/ directory for compilation,
    relative referencing of the typescript definition file was not working properly,
    and the build was failing.
    
    Moving the typings/ folder into src/ fixes this.

--- addon/ng2/blueprints/ng2/files/src/__name__.ts
+++ addon/ng2/blueprints/ng2/files/src/__name__.ts
@@ -1,4 +1,4 @@
-/// <reference path="../typings/angular2/angular2.d.ts"/>
+/// <reference path="./typings/angular2/angular2.d.ts"/>
 import {Component, View, bootstrap} from 'angular2/angular2';
 
 @Component({
