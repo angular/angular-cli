5359e6a61f6d0ff5bf4cb2b2e6a757a1f4362b1d
Ashmeet Sidhu <ashmeet.sidhu7@gmail.com>
2015-07-27 12:37:55 +0530

    chore(karma.conf.js): automatically open chrome on karma start

--- karma.conf.js
+++ karma.conf.js
@@ -59,7 +59,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: [],
+    browsers: ['Chrome'],
 
 
     // Continuous Integration mode
