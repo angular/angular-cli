78cb6f0429a7b5acb3ef24bfbc11715ed7b70498
Ashmeet Sidhu <ashmeet.sidhu7@gmail.com>
2015-07-27 12:32:16 +0530

    fix(karma.conf.js): remove src bundles as these are not required

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,6 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'dist/server/**/*.js',
       'test/**/*spec.js'
     ],
 
@@ -27,8 +26,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'test/**/*spec.js': [ 'browserify' ],
-      'dist/server/**/*.js': [ 'browserify' ]
+      'test/**/*spec.js': [ 'browserify' ]
     },
 
     browserify: {
