6064349f7e912cfd952b231f7af24d95cfa58be9
Ciro Nunes <ciroanunes@gmail.com>
2015-07-31 17:37:19 -0300

    chore(tests): setup tests infrastructure

---
+++ tests/runner.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var Mocha = require('mocha');
+var glob = require('glob');
+
+var root = 'tests/{unit,acceptance}';
+var specFiles = glob.sync(root + '/**/*.spec.js');
+var mocha = new Mocha({
+  timeout: 5000,
+  reporter: 'spec'
+});
+
+specFiles.forEach(mocha.addFile.bind(mocha));
+
+mocha.run(function(failures){
+  process.on('exit', function () {
+    process.exit(failures);
+  });
+});
