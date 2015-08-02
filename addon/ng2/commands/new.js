d89595b60d6324cdd25acf4e9e4929f6bcc50c59
Ciro Nunes <ciroanunes@gmail.com>
2015-08-02 18:49:35 -0300

    chore(new command): ember -> ng
    Closes #27

--- addon/ng2/commands/new.js
+++ addon/ng2/commands/new.js
@@ -28,8 +28,8 @@ module.exports = NewCommand.extend({
         commandOptions.name = rawArgs.shift();
 
         if (!packageName) {
-            message = chalk.yellow('The `ember ' + this.name + '` command requires a ' +
-            'name to be specified. For more details, use `ember help`.');
+            message = chalk.yellow('The `ng ' + this.name + '` command requires a ' +
+            'name to be specified. For more details, use `ng help`.');
 
             return Promise.reject(new SilentError(message));
         }
@@ -39,7 +39,7 @@ module.exports = NewCommand.extend({
         }
 
         if (packageName === '.') {
-            message = 'Trying to generate an application structure in this directory? Use `ember init` instead.';
+            message = 'Trying to generate an application structure in this directory? Use `ng init` instead.';
 
             return Promise.reject(new SilentError(message));
         }
