35478c5816df6c6e8eea92d44baddcdafbe2ae99
gdi2290 <github@gdi2290.com>
2015-08-13 16:03:09 -0700

    feat(server.ts): export server_dom_renderer

--- modules/server/server.ts
+++ modules/server/server.ts
@@ -3,6 +3,7 @@ export * from './src/bootstrap-server';
 export * from './src/express_ng2engine';
 export * from './src/http_server';
 export * from './src/server_router';
+export * from './src/server_dom_renderer';
 export * from './src/render';
 export * from './src/helper';
 export {stringifyElement} from './src/stringifyElement';
