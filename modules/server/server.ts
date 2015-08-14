8a6c41b177f1a09141b7be08aa6c01d72183c393
gdi2290 <github@gdi2290.com>
2015-08-13 17:12:43 -0700

    chore(server_router): rename router_server

--- modules/server/server.ts
+++ modules/server/server.ts
@@ -2,7 +2,7 @@ import './src/server_patch';
 export * from './src/bootstrap-server';
 export * from './src/express_ng2engine';
 export * from './src/http_server';
-export * from './src/server_router';
+export * from './src/router_server';
 export * from './src/server_dom_renderer';
 export * from './src/render';
 export * from './src/helper';
--- modules/server/test/router_server_spec.ts
+++ modules/server/test/router_server_spec.ts
@@ -2,7 +2,7 @@
 
 import 'reflect-metadata';
 import {LocationStrategy} from 'angular2/src/router/location_strategy';
-import {ServerLocationStrategy, locationInjectables} from '../src/server_router';
+import {ServerLocationStrategy, locationInjectables} from '../src/router_server';
 import {Component, Directive, View} from 'angular2/annotations';
 import {routerDirectives, routerInjectables, RouteConfig, Router} from 'angular2/router';
 
@@ -11,9 +11,9 @@ import {routerDirectives, routerInjectables, RouteConfig, Router} from 'angular2
  * place that we can expand in the future
  */
 describe('server_router', () => {
-    
+
     var serverLocationStrategy: ServerLocationStrategy = null;
-    
+
     beforeAll( () => {
         serverLocationStrategy = new ServerLocationStrategy();
     });
@@ -22,37 +22,37 @@ describe('server_router', () => {
     });
 
     describe('ServerLocationStrategy', () => {
-        it('should be defined', () => {    
+        it('should be defined', () => {
             expect(serverLocationStrategy).toBeDefined();
         });
-        
+
         describe('should have all methods defined and functional', () => {
-                        
+
             it('should have method path()', () => {
                 spyOn(serverLocationStrategy, 'path');
                 serverLocationStrategy.path();
                 expect(serverLocationStrategy.path).toHaveBeenCalled();
             });
-            
+
             it('should have method forward()', () => {
                 spyOn(serverLocationStrategy, 'forward');
                 serverLocationStrategy.forward();
                 expect(serverLocationStrategy.forward).toHaveBeenCalled();
             });
-            
+
             it('should have method back()', () => {
                 spyOn(serverLocationStrategy, 'back');
                 serverLocationStrategy.back();
                 expect(serverLocationStrategy.back).toHaveBeenCalled();
             });
-            
+
             it('should have method getBaseHref()', () => {
                 spyOn(serverLocationStrategy, 'getBaseHref').and.callThrough();
                 var baseHref = serverLocationStrategy.getBaseHref();
                 expect(serverLocationStrategy.getBaseHref).toHaveBeenCalled();
                 expect(baseHref).toEqual('/');
             });
-            
+
             it('should have method onPopState()', () => {
                 spyOn(serverLocationStrategy, 'onPopState');
                 var fn = () => {};
@@ -60,7 +60,7 @@ describe('server_router', () => {
                 expect(serverLocationStrategy.onPopState).toHaveBeenCalled();
                 expect(serverLocationStrategy.onPopState).toHaveBeenCalledWith(fn);
             });
-            
+
             it('should have method pushState()', () => {
                 spyOn(serverLocationStrategy, 'pushState');
                 var opts = {
@@ -72,10 +72,10 @@ describe('server_router', () => {
                 expect(serverLocationStrategy.pushState).toHaveBeenCalled();
                 expect(serverLocationStrategy.pushState).toHaveBeenCalledWith(opts.state, opts.title, opts.url);
             });
-            
-            
+
+
         });
-        
+
     });
-	
+
 });
