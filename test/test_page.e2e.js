2afe6a50bc1b6151d88fb1ea8b4df623cc2b79cb
gdi2290 <github@gdi2290.com>
2015-08-18 18:53:23 -0700

    test(test_page.e2e): include wip selection and helpers

--- test/test_page.e2e.js
+++ test/test_page.e2e.js
@@ -19,7 +19,7 @@ describe('Test Page', function() {
     });
 
     runTestSuites(config);
-    
+
   });
 
   describe('Client only render', function() {
@@ -35,126 +35,205 @@ describe('Test Page', function() {
       browser.get(appendQuery('/', config));
       browser.driver.sleep(500); // should be greater or equal to 500ms
     });
-    
+
     runTestSuites(config);
-    
+
   });
 
 
   describe('Server and Client rendered', function() {
-    
+
     var config = {
       server: true,
       client: true,
       preboot: false,
       bootstrap: true
     };
-    
+
     beforeEach(function() {
       browser.get(appendQuery('/', config));
       browser.driver.sleep(500);
     });
-    
+
     runTestSuites(config);
 
   });
 
   describe('Server and Client rendered with Preboot.js', function() {
-    
+
     var config = {
       server: true,
       client: true,
       preboot: true,
       bootstrap: false
     };
-    
+
     beforeEach(function() {
       browser.get(appendQuery('/', config));
     });
-    
+
     runTestSuites(config);
-    
-    xit('should have correct selection in an input', function() {
-      
+    var subject, result;
+
+    it('should have correct selection in an input', function() {
+      var start = browser.executeScript('return document.querySelectorAll("#defaultValueInput")[0].selectionStart');
+      var end   = browser.executeScript('return document.querySelectorAll("#defaultValueInput")[0].selectionEnd');
+
+      expect(start).toBe(0);
+      expect(end).toBe(0);
     });
-    
+
     xit('should have correct selection in an input during bootstrap', function() {
-      
-    })
+      var start, end;
+
+      start = browser.executeScript('return document.querySelectorAll("#defaultValueInput")[0].selectionStart;');
+      end   = browser.executeScript('return document.querySelectorAll("#defaultValueInput")[0].selectionEnd;');
+
+      expect(start).toBe(0);
+      expect(end).toBe(0);
+
+      bootstrapClient();
+
+      start = browser.executeScript('return document.querySelectorAll("#defaultValueInput")[0].selectionStart;');
+      end   = browser.executeScript('return document.querySelectorAll("#defaultValueInput")[0].selectionEnd;');
+
+      // should be 0 not 6
+      expect(start).toBe(0);
+      expect(end).toBe(0);
+    });
+
+
+  });
+
+
+
+  // needed to maintain current element focus
+  describe('Server and Client rendered with Preboot.js and bootstrap', function() {
+
+    var config = {
+      server: true,
+      client: true,
+      preboot: true,
+      bootstrap: true
+    };
+
+    beforeEach(function() {
+      browser.get(appendQuery('/', config));
+    });
+
+    it('should be focused on page load', function() {
+      browser.driver.sleep(500);
+
+      var subject = browser.executeScript('return document.activeElement && document.activeElement.value');
+      var result  = 'value8';
+
+      expect(subject).toBe(result);
+    });
+
 
   });
 
 });
 
-function runTestSuites(config) {
+function runTestSuites(config, env) {
+    var isPreboot      = config.preboot === true;
+    var isServer       = config.server === true  && config.client === false;
+    var isServerCilent = config.server === true  && config.client === true;
+    var isClient       = config.server === false && config.client === true;
+
     var subject, result;
-  
+
     it('should have a displayed input element', function() {
       subject = element.all(by.deepCss('#defaultValueInput')).isDisplayed();
 
       expect(subject).toBeTruthy();
-      
+
     });
 
     it('should have default "value" property for inputs', function() {
-      subject = browser.executeScript('return document.querySelector("#defaultValueInput").value');
+      subject = browser.executeScript('return document.querySelector("#defaultValueInput").value;');
       result = 'value8';
-      
+
       expect(subject).toEqual(result);
-      
+
     });
-    
-    if(config.server === false && config.client === true){
-      
+
+    if (isClient) {
+
       it('should have a NULL "value" attribute (by default) for inputs', function() {
-        subject = browser.executeScript('return document.querySelector("#defaultValueInput").getAttribute("value")');
-  
+        subject = browser.executeScript('return document.querySelector("#defaultValueInput").getAttribute("value");');
+
         expect(subject).toBeNull();
-        
+
       });
-      
+
     }
-    
-    
+
+
     it('should have a defined "value" attribute for inputs', function() {
       subject = browser.executeScript(
-        'document.querySelector("#defaultValueInput").setAttribute("value", "value8");'+
+        'document.querySelector("#defaultValueInput").setAttribute("value", "value8");' +
         'return document.querySelector("#defaultValueInput").getAttribute("value");'
       );
       result = 'value8';
-      
+
       expect(subject).toEqual(result);
-      
+
     });
-    
+
     it('should have attribute "value" NOT EQUAL TO prop "value"', function() {
       browser.executeScript(
-        'var input = document.querySelector("#defaultValueInput");'+
+        'var input = document.querySelector("#defaultValueInput");' +
         'input.value = "PROP"; input.setAttribute("value", "ATTR");'
       );
-      var subject_attr = browser.executeScript('return document.querySelector("#defaultValueInput").getAttribute("value")');
-      var subject_prop = browser.executeScript('return document.querySelector("#defaultValueInput").value');
-      
+      var subject_attr = browser.executeScript('return document.querySelector("#defaultValueInput").getAttribute("value");');
+      var subject_prop = browser.executeScript('return document.querySelector("#defaultValueInput").value;');
+
       expect(subject_attr).not.toEqual(subject_prop);
-      
-    });
-    
-    it('should be focused on page load', function() {
-      subject = browser.executeScript('return document.activeElement && document.activeElement.value');
-      result  = 'value8';
 
-      expect(subject).toBe(result);
-      
     });
 
-    xit('should have checked values for checkbox inputs', function() {
-      subject = element.all(by.deepCss('input[checked]:checked')).count();
-      result  = 3;
-      
-      subject = browser.executeScript('return document.querySelectorAll("input[checked]").length');
+    if (!isServerCilent) {
+      it('should be focused on page load', function() {
+        subject = browser.executeScript('return document.activeElement && document.activeElement.value');
+        result  = 'value8';
 
-      expect(subject).toBe(3);
-    });
+        expect(subject).toBe(result);
+      });
+    }
+
+    if (isServer) {
+
+      it('should have checked values for checkbox inputs', function() {
+        subject = element.all(by.deepCss('input[checked]:checked')).count();
+        result  = 3;
+
+        expect(subject).toBe(3);
+
+        subject = browser.executeScript('return document.querySelectorAll("input[checked]").length');
+
+        expect(subject).toBe(3);
+      });
+    } else if (isPreboot) {
+      it('should have checked values for checkbox inputs', function() {
+        // attr
+        subject = element.all(by.deepCss('input[checked]:checked')).count();
+        result  = 3;
+
+        expect(subject).toBe(3);
+
+        bootstrapClient();
+
+        // prop
+        subject = browser.executeScript(''+
+          'return Array.prototype.filter.call(document.querySelectorAll("input[type=checkbox]"), function(box) {'+
+            'return box.checked'+
+          '}).length;'+
+        '');
+
+        expect(subject).toBe(3);
+      });
+    }
 
 }
 
