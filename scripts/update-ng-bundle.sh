05d373f9604482e5e5e1443c3a00b33f58961c88
gdi2290 <github@gdi2290.com>
2015-08-24 18:13:22 -0700

    chore: update angular to use npm

--- scripts/update-ng-bundle.sh
+++ scripts/update-ng-bundle.sh
@@ -3,7 +3,13 @@ echo ""
 echo "updating scripts from angular/dist/bundle"
 
 mkdir -p ./web_modules
-cp -fR ./angular/dist/. ./web_modules/
+# angular submodule
+
+# cp -fR ./angular/dist/. ./web_modules/
+
+# angular npm
+mkdir -p ./web_modules/js/bundle
+cp -fR ./node_modules/angular2/bundles/. ./web_modules/js/bundle
 
 echo "done!"
 echo ""
