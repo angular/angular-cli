8aa3c02773827a93fce3dd3838c4a8c96576ac33
rodyhaddad <rody@rodyhaddad.com>
2015-09-01 18:48:19 +0300

    chore(ember-cli): change dependency to no longer depend on rodyhaddad's fork
    But we still need to depend on ember-cli's master because the
    ember-cli/ember-cli#4700 hasn't been released yet

---
+++ tests/helpers/tmp.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var fs      = require('fs-extra');
+var existsSync  = require('exists-sync');
+var Promise = require('ember-cli/lib/ext/promise');
+var remove  = Promise.denodeify(fs.remove);
+var root    = process.cwd();
+
+module.exports.setup = function(path) {
+    process.chdir(root);
+
+    return remove(path)
+        .then(function() {
+            fs.mkdirsSync(path);
+        });
+};
+
+module.exports.teardown = function(path) {
+    process.chdir(root);
+
+    if (existsSync(path)) {
+        return remove(path);
+    } else {
+        return Promise.resolve();
+    }
+};
