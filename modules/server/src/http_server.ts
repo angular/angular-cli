05ff6a2b13b027c11bc4f9725196d187e6372938
gdi2290 <github@gdi2290.com>
2015-10-02 00:11:22 -0700

    feat(http_server): preload feature

--- modules/server/src/http_server.ts
+++ modules/server/src/http_server.ts
@@ -17,6 +17,8 @@ import {
   RequestOptions,
   ResponseTypes,
   ResponseOptions,
+  ResponseOptionsArgs,
+  RequestOptionsArgs,
   BaseResponseOptions,
   BaseRequestOptions,
   Request,
@@ -27,9 +29,7 @@ import {
 
 import {EventEmitter} from 'angular2/angular2';
 
-import {
-  ObservableWrapper
-} from 'angular2/src/core/facade/async';
+import {ObservableWrapper} from 'angular2/src/core/facade/async';
 
 import {
   isPresent,
@@ -44,6 +44,8 @@ import { baseUrl } from './helper';
 
 
 export const BASE_URL: OpaqueToken = CONST_EXPR(new OpaqueToken('baseUrl'));
+
+export const PRIME_CACHE: OpaqueToken = CONST_EXPR(new OpaqueToken('primeCache'));
 
 
 class NodeConnection implements Connection {
@@ -83,20 +85,24 @@ class NodeConnection implements Connection {
         status = response ? 200 : 0;
       }
 
-      var responseOptions = <any> new ResponseOptions({body: response, status: status});
+      var responseOptions =  new ResponseOptions<any>({
+        body: response,
+        status: status,
+        url: this.request.url
+      });
 
       if (isPresent(baseResponseOptions)) {
         responseOptions = baseResponseOptions.merge(responseOptions);
       }
 
-      let response = <any>new Response(responseOptions);
+      let res = new Response<any>(responseOptions);
 
-      ObservableWrapper.callNext(this.response, response);
+      ObservableWrapper.callNext(this.response, res);
       // TODO(gdi2290): defer complete if array buffer until done
       ObservableWrapper.callReturn(this.response);
     }));
     this._xhr.addEventListener('error', zone.bind((err) => {
-      var responseOptions = (<any>new ResponseOptions({body: err, type: ResponseTypes.Error, status: this._xhr.status}));
+      var responseOptions = new ResponseOptions<any>({body: err, type: ResponseTypes.Error, status: this._xhr.status});
       if (isPresent(baseResponseOptions)) {
         responseOptions = baseResponseOptions.merge(responseOptions);
       }
@@ -146,6 +152,106 @@ export class NodeBackend {
   }
 }
 
+
+@Injectable()
+export class NgPreloadCacheHttp extends Http {
+  _async: number = 0;
+  _callId: number = 0;
+  _rootNode;
+  _activeNode;
+  constructor(
+    protected _backend: ConnectionBackend,
+    protected _defaultOptions: RequestOptions,
+    @Optional() @Inject(PRIME_CACHE) protected prime?: boolean) {
+    super(_backend, _defaultOptions);
+
+    var _rootNode = { children: [], res: null };
+    this._rootNode = _rootNode;
+    this._activeNode = _rootNode;
+
+
+  }
+
+  preload(factory) {
+
+    // TODO: fix this after the next release with RxNext
+    var obs = new EventEmitter();
+
+    var currentNode = null;
+    if (isPresent(this._activeNode)) {
+      currentNode = { children: [], res: null };
+      this._activeNode.children.push(currentNode);
+    }
+
+    // We need this to ensure all ajax calls are done before rendering the app
+    this._async += 1;
+    let request = factory();
+    ObservableWrapper
+      .subscribe(
+        request,
+        value => {
+          let res = Object.assign({}, value, {
+            headers: value.headers.values()
+          });
+          if (isPresent(currentNode)) {
+            currentNode.res = res;
+          }
+          ObservableWrapper.callNext(obs, value);
+        },
+        e => {
+          // TODO: update Angular 2 Http
+          setTimeout(() => {
+            this._async -= 1;
+            ObservableWrapper.callThrow(obs, e);
+          });
+        },
+        () => {
+          // TODO: update Angular 2 Http
+          setTimeout(() => {
+            this._activeNode = currentNode;
+            this._async -= 1;
+            ObservableWrapper.callReturn(obs);
+            this._activeNode = null;
+          });
+        });
+
+    return obs;
+  }
+
+  request(url: string | Request, options?: RequestOptionsArgs): EventEmitter {
+    return isBlank(this.prime) ? super.request(url, options) : this.preload(() => super.request(url, options));
+  }
+
+  get(url: string, options?: RequestOptionsArgs): EventEmitter {
+    return isBlank(this.prime) ? super.get(url, options) : this.preload(() => super.get(url, options));
+
+  }
+
+  post(url: string, body: string, options?: RequestOptionsArgs): EventEmitter {
+    return isBlank(this.prime) ? super.post(url, body, options) : this.preload(() => super.post(url, body, options));
+  }
+
+  put(url: string, body: string, options?: RequestOptionsArgs): EventEmitter {
+    return isBlank(this.prime) ? super.put(url, body, options) : this.preload(() => super.put(url, body, options));
+  }
+
+  delete(url: string, options?: RequestOptionsArgs): EventEmitter {
+    return isBlank(this.prime) ? super.delete(url, options) : this.preload(() => super.delete(url, options));
+
+  }
+
+  patch(url: string, body: string, options?: RequestOptionsArgs): EventEmitter {
+    return isBlank(this.prime) ? super.patch(url, body, options) : this.preload(() => super.patch(url, body, options));
+  }
+
+  head(url: string, options?: RequestOptionsArgs): EventEmitter {
+    return isBlank(this.prime) ? super.head(url, options) : this.preload(() => super.head(url, options));
+  }
+
+
+}
+
+
 export var HTTP_BINDINGS: Array<any> = [
   bind(RequestOptions).toClass(BaseRequestOptions),
   bind(ResponseOptions).toClass(BaseResponseOptions),
@@ -153,5 +259,5 @@ export var HTTP_BINDINGS: Array<any> = [
   bind(BrowserXhr).toClass(NodeXhr),
   bind(ConnectionBackend).toClass(NodeBackend),
 
-  bind(Http).toClass(Http)
+  bind(Http).toClass(NgPreloadCacheHttp)
 ];
