db042302ec8c7cfa717383f7ac37203601fe9e41
gdi2290 <github@gdi2290.com>
2015-10-13 18:46:03 -0700

    refactor(server_dom_renderer): into /render

--- modules/server/src/render/server_dom_renderer.ts
+++ modules/server/src/render/server_dom_renderer.ts
@@ -1,27 +1,55 @@
-/// <reference path="../typings/tsd.d.ts" />
-import {ListWrapper, MapWrapper, Map, StringMapWrapper, List} from 'angular2/src/core/facade/collection';
+/// <reference path="../../typings/tsd.d.ts" />
+import {
+  bind,
+  Inject,
+  Injectable,
+  Binding,
+  Injector,
+  OpaqueToken
+} from 'angular2/angular2';
+
+import {
+  ListWrapper,
+  MapWrapper,
+  Map,
+  StringMapWrapper,
+  List
+} from 'angular2/src/core/facade/collection';
 import {
   DomRenderer,
+  DomRenderer_,
   RenderElementRef,
   Renderer,
   DOCUMENT
 } from 'angular2/src/core/render/render';
+import {AnimationBuilder} from 'angular2/src/animate/animation_builder';
 import {EventManager} from 'angular2/src/core/render/dom/events/event_manager';
-import {DomSharedStylesHost} from 'angular2/src/core/render/dom/view/shared_styles_host';
+import {DomSharedStylesHost} from 'angular2/src/core/render/dom/shared_styles_host';
 import {TemplateCloner} from 'angular2/src/core/render/dom/template_cloner';
-import {resolveInternalDomView} from 'angular2/src/core/render/dom/view/view';
 import {DOM} from 'angular2/src/core/dom/dom_adapter';
-import {bind, Injectable, Inject} from 'angular2/di';
+
+function resolveInternalDomView(viewRef) {
+  return viewRef;
+}
+
+function resolveInternalDomFragment(fragmentRef) {
+  return fragmentRef.nodes;
+}
+
+export {
+  DOCUMENT,
+  DomRenderer,
+  DomRenderer_
+};
 
 @Injectable()
-export class ServerDomRenderer extends DomRenderer {
+export class ServerDomRenderer_ extends DomRenderer_ {
   constructor(
     private _eventManager: EventManager,
     private _domSharedStylesHost: DomSharedStylesHost,
-    private _templateCloner: TemplateCloner,
-    @Inject(DOCUMENT) document
-  ) {
-     super(_eventManager, _domSharedStylesHost, _templateCloner, document);
+    private _animate: AnimationBuilder,
+    @Inject(DOCUMENT) document) {
+     super(_eventManager, _domSharedStylesHost, _animate, document);
   }
 
   setElementProperty(location: RenderElementRef, propertyName: string, propertyValue: any) {
@@ -52,8 +80,8 @@ export class ServerDomRenderer extends DomRenderer {
 }
 
 
-export var SERVER_DOM_RENDERER_BINDINGS = [
-  bind(ServerDomRenderer).toClass(ServerDomRenderer),
-  bind(Renderer).toClass(ServerDomRenderer)
+export const SERVER_DOM_RENDERER_BINDINGS = [
+  bind(DomRenderer).toClass(ServerDomRenderer_),
+  bind(Renderer).toClass(DomRenderer)
 ];
 
