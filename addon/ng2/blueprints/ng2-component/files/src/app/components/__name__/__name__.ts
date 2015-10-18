7977b5d1244cc243682b308c46b8a216e964e85b
Igor Minar <igor@angularjs.org>
2015-10-18 18:32:18 +0100

    fix(blueprint): fix template/css url path

--- addon/ng2/blueprints/ng2-component/files/src/app/components/__name__/__name__.ts
+++ addon/ng2/blueprints/ng2-component/files/src/app/components/__name__/__name__.ts
@@ -3,8 +3,8 @@ import {Component} from 'angular2/angular2';
 
 @Component({
   selector: '<%= dasherizedModuleName %>',
-  templateUrl: 'app/component/<%= dasherizedModuleName %>/<%= dasherizedModuleName %>.html',
-  styleUrls: ['app/component/<%= dasherizedModuleName %>/<%= dasherizedModuleName %>.css'],
+  templateUrl: 'app/components/<%= dasherizedModuleName %>/<%= dasherizedModuleName %>.html',
+  styleUrls: ['app/components/<%= dasherizedModuleName %>/<%= dasherizedModuleName %>.css'],
   providers: [],
   directives: [],
   pipes: []
