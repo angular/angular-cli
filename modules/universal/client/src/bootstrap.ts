b5640ff41f2d09445c54651c125a7722f29f2965
gdi2290 <github@gdi2290.com>
2015-10-19 18:54:14 +0100

    feat(client): preboot bootstrap

---
+++ modules/universal/client/src/bootstrap.ts
@@ -0,0 +1,17 @@
+import {Type} from 'angular2/src/core/facade/lang';
+import {Provider, bootstrap as bootstrapClient} from 'angular2/angular2';
+import {ComponentRef} from 'angular2/src/core/linker/dynamic_component_loader';
+import {Promise} from 'angular2/src/core/facade/async';
+
+export function bootstrap(appComponentType: /*Type*/ any,
+                          appProviders: Array<Type | Provider | any[]> = null):
+  Promise<ComponentRef> {
+
+  return bootstrapClient(appComponentType, appProviders)
+    .then((appRef: ComponentRef) => {
+      if ('preboot' in window) {
+        (<any>window).preboot.complete();
+      }
+      return appRef;
+    });
+}
