ed9aa0f46c86205b40c198e1d4f0739a853c58ed
gdi2290 <github@gdi2290.com>
2015-10-20 09:30:17 +0100

    refactor(universal): ng2engine default preboot

--- modules/universal/server/src/express/engine.ts
+++ modules/universal/server/src/express/engine.ts
@@ -30,59 +30,59 @@ export interface engineOptions {
   client?: boolean;
 }
 
+// export function ng2engine(filePath: string, options: engineOptions, done: Function) {
+//   // defaults
+//   options = options || <engineOptions>{};
+//   options.providers = options.providers || [];
+
+//   // read file on disk
+//   try {
+//     fs.readFile(filePath, (err, content) => {
+
+//       if (err) { return done(err); }
+
+//       // convert to string
+//       var clientHtml: string = content.toString();
+
+//       // TODO: better build scripts abstraction
+//       if (options.server === false && options.client === false) {
+//         return done(null, clientHtml);
+//       }
+//       if (options.server === false && options.client !== false) {
+//         return done(null, buildClientScripts(clientHtml, options));
+//       }
+
+//       // bootstrap and render component to string
+//       renderToString(options.App, options.providers)
+//         .then(serializedCmp => {
+
+//           let selector: string = selectorResolver(options.App);
+
+//           // selector replacer explained here
+//           // https://gist.github.com/gdi2290/c74afd9898d2279fef9f
+//           // replace our component with serialized version
+//           let rendered: string = clientHtml.replace(
+//             // <selector></selector>
+//             selectorRegExpFactory(selector),
+//             // <selector>{{ serializedCmp }}</selector>
+//             serializedCmp
+//             // TODO: serializedData
+//           );
+
+//           done(null, buildClientScripts(rendered, options));
+//         })
+//         .catch(e => {
+//           console.log(e.stack);
+//           // if server fail then return client html
+//           done(null, buildClientScripts(clientHtml, options));
+//         });
+//     });
+//   } catch (e) {
+//     done(e);
+//   }
+// };
+
 export function ng2engine(filePath: string, options: engineOptions, done: Function) {
-  // defaults
-  options = options || <engineOptions>{};
-  options.providers = options.providers || [];
-
-  // read file on disk
-  try {
-    fs.readFile(filePath, (err, content) => {
-
-      if (err) { return done(err); }
-
-      // convert to string
-      var clientHtml: string = content.toString();
-
-      // TODO: better build scripts abstraction
-      if (options.server === false && options.client === false) {
-        return done(null, clientHtml);
-      }
-      if (options.server === false && options.client !== false) {
-        return done(null, buildClientScripts(clientHtml, options));
-      }
-
-      // bootstrap and render component to string
-      renderToString(options.App, options.providers)
-        .then(serializedCmp => {
-
-          let selector: string = selectorResolver(options.App);
-
-          // selector replacer explained here
-          // https://gist.github.com/gdi2290/c74afd9898d2279fef9f
-          // replace our component with serialized version
-          let rendered: string = clientHtml.replace(
-            // <selector></selector>
-            selectorRegExpFactory(selector),
-            // <selector>{{ serializedCmp }}</selector>
-            serializedCmp
-            // TODO: serializedData
-          );
-
-          done(null, buildClientScripts(rendered, options));
-        })
-        .catch(e => {
-          console.log(e.stack);
-          // if server fail then return client html
-          done(null, buildClientScripts(clientHtml, options));
-        });
-    });
-  } catch (e) {
-    done(e);
-  }
-};
-
-export function ng2engineWithPreboot(filePath: string, options: engineOptions, done: Function) {
   // defaults
   options = options || <engineOptions>{};
   options.providers = options.providers || [];
@@ -133,6 +133,8 @@ export function ng2engineWithPreboot(filePath: string, options: engineOptions, d
     done(e);
   }
 };
+
+export const ng2engineWithPreboot = ng2engine;
 
 export function simpleReplace(filePath: string, options: engineOptions, done: Function) {
   // defaults
