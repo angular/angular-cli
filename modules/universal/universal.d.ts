a4af9cc8d2771245dc2f883f074507f07c572840
gdi2290 <github@gdi2290.com>
2015-10-20 09:46:20 +0100

    chore(universal): include type definition

---
+++ modules/universal/universal.d.ts
@@ -0,0 +1,10 @@
+declare module "angular2-universal-preview" {
+  function ng2engineWithPreboot(): any
+  function ng2engine(): any
+  function bootstrap(component: any, providers: any): any
+}
+declare module "angular2-universal" {
+  function ng2engineWithPreboot(): any
+  function ng2engine(): any
+  function bootstrap(component: any, providers: any): any
+}
