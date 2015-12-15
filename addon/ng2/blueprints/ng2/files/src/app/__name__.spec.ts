3a400f42fa71d316023c9a23dbd53b69e1811ad1
Ciro Nunes <ciroanunes@gmail.com>
2015-12-15 13:25:20 -0200

    chore: update blueprints considering `noImplicityAny`

--- addon/ng2/blueprints/ng2/files/src/app/__name__.spec.ts
+++ addon/ng2/blueprints/ng2/files/src/app/__name__.spec.ts
@@ -4,12 +4,12 @@ import {<%= jsComponentName %>App} from '../app/<%= htmlComponentName %>';
 beforeEachProviders(() => [<%= jsComponentName %>App]);
 
 describe('App: <%= jsComponentName %>', () => {
-  it('should have the `defaultMeaning` as 42', inject([<%= jsComponentName %>App], (app) => {
+  it('should have the `defaultMeaning` as 42', inject([<%= jsComponentName %>App], (app: <%= jsComponentName %>App) => {
     expect(app.defaultMeaning).toBe(42);
   }));
 
   describe('#meaningOfLife', () => {
-    it('should get the meaning of life', inject([<%= jsComponentName %>App], (app) => {
+    it('should get the meaning of life', inject([<%= jsComponentName %>App], (app: <%= jsComponentName %>App) => {
       expect(app.meaningOfLife()).toBe('The meaning of life is 42');
       expect(app.meaningOfLife(22)).toBe('The meaning of life is 22');
     }));
