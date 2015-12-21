b246f7498230e9aa2809fb0a0010a890868e2623
Ciro Nunes <ciroanunes@gmail.com>
2015-12-21 11:45:29 -0200

    feat(angular2-app): allow to easily add vendor files
    to be able to easily add vendor files in generated projects:
    
    var Angular2App = require('angular-cli/lib/broccoli/angular2-app');
    
    module.exports = function(defaults) {
      var app = new Angular2App(defaults, {
        vendorFiles: ['material-design-lite/material.css']
      });
      return app.toTree();
    }

--- addon/ng2/blueprints/ng2/files/ember-cli-build.js
+++ addon/ng2/blueprints/ng2/files/ember-cli-build.js
@@ -3,6 +3,8 @@
 var Angular2App = require('angular-cli/lib/broccoli/angular2-app');
 
 module.exports = function(defaults) {
-  var app = new Angular2App(defaults);
+  var app = new Angular2App(defaults, {
+    vendorFiles: []
+  });
   return app.toTree();
 }
