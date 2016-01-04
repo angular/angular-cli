78f83c308775de5d003e520d8ded79dc4e379682
gdi2290 <github@gdi2290.com>
2016-01-03 23:21:13 -0800

    chore(universal): correctly export and update deps

--- modules/universal/server/server.ts
+++ modules/universal/server/server.ts
@@ -1,14 +1,12 @@
 import './src/server_patch';
-export * from './src/core/application';
-export * from './src/core/application_ref';
-export * from './src/core/application_common';
-export * from './src/core/platform_providers';
 
 export * from './src/directives/server_form';
 
 export * from './src/express/engine';
 
 export * from './src/http/server_http';
+
+export * from './src/platform/node';
 
 export * from './src/render/server_dom_renderer';
 
