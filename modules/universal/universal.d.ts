4ed1f0f26dc09a75c586bcdc883a41d22bf0fa53
playground <jeff.lu@weather.com>
2016-01-08 18:55:37 -0500

    fix(typing): universal.d.ts

--- modules/universal/universal.d.ts
+++ modules/universal/universal.d.ts
@@ -1,3 +1,6 @@
 export function ng2engineWithPreboot(): any;
 export function ng2engine(): any;
 export function bootstrap(component: any, providers: any): any;
+export var SERVER_LOCATION_PROVIDERS: [any];
+export var BASE_URL: string;
+
