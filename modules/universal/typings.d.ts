172592c4945bd6f146fdf79cf009be7964783e94
gdi2290 <github@gdi2290.com>
2016-01-07 16:45:48 -0800

    chore: include typings

---
+++ modules/universal/typings.d.ts
@@ -0,0 +1,3 @@
+declare function ng2engineWithPreboot(): any;
+declare function ng2engine(): any;
+declare function bootstrap(component: any, providers: any): any;
--- modules/universal/universal.d.ts
+++ modules/universal/universal.d.ts
@@ -1,10 +1,10 @@
 declare module "angular2-universal-preview" {
-  function ng2engineWithPreboot(): any
-  function ng2engine(): any
-  function bootstrap(component: any, providers: any): any
+  function ng2engineWithPreboot(): any;
+  function ng2engine(): any;
+  function bootstrap(component: any, providers: any): any;
 }
 declare module "angular2-universal" {
-  function ng2engineWithPreboot(): any
-  function ng2engine(): any
-  function bootstrap(component: any, providers: any): any
+  function ng2engineWithPreboot(): any;
+  function ng2engine(): any;
+  function bootstrap(component: any, providers: any): any;
 }
