5898205c161c85f735f3402134d2d160db5ef389
Sebastian Sobociński <Sebastian Sobociski>
2016-01-10 13:05:25 +0100

    fix(typing): universal.d.ts

--- modules/universal/universal.d.ts
+++ modules/universal/universal.d.ts
@@ -3,4 +3,4 @@ export function ng2engine(): any;
 export function bootstrap(component: any, providers: any): any;
 export var SERVER_LOCATION_PROVIDERS: [any];
 export var BASE_URL: string;
-
+export var HTTP_PROVIDERS: [any];
