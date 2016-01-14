ed1cac7edb524f7309e45c0ccf9f5491d27996e1
gdi2290 <github@gdi2290.com>
2016-01-14 15:56:25 -0800

    style(universal): include more type info

--- modules/universal/universal.d.ts
+++ modules/universal/universal.d.ts
@@ -1,6 +1,11 @@
 export function ng2engineWithPreboot(): any;
 export function ng2engine(): any;
 export function bootstrap(component: any, providers: any): any;
+export function renderToString(AppComponent: any, serverProviders?: any): Promise<string>;
+export function renderToStringWithPreboot(AppComponent: any, serverProviders?: any, prebootConfig: any): Promise<string>;
+export function appRefSyncRender(appRef: any): string;
+export function serializeApplication(element: any, styles: string[], cache?: any): string;
+export function selectorResolver(componentType: any): string;
 export var SERVER_LOCATION_PROVIDERS: [any];
 export var BASE_URL: string;
 export var HTTP_PROVIDERS: [any];
