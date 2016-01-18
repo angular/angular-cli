8ac0bb6a6649a71ea55d075d4af39ed07eeb7c8e
gdi2290 <github@gdi2290.com>
2016-01-17 23:22:44 -0800

    style(universal.d.ts): include more type/helpers

--- modules/universal/universal.d.ts
+++ modules/universal/universal.d.ts
@@ -8,22 +8,36 @@ export class NodeBackend {
 export function ng2engineWithPreboot(): any;
 export function ng2engine(): any;
 export function bootstrap(component: any, providers?: any): any;
+export function prebootComplete(value?: any): any;
 export function renderToString(AppComponent: any, serverProviders?: any): Promise<string>;
-export function renderToStringWithPreboot(AppComponent: any, serverProviders?: any, prebootConfig?: any): Promise<string>;
+export function renderToStringWithPreboot(
+  AppComponent: any,
+  serverProviders?: any,
+  prebootConfig?: any): Promise<string>;
 export function appRefSyncRender(appRef: any): string;
 export function serializeApplication(element: any, styles: string[], cache?: any): string;
 export function selectorResolver(componentType: any): string;
 export function stringifyElement(el: any): string;
 export var SERVER_LOCATION_PROVIDERS: [any];
 export var BASE_URL: string;
+export function buildBaseUrl(url: string, existing?: boolean): any;
 export var HTTP_PROVIDERS: [any];
+export var NODE_PROVIDERS: [any];
+export function buildNodeAppProviders(appComponentType: any, providers?: Array<any>): Array<any>;
+export function buildNodePlatformProviders(
+  appComponentType: any,
+  providers?: Array<any>): Array<any>;
+export var NODE_APPLICATION_COMMON_PROVIDERS: [any];
+export var NODE_APPLICATION_PROVIDERS: [any];
 
 export default {
+  provide,
   NodeXhr,
   NodeBackend,
   ng2engineWithPreboot,
   ng2engine,
   bootstrap,
+  prebootComplete,
   renderToString,
   renderToStringWithPreboot,
   appRefSyncRender,
@@ -32,5 +46,11 @@ export default {
   stringifyElement,
   SERVER_LOCATION_PROVIDERS,
   BASE_URL,
-  HTTP_PROVIDERS
+  buildBaseUrl,
+  HTTP_PROVIDERS,
+  buildNodeAppProviders,
+  buildNodePlatformProviders,
+  NODE_PROVIDERS,
+  NODE_APPLICATION_COMMON_PROVIDERS,
+  NODE_APPLICATION_PROVIDERS
 }
