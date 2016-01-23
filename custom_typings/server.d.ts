8e4fa6f1a6797358d8820d7e6fad04231b42b1d7
gdi2290 <github@gdi2290.com>
2016-01-22 18:45:08 -0800

    chore: remove old types

--- custom_typings/server.d.ts
+++ custom_typings/server.d.ts
@@ -1,11 +1,6 @@
 
 declare module "xhr2" {
-  class XMLHttpRequest {
+  export class XMLHttpRequest {
     nodejsSet(url: any): any;
   }
-  export = XMLHttpRequest;
-}
-
-declare module "angular2_server" {
-  function bootstrap(appComponentType: any, appInjector: any, componentInjectableBindings?: Array<any>, errorReporter?: Function): any;
 }
