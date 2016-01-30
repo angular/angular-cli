b0f897f110b2b4644561289c75f30fdaf63d3997
gdi2290 <github@gdi2290.com>
2016-01-29 20:41:06 -0800

    chore(gulp-prerender): initial source

---
+++ modules/angular2-gulp-prerender/index.js
@@ -0,0 +1,40 @@
+var universal = require('angular2-universal-preview');
+var gulp = require('gulp');
+var through = require('through2');
+var gutil = require('gutil');
+
+var PluginError = gutil.PluginError;
+
+module.exports = function gulpAngular2Render(config) {
+  return through.obj(function transform(file, enc, cb) {
+    if (file.isStream()) {
+      return cb(new PluginError('angular2-gulp-prerender', 'Streaming is not supported'));
+    }
+    var str = file.contents.toString();
+
+    var renderPromise = universal.renderToString;
+    var args = [config.App, config.providers];
+    if (config.preboot) {
+      renderPromise = universal.renderToStringWithPreboot;
+      args.push(config.preboot);
+    }
+
+    renderPromise.apply(null, args)
+    .then(function(serializedApp) {
+
+      file.path = gutil.replaceExtension(file.path, '.html');
+
+      var html = str.replace(
+        // <selector></selector>
+        selectorRegExpFactory(selectorResolver(config.App)),
+        // <selector>{{ serializedCmp }}</selector>
+        serializedApp
+      );
+
+      file.contents = new Buffer(html);
+
+      cb(null, file)
+    });
+  });
+}
+
