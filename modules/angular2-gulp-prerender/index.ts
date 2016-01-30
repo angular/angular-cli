dfc29c89d6ed708e3b8bfa154376c8ed94f87458
Wassim Chegham <maneki.nekko@gmail.com>
2016-01-30 22:20:03 +0100

    refactor(angular2-gulp-prerender): update angular2-gulp-prerender to typescript

---
+++ modules/angular2-gulp-prerender/index.ts
@@ -0,0 +1,4 @@
+import * as GulpAngular2Render from './src/prerender';
+
+export * from './src/prerender';
+export default GulpAngular2Render;
---
+++ modules/angular2-gulp-prerender/src/prerender.ts
@@ -0,0 +1,48 @@
+import universal = require('angular2-universal-preview');
+import gulp = require('gulp');
+import through = require('through2');
+import gutil = require('gutil');
+
+let PluginError = gutil.PluginError;
+
+export interface IUniversalConfig {
+  preboot: boolean;
+  App: any;
+  providers: any[];
+}
+
+export class GulpAngular2Render {
+
+  constructor(config: IUniversalConfig) {
+    
+    return through.obj((file: any, enc: string, cb: Function) => {
+
+      if (file.isStream()) {
+        return cb(new PluginError('angular2-gulp-prerender', 'Streaming is not supported'));
+      }
+      let str: string = file.contents.toString();
+
+      let renderPromise = universal.renderToString;
+      let args = [config.App, config.providers];
+
+      if (config.preboot) {
+        renderPromise = universal.renderToStringWithPreboot;
+        args.push(config.preboot);
+      }
+
+      renderPromise.apply(null, args)
+        .then((serializedApp) => {
+          let html = str.replace(
+            // <selector></selector>
+            universal.selectorRegExpFactory(universal.selectorResolver(config.App)),
+            // <selector>{{ serializedCmp }}</selector>
+            serializedApp
+          );
+
+          file.contents = new Buffer(html);
+
+          cb(null, file)
+        });
+    });
+  }
+}
