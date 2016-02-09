f673988ca40e33bb43019519ef93c70ea36cda78
gdi2290 <github@gdi2290.com>
2016-02-09 15:52:18 -0800

    feat(universal): provide no-patch path

---
+++ modules/universal/server/no-patch.ts
@@ -0,0 +1,16 @@
+export {provide} from 'angular2/core';
+export * from './src/directives/server_form';
+
+export * from './src/http/server_http';
+
+export * from './src/platform/node';
+export * from './src/platform/node_xhr_impl';
+export * from './src/platform/dom/server_dom_renderer';
+
+export * from './src/router/index';
+
+export * from './src/helper';
+export * from './src/ng_preboot';
+export * from './src/render';
+export * from './src/stringifyElement';
+export * from 'angular2-express-engine';
