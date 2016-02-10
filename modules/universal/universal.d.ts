31882a5f9874fe110b5d3ea0fb5b5ac2d84c714d
gdi2290 <github@gdi2290.com>
2016-02-10 13:47:17 -0800

    refactor(universal): update type definitions

--- modules/universal/universal.d.ts
+++ modules/universal/universal.d.ts
@@ -7,19 +7,24 @@ export class NodeBackend {
 }
 export function bootstrap(component: any, providers?: any): any;
 export function prebootComplete(value?: any): any;
-export function renderToString(AppComponent: any, serverProviders?: any): Promise<string>;
+export function renderToString(AppComponent: any, nodeProviders?: any): Promise<string>;
 export function renderToStringWithPreboot(
   AppComponent: any,
-  serverProviders?: any,
+  nodeProviders?: any,
   prebootConfig?: any): Promise<string>;
 export function appRefSyncRender(appRef: any): string;
 export function serializeApplication(element: any, styles: string[], cache?: any): string;
 export function selectorResolver(componentType: any): string;
 export function stringifyElement(el: any): string;
+
+// TODO(gdi2290): remove
 export var SERVER_LOCATION_PROVIDERS: [any];
+export var HTTP_PROVIDERS: [any];
+
+export var NODE_LOCATION_PROVIDERS: [any];
 export var REQUEST_URL: string;
 export function buildBaseUrl(url: string, existing?: boolean): any;
-export var HTTP_PROVIDERS: [any];
+export var NODE_HTTP_PROVIDERS: [any];
 export var NODE_PROVIDERS: [any];
 export function buildNodeAppProviders(appComponentType: any, providers?: Array<any>): Array<any>;
 export function buildNodePlatformProviders(
@@ -29,7 +34,25 @@ export var NODE_APPLICATION_COMMON_PROVIDERS: [any];
 export var NODE_APPLICATION_PROVIDERS: [any];
 export function selectorRegExpFactory(selector: string): RegExp;
 export function ng2engine(): any;
+export function parseDocument(documentHtml: string): Object;
+export function serializeDocument(document: Object): string;
+export function addPrebootHtml(AppComponent: any, html: any, prebootConfig?: any): any;
+export function renderDocument(
+  documentHtml: string,
+  componentType: Type,
+  nodeProviders?: any): Promise<string>;
+export function renderDocumentWithPreboot(
+  documentHtml: string,
+  componentType: Type,
+  nodeProviders?: any,
+  prebootConfig: any = {}
+): Promise<string>;
 export default {
+  renderDocumentWithPreboot,
+  renderDocument,
+  addPrebootHtml,
+  serializeDocument,
+  parseDocument,
   ng2engine,
   provide,
   NodeXhr,
@@ -42,10 +65,15 @@ export default {
   serializeApplication,
   selectorResolver,
   stringifyElement,
+
   SERVER_LOCATION_PROVIDERS,
+  HTTP_PROVIDERS,
+
+
+  NODE_LOCATION_PROVIDERS,
   REQUEST_URL,
   buildBaseUrl,
-  HTTP_PROVIDERS,
+  NODE_HTTP_PROVIDERS,
   buildNodeAppProviders,
   buildNodePlatformProviders,
   NODE_PROVIDERS,
