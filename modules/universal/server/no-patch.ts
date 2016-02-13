90f8e29b68012708a4dc43715fd3a408ba24b2d9
gdi2290 <github@gdi2290.com>
2016-02-12 17:25:10 -0800

    style(universal): different order

--- modules/universal/server/no-patch.ts
+++ modules/universal/server/no-patch.ts
@@ -1,12 +1,13 @@
 export {provide} from 'angular2/core';
+
 export * from './src/directives/node_form';
 
 export * from './src/http/node_http';
 
-export * from './src/platform/node';
-export * from './src/platform/document';
-export * from './src/platform/node_xhr_impl';
 export * from './src/platform/dom/node_dom_renderer';
+export * from './src/platform/document';
+export * from './src/platform/node';
+export * from './src/platform/node_xhr_impl';
 
 export * from './src/router/index';
 
