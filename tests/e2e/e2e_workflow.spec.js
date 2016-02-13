a61d821652d53c97dfe875caa81dd96aeab1d614
Filipe Silva <filipematossilva@gmail.com>
2016-02-13 01:02:47 +0000

    remove commented tests

--- tests/e2e/e2e_workflow.spec.js
+++ tests/e2e/e2e_workflow.spec.js
@@ -43,143 +43,143 @@ describe('Basic end-to-end Workflow', function () {
     sh.exec('npm link ../../angular-cli', { silent: true });
     expect(path.basename(process.cwd())).to.equal('test-project');
   });
-  //
-  // it('Can run `ng build` in created project', function() {
-  //   this.timeout(10000);
-  //
-  //   return ng([
-  //     'build',
-  //     '--silent'
-  //   ]).then(function() {
-  //     expect(fs.existsSync(path.join(process.cwd(), 'dist')));
-  //   });
-  // });
-  //
-  // it('Perform `ng test`', function(done) {
-  //   this.timeout(30000);
-  //
-  //   return ng([
-  //     'test'
-  //   ]).then(function(err) {
-  //     // TODO when `ng test` will be implemented
-  //     //expect(err).to.be.equal(1);
-  //     done();
-  //   });
-  // });
-  //
-  // it('Can create a test component using `ng generate component test-component`', function() {
-  //   return ng([
-  //     'generate',
-  //     'component',
-  //     'test-component'
-  //   ]).then(function() {
-  //     var componentDir = path.join(process.cwd(), 'src', 'app', 'components', 'test-component');
-  //     expect(fs.existsSync(componentDir));
-  //     expect(fs.existsSync(path.join(componentDir, 'test-component.ts')));
-  //     expect(fs.existsSync(path.join(componentDir, 'test-component.html')));
-  //     expect(fs.existsSync(path.join(componentDir, 'test-component.css')));
-  //   });
-  // });
-  //
-  // it('Perform `ng test`', function(done) {
-  //   this.timeout(30000);
-  //
-  //   return ng([
-  //     'test'
-  //   ]).then(function(err) {
-  //     // TODO when `ng test` will be implemented
-  //     //expect(err).to.be.equal(1);
-  //     done();
-  //   });
-  // });
-  //
-  // it('Can create a test service using `ng generate service test-service`', function() {
-  //   return ng([
-  //     'generate',
-  //     'service',
-  //     'test-service'
-  //   ]).then(function() {
-  //     var serviceDir = path.join(process.cwd(), 'src', 'app', 'services', 'test-service');
-  //     expect(fs.existsSync(serviceDir));
-  //     expect(fs.existsSync(path.join(serviceDir, 'test-service.ts')));
-  //     expect(fs.existsSync(path.join(serviceDir, 'test-service.spec.ts')));
-  //   });
-  // });
-  //
-  // it('Perform `ng test`', function(done) {
-  //   this.timeout(30000);
-  //
-  //   return ng([
-  //     'test'
-  //   ]).then(function(err) {
-  //     // TODO when `ng test` will be implemented
-  //     //expect(err).to.be.equal(1);
-  //     done();
-  //   });
-  // });
-  //
-  // it('Can create a test pipe using `ng generate pipe test-pipe`', function() {
-  //   return ng([
-  //     'generate',
-  //     'pipe',
-  //     'test-pipe'
-  //   ]).then(function() {
-  //     var pipeDir = path.join(process.cwd(), 'src', 'app', 'pipes', 'test-pipe');
-  //     expect(fs.existsSync(pipeDir));
-  //     expect(fs.existsSync(path.join(pipeDir, 'test-pipe.ts')));
-  //     expect(fs.existsSync(path.join(pipeDir, 'test-pipe.spec.ts')));
-  //   });
-  // });
-  //
-  // it('Perform `ng test`', function(done) {
-  //   this.timeout(30000);
-  //
-  //   return ng([
-  //     'test'
-  //   ]).then(function(err) {
-  //     // TODO when `ng test` will be implemented
-  //     //expect(err).to.be.equal(1);
-  //     done();
-  //   });
-  // });
-  //
-  // it('Can create a test route using `ng generate route test-route`', function() {
-  //   return ng([
-  //     'generate',
-  //     'route',
-  //     'test-route'
-  //   ]).then(function() {
-  //     var routeDir = path.join(process.cwd(), 'src', 'app', 'test-route');
-  //     expect(fs.existsSync(routeDir));
-  //     expect(fs.existsSync(path.join(routeDir, 'test-pipe-detail.component.css')));
-  //     expect(fs.existsSync(path.join(routeDir, 'test-pipe-detail.component.html')));
-  //     expect(fs.existsSync(path.join(routeDir, 'test-pipe-detail.component.spec.ts')));
-  //     expect(fs.existsSync(path.join(routeDir, 'test-pipe-detail.component.ts')));
-  //     expect(fs.existsSync(path.join(routeDir, 'test-pipe-list.component.css')));
-  //     expect(fs.existsSync(path.join(routeDir, 'test-pipe-list.component.html')));
-  //     expect(fs.existsSync(path.join(routeDir, 'test-pipe-list.component.spec.ts')));
-  //     expect(fs.existsSync(path.join(routeDir, 'test-pipe-list.component.ts')));
-  //     expect(fs.existsSync(path.join(routeDir, 'test-pipe-root.component.ts')));
-  //     expect(fs.existsSync(path.join(routeDir, 'test-pipe-root.service.spec.ts')));
-  //     expect(fs.existsSync(path.join(routeDir, 'test-pipe-root.service.ts')));
-  //   });
-  // });
-  //
-  // it('Perform `ng test`', function(done) {
-  //   this.timeout(300000);
-  //
-  //   return ng([
-  //     'test'
-  //   ]).then(function(err) {
-  //     // TODO when `ng test` will be implemented
-  //     //expect(err).to.be.equal(1);
-  //     // Clean `tmp` folder
-  //
-  //     process.chdir(path.resolve(root, '..'));
-  //     sh.rm('-rf', './tmp'); // tmp.teardown takes too long
-  //
-  //     done();
-  //   });
-  // });
+  
+  it('Can run `ng build` in created project', function() {
+    this.timeout(10000);
+
+    return ng([
+      'build',
+      '--silent'
+    ]).then(function() {
+      expect(fs.existsSync(path.join(process.cwd(), 'dist')));
+    });
+  });
+
+  it('Perform `ng test`', function(done) {
+    this.timeout(30000);
+
+    return ng([
+      'test'
+    ]).then(function(err) {
+      // TODO when `ng test` will be implemented
+      //expect(err).to.be.equal(1);
+      done();
+    });
+  });
+
+  it('Can create a test component using `ng generate component test-component`', function() {
+    return ng([
+      'generate',
+      'component',
+      'test-component'
+    ]).then(function() {
+      var componentDir = path.join(process.cwd(), 'src', 'app', 'components', 'test-component');
+      expect(fs.existsSync(componentDir));
+      expect(fs.existsSync(path.join(componentDir, 'test-component.ts')));
+      expect(fs.existsSync(path.join(componentDir, 'test-component.html')));
+      expect(fs.existsSync(path.join(componentDir, 'test-component.css')));
+    });
+  });
+
+  it('Perform `ng test`', function(done) {
+    this.timeout(30000);
+
+    return ng([
+      'test'
+    ]).then(function(err) {
+      // TODO when `ng test` will be implemented
+      //expect(err).to.be.equal(1);
+      done();
+    });
+  });
+
+  it('Can create a test service using `ng generate service test-service`', function() {
+    return ng([
+      'generate',
+      'service',
+      'test-service'
+    ]).then(function() {
+      var serviceDir = path.join(process.cwd(), 'src', 'app', 'services', 'test-service');
+      expect(fs.existsSync(serviceDir));
+      expect(fs.existsSync(path.join(serviceDir, 'test-service.ts')));
+      expect(fs.existsSync(path.join(serviceDir, 'test-service.spec.ts')));
+    });
+  });
+
+  it('Perform `ng test`', function(done) {
+    this.timeout(30000);
+
+    return ng([
+      'test'
+    ]).then(function(err) {
+      // TODO when `ng test` will be implemented
+      //expect(err).to.be.equal(1);
+      done();
+    });
+  });
+
+  it('Can create a test pipe using `ng generate pipe test-pipe`', function() {
+    return ng([
+      'generate',
+      'pipe',
+      'test-pipe'
+    ]).then(function() {
+      var pipeDir = path.join(process.cwd(), 'src', 'app', 'pipes', 'test-pipe');
+      expect(fs.existsSync(pipeDir));
+      expect(fs.existsSync(path.join(pipeDir, 'test-pipe.ts')));
+      expect(fs.existsSync(path.join(pipeDir, 'test-pipe.spec.ts')));
+    });
+  });
+
+  it('Perform `ng test`', function(done) {
+    this.timeout(30000);
+
+    return ng([
+      'test'
+    ]).then(function(err) {
+      // TODO when `ng test` will be implemented
+      //expect(err).to.be.equal(1);
+      done();
+    });
+  });
+
+  it('Can create a test route using `ng generate route test-route`', function() {
+    return ng([
+      'generate',
+      'route',
+      'test-route'
+    ]).then(function() {
+      var routeDir = path.join(process.cwd(), 'src', 'app', 'test-route');
+      expect(fs.existsSync(routeDir));
+      expect(fs.existsSync(path.join(routeDir, 'test-pipe-detail.component.css')));
+      expect(fs.existsSync(path.join(routeDir, 'test-pipe-detail.component.html')));
+      expect(fs.existsSync(path.join(routeDir, 'test-pipe-detail.component.spec.ts')));
+      expect(fs.existsSync(path.join(routeDir, 'test-pipe-detail.component.ts')));
+      expect(fs.existsSync(path.join(routeDir, 'test-pipe-list.component.css')));
+      expect(fs.existsSync(path.join(routeDir, 'test-pipe-list.component.html')));
+      expect(fs.existsSync(path.join(routeDir, 'test-pipe-list.component.spec.ts')));
+      expect(fs.existsSync(path.join(routeDir, 'test-pipe-list.component.ts')));
+      expect(fs.existsSync(path.join(routeDir, 'test-pipe-root.component.ts')));
+      expect(fs.existsSync(path.join(routeDir, 'test-pipe-root.service.spec.ts')));
+      expect(fs.existsSync(path.join(routeDir, 'test-pipe-root.service.ts')));
+    });
+  });
+
+  it('Perform `ng test`', function(done) {
+    this.timeout(300000);
+
+    return ng([
+      'test'
+    ]).then(function(err) {
+      // TODO when `ng test` will be implemented
+      //expect(err).to.be.equal(1);
+      // Clean `tmp` folder
+
+      process.chdir(path.resolve(root, '..'));
+      sh.rm('-rf', './tmp'); // tmp.teardown takes too long
+
+      done();
+    });
+  });
 
 });
