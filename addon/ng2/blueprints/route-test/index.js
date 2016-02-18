52b1b21855669d4c9f3e5e8d33fc65bf04d5d983
Mike Brocchi <mbrocchi@gmail.com>
2016-02-17 22:15:20 -0500

    fix(test blueprints): Prevent unwanted ember service and route test generation
    The ember-cli generates blueprints for  when generating a new service & route
    By overriding  in  it prevents the ember service & route test generation
    Fixes #202

---
+++ addon/ng2/blueprints/route-test/index.js
@@ -0,0 +1,14 @@
+var stringUtils = require('ember-cli/lib/utilities/string');
+
+module.exports = {
+  description: '',
+  
+  // ******************************************************
+  // ******************************************************
+  // LEAVE THIS HERE
+  // Must override install to prevent ember's route tests
+  // ******************************************************
+  // ******************************************************
+  
+  install: function(options){}
+};
