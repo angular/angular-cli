4256587bb6f38ca9d1510e52a36db11e80ae34cd
gdi2290 <github@gdi2290.com>
2016-02-28 17:24:25 -0800

    fix(examples): correct type

--- examples/app/server/express/server.ts
+++ examples/app/server/express/server.ts
@@ -5,20 +5,16 @@ var path = require('path');
 
 module.exports = function(ROOT) {
   var app = express();
-  var {ng2ExpressEngine} = require('angular2-universal-preview');
+  var {expressEngine} = require('angular2-universal-preview');
   // rendering engine
-
-  app.engine('ng2.html', ng2ExpressEngine);
+  app.engine('ng2.html', expressEngine);
   app.set('views', path.join(ROOT, 'examples'));
   app.set('view engine', 'ng2.html');
   app.set('view options', { doctype: 'html' });
 
-  console.log(__dirname);
-
   var routes = require('./routes');
   var api = require('./api');
   var graphApi = require('./graph_api');
-
 
   app.use(serveStatic(`${ROOT}/dist`));
   app.use(serveStatic(`${ROOT}/examples/app/public`));
