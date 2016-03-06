c77d26133f1f104f84731ae57bda475e8090ec83
gdi2290 <github@gdi2290.com>
2016-03-05 21:38:54 -0800

    style(typings): ObjectConstructor assign

--- custom_typings/_custom.d.ts
+++ custom_typings/_custom.d.ts
@@ -3,3 +3,14 @@
 /// <reference path="gulp-insert.d.ts" />
 /// <reference path="gulp-uglify.d.ts" />
 /// <reference path="vinyl-buffer.d.ts" />
+
+interface ObjectConstructorAssign {
+  assign(): any;
+  assign(obj): any;
+  assign(obj, obj1): any;
+  assign(obj, obj1, obj2): any;
+  assign(obj, obj1, obj2, obj3): any;
+}
+interface ObjectConstructor extends ObjectConstructorAssign {
+
+}
