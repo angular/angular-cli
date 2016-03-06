3da38a53358818e24dfec048761428ea63bc2f51
gdi2290 <github@gdi2290.com>
2016-03-05 20:21:34 -0800

    feat(universal): minify ng_preboot css

--- modules/universal/server/src/ng_preboot.ts
+++ modules/universal/server/src/ng_preboot.ts
@@ -33,7 +33,13 @@ export function createPrebootCode(componentType: any | Array<any>, prebootConfig
   return preboot.getBrowserCode(config).then(code => createPrebootHTML(code, config));
 }
 
-export function getPrebootCSS(): string {
+export function getPrebootCSS(min?: boolean): string {
+  if (min) {
+    /* tslint:disable */
+    // https://cssminifier.com/
+    return `.preboot-overlay{background:grey;opacity:.27}@keyframes spin{to{transform:rotate(1turn)}}.preboot-spinner{position:relative;display:inline-block;width:5em;height:5em;margin:0 .5em;font-size:12px;text-indent:999em;overflow:hidden;animation:spin 1s infinite steps(8)}.preboot-spinner.small{font-size:6px}.preboot-spinner.large{font-size:24px}.preboot-spinner:after,.preboot-spinner:before,.preboot-spinner>div:after,.preboot-spinner>div:before{content:'';position:absolute;top:0;left:2.25em;width:.5em;height:1.5em;border-radius:.2em;box-shadow:0 3.5em #eee;transform-origin:50% 2.5em}.preboot-spinner:before{background:#555}.preboot-spinner:after{transform:rotate(-45deg);background:#777}.preboot-spinner>div:before{transform:rotate(-90deg);background:#999}.preboot-spinner>div:after{transform:rotate(-135deg);background:#bbb}`;
+    /* tslint:enable */
+  }
   return `
 .preboot-overlay {
   background: grey;
@@ -107,7 +113,7 @@ export function createPrebootHTML(code: string, config?: any): string {
 
   html += `
     <style>
-    ${ getPrebootCSS() }
+    ${ getPrebootCSS(config && config.uglify) }
     </style>
   `;
 
