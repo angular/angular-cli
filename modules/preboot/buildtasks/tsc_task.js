42bdc2da5cef6d479b6cc389af627c7b58cb4850
gdi2290 <github@gdi2290.com>
2016-03-06 21:18:52 -0800

    chore(preboot): correct ts declaration

--- modules/preboot/buildtasks/tsc_task.js
+++ modules/preboot/buildtasks/tsc_task.js
@@ -6,8 +6,10 @@ var path = require('path');
 module.exports = function (opts) {
   gulp.task('tsc', [ 'clean' ], function () {
     var tsConfig = path.join(opts.rootDir, 'tsconfig.json');
-    var tsProject = typescript.createProject(tsConfig);
-    
+    var tsProject = typescript.createProject(tsConfig, {
+      declaration: true
+    });
+    // TODO(gdi2290): missing declaration file stream
     return tsProject.src().
       pipe(typescript(tsProject)).
       pipe(size()).
