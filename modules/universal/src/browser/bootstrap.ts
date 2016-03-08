070c44af6b9034fbfe048c10c0c1234908e8209b
gdi2290 <github@gdi2290.com>
2016-03-07 17:29:19 -0800

    refactor(universal/browser): reuse prebootComplete

--- modules/universal/src/browser/bootstrap.ts
+++ modules/universal/src/browser/bootstrap.ts
@@ -3,25 +3,21 @@ import {Provider} from 'angular2/core';
 import {bootstrap as bootstrapClient} from 'angular2/platform/browser';
 import {ComponentRef} from 'angular2/src/core/linker/dynamic_component_loader';
 
-export function bootstrap(appComponentType: /*Type*/ any,
-                          appProviders: Array<Type | Provider | any | any[]> = null):
-  Promise<ComponentRef> {
-
-  return bootstrapClient(appComponentType, appProviders)
-    .then((appRef: ComponentRef) => {
-      if ('preboot' in window) {
-        (<any>window).preboot.complete();
-      }
-      return appRef;
-    });
-}
-
-
 export function prebootComplete(value?: any) {
   if ('preboot' in window) {
     (<any>window).preboot.complete();
   }
   return value;
 }
+
+export function bootstrap(appComponentType: /*Type*/ any,
+                          appProviders: Array<Type | Provider | any | any[]> = null):
+  Promise<ComponentRef> {
+
+  return bootstrapClient(appComponentType, appProviders)
+    .then(prebootComplete);
+}
+
+
 
 
