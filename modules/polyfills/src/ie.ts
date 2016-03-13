9891dbe9496c14a09598b7993ae27844198e2f6d
gdi2290 <github@gdi2290.com>
2016-03-13 12:30:14 -0700

    feat: ie env

---
+++ modules/polyfills/src/ie.ts
@@ -0,0 +1,9 @@
+// polyfills
+import 'es6-promise';
+import 'es6-shim';
+import 'ie-shim';
+// typescript emit metadata
+import 'reflect-metadata';
+// zone.js to track promises
+import 'zone.js/dist/zone-microtask';
+import 'zone.js/dist/long-stack-trace-zone';
