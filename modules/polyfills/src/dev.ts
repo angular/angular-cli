b62bcb24d2bea5064c7c19a389e94d0658c1a04d
gdi2290 <github@gdi2290.com>
2016-03-13 12:28:34 -0700

    feat: dev env

---
+++ modules/polyfills/src/dev.ts
@@ -0,0 +1,8 @@
+// polyfills
+import 'es6-promise';
+import 'es6-shim';
+// typescript emit metadata
+import 'reflect-metadata';
+// zone.js to track promises
+import 'zone.js/dist/zone-microtask';
+import 'zone.js/dist/long-stack-trace-zone';
