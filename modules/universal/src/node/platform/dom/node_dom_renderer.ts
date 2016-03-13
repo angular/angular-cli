4b49f3bd39696bda0f2dc705075b7789d904937b
gdi2290 <github@gdi2290.com>
2016-03-13 15:29:59 -0700

    feat(NodeDomRenderer): include more attributes from properties

--- modules/universal/src/node/platform/dom/node_dom_renderer.ts
+++ modules/universal/src/node/platform/dom/node_dom_renderer.ts
@@ -3,6 +3,7 @@ import {
   isBlank,
   stringify
 } from 'angular2/src/facade/lang';
+import {ListWrapper} from 'angular2/src/facade/collection';
 import {
   provide,
   Inject,
@@ -39,41 +40,217 @@ export class NodeDomRootRenderer_ extends DomRootRenderer {
   }
 }
 
+export const ATTRIBUTES = {
+  textarea: [
+    'autocapitalize',
+    'autocomplete',
+    'autofocus',
+    'cols',
+    'disabled',
+    'form',
+    'maxlength',
+    'minlength',
+    'name',
+    'placeholder',
+    'readonly',
+    'required',
+    'rows',
+    'selectionDirection',
+    'selectionEnd',
+    'selectionStart',
+    'spellcheck',
+    'wrap'
+  ],
+  script: [
+    'async',
+    'integrity',
+    'src',
+    'type',
+    'text',
+    'defer',
+    'crossorigin'
+  ],
+  button: [
+    'autofocus',
+    'autocomplete',
+    'disabled',
+    'form',
+    'formaction',
+    'formenctype',
+    'formmethod',
+    'formnovalidate',
+    'formtarget',
+    'name',
+    'type',
+    'value'
+  ],
+  fieldset: [
+    'disabled',
+    'form',
+    'name'
+  ],
+  a: [
+    'download',
+    'href',
+    'hreflang',
+    'ping',
+    'referrerpolicy',
+    'rel',
+    'target',
+    'type'
+  ],
+  input: [
+    'id',
+
+    'type',
+    'accept',
+    'mozactionhint',
+    'autocapitalize',
+    'autocomplete',
+    'autocorrect',
+    'autofocus',
+    'autosave',
+    'checked',
+    'disabled',
+    'form',
+    'formaction',
+    'formenctype',
+    'formmethod',
+    'formnovalidate',
+    'formtarget',
+    'height',
+    'incremental',
+    'inputmode',
+    'list',
+    'max',
+    'maxlength',
+    'min',
+    'minlength',
+    'multiple',
+    'name',
+    'pattern',
+    'placeholder',
+    'readonly',
+    'required',
+    'results',
+    'selectionDirection',
+    'size',
+    'spellcheck',
+    'src',
+    'step',
+    'tabindex',
+    'value',
+    'width',
+    'x-moz-errormessage'
+  ],
+  output: [
+    'for',
+    'form',
+    'name'
+  ],
+  progress: [
+    'max',
+    'value'
+  ],
+  label: [
+    'accesskey',
+    'for',
+    'form'
+  ],
+  option: [
+    'disabled',
+    'label',
+    'selected',
+    'value'
+  ],
+  select: [
+    'autofocus',
+    'disabled',
+    'multiple',
+    'form',
+    'multiple',
+    'name',
+    'required',
+    'size'
+  ],
+  optgroup: [
+    'disabled',
+    'label'
+  ],
+  form: [
+    'accept-charset',
+    'action',
+    'autocapitalize',
+    'autocomplete',
+    'enctype',
+    'method',
+    'name',
+    'novalidate',
+    'target'
+  ]
+}
 
 export class NodeDomRenderer extends DomRenderer {
   constructor(_rootRenderer: DomRootRenderer | any, _componentProto: RenderComponentType) {
-
     if (_componentProto.encapsulation === ViewEncapsulation.Native) {
       _componentProto.encapsulation = ViewEncapsulation.Emulated;
     }
-
     super(_rootRenderer, _componentProto);
   }
 
   setElementProperty(renderElement: any, propertyName: string, propertyValue: any) {
-    if (propertyName === 'value' || (propertyName === 'checked' && propertyValue !== false)) {
-      if (DOM.nodeName(renderElement) === 'input') {
-        return super.setElementAttribute(renderElement, propertyName, propertyValue);
+    super.setElementProperty(renderElement, propertyName, propertyValue);
+
+    let el = DOM.nodeName(renderElement);
+    let attrList = ATTRIBUTES[el];
+    if (attrList) {
+      let booleanAttr = ListWrapper.contains(attrList, propertyName);
+      if (booleanAttr) {
+        if (propertyName === 'autocomplete') {
+          return this._setOnOffAttribute(renderElement, propertyName, propertyValue);
+        } else {
+          return this._setBooleanAttribute(renderElement, propertyName, propertyValue);
+        }
       }
-    } else if (propertyName === 'src') {
-      return super.setElementAttribute(renderElement, propertyName, propertyValue);
     }
     return super.setElementProperty(renderElement, propertyName, propertyValue);
   }
 
+
   setElementStyle(renderElement: any, styleName: string, styleValue: string): void {
     let styleNameCased = cssHyphenate(styleName);
-    super.setElementStyle(renderElement, styleNameCased, styleValue);
+    return super.setElementStyle(renderElement, styleNameCased, styleValue);
   }
 
   invokeElementMethod(renderElement: any, methodName: string, args: any[]) {
     if (methodName === 'focus') {
       if (DOM.nodeName(renderElement) === 'input') {
-        return super.invokeElementMethod(renderElement, 'autofocus', null);
+        return super.setElementAttribute(renderElement, 'autofocus', '');
       }
 
     }
     return super.invokeElementMethod(location, methodName, args);
   }
 
+  _setOnOffAttribute(renderElement, propertyName, propertyValue) {
+    if (isPresent(propertyValue)) {
+      if (propertyValue === true) {
+        return super.setElementAttribute(renderElement, propertyValue, 'on');
+      } else if (propertyValue === false) {
+        return super.setElementAttribute(renderElement, propertyValue, 'off');
+      }
+    }
+    return super.setElementProperty(renderElement, propertyName, propertyValue);
+
+  }
+  _setBooleanAttribute(renderElement, propertyName, propertyValue) {
+    if (isPresent(propertyValue) && propertyValue !== false) {
+      if (propertyValue === true) {
+        return super.setElementAttribute(renderElement, propertyName, '');
+      } else {
+        return super.setElementAttribute(renderElement, propertyName, propertyValue);
+      }
+    }
+    return super.setElementProperty(renderElement, propertyName, propertyValue);
+  }
 }
