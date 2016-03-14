d273331d9d7c47181e3d7fab26e4420ddd6fbc67
gdi2290 <github@gdi2290.com>
2016-03-13 18:53:39 -0700

    style(typings): correctly extend ObjectConstructor

--- custom_typings/_custom.d.ts
+++ custom_typings/_custom.d.ts
@@ -5,11 +5,7 @@
 /// <reference path="vinyl-buffer.d.ts" />
 
 interface ObjectConstructorAssign {
-  assign(): any;
-  assign(obj): any;
-  assign(obj, obj1): any;
-  assign(obj, obj1, obj2): any;
-  assign(obj, obj1, obj2, obj3): any;
+  assign(target: any, ...sources: any[]): any;
 }
 interface ObjectConstructor extends ObjectConstructorAssign {
 
