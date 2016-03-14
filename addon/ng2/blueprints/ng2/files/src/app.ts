7be364b2d732c8255887c90d397576dd8002fcd4
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2016-03-14 22:33:19 +0100

    fix(ng2 blueprint): register provider on root component
    It's best practice to configure providers on
    the root component rather than with bootstrap.

--- addon/ng2/blueprints/ng2/files/src/app.ts
+++ addon/ng2/blueprints/ng2/files/src/app.ts
@@ -1,7 +1,4 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {<%= jsComponentName %>App} from './app/<%= htmlComponentName %>';
-import {ROUTER_PROVIDERS} from 'angular2/router';
 
-bootstrap(<%= jsComponentName %>App, [
-  ROUTER_PROVIDERS
-]);
+bootstrap(<%= jsComponentName %>App, []);
--- addon/ng2/blueprints/ng2/files/src/app/__name__.ts
+++ addon/ng2/blueprints/ng2/files/src/app/__name__.ts
@@ -1,10 +1,10 @@
 import {Component} from 'angular2/core';
-import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
 
 
 @Component({
   selector: '<%= htmlComponentName %>-app',
-  providers: [],
+  providers: [ROUTER_PROVIDERS],
   templateUrl: 'app/<%= htmlComponentName %>.html',
   directives: [ROUTER_DIRECTIVES],
   pipes: []
@@ -14,7 +14,7 @@ import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 ])
 export class <%= jsComponentName %>App {
   defaultMeaning: number = 42;
-  
+
   meaningOfLife(meaning?: number) {
     return `The meaning of life is ${meaning || this.defaultMeaning}`;
   }
