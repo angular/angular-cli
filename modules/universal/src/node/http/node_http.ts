1b6dc371cc24d947ae8eccf45374abb23960a330
gdi2290 <github@gdi2290.com>
2016-03-17 18:09:36 -0700

    refactor(NodeConnection): use ngzone

--- modules/universal/src/node/http/node_http.ts
+++ modules/universal/src/node/http/node_http.ts
@@ -11,7 +11,7 @@ import {
 } from 'angular2/http';
 import * as utils from 'angular2/src/http/http_utils';
 import {isPresent} from 'angular2/src/facade/lang';
-import {Injectable} from 'angular2/core';
+import {Injectable, NgZone} from 'angular2/core';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import * as http from 'http';
@@ -22,7 +22,7 @@ export class NodeConnection implements Connection {
   public request: Request;
   public response: Observable<Response>;
 
-  constructor(req: Request, baseResponseOptions?: ResponseOptions) {
+  constructor(req: Request, baseResponseOptions?: ResponseOptions, ngZome?: NgZone) {
     this.request = req;
 
     let reqInfo: any = url.parse(req.url);
@@ -34,33 +34,45 @@ export class NodeConnection implements Connection {
     }
 
     this.response = new Observable(responseObserver => {
-      let nodeReq = http.request(reqInfo, (res: http.IncomingMessage) => {
-        let body = '';
-        res.on('data', (chunk) => body += chunk);
+      let nodeReq;
+      ngZome.run(() => {
 
-        let status = res.statusCode;
-        let headers = new Headers(res.headers);
-        let url = res.url;
+        nodeReq = http.request(reqInfo, (res: http.IncomingMessage) => {
+          let body = '';
+          res.on('data', (chunk) => body += chunk);
 
-        res.on('end', () => {
-          let responseOptions = new ResponseOptions({body, status, headers, url});
-          let response = new Response(responseOptions);
+          let status = res.statusCode;
+          let headers = new Headers(res.headers);
+          let url = res.url;
 
-          if (utils.isSuccess(status)) {
-            responseObserver.next(response);
-            responseObserver.complete();
-            return;
-          }
-          responseObserver.error(response);
+          res.on('end', () => {
+            let responseOptions = new ResponseOptions({body, status, headers, url});
+            let response = new Response(responseOptions);
+
+            if (utils.isSuccess(status)) {
+              ngZome.run(() => {
+                responseObserver.next(response);
+              });
+              ngZome.run(() => {
+                responseObserver.complete();
+              });
+              return;
+            }
+            ngZome.run(() => {
+              responseObserver.error(response);
+            });
+          });
         });
-      });
+      })
 
       let onError = (err) => {
         let responseOptions = new ResponseOptions({body: err, type: ResponseType.Error});
         if (isPresent(baseResponseOptions)) {
           responseOptions = baseResponseOptions.merge(responseOptions);
         }
-        responseObserver.error(new Response(responseOptions));
+        ngZome.run(() => {
+          responseObserver.error(new Response(responseOptions));
+        });
       };
 
       nodeReq.on('error', onError);
@@ -79,8 +91,11 @@ export class NodeConnection implements Connection {
 
 @Injectable()
 export class NodeBackend implements ConnectionBackend {
-  constructor(private _baseResponseOptions: ResponseOptions) {}
+  constructor(
+    private _baseResponseOptions: ResponseOptions,
+    private _ngZone: NgZone) {}
+
   public createConnection(request: Request): NodeConnection {
-    return new NodeConnection(request, this._baseResponseOptions);
+    return new NodeConnection(request, this._baseResponseOptions, this._ngZone);
   }
 }
