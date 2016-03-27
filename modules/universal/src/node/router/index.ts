63e7d4f3f10a4bf865bd1c46faee750e982db59b
gdi2290 <github@gdi2290.com>
2016-03-26 23:38:36 -0700

    feat(universal): node router providers

--- modules/universal/src/node/router/index.ts
+++ modules/universal/src/node/router/index.ts
@@ -1,9 +1,16 @@
-import { provide } from 'angular2/core';
-import { PlatformLocation } from 'angular2/router';
-import { NodePlatformLocation } from './node_platform_location';
+import {provide} from 'angular2/core';
+import {PlatformLocation, ROUTER_PROVIDERS_COMMON} from 'angular2/router';
+
+import {NodePlatformLocation} from './node_platform_location';
 
 export * from './node_platform_location';
 
 export const NODE_LOCATION_PROVIDERS: Array<any> = [
   provide(PlatformLocation, { useClass: NodePlatformLocation })
+];
+
+
+export const NODE_ROUTER_PROVIDERS = [
+  ...ROUTER_PROVIDERS_COMMON,
+  ...NODE_LOCATION_PROVIDERS
 ];
