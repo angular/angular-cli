1b4ed40971c9e1cc6326f11d01125ca89a0e6fa8
gdi2290 <github@gdi2290.com>
2016-03-26 17:13:17 -0700

    style(polyfills): remove comments

--- modules/polyfills/src/zone-browser.ts
+++ modules/polyfills/src/zone-browser.ts
@@ -1,8 +1,2 @@
-// ZONE.js workaround beta.10 beta.11
-
-/* tslint:disable */
-
 require('zone.js/dist/zone.js');
 require('zone.js/dist/long-stack-trace-zone');
-
-/* tslint:enable */
--- modules/polyfills/src/zone-node.ts
+++ modules/polyfills/src/zone-node.ts
@@ -1,3 +1,2 @@
-// ZONE.js workaround beta.10 beta.11
 require('zone.js/dist/zone-node.js');
 require('zone.js/dist/long-stack-trace-zone');
