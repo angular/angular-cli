80b7dc59aaaf630570c3a9eaf51c755071a49454
gdi2290 <github@gdi2290.com>
2016-03-26 23:49:35 -0700

    refactor(universal/http): prefer NODE_HTTP_PROVIDERS

--- modules/universal/src/node/http/index.ts
+++ modules/universal/src/node/http/index.ts
@@ -14,24 +14,22 @@ import * as preloadCache from './preload_cache';
 export * from './node_http'
 export * from './preload_cache';
 
+// export var NODE_HTTP_PROVIDERS: Array<any> = [
+//   provide(RequestOptions, {useClass: BaseRequestOptions}),
+//   provide(ResponseOptions, {useClass: BaseResponseOptions}),
+//
+//   provide(nodeHttp.NodeBackend, {
+//     useFactory: (respOpt, ngZone) => {
+//       return new nodeHttp.NodeBackend(respOpt, ngZone);
+//     },
+//     deps: [ResponseOptions, NgZone]
+//   }),
+//
+//   provide(ConnectionBackend, {useClass: nodeHttp.NodeBackend}),
+//   provide(Http, {useClass: Http})
+// ];
+
 export var NODE_HTTP_PROVIDERS: Array<any> = [
-  provide(RequestOptions, {useClass: BaseRequestOptions}),
-  provide(ResponseOptions, {useClass: BaseResponseOptions}),
-
-  provide(nodeHttp.NodeBackend, {
-    useFactory: (respOpt, ngZone) => {
-      return new nodeHttp.NodeBackend(respOpt, ngZone);
-    },
-    deps: [ResponseOptions, NgZone]
-  }),
-
-  provide(ConnectionBackend, {useClass: nodeHttp.NodeBackend}),
-  provide(Http, {useClass: Http})
-];
-
-export var NODE_PRELOAD_CACHE_HTTP_PROVIDERS: Array<any> = [
-  provide(preloadCache.BASE_URL, {useValue: ''}),
-  provide(preloadCache.PRIME_CACHE, {useValue: true}),
   provide(RequestOptions, {useClass: BaseRequestOptions}),
   provide(ResponseOptions, {useClass: BaseResponseOptions}),
 
@@ -49,5 +47,14 @@ export const HTTP_PROVIDERS = NODE_HTTP_PROVIDERS.concat([
     console.warn('DEPRECATION WARNING: `HTTP_PROVIDERS` is no longer supported for `angular2-universal` and will be removed in next release. Please use `NODE_HTTP_PROVIDERS`');
     /* tslint:enable */
   }, multi: true}),
-  NODE_PRELOAD_CACHE_HTTP_PROVIDERS
+  NODE_HTTP_PROVIDERS
+]);
+
+export const NODE_PRELOAD_CACHE_HTTP_PROVIDERS = NODE_HTTP_PROVIDERS.concat([
+  provide(PLATFORM_INITIALIZER, {useValue: () => {
+    /* tslint:disable */
+    console.warn('DEPRECATION WARNING: `NODE_PRELOAD_CACHE_HTTP_PROVIDERS` is no longer supported for `angular2-universal` and will be removed in next release. Please use `NODE_HTTP_PROVIDERS`');
+    /* tslint:enable */
+  }, multi: true}),
+  NODE_HTTP_PROVIDERS
 ]);
