e1a632c2cd1314f2bf4241754a86f9dead311d89
Justin DuJardin <justin@dujardinconsulting.com>
2016-04-04 18:01:35 -0700

    feat(NodeXHRImpl): support file:// url fetching
     - used in conjunction with a NodeUrlResolver this can be used to resolve templateUrl on components during server rendering.

--- modules/universal/src/node/platform/node_xhr_impl.ts
+++ modules/universal/src/node/platform/node_xhr_impl.ts
@@ -1,5 +1,6 @@
 import * as http from 'http';
 import * as url from 'url';
+import * as fs from 'fs';
 
 import {XHR} from 'angular2/compiler';
 import {PromiseWrapper, PromiseCompleter} from 'angular2/src/facade/promise';
@@ -9,30 +10,40 @@ export class NodeXHRImpl extends XHR {
     const completer: PromiseCompleter<string> = PromiseWrapper.completer();
     const parsedUrl = url.parse(templateUrl);
 
-    http.get(parsedUrl, (res) => {
-      res.setEncoding('utf8');
+    if (parsedUrl.protocol === 'file:') {
+      fs.readFile(parsedUrl.path, (err, data) => {
+        if (err) {
+          return completer.reject(`Failed to load ${templateUrl} with error ${err}`);
+        }
+        completer.resolve(data.toString());
+      });
+    }
+    else {
+      http.get(parsedUrl, (res) => {
+        res.setEncoding('utf8');
 
-      const status = res.statusCode;
+        const status = res.statusCode;
 
-      if (200 <= status && status <= 300) {
-        let data = '';
+        if (200 <= status && status <= 300) {
+          let data = '';
 
-        res.on('data', (chunk) => {
-          data += chunk;
-        });
-        res.on('end', () => {
-          completer.resolve(data);
-        });
+          res.on('data', (chunk) => {
+            data += chunk;
+          });
+          res.on('end', () => {
+            completer.resolve(data);
+          });
 
-      } else {
+        } else {
+          completer.reject(`Failed to load ${templateUrl}`, null);
+        }
+
+        // consume response body
+        res.resume();
+      }).on('error', (e) => {
         completer.reject(`Failed to load ${templateUrl}`, null);
-      }
-
-      // consume response body
-      res.resume();
-    }).on('error', (e) => {
-      completer.reject(`Failed to load ${templateUrl}`, null);
-    });
+      });
+    }
 
     return completer.promise;
   }
