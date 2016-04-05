0fd1075273a44f3f573ba04358d365f3d0c58b39
gdi2290 <github@gdi2290.com>
2016-04-05 15:51:55 -0700

    feat(polyfills): zone patch crypto

--- modules/polyfills/src/zone-node/index.ts
+++ modules/polyfills/src/zone-node/index.ts
@@ -21,6 +21,39 @@ if (shouldPatchGlobalTimers) {
 }
 
 
+// Crypto
+var crypto;
+try {
+  crypto = require('crypto');
+} catch (err) {}
+
+// TODO(gdi2290): implement a better way to patch these methods
+if (crypto) {
+  let nativeRandomBytes = crypto.randomBytes;
+  crypto.randomBytes = function randomBytesZone(size: number, callback?: Function) {
+    if (!callback) {
+      return nativeRandomBytes(size);
+    } else {
+      let zone = Zone.current;
+      var source = crypto.constructor.name + '.randomBytes';
+      return nativeRandomBytes(size, zone.wrap(callback, source));
+    }
+  }.bind(crypto);
+
+  let nativePbkdf2 = crypto.pbkdf2;
+  crypto.pbkdf2 = function pbkdf2Zone(...args) {
+    let fn = args[args.length - 1];
+    if (typeof fn === 'function') {
+      let zone = Zone.current;
+      var source = crypto.constructor.name + '.pbkdf2';
+      args[args.length - 1] = zone.wrap(fn, source);
+      return nativePbkdf2(...args);
+    } else {
+      return nativePbkdf2(...args);
+    }
+  }.bind(crypto);
+}
+
 // zone types
 interface TaskData {
   isPeriodic?: boolean;
