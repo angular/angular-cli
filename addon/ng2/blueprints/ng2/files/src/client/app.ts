372354fa03afaa7aababd3e98420ef121f5bbe75
Filipe Silva <filipematossilva@gmail.com>
2016-04-08 17:24:10 +0100

    feat(prod-build): add environment config

--- addon/ng2/blueprints/ng2/files/src/client/app.ts
+++ addon/ng2/blueprints/ng2/files/src/client/app.ts
@@ -1,4 +1,10 @@
 import {bootstrap} from 'angular2/platform/browser';
+import {enableProdMode} from 'angular2/core';
+import {environment} from './app/environment';
 import {<%= jsComponentName %>App} from './app/<%= htmlComponentName %>';
 
-bootstrap(<%= jsComponentName %>App, []);
+if (environment.production) {
+  enableProdMode();
+}
+
+bootstrap(<%= jsComponentName %>App);
