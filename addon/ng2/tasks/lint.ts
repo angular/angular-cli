aec07cf103875cca1528b6e28fde215f5e75b001
Filipe Silva <filipematossilva@gmail.com>
2016-04-11 00:27:44 +0100

    fix(lint): fix listing of files

---
+++ addon/ng2/tasks/lint.ts
@@ -0,0 +1,22 @@
+import * as Promise from 'ember-cli/lib/ext/promise';
+import * as Task from 'ember-cli/lib/models/task';
+import * as chalk from 'chalk';
+import {exec} from 'child_process';
+
+module.exports = Task.extend({
+  run: function () {
+    var ui = this.ui;
+
+    return new Promise(function(resolve) {
+      exec('npm run lint', (err, stdout) => {
+        ui.writeLine(stdout);
+        if (err) {
+          ui.writeLine(chalk.red('Lint errors found in the listed files.'));
+        } else {
+          ui.writeLine(chalk.green('All files pass linting.'));
+        }
+        resolve();
+      });
+    });
+  }
+});
