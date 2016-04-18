81560111459c11440a4dbc922f9e5fb2bd54ae97
Mark Pieszak <mpieszak84@gmail.com>
2016-04-18 01:42:46 -0400

    fix(universal): Fix server file path to index.js (#365)
    File located @ `examples / src / index.js`

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // Set up ts-node to enable loading of TypeScript files.
 require('ts-node/register');
-module.exports = require('./examples/src/server.ts');
+module.exports = require('./examples/src/index.ts');
