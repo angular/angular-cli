ba040af40929691bb7432065ea1f2327a7f49a9e
Mike Brocchi <mbrocchi@gmail.com>
2016-04-19 17:25:51 -0400

    chore: add missing route-based code to route blueprints

--- addon/ng2/blueprints/component/files/__path__/__name__.component.spec.ts
+++ addon/ng2/blueprints/component/files/__path__/__name__.component.spec.ts
@@ -10,14 +10,27 @@ import {
   ComponentFixture,
   TestComponentBuilder
 } from 'angular2/testing';
-import {provide} from 'angular2/core';
+import {provide} from 'angular2/core';<% if (route) { %>
+import {Router, RouteParams} from 'angular2/router';<% } %>
 import {<%= classifiedModuleName %>Component} from './<%= dasherizedModuleName %>.component';
+<% if (route) { %>
+class MockRouter {
+  registerPrimaryOutlet() { }
+}
+
+class MockRouteParams {
+  get() { return 1; }
+}<% } %>
 
 describe('<%= classifiedModuleName %> Component', () => {
-
+<% if (route) { %>
+  beforeEachProviders(() => [
+    provide(Router, { useClass: MockRouter }),
+    provide(RouteParams, { useClass: MockRouteParams }),
+  ]);
+<% } else { %>
   beforeEachProviders((): any[] => []);
-
-
+<% } %>
   it('should ...', injectAsync([TestComponentBuilder], (tcb:TestComponentBuilder) => {
     return tcb.createAsync(<%= classifiedModuleName %>Component).then((fixture: ComponentFixture) => {
       fixture.detectChanges();
--- addon/ng2/blueprints/component/files/__path__/__name__.component.ts
+++ addon/ng2/blueprints/component/files/__path__/__name__.component.ts
@@ -1,11 +1,15 @@
-import {Component, OnInit} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';<% if (route) { %>
+import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';<% } %>
 
 @Component({
   moduleId: __moduleName,
   selector: '<%= dasherizedModuleName %>',
   templateUrl: '<%= dasherizedModuleName %>.component.html',
-  styleUrls: ['<%= dasherizedModuleName %>.component.<%= styleExt %>']
-})
+  styleUrls: ['<%= dasherizedModuleName %>.component.<%= styleExt %>']<% if (route) { %>,
+  directives: [ROUTER_DIRECTIVES]<% } %>
+})<% if (route) { %>
+@RouteConfig([
+])<% } %>
 export class <%= classifiedModuleName %>Component implements OnInit {
 
   constructor() {}
