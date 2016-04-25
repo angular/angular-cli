1e54b69f6dc36c56b960a716954d45dd81478f9e
Mike Brocchi <mbrocchi@gmail.com>
2016-04-25 15:25:19 -0400

    chore: remove unnecessary default code
    Fixes #516

--- addon/ng2/blueprints/ng2/files/src/client/app/__name__.component.spec.ts
+++ addon/ng2/blueprints/ng2/files/src/client/app/__name__.component.spec.ts
@@ -1,18 +1,22 @@
-import {describe, it, expect, beforeEachProviders, inject} from 'angular2/testing';
+import {
+  async,
+  beforeEachProviders,
+  describe,
+  ddescribe,
+  expect,
+  iit,
+  it,
+  inject,
+  ComponentFixture,
+  TestComponentBuilder
+} from 'angular2/testing';
 import {<%= jsComponentName %>App} from '../app/<%= htmlComponentName %>.component';
 
 beforeEachProviders(() => [<%= jsComponentName %>App]);
 
-describe('App: <%= jsComponentName %>', () => {
-  it('should have the `defaultMeaning` as 42', inject([<%= jsComponentName %>App], (app: <%= jsComponentName %>App) => {
-    expect(app.defaultMeaning).toBe(42);
-  }));
-
-  describe('#meaningOfLife', () => {
-    it('should get the meaning of life', inject([<%= jsComponentName %>App], (app: <%= jsComponentName %>App) => {
-      expect(app.meaningOfLife()).toBe('The meaning of life is 42');
-      expect(app.meaningOfLife(22)).toBe('The meaning of life is 22');
-    }));
-  });
+describe('App: <%= jsComponentName %>', () => {  
+  it('should create the app', inject([<%= jsComponentName %>App], (app: <%= jsComponentName %>App) => { 
+    expect(app).toBeTruthy(); 
+  })); 
 });
 
--- addon/ng2/blueprints/ng2/files/src/client/app/__name__.component.ts
+++ addon/ng2/blueprints/ng2/files/src/client/app/__name__.component.ts
@@ -7,9 +7,4 @@ import {Component} from 'angular2/core';
   styleUrls: ['<%= dasherizedModuleName %>.component.<%= styleExt %>']
 })
 export class <%= jsComponentName %>App {
-  defaultMeaning: number = 42;
-
-  meaningOfLife(meaning?: number) {
-    return `The meaning of life is ${meaning || this.defaultMeaning}`;
-  }
 }
