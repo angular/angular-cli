1bf3b4f2de2792ac836766e7d9d791652527c06e
Mike Brocchi <mbrocchi@gmail.com>
2016-04-27 21:15:53 -0400

    chore: clean up component blueprint for routes
    Clean up the inline template markup to not include a router-outlet tag

--- addon/ng2/blueprints/component/files/__path__/__name__.component.ts
+++ addon/ng2/blueprints/component/files/__path__/__name__.component.ts
@@ -1,5 +1,4 @@
-import {Component, OnInit} from 'angular2/core';<% if (route) { %>
-import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';<% } %>
+import {Component, OnInit} from 'angular2/core';
 
 @Component({
   moduleId: __moduleName,
@@ -7,13 +6,11 @@ import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router'
   template: `
     <p>
       <%= dasherizedModuleName %> Works!
-    </p><% if (route) { %>
-    <router-outlet></router-outlet><% } %>
+    </p>
   `,<% } else { %>
   templateUrl: '<%= dasherizedModuleName %>.component.html',<% } if(inlineStyle) { %>
   styles: []<% } else { %>
-  styleUrls: ['<%= dasherizedModuleName %>.component.<%= styleExt %>']<% } if (route) { %>,
-  directives: [ROUTER_DIRECTIVES]<% } %>
+  styleUrls: ['<%= dasherizedModuleName %>.component.<%= styleExt %>']<% } %>
 })
 export class <%= classifiedModuleName %>Component implements OnInit {
 
