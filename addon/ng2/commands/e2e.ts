1e0fcd7054dbaf464657a2ae8b66a8ed1e6cde43
Filipe Silva <filipematossilva@gmail.com>
2016-05-02 02:13:57 +0100

    chore(e2e): remove npm err output from e2e

--- addon/ng2/commands/e2e.ts
+++ addon/ng2/commands/e2e.ts
@@ -1,16 +1,13 @@
-/* jshint node: true */
-'use strict';
-
-var Command = require('ember-cli/lib/models/command');
-const config = require('../models/config');
-var E2ETask = require('../tasks/e2e');
+import * as Command from 'ember-cli/lib/models/command';
+import * as E2ETask from '../tasks/e2e';
+import {CliConfig} from '../models/config';
 
 module.exports = Command.extend({
   name: 'e2e',
   description: 'Run e2e tests in existing project',
   works: 'insideProject',
   run: function () {
-    this.project.ngConfig = this.project.ngConfig || config.CliConfig.fromProject();
+    this.project.ngConfig = this.project.ngConfig || CliConfig.fromProject();
 
     var e2eTask = new E2ETask({
       ui: this.ui,
