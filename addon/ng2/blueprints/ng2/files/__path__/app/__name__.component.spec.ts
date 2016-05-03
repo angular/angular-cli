a9f14a725db33d082ce140c3e89fa674007fb6a0
Hans <hans@hansl.ca>
2016-05-03 09:55:46 -0700

    chore: fix all linting issues with generated app (#611)

--- addon/ng2/blueprints/ng2/files/__path__/app/__name__.component.spec.ts
+++ addon/ng2/blueprints/ng2/files/__path__/app/__name__.component.spec.ts
@@ -1,24 +1,22 @@
 import {
-  async,
   beforeEachProviders,
   describe,
-  ddescribe,
   expect,
-  iit,
   it,
   inject
 } from '@angular/core/testing';
-import { ComponentFixture, TestComponentBuilder } from '@angular/compiler/testing';
 import { <%= jsComponentName %>AppComponent } from '../app/<%= htmlComponentName %>.component';
 
 beforeEachProviders(() => [<%= jsComponentName %>AppComponent]);
 
 describe('App: <%= jsComponentName %>', () => {
-  it('should create the app', inject([<%= jsComponentName %>AppComponent], (app: <%= jsComponentName %>AppComponent) => {
+  it('should create the app',
+      inject([<%= jsComponentName %>AppComponent], (app: <%= jsComponentName %>AppComponent) => {
     expect(app).toBeTruthy();
   }));
 
-  it('should have as title \'<%= htmlComponentName %> works!\'', inject([<%= jsComponentName %>AppComponent], (app: <%= jsComponentName %>AppComponent) => {
+  it('should have as title \'<%= htmlComponentName %> works!\'',
+      inject([<%= jsComponentName %>AppComponent], (app: <%= jsComponentName %>AppComponent) => {
     expect(app.title).toEqual('<%= htmlComponentName %> works!');
   }));
 });
--- addon/ng2/blueprints/pipe/files/__path__/__name__.pipe.spec.ts
+++ addon/ng2/blueprints/pipe/files/__path__/__name__.pipe.spec.ts
@@ -1,20 +1,16 @@
 import {
   it,
-  iit,
   describe,
-  ddescribe,
   expect,
   inject,
   beforeEachProviders
 } from '@angular/core/testing';
-import { TestComponentBuilder } from '@angular/compiler/testing';
-import { provide } from '@angular/core';
 import { <%= classifiedModuleName %> } from './<%= dasherizedModuleName %>.pipe';
 
 describe('<%= classifiedModuleName %> Pipe', () => {
   beforeEachProviders(() => [<%= classifiedModuleName%>]);
 
-  it('should transform the input', inject([<%= classifiedModuleName %>], (pipe:<%= classifiedModuleName %>) => {
+  it('should transform the input', inject([<%= classifiedModuleName %>], (pipe: <%= classifiedModuleName %>) => {
       expect(pipe.transform(true)).toBe(null);
   }));
 });
