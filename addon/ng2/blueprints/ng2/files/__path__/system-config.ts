549890112e69914686ec6952b963b5fab932084d
guillaumeleone <leone@spideo.tv>
2016-05-07 12:48:02 +0200

    fix(generated-project): adding router-deprecated module

--- addon/ng2/blueprints/ng2/files/__path__/system-config.ts
+++ addon/ng2/blueprints/ng2/files/__path__/system-config.ts
@@ -20,6 +20,7 @@ const barrels: string[] = [
   '@angular/compiler',
   '@angular/http',
   '@angular/router',
+  '@angular/router-deprecated',
   '@angular/platform-browser',
   '@angular/platform-browser-dynamic',
 
