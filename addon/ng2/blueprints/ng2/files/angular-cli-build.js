2376a689df9a391925f6cd7818f072a70afee588
Filipe Silva <filipematossilva@gmail.com>
2016-05-19 17:13:38 +0100

    fix(build): fix broken sourcemaps

--- addon/ng2/blueprints/ng2/files/angular-cli-build.js
+++ addon/ng2/blueprints/ng2/files/angular-cli-build.js
@@ -7,11 +7,11 @@ module.exports = function(defaults) {
     vendorNpmFiles: [
       'systemjs/dist/system-polyfills.js',
       'systemjs/dist/system.src.js',
-      'zone.js/dist/*.js',
+      'zone.js/dist/**/*.+(js|js.map)',
       'es6-shim/es6-shim.js',
-      'reflect-metadata/*.js',
-      'rxjs/**/*.js',
-      '@angular/**/*.js'
+      'reflect-metadata/**/*.+(js|js.map)',
+      'rxjs/**/*.+(js|js.map)',
+      '@angular/**/*.+(js|js.map)'
     ]
   });
 };
