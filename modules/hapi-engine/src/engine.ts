af87cb81254ea5ab50225d49abf2b0e111ce2021
PatrickJS <github@gdi2290.com>
2016-05-29 14:33:09 -0700

    fix(hapi-engine): use new directives/providers

--- modules/hapi-engine/src/engine.ts
+++ modules/hapi-engine/src/engine.ts
@@ -52,10 +52,11 @@ class Runtime {
     // bootstrap and render component to string
     const _options = this.options;
     const _template = _options.template || template;
+    const _directives = _options.directives;
     if (HAPI_ANGULAR_APP.template !== _template) {
       disposeHapiPlatform();
 
-      const _directives = _options.directives;
+
       const _Bootloader = Bootloader;
       let _bootloader = _options.bootloader;
       if (_options.bootloader) {
@@ -65,10 +66,11 @@ class Runtime {
         _bootloader = _Bootloader.create(_options);
       }
       HAPI_PLATFORM = _bootloader;
-      HAPI_ANGULAR_APP.directives = _directives;
-      HAPI_ANGULAR_APP.providers = _options.providers;
       HAPI_ANGULAR_APP.template = _template;
     }
+    HAPI_ANGULAR_APP.directives = _directives;
+    HAPI_ANGULAR_APP.providers = _options.providers;
+
 
     HAPI_PLATFORM.serializeApplication(HAPI_ANGULAR_APP)
       .then(html => done(null, this.buildClientScripts(html, context)))
