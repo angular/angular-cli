92debe0fa365d94e5d9dd7fd34ca8635a6a493b9
PatrickJS <github@gdi2290.com>
2016-05-31 14:02:07 -0700

    fix(universal): typings

--- modules/universal/src/typings.ts
+++ modules/universal/src/typings.ts
@@ -4,6 +4,12 @@
 export * from './browser/browser';
 
 // bootstrap exported in both node/browser
-export {bootstrap, NgPreloadCacheHttp, PRIME_CACHE} from './browser/browser';
+export {
+  bootstrap,
+  NgPreloadCacheHttp,
+  PRIME_CACHE,
+  isBrowser,
+  isNode
+} from './browser/browser';
 
 export * from './node/node';
