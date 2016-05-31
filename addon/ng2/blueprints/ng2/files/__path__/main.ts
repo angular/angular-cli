8d23f2f266294ee29bd09e41e3b3358a0bb4ff79
Matt Ma <bigmabig@gmail.com>
2016-05-30 18:07:49 -0700

    remove the trailing folder slash, enhance editor syntax highlight by removing unresolved modules path

--- addon/ng2/blueprints/ng2/files/__path__/main.ts
+++ addon/ng2/blueprints/ng2/files/__path__/main.ts
@@ -1,6 +1,6 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
-import { <%= jsComponentName %>AppComponent, environment } from './app/';<% if(isMobile) { %>
+import { <%= jsComponentName %>AppComponent, environment } from './app';<% if(isMobile) { %>
 import { APP_SHELL_RUNTIME_PROVIDERS } from '@angular/app-shell';<% } %>
 
 if (environment.production) {
