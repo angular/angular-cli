d0c4bf0d6f2eb8aeb27752a39bef0edc0c78b3ac
Marc Sensenich <sensenichm91@gmail.com>
2016-06-07 15:09:15 -0400

    feat: Implementation of flag to install Angular Material on `new` or `init`
    This features adds a `--material` flag to the `new` and `init` options. This
    streamlines the process of the adding of Angular Material to a new project

--- addon/ng2/commands/init.js
+++ addon/ng2/commands/init.js
@@ -25,7 +25,8 @@ module.exports = Command.extend({
     { name: 'source-dir', type: String, default: 'src', aliases: ['sd'] },
     { name: 'style', type: String, default: 'css' },
     { name: 'prefix', type: String, default: 'app', aliases: ['p'] },
-    { name: 'mobile', type: Boolean, default: false }
+    { name: 'mobile', type: Boolean, default: false },
+    { name: 'material', type: Boolean, default: false }    
   ],
 
   anonymousOptions: ['<glob-pattern>'],
@@ -103,7 +104,8 @@ module.exports = Command.extend({
       sourceDir: commandOptions.sourceDir,
       style: commandOptions.style,
       prefix: commandOptions.prefix,
-      mobile: commandOptions.mobile
+      mobile: commandOptions.mobile,
+      material: commandOptions.material
     };
 
     if (!validProjectName(packageName)) {
