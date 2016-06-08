250c5fec331e5c5ad1348715421de91b9e826a1e
Filipe Silva <filipematossilva@gmail.com>
2016-06-08 15:31:28 +0100

    chore(app): rename *AppComponent to just AppComponent

---
+++ addon/ng2/blueprints/ng2/files/__path__/app/app.component.spec.ts
@@ -0,0 +1,22 @@
+import {
+  beforeEachProviders,
+  describe,
+  expect,
+  it,
+  inject
+} from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+beforeEachProviders(() => [AppComponent]);
+
+describe('App: <%= jsComponentName %>', () => {
+  it('should create the app',
+      inject([AppComponent], (app: AppComponent) => {
+    expect(app).toBeTruthy();
+  }));
+
+  it('should have as title \'app works!\'',
+      inject([AppComponent], (app: AppComponent) => {
+    expect(app.title).toEqual('app works!');
+  }));
+});
--- addon/ng2/blueprints/ng2/files/__path__/main.ts
+++ addon/ng2/blueprints/ng2/files/__path__/main.ts
@@ -1,11 +1,11 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
-import { <%= jsComponentName %>AppComponent, environment } from './app/';<% if(isMobile) { %>
+import { AppComponent, environment } from './app/';<% if(isMobile) { %>
 import { APP_SHELL_RUNTIME_PROVIDERS } from '@angular/app-shell';<% } %>
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(<%= jsComponentName %>AppComponent<% if(isMobile) { %>, [ APP_SHELL_RUNTIME_PROVIDERS ]<% } %>);
+bootstrap(AppComponent<% if(isMobile) { %>, [ APP_SHELL_RUNTIME_PROVIDERS ]<% } %>);
 
