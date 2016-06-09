46762e35d11b120d631ea7fbfec7ecb3b4c109a2
Marc Sensenich <sensenichm91@gmail.com>
2016-06-08 21:41:02 -0400

    Matching changed .travis.yml

--- addon/ng2/tasks/e2e.ts
+++ addon/ng2/tasks/e2e.ts
@@ -6,21 +6,19 @@ import {exec} from 'child_process';
 module.exports = Task.extend({
   run: function () {
     var ui = this.ui;
+    var exitCode = 0;
 
     return new Promise((resolve, reject) => {
       exec(`npm run e2e -- ${this.project.ngConfig.e2e.protractor.config}`, (err, stdout, stderr) => {
         ui.writeLine(stdout);
-        if (err) {
-          ui.writeLine(err.name);
-          ui.writeLine(err.message);          
+        if (err) {        
           ui.writeLine(stderr);
           ui.writeLine(chalk.red('Some end-to-end tests failed, see above.'));
-          const exitCode = typeof err.code === 'number' ? err.code : 1;
-          reject(exitCode);
+          exitCode = typeof err.code === 'number' ? err.code : 1;
         } else {
           ui.writeLine(chalk.green('All end-to-end tests pass.'));
-          resolve();
         }
+        resolve(exitCode);
       });
     });
   }
