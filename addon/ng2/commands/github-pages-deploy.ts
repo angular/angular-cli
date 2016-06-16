64bcd6c1a84212f48d26bdd72db0fa64b0c647f0
Jonathan Kuleff <jonathankuleff@gmail.com>
2016-06-17 01:15:15 +1000

    feat: add output path flag to deploy command
    add output path flag to deploy command, allowing default to be overridden.

--- addon/ng2/commands/github-pages-deploy.ts
+++ addon/ng2/commands/github-pages-deploy.ts
@@ -9,6 +9,7 @@ import * as path from 'path';
 import * as BuildTask from 'ember-cli/lib/tasks/build';
 import * as win from 'ember-cli/lib/utilities/windows-admin';
 import * as CreateGithubRepo from '../tasks/create-github-repo';
+import * as CommandHelper from '../utilities/command-helper';
 
 const fsReadFile = Promise.denodeify(fs.readFile);
 const fsWriteFile = Promise.denodeify(fs.writeFile);
@@ -52,7 +53,15 @@ module.exports = Command.extend({
       type: String,
       default: '',
       description: 'Github username'
+    }, {
+      name: 'output-path',
+      type: String,
+      default: 'dist/',
     }],
+
+  beforeRun: function() {      
+    CommandHelper.loadDefaults(this);
+  },
 
   run: function(options, rawArgs) {
     var ui = this.ui;
@@ -77,7 +86,7 @@ module.exports = Command.extend({
 
     var buildOptions = {
       environment: options.environment,
-      outputPath: 'dist/'
+      outputPath: options.outputPath
     };
 
     var createGithubRepoTask = new CreateGithubRepo({
@@ -97,7 +106,7 @@ module.exports = Command.extend({
       .then(saveStartingBranchName)
       .then(createGitHubRepoIfNeeded)
       .then(checkoutGhPages)
-      .then(copyFiles)
+      .then(() => copyFiles(options.outputPath))
       .then(updateBaseHref)
       .then(addAndCommit)
       .then(returnStartingBranch)
@@ -156,14 +165,14 @@ module.exports = Command.extend({
         .then(() => execPromise(`git commit -m \"initial ${ghPagesBranch} commit\"`));
     }
 
-    function copyFiles() {
-      return fsReadDir('dist')
+    function copyFiles(outputPath) {
+      return fsReadDir(outputPath)
         .then((files) => Promise.all(files.map((file) => {
           if (file === '.gitignore'){
             // don't overwrite the .gitignore file
             return Promise.resolve();
           }
-          return fsCopy(path.join('dist', file), path.join('.', file))
+          return fsCopy(path.join(outputPath, file), path.join('.', file))
         })));
     }
 
