e01abc0b35ac1ac50d09d3e9e1e37257f623c299
陳彥澄 <Shyam-Chen@users.noreply.github.com>
2016-06-23 17:34:34 +0800

    Update angular-cli-build.js

--- addon/ng2/blueprints/ng2/files/angular-cli-build.js
+++ addon/ng2/blueprints/ng2/files/angular-cli-build.js
@@ -9,7 +9,6 @@ module.exports = function(defaults) {
       'systemjs/dist/system.src.js',
       'zone.js/dist/**/*.+(js|js.map)',
       'core-js/client/shim.min.+(js|js.map)',
-      'reflect-metadata/**/*.+(js|js.map)',
       'rxjs/**/*.+(js|js.map)',
       '@angular/**/*.+(js|js.map)'
     ]
