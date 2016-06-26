351ebc0d3ce39ca23d21adf0ca20eac7d0ae6b7d
PatrickJS <github@gdi2290.com>
2016-06-26 15:35:33 -0700

    chore(universal): deprecation  BROWSER_HTTP_PROVIDERS

--- modules/universal/src/browser/http.ts
+++ modules/universal/src/browser/http.ts
@@ -1,4 +1,26 @@
 import {HTTP_PROVIDERS, JSONP_PROVIDERS} from '@angular/http';
-
-export const BROWSER_HTTP_PROVIDERS = HTTP_PROVIDERS;
-export const BROWSER_JSONP_PROVIDERS = JSONP_PROVIDERS;
+import {PLATFORM_INITIALIZER} from '@angular/core';
+export const BROWSER_HTTP_PROVIDERS = [
+  {
+    provide: PLATFORM_INITIALIZER,
+    useValue: () => {
+      /* tslint:disable */
+      console.warn('DEPRECATION WARNING: `BROWSER_HTTP_PROVIDERS` is no longer supported for `angular2-universal` and will be removed in next release. Please use `HTTP_PROVIDERS` from @angular/http');
+      /* tslint:enable */
+    },
+    multi: true
+  },
+  ...HTTP_PROVIDERS
+];
+export const BROWSER_JSONP_PROVIDERS = [
+  {
+    provide: PLATFORM_INITIALIZER,
+    useValue: () => {
+      /* tslint:disable */
+      console.warn('DEPRECATION WARNING: `BROWSER_JSONP_PROVIDERS` is no longer supported for `angular2-universal` and will be removed in next release. Please use `JSONP_PROVIDERS` from @angular/http');
+      /* tslint:enable */
+    },
+    multi: true
+  },
+  JSONP_PROVIDERS
+];
