c54c2b9912d0524ae7c060c73baf5f95ef9284de
陳彥澄 <Shyam-Chen@users.noreply.github.com>
2016-06-28 11:45:49 +0800

    Update angular-cli-build.js

--- addon/ng2/blueprints/ng2/files/angular-cli-build.js
+++ addon/ng2/blueprints/ng2/files/angular-cli-build.js
@@ -9,7 +9,6 @@ var Angular2App = require('angular-cli/lib/broccoli/angular2-app');
 module.exports = function(defaults) {
   return new Angular2App(defaults, {
     vendorNpmFiles: [
-      'systemjs/dist/system-polyfills.js',
       'systemjs/dist/system.src.js',
       'zone.js/dist/**/*.+(js|js.map)',
       'core-js/client/shim.min.js',
