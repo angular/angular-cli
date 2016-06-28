ef239b562e7225e41f3ed9a7f4100bce006877c6
陳彥澄 <Shyam-Chen@users.noreply.github.com>
2016-06-28 11:50:40 +0800

    Update karma.conf.js

--- addon/ng2/blueprints/ng2/files/config/karma.conf.js
+++ addon/ng2/blueprints/ng2/files/config/karma.conf.js
@@ -19,7 +19,6 @@ module.exports = function (config) {
     files: [
       { pattern: 'dist/vendor/core-js/client/shim.min.js', included: true, watched: false },
       { pattern: 'dist/vendor/zone.js/dist/zone.js', included: true, watched: false },
-      { pattern: 'dist/vendor/systemjs/dist/system-polyfills.js', included: true, watched: false },
       { pattern: 'dist/vendor/systemjs/dist/system.src.js', included: true, watched: false },
       { pattern: 'dist/vendor/zone.js/dist/async-test.js', included: true, watched: false },
       { pattern: 'dist/vendor/zone.js/dist/fake-async-test.js', included: true, watched: false },
