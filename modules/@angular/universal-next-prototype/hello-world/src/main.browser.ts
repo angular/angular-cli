085ab5cc0f92c88c42fadb1a911d35227fd20d63
PatrickJS <github@gdi2290.com>
2016-07-05 10:39:55 -0700

    refactor(universal): include client deps

--- modules/@angular/universal-next-prototype/hello-world/src/main.browser.ts
+++ modules/@angular/universal-next-prototype/hello-world/src/main.browser.ts
@@ -1,6 +1,12 @@
+import {bootstrap} from '@angular/platform-browser-dynamic';
+import {HTTP_PROVIDERS} from '@angular/http';
 import {isBrowser} from '@angular/universal/browser';
 
+import {App, APP_PROVIDERS} from './app';
 export function main() {
   console.log('isBrowser', isBrowser);
-
+  return bootstrap(App, [
+    ...APP_PROVIDERS,
+    ...HTTP_PROVIDERS
+  ]);
 }
