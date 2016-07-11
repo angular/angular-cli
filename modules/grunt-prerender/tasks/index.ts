f53e58ef655d335e6c051e05a03c1a921cbe8111
Jeff Cross <middlefloor@gmail.com>
2016-07-11 16:52:28 -0700

    chore(grunt): rename src to tasks

--- modules/grunt-prerender/tasks/index.ts
+++ modules/grunt-prerender/tasks/index.ts
@@ -1,6 +1,6 @@
-import * as Prerender from './src/prerender';
+import * as Prerender from './prerender';
 
-export * from './src/prerender';
+export * from './prerender';
 
 // needs to be written like this otherwise Grunt will fail to load this task
 module.exports = Prerender;
