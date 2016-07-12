3db9d02017ec86f645b1fbe4e5f6b42348c68831
Filipe Silva <filipematossilva@gmail.com>
2016-07-12 01:11:10 +0100

    chore: update to ts2.0 beta

--- addon/ng2/blueprints/ng2/files/__path__/typings.d.ts
+++ addon/ng2/blueprints/ng2/files/__path__/typings.d.ts
@@ -2,5 +2,4 @@
 // https://github.com/typings/typings
 // https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
 
-/// <reference path="<%= refToTypings %>/typings/browser.d.ts" />
 <% if(!isMobile) { %>declare var module: { id: string };<% } %>
