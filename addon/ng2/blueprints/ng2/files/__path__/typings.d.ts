5aaddfd1f3a47fb04b131de8f76460995c87286b
Jan Kuri <jkuri88@gmail.com>
2016-07-15 22:29:23 +0200

    chore(): typings upgrade to 1.x (#1353)

--- addon/ng2/blueprints/ng2/files/__path__/typings.d.ts
+++ addon/ng2/blueprints/ng2/files/__path__/typings.d.ts
@@ -2,5 +2,5 @@
 // https://github.com/typings/typings
 // https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
 
-/// <reference path="<%= refToTypings %>/typings/browser.d.ts" />
+/// <reference path="<%= refToTypings %>/typings/index.d.ts" />
 <% if(!isMobile) { %>declare var module: { id: string };<% } %>
