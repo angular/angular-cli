4189b9bb4a88819289136d85314c8d2b81b5a3bc
Filipe Silva <filipematossilva@gmail.com>
2016-07-15 05:04:14 +0100

    chore: fix test warnings

--- addon/ng2/blueprints/ng2/files/__path__/test.ts
+++ addon/ng2/blueprints/ng2/files/__path__/test.ts
@@ -1,20 +1,20 @@
 
 /*global jasmine, __karma__, window*/
-import 'core-js/es6'
-import 'core-js/es7/reflect'
+import 'core-js/es6';
+import 'core-js/es7/reflect';
 
 // Typescript emit helpers polyfill
-import 'ts-helpers'
+import 'ts-helpers';
 
-import 'zone.js/dist/zone'
-import 'zone.js/dist/long-stack-trace-zone'
-import 'zone.js/dist/jasmine-patch'
-import 'zone.js/dist/async-test'
-import 'zone.js/dist/fake-async-test'
-import 'zone.js/dist/sync-test'
+import 'zone.js/dist/zone';
+import 'zone.js/dist/long-stack-trace-zone';
+import 'zone.js/dist/jasmine-patch';
+import 'zone.js/dist/async-test';
+import 'zone.js/dist/fake-async-test';
+import 'zone.js/dist/sync-test';
 
 // RxJS
-import 'rxjs/Rx'
+import 'rxjs/Rx';
 
 Promise.all([
     System.import('@angular/core/testing'),
