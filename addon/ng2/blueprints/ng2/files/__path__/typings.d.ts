2e06ed7ddbcbcc25eacd5575e0494462f443cf7c
Filipe Silva <filipematossilva@gmail.com>
2016-07-22 23:25:58 +0100

    chore(blueprints): re-add outDir

--- addon/ng2/blueprints/ng2/files/__path__/typings.d.ts
+++ addon/ng2/blueprints/ng2/files/__path__/typings.d.ts
@@ -2,7 +2,7 @@
 // https://github.com/typings/typings
 // https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
 
-/// <reference path="<%= refToTypings %>/typings/index.d.ts" />
+/// <reference path="<%= relativeRootPath %>/typings/index.d.ts" />
 <% if(!isMobile) { %>
 declare var System: any;
 <% if(!isMobile) { %>declare var module: { id: string };<% } %>
