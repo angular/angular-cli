29f899c53930a6fd3cf06fb10b9a9cac551229a4
Sean Larkin <sean.larkin@cuw.edu>
2016-07-22 14:25:31 -0500

    fix: update to awesome-typescript-loader 2.1.0 (#1425)
    * fix: update to awesome-typescript-loader 2.1.0 which fixes a few issues shimming typings libs in tsconfig.json
    
    * fix: forgot to set libs in tsconfig blueprint

--- addon/ng2/blueprints/ng2/files/__path__/typings.d.ts
+++ addon/ng2/blueprints/ng2/files/__path__/typings.d.ts
@@ -3,10 +3,6 @@
 // https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
 
 /// <reference path="<%= refToTypings %>/typings/index.d.ts" />
-/// <reference path="../node_modules/typescript/lib/lib.es2015.core.d.ts" />
-/// <reference path="../node_modules/typescript/lib/lib.es2015.collection.d.ts" />
-/// <reference path="../node_modules/typescript/lib/lib.es2015.promise.d.ts" />
-
 <% if(!isMobile) { %>
 declare var System: any;
 <% if(!isMobile) { %>declare var module: { id: string };<% } %>
