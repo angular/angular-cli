660d4349b13896ee5310b750e719e49621f4ff52
Ashok Tamang <ashoktamang52@gmail.com>
2016-08-02 13:52:19 -0700

    feat: add features in get-dependent-files.ts
    add logic to filter out spec file associated with the given component unit while getting all dependent files
    add utlity function to get all the files (.html/stylesheets/.spec.ts) associated with the given component unit
    change the constructor method of the class ModuleResolver (add 'rootPath' as an additional parameter)

--- addon/ng2/utilities/get-dependent-files.ts
+++ addon/ng2/utilities/get-dependent-files.ts
@@ -75,6 +75,26 @@ export function hasIndexFile(dirPath: string): Promise<Boolean> {
 }
 
 /**
+ * Function to get all the templates, stylesheets, and spec files of a given component unit
+ * 
+ * @param fileName 
+ * 
+ * @return absolute paths of '.html/.css/.sass/.spec.ts' files associated with the given file.
+ *
+ */
+export function getAllCorrespondingFiles(fileName: string): Promise<string[]> {
+  let fileDirName = path.dirname(fileName);
+  let componentName = path.basename(fileName).split('.')[0];
+  const globSearch = denodeify(glob);
+  return globSearch(path.join(fileDirName, `${componentName}.*`), { nodir: true })
+    .then((files: string[]) => {
+      return files.filter((file) => {
+        return (path.basename(file) !== 'index.ts');
+      });
+    });
+}
+
+/**
  * Returns a map of all dependent file/s' path with their moduleSpecifier object
  * (specifierText, pos, end)
  * 
@@ -87,6 +107,17 @@ export function hasIndexFile(dirPath: string): Promise<Boolean> {
 export function getDependentFiles(fileName: string, rootPath: string): Promise<ModuleMap> {
   const globSearch = denodeify(glob);
   return globSearch(path.join(rootPath, '**/*.*.ts'), { nodir: true })
+    .then((files: string[]) => {
+      // Filter out the spec file associated with to-be-promoted component unit.
+      let fileBaseName = path.basename(fileName, '.ts');
+      return files.filter((file) => {
+        if (path.extname(path.basename(file, '.ts')) === '.spec') {
+          return path.basename(path.basename(file, '.ts'), '.spec') !== fileBaseName;
+        } else {
+          return true;
+        }
+      });
+    })
     .then((files: string[]) => Promise.all(files.map(file => createTsSourceFile(file)))
     .then((tsFiles: ts.SourceFile[]) => tsFiles.map(file => getImportClauses(file)))
     .then((moduleSpecifiers: ModuleImport[][]) => {
