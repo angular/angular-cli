e85899ec48d749da57ae809d394cc1f89e076d51
Jan Kuri <jkuri88@gmail.com>
2016-08-06 18:20:42 +0200

    refactor(NgModule): update to RC5

--- addon/ng2/blueprints/directive/index.js
+++ addon/ng2/blueprints/directive/index.js
@@ -3,6 +3,8 @@ var Blueprint = require('ember-cli/lib/models/blueprint');
 var dynamicPathParser = require('../../utilities/dynamic-path-parser');
 var addBarrelRegistration = require('../../utilities/barrel-management');
 var getFiles = Blueprint.prototype.files;
+const stringUtils = require('ember-cli-string-utils');
+const astUtils = require('../../utilities/ast-utils');
 
 module.exports = {
   description: '',
@@ -52,15 +54,29 @@ module.exports = {
   },
 
   afterInstall: function(options) {
+    var returns = [];
+    var modulePath = path.resolve(process.env.PWD, this.dynamicPath.appRoot, 'app.module.ts');
+    var classifiedName = 
+      stringUtils.classify(options.entity.name);
+    var importPath = '\'./' + stringUtils.dasherize(`${options.entity.name}.directive';`);
+
     if (!options.flat) {
-      return addBarrelRegistration(
-        this,
-        this.generatePath);
+      returns.push(function() { 
+        return addBarrelRegistration(this, this.generatePath) 
+      });
     } else {
-      return addBarrelRegistration(
-        this,
-        this.generatePath,
-        options.entity.name + '.directive');
+      returns.push(function() { 
+        return addBarrelRegistration(
+          this,
+          this.generatePath,
+          options.entity.name + '.directive')
+      });
     }
+
+    if (!options['skip-import']) {
+      returns.push(astUtils.importComponent(modulePath, classifiedName, importPath));
+    }
+
+    return Promise.all(returns);
   }
 };
