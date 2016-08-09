81f69aae57c2ddbe4221e6f3d467b88b7d7559f9
PatrickJS <github@gdi2290.com>
2016-08-09 15:54:39 -0700

    refactor(examples): move code into universal

--- examples/next-hello-world/src/main.node.ts
+++ examples/next-hello-world/src/main.node.ts
@@ -1,184 +1,32 @@
-import { platformCoreDynamic } from '@angular/compiler';
-// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import {
-  DOCUMENT,
-  // BrowserModule
-  BROWSER_SANITIZATION_PROVIDERS,
-  EVENT_MANAGER_PLUGINS,
-  AnimationDriver,
-  EventManager,
-  // DomSharedStylesHost
-  // DomRootRenderer
-} from '@angular/platform-browser';
-
-
-// PRIVATE
-import { Parse5DomAdapter } from '@angular/platform-server/src/parse5_adapter';
-import { DomEventsPlugin } from '@angular/platform-browser/src/dom/events/dom_events'
-import { KeyEventsPlugin } from '@angular/platform-browser/src/dom/events/key_events'
-import {
-  HammerGesturesPlugin,
-  // EventManager,
-  HAMMER_GESTURE_CONFIG,
-  HammerGestureConfig
-} from '@angular/platform-browser/src/dom/events/hammer_gestures'
-import { DomSharedStylesHost, SharedStylesHost } from '@angular/platform-browser/src/dom/shared_styles_host';
-import { DomRootRenderer } from '@angular/platform-browser/src/dom/dom_renderer';
-import { wtfInit } from '@angular/core/src/profile/wtf_init';
-// PRIVATE
-
-
-// import {APP_BASE_HREF} from '@angular/common';
-import {
-  ExceptionHandler,
-  RootRenderer,
-  Testability,
-  ApplicationModule,
-  createPlatformFactory,
-  PLATFORM_INITIALIZER,
-
-
-  NgModule,
-  ComponentRef,
-  ApplicationRef,
-  NgModuleRef
-} from '@angular/core';
-
-import {
-  CommonModule,
-  PlatformLocation
-} from '@angular/common';
-
-import {
-  parseDocument,
-  serializeDocument,
-  provideDocument,
-  NodeDomRootRenderer_,
-  NodeSharedStylesHost,
-  NodePlatformLocation
-  // ORIGIN_URL,
-  // REQUEST_URL,
-  // NODE_LOCATION_PROVIDERS
-} from '@angular/universal';
-
-import { getDOM } from '@angular/platform-browser/src/dom/dom_adapter';
+import { NgModule } from '@angular/core';
+import { NodeModule, platformDynamicNode } from '@angular/universal';
 
 import { App } from './app';
 
-export function _exceptionHandler(): ExceptionHandler {
-  return new ExceptionHandler(getDOM());
-}
 
-export function _document(): any {
-  return parseDocument(`<!doctype>
-<html lang="en">
-<head>
-  <title>Angular 2 Universal Starter</title>
-  <meta charset="UTF-8">
-  <meta name="description" content="Angular 2 Universal">
-  <meta name="keywords" content="Angular 2,Universal">
-  <meta name="author" content="PatrickJS">
+export const platform = platformDynamicNode([
 
-  <link rel="icon" href="data:;base64,iVBORw0KGgo=">
+]);
 
-  <base href="/">
-<body>
+export function main(doc) {
 
-  <app>
-    Loading...
-  </app>
+  @NgModule({
+    bootstrap: [ App ],
+    declarations: [ App ],
+    imports: [
+      NodeModule.forDocument(doc)
+    ],
+    providers: [
 
-  <script src="dist/public/browser-bundle.js"></script>
-</body>
-</html>
-`)
-}
-
-export function _resolveDefaultAnimationDriver(): AnimationDriver {
-  if (getDOM().supportsWebAnimation()) {
-    return AnimationDriver.NOOP;
-  }
-  return AnimationDriver.NOOP;
-}
-
-
-@NgModule({
-  providers: [
-    BROWSER_SANITIZATION_PROVIDERS,
-    { provide: ExceptionHandler, useFactory: _exceptionHandler, deps: [] },
-    { provide: DOCUMENT, useFactory: _document, deps: [] },
-    { provide: EVENT_MANAGER_PLUGINS, useClass: DomEventsPlugin, multi: true },
-    { provide: EVENT_MANAGER_PLUGINS, useClass: KeyEventsPlugin, multi: true },
-    { provide: EVENT_MANAGER_PLUGINS, useClass: HammerGesturesPlugin, multi: true },
-    { provide: HAMMER_GESTURE_CONFIG, useClass: HammerGestureConfig },
-
-    { provide: DomRootRenderer, useClass: NodeDomRootRenderer_ },
-    { provide: RootRenderer, useExisting: DomRootRenderer },
-
-    NodeSharedStylesHost,
-    {provide: SharedStylesHost, useExisting: NodeSharedStylesHost},
-    {provide: DomSharedStylesHost, useExisting: NodeSharedStylesHost},
-
-
-    { provide: AnimationDriver, useFactory: _resolveDefaultAnimationDriver },
-    Testability,
-    EventManager,
-    // ELEMENT_PROBE_PROVIDERS
-  ],
-  exports: [  CommonModule, ApplicationModule  ]
-})
-export class NodeModule {
-}
-
-
-function initParse5Adapter() {
-  Parse5DomAdapter.makeCurrent();
-  wtfInit();
-}
-
-
-export const INTERNAL_NODE_PLATFORM_PROVIDERS: Array<any /*Type | Provider | any[]*/> = [
-  {provide: PLATFORM_INITIALIZER, useValue: initParse5Adapter, multi: true},
-  {provide: PlatformLocation, useClass: NodePlatformLocation},
-];
-
-
-/**
- * The node platform that supports the runtime compiler.
- *
- * @experimental
- */
-export const platformDynamicNode =
-    createPlatformFactory(platformCoreDynamic, 'nodeDynamic', INTERNAL_NODE_PLATFORM_PROVIDERS);
-
-
-@NgModule({
-  bootstrap: [ App ],
-  declarations: [ App ],
-  imports: [
-    NodeModule
-  ],
-  providers: [
-    { provide: DOCUMENT, useFactory: _document },
-    // {provide: DomRootRenderer, useClass: NodeDomRootRenderer_},
-    // {provide: RootRenderer, useExisting: DomRootRenderer},
-    // {provide: SharedStylesHost, useExisting: DomSharedStylesHost},
-
-  ]
-})
-export class MainModule {}
-
-export const platform = platformDynamicNode();
-
-export function main() {
+    ]
+  })
+  class MainModule {}
 
   return platform
-    .bootstrapModule(MainModule)
-    .then((moduleRef: NgModuleRef<MainModule>) => {
+    .serializeModule(MainModule)
+    .then((html) => {
       console.log('done')
-      let document = moduleRef.injector.get(DOCUMENT);
-      return serializeDocument(document);
+      return html
     });
 };
 
