75bc0cc4e29d41946e9ab2749486153fac02428a
Mike Brocchi <mbrocchi@gmail.com>
2016-08-11 14:15:29 -0400

    chore(module): remove extra imports from app module

--- addon/ng2/blueprints/ng2/files/__path__/app/app.module.ts
+++ addon/ng2/blueprints/ng2/files/__path__/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, ApplicationRef } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+import { HttpModule } from '@angular/http'
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,13 +11,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    CommonModule,
-    FormsModule
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   entryComponents: [AppComponent],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-
-}
+export class AppModule { }
