f40e6f14282669167c7f821a198b2075c4991914
Mike Brocchi <mbrocchi@gmail.com>
2016-08-11 13:25:03 -0400

    feat(module): add generation of modules
    Close #1650

---
+++ addon/ng2/blueprints/module/files/__path__/__name__.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  imports: [ CommonModule ],
+  declarations: []
+})
+export default class <%= classifiedModuleName %>Module { }
