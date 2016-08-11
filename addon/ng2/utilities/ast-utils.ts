c3452ea402484e98ac1940b7d49407bb6e03b32d
Hans Larsen <hans@hansl.ca>
2016-08-10 21:07:16 -0700

    Fix findNodes WRT tests.

--- addon/ng2/utilities/ast-utils.ts
+++ addon/ng2/utilities/ast-utils.ts
@@ -57,25 +57,28 @@ export function getSourceNodes(sourceFile: ts.SourceFile): Observable<ts.Node> {
 
 
 /**
-* Find all nodes from the AST in the subtree of node of SyntaxKind kind.
-* @param node 
-* @param kind
-* @return all nodes of kind, or [] if none is found
+ * Find all nodes from the AST in the subtree of node of SyntaxKind kind.
+ * @param node
+ * @param kind
+ * @param max The maximum number of items to return.
+ * @return all nodes of kind, or [] if none is found
 */
 export function findNodes(node: ts.Node, kind: ts.SyntaxKind, max: number = Infinity): ts.Node[] {
   if (!node || max == 0) {
     return [];
   }
 
-  let arr: ts.Node[] = new Array(Math.min(max, 5));
+  let arr: ts.Node[] = [];
   if (node.kind === kind) {
     arr.push(node);
     max--;
   }
   if (max > 0) {
     for (const child of node.getChildren()) {
-      findNodes(child, kind).forEach(node => {
-        arr.push(node);
+      findNodes(child, kind, max).forEach(node => {
+        if (max > 0) {
+          arr.push(node);
+        }
         max--;
       });
 
