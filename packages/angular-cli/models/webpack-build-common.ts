906becc28e7bb5edde227200086f142e6cc063ef
LasTanzen <mail@stefannegele.de>
2016-08-19 20:34:18 +0200

    feat(build): add loaders for fonts
    Close #1765

--- packages/angular-cli/models/webpack-build-common.ts
+++ packages/angular-cli/models/webpack-build-common.ts
@@ -97,9 +97,8 @@ export function getWebpackCommonConfig(
         { test: /\.(jpg|png|gif)$/, loader: 'url-loader?limit=10000' },
         { test: /\.html$/, loader: 'raw-loader' },
 
-        { test: /\.(otf|woff|ttf|svg)$/, loader: 'url?limit=10000' },
-        { test: /\.woff2$/, loader: 'url?limit=10000&mimetype=font/woff2' },
-        { test: /\.eot$/, loader: 'file' }
+        { test: /\.(otf|ttf|woff|woff2)$/, loader: 'url?limit=10000' },
+        { test: /\.(eot|svg)$/, loader: 'file' }
       ]
     },
     plugins: [
