b6015eb5bf03692953584a97d0a1bc82ead5014c
shairez <psyrez@gmail.com>
2016-08-24 03:29:50 +0300

    refactor(AppModule): removed entryComponents

--- addon/ng2/blueprints/ng2/files/__path__/app/app.module.ts
+++ addon/ng2/blueprints/ng2/files/__path__/app/app.module.ts
@@ -15,7 +15,6 @@ import { AppComponent } from './app.component';
     HttpModule
   ],
   providers: [],
-  entryComponents: [AppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
