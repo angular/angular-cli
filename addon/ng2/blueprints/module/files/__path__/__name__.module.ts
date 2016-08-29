1f4c6fe3e5ac6812f1c40850a086f54b7b698ae3
Mike Brocchi <mbrocchi@gmail.com>
2016-08-29 00:05:32 -0400

    feat(feature): add ability to generate feature modules (#1867)

--- addon/ng2/blueprints/module/files/__path__/__name__.module.ts
+++ addon/ng2/blueprints/module/files/__path__/__name__.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { routing } from './<%= dasherizedModuleName %>.routes';
+import { <%= classifiedModuleName %>Component } from './<%= dasherizedModuleName %>.component';
 
 @NgModule({
-  imports: [ CommonModule ],
-  declarations: []
+  imports: [
+    CommonModule,
+    routing
+  ],
+  declarations: [
+    <%= classifiedModuleName %>Component
+  ]
 })
-export default class <%= classifiedModuleName %>Module { }
+export class <%= classifiedModuleName %>Module { }
