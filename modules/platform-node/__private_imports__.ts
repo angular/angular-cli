4718c7ef2ee5aa608f05c061a0e39eaae8390f1d
PatrickJS <github@gdi2290.com>
2016-09-01 05:32:19 -0700

    refactor: import export privates

---
+++ modules/platform-node/__private_imports__.ts
@@ -0,0 +1,60 @@
+import { __platform_browser_private__ } from '@angular/platform-browser';
+import { __core_private__ } from '@angular/core';
+import { __compiler_private__ } from '@angular/compiler';
+
+// PRIVATE
+const {
+  BROWSER_SANITIZATION_PROVIDERS,
+  SharedStylesHost,
+  DomSharedStylesHost,
+  DomRootRenderer,
+  DomEventsPlugin,
+  KeyEventsPlugin,
+  DomAdapter,
+  setRootDomAdapter,
+  getDOM,
+  HammerGesturesPlugin
+} = __platform_browser_private__;
+
+const {
+  ViewUtils,
+  AnimationKeyframe,
+  AnimationPlayer,
+  AnimationStyles,
+  RenderDebugInfo
+} = __core_private__;
+
+const {
+  SelectorMatcher,
+  CssSelector
+} = __compiler_private__;
+
+
+
+
+export {
+  // platform-browser
+  BROWSER_SANITIZATION_PROVIDERS,
+  SharedStylesHost,
+  DomSharedStylesHost,
+  DomRootRenderer,
+  DomEventsPlugin,
+  DomAdapter,
+  setRootDomAdapter,
+  KeyEventsPlugin,
+  getDOM,
+  HammerGesturesPlugin,
+
+  // compiler
+  SelectorMatcher,
+  CssSelector,
+
+
+  // core
+  ViewUtils,
+  AnimationKeyframe,
+  AnimationPlayer,
+  AnimationStyles,
+  RenderDebugInfo,
+
+}
