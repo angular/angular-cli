95a5d38d007011d354347d7e5083a7f37503df24
KevinYang <chgc_kevin@hotmail.com>
2016-09-02 23:13:57 +0800

    #1719 #1736

--- packages/ast-tools/src/route-utils.ts
+++ packages/ast-tools/src/route-utils.ts
@@ -79,6 +79,7 @@ export function bootstrapItem(
 
 export function insertImport(fileToEdit: string, symbolName: string,
                                   fileName: string, isDefault = false): Change {
+  fileName = fileName.replace(/\\/, '/'); // correction in windows
   let rootNode = getRootNode(fileToEdit);
   let allImports = findNodes(rootNode, ts.SyntaxKind.ImportDeclaration);
 
