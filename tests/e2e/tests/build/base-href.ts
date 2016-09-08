2bf486455f8945cb82b6f2f9ecfc6be866a0fc43
hansl <hans@hansl.ca>
2016-09-08 12:29:16 -0700

    .

--- tests/e2e/tests/build/base-href.ts
+++ tests/e2e/tests/build/base-href.ts
@@ -4,5 +4,11 @@ import {expectFileToMatch} from '../../utils/fs';
 
 export default function() {
   return ng('build', '--base-href', '/myUrl')
+  .then(() => {
+    const fs = require('fs');
+    console.log(fs.readdirSync('./'));
+    console.log('--===--');
+    console.log(fs.readdirSync('dist/'));
+  })
     .then(() => expectFileToMatch('dist/index.html', /<base href="\/myUrl">/));
 }
