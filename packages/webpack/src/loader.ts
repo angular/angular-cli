ee0a940b24e3296e5ff22228c838d75ea2f34107
Rob Wormald <robwormald@gmail.com>
2016-09-08 22:11:35 -0700

    feature(compiler): add ngc compiler host impl

---
+++ packages/webpack/src/loader.ts
@@ -0,0 +1,8 @@
+import * as path from 'path'
+import * as fs from 'fs'
+import * as ts from 'typescript'
+
+export function ngcLoader(sourceFile){
+
+  return ts.transpileModule(sourceFile, {compilerOptions: {target: ts.ScriptTarget.ES5, module: ts.ModuleKind.ES2015}}).outputText;
+}
