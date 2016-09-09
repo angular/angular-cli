5820cec9cfea0f4195b9b01a26dc392114df44a2
Hans Larsen <hans@hansl.ca>
2016-09-09 16:05:02 -0700

    fix: lazy loading now works as expected with latest webpack

--- tests/acceptance/find-lazy-module.spec.ts
+++ tests/acceptance/find-lazy-module.spec.ts
@@ -1,7 +1,7 @@
 import * as mockFs from 'mock-fs';
 import {stripIndents} from 'common-tags';
 import {expect} from 'chai';
-import {join} from 'path';
+import {join, sep} from 'path';
 
 import {findLazyModules} from '../../addon/ng2/models/find-lazy-modules';
 
@@ -39,11 +39,12 @@ describe('find-lazy-module', () => {
   afterEach(() => mockFs.restore());
 
   it('works', () => {
-    expect(findLazyModules('project-root')).to.eql([
-      './' + join('.', 'moduleA.ts'),
-      './' + join('.', 'moduleB.ts'),
-      './' + join('.', 'moduleC.ts'),
-      './' + join('.', 'app/+workspace/+settings/settings.module.ts')
-    ]);
+    expect(findLazyModules('project-root')).to.eql({
+      'moduleA': join(process.cwd(), 'project-root', 'moduleA.ts'),
+      'moduleB': join(process.cwd(), 'project-root', 'moduleB.ts'),
+      'moduleC': join(process.cwd(), 'project-root', 'moduleC.ts'),
+      'app/+workspace/+settings/settings.module':
+          join(process.cwd(), 'project-root', 'app/+workspace/+settings/settings.module.ts'),
+    });
   });
 });
