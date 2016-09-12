549ba1aa568f3bea62fd9faf81e7aac97f597ab9
Hans Larsen <hans@hansl.ca>
2016-09-11 18:54:06 -0700

    build is now a script

--- scripts/publish/build.js
+++ scripts/publish/build.js
@@ -1,23 +1,18 @@
 #!/usr/bin/env node
 'use strict';
 
+const child_process = require('child_process');
+const denodeify = require('denodeify');
 const fs = require('fs');
-const glob = require('glob');
+const glob = denodeify(require('glob'));
 const path = require('path');
+const resolveBin = denodeify(require('resolve-bin'));
+const rimraf = denodeify(require('rimraf'));
 
 
 const root = path.join(__dirname, '../..');
 const dist = path.join(root, 'dist');
 const packagesRoot = path.join(root, 'packages');
-
-if (!fs.existsSync(dist)) {
-  throw new Error('Couldn\'t find dist folder... Did you build?');
-}
-
-if (!fs.existsSync(packagesRoot)) {
-  throw new Error('Could not find angular-cli root.');
-}
-
 
 function copy(from, to) {
   from = path.relative(process.cwd(), from);
@@ -35,48 +30,112 @@ function copy(from, to) {
 }
 
 
-glob.sync(path.join(packagesRoot, '**/*'))
-  .map((fileName) => path.relative(packagesRoot, fileName))
-  .filter((fileName) => {
-    if (/^angular-cli\/blueprints\//.test(fileName)) {
-      return true;
-    }
-    if (/\.d\.ts$/.test(fileName)) {
-      // The last thing we want is d.ts files...
-      return false;
-    }
-    if (/\.spec\.ts$/.test(fileName)) {
-      // Also spec.ts files...
-      return false;
-    }
-    if (/\.ts$/.test(fileName)) {
-      // Verify that it was actually built.
-      if (!fs.existsSync(path.join(dist, fileName).replace(/ts$/, 'js'))) {
-        throw new Error(`Source found but compiled file not found: "${fileName}".`);
-      }
+function patch() {
+  const filePath = path.join(__dirname, '../../node_modules/@types/common-tags/common-tags.d.ts');
+  const content = fs.readFileSync(filePath, 'utf8')
+    .replace(/literals: string\[\]/, 'literals: TemplateStringsArray');
+  fs.writeFileSync(filePath, content, 'utf8');
+}
 
-      // Skip all source files, since we have their JS files now.
-      return false;
-    }
 
-    // The only remaining file we want to ignore is tsconfig and spec files.
-    return !(/tsconfig\.json$/.test(fileName))
-        && !(/\.spec\./.test(fileName))
-        && !(/\/tests\//.test(fileName));
+function tsc() {
+  const args = Array.prototype.slice.call(arguments);
+  return resolveBin('typescript', { executable: 'tsc' })
+    .then(tscBin => {
+      const child = child_process.spawn(tscBin, args);
+
+      child.stdout.on('data', data => console.log(data.toString()));
+      child.stderr.on('data', data => console.error(data.toString()));
+
+      return new Promise((resolve, reject) => {
+        child.on('error', (err) => reject(err));
+        child.on('close', (err) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve();
+          }
+        });
+      })
+    });
+}
+
+
+// First delete the dist folder.
+Promise.resolve()
+  .then(() => console.log('Deleting dist folder...'))
+  .then(() => rimraf(dist))
+  .then(() => console.log('Patching node_modules typings...'))
+  .then(() => patch())
+  .then(() => console.log('Compiling packages...'))
+  .then(() => {
+    const packages = require('../../lib/packages');
+    return Object.keys(packages)
+      .reduce((promise, packageName) => {
+        const pkg = packages[packageName];
+        const name = path.relative(packagesRoot, pkg.root);
+
+        return promise.then(() => {
+          console.log(`  ${name}`);
+          return tsc('-p', pkg.root)
+        });
+      }, Promise.resolve());
   })
-  .forEach((fileName) => {
-    const source = path.join(packagesRoot, fileName);
-    const dest = path.join(dist, fileName);
-
-    if (fs.statSync(source).isDirectory()) {
-      try {
-        fs.mkdirSync(dest);
-      } catch (err) {
-        if (err.code != 'EEXIST') {
-          throw err;
+  .then(() => console.log('Copying uncompiled resources...'))
+  .then(() => glob(path.join(packagesRoot, '**/*')))
+  .then(files => {
+    console.log(`  Found ${files.length} files...`);
+    files
+      .map((fileName) => path.relative(packagesRoot, fileName))
+      .filter((fileName) => {
+        if (/^angular-cli.blueprints/.test(fileName)) {
+          return true;
         }
-      }
-    } else {
-      copy(source, dest);
-    }
+        if (/\.d\.ts$/.test(fileName)) {
+          // The last thing we want is d.ts files...
+          return false;
+        }
+        if (/\.spec\.ts$/.test(fileName)) {
+          // Also spec.ts files...
+          return false;
+        }
+        if (/spec-utils.ts/.test(fileName)) {
+          // TODO: get rid of this by splitting spec utils in its own package.
+          return false;
+        }
+        if (/\.ts$/.test(fileName)) {
+          // Verify that it was actually built.
+          if (!fs.existsSync(path.join(dist, fileName).replace(/ts$/, 'js'))) {
+            throw new Error(`Source found but compiled file not found: "${fileName}".`);
+          }
+
+          // Skip all source files, since we have their JS files now.
+          return false;
+        }
+
+        // The only remaining file we want to ignore is tsconfig and spec files.
+        return !(/tsconfig\.json$/.test(fileName))
+          && !(/\.spec\./.test(fileName))
+          && !(/[\/\\]tests[\/\\]/.test(fileName));
+      })
+      .forEach((fileName) => {
+        const source = path.join(packagesRoot, fileName);
+        const dest = path.join(dist, fileName);
+
+        if (fs.statSync(source).isDirectory()) {
+          try {
+            fs.mkdirSync(dest);
+          } catch (err) {
+            if (err.code != 'EEXIST') {
+              throw err;
+            }
+          }
+        } else {
+          copy(source, dest);
+        }
+      });
+  })
+  .then(() => process.exit(0), (err) => {
+    console.error(err);
+    process.exit(1);
   });
