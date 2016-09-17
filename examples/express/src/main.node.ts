ed931e56ba53fabcd28b08500d81fce69de31c3d
PatrickJS <github@gdi2290.com>
2016-09-16 20:50:40 -0700

    refactor(examples): use app

--- examples/express/src/main.node.ts
+++ examples/express/src/main.node.ts
@@ -2,17 +2,19 @@ import { NgModule, Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { UniversalModule } from 'angular2-universal/node';
 
-@Component({
-  selector: 'app',
-  template: 'Hello Universal App'
-})
-class App {
+import { App, Wat } from './app';
 
-}
+// @Component({
+//   selector: 'app',
+//   template: 'Hello Universal App'
+// })
+// class App {
+
+// }
 
 @NgModule({
   bootstrap: [ App ],
-  declarations: [ App ],
+  declarations: [ App, Wat ],
   imports: [
     UniversalModule,
     FormsModule
