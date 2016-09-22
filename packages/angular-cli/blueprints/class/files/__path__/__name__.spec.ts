91c8cda82284932bd86590c6eb34c36727459650
Danny Blue <dannybluedesign@gmail.com>
2016-09-22 08:00:25 -0400

    remove unneeded imports

--- packages/angular-cli/blueprints/class/files/__path__/__name__.spec.ts
+++ packages/angular-cli/blueprints/class/files/__path__/__name__.spec.ts
@@ -1,6 +1,3 @@
-/* tslint:disable:no-unused-variable */
-
-import { addProviders, async, inject } from '@angular/core/testing';
 import {<%= classifiedModuleName %>} from './<%= fileName %>';
 
 describe('<%= classifiedModuleName %>', () => {
