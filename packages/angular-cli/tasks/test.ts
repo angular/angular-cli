255f7b961914ea74aefaceb45a27bf927c41b359
jodek <joris.dekker@ictu.nl>
2016-09-24 17:25:35 +0200

    requiring karma the default way, to be able to cope with having karma installed in a non-default directory (i.e.: in parent's node_modules)

--- packages/angular-cli/tasks/test.ts
+++ packages/angular-cli/tasks/test.ts
@@ -1,18 +1,11 @@
 const Task = require('ember-cli/lib/models/task');
 import * as path from 'path';
 
-// require dependencies within the target project
-function requireDependency(root: string, moduleName: string) {
-  const packageJson = require(path.join(root, 'node_modules', moduleName, 'package.json'));
-  const main = path.normalize(packageJson.main);
-  return require(path.join(root, 'node_modules', moduleName, main));
-}
-
 export default Task.extend({
   run: function (options: any) {
     const projectRoot = this.project.root;
     return new Promise((resolve) => {
-      const karma = requireDependency(projectRoot, 'karma');
+      const karma = require('karma');
       const karmaConfig = path.join(projectRoot, this.project.ngConfig.config.test.karma.config);
 
       // Convert browsers from a string to an array
