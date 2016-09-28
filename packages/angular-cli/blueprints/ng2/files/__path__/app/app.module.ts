6fb06e9e9045ef2eecccb1d8652a845886e8cec9
Sarun Intaralawan <sarun.intaralawan@gmail.com>
2016-09-29 01:14:21 +0700

    fix(blueprint): fix routing module import

--- packages/angular-cli/blueprints/ng2/files/__path__/app/app.module.ts
+++ packages/angular-cli/blueprints/ng2/files/__path__/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';<% if (routing) { %>
-import { <%= classifiedModuleName %>RoutingModule } from './<%= dasherizedModuleName %>-routing.module';<% } %>
+import { <%= classifiedModuleName %>RoutingModule } from './app-routing.module';<% } %>
 
 import { AppComponent } from './app.component';
 
