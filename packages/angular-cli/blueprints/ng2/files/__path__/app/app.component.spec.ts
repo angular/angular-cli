78c2b9be900fec2c204fb2a7657579f6e482cab7
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2016-09-28 20:33:19 +0200

    fix(generate): use prefix when initializing app (#2046) (#2367)
    E2E test would fail when using a prefix to generate the app

--- packages/angular-cli/blueprints/ng2/files/__path__/app/app.component.spec.ts
+++ packages/angular-cli/blueprints/ng2/files/__path__/app/app.component.spec.ts
@@ -21,13 +21,13 @@ describe('App: <%= jsComponentName %>', () => {
   it(`should have as title 'app works!'`, async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     let app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
+    expect(app.title).toEqual('<%= prefix %> works!');
   }));
 
   it('should render title in a h1 tag', async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     let compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    expect(compiled.querySelector('h1').textContent).toContain('<%= prefix %> works!');
   }));
 });
