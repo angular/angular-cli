0b0928942a8edfcb0cbc49c9c4f8794af1a2b718
Danny Blue <dannybluedesign@gmail.com>
2016-10-03 14:30:43 -0400

    remove accidental arg

--- tests/e2e/tests/generate/pipe.ts
+++ tests/e2e/tests/generate/pipe.ts
@@ -7,7 +7,7 @@ export default function() {
   // Create the pipe in the same directory.
   const pipeDir = join('src', 'app');
 
-  return ng('generate', 'pipe', 'test-pipe', '--spec')
+  return ng('generate', 'pipe', 'test-pipe')
     .then(() => expectFileToExist(pipeDir))
     .then(() => expectFileToExist(join(pipeDir, 'test-pipe.pipe.ts')))
     .then(() => expectFileToExist(join(pipeDir, 'test-pipe.pipe.spec.ts')))
