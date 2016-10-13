45714c2b91fd9793c7824ef9d19dab257625051d
Meligy <eng.meligy@gmail.com>
2016-10-13 22:18:16 +1100

    feat(aot):export default routes array to work with AoT compiler

--- packages/angular-cli/blueprints/module/files/__path__/__name__-routing.module.ts
+++ packages/angular-cli/blueprints/module/files/__path__/__name__-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+export const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
