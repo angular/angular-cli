bf9977e49a9c72fbce39cad5f81faf3a99ca9440
hollannikas <hollannikas@users.noreply.github.com>
2016-10-18 22:43:06 +0300

    feat(build): use appConfig.index to set output index file
    Pass appConfig.index to the HtmlWebpackPlugin’s filename property.
    
    Closes #2241
    Closes #2767

--- packages/angular-cli/models/webpack-build-common.ts
+++ packages/angular-cli/models/webpack-build-common.ts
@@ -86,6 +86,7 @@ export function getWebpackCommonConfig(
     plugins: [
       new HtmlWebpackPlugin({
         template: path.resolve(appRoot, appConfig.index),
+        filename: path.resolve(appConfig.outDir, appConfig.index),
         chunksSortMode: 'dependency'
       }),
       new BaseHrefWebpackPlugin({
