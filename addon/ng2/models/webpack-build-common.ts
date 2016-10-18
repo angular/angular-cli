5a341140bfb5fdd426209e70fa53ef6eaa571a81
LasTanzen <mail@stefannegele.de>
2016-10-18 21:22:28 +0200

    Remove path import for PR merge

--- addon/ng2/models/webpack-build-common.ts
+++ addon/ng2/models/webpack-build-common.ts
@@ -1,4 +1,3 @@
-import * as path from 'path';
 import * as CopyWebpackPlugin from 'copy-webpack-plugin';
 import * as HtmlWebpackPlugin from 'html-webpack-plugin';
 import * as webpack from 'webpack';
