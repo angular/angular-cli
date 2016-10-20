4513f52c6cdd30c1a5e0929aa7975c673be49d10
Stefan Baramov <sbaramov@gmail.com>
2016-10-20 09:53:02 -0400

    feat(ssl): add support for the ssl options of the ng serve task: --ssl, --ssl-cert, and --ssl-key (#2792)

--- tests/e2e/utils/http.ts
+++ tests/e2e/utils/http.ts
@@ -4,11 +4,12 @@ import * as _request from 'request';
 
 export function request(url: string): Promise<string> {
   return new Promise((resolve, reject) => {
-    _request(url, (error: any, response: IncomingMessage, body: string) => {
+    let options = { url: url, agentOptions: { rejectUnauthorized: false }};
+    _request(options, (error: any, response: IncomingMessage, body: string) => {
       if (error) {
         reject(error);
       } else if (response.statusCode >= 400) {
-        reject(new Error(`Requesting "${url}" returned status code ${response.statusCode}.`);
+        reject(new Error(`Requesting "${url}" returned status code ${response.statusCode}.`));
       } else {
         resolve(body);
       }
