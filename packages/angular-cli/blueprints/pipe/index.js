cabc9dcafe1d5a45aba79e335de74a561308fdaf
Mike Brocchi <mbrocchi@gmail.com>
2016-11-01 13:58:30 -0400

    bug(generate): ensure classes and imports are in lower case (#2920)
    Fixes #2833

--- packages/angular-cli/blueprints/pipe/index.js
+++ packages/angular-cli/blueprints/pipe/index.js
@@ -75,7 +75,7 @@ module.exports = {
     const fileName = stringUtils.dasherize(`${options.entity.name}.pipe`);
     const fullGeneratePath = path.join(this.project.root, this.generatePath);
     const moduleDir = path.parse(this.pathToModule).dir;
-    const relativeDir = path.relative(moduleDir, fullGeneratePath);
+    const relativeDir = path.relative(moduleDir, fullGeneratePath).toLowerCase();
     const importPath = relativeDir ? `./${relativeDir}/${fileName}` : `./${fileName}`;
 
     if (!options['skip-import']) {
