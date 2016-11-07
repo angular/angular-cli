62f1c065c228758afa18457ec64e84402b9341fe
Filipe Silva <filipematossilva@gmail.com>
2016-11-07 21:54:24 +0000

    chore(tests): fix filename test

--- tests/e2e/tests/build/filename.ts
+++ tests/e2e/tests/build/filename.ts
@@ -1,10 +1,12 @@
 import {ng} from '../../utils/process';
 import {expectFileToExist} from '../../utils/fs';
 import {updateJsonFile} from '../../utils/project';
+import {copyFile} from '../../utils/fs';
 
 
 export default function() {
   return Promise.resolve()
+    .then(() => copyFile('src/index.html', 'src/config-index.html'))
     .then(() => updateJsonFile('angular-cli.json', configJson => {
       const app = configJson['apps'][0];
       app['outDir'] = 'config-build-output';
