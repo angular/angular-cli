5d4bfe02ed8632010a965a062c176d42a4ddad12
Tom De Smet <tdesmet@gmail.com>
2016-11-11 17:40:22 +0100

    Remove meaning

--- tests/e2e/tests/build/aot-i18n.ts
+++ tests/e2e/tests/build/aot-i18n.ts
@@ -9,17 +9,16 @@ export default function() {
         <xliff version="1.2" xmlns="urn:oasis:names:tc:xliff:document:1.2">
          <file source-language="en" datatype="plaintext" original="ng2.template">
            <body>
-             <trans-unit id="af2ccf4b5dba59616e92cf1531505af02da8f6d2" datatype="html">
+             <trans-unit id="8def8481e91291a52f9baa31cbdb313e6a6ca02b" datatype="html">
                <source>Hello i18n!</source>
                <target>Bonjour i18n!</target>
                <note priority="1" from="description">An introduction header for this sample</note>
-               <note priority="1" from="meaning">User welcome</note>
              </trans-unit>
            </body>
          </file>
         </xliff>`))
     .then(() => appendToFile('src/app/app.component.html',
-      '<h1 i18n="User welcome|An introduction header for this sample">Hello i18n!</h1>'))
+      '<h1 i18n="An introduction header for this sample">Hello i18n!</h1>'))
     .then(() => ng('build', '--aot', '--i18n-file', 'src/locale/messages.fr.xlf', '--i18n-format',
       'xlf', '--locale', 'fr'))
     .then(() => expectFileToMatch('dist/main.bundle.js', /Bonjour i18n!/));
