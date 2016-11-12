636460194eeea64c465ca04ecd141c1d3c0cd387
Yonatan <kra.yonatan@gmail.com>
2016-11-12 21:27:39 +0200

    Fix linting

--- packages/angular-cli/tasks/test.ts
+++ packages/angular-cli/tasks/test.ts
@@ -2,14 +2,14 @@ const Task = require('../ember-cli/lib/models/task');
 import * as path from 'path';
 
 // require dependencies within the target project
-function requireDependency(root: string, moduleName: string) {
+function requireDependency(root:string, moduleName:string) {
   const packageJson = require(path.join(root, 'node_modules', moduleName, 'package.json'));
   const main = path.normalize(packageJson.main);
   return require(path.join(root, 'node_modules', moduleName, main));
 }
 
 export default Task.extend({
-  run: function (options: any) {
+  run: function (options:any) {
     const projectRoot = this.project.root;
     return new Promise((resolve) => {
       const karma = requireDependency(projectRoot, 'karma');
@@ -29,24 +29,24 @@ export default Task.extend({
       options.configFile = karmaConfig;
 
       options.files = [
-        { pattern: './src/test.ts', watched: false }
+        {pattern: './src/test.ts', watched: false}
       ];
 
       // get the project's global scripts
       const scripts = this.project.ngConfig.config.apps[0].scripts;
       // if scripts exist, we should add them to Karma files
-      if (scripts.length){
+      if (scripts.length) {
         // add each script to the files array
-        options.files.push.apply(options.files, scripts.map((script) => {
+        options.files.push.apply(options.files, scripts.map((script:any) => {
           // if script has node modules in it, we assume it is something local
           // otherwise we just take it as a URL
 
-          if (script.indexOf('node_modules') > -1){
-            //script = path.resolve(path.join(rootPath, script));
+          if (script.indexOf('node_modules') > -1) {
+            // script = path.resolve(path.join(rootPath, script));
             script = script.replace('..', '.');
           }
 
-          return { pattern: script, watched: false, included: true, served: true };
+          return {pattern: script, watched: false, included: true, served: true};
         }));
       }
       // :shipit:
