27a034d7e8f140fd0155d741aedbee45c774a3cc
Hans <hans@hansl.ca>
2016-11-12 12:19:58 -0800

    fix(@ngtools/webpack): fixed relative path for AoT. (#3114)
    Re-enabled test for all. Also added the baseUrl option as it makes most sense.

--- tests/e2e/tests/misc/lazy-module.ts
+++ tests/e2e/tests/misc/lazy-module.ts
@@ -6,18 +6,14 @@ import {addImportToModule} from '../../utils/ast';
 
 
 export default function(argv: any) {
-  /** This test is disabled when not on nightly. */
-  if (!argv.nightly) {
-    return Promise.resolve();
-  }
-
   let oldNumberOfFiles = 0;
   return Promise.resolve()
     .then(() => ng('build'))
     .then(() => oldNumberOfFiles = readdirSync('dist').length)
     .then(() => ng('generate', 'module', 'lazy', '--routing'))
     .then(() => addImportToModule('src/app/app.module.ts', oneLine`
-      RouterModule.forRoot([{ path: "lazy", loadChildren: "app/lazy/lazy.module#LazyModule" }])
+      RouterModule.forRoot([{ path: "lazy", loadChildren: "app/lazy/lazy.module#LazyModule" }]),
+      RouterModule.forRoot([{ path: "lazy1", loadChildren: "./lazy/lazy.module#LazyModule" }])
       `, '@angular/router'))
     .then(() => ng('build'))
     .then(() => readdirSync('dist').length)
