c21e1a972c0a75acc3f695d7822deb17c86f2000
Yonatan <kra.yonatan@gmail.com>
2016-11-12 21:05:55 +0200

    fix(karma): Enable test of global scripts
    Fixes https://github.com/angular/angular-cli/issues/2897

--- packages/angular-cli/tasks/test.ts
+++ packages/angular-cli/tasks/test.ts
@@ -28,6 +28,27 @@ export default Task.extend({
       // Assign additional karmaConfig options to the local ngapp config
       options.configFile = karmaConfig;
 
+      options.files = [
+        { pattern: './src/test.ts', watched: false }
+      ];
+
+      // get the project's global scripts
+      const scripts = this.project.ngConfig.config.apps[0].scripts;
+      // if scripts exist, we should add them to Karma files
+      if (scripts.length){
+        // add each script to the files array
+        options.files.push.apply(options.files, scripts.map((script) => {
+          // if script has node modules in it, we assume it is something local
+          // otherwise we just take it as a URL
+
+          if (script.indexOf('node_modules') > -1){
+            //script = path.resolve(path.join(rootPath, script));
+            script = script.replace('..', '.');
+          }
+
+          return { pattern: script, watched: false, included: true, served: true };
+        }));
+      }
       // :shipit:
       const karmaServer = new karma.Server(options, resolve);
       karmaServer.start();
