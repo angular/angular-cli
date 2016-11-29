594414079c87caa73c7a4ef43c460a3552054c2b
Filipe Silva <filipematossilva@gmail.com>
2016-11-29 17:34:19 +0000

    feat(build): add lazy styles/scripts
    Close #3401
    Close #3400

--- tests/e2e/tests/build/styles/css.ts
+++ tests/e2e/tests/build/styles/css.ts
@@ -1,36 +1,26 @@
 import {
   writeMultipleFiles,
-  deleteFile,
   expectFileToMatch,
-  replaceInFile
 } from '../../../utils/fs';
 import { ng } from '../../../utils/process';
 import { stripIndents } from 'common-tags';
-import { updateJsonFile } from '../../../utils/project';
 
 export default function () {
   return writeMultipleFiles({
-    'src/styles.scss': stripIndents`
-      @import './imported-styles.scss';
+    'src/styles.css': stripIndents`
+      @import './imported-styles.css';
       body { background-color: blue; }
     `,
-    'src/imported-styles.scss': stripIndents`
+    'src/imported-styles.css': stripIndents`
       p { background-color: red; }
     `,
-    'src/app/app.component.scss': stripIndents`
-        .outer {
-          .inner {
-            background: #fff;
-          }
+    'src/app/app.component.css': stripIndents`
+      .outer {
+        .inner {
+          background: #fff;
         }
-      `})
-    .then(() => deleteFile('src/app/app.component.css'))
-    .then(() => updateJsonFile('angular-cli.json', configJson => {
-      const app = configJson['apps'][0];
-      app['styles'] = ['styles.scss'];
-    }))
-    .then(() => replaceInFile('src/app/app.component.ts',
-      './app.component.css', './app.component.scss'))
+      }
+    `})
     .then(() => ng('build'))
     .then(() => expectFileToMatch('dist/styles.bundle.css',
       /body\s*{\s*background-color: blue;\s*}/))
