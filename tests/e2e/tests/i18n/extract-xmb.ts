878282e729ffb35522d2a29da02b3a0535947f2a
Carles Galan Cladera <cgcladera@gmail.com>
2016-12-02 10:36:52 +0100

    Remove acceptance tests and create e2e tests

---
+++ tests/e2e/tests/i18n/extract-xmb.ts
@@ -0,0 +1,19 @@
+import {join} from 'path';
+import {ng} from '../../utils/process';
+import {
+  expectFileToExist, deleteFile, writeFile,
+  expectFileToMatch
+} from '../../utils/fs';
+
+
+export default function() {
+  const testComponentDir = join('src/app', 'i18n-test');
+  return ng('generate', 'component', 'i18n-test')
+    .then(() => deleteFile(join(testComponentDir, 'i18n-test.component.html')))
+    .then(() => writeFile(
+      join(testComponentDir, 'i18n-test.component.html'),
+      '<p i18n>Hello world</p>'))
+    .then(() => ng('xi18n', '--format=xmb'))
+    .then(() => expectFileToExist(join('src', 'messages.xmb')))
+    .then(() => expectFileToMatch(join('src', 'messages.xmb'), /Hello world/));
+}
