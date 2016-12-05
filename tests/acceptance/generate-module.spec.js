64f58b4181c0e85602c50a196dc87565cb5be526
baruchvlz <baruchvelez@gmail.com>
2016-12-04 21:48:12 -0500

    fix parent/child compnent test to fit new flag

--- tests/acceptance/generate-module.spec.js
+++ tests/acceptance/generate-module.spec.js
@@ -65,7 +65,7 @@ describe('Acceptance: ng generate module', function () {
       ng(['generate', 'module', 'parent/child']).then(() => {
         expect(existsSync(path.join(testPath, 'parent/child', 'child.module.ts'))).to.equal(true);
         expect(existsSync(path.join(testPath, 'parent/child', 'child.module.spec.ts'))).to.equal(false);
-        expect(existsSync(path.join(testPath, 'parent/child', 'child.component.ts'))).to.equal(true);
+        expect(existsSync(path.join(testPath, 'parent/child', 'child.component.ts'))).to.equal(false);
       })
     );
   });
