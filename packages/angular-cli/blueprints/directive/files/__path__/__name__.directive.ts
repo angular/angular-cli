d2126316cf307b275d9eaaa3a6032951b0e3ed7c
Neo <wenchen.li@jam3.com>
2016-12-08 14:31:08 -0500

    Update constructor in templates

--- packages/angular-cli/blueprints/directive/files/__path__/__name__.directive.ts
+++ packages/angular-cli/blueprints/directive/files/__path__/__name__.directive.ts
@@ -5,6 +5,7 @@ import { Directive } from '@angular/core';
 })
 export class <%= classifiedModuleName %>Directive {
 
-  constructor() { }
+  constructor() {
+  }
 
 }
--- packages/angular-cli/blueprints/service/files/__path__/__name__.service.ts
+++ packages/angular-cli/blueprints/service/files/__path__/__name__.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class <%= classifiedModuleName %>Service {
 
-  constructor() { }
+  constructor() {
+  }
 
 }
