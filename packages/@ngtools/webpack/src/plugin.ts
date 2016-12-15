2a5c89677d8bb72a6e2767b3b89b80c0bc3d6f91
Angular <hans@hansl.ca>
2016-12-15 13:56:27 -0800

    fixing windows again

--- packages/@ngtools/webpack/src/plugin.ts
+++ packages/@ngtools/webpack/src/plugin.ts
@@ -112,16 +112,19 @@ export class AotPlugin implements Tapable {
 
       exclude.forEach((pattern: string) => {
           pattern = pattern
+            // Replace windows path separators to forward slashes.
+            .replace(/\\/g, '/')
             // Escape characters that are used normally in regexes, except stars.
-            .replace(/[\-\[\]\/{}()+?.\\^$|]/g, '\\$&')
+            .replace(/[\-\[\]{}()+?.\\^$|]/g, '\\$&')
             // Two stars replacement.
             .replace(/\*\*/g, '(?:.*)')
-            .replace(/\*/g, '(?:[^/\\\\]*)')
-            // Escape every characters from the basePath.
-            .replace(/^/, `(${basePath.replace(/[\-\[\]\/{}()+?.\\^$|*]/g, '\\$&')})?`);
+            // One star replacement.
+            .replace(/\*/g, '(?:[^/]*)')
+            // Escape characters from the basePath and make sure it's forward slashes.
+            .replace(/^/, `(${basePath.replace(/\\/g, '/').replace(/[\-\[\]\/{}()+?.\\^$|*]/g, '\\$&')})?`);
 
           const re = new RegExp('^' + pattern + '$');
-          fileNames = fileNames.filter(x => !x.match(re));
+          fileNames = fileNames.filter(x => !x.replace(/\\/g, '/').match(re));
         });
     } else {
       fileNames = fileNames.filter(fileName => !/\.spec\.ts$/.test(fileName));
