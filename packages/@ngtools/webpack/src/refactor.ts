65a7d1eb1d2d5cae52703ef921bfaec27d0b1310
Hans Larsen <hans@hansl.ca>
2016-12-15 12:08:15 -0800

    fixing more tests

--- packages/@ngtools/webpack/src/refactor.ts
+++ packages/@ngtools/webpack/src/refactor.ts
@@ -16,7 +16,12 @@ function resolve(filePath: string, host: ts.CompilerHost, program: ts.Program) {
   if (path.isAbsolute(filePath)) {
     return filePath;
   }
-  return path.join(program.getCompilerOptions().baseUrl || process.cwd(), filePath);
+  const compilerOptions = program.getCompilerOptions();
+  const basePath = compilerOptions.baseUrl || compilerOptions.rootDir;
+  if (!basePath) {
+    throw new Error(`Trying to resolve '${filePath}' without a basePath.`);
+  }
+  return path.join(basePath, filePath);
 }
 
 
