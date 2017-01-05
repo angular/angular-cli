8ab9abceb30a0eb53ec57090e88c537acaef4000
andrei <andreiepure@users.noreply.github.com>
2017-01-05 14:23:33 +0100

    Update main.ts imports to respect Style 03-06
    Consider leaving one empty line between third party imports and application imports.
    Consider listing import lines alphabetized by the module.
    https://angular.io/styleguide#!#03-06

--- packages/angular-cli/blueprints/ng2/files/__path__/main.ts
+++ packages/angular-cli/blueprints/ng2/files/__path__/main.ts
@@ -1,9 +1,10 @@
 import './polyfills.ts';
 
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
-import { environment } from './environments/environment';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
 import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
