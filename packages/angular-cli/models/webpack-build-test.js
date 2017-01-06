6dc8ef6590a319861265a94f63af06351bd01707
Juan Sanchez Garcia <juansg.eng@gmail.com>
2017-01-05 16:31:22 -0800

    refactor(build): replace `source` in environments with `environmentDefault` entry
    Replace source in environments with separate environmentDefault entry and updated docs to reflect this change
    
    BREAKING CHANGE: angular-cli.json has changed. A new environmentDefault entry replaces the previous source entry inside environments.
    
    To migrate the code follow the example below:
    
    Before:
    
    "environments": {
    "source": "environments/environment.ts",
    	"dev": "environments/environment.ts",
    	"prod": "environments/environment.prod.ts"
    }
    
    After:
    
    "environmentSource": "environments/environment.ts",
    "environments": {
    	"dev": "environments/environment.ts",
    	"prod": "environments/environment.prod.ts"
    }
    
    Closes #3857

--- packages/angular-cli/models/webpack-build-test.js
+++ packages/angular-cli/models/webpack-build-test.js
@@ -128,7 +128,7 @@ const getWebpackTestConfig = function (projectRoot, environment, appConfig, test
         // This plugin is responsible for swapping the environment files.
         // Since it takes a RegExp as first parameter, we need to escape the path.
         // See https://webpack.github.io/docs/list-of-plugins.html#normalmodulereplacementplugin
-        new RegExp(path.resolve(appRoot, appConfig.environments['source'])
+        new RegExp(path.resolve(appRoot, appConfig['environmentSource'])
           .replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&')),
         path.resolve(appRoot, appConfig.environments[environment])
       ),
