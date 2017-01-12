3c82b774915198cb23d338493a7ce24336bd8ec6
Davin Kevin <davinkevin@users.noreply.github.com>
2017-01-12 20:11:51 +0100

    feat(deploy:github-pages): support usage of gh-token for deployment from external env (#3121)
    In order to being able to push to github from CI (Travis for example), we should be able to provide a token to the cli, as `--gh-token=XYZ`. This modification allow to use this token in the destination url. This also allow user to push to another github repository if it uses a different `--gh-username` from the one used for the checkout.

--- packages/angular-cli/commands/github-pages-deploy.ts
+++ packages/angular-cli/commands/github-pages-deploy.ts
@@ -2,71 +2,71 @@ const Command = require('../ember-cli/lib/models/command');
 import { oneLine } from 'common-tags';
 
 export interface GithubPagesDeployOptions {
-  message?: string;
-  target?: string;
-  environment?: string;
-  userPage?: boolean;
-  skipBuild?: boolean;
-  ghToken?: string;
-  ghUsername?: string;
-  baseHref?: string;
+    message?: string;
+    target?: string;
+    environment?: string;
+    userPage?: boolean;
+    skipBuild?: boolean;
+    ghToken?: string;
+    ghUsername?: string;
+    baseHref?: string;
 }
 
 const githubPagesDeployCommand = Command.extend({
-  name: 'github-pages:deploy',
-  aliases: ['gh-pages:deploy'],
-  description: oneLine`
+    name: 'github-pages:deploy',
+    aliases: ['gh-pages:deploy'],
+    description: oneLine`
     Build the test app for production, commit it into a git branch,
     setup GitHub repo and push to it
   `,
-  works: 'insideProject',
+    works: 'insideProject',
 
-  availableOptions: [
-    {
-      name: 'message',
-      type: String,
-      default: 'new gh-pages version',
-      description: 'The commit message to include with the build, must be wrapped in quotes.'
-    }, {
-      name: 'target',
-      type: String,
-      default: 'production',
-      aliases: ['t', { 'dev': 'development' }, { 'prod': 'production' }]
-    }, {
-      name: 'environment',
-      type: String,
-      default: '',
-      aliases: ['e']
-    }, {
-      name: 'user-page',
-      type: Boolean,
-      default: false,
-      description: 'Deploy as a user/org page'
-    }, {
-      name: 'skip-build',
-      type: Boolean,
-      default: false,
-      description: 'Skip building the project before deploying'
-    }, {
-      name: 'gh-token',
-      type: String,
-      default: '',
-      description: 'Github token'
-    }, {
-      name: 'gh-username',
-      type: String,
-      default: '',
-      description: 'Github username'
-    }, {
-      name: 'base-href',
-      type: String,
-      default: null,
-      aliases: ['bh']
-    }],
+    availableOptions: [
+        {
+            name: 'message',
+            type: String,
+            default: 'new gh-pages version',
+            description: 'The commit message to include with the build, must be wrapped in quotes.'
+        }, {
+            name: 'target',
+            type: String,
+            default: 'production',
+            aliases: ['t', { 'dev': 'development' }, { 'prod': 'production' }]
+        }, {
+            name: 'environment',
+            type: String,
+            default: '',
+            aliases: ['e']
+        }, {
+            name: 'user-page',
+            type: Boolean,
+            default: false,
+            description: 'Deploy as a user/org page'
+        }, {
+            name: 'skip-build',
+            type: Boolean,
+            default: false,
+            description: 'Skip building the project before deploying'
+        }, {
+            name: 'gh-token',
+            type: String,
+            default: '',
+            description: 'Github token'
+        }, {
+            name: 'gh-username',
+            type: String,
+            default: '',
+            description: 'Github username'
+        }, {
+            name: 'base-href',
+            type: String,
+            default: null,
+            aliases: ['bh']
+        }],
 
-  run: function(options: GithubPagesDeployOptions, rawArgs: string[]) {
-    return require('./github-pages-deploy.run').default.call(this, options, rawArgs);
-  }
+    run: function(options: GithubPagesDeployOptions, rawArgs: string[]) {
+        return require('./github-pages-deploy.run').default.call(this, options, rawArgs);
+    }
 });
 
 
