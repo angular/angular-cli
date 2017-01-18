48d1e446c1e401cb9fb547d299ee0cd73595f510
Hans <hans@hansl.ca>
2017-01-18 14:05:41 -0800

    fix(@ngtools/json-schema): serialize object properties better. (#4103)
    The problem was that in an object that allowed additional properties, we were not serializing the object new values properly.
    
    Fixes #4044.

--- packages/@ngtools/json-schema/src/serializers/json.spec.ts
+++ packages/@ngtools/json-schema/src/serializers/json.spec.ts
@@ -7,26 +7,28 @@ import {RootSchemaTreeNode} from '../schema-tree';
 
 
 describe('JsonSerializer', () => {
-  const schemaJsonFilePath = path.join(__dirname, '../../tests/schema1.json');
-  const schemaJson = JSON.parse(fs.readFileSync(schemaJsonFilePath, 'utf-8'));
-  const valueJsonFilePath = path.join(__dirname, '../../tests/value1.json');
-  const valueJson = JSON.parse(fs.readFileSync(valueJsonFilePath, 'utf-8'));
+  for (const nb of [1, 2, 3]) {
+    it(`works (${nb})`, () => {
+      const schemaJsonFilePath = path.join(__dirname, `../../tests/serializer/schema${nb}.json`);
+      const schemaJson = JSON.parse(fs.readFileSync(schemaJsonFilePath, 'utf-8'));
+      const valueJsonFilePath = path.join(__dirname, `../../tests/serializer/value${nb}.json`);
+      const valueJson = JSON.parse(fs.readFileSync(valueJsonFilePath, 'utf-8'));
 
-  const schemaClass = new (SchemaClassFactory(schemaJson))(valueJson);
-  const schema: RootSchemaTreeNode = schemaClass.$$schema();
+      const schemaClass = new (SchemaClassFactory(schemaJson))(valueJson);
+      const schema: RootSchemaTreeNode = schemaClass.$$schema();
 
-  it('works', () => {
-    let str = '';
-    function writer(s: string) {
-      str += s;
-    }
+      let str = '';
+      function writer(s: string) {
+        str += s;
+      }
 
-    const serializer = new JsonSerializer(writer);
+      const serializer = new JsonSerializer(writer);
 
-    serializer.start();
-    schema.serialize(serializer);
-    serializer.end();
+      serializer.start();
+      schema.serialize(serializer);
+      serializer.end();
 
-    expect(JSON.stringify(JSON.parse(str))).toEqual(JSON.stringify(valueJson));
-  });
+      expect(JSON.stringify(JSON.parse(str))).toEqual(JSON.stringify(valueJson));
+    });
+  }
 });
