72f0d72a58ef44b02240db96b46733fba59d82cb
Filipe Silva <filipematossilva@gmail.com>
2017-01-20 23:18:26 +0000

    refactor(mobile): remove unused mobile code (#4143)
    The mobile flag hasn't worked for a while.
    
    When/if it is added again, it's code would need to be done from scratch since the CLI has undergone a lot of changes since it last worked and was tested.
    
    Fix #4141

--- tests/e2e/tests/build/prod-build.ts
+++ tests/e2e/tests/build/prod-build.ts
@@ -1,30 +1,7 @@
 import {join} from 'path';
-import {isMobileTest} from '../../utils/utils';
 import {expectFileToExist, expectFileToMatch} from '../../utils/fs';
 import {ng} from '../../utils/process';
 import {expectGitToBeClean} from '../../utils/git';
-
-
-
-function mobileOnlyChecks() {
-  if (!isMobileTest()) {
-    return;
-  }
-
-  // Check for mobile-specific features in prod builds.
-  return Promise.resolve()
-    // Service Worker
-    .then(() => expectFileToExist('dist/sw.js'))
-    .then(() => expectFileToMatch('dist/index.html', /sw-install\.[0-9a-f]{20}\.bundle\.js/))
-
-    // App Manifest
-    .then(() => expectFileToExist('dist/manifest.webapp'))
-    .then(() => expectFileToMatch('dist/index.html',
-                                  '<link rel="manifest" href="/manifest.webapp">'))
-
-    // Icons folder
-    .then(() => expectFileToExist('dist/icons'));
-}
 
 
 export default function() {
@@ -37,6 +14,5 @@ export default function() {
     .then(() => expectFileToMatch('dist/index.html', /styles\.[0-9a-f]{20}\.bundle\.css/))
 
     // Check that the process didn't change local files.
-    .then(() => expectGitToBeClean())
-    .then(() => mobileOnlyChecks());
+    .then(() => expectGitToBeClean());
 }
