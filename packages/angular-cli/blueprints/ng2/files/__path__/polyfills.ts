a8d4b43ce08130b8983d08c5045d955dec49f41b
Filipe Silva <filipematossilva@gmail.com>
2017-01-20 21:23:29 +0000

    don't actually import the polyfills

--- packages/angular-cli/blueprints/ng2/files/__path__/polyfills.ts
+++ packages/angular-cli/blueprints/ng2/files/__path__/polyfills.ts
@@ -23,16 +23,16 @@ import 'zone.js/dist/zone';
 // Learn more in https://angular.io/docs/ts/latest/guide/browser-support.html
 
 // Needed for: IE9
-import 'classlist.js';
+// import 'classlist.js';
 
 // Animations
 // Needed for: All but Chrome and Firefox, Not supported in IE9
-import 'web-animations-js';
+// import 'web-animations-js';
 
 // Date, currency, decimal and percent pipes
 // Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
-import 'intl';
+// import 'intl';
 
 // NgClass on SVG elements
 // Needed for: IE10, IE11
-import 'classlist.js';
+// import 'classlist.js';
