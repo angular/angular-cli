efad0ba212307b854bb9283a90be77fbaf9962e0
Filipe Silva <filipematossilva@gmail.com>
2017-01-20 21:45:35 +0000

    feat(update): add ng update as alias of ng init
    Fix #4007

--- tests/acceptance/init.spec.js
+++ tests/acceptance/init.spec.js
@@ -20,7 +20,7 @@ var existsSync = require('exists-sync');
 
 var defaultIgnoredFiles = Blueprint.ignoredFiles;
 
-describe('Acceptance: ng init', function () {
+describe('Acceptance: ng update', function () {
   this.timeout(20000);
 
   beforeEach(function () {
@@ -94,14 +94,14 @@ describe('Acceptance: ng init', function () {
     });
   }
 
-  it('ng init', function () {
+  it('ng init does the same as ng update', function () {
     return ng([
       'init',
       '--skip-npm'
     ]).then(confirmBlueprinted);
   });
 
-  it('ng init can run in created folder', function () {
+  it('ng update can run in created folder', function () {
     return tmp.setup('./tmp/foo')
       .then(function () {
         process.chdir('./tmp/foo');
@@ -176,7 +176,7 @@ describe('Acceptance: ng init', function () {
       .then(confirmBlueprinted);
   });
 
-  it('ng init --inline-template does not generate a template file', () => {
+  it('ng update --inline-template does not generate a template file', () => {
     return ng(['init', '--skip-npm', '--skip-git', '--inline-template'])
       .then(() => {
         const templateFile = path.join('src', 'app', 'app.component.html');
@@ -184,7 +184,7 @@ describe('Acceptance: ng init', function () {
       });
   });
 
-  it('ng init --inline-style does not gener a style file', () => {
+  it('ng update --inline-style does not gener a style file', () => {
     return ng(['init', '--skip-npm', '--skip-git', '--inline-style'])
       .then(() => {
         const styleFile = path.join('src', 'app', 'app.component.css');
