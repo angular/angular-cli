1bbb7d510be9b8d5de138d29ba159fb9ecd44f6a
Bram Borggreve <borggreve@gmail.com>
2017-01-21 11:32:20 -0500

    chore: update dependencies in ng2 blueprint

--- packages/@angular/cli/blueprints/ng2/files/protractor.conf.js
+++ packages/@angular/cli/blueprints/ng2/files/protractor.conf.js
@@ -2,7 +2,7 @@
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
 /*global jasmine */
-var SpecReporter = require('jasmine-spec-reporter');
+var SpecReporter = require('jasmine-spec-reporter').SpecReporter;
 
 exports.config = {
   allScriptsTimeout: 11000,
