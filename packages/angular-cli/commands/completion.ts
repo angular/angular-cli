0f3c82e5d41dca8f859a80554a251d34f91301eb
Carlo Dapor <catull@gmail.com>
2017-01-22 02:20:42 +0100

    Rewrote comkpletion.ts, deeply inspired by help.ts.

--- packages/angular-cli/commands/completion.ts
+++ packages/angular-cli/commands/completion.ts
@@ -1,27 +1,11 @@
-import * as path from 'path';
 import * as fs from 'fs';
-import { oneLine } from 'common-tags';
+import * as path from 'path';
 
-import BuildCommand from './build';
-import DestroyCommand from './destroy';
-import DocCommand from './doc';
-import E2eCommand from './e2e';
-import MakeThisAwesomeCommand from './easter-egg';
-import GenerateCommand from '../commands/generate';
-import GetCommand from './get';
-import githubPagesDeployCommand from './github-pages-deploy';
-import HelpCommand from './help';
-import InitCommand from './init';
-import LintCommand from './lint';
-import NewCommand from './new';
-import ServeCommand from './serve';
-import SetCommand from './set';
-import NgCliTestCommand from './test';
-import VersionCommand from './version';
-
+const stringUtils = require('ember-cli-string-utils');
 const Command = require('../ember-cli/lib/models/command');
+const lookupCommand = require('../ember-cli/lib/cli/lookup-command');
 
-function generateOptionVar(opts: any): String {
+function extractOptions(opts: any): String {
   const output: String[] = [];
 
   for (let index = 0; index < opts.length; index++) {
@@ -35,74 +19,13 @@ function generateOptionVar(opts: any): String {
   return output.sort().join(' ');
 }
 
-function collectCommandAndAlias(command: any): String {
-  const result: String[] = [];
-  optsNg.push(command.name);
-  result.push(command.name);
-
-  if (command.aliases) {
-    command.aliases.forEach((element: String) => {
-      optsNg.push(element);
-      result.push(element);
-    });
-  }
-  return result.sort().join('|');
-}
-
-const optsNg: String[] = [];
-
-const caseBuild = collectCommandAndAlias(BuildCommand.prototype);
-optsNg.push('completion');
-// const case_destroy = collectCommandAndAlias(DestroyCommand.prototype);
-// const case_doc = collectCommandAndAlias(DocCommand.prototype);
-// const case_e2e = collectCommandAndAlias(E2eCommand.prototype);
-// const caseGenerate = collectCommandAndAlias(GenerateCommand.prototype);
-const caseGenerate = 'g|generate';
-// const caseGet = collectCommandAndAlias(GetCommand.prototype);
-const caseGhPagesDeploy = collectCommandAndAlias(githubPagesDeployCommand.prototype);
-const caseHelp = collectCommandAndAlias(HelpCommand.prototype);
-const caseInit = collectCommandAndAlias(InitCommand.prototype);
-const caseLint = collectCommandAndAlias(LintCommand.prototype);
-// const caseMakethisawesome = collectCommandAndAlias(MakeThisAwesomeCommand.prototype);
-const caseNew = collectCommandAndAlias(NewCommand.prototype);
-const caseServe = collectCommandAndAlias(ServeCommand.prototype);
-const caseSet = collectCommandAndAlias(SetCommand.prototype);
-const caseTest = collectCommandAndAlias(NgCliTestCommand.prototype);
-const caseVersion = collectCommandAndAlias(VersionCommand.prototype);
-
-const optsHelp = optsNg.sort().join(' ');
-
-let optsBuild = generateOptionVar(BuildCommand.prototype.availableOptions);
-// let optsGenerate = generateMapVar (GenerateCommand.prototype.aliasMap);
-let optsGenerate = oneLine`
-    cl class c component d directive e enum i interface m module p pipe r route s service`;
-let optsGhPagesDeploy = generateOptionVar(githubPagesDeployCommand.prototype.availableOptions);
-let optsInit = generateOptionVar(InitCommand.prototype.availableOptions);
-let optsNew = generateOptionVar(NewCommand.prototype.availableOptions);
-let optsServe = generateOptionVar(ServeCommand.prototype.availableOptions);
-let optsSet = generateOptionVar(SetCommand.prototype.availableOptions);
-let optsTest = generateOptionVar(NgCliTestCommand.prototype.availableOptions);
-let optsVersion = generateOptionVar(VersionCommand.prototype.availableOptions);
-
-let caseBlock =
-     `ng|help) opts="${optsHelp}" ;;
-      ${caseBuild}) opts="${optsBuild}" ;;
-      completion) opts="-a -b --all --bash --zsh" ;;
-      ${caseGenerate}) opts="${optsGenerate}" ;;
-      ${caseGhPagesDeploy}) opts="${optsGhPagesDeploy}" ;;
-      ${caseInit}) opts="${optsInit}" ;;
-      ${caseNew}) opts="${optsNew}" ;;
-      ${caseServe}) opts="${optsServe}" ;;
-      ${caseSet}) opts="${optsSet}" ;;
-      ${caseTest}) opts="${optsTest}" ;;
-      ${caseVersion}) opts="${optsVersion}" ;;
-      *) opts="" ;;`;
-
 export interface CompletionCommandOptions {
   all?: boolean;
   bash?: boolean;
   zsh?: boolean;
 };
+
+const optsNg: String[] = [];
 
 const CompletionCommand = Command.extend({
   name: 'completion',
@@ -117,8 +40,54 @@ const CompletionCommand = Command.extend({
   run: function (commandOptions: CompletionCommandOptions, rawArgs: string[]) {
     commandOptions.all = !commandOptions.bash && !commandOptions.zsh;
 
-    this.ui.writeLine(`
-###-begin-ng-completion###');
+    const commandFiles = fs.readdirSync(__dirname)
+      .filter(file => file.match(/\.ts$/) && !file.match(/\.run.ts$/))
+      .map(file => path.parse(file).name)
+      .map(file => file.toLowerCase());
+
+    const commandMap = commandFiles.reduce((acc: any, curr: string) => {
+      let classifiedName = stringUtils.classify(curr);
+      let defaultImport = require(`./${curr}`).default;
+
+      acc[classifiedName] = defaultImport;
+
+      return acc;
+    }, {});
+
+    let caseBlock = '';
+
+    commandFiles.forEach(cmd => {
+      const Command = lookupCommand(commandMap, cmd);
+      const com: String[] = [];
+
+      const command = new Command({
+        ui: this.ui,
+        project: this.project,
+        commands: this.commands,
+        tasks: this.tasks
+      });
+
+      optsNg.push(command.name);
+      com.push(command.name);
+
+      if (command.aliases) {
+        command.aliases.forEach((element: String) => {
+          optsNg.push(element);
+          com.push(element);
+        });
+      }
+
+      if (command.availableOptions && command.availableOptions[0]) {
+        let opts = extractOptions (command.availableOptions);
+        caseBlock = caseBlock + '    ' + com.sort().join('|') + ') opts="' + opts + '" ;;\n';
+      }
+    });
+
+    caseBlock = 'ng|help) opts="' + optsNg.sort().join(' ') + '" ;;\n' +
+                caseBlock +
+                '    *) opts="" ;;';
+
+    this.ui.writeLine(`###-begin-ng-completion###');
 #
 
 # ng command completion script
@@ -130,70 +99,69 @@ const CompletionCommand = Command.extend({
 #
 # Installation: ng completion -b 1>> ~/.bashrc
 #           or  ng completion -z 1>> ~/.zshrc
-#`);
+#
+`);
 
     if (commandOptions.all && !commandOptions.bash) {
-      this.ui.writeLine(`
-if test ".$(type -t complete 2>/dev/null || true)" = ".builtin"; then`);
+      this.ui.writeLine('if test ".$(type -t complete 2>/dev/null || true)" = ".builtin"; then');
     }
 
     if (commandOptions.all || commandOptions.bash) {
-      this.ui.writeLine(`
-  _ng_completion() {
-    local cword pword opts
+      this.ui.writeLine(`_ng_completion() {
+  local cword pword opts
 
-    COMPREPLY=()
-    cword=\${COMP_WORDS[COMP_CWORD]}
-    pword=\${COMP_WORDS[COMP_CWORD - 1]}
+  COMPREPLY=()
+  cword=\${COMP_WORDS[COMP_CWORD]}
+  pword=\${COMP_WORDS[COMP_CWORD - 1]}
 
-    case \${pword} in
-      ${caseBlock}
-    esac
+  case \${pword} in
+    ${caseBlock}
+  esac
 
-    COMPREPLY=( $(compgen -W '\${opts}' -- $cword) )
+  COMPREPLY=( $(compgen -W '\${opts}' -- $cword) )
 
-    return 0
-  }
+  return 0
+}
 
-  complete -o default -F _ng_completion ng`);
+complete -o default -F _ng_completion ng
+`);
     }
 
     if (commandOptions.all) {
-      this.ui.writeLine(`
-elif test ".$(type -w compctl 2>/dev/null || true)" = ".compctl: builtin" ; then`);
+      this.ui.writeLine(
+        'elif test ".$(type -w compctl 2>/dev/null || true)" = ".compctl: builtin" ; then');
     }
 
     if (commandOptions.all || commandOptions.zsh) {
-      this.ui.writeLine(`
-  _ng_completion () {
-    local words cword opts
-    read -Ac words
-    read -cn cword
-    let cword-=1
+      this.ui.writeLine(`_ng_completion () {
+  local words cword opts
+  read -Ac words
+  read -cn cword
+  let cword-=1
 
-    case $words[cword] in
-      ${caseBlock}
-    esac
+  case $words[cword] in
+    ${caseBlock}
+  esac
 
-    setopt shwordsplit
-    reply=($opts)
-    unset shwordsplit
-  }
+  setopt shwordsplit
+  reply=($opts)
+  unset shwordsplit
+}
 
-  compctl -K _ng_completion ng`);
+compctl -K _ng_completion ng
+`);
     }
 
     if (commandOptions.all) {
-      this.ui.writeLine(`
-else
+      this.ui.writeLine(`else
   echo "Shell builtin command 'complete' or 'compctl' is redefined; cannot perform ng completion."
   return 1
-fi`);
+fi
+`);
     }
 
-    this.ui.writeLine(`
-###-end-ng-completion###
-`);
+    this.ui.writeLine('###-end-ng-completion###');
+
   }
 });
 
