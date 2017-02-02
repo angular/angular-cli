fda15e69d9ba3f33d8a9b6642886f525ed960c77
Mike Brocchi <mbrocchi@gmail.com>
2017-02-01 21:23:09 -0500

    fix(@ngtools/webpack): change of CSS no longer breaks rebuild
    Fixes #4326
    Fixes #4329

---
+++ tests/e2e/tests/build/rebuild-css-change.ts
@@ -0,0 +1,29 @@
+import {
+  killAllProcesses,
+  exec,
+  waitForAnyProcessOutputToMatch,
+  silentExecAndWaitForOutputToMatch
+} from '../../utils/process';
+import {appendToFile} from '../../utils/fs';
+
+const webpackGoodRegEx = /webpack: bundle is now VALID|webpack: Compiled successfully./;
+const webpackBadRegEx = /webpack: bundle is now INVALID|webpack: Compiling.../;
+
+export default function() {
+  if (process.platform.startsWith('win')) {
+    return Promise.resolve();
+  }
+
+  return silentExecAndWaitForOutputToMatch('ng', ['serve'], webpackGoodRegEx)
+    // Should trigger a rebuild.
+    .then(() => exec('touch', 'src/main.ts'))
+    .then(() => waitForAnyProcessOutputToMatch(webpackBadRegEx, 1000))
+    .then(() => waitForAnyProcessOutputToMatch(webpackGoodRegEx, 5000))
+    .then(() => appendToFile('src/app/app.component.css', ':host { color: blue; }'))
+    .then(() => waitForAnyProcessOutputToMatch(webpackBadRegEx, 1000))
+    .then(() => waitForAnyProcessOutputToMatch(webpackGoodRegEx, 5000))
+    .then(() => killAllProcesses(), (err: any) => {
+      killAllProcesses();
+      throw err;
+    });
+}
