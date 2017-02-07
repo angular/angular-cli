72e4db7104fb79a6d8081eb5c3d7458b78557e74
Hans Larsen <hans@hansl.ca>
2017-02-07 15:50:08 -0800

    refactor: more time given to rebuilds
    Apparently travis is still flaky with 5 seconds.

--- tests/e2e/tests/build/rebuild-css-change.ts
+++ tests/e2e/tests/build/rebuild-css-change.ts
@@ -17,11 +17,11 @@ export default function() {
   return silentExecAndWaitForOutputToMatch('ng', ['serve'], webpackGoodRegEx)
     // Should trigger a rebuild.
     .then(() => exec('touch', 'src/main.ts'))
-    .then(() => waitForAnyProcessOutputToMatch(webpackBadRegEx, 5000))
-    .then(() => waitForAnyProcessOutputToMatch(webpackGoodRegEx, 5000))
+    .then(() => waitForAnyProcessOutputToMatch(webpackBadRegEx, 10000))
+    .then(() => waitForAnyProcessOutputToMatch(webpackGoodRegEx, 10000))
     .then(() => appendToFile('src/app/app.component.css', ':host { color: blue; }'))
-    .then(() => waitForAnyProcessOutputToMatch(webpackBadRegEx, 5000))
-    .then(() => waitForAnyProcessOutputToMatch(webpackGoodRegEx, 5000))
+    .then(() => waitForAnyProcessOutputToMatch(webpackBadRegEx, 10000))
+    .then(() => waitForAnyProcessOutputToMatch(webpackGoodRegEx, 10000))
     .then(() => killAllProcesses(), (err: any) => {
       killAllProcesses();
       throw err;
