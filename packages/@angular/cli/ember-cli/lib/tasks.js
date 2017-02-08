ec9d88a005abaf3561331ead251858d911924349
Hans Larsen <hans@hansl.ca>
2017-02-08 10:40:16 -0800

    refactor(@angular/cli): remove destroy command and task

--- packages/@angular/cli/ember-cli/lib/tasks.js
+++ packages/@angular/cli/ember-cli/lib/tasks.js
@@ -1,8 +1,6 @@
 
 module.exports = {
   CreateAndStepIntoDirectory: require('./tasks/create-and-step-into-directory'),
-  DestroyFromBlueprint: require('./tasks/destroy-from-blueprint'),
-  GenerateFromBlueprint: require('./tasks/generate-from-blueprint'),
   GitInit: require('./tasks/git-init'),
   InstallBlueprint: require('./tasks/install-blueprint'),
   NpmInstall: require('./tasks/npm-install'),
