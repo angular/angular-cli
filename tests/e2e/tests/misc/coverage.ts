025ab69e7d9c99c2723ff5b78e65f1b5c030753a
Filipe Silva <filipematossilva@gmail.com>
2017-02-09 16:21:40 +0000

    fix lcov file name

--- tests/e2e/tests/misc/coverage.ts
+++ tests/e2e/tests/misc/coverage.ts
@@ -5,5 +5,5 @@ import {ng} from '../../utils/process';
 export default function() {
   return ng('test', '--single-run', '--code-coverage')
     .then(() => expectFileToExist('coverage/src/app'))
-    .then(() => expectFileToExist('coverage/coverage.lcov'));
+    .then(() => expectFileToExist('coverage/lcov.info'));
 }
