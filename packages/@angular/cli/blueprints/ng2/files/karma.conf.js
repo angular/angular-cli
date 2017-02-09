429b0d2db69c229d7c506009c5b00b49529513b2
Ward Bell <wardbell@hotmail.com>
2017-02-09 09:35:43 -0800

    feat(test): extend karma.conf.js w/ kjhtml reporter
    karma-jasmine-html-reporter (kjhtml) displays Jasmine test runner in the test browser.

--- packages/@angular/cli/blueprints/ng2/files/karma.conf.js
+++ packages/@angular/cli/blueprints/ng2/files/karma.conf.js
@@ -8,9 +8,13 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma')
     ],
+    client:{
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
     files: [
       { pattern: './<%= sourceDir %>/test.ts', watched: false }
     ],
@@ -30,7 +34,7 @@ module.exports = function (config) {
     },
     reporters: config.angularCli && config.angularCli.codeCoverage
               ? ['progress', 'coverage-istanbul']
-              : ['progress'],
+              : ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
