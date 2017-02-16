596f35df2e2569fbc01b4b004ab20dcb1853ce95
Hans Larsen <hans@hansl.ca>
2017-02-16 15:58:55 -0800

    feat(@angular/cli): disable invalid commands on ejected apps

--- packages/@angular/cli/tasks/test.ts
+++ packages/@angular/cli/tasks/test.ts
@@ -1,11 +1,22 @@
-const Task = require('../ember-cli/lib/models/task');
-import { TestOptions } from '../commands/test';
 import * as path from 'path';
+
+import { TestOptions } from '../commands/test';
+import { CliConfig } from '../models/config';
 import { requireProjectModule } from '../utilities/require-project-module';
+
+const Task = require('../ember-cli/lib/models/task');
+const SilentError = require('silent-error');
+
 
 export default Task.extend({
   run: function (options: TestOptions) {
+    const projectConfig = CliConfig.fromProject().config;
     const projectRoot = this.project.root;
+
+    if (projectConfig.project && projectConfig.project.ejected) {
+      throw new SilentError('An ejected project cannot use the build command anymore.');
+    }
+
     return new Promise((resolve) => {
       const karma = requireProjectModule(projectRoot, 'karma');
       const karmaConfig = path.join(projectRoot, options.config ||
