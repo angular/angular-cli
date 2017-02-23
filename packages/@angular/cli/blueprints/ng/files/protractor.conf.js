00f21d3a12bd2669c96f3a255df2f60d712820c9
Filipe Silva <filipematossilva@gmail.com>
2017-02-23 15:46:12 +0000

    refactor: removed unused lint comment (#4950)

--- packages/@angular/cli/blueprints/ng/files/protractor.conf.js
+++ packages/@angular/cli/blueprints/ng/files/protractor.conf.js
@@ -1,7 +1,6 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-/*global jasmine */
 const { SpecReporter } = require('jasmine-spec-reporter');
 
 exports.config = {
