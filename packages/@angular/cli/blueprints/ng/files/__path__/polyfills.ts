17ad2c627b6b89ac8e4a19caf8303bc14cb3a5ea
Rodrigo Pandini <rodrigopandini@gmail.com>
2017-02-27 20:02:08 -0300

    fix(@angular/cli): blueprint for polyfill.ts has redundant information

--- packages/@angular/cli/blueprints/ng/files/__path__/polyfills.ts
+++ packages/@angular/cli/blueprints/ng/files/__path__/polyfills.ts
@@ -40,6 +40,7 @@
 // import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
 
+
 /** Evergreen browsers require these. **/
 import 'core-js/es6/reflect';
 import 'core-js/es7/reflect';
