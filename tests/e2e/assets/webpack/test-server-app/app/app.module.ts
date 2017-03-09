413aed25fa68a43aa5152452c005246da55049e6
FrozenPandaz <jasonjean1993@gmail.com>
2017-03-08 23:17:57 -0500

    test(@ngtools/webpack): add tests for webpack for platform-server

---
+++ tests/e2e/assets/webpack/test-server-app/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule, Component } from '@angular/core';
+import { ServerModule  } from '@angular/platform-server';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+
+@Component({
+  selector: 'home-view',
+  template: 'home!'
+})
+export class HomeView {}
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeView
+  ],
+  imports: [
+    ServerModule,
+    RouterModule.forRoot([
+      {path: 'lazy', loadChildren: './lazy.module#LazyModule'},
+      {path: '', component: HomeView}
+    ])
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
