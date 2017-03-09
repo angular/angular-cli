0de9c82bcff89be4803ef4c3aa0b0450b995e83a
FrozenPandaz <jasonjean1993@gmail.com>
2017-03-09 11:26:15 -0500

    test(@ngtools/webpack): import platform server from correct place

--- tests/e2e/assets/webpack/test-server-app/app/main.ts
+++ tests/e2e/assets/webpack/test-server-app/app/main.ts
@@ -1,5 +1,5 @@
 import 'core-js/es7/reflect';
-import {platformDynamicServer} from '@angular/platform-dynamic-server';
+import {platformDynamicServer} from '@angular/platform-server';
 import {AppModule} from './app.module';
 
 platformDynamicServer().bootstrapModule(AppModule);
