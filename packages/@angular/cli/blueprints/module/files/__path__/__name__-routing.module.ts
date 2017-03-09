97399487b19989b095191f41f2ee465e63c500a3
Mike Brocchi <mbrocchi@gmail.com>
2017-03-09 13:56:18 -0500

    fix(@angular/cli): remove providers from routing modules

--- packages/@angular/cli/blueprints/module/files/__path__/__name__-routing.module.ts
+++ packages/@angular/cli/blueprints/module/files/__path__/__name__-routing.module.ts
@@ -5,7 +5,6 @@ const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
-  providers: []
+  exports: [RouterModule]
 })
 export class <%= classifiedModuleName %>RoutingModule { }
