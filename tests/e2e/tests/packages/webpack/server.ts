cc94ae0e0a6aeb74cf47ffebe96cd58a3d42978f
FrozenPandaz <jasonjean1993@gmail.com>
2017-03-11 16:16:28 -0500

    test(@ngtools/webpack): remove bundle size tests

--- tests/e2e/tests/packages/webpack/server.ts
+++ tests/e2e/tests/packages/webpack/server.ts
@@ -1,15 +1,13 @@
 import {normalize} from 'path';
 import {createProjectFromAsset} from '../../../utils/assets';
 import {exec} from '../../../utils/process';
-import {expectFileSizeToBeUnder, expectFileToMatch} from '../../../utils/fs';
+import {expectFileToMatch} from '../../../utils/fs';
 
 
 export default function(skipCleaning: () => void) {
   return Promise.resolve()
     .then(() => createProjectFromAsset('webpack/test-server-app'))
     .then(() => exec(normalize('node_modules/.bin/webpack'), '-p'))
-    .then(() => expectFileSizeToBeUnder('dist/app.main.js', 420000))
-    .then(() => expectFileSizeToBeUnder('dist/0.app.main.js', 10000))
     .then(() => expectFileToMatch('dist/app.main.js',
       new RegExp('.bootstrapModuleFactory'))
     .then(() => expectFileToMatch('dist/app.main.js',
