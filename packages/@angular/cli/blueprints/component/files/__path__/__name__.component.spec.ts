17ae1406f14a8bb1e98cd99f0c0d6b63e20fc672
Vadim Shabanov <vadim.shabanov@gmail.com>
2017-03-18 23:19:56 +0300

    fix(@angular/cli): fix text descriptions
    Text of initial tests must have sense.
    
    Close #5501

--- packages/@angular/cli/blueprints/component/files/__path__/__name__.component.spec.ts
+++ packages/@angular/cli/blueprints/component/files/__path__/__name__.component.spec.ts
@@ -19,7 +19,7 @@ describe('<%= classifiedModuleName %>Component', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should be created', () => {
     expect(component).toBeTruthy();
   });
 });
