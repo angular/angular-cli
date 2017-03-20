36b8c9bd0277c9683f564cc8f422e2acbb6d6fe8
Charles Lyding <charles@thunderpos.com>
2017-03-20 16:59:15 -0400

    fix(@angular/cli): allow the use of a base-href with scheme

--- tests/e2e/tests/build/css-urls.ts
+++ tests/e2e/tests/build/css-urls.ts
@@ -47,8 +47,22 @@ export default function () {
     .then(() => expectToFail(() => expectFileToExist('dist/component-img-absolute.svg')))
     .then(() => expectFileMatchToExist('./dist', /global-img-relative\.[0-9a-f]{20}\.svg/))
     .then(() => expectFileMatchToExist('./dist', /component-img-relative\.[0-9a-f]{20}\.svg/))
-    // Check urls with scheme are used as is.
+    // Check urls with deploy-url scheme are used as is.
     .then(() => ng('build', '--base-href=/base/', '--deploy-url=http://deploy.url/',
+      '--extract-css'))
+    .then(() => expectFileToMatch('dist/styles.bundle.css',
+      /url\(\'http:\/\/deploy\.url\/assets\/global-img-absolute\.svg\'\)/))
+    .then(() => expectFileToMatch('dist/main.bundle.js',
+      /url\(\'http:\/\/deploy\.url\/assets\/component-img-absolute\.svg\'\)/))
+    // Check urls with base-href scheme are used as is (with deploy-url).
+    .then(() => ng('build', '--base-href=http://base.url/', '--deploy-url=deploy/',
+      '--extract-css'))
+    .then(() => expectFileToMatch('dist/styles.bundle.css',
+      /url\(\'http:\/\/base\.url\/deploy\/assets\/global-img-absolute\.svg\'\)/))
+    .then(() => expectFileToMatch('dist/main.bundle.js',
+      /url\(\'http:\/\/base\.url\/deploy\/assets\/component-img-absolute\.svg\'\)/))
+    // Check urls with deploy-url and base-href scheme only use deploy-url.
+    .then(() => ng('build', '--base-href=http://base.url/', '--deploy-url=http://deploy.url/',
       '--extract-css'))
     .then(() => expectFileToMatch('dist/styles.bundle.css',
       /url\(\'http:\/\/deploy\.url\/assets\/global-img-absolute\.svg\'\)/))
