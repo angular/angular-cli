0df3f7ca142090b566606010ef580347f381b62d
Sumit Arora <er.sumitarora@gmail.com>
2017-03-30 17:40:46 -0400

    test: e2e test for "--base-href"

--- tests/e2e/tests/test/e2e-baseurl.ts
+++ tests/e2e/tests/test/e2e-baseurl.ts
@@ -1,11 +1,18 @@
 import { ng, killAllProcesses } from '../../utils/process';
 import { expectToFail } from '../../utils/utils';
+import { ngServe } from '../../utils/project';
+import { updateJsonFile } from '../../utils/project';
 
 export default function () {
   return Promise.resolve()
-    .then(() => ng('serve', '--port', '4400'))
-    .then(() => expectToFail(() => ng('e2e')))
-    .then(() => ng('e2e', '--no-serve', '--base-url=http://localhost:4400'))
+    .then(() => expectToFail(() => ng('e2e', '--no-serve')))
+    .then(() => updateJsonFile('.angular-cli.json', configJson => {
+      const app = configJson.defaults;
+      app.serve = { port: 4400 };
+    }))
+    .then(() => ngServe())
+    .then(() => expectToFail(() => ng('e2e', '--no-serve')))
+    .then(() => ng('e2e', '--no-serve', '--base-href=http://localhost:4400'))
     .then(() => killAllProcesses(), (err: any) => {
       killAllProcesses();
       throw err;
