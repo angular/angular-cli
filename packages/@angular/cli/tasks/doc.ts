5e39361f2ceec1b86d8648af544b9c52e6dcf216
Sumit Arora <er.sumitarora@gmail.com>
2017-04-10 10:03:53 -0400

    feat(@angular/cli): adding option to search docs as well

--- packages/@angular/cli/tasks/doc.ts
+++ packages/@angular/cli/tasks/doc.ts
@@ -2,8 +2,10 @@ const Task = require('../ember-cli/lib/models/task');
 const opn = require('opn');
 
 export const DocTask: any = Task.extend({
-  run: function(keyword: string) {
-    const searchUrl = `https://angular.io/docs/ts/latest/api/#!?query=${keyword}`;
+  run: function(keyword: string, search: boolean) {
+    const searchUrl = search ? `https://angular.io/search/#stq=${keyword}&stp=1` :
+     `https://angular.io/docs/ts/latest/api/#!?query=${keyword}`;
+
     return opn(searchUrl, { wait: false });
   }
 });
