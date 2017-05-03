3a6699580f8af2759c2ab9b8b782e6201a344307
Hans Larsen <hans@hansl.ca>
2017-05-03 16:57:29 -0700

    ci: remove the branch check from the commit validation

--- scripts/test-commit-messages.js
+++ scripts/test-commit-messages.js
@@ -2,14 +2,10 @@
 
 require('../lib/bootstrap-local');
 
-const fs = require('fs');
-const path = require('path');
 const validateCommitMessage = require('./validate-commit-message');
 const execSync = require('child_process').execSync;
 const chalk = require('chalk');
 const Logger = require('@ngtools/logger').Logger;
-const configPath = path.resolve(__dirname, './validate-commit-message/commit-message.json');
-const config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
 require('rxjs/add/operator/filter');
 
 // Configure logger
@@ -38,54 +34,21 @@ logger
 // Note: This is based on the gulp task found in the angular/angular repository
 execSync('git fetch origin');
 
-// Find the branch
-const branchRefs = {};
-for (const name of config['branches']) {
-  try {
-    const output = execSync(`git show-ref --hash ${name}`, { encoding: 'utf-8' });
-    if (output) {
-      branchRefs[name] = output.replace(/\n/g, '').trim();
-    }
-  } catch (e) {
-    // Ignore.
-  }
-}
-logger.info(`Found refs for branches:\n  ${Object.keys(branchRefs).map(key => {
-  return `${key} => ${JSON.stringify(branchRefs[key])}`;
-}).join('\n  ')}`);
 
-
-const output = execSync('git log --format="%H %s" --no-merges', { encoding: 'utf-8' });
+const output = execSync('git log master.. --reverse --format="%H %s" --no-merges', {
+  encoding: 'utf-8'
+});
 
 if (output.length === 0) {
   logger.warn('There are zero new commits between this HEAD and master');
   process.exit(0);
 }
+const commitsByLine = output.trim().split(/\n/).map(line => {
+  return line.trim().split(' ').slice(1).join(' ');
+});
+logger.info(`Examining ${commitsByLine.length} commit(s) between HEAD and master`);
 
-const commitsByLine = [];
-let branch = null;
-
-// Finding the closest branch marker.
-for (const line of output.split(/\n/)) {
-  const [hash, ...messageArray] = line.split(' ');
-  const message = messageArray.join(' ');
-
-  const maybeBranch = Object.keys(branchRefs).find(branchName => branchRefs[branchName] === hash);
-  if (maybeBranch) {
-    branch = maybeBranch;
-    break;
-  }
-  commitsByLine.push(message);
-}
-
-if (!branch) {
-  logger.fatal('Something wrong happened.');
-  process.exit(1);
-}
-
-logger.info(`Examining ${commitsByLine.length} commit(s) between HEAD and ${branch}`);
-
-const someCommitsInvalid = !commitsByLine.every(message => validateCommitMessage(message, branch));
+const someCommitsInvalid = !commitsByLine.every(message => validateCommitMessage(message));
 
 if (someCommitsInvalid) {
   logger.error('Please fix the failing commit messages before continuing...');
