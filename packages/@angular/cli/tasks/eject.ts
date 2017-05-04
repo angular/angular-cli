2c23cda37ffc103ecb511600f600da01a2b09347
Suguru Inatomi <laco0416@gmail.com>
2017-05-05 03:58:06 +0900

    fix(@angular/cli): install webpack at ejection (#5745)
    After ng eject, webpack package is not installed in the project, so npm run build requires global webpack installation.

--- packages/@angular/cli/tasks/eject.ts
+++ packages/@angular/cli/tasks/eject.ts
@@ -462,6 +462,7 @@ export default Task.extend({
 
         // Update all loaders from webpack, plus postcss plugins.
         [
+          'webpack',
           'autoprefixer',
           'css-loader',
           'cssnano',
