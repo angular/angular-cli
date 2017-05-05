921ff15be89e32b83c87a98e191cd5de527619c6
Sam Lin <maxisam@gmail.com>
2017-05-05 01:37:37 -0500

    fix: import at least one locale-data with intl
    It doesn't work if we just import intl

--- packages/@angular/cli/blueprints/ng/files/__path__/polyfills.ts
+++ packages/@angular/cli/blueprints/ng/files/__path__/polyfills.ts
@@ -66,3 +66,7 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
  * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
  */
 // import 'intl';  // Run `npm install --save intl`.
+/**
+ * Need to import at least one locale-data with intl.
+ */
+// import 'intl/locale-data/jsonp/en';
