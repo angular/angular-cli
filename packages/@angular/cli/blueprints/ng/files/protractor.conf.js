cfdbb670e3de8279a7fff1b68f65d65a81726ba6
Filipe Silva <filipematossilva@gmail.com>
2017-05-10 11:39:01 +0100

    fix(@angular/cli): allow e2e multi capabilities
    Thanks to @raymondkarstensen for this solution.
    
    Fix #975

--- packages/@angular/cli/blueprints/ng/files/protractor.conf.js
+++ packages/@angular/cli/blueprints/ng/files/protractor.conf.js
@@ -19,12 +19,10 @@ exports.config = {
     defaultTimeoutInterval: 30000,
     print: function() {}
   },
-  beforeLaunch: function() {
+  onPrepare() {
     require('ts-node').register({
       project: 'e2e/tsconfig.e2e.json'
     });
-  },
-  onPrepare() {
     jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
   }
 };
