0e1f65c815b302dcbab59a9e26e65db7d3d8eed1
Radoslav Kirilov <smoke@users.noreply.github.com>
2017-05-15 17:43:21 +0300

    fix(@angular/cli): prefix `historyApiFallback.index` with `deployUrl` (#6279)
    when implementing i18n following https://angular.io/docs/ts/latest/cookbook/i18n.html
    and https://medium.com/@feloy/deploying-an-i18n-angular-app-with-angular-cli-fc788f17e358
    
    when running in `development` to be consistent with `production` I am using
    
    ```
    ng serve --deploy-url /en/ --base-href /en/ \
    --aot --locale en --i18n-file ./src/locale/messages.en.xlf
    ```
    
    running the app this way, on routes other than `http://localhost:4200/en/`
    the `webpack-dev-server` was always saying that the route is not found,
    so this commit fixes that

--- tests/e2e/tests/misc/deploy-url.ts
+++ tests/e2e/tests/misc/deploy-url.ts
@@ -1,14 +1,18 @@
 import { killAllProcesses } from '../../utils/process';
 import { request } from '../../utils/http';
-import { expectToFail } from '../../utils/utils';
 import { ngServe } from '../../utils/project';
 
 export default function () {
   return Promise.resolve()
     // check when setup through command line arguments
-    .then(() => ngServe('--deploy-url', '/deployurl', '--base-href', '/deployurl'))
-    .then(() => expectToFail(() => request('http://localhost:4200')))
-    .then(() => request('http://localhost:4200/deployurl'))
+    .then(() => ngServe('--deploy-url', '/deployurl/', '--base-href', '/deployurl/'))
+    .then(() => request('http://localhost:4200'))
+    .then(body => {
+      if (!body.match(/<app-root><\/app-root>/)) {
+        throw new Error('Response does not match expected value.');
+      }
+    })
+    .then(() => request('http://localhost:4200/deployurl/'))
     .then(body => {
       if (!body.match(/<app-root><\/app-root>/)) {
         throw new Error('Response does not match expected value.');
