fe85750cb7db699505faf1cc63e3d35018d47ed2
Filipe Silva <filipematossilva@gmail.com>
2017-05-18 12:53:19 +0100

    feat(@angular/cli): add scope hoisting via webpack 3
    This should result in significant bundle size reduction. For best results, build for production with `--vendor-chunk=false`.
    
    See https://medium.com/webpack/webpack-3-official-release-15fd2dd8f07b for details.

--- packages/@angular/cli/tasks/eject.ts
+++ packages/@angular/cli/tasks/eject.ts
@@ -174,6 +174,9 @@ class JsonWebpackSerializer {
         case webpack.optimize.UglifyJsPlugin:
           this._addImport('webpack.optimize', 'UglifyJsPlugin');
           break;
+        case (webpack.optimize as any).ModuleConcatenationPlugin:
+          this._addImport('webpack.optimize', 'ModuleConcatenationPlugin');
+          break;
         case angularCliPlugins.BaseHrefWebpackPlugin:
         case angularCliPlugins.NamedLazyChunksWebpackPlugin:
         case angularCliPlugins.SuppressExtractedTextChunksWebpackPlugin:
