def3d5391a837ef0b7e87a373e05f82536fb8f6f
Charles Lyding <charles@thunderpos.com>
2017-05-24 08:55:34 -0400

    refactor(@angular/cli): remove additional unneeded ember-cli files

--- packages/@angular/cli/ember-cli/lib/commands.js
+++
@@ -1,2 +0,0 @@
-
-module.exports = { 'Unknown': require('./commands/unknown') };
--- packages/@angular/cli/ember-cli/lib/tasks.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-  DestroyFromBlueprint: require('./tasks/destroy-from-blueprint'),
-  GenerateFromBlueprint: require('./tasks/generate-from-blueprint'),
-  InstallBlueprint: require('./tasks/install-blueprint'),
-  NpmInstall: require('./tasks/npm-install'),
-  NpmTask: require('./tasks/npm-task'),
-};
