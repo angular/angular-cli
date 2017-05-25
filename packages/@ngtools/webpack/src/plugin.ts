608e04288b0211b2729c24f341adf7cb6fed14da
Hans Larsen <hans@hansl.ca>
2017-05-25 10:03:14 -0700

    revert: fix(@ngtools/webpack): fix paths-plugin to allow '*' as alias
    This reverts commit 6da95cdca7b7049ddb1386b3396867afedb78d2b.
    
    See #6451.

--- packages/@ngtools/webpack/src/plugin.ts
+++ packages/@ngtools/webpack/src/plugin.ts
@@ -341,11 +341,7 @@ export class AotPlugin implements Tapable {
           cb();
         }
       });
-    });
-
-    compiler.plugin('normal-module-factory', (nmf: any) => {
       compiler.resolvers.normal.apply(new PathsPlugin({
-        nmf,
         tsConfigPath: this._tsConfigPath,
         compilerOptions: this._compilerOptions,
         compilerHost: this._compilerHost
