27902c031f407c48ac2e463fed944c93c2f9fc38
FrozenPandaz <jasonjean1993@gmail.com>
2017-05-24 22:57:19 -0400

    refactor(@ngtools/webpack): refactor bootstrap refactoring

--- tests/e2e/assets/webpack/test-server-app/app/main.ts
+++ tests/e2e/assets/webpack/test-server-app/app/main.ts
@@ -2,4 +2,6 @@ import 'core-js/es7/reflect';
 import {platformDynamicServer} from '@angular/platform-server';
 import {AppModule} from './app.module';
 
+AppModule.testProp = 'testing';
+
 platformDynamicServer().bootstrapModule(AppModule);
