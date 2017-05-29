e79108f946b39b9d2aaa59046cc38a799d5c5b71
Trotyl Yu <trotyl@qq.com>
2017-05-29 15:33:20 +0800

    fix(@angular/cli): add missing WeakMap polyfill

--- packages/@angular/cli/blueprints/ng/files/__path__/polyfills.ts
+++ packages/@angular/cli/blueprints/ng/files/__path__/polyfills.ts
@@ -31,6 +31,7 @@
 // import 'core-js/es6/array';
 // import 'core-js/es6/regexp';
 // import 'core-js/es6/map';
+// import 'core-js/es6/weak-map';
 // import 'core-js/es6/set';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
