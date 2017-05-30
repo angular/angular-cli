d59f22a192922a60636c44d40ebb6c1e24073dd8
Reto Lehmann <reto.lehmann@me.com>
2017-05-30 17:43:13 +0200

    fix(@angular/cli): Fixed e2e task to respect --publicHost setting as baseUrl for protractor
    angular#6173 added two new settings
    to the serve task. The --publicHost setting is not respected as baseUrl
    for protractor in the e2e-task.
    
    With this fix, if --publicHost is set, it will be used as baseUrl for protrator.

--- packages/@angular/cli/tasks/e2e.ts
+++ packages/@angular/cli/tasks/e2e.ts
@@ -31,6 +31,7 @@ export const E2eTask = Task.extend({
         if (!clientUrl.hostname) {
           return Promise.reject(new SilentError(`'public-host' must be a full URL.`));
         }
+
         const protocol = e2eTaskOptions.ssl ? 'https' : 'http';
         additionalProtractorConfig.baseUrl = `${protocol}://${clientUrl.href}`;
       } else if (e2eTaskOptions.serve) {
