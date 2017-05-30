0a8dceddbd1f8558c8320415d87ff385b080b69c
Hans Larsen <hans@hansl.ca>
2017-05-30 15:54:45 -0700

    fix(@angular/cli): allow node_modules to be linked somewhere else.
    Fixes #6499.

--- packages/@angular/cli/models/webpack-configs/browser.ts
+++ packages/@angular/cli/models/webpack-configs/browser.ts
@@ -1,3 +1,4 @@
+import * as fs from 'fs';
 import * as webpack from 'webpack';
 import * as path from 'path';
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -24,14 +25,20 @@ export function getBrowserConfig(wco: WebpackConfigOptions) {
   if (buildOptions.vendorChunk) {
     // Separate modules from node_modules into a vendor chunk.
     const nodeModules = path.resolve(projectRoot, 'node_modules');
+    // Resolves all symlink to get the actual node modules folder.
+    const realNodeModules = fs.realpathSync(nodeModules);
     // --aot puts the generated *.ngfactory.ts in src/$$_gendir/node_modules.
     const genDirNodeModules = path.resolve(appRoot, '$$_gendir', 'node_modules');
 
     extraPlugins.push(new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor',
       chunks: ['main'],
-      minChunks: (module: any) => module.resource &&
-        (module.resource.startsWith(nodeModules) || module.resource.startsWith(genDirNodeModules))
+      minChunks: (module: any) => {
+        return module.resource
+            && (   module.resource.startsWith(nodeModules)
+                || module.resource.startsWith(genDirNodeModules)
+                || module.resource.startsWith(realNodeModules));
+      }
     }));
   }
 
