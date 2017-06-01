2c005edfe90faaccd811532ff6d50595ef85eda8
Sumit Arora <er.sumitarora@gmail.com>
2017-06-01 13:40:19 -0400

    fix(@angular/cli): fix routing blueprint

--- packages/@angular/cli/blueprints/ng/files/__path__/app/app-routing.module.ts
+++ packages/@angular/cli/blueprints/ng/files/__path__/app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
