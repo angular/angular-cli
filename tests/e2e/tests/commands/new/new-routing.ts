beb7514be6613b85c7979cdd1ea43a1a50530af9
Filipe Silva <filipematossilva@gmail.com>
2017-06-02 11:28:27 +0100

    ci: trim appveyor test suite

--- tests/e2e/tests/commands/new/new-routing.ts
+++ tests/e2e/tests/commands/new/new-routing.ts
@@ -1,8 +1,14 @@
 import {ng} from '../../../utils/process';
 import {createProject} from '../../../utils/project';
+import { getGlobalVariable } from '../../../utils/env';
 
 
 export default function() {
+  // Skip this in Appveyor tests.
+  if (getGlobalVariable('argv').appveyor) {
+    return Promise.resolve();
+  }
+
   return Promise.resolve()
     .then(() => createProject('routing-project', '--routing'))
 
