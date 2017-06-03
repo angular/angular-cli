f9053bf5b7ae8c98683cec8fdd7dae810e3c82a1
Charles Lyding <charles@thunderpos.com>
2017-06-02 23:59:39 -0400

    refactor(@angular/cli): simplify command registration

--- packages/@angular/cli/lib/cli/index.js
+++ packages/@angular/cli/lib/cli/index.js
@@ -10,6 +10,31 @@ const cli = require('../../ember-cli/lib/cli');
 const UI = require('../../ember-cli/lib/ui');
 const path = require('path');
 
+function loadCommands() {
+  return {
+    'build': require('../../commands/build').default,
+    'serve': require('../../commands/serve').default,
+    'eject': require('../../commands/eject').default,
+    'new': require('../../commands/new').default,
+    'generate': require('../../commands/generate').default,
+    'destroy': require('../../commands/destroy').default,
+    'test': require('../../commands/test').default,
+    'e2e': require('../../commands/e2e').default,
+    'help': require('../../commands/help').default,
+    'lint': require('../../commands/lint').default,
+    'version': require('../../commands/version').default,
+    'completion': require('../../commands/completion').default,
+    'doc': require('../../commands/doc').default,
+    'xi18n': require('../../commands/xi18n').default,
+
+    // Easter eggs.
+    'make-this-awesome': require('../../commands/easter-egg').default,
+
+    // Configuration.
+    'set': require('../../commands/set').default,
+    'get': require('../../commands/get').default
+  };
+}
 
 module.exports = function(options) {
 
@@ -21,6 +46,8 @@ module.exports = function(options) {
     root: path.join(__dirname, '..', '..'),
     npmPackage: '@angular/cli'
   };
+
+  options.commands = loadCommands();
 
   // ensure the environemnt variable for dynamic paths
   process.env.PWD = path.normalize(process.env.PWD || process.cwd());
