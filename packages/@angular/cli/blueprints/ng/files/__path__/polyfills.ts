f7847f58a3f80d4031b1593b34eed2486a0dbea1
Rokas Brazdžionis <rokas.brazdzionis@gmail.com>
2017-06-05 17:19:40 +0300

    docs(@angular/cli): correct web animations polyfill description

--- packages/@angular/cli/blueprints/ng/files/__path__/polyfills.ts
+++ packages/@angular/cli/blueprints/ng/files/__path__/polyfills.ts
@@ -37,16 +37,15 @@
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
 
-/** IE10 and IE11 requires the following to support `@angular/animation`. */
-// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
-
-
 /** Evergreen browsers require these. **/
 import 'core-js/es6/reflect';
 import 'core-js/es7/reflect';
 
 
-/** ALL Firefox browsers require the following to support `@angular/animation`. **/
+/**
+ * Required to support Web Animations `@angular/animation`.
+ * Needed for: All but Chrome, Firefox and Opera. http://caniuse.com/#feat=web-animation
+ **/
 // import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
 
