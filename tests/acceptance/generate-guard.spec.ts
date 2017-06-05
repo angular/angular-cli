53417c0f5ce7bf160b78797dc6a7878a0578ba89
Charles Lyding <charles@thunderpos.com>
2017-06-05 09:31:53 -0400

    test: convert acceptance tests to jasmine

--- tests/acceptance/generate-guard.spec.ts
+++ tests/acceptance/generate-guard.spec.ts
@@ -1,29 +1,13 @@
 import * as fs from 'fs-extra';
-import { expect } from 'chai';
 import * as path from 'path';
-
-const ng = require('../helpers/ng');
-const tmp = require('../helpers/tmp');
-const SilentError = require('silent-error');
+import { ng, setupProject } from '../helpers';
 
 const root = process.cwd();
 
 describe('Acceptance: ng generate guard', function () {
-  beforeEach(() => {
-    return tmp.setup('./tmp').then(() => {
-      process.chdir('./tmp');
-    }).then(() => {
-      return ng(['new', 'foo', '--skip-install']);
-    });
-  });
+  setupProject();
 
-  afterEach(() => {
-    this.timeout(10000);
-
-    return tmp.teardown('./tmp');
-  });
-
-  it('ng generate guard my-guard', () => {
+  it('ng generate guard my-guard', (done) => {
     const appRoot = path.join(root, 'tmp/foo');
     const testPath = path.join(appRoot, 'src/app/my-guard.guard.ts');
     const testSpecPath = path.join(appRoot, 'src/app/my-guard.guard.spec.ts');
@@ -31,17 +15,18 @@ describe('Acceptance: ng generate guard', function () {
 
     return ng(['generate', 'guard', 'my-guard'])
       .then(() => {
-        expect(fs.pathExistsSync(testPath)).to.equal(true);
-        expect(fs.pathExistsSync(testSpecPath)).to.equal(true);
+        expect(fs.pathExistsSync(testPath)).toBe(true);
+        expect(fs.pathExistsSync(testSpecPath)).toBe(true);
       })
       .then(() => fs.readFile(appModulePath, 'utf-8'))
       .then((content: string) => {
-        expect(content).not.to.matches(/import.*MyGuardGuard.*from '.\/my-guard.guard';/);
-        expect(content).not.to.matches(/providers:\s*\[MyGuardGuard\]/m);
-      });
+        expect(content).not.toMatch(/import.*MyGuardGuard.*from '.\/my-guard.guard';/);
+        expect(content).not.toMatch(/providers:\s*\[MyGuardGuard\]/m);
+      })
+      .then(done, done.fail);
   });
 
-  it('ng generate guard my-guard --no-spec', () => {
+  it('ng generate guard my-guard --no-spec', (done) => {
     const appRoot = path.join(root, 'tmp/foo');
     const testPath = path.join(appRoot, 'src/app/my-guard.guard.ts');
     const testSpecPath = path.join(appRoot, 'src/app/my-guard.guard.spec.ts');
@@ -49,32 +34,35 @@ describe('Acceptance: ng generate guard', function () {
 
     return ng(['generate', 'guard', 'my-guard', '--no-spec'])
       .then(() => {
-        expect(fs.pathExistsSync(testPath)).to.equal(true);
-        expect(fs.pathExistsSync(testSpecPath)).to.equal(false);
+        expect(fs.pathExistsSync(testPath)).toBe(true);
+        expect(fs.pathExistsSync(testSpecPath)).toBe(false);
       })
       .then(() => fs.readFile(appModulePath, 'utf-8'))
       .then((content: string) => {
-        expect(content).not.to.matches(/import.*MyGuardGuard.*from '.\/my-guard.guard';/);
-        expect(content).not.to.matches(/providers:\s*\[MyGuardGuard\]/m);
-      });
+        expect(content).not.toMatch(/import.*MyGuardGuard.*from '.\/my-guard.guard';/);
+        expect(content).not.toMatch(/providers:\s*\[MyGuardGuard\]/m);
+      })
+      .then(done, done.fail);
   });
 
-  it('ng generate guard test' + path.sep + 'my-guard', () => {
+  it('ng generate guard test' + path.sep + 'my-guard', (done) => {
     fs.mkdirsSync(path.join(root, 'tmp', 'foo', 'src', 'app', 'test'));
     return ng(['generate', 'guard', 'test' + path.sep + 'my-guard']).then(() => {
       const testPath = path.join(root, 'tmp', 'foo', 'src', 'app', 'test', 'my-guard.guard.ts');
-      expect(fs.pathExistsSync(testPath)).to.equal(true);
-    });
+      expect(fs.pathExistsSync(testPath)).toBe(true);
+    })
+    .then(done, done.fail);
   });
 
-  it('ng generate guard test' + path.sep + '..' + path.sep + 'my-guard', () => {
+  it('ng generate guard test' + path.sep + '..' + path.sep + 'my-guard', (done) => {
     return ng(['generate', 'guard', 'test' + path.sep + '..' + path.sep + 'my-guard']).then(() => {
       const testPath = path.join(root, 'tmp', 'foo', 'src', 'app', 'my-guard.guard.ts');
-      expect(fs.pathExistsSync(testPath)).to.equal(true);
-    });
+      expect(fs.pathExistsSync(testPath)).toBe(true);
+    })
+    .then(done, done.fail);
   });
 
-  it('ng generate guard my-guard from a child dir', () => {
+  it('ng generate guard my-guard from a child dir', (done) => {
     fs.mkdirsSync(path.join(root, 'tmp', 'foo', 'src', 'app', '1'));
     return new Promise(function (resolve) {
       process.chdir('./src');
@@ -88,11 +76,12 @@ describe('Acceptance: ng generate guard', function () {
       })
       .then(() => {
         const testPath = path.join(root, 'tmp', 'foo', 'src', 'app', '1', 'my-guard.guard.ts');
-        expect(fs.pathExistsSync(testPath)).to.equal(true);
-      });
+        expect(fs.pathExistsSync(testPath)).toBe(true);
+      })
+      .then(done, done.fail);
   });
 
-  it('ng generate guard child-dir' + path.sep + 'my-guard from a child dir', () => {
+  it('ng generate guard child-dir' + path.sep + 'my-guard from a child dir', (done) => {
     fs.mkdirsSync(path.join(root, 'tmp', 'foo', 'src', 'app', '1', 'child-dir'));
     return new Promise(function (resolve) {
       process.chdir('./src');
@@ -107,12 +96,13 @@ describe('Acceptance: ng generate guard', function () {
       .then(() => {
         const testPath = path.join(
           root, 'tmp', 'foo', 'src', 'app', '1', 'child-dir', 'my-guard.guard.ts');
-        expect(fs.pathExistsSync(testPath)).to.equal(true);
-      });
+        expect(fs.pathExistsSync(testPath)).toBe(true);
+      })
+      .then(done, done.fail);
   });
 
   it('ng generate guard child-dir' + path.sep + '..' + path.sep + 'my-guard from a child dir',
-    () => {
+    (done) => {
       fs.mkdirsSync(path.join(root, 'tmp', 'foo', 'src', 'app', '1'));
       return new Promise(function (resolve) {
         process.chdir('./src');
@@ -128,13 +118,14 @@ describe('Acceptance: ng generate guard', function () {
         .then(() => {
           const testPath =
             path.join(root, 'tmp', 'foo', 'src', 'app', '1', 'my-guard.guard.ts');
-          expect(fs.pathExistsSync(testPath)).to.equal(true);
-        });
+          expect(fs.pathExistsSync(testPath)).toBe(true);
+        })
+        .then(done, done.fail);
     });
 
   it('ng generate guard ' + path.sep + 'my-guard from a child dir, gens under ' +
     path.join('src', 'app'),
-    () => {
+    (done) => {
       fs.mkdirsSync(path.join(root, 'tmp', 'foo', 'src', 'app', '1'));
       return new Promise(function (resolve) {
         process.chdir('./src');
@@ -148,29 +139,33 @@ describe('Acceptance: ng generate guard', function () {
         })
         .then(() => {
           const testPath = path.join(root, 'tmp', 'foo', 'src', 'app', 'my-guard.guard.ts');
-          expect(fs.pathExistsSync(testPath)).to.equal(true);
-        });
+          expect(fs.pathExistsSync(testPath)).toBe(true);
+        })
+        .then(done, done.fail);
     });
 
-  it('ng generate guard ..' + path.sep + 'my-guard from root dir will fail', () => {
-    return ng(['generate', 'guard', '..' + path.sep + 'my-guard']).then(() => {
-      throw new SilentError(`ng generate guard ..${path.sep}my-guard from root dir should fail.`);
-    }, (err: string) => {
-      // tslint:disable-next-line:max-line-length
-      expect(err).to.equal(`Invalid path: "..${path.sep}my-guard" cannot be above the "src${path.sep}app" directory`);
-    });
+  it('ng generate guard ..' + path.sep + 'my-guard from root dir will fail', (done) => {
+    return ng(['generate', 'guard', '..' + path.sep + 'my-guard'])
+      .then(() => done.fail())
+      .catch(err => {
+        // tslint:disable-next-line:max-line-length
+        expect(err).toBe(`Invalid path: "..${path.sep}my-guard" cannot be above the "src${path.sep}app" directory`);
+      })
+      .then(done, done.fail);
   });
 
-  it('should error out when given an incorrect module path', () => {
+  it('should error out when given an incorrect module path', (done) => {
     return Promise.resolve()
       .then(() => ng(['generate', 'guard', 'baz', '--module', 'foo']))
+      .then(() => done.fail())
       .catch((error) => {
-        expect(error).to.equal('Specified module does not exist');
-      });
+        expect(error).toBe('Specified module does not exist');
+      })
+      .then(done, done.fail);
   });
 
   describe('should import and add to provider list', () => {
-    it('when given a root level module with module.ts suffix', () => {
+    it('when given a root level module with module.ts suffix', (done) => {
       const appRoot = path.join(root, 'tmp/foo');
       const modulePath = path.join(appRoot, 'src/app/app.module.ts');
 
@@ -178,12 +173,13 @@ describe('Acceptance: ng generate guard', function () {
         .then(() => ng(['generate', 'guard', 'baz', '--module', 'app.module.ts']))
         .then(() => fs.readFile(modulePath, 'utf-8'))
         .then(content => {
-          expect(content).to.matches(/import.*BazGuard.*from '.\/baz.guard';/);
-          expect(content).to.matches(/providers:\s*\[BazGuard\]/m);
-        });
+          expect(content).toMatch(/import.*BazGuard.*from '.\/baz.guard';/);
+          expect(content).toMatch(/providers:\s*\[BazGuard\]/m);
+        })
+        .then(done, done.fail);
     });
 
-    it('when given a root level module with missing module.ts suffix', () => {
+    it('when given a root level module with missing module.ts suffix', (done) => {
       const appRoot = path.join(root, 'tmp/foo');
       const modulePath = path.join(appRoot, 'src/app/app.module.ts');
 
@@ -191,12 +187,13 @@ describe('Acceptance: ng generate guard', function () {
         .then(() => ng(['generate', 'guard', 'baz', '--module', 'app']))
         .then(() => fs.readFile(modulePath, 'utf-8'))
         .then(content => {
-          expect(content).to.matches(/import.*BazGuard.*from '.\/baz.guard';/);
-          expect(content).to.matches(/providers:\s*\[BazGuard\]/m);
-        });
+          expect(content).toMatch(/import.*BazGuard.*from '.\/baz.guard';/);
+          expect(content).toMatch(/providers:\s*\[BazGuard\]/m);
+        })
+        .then(done, done.fail);
     });
 
-    it('when given a submodule with module.ts suffix', () => {
+    it('when given a submodule with module.ts suffix', (done) => {
       const appRoot = path.join(root, 'tmp/foo');
       const modulePath = path.join(appRoot, 'src/app/foo/foo.module.ts');
 
@@ -205,12 +202,13 @@ describe('Acceptance: ng generate guard', function () {
         .then(() => ng(['generate', 'guard', 'baz', '--module', path.join('foo', 'foo.module.ts')]))
         .then(() => fs.readFile(modulePath, 'utf-8'))
         .then(content => {
-          expect(content).to.matches(/import.*BazGuard.*from '..\/baz.guard';/);
-          expect(content).to.matches(/providers:\s*\[BazGuard\]/m);
-        });
+          expect(content).toMatch(/import.*BazGuard.*from '..\/baz.guard';/);
+          expect(content).toMatch(/providers:\s*\[BazGuard\]/m);
+        })
+        .then(done, done.fail);
     });
 
-    it('when given a submodule with missing module.ts suffix', () => {
+    it('when given a submodule with missing module.ts suffix', (done) => {
       const appRoot = path.join(root, 'tmp/foo');
       const modulePath = path.join(appRoot, 'src/app/foo/foo.module.ts');
 
@@ -219,12 +217,13 @@ describe('Acceptance: ng generate guard', function () {
         .then(() => ng(['generate', 'guard', 'baz', '--module', path.join('foo', 'foo')]))
         .then(() => fs.readFile(modulePath, 'utf-8'))
         .then(content => {
-          expect(content).to.matches(/import.*BazGuard.*from '..\/baz.guard';/);
-          expect(content).to.matches(/providers:\s*\[BazGuard\]/m);
-        });
+          expect(content).toMatch(/import.*BazGuard.*from '..\/baz.guard';/);
+          expect(content).toMatch(/providers:\s*\[BazGuard\]/m);
+        })
+        .then(done, done.fail);
     });
 
-    it('when given a submodule folder', () => {
+    it('when given a submodule folder', (done) => {
       const appRoot = path.join(root, 'tmp/foo');
       const modulePath = path.join(appRoot, 'src/app/foo/foo.module.ts');
 
@@ -233,12 +232,13 @@ describe('Acceptance: ng generate guard', function () {
         .then(() => ng(['generate', 'guard', 'baz', '--module', 'foo']))
         .then(() => fs.readFile(modulePath, 'utf-8'))
         .then(content => {
-          expect(content).to.matches(/import.*BazGuard.*from '..\/baz.guard';/);
-          expect(content).to.matches(/providers:\s*\[BazGuard\]/m);
-        });
+          expect(content).toMatch(/import.*BazGuard.*from '..\/baz.guard';/);
+          expect(content).toMatch(/providers:\s*\[BazGuard\]/m);
+        })
+        .then(done, done.fail);
     });
 
-    it('when given deep submodule folder with missing module.ts suffix', () => {
+    it('when given deep submodule folder with missing module.ts suffix', (done) => {
       const appRoot = path.join(root, 'tmp/foo');
       const modulePath = path.join(appRoot, 'src/app/foo/bar/bar.module.ts');
 
@@ -248,9 +248,10 @@ describe('Acceptance: ng generate guard', function () {
         .then(() => ng(['generate', 'guard', 'baz', '--module', path.join('foo', 'bar')]))
         .then(() => fs.readFile(modulePath, 'utf-8'))
         .then(content => {
-          expect(content).to.matches(/import.*BazGuard.*from '..\/..\/baz.guard';/);
-          expect(content).to.matches(/providers:\s*\[BazGuard\]/m);
-        });
+          expect(content).toMatch(/import.*BazGuard.*from '..\/..\/baz.guard';/);
+          expect(content).toMatch(/providers:\s*\[BazGuard\]/m);
+        })
+        .then(done, done.fail);
     });
   });
 });
--- tests/acceptance/new.spec.ts
+++ tests/acceptance/new.spec.ts
@@ -1,31 +1,29 @@
-// tslint:disable:max-line-length no-unused-expression
+// tslint:disable:max-line-length
 import * as fs from 'fs-extra';
-import { expect } from 'chai';
 import * as path from 'path';
 import * as util from 'util';
 import { EOL } from 'os';
 import { forEach } from 'lodash';
+import { ng } from '../helpers';
 
-const ng = require('../helpers/ng');
 const tmp = require('../helpers/tmp');
 const walkSync = require('walk-sync');
-const SilentError = require('silent-error');
 const Blueprint = require('@angular/cli/ember-cli/lib/models/blueprint');
 
 const root = process.cwd();
 
 
 describe('Acceptance: ng new', function () {
-  beforeEach(function () {
-    return tmp.setup('./tmp').then(function () {
-      process.chdir('./tmp');
-    });
-  });
+  beforeEach((done) => {
+    spyOn(console, 'error');
 
-  afterEach(function () {
-    this.timeout(10000);
+    tmp.setup('./tmp')
+      .then(() => process.chdir('./tmp'))
+      .then(() => done());
+  }, 10000);
 
-    return tmp.teardown('./tmp');
+  afterEach((done) => {
+    tmp.teardown('./tmp').then(() => done());
   });
 
   function confirmBlueprintedForDir(dir: string) {
@@ -45,8 +43,8 @@ describe('Acceptance: ng new', function () {
 
       expected.sort();
 
-      expect(directory).to.equal('foo');
-      expect(expected).to.deep.equal(
+      expect(directory).toBe('foo');
+      expect(expected).toEqual(
         actual,
         EOL + ' expected: ' + util.inspect(expected) + EOL + ' but got: ' + util.inspect(actual));
 
@@ -57,127 +55,142 @@ describe('Acceptance: ng new', function () {
     return confirmBlueprintedForDir('blueprints/ng');
   }
 
-  it('requires a valid name (!)', () => {
+  it('requires a valid name (!)', (done) => {
     return ng(['new', '!', '--skip-install', '--skip-git', '--inline-template'])
-      .then(() => { throw new Error(); }, () => {});
+      .then(() => done.fail(), () => done());
   });
-  it('requires a valid name (abc-.)', () => {
+  it('requires a valid name (abc-.)', (done) => {
     return ng(['new', 'abc-.', '--skip-install', '--skip-git', '--inline-template'])
-      .then(() => { throw new Error(); }, () => {});
+      .then(() => done.fail(), () => done());
   });
-  it('requires a valid name (abc-)', () => {
+  it('requires a valid name (abc-)', (done) => {
     return ng(['new', 'abc-', '--skip-install', '--skip-git', '--inline-template'])
-      .then(() => { throw new Error(); }, () => {});
+      .then(() => done.fail(), () => done());
   });
-  it('requires a valid name (abc-def-)', () => {
+  it('requires a valid name (abc-def-)', (done) => {
     return ng(['new', 'abc-def-', '--skip-install', '--skip-git', '--inline-template'])
-      .then(() => { throw new Error(); }, () => {});
+      .then(() => done.fail(), () => done());
   });
-  it('requires a valid name (abc-123)', () => {
+  it('requires a valid name (abc-123)', (done) => {
     return ng(['new', 'abc-123', '--skip-install', '--skip-git', '--inline-template'])
-      .then(() => { throw new Error(); }, () => {});
+      .then(() => done.fail(), () => done());
   });
-  it('requires a valid name (abc)', () => {
-    return ng(['new', 'abc', '--skip-install', '--skip-git', '--inline-template']);
+  it('requires a valid name (abc)', (done) => {
+    return ng(['new', 'abc', '--skip-install', '--skip-git', '--inline-template'])
+      .then(() => done(), () => done.fail());
   });
-  it('requires a valid name (abc-def)', () => {
-    return ng(['new', 'abc-def', '--skip-install', '--skip-git', '--inline-template']);
-  });
-
-  it('ng new foo, where foo does not yet exist, works', function () {
-    return ng(['new', 'foo', '--skip-install']).then(confirmBlueprinted);
+  it('requires a valid name (abc-def)', (done) => {
+    return ng(['new', 'abc-def', '--skip-install', '--skip-git', '--inline-template'])
+      .then(() => done(), () => done.fail());
   });
 
-  it('ng new with empty app does throw exception', function () {
-    expect(ng(['new', ''])).to.throw;
+  it('ng new foo, where foo does not yet exist, works', (done) => {
+    return ng(['new', 'foo', '--skip-install'])
+      .then(confirmBlueprinted)
+      .then(done, done.fail);
   });
 
-  it('ng new without app name does throw exception', function () {
-    expect(ng(['new', ''])).to.throw;
+  it('ng new with empty app does throw exception', (done) => {
+    return ng(['new', ''])
+      .then(() => done.fail(), () => done());
   });
 
-  it('ng new with app name creates new directory and has a dasherized package name', function () {
-    return ng(['new', 'FooApp', '--skip-install', '--skip-git']).then(function () {
+  it('ng new without app name does throw exception', (done) => {
+    return ng(['new'])
+      .then(() => done.fail(), () => done());
+  });
+
+  it('ng new with app name creates new directory and has a dasherized package name', (done) => {
+    return ng(['new', 'FooApp', '--skip-install', '--skip-git']).then(() => {
       expect(!fs.pathExistsSync('FooApp'));
 
       const pkgJson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
-      expect(pkgJson.name).to.equal('foo-app');
-    });
+      expect(pkgJson.name).toBe('foo-app');
+    })
+    .then(done, done.fail);
   });
 
-  it('ng new has a .editorconfig file', function () {
-    return ng(['new', 'FooApp', '--skip-install', '--skip-git']).then(function () {
+  it('ng new has a .editorconfig file', (done) => {
+    return ng(['new', 'FooApp', '--skip-install', '--skip-git']).then(() => {
       expect(!fs.pathExistsSync('FooApp'));
 
       const editorConfig = fs.readFileSync('.editorconfig', 'utf8');
-      expect(editorConfig).to.exist;
-    });
+      expect(editorConfig).toBeDefined();
+    })
+    .then(done, done.fail);
   });
 
-  it('Cannot run ng new, inside of Angular CLI project', function () {
+  it('Cannot run ng new, inside of Angular CLI project', (done) => {
     return ng(['new', 'foo', '--skip-install', '--skip-git'])
-      .then(function () {
+      .then(() => {
         return ng(['new', 'foo', '--skip-install', '--skip-git']).then(() => {
-          throw new SilentError('Cannot run ng new, inside of Angular CLI project should fail.');
+          done.fail();
         }, () => {
           expect(!fs.pathExistsSync('foo'));
         });
       })
-      .then(confirmBlueprinted);
+      .then(confirmBlueprinted)
+      .then(done, done.fail);
   });
 
-  it('ng new without skip-git flag creates .git dir', function () {
-    return ng(['new', 'foo', '--skip-install']).then(function () {
+  it('ng new without skip-git flag creates .git dir', (done) => {
+    return ng(['new', 'foo', '--skip-install']).then(() => {
       expect(fs.pathExistsSync('.git'));
-    });
+    })
+    .then(done, done.fail);
   });
 
-  it('ng new with --dry-run does not create new directory', function () {
-    return ng(['new', 'foo', '--dry-run']).then(function () {
+  it('ng new with --dry-run does not create new directory', (done) => {
+    return ng(['new', 'foo', '--dry-run']).then(() => {
       const cwd = process.cwd();
-      expect(cwd).to.not.match(/foo/, 'does not change cwd to foo in a dry run');
-      expect(!fs.pathExistsSync(path.join(cwd, 'foo')), 'does not create new directory');
-      expect(!fs.pathExistsSync(path.join(cwd, '.git')), 'does not create git in current directory');
-    });
+      expect(cwd).not.toMatch(/foo/, 'does not change cwd to foo in a dry run');
+      expect(fs.pathExistsSync(path.join(cwd, 'foo'))).toBe(false, 'does not create new directory');
+      expect(fs.pathExistsSync(path.join(cwd, '.git'))).toBe(false, 'does not create git in current directory');
+    })
+    .then(done, done.fail);
   });
 
-  it('ng new with --directory uses given directory name and has correct package name', function () {
+  it('ng new with --directory uses given directory name and has correct package name', (done) => {
     return ng(['new', 'foo', '--skip-install', '--skip-git', '--directory=bar'])
-      .then(function () {
+      .then(() => {
         const cwd = process.cwd();
-        expect(cwd).to.not.match(/foo/, 'does not use app name for directory name');
-        expect(!fs.pathExistsSync(path.join(cwd, 'foo')), 'does not create new directory with app name');
+        expect(cwd).not.toMatch(/foo/, 'does not use app name for directory name');
+        expect(fs.pathExistsSync(path.join(cwd, 'foo'))).toBe(false, 'does not create new directory with app name');
 
-        expect(cwd).to.match(/bar/, 'uses given directory name');
-        expect(fs.pathExistsSync(path.join(cwd, 'bar')), 'creates new directory with specified name');
+        expect(cwd).toMatch(/bar/, 'uses given directory name');
+        expect(fs.pathExistsSync(path.join(cwd, '..', 'bar'))).toBe(true, 'creates new directory with specified name');
 
         const pkgJson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
-        expect(pkgJson.name).to.equal('foo', 'uses app name for package name');
-      });
+        expect(pkgJson.name).toBe('foo', 'uses app name for package name');
+      })
+      .then(done, done.fail);
   });
 
-  it('ng new --inline-template does not generate a template file', () => {
+  it('ng new --inline-template does not generate a template file', (done) => {
     return ng(['new', 'foo', '--skip-install', '--skip-git', '--inline-template'])
       .then(() => {
         const templateFile = path.join('src', 'app', 'app.component.html');
-        expect(fs.pathExistsSync(templateFile)).to.equal(false);
-      });
+        expect(fs.pathExistsSync(templateFile)).toBe(false);
+      })
+      .then(done, done.fail);
   });
 
-  it('ng new --inline-style does not gener a style file', () => {
+  it('ng new --inline-style does not gener a style file', (done) => {
     return ng(['new', 'foo', '--skip-install', '--skip-git', '--inline-style'])
       .then(() => {
         const styleFile = path.join('src', 'app', 'app.component.css');
-        expect(fs.pathExistsSync(styleFile)).to.equal(false);
-      });
+        expect(fs.pathExistsSync(styleFile)).toBe(false);
+      })
+      .then(done, done.fail);
   });
 
-  it('should skip spec files when passed --skip-tests', () => {
+  it('should skip spec files when passed --skip-tests', (done) => {
     return ng(['new', 'foo', '--skip-install', '--skip-git', '--skip-tests'])
       .then(() => {
         const specFile = path.join('src', 'app', 'app.component.spec.ts');
-        expect(fs.pathExistsSync(specFile)).to.equal(false);
-      });
+        expect(fs.pathExistsSync(specFile)).toBe(false);
+      })
+      .then(done, done.fail);
   });
 
 });
