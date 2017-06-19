25edfc62f755ac50f8dbeebb341b3226949c9e6b
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2017-06-19 08:29:07 +0300

    remove unwanted promise

--- packages/@angular/cli/tasks/npm-install.ts
+++ packages/@angular/cli/tasks/npm-install.ts
@@ -13,23 +13,20 @@ export default Task.extend({
     }
 
     return checkYarnOrCNPM().then(function () {
-      return new Promise(function(resolve, reject) {
-        ui.writeLine(chalk.green(`Installing packages for tooling via ${packageManager}.`));
-        let installCommand = `${packageManager} install`;
-        if (packageManager === 'npm') {
-          installCommand = `${packageManager} --quiet install`;
+      ui.writeLine(chalk.green(`Installing packages for tooling via ${packageManager}.`));
+      let installCommand = `${packageManager} install`;
+      if (packageManager === 'npm') {
+        installCommand = `${packageManager} --quiet install`;
+      }
+      exec(installCommand,
+        (err: NodeJS.ErrnoException, _stdout: string, stderr: string) => {
+        if (err) {
+          ui.writeLine(stderr);
+          ui.writeLine(chalk.red('Package install failed, see above.'));
+          throw new Error('Package install failed, see above.');
+        } else {
+          ui.writeLine(chalk.green(`Installed packages for tooling via ${packageManager}.`));
         }
-        exec(installCommand,
-          (err: NodeJS.ErrnoException, _stdout: string, stderr: string) => {
-          if (err) {
-            ui.writeLine(stderr);
-            ui.writeLine(chalk.red('Package install failed, see above.'));
-            reject();
-          } else {
-            ui.writeLine(chalk.green(`Installed packages for tooling via ${packageManager}.`));
-            resolve();
-          }
-        });
       });
     });
   }
