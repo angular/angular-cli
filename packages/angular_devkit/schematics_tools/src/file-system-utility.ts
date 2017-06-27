bef1056edfb439e43ed52b6cf0c633485237d041
Hans Larsen <hans@hansl.ca>
2017-06-27 14:11:49 -0700

    refactor(@angular-devkit/schematics): use the jsonParser from core

--- packages/angular_devkit/schematics_tools/src/file-system-utility.ts
+++ packages/angular_devkit/schematics_tools/src/file-system-utility.ts
@@ -5,6 +5,7 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
+import {JsonParseMode, parseJson} from '@angular-devkit/core';
 import {existsSync, readFileSync} from 'fs';
 
 
@@ -41,62 +42,6 @@ export function readFile(fileName: string): string | null {
 }
 
 
-/**
- * Parse a JSON string and optionally remove comments from it first.
- */
-export function parseJson(content: string, stripComments = true): any {
-  if (stripComments) {
-    // Simple parser to ignore strings.
-    let inString = false;
-    for (let i = 0; i < content.length; i++) {
-      if (inString) {
-        // Skip `\X` characters. Since we only care about \", we don't need to fully process valid
-        // escape sequences (e.g. unicode sequences). This algorithm could accept invalid sequences
-        // but the JSON.parse call will throw on those so the output of the whole function will
-        // still be valid.
-        switch (content[i]) {
-          case '\\': i++; break;
-          case '"': inString = false; break;
-          // Else ignore characters.
-        }
-      } else {
-        switch (content[i]) {
-          case '"': inString = true; break;
-          case '/':
-            let j = 0;
-            switch (content[i + 1]) {
-              case '/':  // Strip until end-of-line.
-                for (j = i + 2; j < content.length; j++) {
-                  if (content[j] == '\n' || content[j] == '\r') {
-                    break;
-                  }
-                }
-                break;
-              case '*':  // Strip until `*/`.
-                for (j = i + 2; j < content.length; j++) {
-                  if (content[j - 1] == '*' && content[j] == '/') {
-                    break;
-                  }
-                }
-                break;
-            }
-            if (j) {
-              // Use `j + 1` to skip the end-of-comment character.
-              content = content.substr(0, i) + content.substr(j + 1);
-              i--;
-            }
-            break;
-        }
-      }
-    }
-    // At this point we don't really care about the validity of the resulting string, since
-    // JSON.parse will validate the JSON itself.
-  }
-
-  return JSON.parse(content);
-}
-
-
 export function readJsonFile(path: string): any {
-  return parseJson(readFile(path) || '{}');
+  return parseJson(readFile(path) || '{}', JsonParseMode.Loose);
 }
