2780c3d7ab9632311a217718694a898b12dd7a78
Filipe Silva <filipematossilva@gmail.com>
2017-07-02 15:24:05 +0100

    fix(@angular/cli): use inline sourcemaps in test
    Fix #6737

--- packages/@angular/cli/models/webpack-configs/common.ts
+++ packages/@angular/cli/models/webpack-configs/common.ts
@@ -65,15 +65,6 @@ export function getCommonConfig(wco: WebpackConfigOptions) {
     extraPlugins.push(new ProgressPlugin({ profile: buildOptions.verbose, colors: true }));
   }
 
-  if (buildOptions.sourcemaps) {
-    extraPlugins.push(new webpack.SourceMapDevToolPlugin({
-      filename: '[file].map[query]',
-      moduleFilenameTemplate: '[resource-path]',
-      fallbackModuleFilenameTemplate: '[resource-path]?[hash]',
-      sourceRoot: 'webpack:///'
-    }));
-  }
-
   if (buildOptions.showCircularDependencies) {
     extraPlugins.push(new CircularDependencyPlugin({
       exclude: /(\\|\/)node_modules(\\|\/)/
