5ec1d980dcceece1d79f8969d6caf57edc0dc42b
Justin Schwartzenberger <schwarty@gmail.com>
2017-07-03 18:52:20 -0700

    feat(@schematics/angular): Add interface schematic

---
+++ packages/schematics/angular/interface/files/__path__/__name@dasherize____type__.ts
@@ -0,0 +1,2 @@
+export interface <%= prefix %><%= classify(name) %> {
+}
---
+++ packages/schematics/angular/interface/index.ts
@@ -0,0 +1,40 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+// TODO: replace `options: any` with an actual type generated from the schema.
+// tslint:disable:no-any
+import {
+  Rule,
+  apply,
+  branchAndMerge,
+  chain,
+  mergeWith,
+  move,
+  template,
+  url,
+} from '@angular-devkit/schematics';
+import * as stringUtils from '../strings';
+
+
+export default function (options: any): Rule {
+  options.prefix = options.prefix ? options.prefix : '';
+  options.type = !!options.type ? `.${options.type}` : '';
+
+  const templateSource = apply(url('./files'), [
+    template({
+      ...stringUtils,
+      ...options,
+    }),
+    move(options.sourceDir),
+  ]);
+
+  return chain([
+    branchAndMerge(chain([
+      mergeWith(templateSource),
+    ])),
+  ]);
+}
