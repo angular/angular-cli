529a02364750782bfee91dcb7b27fc5e88f3033a
Filipe Silva <filipematossilva@gmail.com>
2017-07-05 18:35:10 +0100

    fix(@angular/cli): use a single exclamation mark for blueprint
    ...then shalt thou count to one, no more, no less. One shall be the number thou shalt count, and the number of the counting shall be one. Two shalt thou not count, neither count thou zeroth, excepting that thou then proceed to one. Three is right out.

--- packages/@angular/cli/blueprints/ng/files/__path__/app/app.component.spec.ts
+++ packages/@angular/cli/blueprints/ng/files/__path__/app/app.component.spec.ts
@@ -31,6 +31,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to <%= prefix %>!!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to <%= prefix %>!');
   }));
 });
--- packages/@angular/cli/blueprints/ng/files/e2e/app.e2e-spec.ts
+++ packages/@angular/cli/blueprints/ng/files/e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('<%= htmlComponentName %> App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to <%= prefix %>!!');
+    expect(page.getParagraphText()).toEqual('Welcome to <%= prefix %>!');
   });
 });
