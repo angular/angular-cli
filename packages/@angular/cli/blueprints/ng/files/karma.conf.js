c579b94ec743271cb8b0aa776bc5ed17115d8dd8
Lukas Elmer <lukas.elmer@gmail.com>
2017-07-18 00:12:46 +0200

    docs(@angular/cli): link to latest karma doc

--- packages/@angular/cli/blueprints/ng/files/karma.conf.js
+++ packages/@angular/cli/blueprints/ng/files/karma.conf.js
@@ -1,5 +1,5 @@
 // Karma configuration file, see link for more information
-// https://karma-runner.github.io/0.13/config/configuration-file.html
+// https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
   config.set({
