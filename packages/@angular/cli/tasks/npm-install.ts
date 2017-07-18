3ce685b461d65dad1d15844a177086a7688b2a9b
Filipe Silva <filipematossilva@gmail.com>
2017-07-18 13:37:23 +0100

    fix(@angular/cli): wait for install to finish before exiting

--- packages/@angular/cli/tasks/npm-install.ts
+++ packages/@angular/cli/tasks/npm-install.ts
@@ -1,34 +1,35 @@
 const Task = require('../ember-cli/lib/models/task');
 import * as chalk from 'chalk';
-import {exec} from 'child_process';
-import {checkYarnOrCNPM} from '../utilities/check-package-manager';
+import { exec } from 'child_process';
 
 
 export default Task.extend({
-  run: function() {
+  run: function () {
     const ui = this.ui;
     let packageManager = this.packageManager;
     if (packageManager === 'default') {
       packageManager = 'npm';
     }
 
-    return checkYarnOrCNPM().then(function () {
-      ui.writeLine(chalk.green(`Installing packages for tooling via ${packageManager}.`));
-      let installCommand = `${packageManager} install`;
-      if (packageManager === 'npm') {
-        installCommand = `${packageManager} --quiet install`;
-      }
+    ui.writeLine(chalk.green(`Installing packages for tooling via ${packageManager}.`));
+    let installCommand = `${packageManager} install`;
+    if (packageManager === 'npm') {
+      installCommand = `${packageManager} --quiet install`;
+    }
+
+    return new Promise((resolve, reject) => {
       exec(installCommand,
         (err: NodeJS.ErrnoException, _stdout: string, stderr: string) => {
-        if (err) {
-          ui.writeLine(stderr);
-          const message = 'Package install failed, see above.';
-          ui.writeLine(chalk.red(message));
-          throw new Error(message);
-        } else {
-          ui.writeLine(chalk.green(`Installed packages for tooling via ${packageManager}.`));
-        }
-      });
+          if (err) {
+            ui.writeLine(stderr);
+            const message = 'Package install failed, see above.';
+            ui.writeLine(chalk.red(message));
+            reject(message);
+          } else {
+            ui.writeLine(chalk.green(`Installed packages for tooling via ${packageManager}.`));
+            resolve();
+          }
+        });
     });
   }
 });
