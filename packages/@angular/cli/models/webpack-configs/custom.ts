88bd0fd59d806b80b0a2bf2baa61f711496e5f84
Saulo Vallory <me@saulovallory.com>
2017-07-28 01:54:54 -0300

    Remove console.log calls and fix file check

--- packages/@angular/cli/models/webpack-configs/custom.ts
+++ packages/@angular/cli/models/webpack-configs/custom.ts
@@ -6,9 +6,7 @@ import { WebpackConfigOptions } from '../webpack-config';
 export const getCustomConfig = function (_wco: WebpackConfigOptions) {
   const customConfigPath = path.join(_wco.projectRoot, 'webpack.config.js');
 
-  console.log('checking for webpack custom config in', customConfigPath);
-  if (fs.statSync(customConfigPath)) {
-    console.log('Found! Loading...');
+  if (fs.existsSync(customConfigPath)) {
     return require(customConfigPath);
   }
 
