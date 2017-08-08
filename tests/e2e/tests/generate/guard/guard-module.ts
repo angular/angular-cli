2ecbe3d69025179be4310bb9d21e4593505d61df
Andrew Wong <andrew.kw.w@gmail.com>
2017-08-08 01:22:45 -0400

    test(@angular/cli): Add tests for running 'generate' in a subdirectory (#7135)

--- tests/e2e/tests/generate/guard/guard-module.ts
+++ tests/e2e/tests/generate/guard/guard-module.ts
@@ -1,15 +1,25 @@
+import * as fs from 'fs-extra';
 import {join} from 'path';
 import {ng} from '../../../utils/process';
 import {expectFileToMatch} from '../../../utils/fs';
 
 
 export default function() {
-  const modulePath = join('src', 'app', 'app.module.ts');
+  const root = process.cwd();
+  const modulePath = join(root, 'src', 'app', 'app.module.ts');
+
+  fs.mkdirSync('./src/app/sub-dir');
 
   return ng('generate', 'guard', 'test-guard', '--module', 'app.module.ts')
     .then(() => expectFileToMatch(modulePath,
       /import { TestGuardGuard } from '.\/test-guard.guard'/))
     .then(() => expectFileToMatch(modulePath,
       /providers:\s*\[TestGuardGuard\]/m))
+
+    .then(() => process.chdir(join(root, 'src', 'app')))
+    .then(() => ng('generate', 'guard', 'test-guard2', '--module', 'app.module.ts'))
+    .then(() => expectFileToMatch(modulePath,
+      /import { TestGuard2Guard } from '.\/test-guard2.guard'/))
+
     .then(() => ng('build'));
 }
