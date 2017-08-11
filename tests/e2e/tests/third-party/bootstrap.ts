f83dcb67d80ffeeca6799d6fae5efb175102ecd3
Michael Cebrian <michael.cebrian@gmail.com>
2017-08-11 11:59:43 -0400

    ci: Update Bootstrap beta dependencies for e2e test

--- tests/e2e/tests/third-party/bootstrap.ts
+++ tests/e2e/tests/third-party/bootstrap.ts
@@ -6,19 +6,21 @@ import {oneLineTrim} from 'common-tags';
 
 export default function() {
   return Promise.resolve()
+    .then(() => npm('install', 'jquery@3.2.1'))
+    .then(() => npm('install', 'popper.js@1.11.0'))
     .then(() => npm('install', 'bootstrap@next'))
     .then(() => updateJsonFile('.angular-cli.json', configJson => {
       const app = configJson['apps'][0];
       app['styles'].push('../node_modules/bootstrap/dist/css/bootstrap.css');
       app['scripts'].push(
         '../node_modules/jquery/dist/jquery.js',
-        '../node_modules/tether/dist/js/tether.js',
+        '../node_modules/popper.js/dist/popper.js',
         '../node_modules/bootstrap/dist/js/bootstrap.js'
       );
     }))
     .then(() => ng('build', '--extract-css'))
     .then(() => expectFileToMatch('dist/scripts.bundle.js', '* jQuery JavaScript'))
-    .then(() => expectFileToMatch('dist/scripts.bundle.js', '/*! tether '))
+    .then(() => expectFileToMatch('dist/scripts.bundle.js', '* @fileOverview Kickass library to create and place poppers'))
     .then(() => expectFileToMatch('dist/scripts.bundle.js', '* Bootstrap'))
     .then(() => expectFileToMatch('dist/styles.bundle.css', '* Bootstrap'))
     .then(() => expectFileToMatch('dist/index.html', oneLineTrim`
