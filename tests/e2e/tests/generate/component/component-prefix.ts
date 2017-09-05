fa493f144fff1ec93dbf7e92d321bf8c38a7f2d0
Mike Brocchi <mbrocchi@gmail.com>
2017-09-05 19:32:24 +0200

    fix(@angular/cli): Use the app default prefix when generating
    fixes #7522

---
+++ tests/e2e/tests/generate/component/component-prefix.ts
@@ -0,0 +1,21 @@
+import {join} from 'path';
+import {ng} from '../../../utils/process';
+import {expectFileToMatch} from '../../../utils/fs';
+import { updateJsonFile } from '../../../utils/project';
+
+
+export default function() {
+  const componentDir = join('src', 'app', 'test-component');
+
+  return Promise.resolve()
+    .then(() => updateJsonFile('.angular-cli.json', configJson => {
+      const app = configJson['apps'][0];
+      app['prefix'] = 'pre';
+    }))
+    .then(() => ng('generate', 'component', 'test-component'))
+    .then(() => expectFileToMatch(join(componentDir, 'test-component.component.ts'),
+      /selector: 'pre-/))
+
+    // Try to run the unit tests.
+    .then(() => ng('test', '--single-run'));
+}
