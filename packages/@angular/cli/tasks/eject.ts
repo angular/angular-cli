800484db5979a7949f3e66f0e752b5900f5f71bc
Filipe Silva <filipematossilva@gmail.com>
2017-09-06 15:12:07 +0100

    feat(@angular/cli): support ES2015 target

--- packages/@angular/cli/tasks/eject.ts
+++ packages/@angular/cli/tasks/eject.ts
@@ -24,6 +24,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const SilentError = require('silent-error');
 const CircularDependencyPlugin = require('circular-dependency-plugin');
 const ConcatPlugin = require('webpack-concat-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const Task = require('../ember-cli/lib/models/task');
 
 const ProgressPlugin = require('webpack/lib/ProgressPlugin');
@@ -158,6 +159,10 @@ class JsonWebpackSerializer {
     return plugin.settings;
   }
 
+  private _uglifyjsPlugin(plugin: any) {
+    return plugin.options;
+  }
+
   private _pluginsReplacer(plugins: any[]) {
     return plugins.map(plugin => {
       let args = plugin.options || undefined;
@@ -228,6 +233,10 @@ class JsonWebpackSerializer {
         case ConcatPlugin:
           args = this._concatPlugin(plugin);
           this.variableImports['webpack-concat-plugin'] = 'ConcatPlugin';
+          break;
+        case UglifyJSPlugin:
+          args = this._uglifyjsPlugin(plugin);
+          this.variableImports['uglifyjs-webpack-plugin'] = 'UglifyJsPlugin';
           break;
         default:
           if (plugin.constructor.name == 'AngularServiceWorkerPlugin') {
@@ -546,6 +555,7 @@ export default Task.extend({
           'circular-dependency-plugin',
           'webpack-concat-plugin',
           'copy-webpack-plugin',
+          'uglifyjs-webpack-plugin',
         ].forEach((packageName: string) => {
           packageJson['devDependencies'][packageName] = ourPackageJson['dependencies'][packageName];
         });
