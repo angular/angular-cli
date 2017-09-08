e6f38412bc25f43072a2eae500e9b7c1403d9546
Hans Larsen <hans@hansl.ca>
2017-09-08 12:11:11 -0700

    fix(@angular/cli): take publicPath into account when building entry map
    Fixes #7628

--- tests/e2e/tests/misc/deploy-url.ts
+++ tests/e2e/tests/misc/deploy-url.ts
@@ -1,7 +1,8 @@
 import { killAllProcesses } from '../../utils/process';
 import { request } from '../../utils/http';
-import { ngServe } from '../../utils/project';
+import { ngServe, updateJsonFile } from '../../utils/project';
 import { getGlobalVariable } from '../../utils/env';
+import { writeMultipleFiles } from '../../utils/fs';
 
 export default function () {
   // Skip this in Appveyor tests.
@@ -10,18 +11,29 @@ export default function () {
   }
 
   return Promise.resolve()
+    .then(() => writeMultipleFiles({
+      'src/string-script.js': 'console.log(\'string-script\'); var number = 1+1;',
+    })
+    .then(() => updateJsonFile('.angular-cli.json', configJson => {
+      configJson['apps'][0]['scripts'] = [
+        'string-script.js',
+      ];
+    }))
     // check when setup through command line arguments
     .then(() => ngServe('--deploy-url', '/deployurl/', '--base-href', '/deployurl/'))
     .then(() => request('http://localhost:4200'))
     .then(body => {
       if (!body.match(/<app-root><\/app-root>/)) {
-        throw new Error('Response does not match expected value.');
+        throw new Error('Response does not match expected value. (1)');
+      }
+      if (!body.match(/"\/deployurl\/scripts.bundle.js"/)) {
+        throw new Error('Response does not match expected value. (2)');
       }
     })
     .then(() => request('http://localhost:4200/deployurl/'))
     .then(body => {
       if (!body.match(/<app-root><\/app-root>/)) {
-        throw new Error('Response does not match expected value.');
+        throw new Error('Response does not match expected value. (3)');
       }
     })
     .then(() => killAllProcesses(), (err) => { killAllProcesses(); throw err; });
