66eda91df56a78cd4715889b8c33cec7598ad088
laco0416 <laco0416+github@gmail.com>
2017-09-15 14:13:42 +0900

    docs(@schematics/angular): fix wrong package name for animations

--- packages/schematics/angular/application/files/__path__/polyfills.ts
+++ packages/schematics/angular/application/files/__path__/polyfills.ts
@@ -43,7 +43,7 @@ import 'core-js/es7/reflect';
 
 
 /**
- * Required to support Web Animations `@angular/animation`.
+ * Required to support Web Animations `@angular/platform-browser/animations`.
  * Needed for: All but Chrome, Firefox and Opera. http://caniuse.com/#feat=web-animation
  **/
 // import 'web-animations-js';  // Run `npm install --save web-animations-js`.
