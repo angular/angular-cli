5e3125306179fc4990257e062546f0132b0c3c2a
Fabian Wiles <fabianwiles@live.com>
2017-09-22 15:30:31 +1200

    fix(packaging): fail build.sh when a package does not build (#803)

--- build.sh
+++ build.sh
@@ -2,22 +2,22 @@ rm -rf dist
 
 set -x
 
-npm run build:express-engine
+npm run build:express-engine || exit 1
 
 cp modules/express-engine/package.json dist/express-engine/package.json
 cp modules/express-engine/README.md dist/express-engine/README.md
 
-npm run build:aspnetcore-engine
+npm run build:aspnetcore-engine || exit 1
 
 cp modules/aspnetcore-engine/package.json dist/aspnetcore-engine/package.json
 cp modules/aspnetcore-engine/README.md dist/aspnetcore-engine/README.md
 
-npm run build:hapi-engine
+npm run build:hapi-engine || exit 1
 
 cp modules/hapi-engine/package.json dist/hapi-engine/package.json
 cp modules/hapi-engine/README.md dist/hapi-engine/README.md
 
-npm run build:module-map-ngfactory-loader
+npm run build:module-map-ngfactory-loader || exit 1
 
 cp modules/module-map-ngfactory-loader/package.json dist/module-map-ngfactory-loader/package.json
 cp modules/module-map-ngfactory-loader/README.md dist/module-map-ngfactory-loader/README.md
