5f6aec746a4ea1089e1ed5d0f12d4aea200f814c
Charles Lyding <19598772+clydin@users.noreply.github.com>
2017-09-23 14:03:58 -0400

    test: convert helpers to typescript

--- tests/helpers/ng.ts
+++ tests/helpers/ng.ts
@@ -1,18 +1,16 @@
-'use strict';
-
+import cli from '@angular/cli/lib/cli';
 const UI = require('@angular/cli/ember-cli/lib/ui');
 const through = require('through');
 
-module.exports = MockUI;
 function MockUI() {
   this.output = '';
 
   UI.call(this, {
     inputStream: through(),
-    outputStream: through(function (data) {
+    outputStream: through(function (data: any) {
       this.output += data;
     }.bind(this)),
-    errorStream: through(function (data) {
+    errorStream: through(function (data: any) {
       this.errors += data;
     }.bind(this))
   });
@@ -23,3 +21,15 @@ MockUI.prototype.constructor = MockUI;
 MockUI.prototype.clear = function () {
   this.output = '';
 };
+
+export function ng(args: any) {
+  process.env.PWD = process.cwd();
+
+  return cli({
+    inputStream: [],
+    outputStream: [],
+    cliArgs: args,
+    UI: MockUI,
+    testing: true
+  });
+}
