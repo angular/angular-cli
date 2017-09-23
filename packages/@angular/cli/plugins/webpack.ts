27636014a42fc6ba1aca502a257e4c92e9b52ac2
Charles Lyding <19598772+clydin@users.noreply.github.com>
2017-09-23 13:48:46 -0400

    refactor(@angular/cli): convert additional files to typescript

---
+++ packages/@angular/cli/plugins/webpack.ts
@@ -0,0 +1,6 @@
+// Exports the webpack plugins we use internally.
+export { BaseHrefWebpackPlugin } from '../lib/base-href-webpack/base-href-webpack-plugin';
+export { GlobCopyWebpackPlugin, GlobCopyWebpackPluginOptions } from './glob-copy-webpack-plugin';
+export { InsertConcatAssetsWebpackPlugin } from './insert-concat-assets-webpack-plugin';
+export { NamedLazyChunksWebpackPlugin } from './named-lazy-chunks-webpack-plugin';
+export { SuppressExtractedTextChunksWebpackPlugin } from './suppress-entry-chunks-webpack-plugin';
