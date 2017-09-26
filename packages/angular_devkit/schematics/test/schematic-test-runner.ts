a617ae0d6536967b73d7a4f7a92417ef05cca707
Hans Larsen <hans@hansl.ca>
2017-09-25 19:11:47 -0700

    feat(@angular-devkit/schematics): use the core json schema for SchematicTestRunner

--- packages/angular_devkit/schematics/test/schematic-test-runner.ts
+++ packages/angular_devkit/schematics/test/schematic-test-runner.ts
@@ -5,6 +5,7 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
+import { Logger, schema } from '@angular-devkit/core';
 import {
   Collection,
   SchematicEngine,
@@ -15,7 +16,6 @@ import {
   FileSystemSchematicDesc,
   NodeModulesTestEngineHost,
 } from '@angular-devkit/schematics/tools';
-import { SchemaClassFactory } from '@ngtools/json-schema';
 import { Observable } from 'rxjs/Observable';
 
 
@@ -25,9 +25,13 @@ export class SchematicTestRunner {
   private _engineHost = new NodeModulesTestEngineHost();
   private _engine: SchematicEngine<{}, {}> = new SchematicEngine(this._engineHost);
   private _collection: Collection<{}, {}>;
+  private _logger: Logger;
+  private _registry: schema.JsonSchemaRegistry;
 
   constructor(private _collectionName: string, collectionPath: string) {
     this._engineHost.registerCollection(_collectionName, collectionPath);
+    this._logger = new Logger('test');
+    this._registry = new schema.JsonSchemaRegistry();
 
     this._engineHost.registerOptionsTransform((
       schematicDescription: {},
@@ -36,10 +40,13 @@ export class SchematicTestRunner {
       const schematic: FileSystemSchematicDesc = schematicDescription as FileSystemSchematicDesc;
 
       if (schematic.schema && schematic.schemaJson) {
-        const SchemaMetaClass = SchemaClassFactory<SchematicSchemaT>(schematic.schemaJson);
-        const schemaClass = new SchemaMetaClass(opts);
+        const schemaJson = schematic.schemaJson as schema.JsonSchemaObject;
+        const name = schemaJson.id || schematic.name;
+        this._registry.addSchema(name, schemaJson);
+        const serializer = new schema.serializers.JavascriptSerializer();
+        const fn = serializer.serialize(name, this._registry);
 
-        return schemaClass.$$root();
+        return fn(opts);
       }
 
       return opts;
@@ -48,11 +55,13 @@ export class SchematicTestRunner {
     this._collection = this._engine.createCollection(this._collectionName);
   }
 
+  get logger() { return this._logger; }
+
   runSchematicAsync(schematicName: string, opts?: SchematicSchemaT, tree?: Tree): Observable<Tree> {
     const schematic = this._collection.createSchematic(schematicName);
     const host = Observable.of(tree || new VirtualTree);
 
-    return schematic.call(opts || {}, host);
+    return schematic.call(opts || {}, host, { logger: this._logger });
   }
 
   runSchematic(schematicName: string, opts?: SchematicSchemaT, tree?: Tree): Tree {
@@ -61,7 +70,7 @@ export class SchematicTestRunner {
     let result: Tree | null = null;
     const host = Observable.of(tree || new VirtualTree);
 
-    schematic.call(opts || {}, host)
+    schematic.call(opts || {}, host, { logger: this._logger })
       .subscribe(t => result = t);
 
     if (result === null) {
