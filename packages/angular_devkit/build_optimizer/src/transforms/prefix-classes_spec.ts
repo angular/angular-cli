2d07d46b74a8f4fc89626c22800289c999a49a80
Charles Lyding <19598772+clydin@users.noreply.github.com>
2017-10-03 23:07:43 -0400

    test(@angular-devkit/build-optimizer): increase prefix class TS version coverage

--- packages/angular_devkit/build_optimizer/src/transforms/prefix-classes_spec.ts
+++ packages/angular_devkit/build_optimizer/src/transforms/prefix-classes_spec.ts
@@ -7,76 +7,205 @@
  */
 import { tags } from '@angular-devkit/core';
 import { transformJavascript } from '../helpers/transform-javascript';
-import { getPrefixClassesTransformer, testPrefixClasses } from './prefix-classes';
+import { getPrefixClassesTransformer } from './prefix-classes';
 
 
 const transform = (content: string) => transformJavascript(
   { content, getTransforms: [getPrefixClassesTransformer] }).content;
 
 describe('prefix-classes', () => {
-  it('prefix downleveled classes with /*@__PURE__*/', () => {
+  it('prefix TS 2.0 - 2.4 downlevel class', () => {
     const input = tags.stripIndent`
-      var ReplayEvent = (function () {
-          function ReplayEvent(time, value) {
-              this.time = time;
-              this.value = value;
-          }
-          return ReplayEvent;
+      var BasicTestCase = (function () {
+        function BasicTestCase() {
+        }
+        return BasicTestCase;
       }());
     `;
     const output = tags.stripIndent`
-      var ReplayEvent = /*@__PURE__*/ (function () {
-          function ReplayEvent(time, value) {
-              this.time = time;
-              this.value = value;
-          }
-          return ReplayEvent;
+      var BasicTestCase = /*@__PURE__*/ (function () {
+        function BasicTestCase() {
+        }
+        return BasicTestCase;
       }());
     `;
 
-    expect(testPrefixClasses(input)).toBeTruthy();
     expect(tags.oneLine`${transform(input)}`).toEqual(tags.oneLine`${output}`);
   });
 
-  // tslint:disable:max-line-length
-  it('prefix downleveled classes that extend another class with /*@__PURE__*/', () => {
+  // NOTE: TS 2.1+ uses a standalone export after the variable statement
+  it('prefix TS 2.0 exported downlevel class with ES2015 modules', () => {
     const input = tags.stripIndent`
-      var TakeUntilSubscriber = (function (_super) {
-          __extends(TakeUntilSubscriber, _super);
-          function TakeUntilSubscriber(destination, notifier) {
-              _super.call(this, destination);
-              this.notifier = notifier;
-              this.add(subscribeToResult_1.subscribeToResult(this, notifier));
-          }
-          TakeUntilSubscriber.prototype.notifyNext = function (outerValue, innerValue, outerIndex, innerIndex, innerSub) {
-              this.complete();
-          };
-          TakeUntilSubscriber.prototype.notifyComplete = function () {
-              // noop
-          };
-          return TakeUntilSubscriber;
-      }(OuterSubscriber_1.OuterSubscriber));
+      export var OuterSubscriber = (function (_super) {
+        __extends(OuterSubscriber, _super);
+        function OuterSubscriber() {
+            _super.apply(this, arguments);
+        }
+        return OuterSubscriber;
+      }());
     `;
     const output = tags.stripIndent`
-      var TakeUntilSubscriber = /*@__PURE__*/ (function (_super) {
-          __extends(TakeUntilSubscriber, _super);
-          function TakeUntilSubscriber(destination, notifier) {
-              _super.call(this, destination);
-              this.notifier = notifier;
-              this.add(subscribeToResult_1.subscribeToResult(this, notifier));
-          }
-          TakeUntilSubscriber.prototype.notifyNext = function (outerValue, innerValue, outerIndex, innerIndex, innerSub) {
-              this.complete();
-          };
-          TakeUntilSubscriber.prototype.notifyComplete = function () {
-              // noop
-          };
-          return TakeUntilSubscriber;
-      }(OuterSubscriber_1.OuterSubscriber));
+      export var OuterSubscriber = /*@__PURE__*/ (function (_super) {
+        __extends(OuterSubscriber, _super);
+        function OuterSubscriber() {
+            _super.apply(this, arguments);
+        }
+        return OuterSubscriber;
+      }());
     `;
 
-    expect(testPrefixClasses(input)).toBeTruthy();
     expect(tags.oneLine`${transform(input)}`).toEqual(tags.oneLine`${output}`);
   });
-  // tslint:enable:max-line-length
+
+  it('prefix TS 2.0 downlevel class with extends', () => {
+    const input = tags.stripIndent`
+      var ExtendedClass = (function (_super) {
+        __extends(ExtendedClass, _super);
+        function ExtendedClass() {
+            _super.apply(this, arguments);
+        }
+        return ExtendedClass;
+      }(StaticTestCase));
+    `;
+    const output = tags.stripIndent`
+      var ExtendedClass = /*@__PURE__*/ (function (_super) {
+        __extends(ExtendedClass, _super);
+        function ExtendedClass() {
+            _super.apply(this, arguments);
+        }
+        return ExtendedClass;
+      }(StaticTestCase));
+    `;
+
+    expect(tags.oneLine`${transform(input)}`).toEqual(tags.oneLine`${output}`);
+  });
+
+  it('prefix TS 2.1 - 2.3 downlevel class with static variable', () => {
+    const input = tags.stripIndent`
+      var StaticTestCase = (function () {
+        function StaticTestCase() {
+        }
+        return StaticTestCase;
+      }());
+      StaticTestCase.StaticTest = true;
+    `;
+    const output = tags.stripIndent`
+      var StaticTestCase = /*@__PURE__*/ (function () {
+        function StaticTestCase() {
+        }
+        return StaticTestCase;
+      }());
+      StaticTestCase.StaticTest = true;
+    `;
+
+    expect(tags.oneLine`${transform(input)}`).toEqual(tags.oneLine`${output}`);
+  });
+
+  it('prefix TS 2.1 - 2.4 downlevel class with extends', () => {
+    const input = tags.stripIndent`
+      var ExtendedClass = (function (_super) {
+        __extends(ExtendedClass, _super);
+        function ExtendedClass() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        return ExtendedClass;
+      }(StaticTestCase));
+    `;
+    const output = tags.stripIndent`
+      var ExtendedClass = /*@__PURE__*/ (function (_super) {
+        __extends(ExtendedClass, _super);
+        function ExtendedClass() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        return ExtendedClass;
+      }(StaticTestCase));
+    `;
+
+    expect(tags.oneLine`${transform(input)}`).toEqual(tags.oneLine`${output}`);
+  });
+
+  it('prefix TS 2.0 & 2.4 downlevel class with static variable', () => {
+    const input = tags.stripIndent`
+      var StaticTestCase = (function () {
+        function StaticTestCase() {
+        }
+        StaticTestCase.StaticTest = true;
+        return StaticTestCase;
+      }());
+    `;
+    const output = tags.stripIndent`
+      var StaticTestCase = /*@__PURE__*/ (function () {
+        function StaticTestCase() {
+        }
+        StaticTestCase.StaticTest = true;
+        return StaticTestCase;
+      }());
+    `;
+
+    expect(tags.oneLine`${transform(input)}`).toEqual(tags.oneLine`${output}`);
+  });
+
+  it('prefix TS 2.5 downlevel class', () => {
+    const input = tags.stripIndent`
+      var BasicTestCase = /** @class */ (function () {
+        function BasicTestCase() {
+        }
+        return BasicTestCase;
+      }());
+    `;
+    const output = tags.stripIndent`
+      var BasicTestCase = /*@__PURE__*/ (function () {
+        function BasicTestCase() {
+        }
+        return BasicTestCase;
+      }());
+    `;
+
+    expect(tags.oneLine`${transform(input)}`).toEqual(tags.oneLine`${output}`);
+  });
+
+  it('prefix TS 2.5 downlevel class with static variable', () => {
+    const input = tags.stripIndent`
+      var StaticTestCase = /** @class */ (function () {
+        function StaticTestCase() {
+        }
+        StaticTestCase.StaticTest = true;
+        return StaticTestCase;
+      }());
+    `;
+    const output = tags.stripIndent`
+      var StaticTestCase = /*@__PURE__*/ (function () {
+        function StaticTestCase() {
+        }
+        StaticTestCase.StaticTest = true;
+        return StaticTestCase;
+      }());
+    `;
+
+    expect(tags.oneLine`${transform(input)}`).toEqual(tags.oneLine`${output}`);
+  });
+
+  it('prefix TS 2.5 downlevel class with extends', () => {
+    const input = tags.stripIndent`
+      var ExtendedClass = /** @class */ (function (_super) {
+        __extends(ExtendedClass, _super);
+        function ExtendedClass() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        return ExtendedClass;
+      }(StaticTestCase));
+    `;
+    const output = tags.stripIndent`
+      var ExtendedClass = /*@__PURE__*/ (function (_super) {
+        __extends(ExtendedClass, _super);
+        function ExtendedClass() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        return ExtendedClass;
+      }(StaticTestCase));
+    `;
+
+    expect(tags.oneLine`${transform(input)}`).toEqual(tags.oneLine`${output}`);
+  });
+
 });
