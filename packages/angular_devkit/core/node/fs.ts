5589884c44c435429400251628e01d22ced62a46
Hans Larsen <hans@hansl.ca>
2017-10-05 14:24:26 -0700

    feat(@angular-devkit/core): add isDirectory to core/node

--- packages/angular_devkit/core/node/fs.ts
+++ packages/angular_devkit/core/node/fs.ts
@@ -21,3 +21,18 @@ export function isFile(filePath: string): boolean {
 
   return stat.isFile() || stat.isFIFO();
 }
+
+
+export function isDirectory(filePath: string): boolean {
+  let stat;
+  try {
+    stat = fs.statSync(filePath);
+  } catch (e) {
+    if (e && (e.code === 'ENOENT' || e.code === 'ENOTDIR')) {
+      return false;
+    }
+    throw e;
+  }
+
+  return stat.isDirectory();
+}
