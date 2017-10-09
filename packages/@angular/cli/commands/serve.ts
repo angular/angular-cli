2e1c9663cb2729b59d4f6ebeca6a15def5b0301b
Filipe Silva <filipematossilva@gmail.com>
2017-10-09 17:26:44 +0100

    fix(@angular/cli): fix eval-sourcemap when sourcemap is undefined
    Also add test to ensure it doesn't break.
    
    Followup to #7919

--- packages/@angular/cli/commands/serve.ts
+++ packages/@angular/cli/commands/serve.ts
@@ -135,11 +135,8 @@ const ServeCommand = Command.extend({
       commandOptions.vendorChunk = !commandOptions.buildOptimizer;
     }
 
-    // Default evalSourcemaps to true when sourcemaps are true.
-    // This makes rebuilds faster.
-    if (commandOptions.sourcemaps === true) {
-      commandOptions.evalSourcemaps = true;
-    }
+    // Default evalSourcemaps to true for serve. This makes rebuilds faster.
+    commandOptions.evalSourcemaps = true;
 
     return checkPort(commandOptions.port, commandOptions.host, defaultPort)
       .then(port => {
