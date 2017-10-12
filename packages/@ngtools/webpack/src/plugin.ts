e83dd931da7a1bb7828bd3326a6c561e50ecfd9b
Hans Larsen <hans@hansl.ca>
2017-10-12 08:09:19 -0700

    fix(@ngtools/webpack): fix for TypeScript module resolution cache missing in old version

--- packages/@ngtools/webpack/src/plugin.ts
+++ packages/@ngtools/webpack/src/plugin.ts
@@ -50,7 +50,7 @@ export class AotPlugin implements Tapable {
   private _compilerOptions: ts.CompilerOptions;
   private _angularCompilerOptions: any;
   private _program: ts.Program;
-  private _moduleResolutionCache: ts.ModuleResolutionCache;
+  private _moduleResolutionCache?: ts.ModuleResolutionCache;
   private _rootFilePath: string[];
   private _compilerHost: WebpackCompilerHost;
   private _resourceLoader: WebpackResourceLoader;
@@ -225,9 +225,11 @@ export class AotPlugin implements Tapable {
     this._program = ts.createProgram(
       this._rootFilePath, this._compilerOptions, this._compilerHost);
 
-    // We use absolute paths everywhere.
-    this._moduleResolutionCache = ts.createModuleResolutionCache(
-      this._basePath, (fileName: string) => fileName);
+    if (ts.createModuleResolutionCache) {
+      // We use absolute paths everywhere.
+      this._moduleResolutionCache = ts.createModuleResolutionCache(
+          this._basePath, (fileName: string) => fileName);
+    }
 
     // We enable caching of the filesystem in compilerHost _after_ the program has been created,
     // because we don't want SourceFile instances to be cached past this point.
