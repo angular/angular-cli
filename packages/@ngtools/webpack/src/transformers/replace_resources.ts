d208e6602b8b01912c4593e34d867b6cc44eb909
Filipe Silva <filipematossilva@gmail.com>
2017-10-13 17:02:03 +0100

    fix(@ngtools/webpack): rebuild only changed ngfactories
    This should improve AOT compilation times.

--- packages/@ngtools/webpack/src/transformers/replace_resources.ts
+++ packages/@ngtools/webpack/src/transformers/replace_resources.ts
@@ -10,53 +10,13 @@ import {
 
 export function replaceResources(sourceFile: ts.SourceFile): TransformOperation[] {
   const ops: TransformOperation[] = [];
+  const replacements = findResources(sourceFile);
 
-  // Find all object literals.
-  findAstNodes<ts.ObjectLiteralExpression>(null, sourceFile,
-    ts.SyntaxKind.ObjectLiteralExpression, true)
-    // Get all their property assignments.
-    .map(node => findAstNodes<ts.PropertyAssignment>(node, sourceFile,
-      ts.SyntaxKind.PropertyAssignment))
-    // Flatten into a single array (from an array of array<property assignments>).
-    .reduce((prev, curr) => curr ? prev.concat(curr) : prev, [])
-    // We only want property assignments for the templateUrl/styleUrls keys.
-    .filter((node: ts.PropertyAssignment) => {
-      const key = _getContentOfKeyLiteral(node.name);
-      if (!key) {
-        // key is an expression, can't do anything.
-        return false;
-      }
-      return key == 'templateUrl' || key == 'styleUrls';
-    })
-    // Replace templateUrl/styleUrls key with template/styles, and and paths with require('path').
-    .forEach((node: ts.PropertyAssignment) => {
-      const key = _getContentOfKeyLiteral(node.name);
+  if (replacements.length > 0) {
 
-      if (key == 'templateUrl') {
-        const requireCall = _createRequireCall(_getResourceRequest(node.initializer, sourceFile));
-        const propAssign = ts.createPropertyAssignment('template', requireCall);
-        ops.push(new ReplaceNodeOperation(sourceFile, node, propAssign));
-      } else if (key == 'styleUrls') {
-        const arr = findAstNodes<ts.ArrayLiteralExpression>(node, sourceFile,
-          ts.SyntaxKind.ArrayLiteralExpression, false);
-        if (!arr || arr.length == 0 || arr[0].elements.length == 0) {
-          return;
-        }
+    // Add the replacement operations.
+    ops.push(...(replacements.map((rep) => rep.replaceNodeOperation)));
 
-        const stylePaths = arr[0].elements.map((element: ts.Expression) => {
-          return _getResourceRequest(element, sourceFile);
-        });
-
-        const requireArray = ts.createArrayLiteral(
-          stylePaths.map((path) => _createRequireCall(path))
-        );
-
-        const propAssign = ts.createPropertyAssignment('styles', requireArray);
-        ops.push(new ReplaceNodeOperation(sourceFile, node, propAssign));
-      }
-    });
-
-  if (ops.length > 0) {
     // If we added a require call, we need to also add typings for it.
     // The typings need to be compatible with node typings, but also work by themselves.
 
@@ -80,6 +40,69 @@ export function replaceResources(sourceFile: ts.SourceFile): TransformOperation[
   }
 
   return ops;
+}
+
+export interface ResourceReplacement {
+  resourcePaths: string[];
+  replaceNodeOperation: ReplaceNodeOperation;
+}
+
+export function findResources(sourceFile: ts.SourceFile): ResourceReplacement[] {
+  const replacements: ResourceReplacement[] = [];
+
+  // Find all object literals.
+  findAstNodes<ts.ObjectLiteralExpression>(null, sourceFile,
+    ts.SyntaxKind.ObjectLiteralExpression, true)
+    // Get all their property assignments.
+    .map(node => findAstNodes<ts.PropertyAssignment>(node, sourceFile,
+      ts.SyntaxKind.PropertyAssignment))
+    // Flatten into a single array (from an array of array<property assignments>).
+    .reduce((prev, curr) => curr ? prev.concat(curr) : prev, [])
+    // We only want property assignments for the templateUrl/styleUrls keys.
+    .filter((node: ts.PropertyAssignment) => {
+      const key = _getContentOfKeyLiteral(node.name);
+      if (!key) {
+        // key is an expression, can't do anything.
+        return false;
+      }
+      return key == 'templateUrl' || key == 'styleUrls';
+    })
+    // Replace templateUrl/styleUrls key with template/styles, and and paths with require('path').
+    .forEach((node: ts.PropertyAssignment) => {
+      const key = _getContentOfKeyLiteral(node.name);
+
+      if (key == 'templateUrl') {
+        const resourcePath = _getResourceRequest(node.initializer, sourceFile);
+        const requireCall = _createRequireCall(resourcePath);
+        const propAssign = ts.createPropertyAssignment('template', requireCall);
+        replacements.push({
+          resourcePaths: [resourcePath],
+          replaceNodeOperation: new ReplaceNodeOperation(sourceFile, node, propAssign)
+        });
+      } else if (key == 'styleUrls') {
+        const arr = findAstNodes<ts.ArrayLiteralExpression>(node, sourceFile,
+          ts.SyntaxKind.ArrayLiteralExpression, false);
+        if (!arr || arr.length == 0 || arr[0].elements.length == 0) {
+          return;
+        }
+
+        const stylePaths = arr[0].elements.map((element: ts.Expression) => {
+          return _getResourceRequest(element, sourceFile);
+        });
+
+        const requireArray = ts.createArrayLiteral(
+          stylePaths.map((path) => _createRequireCall(path))
+        );
+
+        const propAssign = ts.createPropertyAssignment('styles', requireArray);
+        replacements.push({
+          resourcePaths: stylePaths,
+          replaceNodeOperation: new ReplaceNodeOperation(sourceFile, node, propAssign)
+        });
+      }
+    });
+
+  return replacements;
 }
 
 function _getContentOfKeyLiteral(node?: ts.Node): string | null {
---
+++ tests/e2e/tests/build/rebuild-ngfactories.ts
@@ -0,0 +1,95 @@
+import {
+  killAllProcesses,
+  waitForAnyProcessOutputToMatch,
+  execAndWaitForOutputToMatch,
+} from '../../utils/process';
+import { appendToFile, writeMultipleFiles, replaceInFile } from '../../utils/fs';
+import { request } from '../../utils/http';
+import { getGlobalVariable } from '../../utils/env';
+
+const validBundleRegEx = /webpack: bundle is now VALID|webpack: Compiled successfully./;
+
+export default function () {
+  if (process.platform.startsWith('win')) {
+    return Promise.resolve();
+  }
+  // Skip this in ejected tests.
+  if (getGlobalVariable('argv').eject) {
+    return Promise.resolve();
+  }
+
+  return execAndWaitForOutputToMatch('ng', ['serve', '--aot'], validBundleRegEx)
+    .then(() => writeMultipleFiles({
+      'src/app/app.component.css': `
+        @import './imported-styles.css';
+        body {background-color: #00f;}
+      `,
+      'src/app/imported-styles.css': 'p {color: #f00;}',
+    }))
+    // Trigger a few rebuilds first.
+    // The AOT compiler is still optimizing rebuilds on the first rebuild.
+    .then(() => Promise.all([
+      waitForAnyProcessOutputToMatch(validBundleRegEx, 10000),
+      appendToFile('src/main.ts', 'console.log(1)\n')
+    ]))
+    .then(() => Promise.all([
+      waitForAnyProcessOutputToMatch(validBundleRegEx, 10000),
+      appendToFile('src/main.ts', 'console.log(1)\n')
+    ]))
+    // Check if html changes are built.
+    .then(() => Promise.all([
+      waitForAnyProcessOutputToMatch(validBundleRegEx, 10000),
+      appendToFile('src/app/app.component.html', '<p>HTML_REBUILD_STRING<p>')
+    ]))
+    .then(() => request('http://localhost:4200/main.bundle.js'))
+    .then((body) => {
+      if (!body.match(/HTML_REBUILD_STRING/)) {
+        throw new Error('Expected HTML_REBUILD_STRING but it wasn\'t in bundle.');
+      }
+    })
+    // Check if css changes are built.
+    .then(() => Promise.all([
+      waitForAnyProcessOutputToMatch(validBundleRegEx, 10000),
+      appendToFile('src/app/app.component.css', 'CSS_REBUILD_STRING {color: #f00;}')
+    ]))
+    .then(() => request('http://localhost:4200/main.bundle.js'))
+    .then((body) => {
+      if (!body.match(/CSS_REBUILD_STRING/)) {
+        throw new Error('Expected CSS_REBUILD_STRING but it wasn\'t in bundle.');
+      }
+    })
+    // Check if css dependency changes are built.
+    .then(() => Promise.all([
+      waitForAnyProcessOutputToMatch(validBundleRegEx, 10000),
+      appendToFile('src/app/imported-styles.css', 'CSS_DEP_REBUILD_STRING {color: #f00;}')
+    ]))
+    .then(() => request('http://localhost:4200/main.bundle.js'))
+    .then((body) => {
+      if (!body.match(/CSS_DEP_REBUILD_STRING/)) {
+        throw new Error('Expected CSS_DEP_REBUILD_STRING but it wasn\'t in bundle.');
+      }
+    })
+    .then(() => {
+      // Skip in non-nightly tests. Switch this check around when ng5 is out.
+      if (!getGlobalVariable('argv').nightly) {
+        return Promise.resolve();
+      }
+
+      // Check if component metadata changes are built.
+      return Promise.resolve()
+        .then(() => Promise.all([
+          waitForAnyProcessOutputToMatch(validBundleRegEx, 10000),
+          replaceInFile('src/app/app.component.ts', 'app-root', 'app-root-FACTORY_REBUILD_STRING')
+        ]))
+        .then(() => request('http://localhost:4200/main.bundle.js'))
+        .then((body) => {
+          if (!body.match(/FACTORY_REBUILD_STRING/)) {
+            throw new Error('Expected FACTORY_REBUILD_STRING but it wasn\'t in bundle.');
+          }
+        });
+    })
+    .then(() => killAllProcesses(), (err: any) => {
+      killAllProcesses();
+      throw err;
+    });
+}
