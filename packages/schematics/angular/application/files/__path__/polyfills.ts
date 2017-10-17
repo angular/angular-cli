04dd7f579206019f6934588b35d18c6d3c4f2949
Hans Larsen <hans@hansl.ca>
2017-10-17 11:27:18 -0700

    fix(@schematics/angular): remove the Reflect polyfill
    Only IE11 and less needs it. See http://kangax.github.io/compat-table/es6/\#test-Reflect
    
    Fixes #206.

--- packages/schematics/angular/application/files/__path__/polyfills.ts
+++ packages/schematics/angular/application/files/__path__/polyfills.ts
@@ -37,8 +37,12 @@
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
 
+/** IE10 and IE11 requires the following for the Reflect API. */
+// import 'core-js/es6/reflect';
+
+
 /** Evergreen browsers require these. **/
-import 'core-js/es6/reflect';
+// Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
 import 'core-js/es7/reflect';
 
 
