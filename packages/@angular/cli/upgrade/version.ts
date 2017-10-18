f4881e6f48b24729a2df46c2ecb0400efbe9085a
Hans Larsen <hans@hansl.ca>
2017-10-18 11:25:26 -0700

    fix(@angular/cli): show warning on TypeScript 2.5
    It is not officially supported by Angular 5. Might be in 5.1 or above,
    and will revisit then.

--- packages/@angular/cli/upgrade/version.ts
+++ packages/@angular/cli/upgrade/version.ts
@@ -167,7 +167,7 @@ export class Version {
     const versionCombos = [
       { compiler: '>=2.3.1 <3.0.0', typescript: '>=2.0.2 <2.3.0' },
       { compiler: '>=4.0.0 <5.0.0', typescript: '>=2.1.0 <2.4.0' },
-      { compiler: '>=5.0.0 <6.0.0', typescript: '>=2.4.0 <2.6.0' }
+      { compiler: '>=5.0.0 <6.0.0', typescript: '>=2.4.0 <2.5.0' }
     ];
 
     const currentCombo = versionCombos.find((combo) => satisfies(compilerVersion, combo.compiler));
