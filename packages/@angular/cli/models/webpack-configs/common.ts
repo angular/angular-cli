angular/angular-cli

commit 776af8617bb867197c2c385a939940ff3960080d

fix(@angular/cli): update file-loader and url-loader
This should fix errors like the one below for 0.6.0:
```
URL Loader Invalid Options

options['name'] should NOT have additional properties
options.limit should be number
```

packages/@angular/cli/models/webpack-configs/common.ts
@@ -178,10 +178,21 @@ export function getCommonConfig(wco: WebpackConfigOptions) {
       rules: [
         { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader', exclude: [nodeModules] },
         { test: /\.html$/, loader: 'raw-loader' },
-        { test: /\.(eot|svg|cur)$/, loader: `file-loader?name=[name]${hashFormat.file}.[ext]` },
+        {
+          test: /\.(eot|svg|cur)$/,
+          loader: 'file-loader',
+          options: {
+            name: `[name]${hashFormat.file}.[ext]`,
+            limit: 10000
+          }
+        },
         {
           test: /\.(jpg|png|webp|gif|otf|ttf|woff|woff2|ani)$/,
-          loader: `url-loader?name=[name]${hashFormat.file}.[ext]&limit=10000`
+          loader: 'url-loader',
+          options: {
+            name: `[name]${hashFormat.file}`,
+            limit: 10000
+          }
         }
       ].concat(extraRules)
     },

commit be65aede817eba5a8519313815dd78a3080544e5

fix(@angular/cli): fix url-loader filename formatting
Fix file loading by readding the extension indicator.

Broken in commit 776af8617bb867197c2c385a939940ff3960080d .

packages/@angular/cli/models/webpack-configs/common.ts
@@ -191,7 +191,7 @@ export function getCommonConfig(wco: WebpackConfigOptions) {
           test: /\.(jpg|png|webp|gif|otf|ttf|woff|woff2|ani)$/,
           loader: 'url-loader',
           options: {
-            name: `[name]${hashFormat.file}`,
+            name: `[name]${hashFormat.file}.[ext]`,
             limit: 10000
           }
         }