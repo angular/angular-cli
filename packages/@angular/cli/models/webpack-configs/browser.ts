3d5f1631be41c6eaceb613ab49f30959569a94c6
Filipe Silva <filipematossilva@gmail.com>
2017-10-20 16:58:57 +0100

    fix(@angular/cli): disable node global
    The node only `global` object had been left in because it used to cause a build size regression with Angular.
    
    This doesn't seem to be the case anymore so it should be removed because it causes problematic interactions with some libraries.
    
    Fix #5804
    Supersedes #7931

--- packages/@angular/cli/models/webpack-configs/browser.ts
+++ packages/@angular/cli/models/webpack-configs/browser.ts
@@ -114,9 +114,7 @@ export function getBrowserConfig(wco: WebpackConfigOptions) {
     ].concat(extraPlugins),
     node: {
       fs: 'empty',
-      // `global` should be kept true, removing it resulted in a
-      // massive size increase with Build Optimizer on AIO.
-      global: true,
+      global: false,
       crypto: 'empty',
       tls: 'empty',
       net: 'empty',
