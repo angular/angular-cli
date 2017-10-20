774219c595c98d21851cc8b628320a3032d322fb
Filipe Silva <filipematossilva@gmail.com>
2017-10-20 16:58:57 +0100

    fix(@angular/cli): disable node global
    The node only `global` object had been left in because it used to cause a build size regression with Angular.
    
    This doesn't seem to be the case anymore so it should be removed because it causes problematic interactions with some libraries.
    
    Fix #5804
    Supersedes #7931

--- tests/e2e/tests/build/script-target.ts
+++ tests/e2e/tests/build/script-target.ts
@@ -1,28 +1,10 @@
-import { appendToFile, expectFileToMatch } from '../../utils/fs';
+import { expectFileToMatch } from '../../utils/fs';
 import { ng } from '../../utils/process';
-import { expectToFail } from '../../utils/utils';
 import { updateJsonFile } from '../../utils/project';
-import { getGlobalVariable } from '../../utils/env';
 
 
 export default function () {
-
-  let knownES6Module = '@angular/core/@angular/core';
-
-  // TODO: swap this check for ng5.
-  if (getGlobalVariable('argv').nightly) {
-    // Angular 5 has a different folder structure.
-    knownES6Module = '@angular/core/esm2015/core';
-  }
-
   return Promise.resolve()
-    // Force import a known ES6 module and build with prod.
-    // ES6 modules will cause UglifyJS to fail on a ES5 compilation target (default).
-    .then(() => appendToFile('src/main.ts', `
-      import * as es6module from '${knownES6Module}';
-      console.log(es6module);
-    `))
-    .then(() => expectToFail(() => ng('build', '--prod')))
     .then(() => updateJsonFile('tsconfig.json', configJson => {
       const compilerOptions = configJson['compilerOptions'];
       compilerOptions['target'] = 'es2015';
