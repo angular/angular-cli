d9a4f88560fe782cff9c4ab90553fb4d9e71395f
Filipe Silva <filipematossilva@gmail.com>
2017-10-20 16:58:57 +0100

    fix(@angular/cli): disable node global
    The node only `global` object had been left in because it used to cause a build size regression with Angular.
    
    This doesn't seem to be the case anymore so it should be removed because it causes problematic interactions with some libraries.
    
    Fix #5804
    Supersedes #7931

--- packages/@angular/cli/models/webpack-configs/production.ts
+++ packages/@angular/cli/models/webpack-configs/production.ts
@@ -127,34 +127,6 @@ export function getProdConfig(wco: WebpackConfigOptions) {
   const supportES2015 = tsConfig.options.target !== ts.ScriptTarget.ES3
     && tsConfig.options.target !== ts.ScriptTarget.ES5;
 
-  if (supportES2015) {
-    extraPlugins.push(new UglifyJSPlugin({
-      sourceMap: buildOptions.sourcemaps,
-      uglifyOptions: {
-        ecma: 6,
-        warnings: buildOptions.verbose,
-        ie8: false,
-        mangle: true,
-        compress: uglifyCompressOptions,
-        output: {
-          ascii_only: true,
-          comments: false
-        },
-      }
-    }));
-  } else {
-    uglifyCompressOptions.screw_ie8 = true;
-    uglifyCompressOptions.warnings = buildOptions.verbose;
-    extraPlugins.push(new webpack.optimize.UglifyJsPlugin(<any>{
-      mangle: { screw_ie8: true },
-      compress: uglifyCompressOptions,
-      output: { ascii_only: true },
-      sourceMap: buildOptions.sourcemaps,
-      comments: false
-    }));
-
-  }
-
   return {
     entry: entryPoints,
     plugins: [
@@ -163,6 +135,20 @@ export function getProdConfig(wco: WebpackConfigOptions) {
       }),
       new webpack.HashedModuleIdsPlugin(),
       new webpack.optimize.ModuleConcatenationPlugin(),
+      new UglifyJSPlugin({
+        sourceMap: buildOptions.sourcemaps,
+        uglifyOptions: {
+          ecma: supportES2015 ? 6 : 5,
+          warnings: buildOptions.verbose,
+          ie8: false,
+          mangle: true,
+          compress: uglifyCompressOptions,
+          output: {
+            ascii_only: true,
+            comments: false
+          },
+        }
+      }),
       ...extraPlugins
     ]
   };
