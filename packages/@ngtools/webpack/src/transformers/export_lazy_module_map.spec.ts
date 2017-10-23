3ec06290dd3406414f8ca00d66acb40ae6e59fe3
Mike Brocchi <mbrocchi@gmail.com>
2017-10-23 15:38:03 -0400

    fix(@ngtools/webpack): Handle factory files for the LAZY_MODULE_MAP
    Fixes: 7901

--- packages/@ngtools/webpack/src/transformers/export_lazy_module_map.spec.ts
+++ packages/@ngtools/webpack/src/transformers/export_lazy_module_map.spec.ts
@@ -3,8 +3,8 @@ import { oneLine, stripIndent } from 'common-tags';
 import { transformTypescript } from './ast_helpers';
 import { exportLazyModuleMap } from './export_lazy_module_map';
 
-describe('@ngtools/webpack transformers', () => {
-  describe('replace_resources', () => {
+describe('export_lazy_module_map', () => {
+  it('should create module map for JIT', () => {
     it('should replace resources', () => {
       const input = stripIndent`
         export { AppModule } from './app/app.module';
@@ -26,5 +26,29 @@ describe('@ngtools/webpack transformers', () => {
 
       expect(oneLine`${result}`).toEqual(oneLine`${output}`);
     });
+  });
+
+  it('should create module map for AOT', () => {
+    const input = stripIndent`
+      export { AppModule } from './app/app.module';
+    `;
+    // tslint:disable:max-line-length
+    const output = stripIndent`
+      import * as __lazy_0__ from "app/lazy/lazy.module.ts";
+      import * as __lazy_1__ from "app/lazy2/lazy2.module.ts";
+      export { AppModule } from './app/app.module';
+      export var LAZY_MODULE_MAP = { "./lazy/lazy.module#LazyModule": __lazy_0__.LazyModule, "./lazy2/lazy2.module#LazyModule2": __lazy_1__.LazyModule2 };
+    `;
+    // tslint:enable:max-line-length
+
+    const transformOpsCb = (sourceFile: ts.SourceFile) => exportLazyModuleMap(sourceFile, {
+      './lazy/lazy.module.ngfactory#LazyModuleNgFactory':
+        '/project/src/app/lazy/lazy.module.ngfactory.ts',
+      './lazy2/lazy2.module.ngfactory#LazyModule2NgFactory':
+        '/project/src/app/lazy2/lazy2.module.ngfactory.ts',
+    });
+    const result = transformTypescript(input, transformOpsCb);
+
+    expect(oneLine`${result}`).toEqual(oneLine`${output}`);
   });
 });
