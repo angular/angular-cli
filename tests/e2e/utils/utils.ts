0b80fba4581c21d86d4067f69a8eb79a19c23960
Filipe Silva <filipematossilva@gmail.com>
2017-10-26 19:10:07 +0100

    fix(@ngtools/webpack): fix and improve error reporting
    Fix #7925

--- tests/e2e/utils/utils.ts
+++ tests/e2e/utils/utils.ts
@@ -1,12 +1,12 @@
 
-export function expectToFail(fn: () => Promise<any>, errorMessage?: string): Promise<void> {
+export function expectToFail(fn: () => Promise<any>, errorMessage?: string): Promise<any> {
   return fn()
     .then(() => {
       const functionSource = fn.name || (<any>fn).source || fn.toString();
       const errorDetails = errorMessage ? `\n\tDetails:\n\t${errorMessage}` : '';
       throw new Error(
         `Function ${functionSource} was expected to fail, but succeeded.${errorDetails}`);
-    }, () => { });
+    }, (err) => { return err; });
 }
 
 export function wait(msecs: number): Promise<void> {
