5f979d784053adea11fa3af705154e739a6e1ae2
Filipe Silva <filipematossilva@gmail.com>
2017-10-30 18:22:30 +0000

    fix(@angular/cli): don't search for lazy routes in Angular 5 ng test
    Fix #8066

--- tests/e2e/setup/500-create-project.ts
+++ tests/e2e/setup/500-create-project.ts
@@ -52,16 +52,20 @@ export default function() {
     .then(() => updateTsConfig(json => {
       json['compilerOptions']['sourceRoot'] = '/';
     }))
-    .then(() => updateJsonFile('src/tsconfig.spec.json', json => {
+    .then(() => {
       if (argv.nightly) {
         // *************************************************************************************
         // REMOVE THIS WITH UPDATED NG5 SCHEMATICS
         // In ng5 we have to tell users users to update their tsconfig.json.
-        // `src/tsconfig.spec.json` needs to be updated with `"include": [ "**/*.ts" ]`
+        // `src/tsconfig.spec.json` needs to be updated with "polyfills.ts" on `include`.
         // *************************************************************************************
-        json['include'] = ['**/*.ts'];
+        return updateJsonFile('src/tsconfig.spec.json', json => {
+          json['include'] = json['include'].concat('polyfills.ts');
+        })
+        // Ignore error if file doesn't exist.
+        .catch(() => { return; });
       }
-    }))
+    })
     .then(() => git('config', 'user.email', 'angular-core+e2e@google.com'))
     .then(() => git('config', 'user.name', 'Angular CLI E2e'))
     .then(() => git('config', 'commit.gpgSign', 'false'))
