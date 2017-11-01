7f1ffadc42288842d7d2535c48728f02b9287242
Trotyl Yu <trotyl@qq.com>
2017-11-01 03:17:21 -0500

    docs(@schematics/angular): adjust statement of Zone.js prolyfill

--- packages/schematics/angular/application/files/__path__/polyfills.ts
+++ packages/schematics/angular/application/files/__path__/polyfills.ts
@@ -55,7 +55,7 @@ import 'core-js/es7/reflect';
 
 
 /***************************************************************************************************
- * Zone JS is required by Angular itself.
+ * Zone JS is required by default for Angular itself.
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 
