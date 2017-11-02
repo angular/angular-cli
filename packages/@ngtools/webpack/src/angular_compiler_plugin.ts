7cfbda56616b4594a5f7f41cacc504f1923dac29
Filipe Silva <filipematossilva@gmail.com>
2017-11-02 16:59:03 +0000

    fix(@ngtools/webpack): remove default type checker memory limit
    Seems to be causing trouble on Appveyor and we use the parent one anyway.

--- packages/@ngtools/webpack/src/angular_compiler_plugin.ts
+++ packages/@ngtools/webpack/src/angular_compiler_plugin.ts
@@ -464,25 +464,13 @@ export class AngularCompilerPlugin implements Tapable {
       ? './type_checker_bootstrap.js'
       : './type_checker.js';
 
-    let hasMemoryFlag = false;
-    const memoryFlagRegex = /--max-old-space-size/;
     const debugArgRegex = /--inspect(?:-brk|-port)?|--debug(?:-brk|-port)/;
 
     const execArgv = process.execArgv.filter((arg) => {
-      // Check if memory is being set by parent process.
-      if (memoryFlagRegex.test(arg)) {
-        hasMemoryFlag = true;
-      }
-
       // Remove debug args.
       // Workaround for https://github.com/nodejs/node/issues/9435
       return !debugArgRegex.test(arg);
     });
-
-    if (!hasMemoryFlag) {
-      // Force max 8gb ram.
-      execArgv.push('--max-old-space-size=8192');
-    }
 
     const forkOptions: ForkOptions = { execArgv };
 
