39631bf844ba10b39ec868731019d17da157d94a
Zhicheng Wang <zcwang@thoughtworks.com>
2017-11-02 17:01:49 +0800

    fix(@angular/cli): Don't specify the extract-css option as precondition for css sourcemaps
    Instead, use `convertToAbsoluteUrls: true`. See <https://github.com/webpack-contrib/style-loader#converttoabsoluteurls>.

--- packages/@angular/cli/models/webpack-configs/styles.ts
+++ packages/@angular/cli/models/webpack-configs/styles.ts
@@ -39,7 +39,7 @@ export function getStylesConfig(wco: WebpackConfigOptions) {
   // style-loader does not support sourcemaps without absolute publicPath, so it's
   // better to disable them when not extracting css
   // https://github.com/webpack-contrib/style-loader#recommended-configuration
-  const cssSourceMap = buildOptions.extractCss && buildOptions.sourcemaps;
+  const cssSourceMap = buildOptions.sourcemaps;
 
   // Minify/optimize css in production.
   const minimizeCss = buildOptions.target === 'production';
@@ -199,8 +199,12 @@ export function getStylesConfig(wco: WebpackConfigOptions) {
       const ret: any = {
         include: globalStylePaths,
         test,
-        use: buildOptions.extractCss ? ExtractTextPlugin.extract(extractTextPlugin)
-                                     : ['style-loader', ...extractTextPlugin.use]
+        use: buildOptions.extractCss ? ExtractTextPlugin.extract(extractTextPlugin) : [{
+          loader: 'style-loader',
+          options: {
+            convertToAbsoluteUrls: cssSourceMap,
+          },
+        }, ...extractTextPlugin.use],
       };
       // Save the original options as arguments for eject.
       if (buildOptions.extractCss) {
