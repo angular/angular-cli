68314923cd3d9674255711553f517bfd43849d61
Hans Larsen <hans@hansl.ca>
2017-11-16 11:34:54 -0800

    fix(@angular/cli): update postcss loader and URL versions.

--- packages/@angular/cli/models/webpack-configs/styles.ts
+++ packages/@angular/cli/models/webpack-configs/styles.ts
@@ -59,23 +59,24 @@ export function getStylesConfig(wco: WebpackConfigOptions) {
 
     return [
       postcssUrl({
-        url: (URL: string) => {
+        url: (URL: { url: string }) => {
+          const { url } = URL;
           // Only convert root relative URLs, which CSS-Loader won't process into require().
-          if (!URL.startsWith('/') || URL.startsWith('//')) {
-            return URL;
+          if (!url.startsWith('/') || url.startsWith('//')) {
+            return URL.url;
           }
 
           if (deployUrl.match(/:\/\//)) {
             // If deployUrl contains a scheme, ignore baseHref use deployUrl as is.
-            return `${deployUrl.replace(/\/$/, '')}${URL}`;
+            return `${deployUrl.replace(/\/$/, '')}${url}`;
           } else if (baseHref.match(/:\/\//)) {
             // If baseHref contains a scheme, include it as is.
             return baseHref.replace(/\/$/, '') +
-                `/${deployUrl}/${URL}`.replace(/\/\/+/g, '/');
+                `/${deployUrl}/${url}`.replace(/\/\/+/g, '/');
           } else {
             // Join together base-href, deploy-url and the original URL.
             // Also dedupe multiple slashes into single ones.
-            return `/${baseHref}/${deployUrl}/${URL}`.replace(/\/\/+/g, '/');
+            return `/${baseHref}/${deployUrl}/${url}`.replace(/\/\/+/g, '/');
           }
         }
       }),
