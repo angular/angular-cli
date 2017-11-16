b2fd67e8f0410d64c94bbd84d26dc40f6507b4a8
cexbrayat <cedric@ninja-squad.com>
2017-11-16 17:51:27 -0500

    fix(@schematics/angular): remove Intl polyfill

--- packages/schematics/angular/application/files/__path__/polyfills.ts
+++ packages/schematics/angular/application/files/__path__/polyfills.ts
@@ -64,13 +64,3 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
-
-/**
- * Date, currency, decimal and percent pipes.
- * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
- */
-// import 'intl';  // Run `npm install --save intl`.
-/**
- * Need to import at least one locale-data with intl.
- */
-// import 'intl/locale-data/jsonp/en';
