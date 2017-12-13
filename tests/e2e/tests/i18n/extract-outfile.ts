f700ba7e99e06178022146fba97240f513dfd14f
Olivier Combe <olivier.combe@gmail.com>
2017-12-13 14:29:39 +0100

    fix(@ngtools/webpack): set output locale for ng xi18n
    Fixes #8680

--- tests/e2e/tests/i18n/extract-outfile.ts
+++ tests/e2e/tests/i18n/extract-outfile.ts
@@ -1,9 +1,6 @@
 import {join} from 'path';
 import {ng} from '../../utils/process';
-import {
-  expectFileToExist, writeFile,
-  expectFileToMatch
-} from '../../utils/fs';
+import { writeFile, expectFileToMatch } from '../../utils/fs';
 
 
 export default function() {
@@ -14,8 +11,7 @@ export default function() {
     .then(() => ng('xi18n', '--out-file', 'messages.fr.xlf'))
     .then((output) => {
       if (!output.stdout.match(/starting from Angular v4/)) {
-        expectFileToExist(join('src', 'messages.fr.xlf'));
-        expectFileToMatch(join('src', 'messages.fr.xlf'), /Hello world/);
+        return expectFileToMatch(join('src', 'messages.fr.xlf'), 'Hello world');
       }
     });
 }
