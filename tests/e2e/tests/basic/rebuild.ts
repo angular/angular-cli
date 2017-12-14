9177ab3a3405ed9a94062d0125b548e0d3d43862
Filipe Silva <filipematossilva@gmail.com>
2017-12-14 14:56:38 +0000

    fix(@angular/cli): show only changed chunks on build
    For projects with a lot of lazy modules the rebuild messages can easily fill the whole console window.
    
    This PR shows only chunks that changed instead of showing all chunks.
    
    Before:
    ```
    $ ng serve
    ** NG Live Development Server is listening on localhost:4200, open your browser on http://localhost:4200/ **
    Date: 2017-12-14T14:56:13.707Z
    Hash: 7490b2942c48ffcf0f0f
    Time: 7317ms
    chunk {inline} inline.bundle.js (inline) 5.79 kB [entry] [rendered]
    chunk {main} main.bundle.js (main) 725 kB [initial] [rendered]
    chunk {polyfills} polyfills.bundle.js (polyfills) 577 kB [initial] [rendered]
    chunk {styles} styles.bundle.js (styles) 35 kB [initial] [rendered]
    chunk {vendor} vendor.bundle.js (vendor) 6.45 MB [initial] [rendered]
    
    webpack: Compiled successfully.
    webpack: Compiling...
    Date: 2017-12-14T14:56:17.054Z
    Hash: dbb03cc0994e8bf69e76
    Time: 310ms
    chunk {inline} inline.bundle.js (inline) 5.79 kB [entry]
    chunk {main} main.bundle.js (main) 725 kB [initial] [rendered]
    chunk {polyfills} polyfills.bundle.js (polyfills) 577 kB [initial]
    chunk {styles} styles.bundle.js (styles) 35 kB [initial]
    chunk {vendor} vendor.bundle.js (vendor) 6.45 MB [initial]
    
    webpack: Compiled successfully.
    webpack: Compiling...
    Date: 2017-12-14T14:56:20.290Z
    Hash: fe53cbcd529dd2508cfc
    Time: 267ms
    chunk {inline} inline.bundle.js (inline) 5.79 kB [entry]
    chunk {main} main.bundle.js (main) 725 kB [initial]
    chunk {polyfills} polyfills.bundle.js (polyfills) 577 kB [initial] [rendered]
    chunk {styles} styles.bundle.js (styles) 35 kB [initial]
    chunk {vendor} vendor.bundle.js (vendor) 6.45 MB [initial]
    
    webpack: Compiled successfully.
    ```
    
    After:
    ```
    $ ng serve
    ** NG Live Development Server is listening on localhost:4200, open your browser on http://localhost:4200/ **
    Date: 2017-12-14T14:53:40.216Z
    Hash: 44065f5ec5c4c8cf884a
    Time: 7312ms
    chunk {inline} inline.bundle.js (inline) 5.79 kB [entry] [rendered]
    chunk {main} main.bundle.js (main) 724 kB [initial] [rendered]
    chunk {polyfills} polyfills.bundle.js (polyfills) 576 kB [initial] [rendered]
    chunk {styles} styles.bundle.js (styles) 35 kB [initial] [rendered]
    chunk {vendor} vendor.bundle.js (vendor) 6.45 MB [initial] [rendered]
    
    webpack: Compiled successfully.
    webpack: Compiling...
    Date: 2017-12-14T14:53:42.089Z
    Hash: 492a7350b12ec1557b61
    Time: 241ms
    chunk {main} main.bundle.js (main) 725 kB [initial] [rendered]
    4 unchanged chunks
    
    webpack: Compiled successfully.
    webpack: Compiling...
    Date: 2017-12-14T14:53:49.762Z
    Hash: 7490b2942c48ffcf0f0f
    Time: 296ms
    chunk {polyfills} polyfills.bundle.js (polyfills) 577 kB [initial] [rendered]
    4 unchanged chunks
    
    webpack: Compiled successfully.
    ```
    
    Fix #8621

--- tests/e2e/tests/basic/rebuild.ts
+++ tests/e2e/tests/basic/rebuild.ts
@@ -20,14 +20,9 @@ export default function() {
     return Promise.resolve();
   }
 
-  let oldNumberOfChunks = 0;
-  const chunkRegExp = /chunk\s+\{/g;
+  const lazyChunkRegExp = /lazy\.module\.chunk\.js/g;
 
   return execAndWaitForOutputToMatch('ng', ['serve'], validBundleRegEx)
-    // Count the bundles.
-    .then(({ stdout }) => {
-      oldNumberOfChunks = stdout.split(chunkRegExp).length;
-    })
     // Add a lazy module.
     .then(() => ng('generate', 'module', 'lazy', '--routing'))
     // Should trigger a rebuild with a new bundle.
@@ -65,8 +60,7 @@ export default function() {
     // Count the bundles.
     .then((results) => {
       const stdout = results[0].stdout;
-      let newNumberOfChunks = stdout.split(chunkRegExp).length;
-      if (oldNumberOfChunks >= newNumberOfChunks) {
+      if (!lazyChunkRegExp.test(stdout)) {
         throw new Error('Expected webpack to create a new chunk, but did not.');
       }
     })
