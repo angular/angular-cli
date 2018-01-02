73d84108c3584631c3177e7fab7fb9267f36f3f0
Filipe Silva <filipematossilva@gmail.com>
2018-01-02 16:49:27 +0000

    ci: fix Invalid string length for process buffer on circleci

--- tests/e2e/setup/100-global-cli.ts
+++ tests/e2e/setup/100-global-cli.ts
@@ -13,7 +13,9 @@ export default function () {
       }
 
       // Install global Angular CLI.
-      return silentNpm('install', '-g', packages['@angular/cli'].tar);
+      // --unsafe-perm is needed for circleci
+      // because of https://github.com/sass/node-sass/issues/2006
+      return silentNpm('install', '-g', packages['@angular/cli'].tar, '--unsafe-perm');
     })
     .then(() => exec(process.platform.startsWith('win') ? 'where' : 'which', 'ng'));
 }
