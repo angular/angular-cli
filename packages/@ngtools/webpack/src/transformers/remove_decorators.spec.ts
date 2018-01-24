55bc89a56627bf2a210972c0a685a36efc3f5651
Charles Lyding <19598772+clydin@users.noreply.github.com>
2018-01-24 12:09:19 -0500

    test: add custom decorator class removal test

--- packages/@ngtools/webpack/src/transformers/remove_decorators.spec.ts
+++ packages/@ngtools/webpack/src/transformers/remove_decorators.spec.ts
@@ -124,6 +124,46 @@ describe('@ngtools/webpack transformers', () => {
       expect(oneLine`${result}`).toEqual(oneLine`${output}`);
     });
 
+    it('should keep other decorators on class declaration', () => {
+      const input = stripIndent`
+        import { Component } from '@angular/core';
+        import { AnotherDecorator } from 'another-lib';
+
+        @AnotherDecorator()
+        @Component({
+          selector: 'app-root',
+          templateUrl: './app.component.html',
+          styleUrls: ['./app.component.css']
+        })
+        export class AppComponent {
+          title = 'app';
+        }
+      `;
+      const output = stripIndent`
+        import * as tslib_1 from "tslib";
+        import { AnotherDecorator } from 'another-lib';
+
+        let AppComponent = class AppComponent {
+          constructor() {
+              this.title = 'app';
+          }
+        };
+        AppComponent = tslib_1.__decorate([
+          AnotherDecorator()
+        ], AppComponent);
+        export { AppComponent };
+      `;
+
+      const { program, compilerHost } = createTypescriptContext(input);
+      const transformer = removeDecorators(
+        () => true,
+        () => program.getTypeChecker(),
+      );
+      const result = transformTypescript(undefined, [transformer], program, compilerHost);
+
+      expect(oneLine`${result}`).toEqual(oneLine`${output}`);
+    });
+
     it('should remove imports for identifiers within the decorator', () => {
       const input = stripIndent`
         import { Component } from '@angular/core';
