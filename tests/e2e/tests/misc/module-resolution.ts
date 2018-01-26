acff2727d7953a2718936754a83e28ff0e59b9ec
Charles Lyding <19598772+clydin@users.noreply.github.com>
2018-01-26 13:45:17 -0500

    fix(@ngtools/webpack): ensure webpack handles module resolution

--- tests/e2e/tests/misc/module-resolution.ts
+++ tests/e2e/tests/misc/module-resolution.ts
@@ -1,13 +1,49 @@
 import { appendToFile, prependToFile } from '../../utils/fs';
 import { ng, silentNpm } from '../../utils/process';
+import { updateJsonFile } from '../../utils/project';
+import { expectToFail } from '../../utils/utils';
 
 
 export default async function () {
-  await silentNpm('install', 'firebase@3.7.8');
-
   await prependToFile('src/app/app.module.ts', 'import * as firebase from \'firebase\';');
   await appendToFile('src/app/app.module.ts', 'firebase.initializeApp({});');
 
-  await ng('build');
+  await silentNpm('install', 'firebase@3.7.8');
   await ng('build', '--aot');
+  await ng('test', '--single-run');
+
+  await silentNpm('install', 'firebase@4.9.0');
+  await ng('build', '--aot');
+  await ng('test', '--single-run');
+
+  // await prependToFile('src/app/app.module.ts', 'import * as firebase from \'firebase\';');
+  // await appendToFile('src/app/app.module.ts', 'firebase.initializeApp({});');
+  // await ng('build');
+
+  await updateJsonFile('src/tsconfig.app.json', tsconfig => {
+    tsconfig.compilerOptions.paths = {};
+  });
+  await ng('build');
+
+  await updateJsonFile('src/tsconfig.app.json', tsconfig => {
+    tsconfig.compilerOptions.paths = {
+      '@app/*': ['*'],
+      '@lib/*/test': ['*/test'],
+    };
+  });
+  await ng('build');
+
+  await updateJsonFile('src/tsconfig.app.json', tsconfig => {
+    tsconfig.compilerOptions.paths = {
+      '@firebase/polyfill': ['@firebase/polyfill/index.ts'],
+    };
+  });
+  await expectToFail(() => ng('build'));
+
+  await updateJsonFile('src/tsconfig.app.json', tsconfig => {
+    tsconfig.compilerOptions.paths = {
+      '@firebase/polyfill*': ['@firebase/polyfill/index.ts'],
+    };
+  });
+  await expectToFail(() => ng('build'));
 }
