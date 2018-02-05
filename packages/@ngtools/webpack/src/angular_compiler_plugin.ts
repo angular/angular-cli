b32a59e78e52f4b70d03760b3f529cbcc206c26b
Charles Lyding <19598772+clydin@users.noreply.github.com>
2018-02-05 11:25:16 -0500

    fix(@ngtools/webpack): normalize lazy route names and paths

--- packages/@ngtools/webpack/src/angular_compiler_plugin.ts
+++ packages/@ngtools/webpack/src/angular_compiler_plugin.ts
@@ -450,19 +450,20 @@ export class AngularCompilerPlugin implements Tapable {
       .forEach(lazyRouteKey => {
         const [lazyRouteModule, moduleName] = lazyRouteKey.split('#');
 
-        if (!lazyRouteModule || !moduleName) {
+        if (!lazyRouteModule) {
           return;
         }
 
-        const lazyRouteTSFile = discoveredLazyRoutes[lazyRouteKey];
+        const lazyRouteTSFile = discoveredLazyRoutes[lazyRouteKey].replace(/\\/g, '/');
         let modulePath: string, moduleKey: string;
 
         if (this._JitMode) {
           modulePath = lazyRouteTSFile;
-          moduleKey = lazyRouteKey;
+          moduleKey = `${lazyRouteModule}${moduleName ? '#' + moduleName : ''}`;
         } else {
           modulePath = lazyRouteTSFile.replace(/(\.d)?\.ts$/, `.ngfactory.js`);
-          moduleKey = `${lazyRouteModule}.ngfactory#${moduleName}NgFactory`;
+          const factoryModuleName = moduleName ? `#${moduleName}NgFactory` : '';
+          moduleKey = `${lazyRouteModule}.ngfactory${factoryModuleName}`;
         }
 
         if (moduleKey in this._lazyRoutes) {
