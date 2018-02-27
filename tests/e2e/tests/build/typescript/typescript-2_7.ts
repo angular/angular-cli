4a4189035329442c8f3fa104ca8803525d41ccdc
Charles Lyding <19598772+clydin@users.noreply.github.com>
2018-02-27 11:30:15 -0500

    test: disable strict TS version check for TS2.7 test

--- tests/e2e/tests/build/typescript/typescript-2_7.ts
+++ tests/e2e/tests/build/typescript/typescript-2_7.ts
@@ -1,6 +1,15 @@
 import { ng, npm } from '../../../utils/process';
+import { updateJsonFile } from '../../../utils/project';
 
 export default async function () {
+  // Disable the strict TS version check for nightly
+  await updateJsonFile('src/tsconfig.app.json', configJson => {
+    configJson.angularCompilerOptions = {
+      ...configJson.angularCompilerOptions,
+      disableTypeScriptVersionCheck: true,
+    };
+  });
+
   await npm('install', 'typescript@2.7');
   await ng('build');
   await ng('build', '--prod');
