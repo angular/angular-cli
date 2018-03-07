90f76a7133cc72e6295295c614f83c25513fc8ec
Mike Brocchi <mbrocchi@gmail.com>
2018-03-07 16:27:44 -0500

    Update e2e command to use architect

--- packages/@angular/cli/commands/e2e.ts
+++ packages/@angular/cli/commands/e2e.ts
@@ -1,31 +1,36 @@
-import { Command, CommandScope, Option } from '../models/command';
-import { ServeTaskOptions } from './serve';
+import { CommandScope, Option } from '../models/command';
+import { ArchitectCommand } from '../models/architect-command';
 
-export interface E2eTaskOptions extends ServeTaskOptions {
-  config: string;
-  serve: boolean;
-  webdriverUpdate: boolean;
-  specs: string[];
-  suite: string;
-  elementExplorer: boolean;
+export interface RunOptions {
+  app?: string;
+  configuration?: string;
+  prod: boolean;
 }
 
-export default class E2eCommand extends Command {
+export default class E2eCommand extends ArchitectCommand {
   public readonly name = 'e2e';
+  public readonly target = 'protractor';
   public readonly description = 'Run e2e tests in existing project.';
   public static aliases: string[] = ['e'];
   public readonly scope = CommandScope.inProject;
-  public readonly arguments: string[] = [];
-  public readonly options: Option[] = [];
+  public readonly arguments: string[] = ['app'];
+  public readonly options: Option[] = [
+    this.prodOption,
+    this.configurationOption
+  ];
 
-  public run(options: E2eTaskOptions) {
-    const E2eTask = require('../tasks/e2e').E2eTask;
-
-    const e2eTask = new E2eTask({
-      ui: this.ui,
-      project: this.project
+  public run(options: RunOptions) {
+    let configuration = options.configuration;
+    if (!configuration && options.prod) {
+      configuration = 'production';
+    }
+    const overrides = {...options};
+    delete overrides.app;
+    delete overrides.prod;
+    return this.runArchitect({
+      app: options.app,
+      configuration,
+      overrides
     });
-
-    return e2eTask.run(options);
   }
 }
