ed7249bc526e5b7e6ae7357ff69e9164c01b0d82
Mike Brocchi <mbrocchi@gmail.com>
2018-03-08 10:38:05 -0500

    Update serve command to use architect

--- packages/@angular/cli/commands/serve.ts
+++ packages/@angular/cli/commands/serve.ts
@@ -1,51 +1,47 @@
-import { Command, CommandScope } from '../models/command';
-import { BuildOptions } from '../models/build-options';
+import { CommandScope, Option } from '../models/command';
 import { Version } from '../upgrade/version';
-import { ServeTaskOptions } from './serve';
-
-// const Command = require('../ember-cli/lib/models/command');
-
-export interface ServeTaskOptions extends BuildOptions {
-  port?: number;
-  host?: string;
-  proxyConfig?: string;
-  liveReload?: boolean;
-  publicHost?: string;
-  disableHostCheck?: boolean;
-  ssl?: boolean;
-  sslKey?: string;
-  sslCert?: string;
-  open?: boolean;
-  hmr?: boolean;
-  servePath?: string;
-}
+import { ArchitectCommand } from '../models/architect-command';
 
 // Expose options unrelated to live-reload to other commands that need to run serve
 export const baseServeCommandOptions: any = [];
 
-export default class ServeCommand extends Command {
+export interface Options {
+  app?: string;
+  configuration?: string;
+  prod: boolean;
+}
+
+export default class ServeCommand extends ArchitectCommand {
   public readonly name = 'serve';
+  public readonly target = 'dev-server';
   public readonly description = 'Builds and serves your app, rebuilding on file changes.';
   public static aliases = ['server', 's'];
   public readonly scope = CommandScope.inProject;
   public readonly arguments: string[] = [];
-  public readonly options = baseServeCommandOptions;
+  public readonly options: Option[] = [
+    this.prodOption,
+    this.configurationOption
+  ];
 
-  public validate(_options: ServeTaskOptions) {
+  public validate(_options: Options) {
     // Check Angular and TypeScript versions.
     Version.assertAngularVersionIs2_3_1OrHigher(this.project.root);
     Version.assertTypescriptVersion(this.project.root);
     return true;
   }
 
-  public async run(options: ServeTaskOptions) {
-    const ServeTask = require('../tasks/serve').default;
-
-    const serve = new ServeTask({
-      ui: this.ui,
-      project: this.project,
+  public async run(options: Options) {
+    let configuration = options.configuration;
+    if (options.prod) {
+      configuration = 'production';
+    }
+    const overrides = {...options};
+    delete overrides.app;
+    delete overrides.prod;
+    return this.runArchitect({
+      app: options.app,
+      configuration,
+      overrides
     });
-
-    return await serve.run(options);
   }
 }
