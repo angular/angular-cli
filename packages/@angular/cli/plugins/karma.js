916731c88f62f9986d75f73edbea6b16777042a7
Filipe Silva <filipematossilva@gmail.com>
2018-03-20 23:30:18 +0000

    fix(@angular/cli): show warning when old karma plugin is imported

---
+++ packages/@angular/cli/plugins/karma.js
@@ -0,0 +1,4 @@
+throw new Error(
+  'In Angular CLI >6.0 the Karma plugin is now exported by "@angular-devkit/build-webpack" instead.\n'
+  + 'Please replace "@angular/cli" with "@angular-devkit/build-webpack" in your "karma.conf.js" file.'
+);
