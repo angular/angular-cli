33af719bad1d34cca814f59ab3c0796204af9c39
Filipe Silva <filipematossilva@gmail.com>
2018-03-21 19:12:24 +0000

    fix(@schematics/angular): remove baseUrl from project tsconfigs
    In a multiple project environment, the workspace tsconfig should be used for path mapping and tooling. Having a baseUrl in the project tsconfigs would lead to either tooling or builds to be broken.

--- packages/angular_devkit/build_webpack/test/browser/tsconfig-paths_spec_large.ts
+++ packages/angular_devkit/build_webpack/test/browser/tsconfig-paths_spec_large.ts
@@ -15,18 +15,12 @@ describe('Browser Builder tsconfig paths', () => {
   afterEach(done => host.restore().subscribe(undefined, done.fail, done));
 
   it('works', (done) => {
-    host.writeMultipleFiles({
-      'src/meaning-too.ts': 'export var meaning = 42;',
-      'src/app/shared/meaning.ts': 'export var meaning = 42;',
-      'src/app/shared/index.ts': `export * from './meaning'`,
-    });
-
     host.replaceInFile('src/app/app.module.ts', './app.component', '@root/app/app.component');
-    host.replaceInFile('src/tsconfig.app.json', /"baseUrl": "[^"]*",/, `
+    host.replaceInFile('tsconfig.json', /"baseUrl": ".\/",/, `
       "baseUrl": "./",
       "paths": {
         "@root/*": [
-          "./*"
+          "./src/*"
         ]
       },
     `);
@@ -42,27 +36,27 @@ describe('Browser Builder tsconfig paths', () => {
       'src/app/shared/meaning.ts': 'export var meaning = 42;',
       'src/app/shared/index.ts': `export * from './meaning'`,
     });
-    host.replaceInFile('src/tsconfig.app.json', /"baseUrl": "[^"]*",/, `
+    host.replaceInFile('tsconfig.json', /"baseUrl": ".\/",/, `
       "baseUrl": "./",
       "paths": {
         "@shared": [
-          "app/shared"
+          "src/app/shared"
         ],
         "@shared/*": [
-          "app/shared/*"
+          "src/app/shared/*"
         ],
         "*": [
           "*",
-          "app/shared/*"
+          "src/app/shared/*"
         ]
       },
     `);
     host.appendToFile('src/app/app.component.ts', `
-      import { meaning } from 'app/shared/meaning';
+      import { meaning } from 'src/app/shared/meaning';
       import { meaning as meaning2 } from '@shared';
       import { meaning as meaning3 } from '@shared/meaning';
       import { meaning as meaning4 } from 'meaning';
-      import { meaning as meaning5 } from 'meaning-too';
+      import { meaning as meaning5 } from 'src/meaning-too';
 
       // need to use imports otherwise they are ignored and
       // no error is outputted, even if baseUrl/paths don't work
