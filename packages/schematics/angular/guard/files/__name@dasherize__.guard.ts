4f6c73e33cd4572985672470856c3d988d76ffc3
Charles Lyding <19598772+clydin@users.noreply.github.com>
2018-03-21 15:59:50 -0400

    feat(@schematics/angular): update guard for rxjs 6

--- packages/schematics/angular/guard/files/__name@dasherize__.guard.ts
+++ packages/schematics/angular/guard/files/__name@dasherize__.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class <%= classify(name) %>Guard implements CanActivate {
