eb653d685155ff7af308b838327642ee13a2c265
Hans <hans@hansl.ca>
2018-03-23 12:05:09 -0700

    fix(@schematics/angular): fix performance
    By using a new empty tree instead of moving on all files. Also,
    we should optimize actions before merging to avoid weird side effects.

--- packages/schematics/angular/ng-new/index.ts
+++ packages/schematics/angular/ng-new/index.ts
@@ -10,7 +10,10 @@ import {
   SchematicContext,
   SchematicsException,
   Tree,
+  apply,
   chain,
+  empty,
+  mergeWith,
   move,
   schematic,
 } from '@angular-devkit/schematics';
@@ -25,53 +28,58 @@ import { Schema as NgNewOptions } from './schema';
 
 
 export default function (options: NgNewOptions): Rule {
-  return (host: Tree, context: SchematicContext) => {
-    if (!options.name) {
-      throw new SchematicsException(`Invalid options, "name" is required.`);
-    }
+  if (!options.name) {
+    throw new SchematicsException(`Invalid options, "name" is required.`);
+  }
 
-    if (!options.directory) {
-      options.directory = options.name;
-    }
-    let packageTask;
-    if (!options.skipInstall) {
-      packageTask = context.addTask(new NodePackageInstallTask(options.directory));
-      if (options.linkCli) {
-        packageTask = context.addTask(
-          new NodePackageLinkTask('@angular/cli', options.directory),
-          [packageTask],
+  if (!options.directory) {
+    options.directory = options.name;
+  }
+
+  const workspaceOptions: WorkspaceOptions = {
+    name: options.name,
+    version: options.version,
+    newProjectRoot: options.newProjectRoot || 'projects',
+  };
+  const applicationOptions: ApplicationOptions = {
+    name: options.name,
+    inlineStyle: options.inlineStyle,
+    inlineTemplate: options.inlineTemplate,
+    viewEncapsulation: options.viewEncapsulation,
+    routing: options.routing,
+    style: options.style,
+    skipTests: options.skipTests,
+  };
+
+  return chain([
+    (host: Tree, context: SchematicContext) => {
+      let packageTask;
+      if (!options.skipInstall) {
+        packageTask = context.addTask(new NodePackageInstallTask(options.directory));
+        if (options.linkCli) {
+          packageTask = context.addTask(
+            new NodePackageLinkTask('@angular/cli', options.directory),
+            [packageTask],
+          );
+        }
+      }
+      if (!options.skipGit) {
+        context.addTask(
+          new RepositoryInitializerTask(
+            options.directory,
+            options.commit,
+          ),
+          packageTask ? [packageTask] : [],
         );
       }
-    }
-    if (!options.skipGit) {
-      context.addTask(
-        new RepositoryInitializerTask(
-          options.directory,
-          options.commit,
-        ),
-        packageTask ? [packageTask] : [],
-      );
-    }
-
-    const workspaceOptions: WorkspaceOptions = {
-      name: options.name,
-      version: options.version,
-      newProjectRoot: options.newProjectRoot || 'projects',
-    };
-    const applicationOptions: ApplicationOptions = {
-      name: options.name,
-      inlineStyle: options.inlineStyle,
-      inlineTemplate: options.inlineTemplate,
-      viewEncapsulation: options.viewEncapsulation,
-      routing: options.routing,
-      style: options.style,
-      skipTests: options.skipTests,
-    };
-
-    return chain([
-      schematic('workspace', workspaceOptions),
-      schematic('application', applicationOptions),
-      move(options.directory || options.name),
-    ])(host, context);
-  };
+    },
+    mergeWith(
+      apply(empty(), [
+        schematic('workspace', workspaceOptions),
+        schematic('application', applicationOptions),
+        move(options.directory || options.name),
+        tree => Tree.optimize(tree),
+      ]),
+    ),
+  ]);
 }
