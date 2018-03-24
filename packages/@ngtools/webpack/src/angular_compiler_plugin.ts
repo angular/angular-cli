27eaf2b9f9db2e71068e0801875dfa43b51630ae
patrikx3 <alabard@gmail.com>
2018-03-24 20:47:18 +0100

    feat(@ngtools/webpack): made it possible to skip removal of decorators in aot mode

--- packages/@ngtools/webpack/src/angular_compiler_plugin.ts
+++ packages/@ngtools/webpack/src/angular_compiler_plugin.ts
@@ -59,6 +59,7 @@ export interface AngularCompilerPluginOptions {
   entryModule?: string;
   mainPath?: string;
   skipCodeGeneration?: boolean;
+  skipRemoveDecorators?: boolean;
   hostReplacementPaths?: { [path: string]: string };
   forkTypeChecker?: boolean;
   // TODO: remove singleFileIncludes for 2.0, this is just to support old projects that did not
@@ -712,7 +713,9 @@ export class AngularCompilerPlugin {
       this._transformers.push(replaceResources(isAppPath));
     } else {
       // Remove unneeded angular decorators.
-      this._transformers.push(removeDecorators(isAppPath, getTypeChecker));
+      if(!this._options.skipRemoveDecorators) {
+        this._transformers.push(removeDecorators(isAppPath, getTypeChecker));
+      }
     }
 
     if (this._platform === PLATFORM.Browser) {
