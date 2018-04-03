e597cf6cc859ba12935671f50193713f41e103e7
Filipe Silva <filipematossilva@gmail.com>
2018-04-03 09:43:48 +0100

    fix(@schematics/angular): update new project Angular version

--- packages/schematics/angular/utility/latest-versions.ts
+++ packages/schematics/angular/utility/latest-versions.ts
@@ -7,10 +7,12 @@
  */
 
 export const latestVersions = {
-  Angular: '^5.2.9',
-  RxJs: '^5.5.8',
-  ZoneJs: '^0.8.20',
-  TypeScript: '~2.6.2',
+  // These versions should be kept up to date with latest Angular peer dependencies.
+  Angular: '^6.0.0-rc.1',
+  RxJs: '^6.0.0-rc.0',
+  ZoneJs: '^0.8.24',
+  TypeScript: '~2.7.2',
+  // The versions below must be manually updated when making a new devkit release.
   DevkitBuildWebpack: '~0.0.9',
   DevkitBuildNgPackagr: '~0.0.3',
 };
