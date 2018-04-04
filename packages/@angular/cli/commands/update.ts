f6f774a3b0e3b2baa0be4de8ee79fff010ca1814
Mike Brocchi <mbrocchi@gmail.com>
2018-04-04 12:24:46 -0400

    fix(@angular/cli): Fix update command help options
    fixes #10150

--- packages/@angular/cli/commands/update.ts
+++ packages/@angular/cli/commands/update.ts
@@ -13,18 +13,34 @@ export default class UpdateCommand extends SchematicCommand {
   public static aliases: string[] = [];
   public readonly scope = CommandScope.inProject;
   public readonly arguments: string[] = [ 'packages' ];
-  public readonly options: Option[] = [
+  public options: Option[] = [
     ...this.coreOptions,
   ];
   public readonly allowMissingWorkspace = true;
 
+  private collectionName = '@schematics/update';
+  private schematicName = 'update';
+
+  private initialized = false;
+  public async initialize(options: any) {
+    if (this.initialized) {
+      return;
+    }
+    super.initialize(options);
+    this.initialized = true;
+
+    const availableOptions: Option[] = await this.getOptions({
+      schematicName: this.schematicName,
+      collectionName: this.collectionName,
+    });
+    this.options = this.options.concat( availableOptions || []);
+  }
+
   public async run(options: UpdateOptions) {
-    const collectionName = '@schematics/update';
-    const schematicName = 'update';
 
     const schematicRunOptions = {
-      collectionName,
-      schematicName,
+      collectionName: this.collectionName,
+      schematicName: this.schematicName,
       schematicOptions: options,
       dryRun: options.dryRun,
       force: options.force,
