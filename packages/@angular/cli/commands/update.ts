7c7c350d4ef500f0a2b2a00a34ee32c140b173dd
Hans Larsen <hans@hansl.ca>
2018-04-23 15:25:21 -0700

    fix(@angular/cli): if using ng update @angular/cli only, add migrate and from
    Fix #10439

--- packages/@angular/cli/commands/update.ts
+++ packages/@angular/cli/commands/update.ts
@@ -39,10 +39,18 @@ export default class UpdateCommand extends SchematicCommand {
   }
 
   public async run(options: UpdateOptions) {
+    const schematicOptions: any = { ...options };
+    if (schematicOptions._[0] == '@angular/cli'
+        && !schematicOptions.migrateOnly
+        && !schematicOptions.from) {
+      schematicOptions.migrateOnly = true;
+      schematicOptions.from = '1.0.0';
+    }
+
     return this.runSchematic({
       collectionName: this.collectionName,
       schematicName: this.schematicName,
-      schematicOptions: options,
+      schematicOptions,
       dryRun: options.dryRun,
       force: false,
       showNothingDone: false,
