c827938d313b6ad2e8a28092acd11bc16dd8fb1e
Hans Larsen <hans@hansl.ca>
2018-04-27 14:52:40 -0700

    ci: update hoek version or packages in between to latest.
    Fixes angular/angular-cli#10480

--- packages/angular_devkit/build_angular/test/utils/request.ts
+++ packages/angular_devkit/build_angular/test/utils/request.ts
@@ -5,25 +5,50 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import { IncomingMessage } from 'http';
-import * as _request from 'request';
+import * as http from 'http';
+import * as https from 'https';
+import * as Url from 'url';
 
 export function request(url: string, headers = {}): Promise<string> {
   return new Promise((resolve, reject) => {
-    const options = {
-      url: url,
+    const u = Url.parse(url);
+    const options: http.RequestOptions = {
+      hostname: u.hostname,
+      protocol: u.protocol,
+      host: u.host,
+      port: u.port,
+      path: u.path,
       headers: { 'Accept': 'text/html', ...headers },
-      agentOptions: { rejectUnauthorized: false },
     };
-    // tslint:disable-next-line:no-any
-    _request(options, (error: any, response: IncomingMessage, body: string) => {
-      if (error) {
-        reject(error);
-      } else if (response.statusCode && response.statusCode >= 400) {
-        reject(new Error(`Requesting "${url}" returned status code ${response.statusCode}.`));
+
+    function _callback(res: http.IncomingMessage) {
+      if (!res.statusCode || res.statusCode >= 400) {
+        // Consume the rest of the data to free memory.
+        res.resume();
+        reject(new Error(`Requesting "${url}" returned status code ${res.statusCode}.`));
       } else {
-        resolve(body);
+        res.setEncoding('utf8');
+        let data = '';
+        res.on('data', chunk => {
+          data += chunk;
+        });
+        res.on('end', () => {
+          try {
+            resolve(data);
+          } catch (err) {
+            reject(err);
+          }
+        });
       }
-    });
+    }
+
+    if (u.protocol == 'https:') {
+      options.agent = new https.Agent({ rejectUnauthorized: false });
+      https.get(options, _callback);
+    } else if (u.protocol == 'http:') {
+      http.get(options, _callback);
+    } else {
+      throw new Error(`Unknown protocol: ${JSON.stringify(u.protocol)}.`);
+    }
   });
 }
