762543b2ddd04f2e70f940e71b1622c8567a3026
Fabian Wiles <fabianwiles@live.com>
2018-04-28 14:55:38 +1200

    build: remove old build system (#971)

--- tools/package-tools/index.ts
+++
@@ -1,7 +0,0 @@
-// Expose general package utilities.
-export * from './build-config';
-export * from './build-bundles';
-export * from './build-release';
-export * from './build-package';
-export * from './copy-files';
-export * from './packages';
--- tools/tslint-rules/tsLoaderRule.js
+++ tools/tslint-rules/tsLoaderRule.js
@@ -4,7 +4,7 @@ const Lint = require('tslint');
 // Custom rule that registers all of the custom rules, written in TypeScript, with ts-node.
 // This is necessary, because `tslint` and IDEs won't execute any rules that aren't in a .js file.
 require('ts-node').register({
-  project: path.join(__dirname, '../tsconfig.json')
+  project: path.join(__dirname, './tsconfig.json')
 });
 
 // Add a noop rule so tslint doesn't complain.
