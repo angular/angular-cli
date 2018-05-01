3cf1f11958946afcb5564a706f3cfbd81ebb5f85
Filipe Silva <filipematossilva@gmail.com>
2018-05-01 15:20:14 -0700

    test: add more checks to 1.7 update test

--- tests/e2e/tests/basic/update-1.7-longhand.ts
+++ tests/e2e/tests/basic/update-1.7-longhand.ts
@@ -7,6 +7,6 @@ export default function () {
   return Promise.resolve()
     .then(() => createProjectFromAsset('1.7-project'))
     .then(() => expectToFail(() => ng('build')))
-    .then(() => ng('update', '@angular/cli'))
+    .then(() => ng('update', '@angular/cli', '--migrate-only', '--from=1.7.1'))
     .then(() => ng('build'));
 }
