62232e880308175d3c4878136794d3eb4089bcde
Hans Larsen <hans@hansl.ca>
2018-05-01 15:34:50 -0700

    test: add new tests for ng add

--- tests/e2e/tests/commands/add/add-version.ts
+++ tests/e2e/tests/commands/add/add-version.ts
@@ -3,8 +3,8 @@ import { ng } from '../../../utils/process';
 
 
 export default async function () {
-  await ng('add', '@angular-devkit-tests/ng-add-simple');
-  await expectFileToMatch('package.json', /@angular-devkit-tests\/ng-add-simple/);
+  await ng('add', '@angular-devkit-tests/ng-add-simple@^1.0.0');
+  await expectFileToMatch('package.json', /\/ng-add-simple.*\^1\.0\.0/);
   await expectFileToExist('ng-add-test');
   await rimraf('node_modules/@angular-devkit-tests/ng-add-simple');
 }
