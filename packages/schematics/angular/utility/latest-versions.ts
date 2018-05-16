2abbdba6048059ad5ceddebfe2e734d15b46194a
Mike Brocchi <mbrocchi@gmail.com>
2018-05-16 18:04:14 -0400

    release: v6.0.2

--- packages/schematics/angular/utility/latest-versions.ts
+++ packages/schematics/angular/utility/latest-versions.ts
@@ -8,11 +8,11 @@
 
 export const latestVersions = {
   // These versions should be kept up to date with latest Angular peer dependencies.
-  Angular: '^6.0.0',
+  Angular: '^6.0.2',
   RxJs: '^6.0.0',
   ZoneJs: '^0.8.26',
   TypeScript: '~2.7.2',
   // The versions below must be manually updated when making a new devkit release.
-  DevkitBuildAngular: '~0.6.1',
-  DevkitBuildNgPackagr: '~0.6.1',
+  DevkitBuildAngular: '~0.6.2',
+  DevkitBuildNgPackagr: '~0.6.2',
 };
