6e0c321492e0e65f0616dbc0861d9e48f6ce15e6
Mike Brocchi <mbrocchi@gmail.com>
2018-05-16 22:26:49 -0400

    fix(@schematics/angular): Fix broken unit test on new app

--- packages/schematics/angular/application/other-files/app.component.spec.ts
+++ packages/schematics/angular/application/other-files/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to <%= prefix %>!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to <%= name %>!');
   }));
 });
