beafb8a5bfad054bee0c8c3efd6b54971073b2c0
kevin schuchard <schuchard.kevin@gmail.com>
2018-06-19 15:59:06 -0600

    feat(@schematics/angular): get npm package version
    Add method to get a npm packages latest registry version and provide a default value of "latest" if any errors occur.

--- packages/schematics/angular/utility/dependencies.ts
+++ packages/schematics/angular/utility/dependencies.ts
@@ -12,7 +12,7 @@ import {
   findPropertyInAstObject,
   insertPropertyInAstObjectInOrder,
  } from './json-utils';
-
+import { get } from 'http';
 
 const pkgJsonPath = '/package.json';
 export enum NodeDependencyType {
@@ -27,6 +27,11 @@ export interface NodeDependency {
   name: string;
   version: string;
   overwrite?: boolean;
+}
+
+export interface NodePackage {
+  name: string;
+  version: string;
 }
 
 export function addPackageJsonDependency(tree: Tree, dependency: NodeDependency): void {
@@ -104,4 +109,29 @@ function _readPackageJson(tree: Tree): JsonAstObject {
   }
 
   return packageJson;
+}
+
+export function getLatestNodeVersion(packageName: string): Promise<NodePackage> {
+  const DEFAULT_VERSION = 'latest';
+
+  return new Promise((resolve) => {
+    return get(`http://registry.npmjs.org/${packageName}`, (res) => {
+      let rawData = '';
+      res.on('data', (chunk) => (rawData += chunk));
+      res.on('end', () => {
+        try {
+          const response = JSON.parse(rawData);
+          const version = response && response['dist-tags'] || {};
+
+          resolve(buildPackage(packageName, version.latest));
+        } catch (e) {
+          resolve(buildPackage(packageName));
+        }
+      });
+    }).on('error', () => resolve(buildPackage(packageName)));
+  });
+
+  function buildPackage(name: string, version: string = DEFAULT_VERSION): NodePackage {
+    return { name, version };
+  }
 }
