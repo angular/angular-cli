5e58cc48657edc96b3a36bb02bfdcc1b61d7c963
Olaf Luijks <oluijks@protonmail.com>
2018-06-23 15:32:54 +0200

    feat(@schematics/angular): search in specific doc version
    Search in specific doc version: [v4, v5, next]
    Add option:
    `--search-doc-version (-v)`
    `  Search in specific doc version: [v4, v5, next]`

--- packages/angular/cli/commands/doc.ts
+++ packages/angular/cli/commands/doc.ts
@@ -12,13 +12,22 @@ const opn = require('opn');
 export interface Options {
   keyword: string;
   search?: boolean;
+  searchDocVersion?: boolean;
+  version: string;
+}
+
+enum DocumentVersions {
+  v4 = 'v4',
+  v5 = 'v5',
+  next = 'next',
 }
 
 export default class DocCommand extends Command {
   public readonly name = 'doc';
-  public readonly description = 'Opens the official Angular API documentation for a given keyword.';
+  public readonly description =
+    'Opens the official Angular API documentation for a given keyword.';
   public static aliases = ['d'];
-  public readonly arguments = ['keyword'];
+  public readonly arguments = ['keyword', 'version'];
   public readonly options = [
     {
       name: 'search',
@@ -26,6 +35,13 @@ export default class DocCommand extends Command {
       type: Boolean,
       default: false,
       description: 'Search whole angular.io instead of just api.',
+    },
+    {
+      name: 'search-doc-version',
+      aliases: ['v'],
+      type: Boolean,
+      default: false,
+      description: 'Search in specific doc version: [v4, v5, next].',
     },
   ];
 
@@ -40,9 +56,30 @@ export default class DocCommand extends Command {
   }
 
   public async run(options: Options) {
-    let searchUrl = `https://angular.io/api?query=${options.keyword}`;
+    let version = '';
+    let searchUrl = '';
+
+    if (options.searchDocVersion) {
+      switch (options.version) {
+        case 'v4':
+          version = DocumentVersions.v4;
+          break;
+        case 'v5':
+          version = DocumentVersions.v5;
+          break;
+        case 'next':
+          version = DocumentVersions.next;
+          break;
+      }
+      searchUrl = `https://${version}.angular.io/api?query=${options.keyword}`;
+    } else {
+      searchUrl = `https://angular.io/api?query=${options.keyword}`;
+    }
+
     if (options.search) {
-      searchUrl = `https://www.google.com/search?q=site%3Aangular.io+${options.keyword}`;
+      searchUrl = `https://www.google.com/search?q=site%3Aangular.io+${
+        options.keyword
+      }`;
     }
 
     return opn(searchUrl);
