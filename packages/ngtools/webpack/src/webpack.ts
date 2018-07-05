2761d5017e1e51a380d45ed5fc2d96791a7a843e
Charles Lyding <19598772+clydin@users.noreply.github.com>
2018-07-05 11:32:20 -0400

    refactor(@ngtools/webpack): cleanup webpack types and use tapPromise

--- packages/ngtools/webpack/src/webpack.ts
+++ packages/ngtools/webpack/src/webpack.ts
@@ -9,38 +9,9 @@ import { Stats } from 'fs';
 
 // Declarations for (some) Webpack types. Only what's needed.
 
-export interface Request {
-  request?: Request;
-  relativePath: string;
-}
-
-export interface Callback<T> {
+// tslint:disable-next-line:no-any
+export interface Callback<T = any> {
   (err?: Error | null, result?: T): void;
-}
-
-export interface ResolverCallback {
-  (request: Request, callback: Callback<void>): void;
-}
-
-export interface Tapable {
-  apply(plugin: ResolverPlugin): void;
-}
-
-export interface ResolverPlugin extends Tapable {
-  plugin(source: string, cb: ResolverCallback): void;
-  // tslint:disable-next-line:no-any
-  doResolve(target: string, req: Request, desc: string, callback: Callback<any>): void;
-  join(relativePath: string, innerRequest: Request): Request;
-}
-
-export interface LoaderCallback {
-  (err: Error | null, source?: string, sourceMap?: string): void;
-}
-
-export interface NormalModuleFactory {
-  plugin(event: string,
-         // tslint:disable-next-line:no-any
-         callback: (data: NormalModuleFactoryRequest, callback: Callback<any>) => void): any;
 }
 
 export interface NormalModuleFactoryRequest {
@@ -52,8 +23,7 @@ export interface InputFileSystem {
   stat(path: string, callback: Callback<Stats>): void;
   readdir(path: string, callback: Callback<string[]>): void;
   readFile(path: string, callback: Callback<string | Buffer>): void;
-  // tslint:disable-next-line:no-any
-  readJson(path: string, callback: Callback<any>): void;
+  readJson(path: string, callback: Callback): void;
   readlink(path: string, callback: Callback<string>): void;
   statSync(path: string): Stats;
   readdirSync(path: string): string[];
