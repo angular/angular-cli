1188e35b278564889c85348b14fa056fdbcf5e2b
Charles Lyding <19598772+clydin@users.noreply.github.com>
2018-07-12 10:59:36 -0400

    refactor(@angular-devkit/build-angular): minor CSS processing cleanup

--- packages/angular_devkit/build_angular/src/angular-cli-files/plugins/raw-css-loader.ts
+++ packages/angular_devkit/build_angular/src/angular-cli-files/plugins/raw-css-loader.ts
@@ -8,7 +8,7 @@
 
 export default function(content: string, map: object) {
   const stringifiedContent = JSON.stringify(content);
-  const stringifiedMap = map && JSON.stringify(map);
+  const stringifiedMap = map ? JSON.stringify(map) : `''`;
 
   return `module.exports = [[module.id, ${stringifiedContent}, '', ${stringifiedMap}]]`;
 }
