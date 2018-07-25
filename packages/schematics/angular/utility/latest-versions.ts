ad5169376e9c954a40265cd37b8415bad9d2ae6f
Charles Lyding <19598772+clydin@users.noreply.github.com>
2018-07-25 11:58:43 -0400

    fix(@schematics/angular): adjust zonejs version in new projects for 6.1

--- packages/schematics/angular/utility/latest-versions.ts
+++ packages/schematics/angular/utility/latest-versions.ts
@@ -10,7 +10,7 @@ export const latestVersions = {
   // These versions should be kept up to date with latest Angular peer dependencies.
   Angular: '^6.1.0',
   RxJs: '^6.0.0',
-  ZoneJs: '^0.8.26',
+  ZoneJs: '~0.8.26',
   TypeScript: '~2.7.2',
   // The versions below must be manually updated when making a new devkit release.
   DevkitBuildAngular: '~0.7.0',
