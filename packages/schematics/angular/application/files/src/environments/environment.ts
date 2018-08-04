68d87e82d1696cb14132e1f5e55c121833774d34
Qais Patankar <qaisjp@gmail.com>
2018-08-04 13:54:13 +0100

    style(@schematics/angular): fix English in environment.ts

--- packages/schematics/angular/application/files/src/environments/environment.ts
+++ packages/schematics/angular/application/files/src/environments/environment.ts
@@ -7,9 +7,9 @@ export const environment = {
 };
 
 /*
- * In development mode, to ignore zone related error stack frames such as
- * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
- * import the following file, but please comment it out in production mode
- * because it will have performance impact when throw error
+ * In development mode, for easier debugging, you can ignore zone related error
+ * stack frames such as `zone.run`/`zoneDelegate.invokeTask` by importing the
+ * below file. Don't forget to comment it out in production mode
+ * because it will have a performance impact when errors are thrown
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
