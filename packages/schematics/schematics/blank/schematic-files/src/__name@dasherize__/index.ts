ed731b5f1211e874038ef26de6417ea6eb82d171
sis0k0 <stanimira.vlaeva@gmail.com>
2018-08-16 17:25:41 +0300

    fix(@schematics/schematics): prefix unused argument with underscore
    The `options` arguments is not used in the function. This causes the
    TypeScript compilation to fail.
    
    fixes #11916

--- packages/schematics/schematics/blank/schematic-files/src/__name@dasherize__/index.ts
+++ packages/schematics/schematics/blank/schematic-files/src/__name@dasherize__/index.ts
@@ -3,7 +3,7 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function <%= camelize(name) %>(options: any): Rule {
+export function <%= camelize(name) %>(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
     return tree;
   };
