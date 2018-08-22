ab3606dd747141b9ffc6170052ab4fd1923a5710
Alan Agius <alan.agius4@gmail.com>
2018-08-22 22:40:37 +0200

    fix(@angular-devkit/build-angular): only collect coverage from files under `sourceRoot`
    In some cases when having libraries within the workspace this is causing a `Maximum call stack size exceeded`, Also for libraries coverage should be collected with the respective `ng test`
    
    Closes #11934

--- packages/angular_devkit/build_angular/test/karma/code-coverage_spec_large.ts
+++ packages/angular_devkit/build_angular/test/karma/code-coverage_spec_large.ts
@@ -56,4 +56,52 @@ describe('Karma Builder code coverage', () => {
       }),
     ).toPromise().then(done, done.fail);
   }, 120000);
+
+  it(`should collect coverage from paths in 'sourceRoot'`, (done) => {
+    const overrides: Partial<NormalizedKarmaBuilderSchema> = { codeCoverage: true };
+
+    const files: { [path: string]: string } = {
+      './dist/my-lib/index.d.ts': `
+        export declare const title = 'app';
+      `,
+      './dist/my-lib/index.js': `
+        export const title = 'app';
+      `,
+      './src/app/app.component.ts': `
+        import { Component } from '@angular/core';
+        import { title } from 'my-lib';
+
+        @Component({
+          selector: 'app-root',
+          templateUrl: './app.component.html',
+          styleUrls: ['./app.component.css']
+        })
+        export class AppComponent {
+          title = title;
+        }
+      `,
+    };
+
+    host.writeMultipleFiles(files);
+
+    host.replaceInFile('tsconfig.json', /"baseUrl": ".\/",/, `
+      "baseUrl": "./",
+      "paths": {
+        "my-lib": [
+          "./dist/my-lib"
+        ]
+      },
+    `);
+
+    runTargetSpec(host, karmaTargetSpec, overrides).pipe(
+      // It seems like the coverage files take a while being written to disk, so we wait 500ms here.
+      debounceTime(500),
+      tap(buildEvent => {
+        expect(buildEvent.success).toBe(true);
+        expect(host.scopedSync().exists(coverageFilePath)).toBe(true);
+        const content = virtualFs.fileBufferToString(host.scopedSync().read(coverageFilePath));
+        expect(content).not.toContain('my-lib');
+      }),
+    ).toPromise().then(done, done.fail);
+  }, 120000);
 });
