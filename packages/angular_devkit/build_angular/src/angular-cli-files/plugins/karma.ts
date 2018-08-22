e38ddd1439cbe1095a5d2c87abc342dc47484326
Alan Agius <alan.agius4@gmail.com>
2018-08-22 12:00:44 +0200

    fix(@angular-devkit/build-angular): fixes cors issues with karma
    Checking the stack trace in the issue one can noticed that the paths for the components are being served via the `webpack://` protocol which is causing a `cors` issue between `http` and `webpack` protocol
    
    This PR removed the `webpack` protocol from the `devtoolModuleFilenameTemplate`
    
    Closes #11966

--- packages/angular_devkit/build_angular/src/angular-cli-files/plugins/karma.ts
+++ packages/angular_devkit/build_angular/src/angular-cli-files/plugins/karma.ts
@@ -144,6 +144,7 @@ const init: any = (config: any, emitter: any, customFileHandlers: any) => {
   // Files need to be served from a custom path for Karma.
   webpackConfig.output.path = '/_karma_webpack_/';
   webpackConfig.output.publicPath = '/_karma_webpack_/';
+  webpackConfig.output.devtoolModuleFilenameTemplate = '[namespace]/[resource-path]?[loaders]';
 
   let compiler: any;
   try {
