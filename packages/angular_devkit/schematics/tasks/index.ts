18f6c1b9fbac623b80bec999694fe68de93b32fa
Yi Qi <qiyi@google.com>
2018-08-24 15:22:21 -0700

    fix(@angular-devkit/schematics): export task executor

--- packages/angular_devkit/schematics/tasks/index.ts
+++ packages/angular_devkit/schematics/tasks/index.ts
@@ -5,6 +5,7 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
+export { BuiltinTaskExecutor } from './node';
 export { NodePackageInstallTask } from './node-package/install-task';
 export { NodePackageLinkTask } from './node-package/link-task';
 export { RepositoryInitializerTask } from './repo-init/init-task';
