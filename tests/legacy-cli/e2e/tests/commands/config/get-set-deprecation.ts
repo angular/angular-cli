4a25d7c9b4ebfabca2a4498c977003edbc792467
Hans <hans@hansl.ca>
2018-09-04 14:04:43 -0700

    test: get-set deprecation should check stderr now

--- tests/legacy-cli/e2e/tests/commands/config/get-set-deprecation.ts
+++ tests/legacy-cli/e2e/tests/commands/config/get-set-deprecation.ts
@@ -5,14 +5,14 @@ export default function() {
   const depRegEx = /get\/set have been deprecated in favor of the config command\./;
   return Promise.resolve()
     .then(() => ng('get'))
-    .then(({ stdout }) => {
-      if (!stdout.match(depRegEx)) {
+    .then(({ stderr }) => {
+      if (!stderr.match(depRegEx)) {
         throw new Error(`Expected deprecation warning.`);
       }
     })
     .then(() => ng('set'))
-    .then(({ stdout }) => {
-      if (!stdout.match(depRegEx)) {
+    .then(({ stderr }) => {
+      if (!stderr.match(depRegEx)) {
         throw new Error(`Expected deprecation warning.`);
       }
     });
