b24101d5a9ce34975a37788cf1d0ca4f1fe495d6
Hans <hans@hansl.ca>
2018-09-06 16:15:35 -0700

    refactor(@schematics/update): add automated JSON schema generation

--- packages/schematics/update/migrate/schema.ts
+++ packages/schematics/update/migrate/schema.ts
@@ -5,9 +5,21 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-export interface PostUpdateSchema {
-  package: string;
-  collection: string;
-  from: string;
-  to: string;
+export interface Schema {
+    /**
+     * The collection to load the migrations from.
+     */
+    collection: string;
+    /**
+     * The version installed previously.
+     */
+    from: string;
+    /**
+     * The package to migrate.
+     */
+    package: string;
+    /**
+     * The version to migrate to.
+     */
+    to: string;
 }
--- packages/schematics/update/update/schema.ts
+++ packages/schematics/update/update/schema.ts
@@ -5,13 +5,40 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-export interface UpdateSchema {
-  packages?: string | string[];
-  force: boolean;
-  all: boolean;
-  next: boolean;
-  migrateOnly: boolean;
-  from?: string;
-  to?: string;
-  registry: string;
+export interface Schema {
+    /**
+     * Whether to update all packages in package.json.
+     */
+    all: boolean;
+    /**
+     * If false, will error out if installed packages are incompatible with the update.
+     */
+    force: boolean;
+    /**
+     * Version from which to migrate from. Only available with a single package being updated,
+     * and only on migration only.
+     */
+    from?: string;
+    /**
+     * Only perform a migration, does not update the installed version.
+     */
+    migrateOnly: boolean;
+    /**
+     * Use the largest version, including beta and RCs.
+     */
+    next: boolean;
+    /**
+     * The packages to get.
+     */
+    packages?: string[];
+    /**
+     * The NPM registry to use.
+     */
+    registry?: string;
+    /**
+     * Version up to which to apply migrations. Only available with a single package being
+     * updated, and only on migrations only. Requires from to be specified. Default to the
+     * installed version detected.
+     */
+    to?: string;
 }
