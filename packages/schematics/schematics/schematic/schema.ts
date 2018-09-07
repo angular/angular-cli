0daa9fa5f1ee8330ce333e67dfb3f4839fe09f8d
Hans <hans@hansl.ca>
2018-09-06 17:37:11 -0700

    refactor(@schematics/schematics): automated JSON schema interface generation

--- packages/schematics/schematics/schematic/schema.ts
+++ packages/schematics/schematics/schematic/schema.ts
@@ -5,14 +5,13 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-
 export interface Schema {
-    /**
-     * The package name for the new schematic.
-     */
-    name: string;
     /**
      * Author for the new schematic.
      */
     author?: string;
+    /**
+     * The package name for the new schematic.
+     */
+    name: string;
 }
