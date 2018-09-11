0247b1c8d8e836baebeba928249e13a72dcb8142
Charles Lyding <19598772+clydin@users.noreply.github.com>
2018-09-11 13:48:16 -0400

    test: update license exceptions

--- scripts/validate-licenses.ts
+++ scripts/validate-licenses.ts
@@ -64,8 +64,8 @@ const ignoredPackages = [
   // Us.
   '@angular/devkit-repo@0.0.0',  // Hey, that's us!
   // * Development only
-  'spdx-license-ids@3.0.0',  // CC0 but it's content only (index.json, no code) and not distributed.
-  'tslint-sonarts@1.7.0', // LGPL-3.0 but only used as a tool, not linked in the build.
+  'spdx-license-ids@3.0.1',  // CC0 but it's content only (index.json, no code) and not distributed.
+  'tslint-sonarts@1.8.0', // LGPL-3.0 but only used as a tool, not linked in the build.
 
   // * Broken license fields
   'bitsyntax@0.0.4', // MIT but no license field in package.json
