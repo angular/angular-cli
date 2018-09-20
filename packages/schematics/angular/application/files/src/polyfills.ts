0432a6fca3f8767722dbfacac4b2e9daf33d9bda
Charles Lyding <19598772+clydin@users.noreply.github.com>
2018-09-20 19:55:18 -0400

    fix(@schematics/angular): remove redundant reflect polyfill

--- packages/schematics/angular/application/files/src/polyfills.ts
+++ packages/schematics/angular/application/files/src/polyfills.ts
@@ -40,12 +40,6 @@
 /** IE10 and IE11 requires the following for the Reflect API. */
 // import 'core-js/es6/reflect';
 
-
-/** Evergreen browsers require these. **/
-// Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
-import 'core-js/es7/reflect';
-
-
 /**
  * Web Animations `@angular/platform-browser/animations`
  * Only required if AnimationBuilder is used within the application and using IE/Edge or Safari.
