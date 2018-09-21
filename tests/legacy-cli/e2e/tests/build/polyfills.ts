f92419774335f0bc8f0a957e3ab9fbcf1812d792
Charles Lyding <19598772+clydin@users.noreply.github.com>
2018-09-21 17:11:47 -0400

    test: update E2E polyfills test for JIT mode

--- tests/legacy-cli/e2e/tests/build/polyfills.ts
+++ tests/legacy-cli/e2e/tests/build/polyfills.ts
@@ -1,18 +1,32 @@
-import { expectFileToMatch } from '../../utils/fs';
-import { ng } from '../../utils/process';
 import { oneLineTrim } from 'common-tags';
+import {
+  expectFileSizeToBeUnder,
+  expectFileToExist,
+  expectFileToMatch,
+  getFileSize,
+} from '../../utils/fs';
+import { ng } from '../../utils/process';
+import { expectToFail } from '../../utils/utils';
 
-export default function () {
-  // TODO(architect): Delete this test. It is now in devkit/build-angular.
-
-  return Promise.resolve()
-    .then(() => ng('build'))
+export default async function () {
+    await ng('build');
     // files were created successfully
-    .then(() => expectFileToMatch('dist/test-project/polyfills.js', 'core-js'))
-    .then(() => expectFileToMatch('dist/test-project/polyfills.js', 'zone.js'))
-    // index.html lists the right bundles
-    .then(() => expectFileToMatch('dist/test-project/index.html', oneLineTrim`
+    await expectFileToMatch('dist/test-project/polyfills.js', 'core-js/es7/reflect');
+    await expectFileToMatch('dist/test-project/polyfills.js', 'zone.js');
+    expectFileToMatch('dist/test-project/index.html', oneLineTrim`
       <script type="text/javascript" src="runtime.js"></script>
       <script type="text/javascript" src="polyfills.js"></script>
-    `));
+    `);
+    const jitPolyfillSize = await getFileSize('dist/test-project/polyfills.js');
+
+    await ng('build', '--aot');
+    // files were created successfully
+    await expectFileToExist('dist/test-project/polyfills.js');
+    await expectFileSizeToBeUnder('dist/test-project/polyfills.js', jitPolyfillSize);
+    await expectToFail(() => expectFileToMatch('dist/test-project/polyfills.js', 'core-js/es7/reflect'));
+    await expectFileToMatch('dist/test-project/polyfills.js', 'zone.js');
+    expectFileToMatch('dist/test-project/index.html', oneLineTrim`
+      <script type="text/javascript" src="runtime.js"></script>
+      <script type="text/javascript" src="polyfills.js"></script>
+    `);
 }
