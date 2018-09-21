38194b6ca4723b46413edccb8d491f5da8d1f6be
clydin <19598772+clydin@users.noreply.github.com>
2018-09-21 15:27:41 -0400

    ci: add size check of new project production build (#12328)

--- tests/legacy-cli/e2e/tests/basic/build.ts
+++ tests/legacy-cli/e2e/tests/basic/build.ts
@@ -1,9 +1,20 @@
-import {ng} from '../../utils/process';
-import {expectFileToMatch} from '../../utils/fs';
+import { expectFileToMatch, moveDirectory } from '../../utils/fs';
+import { ng } from '../../utils/process';
 
-export default function() {
-  return ng('build')
-    .then(() => expectFileToMatch('dist/test-project/index.html', 'main.js'))
-    .then(() => ng('build', '--prod'))
-    .then(() => expectFileToMatch('dist/test-project/index.html', /main\.[a-zA-Z0-9]{20}\.js/));
+
+export default async function() {
+  // Development build
+  await ng('build');
+  await expectFileToMatch('dist/test-project/index.html', 'main.js');
+
+
+  // Production build
+  await ng('build', '--prod');
+  await expectFileToMatch('dist/test-project/index.html', /main\.[a-zA-Z0-9]{20}\.js/);
+
+  // Store the production build for artifact storage on CircleCI
+  if (process.env['CIRCLECI']) {
+    await ng('build', '--prod', '--output-hashing=none');
+    await moveDirectory('dist', '/tmp/dist');
+  }
 }
