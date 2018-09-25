89ab69d24516c01fa39a064c4e414cd72369ae85
Charles Lyding <19598772+clydin@users.noreply.github.com>
2018-09-25 09:50:56 -0400

    build: update rxjs to 6.3

--- packages/schematics/angular/utility/latest-versions.ts
+++ packages/schematics/angular/utility/latest-versions.ts
@@ -9,7 +9,7 @@
 export const latestVersions = {
   // These versions should be kept up to date with latest Angular peer dependencies.
   Angular: '~7.0.0-beta.5',
-  RxJs: '~6.3.2',
+  RxJs: '~6.3.3',
   ZoneJs: '~0.8.26',
   TypeScript: '~3.0.1',
   // The versions below must be manually updated when making a new devkit release.
