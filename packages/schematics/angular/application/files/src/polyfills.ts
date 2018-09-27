4d46c4ac492e2ff460a52ca2a3f316c554df1521
Masahiko Sakakibara <sakakibara@rdlabo.jp>
2018-09-28 02:39:34 +0900

    fix(@schematics/angular): support `Fetch as Google`（Google bot) (#11815)

--- packages/schematics/angular/application/files/src/polyfills.ts
+++ packages/schematics/angular/application/files/src/polyfills.ts
@@ -28,11 +28,18 @@
 // import 'core-js/es6/math';
 // import 'core-js/es6/string';
 // import 'core-js/es6/date';
-// import 'core-js/es6/array';
 // import 'core-js/es6/regexp';
 // import 'core-js/es6/map';
 // import 'core-js/es6/weak-map';
 // import 'core-js/es6/set';
+
+/**
+ * If your app need to indexed by Google Search, your app require polyfills 'core-js/es6/array'
+ * Google bot use ES5.
+ * FYI: Googlebot uses a renderer following the similar spec to Chrome 41.
+ * https://developers.google.com/search/docs/guides/rendering
+ **/
+// import 'core-js/es6/array';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
