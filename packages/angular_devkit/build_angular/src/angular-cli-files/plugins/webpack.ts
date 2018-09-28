6553737b9d19e669588a9d3009593093174e8172
Charles Lyding <19598772+clydin@users.noreply.github.com>
2018-09-28 10:48:41 -0400

    refactor(@angular-devkit/build-angular): remove unused html webpack plugins

--- packages/angular_devkit/build_angular/src/angular-cli-files/plugins/webpack.ts
+++ packages/angular_devkit/build_angular/src/angular-cli-files/plugins/webpack.ts
@@ -7,7 +7,6 @@
  */
 
 // Exports the webpack plugins we use internally.
-export { BaseHrefWebpackPlugin } from '../lib/base-href-webpack/base-href-webpack-plugin';
 export { CleanCssWebpackPlugin, CleanCssWebpackPluginOptions } from './cleancss-webpack-plugin';
 export { BundleBudgetPlugin, BundleBudgetPluginOptions } from './bundle-budget';
 export { ScriptsWebpackPlugin, ScriptsWebpackPluginOptions } from './scripts-webpack-plugin';
