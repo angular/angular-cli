3bf2bb077442e9d884a44fcfc7c15f0ce0f1ef14
Charles Lyding <19598772+clydin@users.noreply.github.com>
2018-09-28 10:48:41 -0400

    refactor(@angular-devkit/build-angular): remove unused html webpack plugins

--- packages/angular_devkit/build_angular/src/angular-cli-files/models/webpack-configs/browser.ts
+++ packages/angular_devkit/build_angular/src/angular-cli-files/models/webpack-configs/browser.ts
@@ -9,11 +9,9 @@
 // TODO: cleanup this file, it's copied as is from Angular CLI.
 
 import * as path from 'path';
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const SubresourceIntegrityPlugin = require('webpack-subresource-integrity');
 import { LicenseWebpackPlugin } from 'license-webpack-plugin';
-import { generateEntryPoints, packageChunkSort } from '../../utilities/package-chunk-sort';
-import { BaseHrefWebpackPlugin } from '../../lib/base-href-webpack';
+import { generateEntryPoints } from '../../utilities/package-chunk-sort';
 import { IndexHtmlWebpackPlugin } from '../../plugins/index-html-webpack-plugin';
 import { WebpackConfigOptions } from '../build-options';
 import { normalizeExtraEntryPoints } from './utils';
@@ -26,36 +24,8 @@ import { normalizeExtraEntryPoints } from './utils';
 + */
 
 export function getBrowserConfig(wco: WebpackConfigOptions) {
-  const { root, projectRoot, buildOptions } = wco;
-
-
+  const { root, buildOptions } = wco;
   let extraPlugins: any[] = [];
-
-  // Figure out which are the lazy loaded bundle names.
-  const lazyChunkBundleNames = normalizeExtraEntryPoints(
-    // We don't really need a default name because we pre-filtered by lazy only entries.
-    [...buildOptions.styles, ...buildOptions.scripts], 'not-lazy')
-    .filter(entry => entry.lazy)
-    .map(entry => entry.bundleName)
-
-  const generateIndexHtml = false;
-  if (generateIndexHtml) {
-    extraPlugins.push(new HtmlWebpackPlugin({
-      template: path.resolve(root, buildOptions.index),
-      filename: path.resolve(buildOptions.outputPath, buildOptions.index),
-      chunksSortMode: packageChunkSort(buildOptions),
-      excludeChunks: lazyChunkBundleNames,
-      xhtml: true,
-      minify: buildOptions.optimization ? {
-        caseSensitive: true,
-        collapseWhitespace: true,
-        keepClosingSlash: true
-      } : false
-    }));
-    extraPlugins.push(new BaseHrefWebpackPlugin({
-      baseHref: buildOptions.baseHref as string
-    }));
-  }
 
   let sourcemaps: string | false = false;
   if (buildOptions.sourceMap) {
