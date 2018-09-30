8bf88e2b37cbc90f13788d69b68c38c19cb261a4
Alan Agius <alan.agius4@gmail.com>
2018-09-30 10:00:04 +0200

    feat(@schematics/angular): update to latest versions of `TypeScript` and `Angular`

--- packages/schematics/angular/utility/latest-versions.ts
+++ packages/schematics/angular/utility/latest-versions.ts
@@ -8,10 +8,10 @@
 
 export const latestVersions = {
   // These versions should be kept up to date with latest Angular peer dependencies.
-  Angular: '~7.0.0-beta.5',
+  Angular: '~7.0.0-rc.0',
   RxJs: '~6.3.3',
   ZoneJs: '~0.8.26',
-  TypeScript: '~3.0.1',
+  TypeScript: '~3.1.1',
   // The versions below must be manually updated when making a new devkit release.
   DevkitBuildAngular: '~0.9.0-beta.1',
   DevkitBuildNgPackagr: '~0.9.0-beta.1',
