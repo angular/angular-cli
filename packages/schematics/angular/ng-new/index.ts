f9d87261dfae4e62d11e06e11646178626c23467
Rohit Sarkar <rohitsarkar5398@gmail.com>
2018-10-06 03:33:10 +0530

    feat(@schematics/angular): Add --minimal flag to generate a barebones project

--- packages/schematics/angular/ng-new/index.ts
+++ packages/schematics/angular/ng-new/index.ts
@@ -42,6 +42,7 @@ export default function (options: NgNewOptions): Rule {
     version: options.version,
     experimentalAngularNext: options.experimentalIvy,
     newProjectRoot: options.newProjectRoot || 'projects',
+    minimal: options.minimal,
   };
   const applicationOptions: ApplicationOptions = {
     projectRoot: '',
--- tests/legacy-cli/e2e_runner.ts
+++ tests/legacy-cli/e2e_runner.ts
@@ -114,7 +114,6 @@ const allTests = glob.sync(path.join(e2eRoot, testGlob), { nodir: true, ignore: 
   // Replace windows slashes.
   .map(name => name.replace(/\\/g, '/'))
   .filter(name => !name.endsWith('/build-app-shell-with-schematic.ts'))
-  .filter(name => !name.endsWith('/new-minimal.ts'))
   // IS this test still valid? \/
   .filter(name => !name.endsWith('/module-id.ts'))
   // Do we want to support this?
