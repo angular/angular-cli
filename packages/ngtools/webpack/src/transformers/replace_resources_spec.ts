f4e5141528852546e1ae2a1957fd26807ecb7835
Alan Agius <alan.agius4@gmail.com>
2018-10-09 17:52:42 +0200

    fix(@ngtools/webpack): replace resources should effect only class decorators
    At the moment we are processing all property assignments in object literals irrespective if they are in a decorator or not. With this change we will process only property assignments found under in a component decorator.
    
    Fixes #12488, Fixes #6007, Fixes: #6498 and Fixes: #8295

--- packages/ngtools/webpack/src/transformers/replace_resources_spec.ts
+++ packages/ngtools/webpack/src/transformers/replace_resources_spec.ts
@@ -6,11 +6,21 @@
  * found in the LICENSE file at https://angular.io/license
  */
 import { tags } from '@angular-devkit/core';  // tslint:disable-line:no-implicit-dependencies
-import { transformTypescript } from './ast_helpers';
+import { createTypescriptContext, transformTypescript } from './ast_helpers';
 import { replaceResources } from './replace_resources';
 
+function transform(input: string, shouldTransform = true) {
+  const { program } = createTypescriptContext(input);
+  const getTypeChecker = () => program.getTypeChecker();
+  const transformer = replaceResources(() => shouldTransform, getTypeChecker);
+
+  return transformTypescript(input, [transformer]);
+}
+
+// tslint:disable-next-line:no-big-function
 describe('@ngtools/webpack transformers', () => {
-  describe('replace_resources', () => {
+  // tslint:disable-next-line:no-big-function
+  describe('find_resources', () => {
     it('should replace resources', () => {
       const input = tags.stripIndent`
         import { Component } from '@angular/core';
@@ -42,9 +52,43 @@ describe('@ngtools/webpack transformers', () => {
         export { AppComponent };
       `;
 
-      const transformer = replaceResources(() => true);
-      const result = transformTypescript(input, [transformer]);
+      const result = transform(input);
+      expect(tags.oneLine`${result}`).toEqual(tags.oneLine`${output}`);
+    });
 
+    it('should merge styleUrls with styles', () => {
+      const input = tags.stripIndent`
+        import { Component } from '@angular/core';
+
+        @Component({
+          selector: 'app-root',
+          templateUrl: './app.component.html',
+          styles: ['a { color: red }'],
+          styleUrls: ['./app.component.css'],
+        })
+        export class AppComponent {
+          title = 'app';
+        }
+      `;
+      const output = tags.stripIndent`
+        import * as tslib_1 from "tslib";
+        import { Component } from '@angular/core';
+        let AppComponent = class AppComponent {
+            constructor() {
+                this.title = 'app';
+            }
+        };
+        AppComponent = tslib_1.__decorate([
+            Component({
+                selector: 'app-root',
+                template: require("./app.component.html"),
+                styles: ["a { color: red }", require("./app.component.css")]
+            })
+        ], AppComponent);
+        export { AppComponent };
+      `;
+
+      const result = transform(input);
       expect(tags.oneLine`${result}`).toEqual(tags.oneLine`${output}`);
     });
 
@@ -79,9 +123,177 @@ describe('@ngtools/webpack transformers', () => {
         export { AppComponent };
       `;
 
-      const transformer = replaceResources(() => true);
+      const result = transform(input);
+      expect(tags.oneLine`${result}`).toEqual(tags.oneLine`${output}`);
+    });
+
+    it('should replace resources if Component decorator is aliased', () => {
+      const input = tags.stripIndent`
+        import { Component as NgComponent } from '@angular/core';
+
+        @NgComponent({
+          selector: 'app-root',
+          templateUrl: './app.component.html',
+          styleUrls: ['./app.component.css', './app.component.2.css']
+        })
+        export class AppComponent {
+          title = 'app';
+        }
+      `;
+      const output = tags.stripIndent`
+        import * as tslib_1 from "tslib";
+        import { Component as NgComponent } from '@angular/core';
+        let AppComponent = class AppComponent {
+            constructor() {
+                this.title = 'app';
+            }
+        };
+        AppComponent = tslib_1.__decorate([
+          NgComponent({
+                selector: 'app-root',
+                template: require("./app.component.html"),
+                styles: [require("./app.component.css"), require("./app.component.2.css")]
+            })
+        ], AppComponent);
+        export { AppComponent };
+      `;
+
+      const { program } = createTypescriptContext(input);
+      const getTypeChecker = () => program.getTypeChecker();
+      const transformer = replaceResources(() => true, getTypeChecker);
       const result = transformTypescript(input, [transformer]);
 
+      expect(tags.oneLine`${result}`).toEqual(tags.oneLine`${output}`);
+    });
+
+    it('should replace resources if Angular Core import is namespaced', () => {
+      const input = tags.stripIndent`
+        import * as ng from '@angular/core';
+
+        @ng.Component({
+          selector: 'app-root',
+          templateUrl: './app.component.html',
+          styleUrls: ['./app.component.css', './app.component.2.css']
+        })
+        export class AppComponent {
+          title = 'app';
+        }
+      `;
+      const output = tags.stripIndent`
+        import * as tslib_1 from "tslib";
+        import * as ng from '@angular/core';
+        let AppComponent = class AppComponent {
+            constructor() {
+                this.title = 'app';
+            }
+        };
+        AppComponent = tslib_1.__decorate([
+          ng.Component({
+                selector: 'app-root',
+                template: require("./app.component.html"),
+                styles: [require("./app.component.css"), require("./app.component.2.css")]
+            })
+        ], AppComponent);
+        export { AppComponent };
+      `;
+
+      const result = transform(input);
+      expect(tags.oneLine`${result}`).toEqual(tags.oneLine`${output}`);
+    });
+
+    it('should not replace resources if not in Component decorator', () => {
+      const input = tags.stripIndent`
+        import { Component } from '@angular/core';
+
+        @Component({
+          selector: 'app-root',
+          templateUrl: './app.component.html',
+          styleUrls: ['./app.component.css']
+        })
+        export class AppComponent {
+          obj = [
+            {
+              'labels': 'Content',
+              'templateUrl': 'content.html'
+            }
+          ];
+        }
+      `;
+
+      const output = tags.stripIndent`
+        import * as tslib_1 from "tslib";
+        import { Component } from '@angular/core';
+
+        let AppComponent = class AppComponent {
+          constructor() {
+            this.obj = [
+              {
+                'labels': 'Content',
+                'templateUrl': 'content.html'
+              }
+            ];
+          }
+        };
+
+        AppComponent = tslib_1.__decorate([
+            Component({
+                selector: 'app-root',
+                template: require("./app.component.html"),
+                styles: [require("./app.component.css")]
+            })
+        ], AppComponent);
+        export { AppComponent };
+      `;
+
+      const result = transform(input);
+      expect(tags.oneLine`${result}`).toEqual(tags.oneLine`${output}`);
+    });
+
+    it('should not replace resources if not in an NG Component decorator', () => {
+      const input = tags.stripIndent`
+        import { Component } from 'foo';
+
+        @Component({
+          selector: 'app-root',
+          templateUrl: './app.component.html',
+          styleUrls: ['./app.component.css']
+        })
+        export class AppComponent {
+          obj = [
+            {
+              'labels': 'Content',
+              'templateUrl': 'content.html'
+            }
+          ];
+        }
+      `;
+
+      const output = tags.stripIndent`
+        import * as tslib_1 from "tslib";
+        import { Component } from 'foo';
+
+        let AppComponent = class AppComponent {
+          constructor() {
+            this.obj = [
+              {
+                'labels': 'Content',
+                'templateUrl': 'content.html'
+              }
+            ];
+          }
+        };
+
+        AppComponent = tslib_1.__decorate([
+            Component({
+                selector: 'app-root',
+                templateUrl: './app.component.html',
+                styleUrls: ['./app.component.css']
+            })
+        ], AppComponent);
+        export { AppComponent };
+      `;
+
+      const result = transform(input);
       expect(tags.oneLine`${result}`).toEqual(tags.oneLine`${output}`);
     });
 
@@ -116,9 +328,7 @@ describe('@ngtools/webpack transformers', () => {
         export { AppComponent };
       `;
 
-      const transformer = replaceResources(() => false);
-      const result = transformTypescript(input, [transformer]);
-
+      const result = transform(input, false);
       expect(tags.oneLine`${result}`).toEqual(tags.oneLine`${output}`);
     });
   });
