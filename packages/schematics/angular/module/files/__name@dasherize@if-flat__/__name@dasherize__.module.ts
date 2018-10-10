739e05b66fa60e81ec822d2c758ef03752ce1db2
Zion Dials <me@ziondials.com>
2018-10-09 20:46:24 -0400

    Updated module generation @NgModule order
    Moved declarations prior to that of imports so that it matches original CLI module generation order. (https://github.com/angular/angular-cli/blob/master/packages/schematics/angular/application/other-files/app.module.ts)

--- packages/schematics/angular/module/files/__name@dasherize@if-flat__/__name@dasherize__.module.ts
+++ packages/schematics/angular/module/files/__name@dasherize@if-flat__/__name@dasherize__.module.ts
@@ -4,10 +4,10 @@ import { CommonModule } from '@angular/common';<% } %><% if (routing) { %>
 import { <%= classify(name) %>RoutingModule } from './<%= dasherize(name) %>-routing.module';<% } %>
 
 @NgModule({
+  declarations: [],
   imports: [<% if (commonModule) { %>
     CommonModule<%= routing ? ',' : '' %><% } %><% if (routing) { %>
     <%= classify(name) %>RoutingModule<% } %>
-  ],
-  declarations: []
+  ]
 })
 export class <%= classify(name) %>Module { }
