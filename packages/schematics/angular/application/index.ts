6e7c5e88f6cf02e822b82a4f3859bc67b66268a1
Noel Mace <contact@noelmace.com>
2018-10-24 22:45:08 +0200

    fix(@schematics/angular): --minimal should prevent generating e2e
    Don't run the e2e schematic when using application or ng-new schematics
    with option.minimal set to true.
    
    fix #12739

--- packages/schematics/angular/application/index.ts
+++ packages/schematics/angular/application/index.ts
@@ -372,7 +372,7 @@ export default function (options: ApplicationOptions): Rule {
           }),
           move(sourceDir),
         ]), MergeStrategy.Overwrite),
-      schematic('e2e', e2eOptions),
+      !options.minimal ? schematic('e2e', e2eOptions) : noop(),
       options.skipPackageJson ? noop() : addDependenciesToPackageJson(options),
     ]);
   };
