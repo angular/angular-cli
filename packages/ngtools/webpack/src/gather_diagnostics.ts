6803b91ea5efe31b95141132595dcf6a07d4b774
Filipe Silva <filipematossilva@gmail.com>
2018-10-31 15:43:48 +0000

    feat(@ngtools/webpack): only do type checking on forked type checker
    Syntactic errors were only being reported in the type checker when it was running. This caused rebuilds to finish successfully if they had syntactic errors, which could lead to very weird behaviour on rebuilds.

--- packages/ngtools/webpack/src/gather_diagnostics.ts
+++ packages/ngtools/webpack/src/gather_diagnostics.ts
@@ -9,6 +9,13 @@ import { Diagnostic, Diagnostics, Program } from '@angular/compiler-cli';
 import * as ts from 'typescript';
 import { time, timeEnd } from './benchmark';
 
+export enum DiagnosticMode {
+  Syntactic = 1 << 0,
+  Semantic = 1 << 1,
+
+  All = Syntactic | Semantic,
+  Default = All,
+}
 
 export class CancellationToken implements ts.CancellationToken {
   private _isCancelled = false;
@@ -36,6 +43,7 @@ export function gatherDiagnostics(
   program: ts.Program | Program,
   jitMode: boolean,
   benchmarkLabel: string,
+  mode = DiagnosticMode.All,
   cancellationToken?: CancellationToken,
 ): Diagnostics {
   const allDiagnostics: Array<ts.Diagnostic | Diagnostic> = [];
@@ -52,34 +60,44 @@ export function gatherDiagnostics(
     }
   }
 
+  const gatherSyntacticDiagnostics = (mode & DiagnosticMode.Syntactic) != 0;
+  const gatherSemanticDiagnostics = (mode & DiagnosticMode.Semantic) != 0;
+
   if (jitMode) {
     const tsProgram = program as ts.Program;
-    // Check syntactic diagnostics.
-    time(`${benchmarkLabel}.gatherDiagnostics.ts.getSyntacticDiagnostics`);
-    checkDiagnostics(tsProgram.getSyntacticDiagnostics.bind(tsProgram));
-    timeEnd(`${benchmarkLabel}.gatherDiagnostics.ts.getSyntacticDiagnostics`);
+    if (gatherSyntacticDiagnostics) {
+      // Check syntactic diagnostics.
+      time(`${benchmarkLabel}.gatherDiagnostics.ts.getSyntacticDiagnostics`);
+      checkDiagnostics(tsProgram.getSyntacticDiagnostics.bind(tsProgram));
+      timeEnd(`${benchmarkLabel}.gatherDiagnostics.ts.getSyntacticDiagnostics`);
+    }
 
-    // Check semantic diagnostics.
-    time(`${benchmarkLabel}.gatherDiagnostics.ts.getSemanticDiagnostics`);
-    checkDiagnostics(tsProgram.getSemanticDiagnostics.bind(tsProgram));
-    timeEnd(`${benchmarkLabel}.gatherDiagnostics.ts.getSemanticDiagnostics`);
+    if (gatherSemanticDiagnostics) {
+      // Check semantic diagnostics.
+      time(`${benchmarkLabel}.gatherDiagnostics.ts.getSemanticDiagnostics`);
+      checkDiagnostics(tsProgram.getSemanticDiagnostics.bind(tsProgram));
+      timeEnd(`${benchmarkLabel}.gatherDiagnostics.ts.getSemanticDiagnostics`);
+    }
   } else {
     const angularProgram = program as Program;
+    if (gatherSyntacticDiagnostics) {
+      // Check TypeScript syntactic diagnostics.
+      time(`${benchmarkLabel}.gatherDiagnostics.ng.getTsSyntacticDiagnostics`);
+      checkDiagnostics(angularProgram.getTsSyntacticDiagnostics.bind(angularProgram));
+      timeEnd(`${benchmarkLabel}.gatherDiagnostics.ng.getTsSyntacticDiagnostics`);
+    }
 
-    // Check TypeScript syntactic diagnostics.
-    time(`${benchmarkLabel}.gatherDiagnostics.ng.getTsSyntacticDiagnostics`);
-    checkDiagnostics(angularProgram.getTsSyntacticDiagnostics.bind(angularProgram));
-    timeEnd(`${benchmarkLabel}.gatherDiagnostics.ng.getTsSyntacticDiagnostics`);
+    if (gatherSemanticDiagnostics) {
+      // Check TypeScript semantic and Angular structure diagnostics.
+      time(`${benchmarkLabel}.gatherDiagnostics.ng.getTsSemanticDiagnostics`);
+      checkDiagnostics(angularProgram.getTsSemanticDiagnostics.bind(angularProgram));
+      timeEnd(`${benchmarkLabel}.gatherDiagnostics.ng.getTsSemanticDiagnostics`);
 
-    // Check TypeScript semantic and Angular structure diagnostics.
-    time(`${benchmarkLabel}.gatherDiagnostics.ng.getTsSemanticDiagnostics`);
-    checkDiagnostics(angularProgram.getTsSemanticDiagnostics.bind(angularProgram));
-    timeEnd(`${benchmarkLabel}.gatherDiagnostics.ng.getTsSemanticDiagnostics`);
-
-    // Check Angular semantic diagnostics
-    time(`${benchmarkLabel}.gatherDiagnostics.ng.getNgSemanticDiagnostics`);
-    checkDiagnostics(angularProgram.getNgSemanticDiagnostics.bind(angularProgram));
-    timeEnd(`${benchmarkLabel}.gatherDiagnostics.ng.getNgSemanticDiagnostics`);
+      // Check Angular semantic diagnostics
+      time(`${benchmarkLabel}.gatherDiagnostics.ng.getNgSemanticDiagnostics`);
+      checkDiagnostics(angularProgram.getNgSemanticDiagnostics.bind(angularProgram));
+      timeEnd(`${benchmarkLabel}.gatherDiagnostics.ng.getNgSemanticDiagnostics`);
+    }
   }
 
   return allDiagnostics;
