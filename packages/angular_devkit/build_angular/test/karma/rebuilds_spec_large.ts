7dd79555503f95690aa9a55b8999ffc2ff638a05
Filipe Silva <filipematossilva@gmail.com>
2018-10-31 10:55:04 +0000

    feat(@angular-devkit/build-angular): stop karma gracefully
    Followup to https://github.com/karma-runner/karma/pull/3153

--- packages/angular_devkit/build_angular/test/karma/rebuilds_spec_large.ts
+++ packages/angular_devkit/build_angular/test/karma/rebuilds_spec_large.ts
@@ -10,64 +10,18 @@ import { runTargetSpec } from '@angular-devkit/architect/testing';
 import { debounceTime, take, tap } from 'rxjs/operators';
 import { host, karmaTargetSpec } from '../utils';
 
-
-// Karma watch mode is currently bugged:
-// - errors print a huge stack trace
-// - karma does not have a way to close the server
-//   gracefully (https://github.com/karma-runner/karma/issues/3149)
-// TODO: fix these before 6.0 final.
-xdescribe('Karma Builder watch mode', () => {
+describe('Karma Builder watch mode', () => {
   beforeEach(done => host.initialize().toPromise().then(done, done.fail));
   afterEach(done => host.restore().toPromise().then(done, done.fail));
 
-  it('works', (done) => {
+  it('works', async () => {
     const overrides = { watch: true };
-    runTargetSpec(host, karmaTargetSpec, overrides).pipe(
+    const res = await runTargetSpec(host, karmaTargetSpec, overrides).pipe(
       debounceTime(500),
       tap((buildEvent) => expect(buildEvent.success).toBe(true)),
       take(1),
-    ).toPromise().then(done, done.fail);
-  }, 30000);
+    ).toPromise();
 
-  it('recovers from compilation failures in watch mode', (done) => {
-    const overrides = { watch: true };
-    let buildNumber = 0;
-
-    runTargetSpec(host, karmaTargetSpec, overrides).pipe(
-      debounceTime(500),
-      tap((buildEvent) => {
-        buildNumber += 1;
-        switch (buildNumber) {
-          case 1:
-            // Karma run should succeed.
-            // Add a compilation error.
-            expect(buildEvent.success).toBe(true);
-            host.writeMultipleFiles({
-              'src/app/app.component.spec.ts': '<p> definitely not typescript </p>',
-            });
-            break;
-
-          case 2:
-            // Karma run should fail due to compilation error. Fix it.
-            expect(buildEvent.success).toBe(false);
-            host.writeMultipleFiles({ 'src/foo.spec.ts': '' });
-            break;
-
-          case 3:
-            // Karma run should succeed again.
-            expect(buildEvent.success).toBe(true);
-            break;
-
-          default:
-            break;
-        }
-      }),
-      take(3),
-    ).toPromise().then(done, done.fail);
-  }, 30000);
-
-  it('does not rebuild when nothing changed', (done) => {
-    // Start the server in watch mode, wait for the first build to finish, touch
-    // test.js without changing it, wait 5s then exit unsuscribe, verify only one event was emitted.
+    expect(res).toEqual({ success: true });
   }, 30000);
 });
