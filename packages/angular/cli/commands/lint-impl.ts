bc76d0c2736a93e9137c0b2cf376bdc6869bbe45
Alan Agius <alan.agius4@gmail.com>
2018-11-12 10:29:42 +0100

    refactor(@angular/cli): move project name being linted logic to builder

--- packages/angular/cli/commands/lint-impl.ts
+++ packages/angular/cli/commands/lint-impl.ts
@@ -5,7 +5,6 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import { TargetSpecifier } from '@angular-devkit/architect';
 import { ArchitectCommand, ArchitectCommandOptions } from '../models/architect-command';
 import { Arguments } from '../models/interface';
 import { Schema as LintCommandSchema } from './lint';
@@ -13,12 +12,6 @@ import { Schema as LintCommandSchema } from './lint';
 export class LintCommand extends ArchitectCommand<LintCommandSchema> {
   public readonly target = 'lint';
   public readonly multiTarget = true;
-
-  protected async runSingleTarget(targetSpec: TargetSpecifier, options: string[]) {
-    this.logger.info(`Linting ${JSON.stringify(targetSpec.project)}...`);
-
-    return super.runSingleTarget(targetSpec, options);
-  }
 
   public async run(options: ArchitectCommandOptions & Arguments) {
     return this.runArchitectTarget(options);
