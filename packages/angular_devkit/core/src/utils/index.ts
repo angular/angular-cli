c89a0420d5611d074d2c8d3dcd5f0016db1ebdf7
Hans <hans@hansl.ca>
2018-11-15 12:00:32 -0800

    feat(@angular-devkit/core): add DeepReadonly<> and Readwrite<> types
    Readwrite removes readonly, while DeepReadonly recursively sets it. A ReadonlyArray
    should be used if the type is an array, but that is too much of a change to our API
    (JsonValue includes JsonArray but then we would need a ReadonlyJsonValue, and same
    for object, which has deep roots in our API).
    
    We should fix this in a further refactor at some point.

--- packages/angular_devkit/core/src/utils/index.ts
+++ packages/angular_devkit/core/src/utils/index.ts
@@ -16,3 +16,20 @@ export * from './priority-queue';
 export * from './lang';
 
 export { tags, strings };
+
+export type DeepReadonly<T> =
+  T extends (infer R)[] ? DeepReadonlyArray<R> :
+    T extends Function ? T :
+      T extends object ? DeepReadonlyObject<T> :
+        T;
+
+// This should be ReadonlyArray but it has implications.
+export interface DeepReadonlyArray<T> extends Array<DeepReadonly<T>> {}
+
+export type DeepReadonlyObject<T> = {
+  readonly [P in keyof T]: DeepReadonly<T[P]>;
+};
+
+export type Readwrite<T> = {
+  -readonly [P in keyof T]: T[P];
+};
