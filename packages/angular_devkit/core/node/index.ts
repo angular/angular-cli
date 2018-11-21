ee6a8b282eb0696978249da4e15ae89c8ab635b2
Alan Agius <alan.agius4@gmail.com>
2018-11-21 14:21:31 +0100

    refactor: create `fs` namespace instead of aliased export

--- packages/angular_devkit/core/node/index.ts
+++ packages/angular_devkit/core/node/index.ts
@@ -5,10 +5,7 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import * as fs from './fs';
-
+export * from './fs';
 export * from './cli-logger';
 export * from './host';
 export { ModuleNotFoundException, ResolveOptions, resolve } from './resolve';
-
-export { fs };
