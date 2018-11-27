3a70eb73c4de534a95c4aff68eb543edb5db84b4
Filipe Silva <filipematossilva@gmail.com>
2018-11-27 16:06:01 +0000

    ci: add windows buildkite

--- appveyor-e2e.bat
+++ appveyor-e2e.bat
@@ -1,6 +1,12 @@
 @ECHO OFF
-IF defined APPVEYOR_PULL_REQUEST_NUMBER (
-    node tests\legacy-cli\run_e2e.js --appveyor "--glob=tests/basic/**"
-) ELSE (
+IF not defined APPVEYOR_PULL_REQUEST_NUMBER (
+  IF not defined BUILDKITE_PULL_REQUEST (
+    REM Run full test suite if not on a Appveyor/Buildkite PR.
     node tests\legacy-cli\run_e2e.js --appveyor "--glob=tests/{basic,commands,generate,build/styles}/**"
-)
+    exit
+  )
+) 
+
+REM Run partial test suite.
+node tests\legacy-cli\run_e2e.js --appveyor "--glob=tests/basic/**"
+
