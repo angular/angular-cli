549c4a12d4757a4516e89090abbb038fe9179a70
ameerthehacker <ameerjhanprof@gmail.com>
2018-12-03 21:06:04 +0530

    refactor: replace karma jasmine html reporter with karma material reporter

--- packages/schematics/angular/application/files/root/karma.conf.js
+++ packages/schematics/angular/application/files/root/karma.conf.js
@@ -8,7 +8,7 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
+      require('karma-material-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
@@ -20,7 +20,7 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'material'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
