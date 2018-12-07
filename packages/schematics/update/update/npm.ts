a4d3eeb7112a9a74e92f8ec4a32d3fd3a946d251
Charles Lyding <19598772+clydin@users.noreply.github.com>
2018-12-07 13:14:42 -0500

    fix(@schematics/update): only set registry option when used

--- packages/schematics/update/update/npm.ts
+++ packages/schematics/update/update/npm.ts
@@ -112,7 +112,7 @@ export function getNpmPackageJson(
     {
       'full-metadata': true,
       ...npmrc,
-      registry: registryUrl,
+      ...(registryUrl ? { registry: registryUrl } : {}),
     },
   );
 
