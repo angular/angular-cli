4c8c7b701f894eb406c6e19ed7760902cee8ba87
Alan Agius <alan.agius@betssongroup.com>
2018-12-12 15:20:28 +0100

    refactor: `buildOptimizerLoader` to use async methods of source-map
    `source-map` methods are no longer sync

--- packages/angular_devkit/build_optimizer/src/build-optimizer/webpack-loader.ts
+++ packages/angular_devkit/build_optimizer/src/build-optimizer/webpack-loader.ts
@@ -17,66 +17,84 @@ interface BuildOptimizerLoaderOptions {
   sourceMap: boolean;
 }
 
+interface BuildOptimizerResult {
+  content: string;
+  sourceMap: RawSourceMap | undefined;
+}
+
 export default function buildOptimizerLoader
   (this: webpack.loader.LoaderContext, content: string, previousSourceMap: RawSourceMap) {
   this.cacheable();
   const options: BuildOptimizerLoaderOptions = loaderUtils.getOptions(this) || {};
+  const callback = this.async();
 
-  // Make up names of the intermediate files so we can chain the sourcemaps.
-  const inputFilePath = this.resourcePath + '.pre-build-optimizer.js';
-  const outputFilePath = this.resourcePath + '.post-build-optimizer.js';
-
-  const boOutput = buildOptimizer({
-    content,
-    originalFilePath: this.resourcePath,
-    inputFilePath,
-    outputFilePath,
-    emitSourceMap: options.sourceMap,
-    isSideEffectFree: this._module
-                      && this._module.factoryMeta
-                      && this._module.factoryMeta.sideEffectFree,
-  });
-
-  if (boOutput.emitSkipped || boOutput.content === null) {
-    // Webpack typings for previousSourceMap are wrong, they are JSON objects and not strings.
-    // tslint:disable-next-line:no-any
-    this.callback(null, content, previousSourceMap as any);
-
+  if (!callback) {
     return;
   }
 
-  const intermediateSourceMap = boOutput.sourceMap;
-  let newContent = boOutput.content;
+  const remapSourceMap = async (): Promise<BuildOptimizerResult> => {
+    // Make up names of the intermediate files so we can chain the sourcemaps.
+    const inputFilePath = this.resourcePath + '.pre-build-optimizer.js';
+    const outputFilePath = this.resourcePath + '.post-build-optimizer.js';
 
-  let newSourceMap;
+    const boOutput = buildOptimizer({
+      content,
+      originalFilePath: this.resourcePath,
+      inputFilePath,
+      outputFilePath,
+      emitSourceMap: options.sourceMap,
+      isSideEffectFree: this._module
+        && this._module.factoryMeta
+        && this._module.factoryMeta.sideEffectFree,
+    });
 
-  if (options.sourceMap && intermediateSourceMap) {
-    // Webpack doesn't need sourceMappingURL since we pass them on explicitely.
-    newContent = newContent.replace(/^\/\/# sourceMappingURL=[^\r\n]*/gm, '');
-
-    if (previousSourceMap) {
-      // If there's a previous sourcemap, we have to chain them.
-      // See https://github.com/mozilla/source-map/issues/216#issuecomment-150839869 for a simple
-      // source map chaining example.
-      // Use http://sokra.github.io/source-map-visualization/ to validate sourcemaps make sense.
-
-      // Force the previous sourcemap to use the filename we made up for it.
-      // In order for source maps to be chained, the consumed source map `file` needs to be in the
-      // consumers source map `sources` array.
-      previousSourceMap.file = inputFilePath;
-
-      // Chain the sourcemaps.
-      const consumer = new SourceMapConsumer(intermediateSourceMap);
-      const generator = SourceMapGenerator.fromSourceMap(consumer);
-      generator.applySourceMap(new SourceMapConsumer(previousSourceMap));
-      newSourceMap = generator.toJSON();
-    } else {
-      // Otherwise just return our generated sourcemap.
-      newSourceMap = intermediateSourceMap;
+    if (boOutput.emitSkipped || boOutput.content === null) {
+      // Webpack typings for previousSourceMap are wrong, they are JSON objects and not strings.
+      // tslint:disable-next-line:no-any
+      return { content, sourceMap: previousSourceMap };
     }
-  }
 
-  // Webpack typings for previousSourceMap are wrong, they are JSON objects and not strings.
-  // tslint:disable-next-line:no-any
-  this.callback(null, newContent, newSourceMap as any);
+    const intermediateSourceMap = boOutput.sourceMap;
+    let newContent = boOutput.content;
+
+    let newSourceMap;
+
+    if (options.sourceMap && intermediateSourceMap) {
+      // Webpack doesn't need sourceMappingURL since we pass them on explicitely.
+      newContent = newContent.replace(/^\/\/# sourceMappingURL=[^\r\n]*/gm, '');
+
+      if (previousSourceMap) {
+        // If there's a previous sourcemap, we have to chain them.
+        // See https://github.com/mozilla/source-map/issues/216#issuecomment-150839869 for a simple
+        // source map chaining example.
+        // Use http://sokra.github.io/source-map-visualization/ to validate sourcemaps make sense.
+
+        // Force the previous sourcemap to use the filename we made up for it.
+        // In order for source maps to be chained, the consumed source map `file` needs to be in the
+        // consumers source map `sources` array.
+        previousSourceMap.file = inputFilePath;
+
+        // Chain the sourcemaps.
+        const [consumer, prevConsumer] = await Promise.all([
+          new SourceMapConsumer(intermediateSourceMap),
+          new SourceMapConsumer(previousSourceMap),
+        ]);
+        const generator = SourceMapGenerator.fromSourceMap(consumer);
+        generator.applySourceMap(prevConsumer);
+        newSourceMap = generator.toJSON();
+      } else {
+        // Otherwise just return our generated sourcemap.
+        newSourceMap = intermediateSourceMap;
+      }
+    }
+
+    return { content: newContent, sourceMap: newSourceMap };
+  };
+
+  remapSourceMap()
+    // Webpack typings for previousSourceMap are wrong, they are JSON objects and not strings.
+    // tslint:disable-next-line:no-any
+    .then(({ content, sourceMap }) => callback(null, content, sourceMap as any))
+    .catch(err => callback(err));
+
 }
