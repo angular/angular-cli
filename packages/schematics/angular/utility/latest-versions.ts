7896343d47131fc069529e38733b2df7d5644c71
Alan Agius <alan.agius@betssongroup.com>
2018-12-19 11:19:34 +0100

    feat(@schematics/angular): add support for TypeScript 3.2 and Angular 7.2.0-rc.0

--- packages/schematics/angular/utility/latest-versions.ts
+++ packages/schematics/angular/utility/latest-versions.ts
@@ -8,10 +8,10 @@
 
 export const latestVersions = {
   // These versions should be kept up to date with latest Angular peer dependencies.
-  Angular: '~7.1.0',
+  Angular: '~7.2.0-rc.0',
   RxJs: '~6.3.3',
   ZoneJs: '~0.8.26',
-  TypeScript: '~3.1.6',
+  TypeScript: '~3.2.2',
   TsLib: '^1.9.0',
   // The versions below must be manually updated when making a new devkit release.
   DevkitBuildAngular: '~0.12.0-beta.2',
