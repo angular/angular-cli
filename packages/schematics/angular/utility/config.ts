8b4f3672cb42217dabd9284aabcd0f52d73fc94d
Thinh Le <thinhlv2@fsoft.com.vn>
2018-12-27 11:08:03 +0700

    fix shadow variable name, use `find` method instead of `filter`

--- packages/schematics/angular/utility/config.ts
+++ packages/schematics/angular/utility/config.ts
@@ -480,9 +480,9 @@ export interface CliConfig {
 
 export function getWorkspacePath(host: Tree): string {
   const possibleFiles = [ '/angular.json', '/.angular.json' ];
-  const path = possibleFiles.filter(path => host.exists(path))[0];
+  const path = possibleFiles.find(value => host.exists(value));
 
-  return path;
+  return path as string;
 }
 
 export function getWorkspace(host: Tree): WorkspaceSchema {
