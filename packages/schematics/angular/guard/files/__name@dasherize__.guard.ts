8863bc002aad20329194a5f7716b87b32b4a0b3a
Unknown <okeefem355@gmail.com>
2019-01-15 06:06:36 -0500

    feat(@schematics/angular): Added multiselect guard implements option
    Adds support for the guard schematic to use multiselect for which interfaces the guard should implement.
    
    Fixes #13400

--- packages/schematics/angular/guard/files/__name@dasherize__.guard.ts
+++ packages/schematics/angular/guard/files/__name@dasherize__.guard.ts
@@ -1,14 +1,24 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { <%= implementationImports %>ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class <%= classify(name) %>Guard implements CanActivate {
-  canActivate(
+export class <%= classify(name) %>Guard implements <%= implementations %> {
+  <% if (implements.includes('CanActivate')) { %>canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return true;
   }
+  <% } %><% if (implements.includes('CanActivateChild')) { %>canActivateChild(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return true;
+  }
+  <% } %><% if (implements.includes('CanLoad')) { %>canLoad(
+    route: Route,
+    segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
+    return true;
+  }<% } %>
 }
