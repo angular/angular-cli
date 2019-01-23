364b38a9df900aebfcc7aac4d662110b36e1990a
Keen Yee Liau <kyliau@google.com>
2019-01-23 10:47:50 -0800

    fix(@schematics/angular): Add types annotations to e2e AppPage
    This is a cherry-pick of dc2e40a100040ad3ff4265a9bc7303fde3d3a800
    Original PR: https://github.com/angular/angular-cli/pull/13406
    
    When app.po.ts is compiled under Bazel with Typescript 3.2,
    AppPage produces the following error:
    
    e2e/src/app.po.ts:4:3 - error TS2742: The inferred type of 'navigateTo' cannot be named without a reference to '../../external/npm/node_modules/@types/selenium-webdriver/index'. This is likely not portable. A type annotation is necessary.
    
    4 navigateTo() {
    ~~~~~~~~~~
    e2e/src/app.po.ts:8:3 - error TS2742: The inferred type of 'getTitleText' cannot be named without a reference to '../../external/npm/node_modules/@types/selenium-webdriver/index'. This is likely not portable. A type annotation is necessary.
    
    8 getTitleText() {
    ~~~~~~~~~~~~

--- packages/schematics/angular/e2e/files/src/app.po.ts
+++ packages/schematics/angular/e2e/files/src/app.po.ts
@@ -2,10 +2,10 @@ import { browser, by, element } from 'protractor';
 
 export class AppPage {
   navigateTo() {
-    return browser.get('/');
+    return browser.get('/') as Promise<any>;
   }
 
   getTitleText() {
-    return element(by.css('<%= rootSelector %> h1')).getText();
+    return element(by.css('<%= rootSelector %> h1')).getText() as Promise<string>;
   }
 }
