68e891976354451f94e35f49565b2e7c915ee1c1
Angular Builds <circleci@angular.io>
2019-01-30 17:27:17 -0800

    release: v7.3.0

--- packages/schematics/angular/utility/latest-versions.ts
+++ packages/schematics/angular/utility/latest-versions.ts
@@ -8,12 +8,12 @@
 
 export const latestVersions = {
   // These versions should be kept up to date with latest Angular peer dependencies.
-  Angular: '~7.2.0-rc.0',
+  Angular: '~7.2.0',
   RxJs: '~6.3.3',
   ZoneJs: '~0.8.26',
   TypeScript: '~3.2.2',
   TsLib: '^1.9.0',
   // The versions below must be manually updated when making a new devkit release.
-  DevkitBuildAngular: '~0.13.0-beta.0',
-  DevkitBuildNgPackagr: '~0.13.0-beta.0',
+  DevkitBuildAngular: '~0.13.0',
+  DevkitBuildNgPackagr: '~0.13.0',
 };
