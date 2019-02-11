81ec711d3e0727e9ab13ed2aee23ded879c7c3a5
NisStrom <Stephan.Meier@nis.ch>
2019-02-11 08:59:40 +0100

    feat(@angular/cli): fix lint errors whitespace
    fix trailing whitespace

--- packages/angular/cli/lib/cli/index.ts
+++ packages/angular/cli/lib/cli/index.ts
@@ -36,7 +36,7 @@ export default async function(options: { testing?: boolean, cliArgs: string[] })
 
       return maybeExitCode;
     }
-    
+
     return 0;
   } catch (err) {
     if (options.cliArgs.includes('e2e') && options.cliArgs[options.cliArgs.findIndex((element) => {
@@ -44,7 +44,7 @@ export default async function(options: { testing?: boolean, cliArgs: string[] })
       }) + 1] === 'true') {
       logger.fatal('An unexpected error occurred while e2e run: ' + JSON.stringify(err)
         + ';  Used --force to continue');
-      
+
       return 0;
     } else if (err instanceof Error) {
       logger.fatal(err.message);
