cb38f79aad53ca0b1c708b55a20c117e9385be5c
NisStrom <Stephan.Meier@nis.ch>
2019-02-11 08:23:55 +0100

    feat(@angular/cli): fix lint errors

--- packages/angular/cli/lib/cli/index.ts
+++ packages/angular/cli/lib/cli/index.ts
@@ -40,11 +40,11 @@ export default async function(options: { testing?: boolean, cliArgs: string[] })
     return 0;
   } catch (err) {
     if (options.cliArgs.includes('e2e') && options.cliArgs[options.cliArgs.findIndex((element) => {
-      return element == '--force';
-    }
-    ) + 1] === 'true') {
+        return element == '--force';
+      }) + 1] === 'true') {
       logger.fatal('An unexpected error occurred while e2e run: ' + JSON.stringify(err)
         + ';  Used --force to continue');
+      
       return 0;
     } else if (err instanceof Error) {if (err instanceof Error) {
       logger.fatal(err.message);
