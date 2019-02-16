df241dc4511ccff1cc2e8ea2a9486857214a6587
Charles Lyding <19598772+clydin@users.noreply.github.com>
2019-02-15 20:48:55 -0500

    fix(@angular-devkit/build-angular): improve webpack loader resolution
    Previously, all loaders either needed to be in the workspace's node modules directory or a node modules directory directly within the build angular package.  A package manager can potentially hoist a loader to a node modules location inbetween the two and causing loader resolution to fail.  This change causes webpack to check all intermediate node modules directories in addition to the initial two locations.

--- packages/angular_devkit/build_angular/src/angular-cli-files/utilities/find-up.ts
+++ packages/angular_devkit/build_angular/src/angular-cli-files/utilities/find-up.ts
@@ -5,11 +5,9 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-// tslint:disable
-// TODO: cleanup this file, it's copied as is from Angular CLI.
-
-import * as path from 'path';
 import { existsSync } from 'fs';
+import * as path from 'path';
+import { isDirectory } from './is-directory';
 
 export function findUp(names: string | string[], from: string, stopOnNodeModules = false) {
   if (!Array.isArray(names)) {
@@ -37,4 +35,24 @@ export function findUp(names: string | string[], from: string, stopOnNodeModules
   }
 
   return null;
+}
+
+export function findAllNodeModules(from: string, root?: string) {
+  const nodeModules: string[] = [];
+
+  let current = from;
+  while (current && current !== root) {
+    const potential = path.join(current, 'node_modules');
+    if (existsSync(potential) && isDirectory(potential)) {
+      nodeModules.push(potential);
+    }
+
+    const next = path.dirname(current);
+    if (next === current) {
+      break;
+    }
+    current = next;
+  }
+
+  return nodeModules;
 }
