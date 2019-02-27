7f16cc7129ba47b7972e891f314023c2cad980d4
Alan <alan.agius@betssongroup.com>
2019-02-27 16:13:21 +0100

    test: update e2e setup to cater for e2e within an application

--- tests/legacy-cli/e2e/tests/basic/e2e.ts
+++ tests/legacy-cli/e2e/tests/basic/e2e.ts
@@ -19,18 +19,18 @@ export default function () {
     scripts['wd:clean'] = 'webdriver-manager clean';
   })
     .then(() => npm('run', 'wd:clean'))
-    .then(() => expectToFail(() => ng('e2e', 'test-project-e2e', '--no-webdriver-update', '--devServerTarget=')))
+    .then(() => expectToFail(() => ng('e2e', 'test-project', '--no-webdriver-update', '--devServerTarget=')))
     // Add back the pre-defined version of webdriver. This script is defined when making projects.
     .then(() => npm('run', 'webdriver-update'))
     // Should fail without serving
-    .then(() => expectToFail(() => ng('e2e', 'test-project-e2e', '--devServerTarget=')))
+    .then(() => expectToFail(() => ng('e2e', 'test-project', '--devServerTarget=')))
     // These should work.
-    .then(() => ng('e2e', 'test-project-e2e'))
-    .then(() => ng('e2e', 'test-project-e2e', '--devServerTarget=test-project:serve:production'))
+    .then(() => ng('e2e', 'test-project'))
+    .then(() => ng('e2e', 'test-project', '--devServerTarget=test-project:serve:production'))
     // Should accept different config file
     .then(() => moveFile('./e2e/protractor.conf.js',
       './e2e/renamed-protractor.conf.js'))
-    .then(() => ng('e2e', 'test-project-e2e',
+    .then(() => ng('e2e', 'test-project',
       '--protractorConfig=e2e/renamed-protractor.conf.js'))
     .then(() => moveFile('./e2e/renamed-protractor.conf.js', './e2e/protractor.conf.js'))
     // Should accept different multiple spec files
@@ -38,7 +38,7 @@ export default function () {
       './e2e/src/renamed-app.e2e-spec.ts'))
     .then(() => copyFile('./e2e/src/renamed-app.e2e-spec.ts',
       './e2e/src/another-app.e2e-spec.ts'))
-    .then(() => ng('e2e', 'test-project-e2e', '--specs', './e2e/renamed-app.e2e-spec.ts',
+    .then(() => ng('e2e', 'test-project', '--specs', './e2e/renamed-app.e2e-spec.ts',
       '--specs', './e2e/another-app.e2e-spec.ts'))
     // Rename the spec back to how it was.
     .then(() => moveFile('./e2e/src/renamed-app.e2e-spec.ts',
@@ -50,7 +50,7 @@ export default function () {
             app: './e2e/src/app.e2e-spec.ts'
           },
     `))
-    .then(() => ng('e2e', 'test-project-e2e', '--suite=app'))
+    .then(() => ng('e2e', 'test-project', '--suite=app'))
     // Remove suites block from protractor.conf.js file after testing suites
     .then(() => replaceInFile('e2e/protractor.conf.js', `allScriptsTimeout: 11000,
           suites: {
@@ -59,7 +59,7 @@ export default function () {
     `, `allScriptsTimeout: 11000,`
     ))
     // Should start up Element Explorer
-    .then(() => execAndWaitForOutputToMatch('ng', ['e2e', 'test-project-e2e', '--element-explorer'],
+    .then(() => execAndWaitForOutputToMatch('ng', ['e2e', 'test-project', '--element-explorer'],
       /Element Explorer/))
     .then(() => killAllProcesses(), (err: any) => {
       killAllProcesses();
@@ -68,7 +68,7 @@ export default function () {
     // Should run side-by-side with `ng serve`
     .then(() => execAndWaitForOutputToMatch('ng', ['serve'],
       /: Compiled successfully./))
-    .then(() => ng('e2e', 'test-project-e2e', '--devServerTarget='))
+    .then(() => ng('e2e', 'test-project', '--devServerTarget='))
     .then(() => killAllProcesses(), (err: any) => {
       killAllProcesses();
       throw err;
