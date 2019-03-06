15d7aea2b79f2a3a3a6669099e47fef086d3a5f5
Hans Larsen <hansl@users.noreply.github.com>
2019-03-06 10:16:47 -0800

    feat(@angular-devkit/core): analytics interfaces and basic implementations
    This is a preliminary analytics interface. It is meant to be used by every
    other subsystem where useful, but can be replaced by implementations.

--- packages/angular_devkit/core/src/index.ts
+++ packages/angular_devkit/core/src/index.ts
@@ -5,6 +5,7 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
+import * as analytics from './analytics';
 import * as experimental from './experimental';
 import * as json from './json/index';
 import * as logging from './logger/index';
@@ -16,6 +17,7 @@ export * from './utils/index';
 export * from './virtual-fs/index';
 
 export {
+  analytics,
   experimental,
   json,
   logging,
