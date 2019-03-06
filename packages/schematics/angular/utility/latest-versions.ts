fd5cb7a7f0c36e737fb3bb5e64fb2b66c2fef55d
Hans Larsen <hansl@users.noreply.github.com>
2019-03-06 14:01:53 -0800

    release: v8.0.0-beta.5

--- packages/schematics/angular/utility/latest-versions.ts
+++ packages/schematics/angular/utility/latest-versions.ts
@@ -8,12 +8,12 @@
 
 export const latestVersions = {
   // These versions should be kept up to date with latest Angular peer dependencies.
-  Angular: '~8.0.0-beta.5',
+  Angular: '~8.0.0-beta.7',
   RxJs: '~6.4.0',
   ZoneJs: '~0.8.26',
   TypeScript: '~3.2.2',
   TsLib: '^1.9.0',
   // The versions below must be manually updated when making a new devkit release.
-  DevkitBuildAngular: '~0.14.0-beta.4',
-  DevkitBuildNgPackagr: '~0.14.0-beta.4',
+  DevkitBuildAngular: '~0.14.0-beta.5',
+  DevkitBuildNgPackagr: '~0.14.0-beta.5',
 };
