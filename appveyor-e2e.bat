a9ebfc807684a355fad0abcae2a0e971bae622d1
Alan <alan.agius@betssongroup.com>
2019-03-07 14:28:03 +0100

    ci: include ivy tests in windows ci
    With this change we include ivy tests to run under windows, also this changes `ivy-lazy-load` test to create a project only 1 time. This is important for windows, as `npm install` & `ngcc` take a lot of time, and for this specific test it is done 3 times.
    
    Now `npm install` and `ngcc` will only run once

--- appveyor-e2e.bat
+++ appveyor-e2e.bat
@@ -2,11 +2,11 @@
 IF not defined APPVEYOR_PULL_REQUEST_NUMBER (
   IF not defined BUILDKITE_PULL_REQUEST (
     REM Run full test suite if not on a Appveyor/Buildkite PR.
-    node tests\legacy-cli\run_e2e.js --appveyor "--glob=tests/{basic,commands,generate,build/styles}/**"
+    node tests\legacy-cli\run_e2e.js --appveyor "--glob=tests/{basic,commands,generate,build/styles,ivy}/**"
     exit
   )
 ) 
 
 REM Run partial test suite.
-node tests\legacy-cli\run_e2e.js --appveyor "--glob=tests/basic/**"
+node tests\legacy-cli\run_e2e.js --appveyor "--glob=tests/{basic,ivy}/**"
 
