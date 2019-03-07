7727c28ebda13aad3e1ac53ad9cc3823b9646284
Alan <alan.agius@betssongroup.com>
2019-03-07 14:28:03 +0100

    ci: include ivy tests in windows ci
    With this change we include ivy tests to run under windows, also this changes `ivy-lazy-load` test to create a project only 1 time. This is important for windows, as `npm install` & `ngcc` take a lot of time, and for this specific test it is done 3 times.
    
    Now `npm install` and `ngcc` will only run once

--- tests/legacy-cli/e2e/tests/ivy/ivy-lazy-load.ts
+++ tests/legacy-cli/e2e/tests/ivy/ivy-lazy-load.ts
@@ -5,26 +5,35 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import { replaceInFile, writeFile } from '../../utils/fs';
+import { readFile, replaceInFile, writeFile } from '../../utils/fs';
 import { ng } from '../../utils/process';
 import { createProject, updateJsonFile } from '../../utils/project';
 import { expectToFail } from '../../utils/utils';
 
-export async function ivyLazyLoadSetup(projectName: string) {
+export default async function () {
+  const projectName = 'ivy-lazy-loading';
+  const appRoutingModulePath = 'src/app/app-routing.module.ts';
+
   // Make Ivy project.
   await createProject(projectName, '--enable-ivy', '--routing');
 
+  const originalAppRoutingModule = await readFile(appRoutingModulePath);
+  // helper to replace loadChildren
+  const replaceLoadChildren = async (route: string) => {
+    const content = originalAppRoutingModule.replace('const routes: Routes = [];', `
+      const routes: Routes = [{ path: 'lazy', loadChildren: ${route} }];
+    `);
+
+    return writeFile(appRoutingModulePath, content);
+  };
+
   // Add lazy route.
   await ng('generate', 'module', 'lazy', '--routing');
-  await replaceInFile('src/app/app-routing.module.ts', 'const routes: Routes = [];', `
-    const routes: Routes = [{ path: 'lazy', loadChildren: LOAD_CHILDREN_MARKER }];
-  `);
   await ng('generate', 'component', 'lazy/lazy-comp');
   await replaceInFile('src/app/lazy/lazy-routing.module.ts', 'const routes: Routes = [];', `
     import { LazyCompComponent } from './lazy-comp/lazy-comp.component';
     const routes: Routes = [{ path: '', component: LazyCompComponent }];
   `);
-  await writeFile('src/app/app.component.html', '<router-outlet></router-outlet>');
 
   // Add lazy route e2e
   await writeFile('e2e/src/app.e2e-spec.ts', `
@@ -59,30 +68,23 @@ export async function ivyLazyLoadSetup(projectName: string) {
     buildTarget['options']['aot'] = false;
     buildTarget['configurations']['production'] = { aot: true };
   });
-}
 
-export default async function () {
   // Test `import()` style lazy load.
-  await ivyLazyLoadSetup('ivy-lazy-import');
-  await replaceInFile('src/app/app-routing.module.ts', 'LOAD_CHILDREN_MARKER', `
-   () => import('./lazy/lazy.module').then(m => m.LazyModule)
-  `);
+  await replaceLoadChildren(`() => import('./lazy/lazy.module').then(m => m.LazyModule)`);
   await ng('e2e');
   await ng('e2e', '--prod');
 
   // Test string import with factory shims.
-  await ivyLazyLoadSetup('ivy-lazy-string-shims');
-  await replaceInFile('src/app/app-routing.module.ts', 'LOAD_CHILDREN_MARKER',
-    `'./lazy/lazy.module#LazyModule'`);
+  await replaceLoadChildren(`'./lazy/lazy.module#LazyModule'`);
   await replaceInFile('tsconfig.app.json', `"allowEmptyCodegenFiles": false`,
     `"allowEmptyCodegenFiles": true`);
   await expectToFail(() => ng('e2e')); // Currently broken.
   await ng('e2e', '--prod');
 
   // Test string import without factory shims.
-  await ivyLazyLoadSetup('ivy-lazy-string-no-shims');
-  await replaceInFile('src/app/app-routing.module.ts', 'LOAD_CHILDREN_MARKER',
-    `'./lazy/lazy.module#LazyModule'`);
+  await replaceLoadChildren(`'./lazy/lazy.module#LazyModule'`);
+  await replaceInFile('tsconfig.app.json', `"allowEmptyCodegenFiles": true`,
+  `"allowEmptyCodegenFiles": false`);
   await expectToFail(() => ng('e2e')); // Not supported.
   await expectToFail(() => ng('e2e', '--prod')); // Not supported.
 }
