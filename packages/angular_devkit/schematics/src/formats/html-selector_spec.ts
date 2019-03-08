b1477425d9bf6203ab237c3e30342964f8fb91a4
clydin <19598772+clydin@users.noreply.github.com>
2019-03-07 19:01:41 -0500

    feat(@angular-devkit/schematics): support unicode character HTML element names (#13837)
    The HTML specification allows for a wide variety of characters to be present within a custom element name.  The previous behavior limited the names to mostly alphanumeric characters.  This change opens up the names to include the characters specified within the specification for custom element names.

--- packages/angular_devkit/schematics/src/formats/html-selector_spec.ts
+++ packages/angular_devkit/schematics/src/formats/html-selector_spec.ts
@@ -5,11 +5,9 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-
 import { map } from 'rxjs/operators';
 import { formatValidator } from './format-validator';
 import { htmlSelectorFormat } from './html-selector';
-
 
 describe('Schematics HTML selector format', () => {
   it('accepts correct selectors', done => {
@@ -45,14 +43,25 @@ describe('Schematics HTML selector format', () => {
       .toPromise().then(done, done.fail);
   });
 
-  it('rejects selectors with non-letter after dash', done => {
+  it('accepts selectors with non-letter after dash', done => {
     const data = { selector: 'my-1selector' };
     const dataSchema = {
       properties: { selector: { type: 'string', format: 'html-selector' } },
     };
 
     formatValidator(data, dataSchema, [htmlSelectorFormat])
-      .pipe(map(result => expect(result.success).toBe(false)))
+      .pipe(map(result => expect(result.success).toBe(true)))
+      .toPromise().then(done, done.fail);
+  });
+
+  it('accepts selectors with unicode', done => {
+    const data = { selector: 'app-root😀' };
+    const dataSchema = {
+      properties: { selector: { type: 'string', format: 'html-selector' } },
+    };
+
+    formatValidator(data, dataSchema, [htmlSelectorFormat])
+      .pipe(map(result => expect(result.success).toBe(true)))
       .toPromise().then(done, done.fail);
   });
 });
