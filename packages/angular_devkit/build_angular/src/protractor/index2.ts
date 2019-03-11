a49d278baa8561a39566dffd5c947bcc79b485c0
Charles Lyding <19598772+clydin@users.noreply.github.com>
2019-03-11 12:04:03 -0400

    feat(@angular-devkit/build-angular): implement stable architect API for protractor

---
+++ packages/angular_devkit/build_angular/src/protractor/index2.ts
@@ -0,0 +1,152 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import {
+  BuilderContext,
+  BuilderOutput,
+  createBuilder,
+  targetFromTargetString,
+} from '@angular-devkit/architect/src/index2';
+import { JsonObject, tags } from '@angular-devkit/core';
+import { resolve } from 'path';
+import * as url from 'url';
+import { runModuleAsObservableFork } from '../utils';
+import { Schema as ProtractorBuilderOptions } from './schema';
+
+function runProtractor(root: string, options: ProtractorBuilderOptions): Promise<BuilderOutput> {
+  const additionalProtractorConfig: Partial<ProtractorBuilderOptions> = {
+    elementExplorer: options.elementExplorer,
+    baseUrl: options.baseUrl,
+    specs: options.specs && options.specs.length ? options.specs : undefined,
+    suite: options.suite,
+  };
+
+  // TODO: Protractor manages process.exit itself, so this target will allways quit the
+  // process. To work around this we run it in a subprocess.
+  // https://github.com/angular/protractor/issues/4160
+  return runModuleAsObservableFork(
+    root,
+    'protractor/built/launcher',
+    'init',
+    [resolve(root, options.protractorConfig), additionalProtractorConfig],
+  ).toPromise() as Promise<BuilderOutput>;
+}
+
+async function updateWebdriver() {
+  // The webdriver-manager update command can only be accessed via a deep import.
+  const webdriverDeepImport = 'webdriver-manager/built/lib/cmds/update';
+  const importOptions = [
+    // When using npm, webdriver is within protractor/node_modules.
+    `protractor/node_modules/${webdriverDeepImport}`,
+    // When using yarn, webdriver is found as a root module.
+    webdriverDeepImport,
+  ];
+
+  let path;
+  for (const importOption of importOptions) {
+    try {
+      path = require.resolve(importOption);
+    } catch (error) {
+      if (error.code !== 'MODULE_NOT_FOUND') {
+        throw error;
+      }
+    }
+  }
+
+  if (!path) {
+    throw new Error(tags.stripIndents`
+      Cannot automatically find webdriver-manager to update.
+      Update webdriver-manager manually and run 'ng e2e --no-webdriver-update' instead.
+    `);
+  }
+
+  // tslint:disable-next-line:max-line-length no-implicit-dependencies
+  const webdriverUpdate = await import(path) as typeof import ('webdriver-manager/built/lib/cmds/update');
+
+  // run `webdriver-manager update --standalone false --gecko false --quiet`
+  // if you change this, update the command comment in prev line
+  return webdriverUpdate.program.run({
+    standalone: false,
+    gecko: false,
+    quiet: true,
+  } as unknown as JSON);
+}
+
+export async function execute(
+  options: ProtractorBuilderOptions,
+  context: BuilderContext,
+): Promise<BuilderOutput> {
+  // ensure that only one of these options is used
+  if (options.devServerTarget && options.baseUrl) {
+    throw new Error(tags.stripIndents`
+    The 'baseUrl' option cannot be used with 'devServerTarget'.
+    When present, 'devServerTarget' will be used to automatically setup 'baseUrl' for Protractor.
+    `);
+  }
+
+  if (options.webdriverUpdate) {
+    try {
+      await updateWebdriver();
+    } catch (error) {
+      context.reportStatus('Error: ' + error);
+
+      return { success: false };
+    }
+  }
+
+  let baseUrl;
+  let server;
+  if (options.devServerTarget) {
+    const target = targetFromTargetString(options.devServerTarget);
+    const serverOptions = await context.getTargetOptions(target);
+
+    const overrides: Record<string, string | number | boolean> = { watch: false };
+    if (options.host !== undefined) { overrides.host = options.host; }
+    if (options.port !== undefined) { overrides.port = options.port; }
+    server = await context.scheduleTarget(target, overrides);
+
+    let result;
+    try {
+      result = await server.result;
+    } catch (error) {
+      context.reportStatus('Error: ' + error);
+    }
+
+    if (!result || !result.success) {
+      return { success: false };
+    }
+
+    if (typeof serverOptions.publicHost === 'string') {
+      let publicHost = serverOptions.publicHost as string;
+      if (!/^\w+:\/\//.test(publicHost)) {
+        publicHost = `${serverOptions.ssl
+          ? 'https'
+          : 'http'}://${publicHost}`;
+      }
+      const clientUrl = url.parse(publicHost);
+      baseUrl = url.format(clientUrl);
+    } else if (typeof result.port === 'number') {
+      baseUrl = url.format({
+        protocol: serverOptions.ssl ? 'https' : 'http',
+        hostname: options.host,
+        port: result.port.toString(),
+      });
+    }
+  }
+
+  try {
+    return await runProtractor(context.workspaceRoot, { ...options, baseUrl });
+  } catch {
+    return { success: false };
+  } finally {
+    if (server) {
+      await server.stop();
+    }
+  }
+}
+
+export default createBuilder<JsonObject & ProtractorBuilderOptions>(execute);
--- packages/angular_devkit/build_angular/test/protractor/works_spec_large.ts
+++ packages/angular_devkit/build_angular/test/protractor/works_spec_large.ts
@@ -5,47 +5,58 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-
-import { runTargetSpec } from '@angular-devkit/architect/testing';
-import { normalize } from '@angular-devkit/core';
-import { retry } from 'rxjs/operators';
-import { host, protractorTargetSpec } from '../utils';
-
+import { Architect } from '@angular-devkit/architect/src/index2';
+import { JsonObject, normalize } from '@angular-devkit/core';
+import { createArchitect, host, protractorTargetSpec } from '../utils';
 
 describe('Protractor Builder', () => {
-  beforeEach(done => host.initialize().toPromise().then(done, done.fail));
-  afterEach(done => host.restore().toPromise().then(done, done.fail));
+  let architect: Architect;
 
-  it('works', (done) => {
-    runTargetSpec(host, protractorTargetSpec).pipe(
-      retry(3),
-    ).toPromise().then(done, done.fail);
+  beforeEach(async () => {
+    await host.initialize().toPromise();
+    architect = (await createArchitect(host.root())).architect;
+  });
+
+  afterEach(() => host.restore().toPromise());
+
+  it('executes tests with automatic dev server usage', async () => {
+    const run = await architect.scheduleTarget(protractorTargetSpec);
+
+    await expectAsync(run.result).toBeResolvedTo(jasmine.objectContaining({ success: true }));
+
+    await run.stop();
   }, 30000);
 
-  it('works with no devServerTarget', (done) => {
-    const overrides = { devServerTarget: undefined };
+  it('fails with no devServerTarget and no standalone server', async () => {
+    const overrides = { devServerTarget: undefined } as unknown as JsonObject;
+    const run = await architect.scheduleTarget(protractorTargetSpec, overrides);
 
-    runTargetSpec(host, protractorTargetSpec, overrides).pipe(
-      // This should fail because no server is available for connection.
-    ).subscribe(undefined, () => done(), done.fail);
+    await expectAsync(run.result).toBeResolvedTo(jasmine.objectContaining({ success: false }));
+
+    await run.stop();
   }, 30000);
 
-  it('overrides protractor specs', (done) => {
-    host.scopedSync().rename(normalize('./e2e/app.e2e-spec.ts'),
-      normalize('./e2e/renamed-app.e2e.spec.ts'));
+  it('overrides protractor specs', async () => {
+    host.scopedSync().rename(
+      normalize('./e2e/app.e2e-spec.ts'),
+      normalize('./e2e/renamed-app.e2e.spec.ts'),
+    );
 
     const overrides = { specs: ['./e2e/renamed-app.e2e.spec.ts'] };
+    const run = await architect.scheduleTarget(protractorTargetSpec, overrides);
 
-    runTargetSpec(host, protractorTargetSpec, overrides).pipe(
-      retry(3),
-    ).toPromise().then(done, done.fail);
+    await expectAsync(run.result).toBeResolvedTo(jasmine.objectContaining({ success: true }));
+
+    await run.stop();
   }, 60000);
 
-  it('overrides protractor suites', (done) => {
-    host.scopedSync().rename(normalize('./e2e/app.e2e-spec.ts'),
-      normalize('./e2e/renamed-app.e2e-spec.ts'));
+  it('overrides protractor suites', async () => {
+    host.scopedSync().rename(
+      normalize('./e2e/app.e2e-spec.ts'),
+      normalize('./e2e/renamed-app.e2e-spec.ts'),
+    );
 
-    // Suites block need to be added in the protractor.conf.js file to test suites
+    // Suites block needs to be added in the protractor.conf.js file to test suites
     host.replaceInFile('protractor.conf.js', `allScriptsTimeout: 11000,`, `
       allScriptsTimeout: 11000,
       suites: {
@@ -54,23 +65,20 @@ describe('Protractor Builder', () => {
     `);
 
     const overrides = { suite: 'app' };
+    const run = await architect.scheduleTarget(protractorTargetSpec, overrides);
 
-    runTargetSpec(host, protractorTargetSpec, overrides).pipe(
-      retry(3),
-    ).toPromise().then(done, done.fail);
+    await expectAsync(run.result).toBeResolvedTo(jasmine.objectContaining({ success: true }));
+
+    await run.stop();
   }, 60000);
 
-  it('works with port 0', (done) => {
-    runTargetSpec(host, protractorTargetSpec, { port: 0 }).pipe(
-      retry(3),
-    ).toPromise().then(done, done.fail);
+  it('supports automatic port assignment (port = 0)', async () => {
+    const overrides = { port: 0 };
+    const run = await architect.scheduleTarget(protractorTargetSpec, overrides);
+
+    await expectAsync(run.result).toBeResolvedTo(jasmine.objectContaining({ success: true }));
+
+    await run.stop();
   }, 30000);
 
-  // TODO: test `element-explorer` when the protractor builder emits build events with text.
-  // .then(() => execAndWaitForOutputToMatch('ng', ['e2e', '--element-explorer'],
-  // /Element Explorer/))
-  // .then(() => killAllProcesses(), (err: any) => {
-  //   killAllProcesses();
-  //   throw err;
-  // })
 });
