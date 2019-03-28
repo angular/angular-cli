1327ee3ec9ba525c2ba30c1f393461cb6fea94ca
Charles Lyding <19598772+clydin@users.noreply.github.com>
2019-03-28 13:25:38 -0400

    test(@angular-devkit/build-angular): update service worker browser tests to stable API

--- packages/angular_devkit/build_angular/test/browser/service-worker_spec_large.ts
+++ packages/angular_devkit/build_angular/test/browser/service-worker_spec_large.ts
@@ -5,12 +5,9 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-
-import { runTargetSpec } from '@angular-devkit/architect/testing';
+import { Architect } from '@angular-devkit/architect/src/index2';
 import { normalize, virtualFs } from '@angular-devkit/core';
-import { tap } from 'rxjs/operators';
-import { browserTargetSpec, host } from '../utils';
-
+import { createArchitect, host } from '../utils';
 
 describe('Browser Builder service worker', () => {
   const manifest = {
@@ -43,19 +40,26 @@ describe('Browser Builder service worker', () => {
     ],
   };
 
-  beforeEach(done => host.initialize().toPromise().then(done, done.fail));
-  afterEach(done => host.restore().toPromise().then(done, done.fail));
+  const target = { project: 'app', target: 'build' };
+  let architect: Architect;
 
-  it('errors if no ngsw-config.json is present', (done) => {
+  beforeEach(async () => {
+    await host.initialize().toPromise();
+    architect = (await createArchitect(host.root())).architect;
+  });
+  afterEach(async () => host.restore().toPromise());
+
+  it('errors if no ngsw-config.json is present', async () => {
     const overrides = { serviceWorker: true };
 
-    runTargetSpec(host, browserTargetSpec, overrides)
-      .subscribe(event => {
-        expect(event.success).toBe(false);
-      }, () => done(), done.fail);
+    const run = await architect.scheduleTarget(target, overrides);
+
+    await expectAsync(run.result).toBeResolvedTo(jasmine.objectContaining({ success: false }));
+
+    await run.stop();
   });
 
-  it('works with service worker', (done) => {
+  it('works with service worker', async () => {
     host.writeMultipleFiles({
       'src/ngsw-config.json': JSON.stringify(manifest),
       'src/assets/folder-asset.txt': 'folder-asset.txt',
@@ -63,108 +67,110 @@ describe('Browser Builder service worker', () => {
     });
 
     const overrides = { serviceWorker: true };
-    runTargetSpec(host, browserTargetSpec, overrides).pipe(
-      tap(buildEvent => {
-        expect(buildEvent.success).toBe(true);
-        expect(host.scopedSync().exists(normalize('dist/ngsw.json')));
-        const ngswJson = JSON.parse(virtualFs.fileBufferToString(
-          host.scopedSync().read(normalize('dist/ngsw.json'))));
-        // Verify index and assets are there.
-        expect(ngswJson).toEqual(jasmine.objectContaining({
-          configVersion: 1,
-          index: '/index.html',
-          navigationUrls: [
-            { positive: true, regex: '^\\\/.*$' },
-            { positive: false, regex: '^\\\/(?:.+\\\/)?[^\/]*\\.[^\/]*$' },
-            { positive: false, regex: '^\\\/(?:.+\\\/)?[^\/]*__[^\/]*$' },
-            { positive: false, regex: '^\\\/(?:.+\\\/)?[^\/]*__[^\/]*\\\/.*$' },
+    const run = await architect.scheduleTarget(target, overrides);
+
+    await expectAsync(run.result).toBeResolvedTo(jasmine.objectContaining({ success: true }));
+
+    expect(host.scopedSync().exists(normalize('dist/ngsw.json')));
+    const ngswJson = JSON.parse(virtualFs.fileBufferToString(
+      host.scopedSync().read(normalize('dist/ngsw.json'))));
+    // Verify index and assets are there.
+    expect(ngswJson).toEqual(jasmine.objectContaining({
+      configVersion: 1,
+      index: '/index.html',
+      navigationUrls: [
+        { positive: true, regex: '^\\\/.*$' },
+        { positive: false, regex: '^\\\/(?:.+\\\/)?[^\/]*\\.[^\/]*$' },
+        { positive: false, regex: '^\\\/(?:.+\\\/)?[^\/]*__[^\/]*$' },
+        { positive: false, regex: '^\\\/(?:.+\\\/)?[^\/]*__[^\/]*\\\/.*$' },
+      ],
+      assetGroups: [
+        {
+          name: 'app',
+          installMode: 'prefetch',
+          updateMode: 'prefetch',
+          urls: [
+            '/favicon.ico',
+            '/index.html',
           ],
-          assetGroups: [
-            {
-              name: 'app',
-              installMode: 'prefetch',
-              updateMode: 'prefetch',
-              urls: [
-                '/favicon.ico',
-                '/index.html',
-              ],
-              patterns: [],
-            },
-            {
-              name: 'assets',
-              installMode: 'lazy',
-              updateMode: 'prefetch',
-              urls: [
-                '/assets/folder-asset.txt',
-                '/spectrum.png',
-              ],
-              patterns: [],
-            },
+          patterns: [],
+        },
+        {
+          name: 'assets',
+          installMode: 'lazy',
+          updateMode: 'prefetch',
+          urls: [
+            '/assets/folder-asset.txt',
+            '/spectrum.png',
           ],
-          dataGroups: [],
-          hashTable: {
-            '/favicon.ico': '84161b857f5c547e3699ddfbffc6d8d737542e01',
-            '/assets/folder-asset.txt': '617f202968a6a81050aa617c2e28e1dca11ce8d4',
-            '/index.html': '1bcafd53046ffb270ac5e6f3cab23e0442f95c4f',
-            '/spectrum.png': '8d048ece46c0f3af4b598a95fd8e4709b631c3c0',
-          },
-        }));
-      }),
-    ).toPromise().then(done, done.fail);
+          patterns: [],
+        },
+      ],
+      dataGroups: [],
+      hashTable: {
+        '/favicon.ico': '84161b857f5c547e3699ddfbffc6d8d737542e01',
+        '/assets/folder-asset.txt': '617f202968a6a81050aa617c2e28e1dca11ce8d4',
+        '/index.html': '1bcafd53046ffb270ac5e6f3cab23e0442f95c4f',
+        '/spectrum.png': '8d048ece46c0f3af4b598a95fd8e4709b631c3c0',
+      },
+    }));
+
+    await run.stop();
   });
 
-  it('works with service worker and baseHref', (done) => {
+  it('works with service worker and baseHref', async () => {
     host.writeMultipleFiles({
       'src/ngsw-config.json': JSON.stringify(manifest),
       'src/assets/folder-asset.txt': 'folder-asset.txt',
     });
 
     const overrides = { serviceWorker: true, baseHref: '/foo/bar' };
-    runTargetSpec(host, browserTargetSpec, overrides).pipe(
-      tap(buildEvent => {
-        expect(buildEvent.success).toBe(true);
-        expect(host.scopedSync().exists(normalize('dist/ngsw.json')));
-        const ngswJson = JSON.parse(virtualFs.fileBufferToString(
-          host.scopedSync().read(normalize('dist/ngsw.json'))));
-        // Verify index and assets include the base href.
-        expect(ngswJson).toEqual(jasmine.objectContaining({
-          configVersion: 1,
-          index: '/foo/bar/index.html',
-          navigationUrls: [
-            { positive: true, regex: '^\\\/.*$' },
-            { positive: false, regex: '^\\\/(?:.+\\\/)?[^\/]*\\.[^\/]*$' },
-            { positive: false, regex: '^\\\/(?:.+\\\/)?[^\/]*__[^\/]*$' },
-            { positive: false, regex: '^\\\/(?:.+\\\/)?[^\/]*__[^\/]*\\\/.*$' },
+    const run = await architect.scheduleTarget(target, overrides);
+
+    await expectAsync(run.result).toBeResolvedTo(jasmine.objectContaining({ success: true }));
+
+    expect(host.scopedSync().exists(normalize('dist/ngsw.json')));
+    const ngswJson = JSON.parse(virtualFs.fileBufferToString(
+      host.scopedSync().read(normalize('dist/ngsw.json'))));
+    // Verify index and assets include the base href.
+    expect(ngswJson).toEqual(jasmine.objectContaining({
+      configVersion: 1,
+      index: '/foo/bar/index.html',
+      navigationUrls: [
+        { positive: true, regex: '^\\\/.*$' },
+        { positive: false, regex: '^\\\/(?:.+\\\/)?[^\/]*\\.[^\/]*$' },
+        { positive: false, regex: '^\\\/(?:.+\\\/)?[^\/]*__[^\/]*$' },
+        { positive: false, regex: '^\\\/(?:.+\\\/)?[^\/]*__[^\/]*\\\/.*$' },
+      ],
+      assetGroups: [
+        {
+          name: 'app',
+          installMode: 'prefetch',
+          updateMode: 'prefetch',
+          urls: [
+            '/foo/bar/favicon.ico',
+            '/foo/bar/index.html',
           ],
-          assetGroups: [
-            {
-              name: 'app',
-              installMode: 'prefetch',
-              updateMode: 'prefetch',
-              urls: [
-                '/foo/bar/favicon.ico',
-                '/foo/bar/index.html',
-              ],
-              patterns: [],
-            },
-            {
-              name: 'assets',
-              installMode: 'lazy',
-              updateMode: 'prefetch',
-              urls: [
-                '/foo/bar/assets/folder-asset.txt',
-              ],
-              patterns: [],
-            },
+          patterns: [],
+        },
+        {
+          name: 'assets',
+          installMode: 'lazy',
+          updateMode: 'prefetch',
+          urls: [
+            '/foo/bar/assets/folder-asset.txt',
           ],
-          dataGroups: [],
-          hashTable: {
-            '/foo/bar/favicon.ico': '84161b857f5c547e3699ddfbffc6d8d737542e01',
-            '/foo/bar/assets/folder-asset.txt': '617f202968a6a81050aa617c2e28e1dca11ce8d4',
-            '/foo/bar/index.html': '925d80777b6ba64b526b0be79761d254dfe94c65',
-          },
-        }));
-      }),
-    ).toPromise().then(done, done.fail);
+          patterns: [],
+        },
+      ],
+      dataGroups: [],
+      hashTable: {
+        '/foo/bar/favicon.ico': '84161b857f5c547e3699ddfbffc6d8d737542e01',
+        '/foo/bar/assets/folder-asset.txt': '617f202968a6a81050aa617c2e28e1dca11ce8d4',
+        '/foo/bar/index.html': '925d80777b6ba64b526b0be79761d254dfe94c65',
+      },
+    }));
+
+    await run.stop();
   });
 });
