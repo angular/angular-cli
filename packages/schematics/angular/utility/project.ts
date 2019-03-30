02b1cb0b76510c7ecae9388bc6ca6ee037d99d18
hawkgs <hawk.dvp@gmail.com>
2019-03-30 21:44:01 +0200

    feat(@schematics/angular): add util for finding whether Ivy is enabled

--- packages/schematics/angular/utility/project.ts
+++ packages/schematics/angular/utility/project.ts
@@ -10,15 +10,18 @@ import { getWorkspace } from '../utility/config';
 import { ProjectType, WorkspaceProject, WorkspaceSchema } from '../utility/workspace-models';
 
 
+export function getProjectRoot(project: WorkspaceProject) {
+  return project.sourceRoot
+    ? `/${project.sourceRoot}/`
+    : `/${project.root}/src/`;
+}
+
 /**
  * Build a default project path for generating.
  * @param project The project to build the path for.
  */
 export function buildDefaultPath(project: WorkspaceProject): string {
-  const root = project.sourceRoot
-    ? `/${project.sourceRoot}/`
-    : `/${project.root}/src/`;
-
+  const root = getProjectRoot(project);
   const projectDirName = project.projectType === ProjectType.Application ? 'app' : 'lib';
 
   return `${root}${projectDirName}`;
@@ -35,6 +38,23 @@ export function getProject<TProjectType extends ProjectType = ProjectType.Applic
   return workspace.projects[projectName] as WorkspaceProject<TProjectType>;
 }
 
+export function getProjectConfig(host: Tree, project: WorkspaceProject) {
+  const root = getProjectRoot(project);
+  const configFile = host.read(root + 'tsconfig.app.json');
+  if (!configFile) {
+    throw new Error(`Couldn't find project config (tsconfig.app.json).`);
+  }
+  const configText = configFile.toString('utf-8');
+  let config: any;
+  try {
+    config = JSON.parse(configText);
+  } catch {
+    throw new Error(`Couldn't parse project config (tsconfig.app.json).`);
+  }
+  return config;
+}
+
+
 // TODO(hans): change this any to unknown when google3 supports TypeScript 3.0.
 // tslint:disable-next-line:no-any
 export function isWorkspaceSchema(workspace: any): workspace is WorkspaceSchema {
@@ -45,4 +65,13 @@ export function isWorkspaceSchema(workspace: any): workspace is WorkspaceSchema 
 // tslint:disable-next-line:no-any
 export function isWorkspaceProject(project: any): project is WorkspaceProject {
   return !!(project && (project as WorkspaceProject).projectType);
+}
+
+export function isProjectUsingIvy(host: Tree, project: WorkspaceProject) {
+  const config = getProjectConfig(host, project);
+
+  if (config && config.angularCompilerOptions) {
+    return !!config.angularCompilerOptions.enableIvy;
+  }
+  return false;
 }
