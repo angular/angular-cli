a5dd1de64bfd98c40f8aae802b7bd7d7f4d78573
Minko Gechev <mgechev@google.com>
2019-04-04 01:32:34 +0200

    release: v8.0.0-beta.11

--- packages/schematics/angular/utility/latest-versions.ts
+++ packages/schematics/angular/utility/latest-versions.ts
@@ -8,7 +8,7 @@
 
 export const latestVersions = {
   // These versions should be kept up to date with latest Angular peer dependencies.
-  Angular: '~8.0.0-beta.10',
+  Angular: '~8.0.0-beta.11',
   RxJs: '~6.4.0',
   ZoneJs: '~0.9.0',
   TypeScript: '~3.3.3333',
