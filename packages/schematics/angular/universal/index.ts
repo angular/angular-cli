64c993aebb6328f55fe468efeadd6c479b3a2c22
Alan Agius <alan.agius4@gmail.com>
2019-04-04 10:55:48 +0200

    feat(@schematics/angular): stop adding `@angular/http` dependency when running universal schematic
    `@angular/platform-server` is no longer dependent on
    `@angular/http`
    
    More info: https://github.com/angular/angular/commit/9745f55

--- packages/schematics/angular/universal/index.ts
+++ packages/schematics/angular/universal/index.ts
@@ -208,12 +208,7 @@ function addDependencies(): Rule {
       ...coreDep,
       name: '@angular/platform-server',
     };
-    const httpDep = {
-      ...coreDep,
-      name: '@angular/http',
-    };
     addPackageJsonDependency(host, platformServerDep);
-    addPackageJsonDependency(host, httpDep);
 
     return host;
   };
