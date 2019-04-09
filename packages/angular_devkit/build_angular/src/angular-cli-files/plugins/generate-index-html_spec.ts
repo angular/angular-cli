dcfddfa838f2cea60f9fc00f759498a40708767a
Alan Agius <alan.agius@betssongroup.com>
2019-04-09 10:36:05 +0200

    feat(@angular-devkit/build-angular): improve handling of nomodules and modules in index generation
    Since when having differential loading we already know which files originated from which build. We shouldn't need to merge and transform this data.
    
    With this change, the index generator accepts a couple of new inputs.
    1. `files` - used for Js and CSS files which require nomodule nor module attributes
    2. `moduleFiles` - Js files that need to have a `module` attribute
    3. `noModuleFiles`  - Js files that need to have a `nomodule` attribute
    4. `entrypoints` - used to sort the insertion of files in the HTML file

--- packages/angular_devkit/build_angular/src/angular-cli-files/plugins/generate-index-html_spec.ts
+++ packages/angular_devkit/build_angular/src/angular-cli-files/plugins/generate-index-html_spec.ts
@@ -5,32 +5,92 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import { generateIndexHtml } from './generate-index-html';
+import { tags } from '@angular-devkit/core';
+import { FileInfo, GenerateIndexHtmlOptions, generateIndexHtml } from './generate-index-html';
 
 describe('index-html-webpack-plugin', () => {
+  const indexGeneratorOptions: GenerateIndexHtmlOptions = {
+    input: 'index.html',
+    inputContent: '<html><head></head><body></body></html>',
+    baseHref: '/',
+    sri: false,
+    files: [],
+    loadOutputFile: async (_fileName: string) => '',
+    entrypoints: ['polyfills', 'main', 'styles'],
+  };
 
-  it('can generate index.html', () => {
+  const oneLineHtml = (html: TemplateStringsArray) =>
+     tags.stripIndents`${html}`.replace(/(\>\s+)/g, '>');
 
+  it('can generate index.html', async () => {
     const source = generateIndexHtml({
-      input: 'index.html',
-      inputContent: '<html><head></head><body></body></html>',
-      baseHref: '/',
-      sri: false,
-      loadOutputFile: (fileName: string) => '',
-      unfilteredSortedFiles: [
-        {file: 'a.js', type: 'module'},
-        {file: 'b.js', type: 'nomodule'},
-        {file: 'c.js', type: 'none'},
+      ...indexGeneratorOptions,
+      files: [
+        { fileName: 'styles.css', extension: '.css', name: 'styles' },
+        { fileName: 'runtime.js', extension: '.js', name: 'main' },
+        { fileName: 'main.js', extension: '.js', name: 'main' },
+        { fileName: 'runtime.js', extension: '.js', name: 'polyfills' },
+        { fileName: 'polyfills.js', extension: '.js', name: 'polyfills' },
       ],
-      noModuleFiles: new Set<string>(),
     });
 
-    const html = source.source();
+    const html = (await source).source();
+    expect(html).toEqual(oneLineHtml`
+      <html>
+        <head><base href="/">
+          <link rel="stylesheet" href="styles.css">
+        </head>
+        <body>
+          <script src="runtime.js"></script>
+          <script src="polyfills.js"></script>
+          <script src="main.js"></script>
+        </body>
+      </html>
+    `);
+  });
 
-    expect(html).toContain('<script src="a.js" type="module"></script>');
-    expect(html).toContain('<script src="b.js" nomodule></script>');
-    expect(html).toContain('<script src="c.js"></script>');
+  it('should emit correct script tags when having module and non-module js', async () => {
+    const es2015JsFiles: FileInfo[] = [
+      { fileName: 'runtime-es2015.js', extension: '.js', name: 'main' },
+      { fileName: 'main-es2015.js', extension: '.js', name: 'main' },
+      { fileName: 'runtime-es2015.js', extension: '.js', name: 'polyfills' },
+      { fileName: 'polyfills-es2015.js', extension: '.js', name: 'polyfills' },
+    ];
 
+    const es5JsFiles: FileInfo[] = [
+      { fileName: 'runtime-es5.js', extension: '.js', name: 'main' },
+      { fileName: 'main-es5.js', extension: '.js', name: 'main' },
+      { fileName: 'runtime-es5.js', extension: '.js', name: 'polyfills' },
+      { fileName: 'polyfills-es5.js', extension: '.js', name: 'polyfills' },
+    ];
+
+    const source = generateIndexHtml({
+      ...indexGeneratorOptions,
+      files: [
+        { fileName: 'styles.css', extension: '.css', name: 'styles' },
+        { fileName: 'styles.css', extension: '.css', name: 'styles' },
+      ],
+      moduleFiles: es2015JsFiles,
+      noModuleFiles: es5JsFiles,
+    });
+
+    const html = (await source).source();
+    expect(html).toEqual(oneLineHtml`
+      <html>
+        <head>
+          <base href="/">
+          <link rel="stylesheet" href="styles.css">
+        </head>
+        <body>
+          <script src="runtime-es5.js" nomodule></script>
+          <script src="polyfills-es5.js" nomodule></script>
+          <script src="runtime-es2015.js" type="module"></script>
+          <script src="polyfills-es2015.js" type="module"></script>
+          <script src="main-es5.js" nomodule></script>
+          <script src="main-es2015.js" type="module"></script>
+        </body>
+      </html>
+    `);
   });
 
 });
