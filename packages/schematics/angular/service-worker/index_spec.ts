8e70f18e0ca94ee0553f3e3b4d7e005520f31860
Charles Lyding <19598772+clydin@users.noreply.github.com>
2019-04-15 22:41:11 -0400

    refactor(@schematics/angular): update service-worker to use new workspace rules

--- packages/schematics/angular/service-worker/index_spec.ts
+++ packages/schematics/angular/service-worker/index_spec.ts
@@ -39,13 +39,15 @@ describe('Service Worker Schematic', () => {
     skipPackageJson: false,
   };
 
-  beforeEach(() => {
+  beforeEach(async () => {
     appTree = schematicRunner.runSchematic('workspace', workspaceOptions);
-    appTree = schematicRunner.runSchematic('application', appOptions, appTree);
+    appTree = await schematicRunner.runSchematicAsync('application', appOptions, appTree)
+      .toPromise();
   });
 
-  it('should update the production configuration', () => {
-    const tree = schematicRunner.runSchematic('service-worker', defaultOptions, appTree);
+  it('should update the production configuration', async () => {
+    const tree = await schematicRunner.runSchematicAsync('service-worker', defaultOptions, appTree)
+      .toPromise();
     const configText = tree.readContent('/angular.json');
     const config = JSON.parse(configText);
     const swFlag = config.projects.bar.architect
@@ -53,9 +55,10 @@ describe('Service Worker Schematic', () => {
     expect(swFlag).toEqual(true);
   });
 
-  it('should update the target options if no configuration is set', () => {
+  it('should update the target options if no configuration is set', async () => {
     const options = { ...defaultOptions, configuration: '' };
-    const tree = schematicRunner.runSchematic('service-worker', options, appTree);
+    const tree = await schematicRunner.runSchematicAsync('service-worker', options, appTree)
+      .toPromise();
     const configText = tree.readContent('/angular.json');
     const config = JSON.parse(configText);
     const swFlag = config.projects.bar.architect
@@ -63,36 +66,41 @@ describe('Service Worker Schematic', () => {
     expect(swFlag).toEqual(true);
   });
 
-  it('should add the necessary dependency', () => {
-    const tree = schematicRunner.runSchematic('service-worker', defaultOptions, appTree);
+  it('should add the necessary dependency', async () => {
+    const tree = await schematicRunner.runSchematicAsync('service-worker', defaultOptions, appTree)
+      .toPromise();
     const pkgText = tree.readContent('/package.json');
     const pkg = JSON.parse(pkgText);
     const version = pkg.dependencies['@angular/core'];
     expect(pkg.dependencies['@angular/service-worker']).toEqual(version);
   });
 
-  it('should import ServiceWorkerModule', () => {
-    const tree = schematicRunner.runSchematic('service-worker', defaultOptions, appTree);
+  it('should import ServiceWorkerModule', async () => {
+    const tree = await schematicRunner.runSchematicAsync('service-worker', defaultOptions, appTree)
+      .toPromise();
     const pkgText = tree.readContent('/projects/bar/src/app/app.module.ts');
     expect(pkgText).toMatch(/import \{ ServiceWorkerModule \} from '@angular\/service-worker'/);
   });
 
-  it('should import environment', () => {
-    const tree = schematicRunner.runSchematic('service-worker', defaultOptions, appTree);
+  it('should import environment', async () => {
+    const tree = await schematicRunner.runSchematicAsync('service-worker', defaultOptions, appTree)
+      .toPromise();
     const pkgText = tree.readContent('/projects/bar/src/app/app.module.ts');
     expect(pkgText).toMatch(/import \{ environment \} from '\.\.\/environments\/environment'/);
   });
 
-  it('should add the SW import to the NgModule imports', () => {
-    const tree = schematicRunner.runSchematic('service-worker', defaultOptions, appTree);
+  it('should add the SW import to the NgModule imports', async () => {
+    const tree = await schematicRunner.runSchematicAsync('service-worker', defaultOptions, appTree)
+      .toPromise();
     const pkgText = tree.readContent('/projects/bar/src/app/app.module.ts');
     // tslint:disable-next-line:max-line-length
     const expectedText = 'ServiceWorkerModule.register(\'ngsw-worker.js\', { enabled: environment.production })';
     expect(pkgText).toContain(expectedText);
   });
 
-  it('should put the ngsw-config.json file in the project root', () => {
-    const tree = schematicRunner.runSchematic('service-worker', defaultOptions, appTree);
+  it('should put the ngsw-config.json file in the project root', async () => {
+    const tree = await schematicRunner.runSchematicAsync('service-worker', defaultOptions, appTree)
+      .toPromise();
     const path = '/projects/bar/ngsw-config.json';
     expect(tree.exists(path)).toEqual(true);
 
@@ -101,7 +109,7 @@ describe('Service Worker Schematic', () => {
       .toBe('projects/bar/ngsw-config.json');
   });
 
-  it('should add $schema in ngsw-config.json with correct relative path', () => {
+  it('should add $schema in ngsw-config.json with correct relative path', async () => {
     const pathToNgswConfigSchema = 'node_modules/@angular/service-worker/config/schema.json';
 
     const name = 'foo';
@@ -114,38 +122,52 @@ describe('Service Worker Schematic', () => {
       ...defaultOptions,
       project: name,
     };
-    const rootAppTree = schematicRunner.runSchematic('application', rootAppOptions, appTree);
-    const treeInRoot = schematicRunner.runSchematic('service-worker', rootSWOptions, rootAppTree);
+    const rootAppTree = await schematicRunner.runSchematicAsync(
+      'application',
+      rootAppOptions,
+      appTree,
+    ).toPromise();
+    const treeInRoot = await schematicRunner.runSchematicAsync(
+      'service-worker',
+      rootSWOptions,
+      rootAppTree,
+    ).toPromise();
     const pkgTextInRoot = treeInRoot.readContent('/ngsw-config.json');
     const configInRoot = JSON.parse(pkgTextInRoot);
     expect(configInRoot.$schema).toBe(`./${pathToNgswConfigSchema}`);
 
-    const treeNotInRoot = schematicRunner.runSchematic('service-worker', defaultOptions, appTree);
+    const treeNotInRoot = await schematicRunner.runSchematicAsync(
+      'service-worker',
+      defaultOptions,
+      appTree,
+    ).toPromise();
     const pkgTextNotInRoot = treeNotInRoot.readContent('/projects/bar/ngsw-config.json');
     const configNotInRoot = JSON.parse(pkgTextNotInRoot);
     expect(configNotInRoot.$schema).toBe(`../../${pathToNgswConfigSchema}`);
   });
 
-  it('should add root assets RegExp', () => {
-    const tree = schematicRunner.runSchematic('service-worker', defaultOptions, appTree);
+  it('should add root assets RegExp', async () => {
+    const tree = await schematicRunner.runSchematicAsync('service-worker', defaultOptions, appTree)
+      .toPromise();
     const pkgText = tree.readContent('/projects/bar/ngsw-config.json');
     const config = JSON.parse(pkgText);
     expect(config.assetGroups[1].resources.files)
       .toContain('/*.(eot|svg|cur|jpg|png|webp|gif|otf|ttf|woff|woff2|ani)');
   });
 
-  it('should add resourcesOutputPath to root assets when specified', () => {
+  it('should add resourcesOutputPath to root assets when specified', async () => {
     const config = JSON.parse(appTree.readContent('/angular.json'));
     config.projects.bar.architect.build.configurations.production.resourcesOutputPath = 'outDir';
     appTree.overwrite('/angular.json', JSON.stringify(config));
-    const tree = schematicRunner.runSchematic('service-worker', defaultOptions, appTree);
+    const tree = await schematicRunner.runSchematicAsync('service-worker', defaultOptions, appTree)
+      .toPromise();
     const pkgText = tree.readContent('/projects/bar/ngsw-config.json');
     const ngswConfig = JSON.parse(pkgText);
     expect(ngswConfig.assetGroups[1].resources.files)
       .toContain('/outDir/*.(eot|svg|cur|jpg|png|webp|gif|otf|ttf|woff|woff2|ani)');
   });
 
-  it('should generate ngsw-config.json in root when the application is at root level', () => {
+  it('should generate ngsw-config.json in root when the application is at root level', async () => {
     const name = 'foo';
     const rootAppOptions: ApplicationOptions = {
       ...appOptions,
@@ -157,8 +179,10 @@ describe('Service Worker Schematic', () => {
       project: name,
     };
 
-    let tree = schematicRunner.runSchematic('application', rootAppOptions, appTree);
-    tree = schematicRunner.runSchematic('service-worker', rootSWOptions, tree);
+    let tree = await schematicRunner.runSchematicAsync('application', rootAppOptions, appTree)
+      .toPromise();
+    tree = await schematicRunner.runSchematicAsync('service-worker', rootSWOptions, tree)
+      .toPromise();
     expect(tree.exists('/ngsw-config.json')).toBe(true);
 
     const { projects } = JSON.parse(tree.readContent('/angular.json'));
