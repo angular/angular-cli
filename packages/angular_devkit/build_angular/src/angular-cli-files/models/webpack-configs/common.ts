1af164b39ecc902c3c4816ce009fbb93b189b313
Alan Agius <alan.agius4@gmail.com>
2019-04-17 08:05:16 +0200

    refactor: rename es5 polyfills entrypoint name to polyfills-es5

--- packages/angular_devkit/build_angular/src/angular-cli-files/models/webpack-configs/common.ts
+++ packages/angular_devkit/build_angular/src/angular-cli-files/models/webpack-configs/common.ts
@@ -83,9 +83,9 @@ export function getCommonConfig(wco: WebpackConfigOptions): Configuration {
     // For NON differential loading we want to have 2 polyfill bundles
     if (buildOptions.es5BrowserSupport
       || (buildOptions.es5BrowserSupport === undefined && isEs5SupportNeeded(projectRoot))) {
-      entryPoints['polyfills.es5'] = [es5Polyfills];
+      entryPoints['polyfills-es5'] = [es5Polyfills];
       if (!buildOptions.aot) {
-        entryPoints['polyfills.es5'].push(es5JitPolyfills);
+        entryPoints['polyfills-es5'].push(es5JitPolyfills);
       }
     }
   }
