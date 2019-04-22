f53d5eca42476836b98083bdad32337b1d754ab3
Filipe Silva <filipematossilva@gmail.com>
2019-04-22 12:54:08 +0100

    test: add 7.0 update test

---
+++ tests/legacy-cli/e2e/assets/7.0-project/e2e/src/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getTitleText() {
+    return element(by.css('app-root h1')).getText();
+  }
+}
--- tests/legacy-cli/e2e/tests/basic/update-1.7-longhand.ts
+++ tests/legacy-cli/e2e/tests/basic/update-1.7-longhand.ts
@@ -1,6 +1,6 @@
 import { createProjectFromAsset } from '../../utils/assets';
 import { ng, silentNpm } from '../../utils/process';
-import { isPrereleaseCli, removeHttpDep, useBuiltPackages } from '../../utils/project';
+import { isPrereleaseCli, useBuiltPackages } from '../../utils/project';
 import { expectToFail } from '../../utils/utils';
 
 
@@ -12,7 +12,6 @@ export default async function () {
   await ng('update', '@angular/cli', '--migrate-only', '--from=1.7.1');
   await useBuiltPackages();
   await silentNpm('install');
-  await removeHttpDep();
   await ng('update', '@angular/core', ...extraUpdateArgs);
   await ng('build');
 }
