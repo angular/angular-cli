2916f70a81eb1551a83254e6a721c4a22560d43f
Alan <alan.agius@betssongroup.com>
2019-04-30 08:42:28 +0200

    fix(@angular-devkit/build-angular): e2e does not respect dev-server host and port settings
    Fixes #14151

--- packages/angular_devkit/build_angular/src/protractor/index.ts
+++ packages/angular_devkit/build_angular/src/protractor/index.ts
@@ -105,10 +105,21 @@ async function execute(
     const serverOptions = await context.getTargetOptions(target);
 
     const overrides: Record<string, string | number | boolean> = { watch: false };
-    if (options.host !== undefined) { overrides.host = options.host; }
-    if (options.port !== undefined) { overrides.port = options.port; }
-    server = await context.scheduleTarget(target, overrides);
+    if (options.host !== undefined) {
+      overrides.host = options.host;
+    } else if (typeof serverOptions.host === 'string') {
+      options.host = serverOptions.host;
+    } else {
+      options.host = overrides.host = 'localhost';
+    }
 
+    if (options.port !== undefined) {
+      overrides.port = options.port;
+    } else if (typeof serverOptions.port === 'number') {
+      options.port = serverOptions.port;
+    }
+
+    server = await context.scheduleTarget(target, overrides);
     let result;
     try {
       result = await server.result;
