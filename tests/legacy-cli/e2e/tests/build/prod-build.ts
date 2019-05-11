a7e274d2b850797d5cff175743a71ad1bfeec68e
Stefanie Fluin <sjtrimble@gmail.com>
2019-05-10 20:29:12 -0700

    feat(@schematics/angular): cli app redesign

--- tests/legacy-cli/e2e/tests/build/prod-build.ts
+++ tests/legacy-cli/e2e/tests/build/prod-build.ts
@@ -54,11 +54,11 @@ export default async function () {
 
   // Size checks in bytes
   if (ivyProject) {
-    verifySize(mainES5Path, 147789);
-    verifySize(mainES2015Path, 130153);
+    verifySize(mainES5Path, 167355);
+    verifySize(mainES2015Path, 149806);
   } else {
-    verifySize(mainES5Path, 155523);
-    verifySize(mainES2015Path, 135394);
+    verifySize(mainES5Path, 184470);
+    verifySize(mainES2015Path, 163627);
   }
 
   // Check that the process didn't change local files.
