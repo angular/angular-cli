7d6b50238761a6328ca9630a654e3d85be4b3cf5
Charles Lyding <19598772+clydin@users.noreply.github.com>
2019-05-12 19:43:10 -0400

    fix(@angular-devkit/build-optimizer): remove deprecated test functions from public API

--- packages/angular_devkit/build_optimizer/src/index.ts
+++ packages/angular_devkit/build_optimizer/src/index.ts
@@ -11,12 +11,11 @@ export { buildOptimizer } from './build-optimizer/build-optimizer';
 export { transformJavascript } from './helpers/transform-javascript';
 
 export { getFoldFileTransformer } from './transforms/class-fold';
-export { getImportTslibTransformer, testImportTslib } from './transforms/import-tslib';
-export { getPrefixClassesTransformer, testPrefixClasses } from './transforms/prefix-classes';
+export { getImportTslibTransformer } from './transforms/import-tslib';
+export { getPrefixClassesTransformer } from './transforms/prefix-classes';
 export { getPrefixFunctionsTransformer } from './transforms/prefix-functions';
 export {
   getScrubFileTransformer,
   getScrubFileTransformerForCore,
-  testScrubFile,
 } from './transforms/scrub-file';
 export { getWrapEnumsTransformer } from './transforms/wrap-enums';
