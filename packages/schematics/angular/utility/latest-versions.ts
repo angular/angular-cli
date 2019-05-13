bfed4302ba536149e9d9c86337d3ea076fcc7bb8
Filipe Silva <filipematossilva@gmail.com>
2019-05-13 19:43:54 +0100

    fix(@schematics/angular): update zone.js to 0.9.1

--- packages/schematics/angular/utility/latest-versions.ts
+++ packages/schematics/angular/utility/latest-versions.ts
@@ -10,7 +10,7 @@ export const latestVersions = {
   // These versions should be kept up to date with latest Angular peer dependencies.
   Angular: '~8.0.0-beta.14',
   RxJs: '~6.4.0',
-  ZoneJs: '~0.9.0',
+  ZoneJs: '~0.9.1',
   TypeScript: '~3.4.3',
   TsLib: '^1.9.0',
   // The versions below must be manually updated when making a new devkit release.
