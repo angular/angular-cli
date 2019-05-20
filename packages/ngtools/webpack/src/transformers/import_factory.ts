00253f9f2343eeaf7f6dd88456b9da3997e17170
Alex Eagle <alexeagle@angular.io>
2019-05-20 11:27:22 -0700

    docs: don't reference next.angular.io
    When we cut a release, this moves to angular.io.
    Use v8.angular.io in places where angular.io is currently a 404.
    
    Fixes https://github.com/angular/angular/issues/30407

--- packages/ngtools/webpack/src/transformers/import_factory.ts
+++ packages/ngtools/webpack/src/transformers/import_factory.ts
@@ -66,7 +66,7 @@ loadChildren: () => import('IMPORT_STRING').then(M => M.EXPORT_NAME)
 
 Please note that only IMPORT_STRING, M, and EXPORT_NAME can be replaced in this format.
 
-Visit https://next.angular.io/guide/ivy for more information on using Ivy.
+Visit https://v8.angular.io/guide/ivy for more information on using Ivy.
 `;
 
       const emitWarning = () => warningCb(warning);
