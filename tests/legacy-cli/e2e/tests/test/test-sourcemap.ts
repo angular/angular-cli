379bdd922ef5341f4fa0b8a64853c572e5db9c59
Alan Agius <alan.agius4@gmail.com>
2019-05-20 14:19:30 +0200

    fix(@angular-devkit/build-angular): normalize sourceMap options in karma webpack plugin
    `sourceMap` option can be either a boolean or an object,we need to normalize it before trying to get the `script` value.
    
    Fixes #14457

--- tests/legacy-cli/e2e/tests/test/test-sourcemap.ts
+++ tests/legacy-cli/e2e/tests/test/test-sourcemap.ts
@@ -1,8 +1,14 @@
 import { writeFile } from '../../utils/fs';
-import { execAndWaitForOutputToMatch, killAllProcesses } from '../../utils/process';
+import { ng } from '../../utils/process';
 import { updateJsonFile } from '../../utils/project';
 
 export default async function () {
+  await writeFile('src/app/app.component.spec.ts', `
+      it('show fail', () => {
+        expect(undefined).toBeTruthy();
+      });
+    `);
+
   await updateJsonFile('angular.json', configJson => {
     const appArchitect = configJson.projects['test-project'].architect;
     appArchitect.test.options.sourceMap = {
@@ -10,20 +16,43 @@ export default async function () {
     };
   });
 
-  await writeFile('src/app/app.component.spec.ts', `
-      it('show fail', () => {
-        expect(undefined).toBeTruthy();
-      });
-    `);
-
-  // when sourcemaps are not working the stacktrace won't point to the spec.ts file.
+  // when sourcemaps are 'on' the stacktrace will point to the spec.ts file.
   try {
-    await execAndWaitForOutputToMatch(
-      'ng',
-      ['test', '--watch', 'false'],
-      /app\.component\.spec\.ts/,
-    );
-  } finally {
-    killAllProcesses();
+    await ng('test', '--watch', 'false');
+    throw new Error('ng test should have failed.');
+  } catch (error) {
+    if (!error.message.includes('app.component.spec.ts')) {
+      throw error;
+    };
+  }
+
+  await updateJsonFile('angular.json', configJson => {
+    const appArchitect = configJson.projects['test-project'].architect;
+    appArchitect.test.options.sourceMap = true;
+  });
+
+  // when sourcemaps are 'on' the stacktrace will point to the spec.ts file.
+  try {
+    await ng('test', '--watch', 'false');
+    throw new Error('ng test should have failed.');
+  } catch (error) {
+    if (!error.message.includes('app.component.spec.ts')) {
+      throw error;
+    };
+  }
+
+  await updateJsonFile('angular.json', configJson => {
+    const appArchitect = configJson.projects['test-project'].architect;
+    appArchitect.test.options.sourceMap = false;
+  });
+
+  // when sourcemaps are 'off' the stacktrace won't point to the spec.ts file.
+  try {
+    await ng('test', '--watch', 'false');
+    throw new Error('ng test should have failed.');
+  } catch (error) {
+    if (!error.message.includes('main.js')) {
+      throw error;
+    };
   }
 }
