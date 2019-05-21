57189f6eca9447ae5723a8f5d04044ee43ece94f
Charles Lyding <19598772+clydin@users.noreply.github.com>
2019-05-21 16:28:49 -0400

    fix(@angular/cli): standardize TTY checks

---
+++ packages/angular/cli/utilities/tty.ts
@@ -0,0 +1,15 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+export function isTTY(): boolean {
+  const force = process.env['NG_FORCE_TTY'];
+  if (force !== undefined) {
+    return !(force === '0' || force.toUpperCase() === 'FALSE');
+  }
+
+  return !!process.stdout.isTTY && !!process.stdin.isTTY;
+}
