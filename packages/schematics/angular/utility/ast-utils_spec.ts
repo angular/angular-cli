22d77285e1ec01ffefdb50199fcddee1779dc8de
Alan <alan.agius@betssongroup.com>
2019-05-22 14:56:56 +0200

    fix(@schematics/angular): handle newline after `@` of a decorator
    Fixes #14490

--- packages/schematics/angular/utility/ast-utils_spec.ts
+++ packages/schematics/angular/utility/ast-utils_spec.ts
@@ -169,6 +169,22 @@ describe('ast utils', () => {
     expect(output).toMatch(/imports: \[HelloWorld],\r?\n/m);
   });
 
+  it(`should handle NgModule with newline after '@'`, () => {
+    const moduleContent = `
+      import { BrowserModule } from '@angular/platform-browser';
+      import { NgModule } from '@angular/core';
+
+      @
+      NgModule({imports: [BrowserModule], declarations: []})
+      export class AppModule { }
+    `;
+    const source = getTsSource(modulePath, moduleContent);
+    const changes = addExportToModule(source, modulePath, 'FooComponent', './foo.component');
+    const output = applyChanges(modulePath, moduleContent, changes);
+    expect(output).toMatch(/import { FooComponent } from '.\/foo.component';/);
+    expect(output).toMatch(/exports: \[FooComponent\]/);
+  });
+
   it('should handle NgModule with no newlines', () => {
     const moduleContent = `
       import { BrowserModule } from '@angular/platform-browser';
