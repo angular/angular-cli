413893e1ce2f63daf0fb4e9cfc5c87451a714026
Charles Lyding <19598772+clydin@users.noreply.github.com>
2019-05-29 19:46:06 -0400

    fix(@angular/cli): check for CI environment variable in tty helper
    Fixes #14563

--- packages/angular/cli/utilities/tty.ts
+++ packages/angular/cli/utilities/tty.ts
@@ -11,5 +11,5 @@ export function isTTY(): boolean {
     return !(force === '0' || force.toUpperCase() === 'FALSE');
   }
 
-  return !!process.stdout.isTTY && !!process.stdin.isTTY;
+  return !!process.stdout.isTTY && !!process.stdin.isTTY && !('CI' in process.env);
 }
