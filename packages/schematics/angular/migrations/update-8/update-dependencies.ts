c0a366d55f9a8ed98f5a284e955c3d4ce396291d
Alan Agius <alan.agius4@gmail.com>
2019-05-30 22:00:13 +0200

    fix(@schematics/angular): update `ng-packagr` and `tsickle` when migrating to version 8

--- packages/schematics/angular/migrations/update-8/update-dependencies.ts
+++ packages/schematics/angular/migrations/update-8/update-dependencies.ts
@@ -11,55 +11,29 @@ import { latestVersions } from '../../utility/latest-versions';
 
 export function updateDependencies() {
   return (host: Tree) => {
-    let current = getPackageJsonDependency(host, '@angular-devkit/build-angular');
-    if (current && current.version !== latestVersions.DevkitBuildAngular) {
-      addPackageJsonDependency(
-        host,
-        {
-          type: current.type,
-          name: '@angular-devkit/build-angular',
-          version: latestVersions.DevkitBuildAngular,
-          overwrite: true,
-        },
-      );
-    }
 
-    current = getPackageJsonDependency(host, '@angular-devkit/build-ng-packagr');
-    if (current && current.version !== latestVersions.DevkitBuildNgPackagr) {
-      addPackageJsonDependency(
-        host,
-        {
-          type: current.type,
-          name: '@angular-devkit/build-ng-packagr',
-          version: latestVersions.DevkitBuildNgPackagr,
-          overwrite: true,
-        },
-      );
-    }
+    const dependenciesToUpdate: Record<string, string> = {
+      '@angular-devkit/build-angular': latestVersions.DevkitBuildAngular,
+      '@angular-devkit/build-ng-packagr': latestVersions.DevkitBuildNgPackagr,
+      'zone.js': latestVersions.ZoneJs,
+      'tsickle': latestVersions.tsickle,
+      'ng-packagr': latestVersions.ngPackagr,
+      // FIXME: change to ^2.3.2 as soon as it's released with the pr208 fix
+      'web-animations-js': 'github:angular/web-animations-js#release_pr208',
+    };
 
-    current = getPackageJsonDependency(host, 'zone.js');
-    if (current && current.version !== latestVersions.ZoneJs) {
-      addPackageJsonDependency(
-        host,
-        {
-          type: current.type,
-          name: 'zone.js',
-          version: latestVersions.ZoneJs,
-          overwrite: true,
-        },
-      );
-    }
+    for (const [name, version] of Object.entries(dependenciesToUpdate)) {
+      const current = getPackageJsonDependency(host, name);
+      if (!current || current.version === version) {
+        continue;
+      }
 
-    // FIXME: change to ^2.3.2 as soon as it's released with the pr208 fix
-    const webAnimationsJsVersion = 'github:angular/web-animations-js#release_pr208';
-    current = getPackageJsonDependency(host, 'web-animations-js');
-    if (current && current.version !== webAnimationsJsVersion) {
       addPackageJsonDependency(
         host,
         {
           type: current.type,
-          name: 'web-animations-js',
-          version: webAnimationsJsVersion,
+          name,
+          version,
           overwrite: true,
         },
       );
