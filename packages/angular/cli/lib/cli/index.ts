a70932b1acf97d20cc04cb7c875cdddd2fe11637
Charles Lyding <19598772+clydin@users.noreply.github.com>
2019-06-05 19:48:41 -0400

    refactor(@angular/cli): standardize color handling and support checking
    Node.js 10+ provides built-in functionality to test for color support based on chalk's `supports-color` package as well as several others. This alleviates the need for custom code or third-party packages to determine color support.  In addition for this PR, the `ansi-colors` package is added to the CLI which provides color, cross-platform symbol, and style/color removal support.  The package is light-weight, contains typings, and has no dependencies. The removal support is leveraged to remove all styling from logger messages when color is not supported.  This removes a current defect in which color/styling is still displayed if generated manually or via methods that do not perform supportability checks.  Finally, the typically used console functions are overriden to leverage the logger to ensure that the color processing is applied to third-party code (e.g., webpack internally as well as some of its loaders and plugins) which output to the console directly.

--- packages/angular/cli/lib/cli/index.ts
+++ packages/angular/cli/lib/cli/index.ts
@@ -5,24 +5,37 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import { terminal } from '@angular-devkit/core';
 import { createConsoleLogger } from '@angular-devkit/core/node';
+import * as colors from 'ansi-colors';
+import { WriteStream } from 'tty';
+import { format } from 'util';
 import { runCommand } from '../../models/command-runner';
 import { getWorkspaceRaw } from '../../utilities/config';
 import { getWorkspaceDetails } from '../../utilities/project';
 
-
 export default async function(options: { testing?: boolean, cliArgs: string[] }) {
+  // Typings do not contain the function call (added in Node.js v9.9.0)
+  const supportsColor = process.stdout instanceof WriteStream &&
+    (process.stdout as unknown as { getColorDepth(): number }).getColorDepth() > 1;
+
   const logger = createConsoleLogger(
     false,
     process.stdout,
     process.stderr,
     {
-      warn: s => terminal.bold(terminal.yellow(s)),
-      error: s => terminal.bold(terminal.red(s)),
-      fatal: s => terminal.bold(terminal.red(s)),
+      info: s => supportsColor ? s : colors.unstyle(s),
+      debug: s => supportsColor ? s : colors.unstyle(s),
+      warn: s => supportsColor ? colors.bold.yellow(s) : colors.unstyle(s),
+      error: s => supportsColor ? colors.bold.red(s) : colors.unstyle(s),
+      fatal: s => supportsColor ? colors.bold.red(s) : colors.unstyle(s),
     },
   );
+
+  // Redirect console to logger
+  console.log = function() { logger.info(format.apply(null, arguments)); };
+  console.info = function() { logger.info(format.apply(null, arguments)); };
+  console.warn = function() { logger.warn(format.apply(null, arguments)); };
+  console.error = function() { logger.error(format.apply(null, arguments)); };
 
   let projectDetails = getWorkspaceDetails();
   if (projectDetails === null) {
