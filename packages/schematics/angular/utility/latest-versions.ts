905f3acae80fa7847b2f5098a26958c38246d3ac
Alan Agius <alan.agius4@gmail.com>
2019-06-10 20:05:28 +0200

    build: update angular framework packages to next`

--- packages/schematics/angular/utility/latest-versions.ts
+++ packages/schematics/angular/utility/latest-versions.ts
@@ -8,7 +8,7 @@
 
 export const latestVersions = {
   // These versions should be kept up to date with latest Angular peer dependencies.
-  Angular: '~8.0.0',
+  Angular: '~8.1.0-next.1',
   RxJs: '~6.4.0',
   ZoneJs: '~0.9.1',
   TypeScript: '~3.4.3',
