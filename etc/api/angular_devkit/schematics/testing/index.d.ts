1c018b91784a8ab74e06ff88271b88e905b9cafe
ukrukarg <ukrukar@google.com>
2019-06-19 14:51:52 -0400

    feat(@angular-devkit/schematics): Add registerCollection to SchematicsTestRunner
    Fixes #14833

--- etc/api/angular_devkit/schematics/testing/index.d.ts
+++ etc/api/angular_devkit/schematics/testing/index.d.ts
@@ -4,6 +4,7 @@ export declare class SchematicTestRunner {
     readonly tasks: TaskConfiguration[];
     constructor(_collectionName: string, collectionPath: string);
     callRule(rule: Rule, tree: Tree, parentContext?: Partial<SchematicContext>): Observable<Tree>;
+    registerCollection(collectionName: string, collectionPath: string): void;
     runExternalSchematic<SchematicSchemaT>(collectionName: string, schematicName: string, opts?: SchematicSchemaT, tree?: Tree): UnitTestTree;
     runExternalSchematicAsync<SchematicSchemaT>(collectionName: string, schematicName: string, opts?: SchematicSchemaT, tree?: Tree): Observable<UnitTestTree>;
     runSchematic<SchematicSchemaT>(schematicName: string, opts?: SchematicSchemaT, tree?: Tree): UnitTestTree;
