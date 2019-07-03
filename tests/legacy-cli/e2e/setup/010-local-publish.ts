3500e5c5d1c6808dbea13c3528e8ec9d0056f2cd
Charles Lyding <19598772+clydin@users.noreply.github.com>
2019-07-02 20:07:01 -0400

    test: use registry option for E2E local package repository publish

--- tests/legacy-cli/e2e/setup/010-local-publish.ts
+++ tests/legacy-cli/e2e/setup/010-local-publish.ts
@@ -1,30 +1,22 @@
-import * as fs from 'fs';
 import { prerelease } from 'semver';
 import { packages } from '../../../../lib/packages';
 import { npm } from '../utils/process';
 
 export default async function() {
+  const publishArgs = [
+    'run',
+    'admin',
+    '--',
+    'publish',
+    '--versionCheck=false',
+    '--branchCheck=false',
+    '--registry=http://localhost:4873',
+  ];
+
   const pre = prerelease(packages['@angular/cli'].version);
-
-  fs.writeFileSync('.npmrc', 'registry = http://localhost:4873', 'utf8');
-
-  try {
-    const publishArgs = [
-      'run',
-      'admin',
-      '--',
-      'publish',
-      '--versionCheck',
-      'false',
-      '--branchCheck',
-      'false',
-    ];
-    if (pre && pre.length > 0) {
-      publishArgs.push('--tag');
-      publishArgs.push('next');
-    }
-    await npm(...publishArgs);
-  } finally {
-    fs.unlinkSync('.npmrc');
+  if (pre && pre.length > 0) {
+    publishArgs.push('--tag', 'next');
   }
+
+  await npm(...publishArgs);
 }
