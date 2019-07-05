445f3873b9df51deda708c6b647c8b9e1ac4254f
Charles Lyding <19598772+clydin@users.noreply.github.com>
2019-07-05 19:05:08 -0400

    fix(@angular-devkit/build-angular): use query option to set sockJS path in dev-server
    Fixes #15002

--- packages/angular_devkit/build_angular/src/dev-server/index.ts
+++ packages/angular_devkit/build_angular/src/dev-server/index.ts
@@ -417,11 +417,13 @@ function _addLiveReload(
 
   // If a custom path is provided the webpack dev server client drops the sockjs-node segment.
   // This adds it back so that behavior is consistent when using a custom URL path
+  let sockjsPath = '';
   if (clientAddress.pathname) {
-    clientAddress.pathname = path.posix.join(clientAddress.pathname, 'sockjs-node');
+      clientAddress.pathname = path.posix.join(clientAddress.pathname, 'sockjs-node');
+      sockjsPath = '&sockPath=' + clientAddress.pathname;
   }
 
-  const entryPoints = [`${webpackDevServerPath}?${url.format(clientAddress)}`];
+  const entryPoints = [`${webpackDevServerPath}?${url.format(clientAddress)}${sockjsPath}`];
   if (options.hmr) {
     const webpackHmrLink = 'https://webpack.js.org/guides/hot-module-replacement';
 
