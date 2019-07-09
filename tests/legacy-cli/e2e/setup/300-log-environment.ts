bc1720e05842d77eeb235a800c1ea6a3e2f86345
Charles Lyding <19598772+clydin@users.noreply.github.com>
2019-07-09 13:28:30 -0400

    test: control E2E environment logging

--- tests/legacy-cli/e2e/setup/300-log-environment.ts
+++ tests/legacy-cli/e2e/setup/300-log-environment.ts
@@ -4,6 +4,12 @@ export default async function() {
   console.log('Environment:');
 
   Object.keys(process.env).forEach(envName => {
+    // CI Logs should not contain environment variables that are considered secret
+    const lowerName = envName.toLowerCase();
+    if (lowerName.includes('key') || lowerName.includes('secret')) {
+      return;
+    }
+
     console.log(`  ${envName}: ${process.env[envName].replace(/[\n\r]+/g, '\n        ')}`);
   });
 
