e2d79c622ed9392f4964c8c750347a2f303d30df
Alan Agius <alan.agius4@gmail.com>
2019-07-29 12:40:06 +0200

    build: add ng packagr test large for ivy

---
+++ tests/angular_devkit/build_ng_packagr/ng-packaged-ivy/projects/lib/src/lib/lib.module.ts
@@ -0,0 +1,18 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { NgModule } from '@angular/core';
+import { LibComponent } from './lib.component';
+import { LibService } from './lib.service';
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [LibComponent],
+  providers: [LibService]
+})
+export class LibModule { }
