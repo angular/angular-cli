168f07e9d033215a376b4b3d9fce6eecf44536fe
vikerman <viks@google.com>
2019-07-29 12:11:01 -0700

    release: v8.2.0-rc.0

--- packages/schematics/angular/utility/latest-versions.ts
+++ packages/schematics/angular/utility/latest-versions.ts
@@ -15,10 +15,10 @@ export const latestVersions = {
   TsLib: '^1.10.0',
 
   // The versions below must be manually updated when making a new devkit release.
-  DevkitBuildAngular: '~0.802.0-next.2',
-  DevkitBuildNgPackagr: '~0.802.0-next.2',
-  DevkitBuildWebpack: '~0.802.0-next.2',
-  AngularPWA: '~0.802.0-next.2',
+  DevkitBuildAngular: '~0.802.0-rc.0',
+  DevkitBuildNgPackagr: '~0.802.0-rc.0',
+  DevkitBuildWebpack: '~0.802.0-rc.0',
+  AngularPWA: '~0.802.0-rc.0',
 
   tsickle: '^0.36.0',
   ngPackagr: '^5.3.0',
