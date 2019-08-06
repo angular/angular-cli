9de282ca3209c3c8895dcfc1da36c30e4a05fc67
FG-33 <fg.95@gmx.de>
2019-08-06 22:13:51 +0200

    feat(@schematics/angular): add generator for interceptor
    Rework.

---
+++ packages/schematics/angular/interceptor/index.ts
@@ -0,0 +1,51 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { strings } from '@angular-devkit/core';
+import {
+  Rule,
+  Tree,
+  apply,
+  applyTemplates,
+  chain,
+  filter,
+  mergeWith,
+  move,
+  noop,
+  url,
+} from '@angular-devkit/schematics';
+import { applyLintFix } from '../utility/lint-fix';
+import { parseName } from '../utility/parse-name';
+import { createDefaultPath } from '../utility/workspace';
+import { Schema as InterceptorOptions } from './schema';
+
+export default function (options: InterceptorOptions): Rule {
+  return async (host: Tree) => {
+    if (options.path === undefined) {
+      options.path = await createDefaultPath(host, options.project as string);
+    }
+
+    const parsedPath = parseName(options.path, options.name);
+    options.name = parsedPath.name;
+    options.path = parsedPath.path;
+
+    const templateSource = apply(url('./files'), [
+      options.skipTests ? filter(path => !path.endsWith('.spec.ts.template')) : noop(),
+      applyTemplates({
+        ...strings,
+        'if-flat': (s: string) => options.flat ? '' : s,
+        ...options,
+      }),
+      move(parsedPath.path),
+    ]);
+
+    return chain([
+      mergeWith(templateSource),
+      options.lintFix ? applyLintFix(options.path) : noop(),
+    ]);
+  };
+}
