2383641b95f4d6a485dd4b1cb79525b07c2414fd
Filipe Silva <filipematossilva@gmail.com>
2019-08-20 15:14:06 +0100

    test: fix basic aot e2e to run with Ivy

--- tests/legacy-cli/e2e/tests/basic/aot.ts
+++ tests/legacy-cli/e2e/tests/basic/aot.ts
@@ -1,10 +1,19 @@
+import { getGlobalVariable } from '../../utils/env';
 import { expectFileToMatch } from '../../utils/fs';
 import { ng } from '../../utils/process';
 
 export default async function () {
   await ng('build', '--aot=true');
-  await expectFileToMatch('dist/test-project/main-es5.js',
-    /platformBrowser.*bootstrapModuleFactory.*AppModuleNgFactory/);
-  await expectFileToMatch('dist/test-project/main-es2015.js',
-    /platformBrowser.*bootstrapModuleFactory.*AppModuleNgFactory/);
+
+  if (getGlobalVariable('argv')['ve']) {
+    await expectFileToMatch('dist/test-project/main-es5.js',
+      /platformBrowser.*bootstrapModuleFactory.*AppModuleNgFactory/);
+    await expectFileToMatch('dist/test-project/main-es2015.js',
+      /platformBrowser.*bootstrapModuleFactory.*AppModuleNgFactory/);
+  } else {
+    await expectFileToMatch('dist/test-project/main-es5.js',
+      /platformBrowser.*bootstrapModule.*AppModule/);
+    await expectFileToMatch('dist/test-project/main-es2015.js',
+      /platformBrowser.*bootstrapModule.*AppModule/);
+  }
 }
