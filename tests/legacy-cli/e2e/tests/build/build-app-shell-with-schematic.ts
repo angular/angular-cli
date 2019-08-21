a219d495b83ae9f87a679107d6d89cccd2df09de
Alan Agius <alan.agius4@gmail.com>
2019-08-21 10:47:55 +0200

    test: enable app-shell e2e test

--- tests/legacy-cli/e2e/tests/build/build-app-shell-with-schematic.ts
+++ tests/legacy-cli/e2e/tests/build/build-app-shell-with-schematic.ts
@@ -1,37 +1,26 @@
-import { ng, npm } from '../../utils/process';
-import { expectFileToMatch, appendToFile } from '../../utils/fs';
 import { getGlobalVariable } from '../../utils/env';
-import { expectToFail } from '../../utils/utils';
+import { appendToFile, expectFileToMatch } from '../../utils/fs';
+import { ng, silentNpm } from '../../utils/process';
 import { updateJsonFile } from '../../utils/project';
 import { readNgVersion } from '../../utils/version';
 
 
-export default function () {
+export default async function () {
   // Skip this test in Angular 2/4.
   if (getGlobalVariable('argv').ng2 || getGlobalVariable('argv').ng4) {
-    return Promise.resolve();
+    return;
   }
 
-  let platformServerVersion = readNgVersion();
+  await appendToFile('src/app/app.component.html', '<router-outlet></router-outlet>');
+  await ng('generate', 'appShell', '--client-project', 'test-project');
+  await updateJsonFile('package.json', packageJson => {
+    const dependencies = packageJson['dependencies'];
+    dependencies['@angular/platform-server'] = getGlobalVariable('argv')['ng-snapshots']
+      ? 'github:angular/platform-server-builds'
+      : readNgVersion();
+  });
 
-  if (getGlobalVariable('argv')['ng-snapshots']) {
-    platformServerVersion = 'github:angular/platform-server-builds';
-  }
-
-
-  return Promise.resolve()
-    .then(() => expectToFail(() => {
-      return ng('generate', 'appShell', '--universal-app', 'universal');
-    })
-    .then(() => appendToFile('src/app/app.component.html', '<router-outlet></router-outlet>'))
-    .then(() => ng('generate', 'appShell', '--universal-app', 'universal'))
-    .then(() => updateJsonFile('package.json', packageJson => {
-      const dependencies = packageJson['dependencies'];
-      dependencies['@angular/platform-server'] = platformServerVersion;
-    })
-    .then(() => npm('install'))
-    .then(() => ng('build', '--optimization'))
-    .then(() => expectFileToMatch('dist/test-project/index.html', /app-shell works!/))
-    .then(() => ng('build', '--optimization', '--skip-app-shell'))
-    .then(() => expectToFail(() => expectFileToMatch('dist/test-project/index.html', /app-shell works!/)));
+  await silentNpm('install');
+  await ng('run', 'test-project:app-shell');
+  await expectFileToMatch('dist/test-project/index.html', /app-shell works!/);
 }
