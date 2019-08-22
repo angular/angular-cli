ad8acb0d2379007fd40306a159efa42a0152ef86
Alan Agius <alan.agius4@gmail.com>
2019-08-22 15:19:01 +0200

    build: bump angular dev packages to latest next versions

--- tests/legacy-cli/e2e/tests/packages/webpack/test-app.ts
+++ tests/legacy-cli/e2e/tests/packages/webpack/test-app.ts
@@ -1,25 +1,37 @@
-import {normalize} from 'path';
-import {createProjectFromAsset} from '../../../utils/assets';
-import {exec} from '../../../utils/process';
-import {expectFileSizeToBeUnder, replaceInFile, expectFileToMatch} from '../../../utils/fs';
+import { normalize } from 'path';
+import { createProjectFromAsset } from '../../../utils/assets';
+import { getGlobalVariable } from '../../../utils/env';
+import { expectFileSizeToBeUnder, expectFileToMatch, replaceInFile } from '../../../utils/fs';
+import { exec } from '../../../utils/process';
+import { updateJsonFile } from '../../../utils/project';
 
 
-export default function(skipCleaning: () => void) {
-  return Promise.resolve()
-    .then(() => createProjectFromAsset('webpack/test-app'))
-    .then(() => exec(normalize('node_modules/.bin/webpack-cli')))
-    // Note: these sizes are without Build Optimizer or any advanced optimizations in the CLI.
-    .then(() => expectFileSizeToBeUnder('dist/app.main.js', 483 * 1024))
-    .then(() => expectFileSizeToBeUnder('dist/0.app.main.js', 25 * 1024))
-    .then(() => expectFileSizeToBeUnder('dist/1.app.main.js', 2 * 1024))
-    // test resource urls without ./
-    .then(() => replaceInFile('app/app.component.ts',
-      './app.component.html', 'app.component.html'))
-    .then(() => replaceInFile('app/app.component.ts',
-      './app.component.scss', 'app.component.scss'))
-    // test the inclusion of metadata
-    // This build also test resource URLs without ./
-    .then(() => exec(normalize('node_modules/.bin/webpack-cli'), '--mode=development'))
-    .then(() => expectFileToMatch('dist/app.main.js', 'AppModuleNgFactory'))
-    .then(() => skipCleaning());
+export default async function (skipCleaning: () => void) {
+  const webpackCLIBin = normalize('node_modules/.bin/webpack-cli');
+  const isIvy = getGlobalVariable('argv')['ivy'];
+
+  await createProjectFromAsset('webpack/test-app');
+  if (!isIvy) {
+    await updateJsonFile('tsconfig.json', config => {
+      config.angularCompilerOptions.enableIvy = false;
+    });
+  }
+
+  await exec(webpackCLIBin);
+
+  // Note: these sizes are without Build Optimizer or any advanced optimizations in the CLI.
+  await expectFileSizeToBeUnder('dist/app.main.js', (isIvy ? 565 : 483) * 1024);
+  await expectFileSizeToBeUnder('dist/0.app.main.js', 1 * 1024);
+  await expectFileSizeToBeUnder('dist/1.app.main.js', 2 * 1024);
+
+  // test resource urls without ./
+  await replaceInFile('app/app.component.ts', './app.component.html', 'app.component.html');
+  await replaceInFile('app/app.component.ts', './app.component.scss', 'app.component.scss');
+
+  // test the inclusion of metadata
+  // This build also test resource URLs without ./
+  await exec(webpackCLIBin, '--mode=development');
+  await expectFileToMatch('dist/app.main.js', `AppModule${isIvy ? '' : 'NgFactory'}`);
+
+  skipCleaning();
 }
