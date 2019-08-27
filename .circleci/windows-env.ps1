4ce4c0c452ab1bc8e481e96dc634f91c796a84c5
Joey Perrott <josephperrott.github@gmail.com>
2019-08-27 07:39:01 -0700

    build: bump yarn requirement to 1.17.3 and ensure all https in yarn lock

--- .circleci/windows-env.ps1
+++ .circleci/windows-env.ps1
@@ -1,6 +1,6 @@
 # Install nodejs and yarn via Chocolatey.
 choco install nodejs --version 12.1.0 --no-progress
-choco install yarn --version 1.16.0 --no-progress
+choco install yarn --version 1.17.3 --no-progress
 
 # Add PATH modifications to the Powershell profile. This is the win equivalent of .bash_profile.
 # https://docs.microsoft.com/en-us/previous-versions//bb613488(v=vs.85)
