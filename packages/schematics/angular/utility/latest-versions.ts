1dec90bef9afb7d231b4c26147e2882c40fba9d5
mgechev <mgechev@gmail.com>
2019-08-28 17:52:47 +0900

    release: v9.0.0-next.1

--- packages/schematics/angular/utility/latest-versions.ts
+++ packages/schematics/angular/utility/latest-versions.ts
@@ -15,10 +15,10 @@ export const latestVersions = {
   TsLib: '^1.10.0',
 
   // The versions below must be manually updated when making a new devkit release.
-  DevkitBuildAngular: '~0.900.0-next.0',
-  DevkitBuildNgPackagr: '~0.900.0-next.0',
-  DevkitBuildWebpack: '~0.900.0-next.0',
-  AngularPWA: '~0.900.0-next.0',
+  DevkitBuildAngular: '~0.900.0-next.1',
+  DevkitBuildNgPackagr: '~0.900.0-next.1',
+  DevkitBuildWebpack: '~0.900.0-next.1',
+  AngularPWA: '~0.900.0-next.1',
 
   tsickle: '^0.37.0',
   ngPackagr: '^5.4.0',
