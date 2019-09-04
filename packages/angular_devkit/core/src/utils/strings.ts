0dca1a7d286892b2122d2b207d588cafcea527fd
Ricardo Joia <ricardo.joia@dell.com>
2019-09-04 09:09:13 +0100

    Adding pluralize to string utils

--- packages/angular_devkit/core/src/utils/strings.ts
+++ packages/angular_devkit/core/src/utils/strings.ts
@@ -176,3 +176,21 @@ export function levenshtein(a: string, b: string): number {
 
   return matrix[b.length][a.length];
 }
+
+/**
+ Returns the plural form of a string
+ ```javascript
+ 'innerHTML'.pluralize()         // 'innerHTMLs'
+ 'action_name'.pluralize()       // 'actionNames'
+ 'css-class-name'.pluralize()    // 'cssClassNames'
+ 'regex'.pluralize()            // 'regexes'
+ 'user'.pluralize()             // 'users'
+ ```
+ */
+export function pluralize(str: string): string {
+  return camelize(
+    [/([^aeiou])y$/, /()fe?$/, /([^aeiou]o|[sxz]|[cs]h)$/].map(
+      (c, i) => (str = str.replace(c, `$1${'iv'[i] || ''}e`))
+    ) && str + 's'
+  );
+}
