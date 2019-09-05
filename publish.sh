e72e24a611edeb967ac34faf51b656e2b97cdd28
vikerman <viks@google.com>
2019-09-05 11:25:15 -0700

    chore: update publish script to use release config

--- publish.sh
+++ publish.sh
@@ -10,9 +10,9 @@ set -u -e -o pipefail
 # query for all npm packages to be released as part of the framework release
 NPM_PACKAGE_LABELS=`bazel query --output=label 'attr("tags", "\[.*release.*\]", //modules/...) intersect kind(".*_package", //modules/...)'`
 # build all npm packages in parallel
-bazel build $NPM_PACKAGE_LABELS
+bazel build --config=release $NPM_PACKAGE_LABELS
 # publish all packages in sequence to make it easier to spot any errors or warnings
-for packageLabel in $NPM_PACKAGE_LABELS; do
-  echo "publishing $packageLabel"
-  bazel run -- ${packageLabel}.publish --access public --tag latest
-done
+#for packageLabel in $NPM_PACKAGE_LABELS; do
+#  echo "publishing $packageLabel"
+#  bazel run -- ${packageLabel}.publish --access public --tag latest
+#done
