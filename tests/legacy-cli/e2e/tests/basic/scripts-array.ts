d3e4dfa0c06fdc658c91fe1e146e2b368028074a
Charles Lyding <19598772+clydin@users.noreply.github.com>
2019-09-23 13:24:56 -0400

    fix(@angular-devkit/build-angular): display accurate sizes for downlevelled files
    Fixes #15425

--- tests/legacy-cli/e2e/tests/basic/scripts-array.ts
+++ tests/legacy-cli/e2e/tests/basic/scripts-array.ts
@@ -73,15 +73,15 @@ export default async function () {
     await expectFileToMatch(
       'dist/test-project/index.html',
       oneLineTrim`
+      <script src="runtime-es2015.js" type="module"></script>
+      <script src="runtime-es5.js" nomodule defer></script>
       <script src="polyfills-es5.js" nomodule defer></script>
       <script src="polyfills-es2015.js" type="module"></script>
       <script src="scripts.js" defer></script>
       <script src="renamed-script.js" defer></script>
-      <script src="runtime-es2015.js" type="module"></script>
       <script src="vendor-es2015.js" type="module"></script>
-      <script src="main-es2015.js" type="module"></script>
-      <script src="runtime-es5.js" nomodule defer></script>
       <script src="vendor-es5.js" nomodule defer></script>
+      <script src="main-es2015.js" type="module"></script>
       <script src="main-es5.js" nomodule defer></script>
     `,
     );
--- tests/legacy-cli/e2e/tests/basic/styles-array.ts
+++ tests/legacy-cli/e2e/tests/basic/styles-array.ts
@@ -61,13 +61,13 @@ export default async function() {
     await expectFileToMatch(
       'dist/test-project/index.html',
       oneLineTrim`
+        <script src="runtime-es2015.js" type="module"></script>
+        <script src="runtime-es5.js" nomodule defer></script>
         <script src="polyfills-es5.js" nomodule defer></script>
         <script src="polyfills-es2015.js" type="module"></script>
-        <script src="runtime-es2015.js" type="module"></script>
         <script src="vendor-es2015.js" type="module"></script>
-        <script src="main-es2015.js" type="module"></script>
-        <script src="runtime-es5.js" nomodule defer></script>
         <script src="vendor-es5.js" nomodule defer></script>
+        <script src="main-es2015.js" type="module"></script>
         <script src="main-es5.js" nomodule defer></script>
       `,
     );
