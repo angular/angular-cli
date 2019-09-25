b4d5921dc993d3530945ef1d12a574fdb98d674c
vikerman <viks@google.com>
2019-09-25 12:23:20 -0700

    release: v9.0.0-next.6

--- packages/schematics/angular/utility/latest-versions.ts
+++ packages/schematics/angular/utility/latest-versions.ts
@@ -8,17 +8,17 @@
 
 export const latestVersions = {
   // These versions should be kept up to date with latest Angular peer dependencies.
-  Angular: '~9.0.0-next.7',
+  Angular: '~9.0.0-next.8',
   RxJs: '~6.5.3',
   ZoneJs: '~0.10.2',
   TypeScript: '~3.5.3',
   TsLib: '^1.10.0',
 
   // The versions below must be manually updated when making a new devkit release.
-  DevkitBuildAngular: '~0.900.0-next.5',
-  DevkitBuildNgPackagr: '~0.900.0-next.5',
-  DevkitBuildWebpack: '~0.900.0-next.5',
-  AngularPWA: '~0.900.0-next.5',
+  DevkitBuildAngular: '~0.900.0-next.6',
+  DevkitBuildNgPackagr: '~0.900.0-next.6',
+  DevkitBuildWebpack: '~0.900.0-next.6',
+  AngularPWA: '~0.900.0-next.6',
 
   ngPackagr: '^5.5.1',
 };
