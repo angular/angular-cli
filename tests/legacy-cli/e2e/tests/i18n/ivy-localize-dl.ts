3740ce777cb939e68967b06781db475351bf1d5d
Filipe Silva <filipematossilva@gmail.com>
2019-10-31 16:54:29 +0000

    test: refactor i18n tests
    The app-shell and service worker tests were not refactored because they are disabled. They should be refactored when re-enabled.

--- tests/legacy-cli/e2e/tests/i18n/ivy-localize-dl.ts
+++ tests/legacy-cli/e2e/tests/i18n/ivy-localize-dl.ts
@@ -1,162 +1,44 @@
-import * as express from 'express';
-import { resolve } from 'path';
-import { getGlobalVariable } from '../../utils/env';
-import {
-  appendToFile,
-  copyFile,
-  expectFileToExist,
-  expectFileToMatch,
-  replaceInFile,
-  writeFile,
-} from '../../utils/fs';
-import { ng, npm } from '../../utils/process';
+import { appendToFile, expectFileToMatch } from '../../utils/fs';
+import { ng } from '../../utils/process';
 import { updateJsonFile } from '../../utils/project';
 import { expectToFail } from '../../utils/utils';
-import { readNgVersion } from '../../utils/version';
+import { baseDir, externalServer, langTranslations, setupI18nConfig } from './legacy';
 
-export default async function() {
-  let localizeVersion = '@angular/localize@' + readNgVersion();
-  if (getGlobalVariable('argv')['ng-snapshots']) {
-    localizeVersion = require('../../ng-snapshot/package.json').dependencies['@angular/localize'];
-  }
-  await npm('install', `${localizeVersion}`);
 
+export default async function () {
+  // Setup i18n tests and config.
+  await setupI18nConfig();
+
+  // Ensure a DL build is used.
   await updateJsonFile('tsconfig.json', config => {
     config.compilerOptions.target = 'es2015';
     config.angularCompilerOptions.disableTypeScriptVersionCheck = true;
   });
 
-  const baseDir = 'dist/test-project';
-
-  // Set configurations for each locale.
-  const langTranslations = [
-    // TODO: re-enable all locales once localeData support is added.
-    // { lang: 'en-US', translation: 'Hello i18n!' },
-    { lang: 'fr', translation: 'Bonjour i18n!' },
-  ];
-
-  await updateJsonFile('angular.json', workspaceJson => {
-    const appProject = workspaceJson.projects['test-project'];
-    const appArchitect = appProject.architect || appProject.targets;
-    const serveConfigs = appArchitect['serve'].configurations;
-    const e2eConfigs = appArchitect['e2e'].configurations;
-    const buildConfigs = appArchitect['build'].configurations;
-
-    // Make default builds prod.
-    appArchitect['build'].options.optimization = true;
-    appArchitect['build'].options.buildOptimizer = true;
-    appArchitect['build'].options.aot = true;
-    appArchitect['build'].options.fileReplacements = [
-      {
-        replace: 'src/environments/environment.ts',
-        with: 'src/environments/environment.prod.ts',
-      },
-    ];
-
-    // Enable localization for all locales
-    // TODO: re-enable all locales once localeData support is added.
-    // appArchitect['build'].options.localize = true;
-    appArchitect['build'].options.localize = ['fr'];
-    // Always error on missing translations.
-    appArchitect['build'].options.i18nMissingTranslation = 'error';
-
-    // Add locale definitions to the project
-    // tslint:disable-next-line: no-any
-    const i18n: Record<string, any> = (appProject.i18n = { locales: {} });
-    for (const { lang } of langTranslations) {
-      if (lang == 'en-US') {
-        i18n.sourceLocale = lang;
-      } else {
-        i18n.locales[lang] = `src/locale/messages.${lang}.xlf`;
-      }
-
-      buildConfigs[lang] = { localize: [lang] };
-      serveConfigs[lang] = { browserTarget: `test-project:build:${lang}` };
-      e2eConfigs[lang] = {
-        specs: [`./src/app.${lang}.e2e-spec.ts`],
-        devServerTarget: `test-project:serve:${lang}`,
-      };
-    }
-  });
-
-  // Add a translatable element.
-  await writeFile(
-    'src/app/app.component.html',
-    '<h1 i18n="An introduction header for this sample">Hello i18n!</h1>',
-  );
-
-  // Extract the translation messages and copy them for each language.
-  await ng('xi18n', '--output-path=src/locale');
-  await expectFileToExist('src/locale/messages.xlf');
-  await expectFileToMatch('src/locale/messages.xlf', `source-language="en-US"`);
-  await expectFileToMatch('src/locale/messages.xlf', `An introduction header for this sample`);
-
-  for (const { lang, translation } of langTranslations) {
-    if (lang != 'en-US') {
-      await copyFile('src/locale/messages.xlf', `src/locale/messages.${lang}.xlf`);
-      await replaceInFile(
-        `src/locale/messages.${lang}.xlf`,
-        'source-language="en-US"',
-        `source-language="en-US" target-language="${lang}"`,
-      );
-      await replaceInFile(
-        `src/locale/messages.${lang}.xlf`,
-        '<source>Hello i18n!</source>',
-        `<source>Hello i18n!</source>\n<target>${translation}</target>`,
-      );
-    }
-  }
+  // TODO: re-enable all locales once localeData support is added.
+  const tempLangTranslations = langTranslations.filter(l => l.lang == 'fr');
 
   // Build each locale and verify the output.
   // NOTE: this should not fail in general, but multi-locale translation is currently disabled.
   // TODO: remove expectToFail once localeData support is added.
   await expectToFail(() => ng('build', '--localize', 'true'));
   await ng('build');
-  for (const { lang, translation } of langTranslations) {
-    await expectFileToMatch(`${baseDir}/${lang}/main-es5.js`, translation);
-    await expectFileToMatch(`${baseDir}/${lang}/main-es2015.js`, translation);
-    await expectToFail(() => expectFileToMatch(`${baseDir}/${lang}/main-es5.js`, '$localize`'));
-    await expectToFail(() => expectFileToMatch(`${baseDir}/${lang}/main-es2015.js`, '$localize`'));
-    await expectFileToMatch(`${baseDir}/${lang}/main-es5.js`, lang);
-    await expectFileToMatch(`${baseDir}/${lang}/main-es2015.js`, lang);
+  for (const { lang, outputPath, translation } of tempLangTranslations) {
+    await expectFileToMatch(`${outputPath}/main-es5.js`, translation.helloPartial);
+    await expectFileToMatch(`${outputPath}/main-es2015.js`, translation.helloPartial);
+    await expectToFail(() => expectFileToMatch(`${outputPath}/main-es5.js`, '$localize`'));
+    await expectToFail(() => expectFileToMatch(`${outputPath}/main-es2015.js`, '$localize`'));
+    await expectFileToMatch(`${outputPath}/main-es5.js`, lang);
+    await expectFileToMatch(`${outputPath}/main-es2015.js`, lang);
 
-    // Ivy i18n doesn't yet work with `ng serve` so we must use a separate server.
-    const app = express();
-    app.use(express.static(resolve(baseDir, lang)));
-    const server = app.listen(4200, 'localhost');
+    const server = externalServer(outputPath);
     try {
-      // Add E2E test for locale
-      await writeFile(
-        'e2e/src/app.e2e-spec.ts',
-        `
-        import { browser, logging, element, by } from 'protractor';
-        describe('workspace-project App', () => {
-          it('should display welcome message', () => {
-            browser.get(browser.baseUrl);
-            expect(element(by.css('h1')).getText()).toEqual('${translation}');
-          });
-          afterEach(async () => {
-            // Assert that there are no errors emitted from the browser
-            const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-            expect(logs).not.toContain(jasmine.objectContaining({
-              level: logging.Level.SEVERE,
-            } as logging.Entry));
-          });
-        });
-      `,
-      );
-
       // Execute without a devserver.
-      await ng('e2e', '--devServerTarget=');
+      await ng('e2e', `--configuration=${lang}`, '--devServerTarget=');
     } finally {
       server.close();
     }
   }
-
-  // Verify locale data registration (currently only for single locale builds)
-  await ng('build', '--optimization', 'false', '-c', 'fr', '--i18n-missing-translation', 'error');
-  await expectFileToMatch(`${baseDir}/fr/main-es5.js`, 'registerLocaleData');
-  await expectFileToMatch(`${baseDir}/fr/main-es2015.js`, 'registerLocaleData');
 
   // Verify missing translation behaviour.
   await appendToFile('src/app/app.component.html', '<p i18n>Other content</p>');
---
+++ tests/legacy-cli/e2e/tests/i18n/legacy.ts
@@ -0,0 +1,238 @@
+import * as express from 'express';
+import { resolve } from 'path';
+import { getGlobalVariable } from '../../utils/env';
+import { appendToFile, copyFile, expectFileToExist, expectFileToMatch, replaceInFile, writeFile } from '../../utils/fs';
+import { ng, npm } from '../../utils/process';
+import { updateJsonFile } from '../../utils/project';
+import { expectToFail } from '../../utils/utils';
+import { readNgVersion } from '../../utils/version';
+
+
+// Configurations for each locale.
+export const baseDir = 'dist/test-project';
+export const langTranslations = [
+  {
+    lang: 'en-US', outputPath: `${baseDir}/en`,
+    translation: {
+      helloPartial: 'Hello',
+      hello: 'Hello i18n!',
+      plural: 'Updated 3 minutes ago',
+      date: 'January',
+    },
+  },
+  {
+    lang: 'fr', outputPath: `${baseDir}/fr`,
+    translation: {
+      helloPartial: 'Bonjour',
+      hello: 'Bonjour i18n!',
+      plural: 'Mis à jour Il y a 3 minutes',
+      date: 'janvier',
+    },
+    translationReplacements: [
+      [/source/g, 'target'],
+      ['Hello', 'Bonjour'],
+      ['Updated', 'Mis à jour'],
+      ['just now', 'juste maintenant'],
+      ['one minute ago', 'il y a une minute'],
+      ['other {', 'other {Il y a'],
+      ['minutes ago', 'minutes'],
+    ],
+  },
+  {
+    lang: 'de', outputPath: `${baseDir}/de`,
+    translation: {
+      helloPartial: 'Hallo',
+      hello: 'Hallo i18n!',
+      plural: 'Aktualisiert vor 3 Minuten',
+      date: 'Januar',
+    },
+    translationReplacements: [
+      [/source/g, 'target'],
+      ['Hello', 'Hallo'],
+      ['Updated', 'Aktualisiert'],
+      ['just now', 'gerade jetzt'],
+      ['one minute ago', 'vor einer Minute'],
+      ['other {', 'other {vor'],
+      ['minutes ago', 'Minuten'],
+    ],
+  },
+];
+export const sourceLocale = langTranslations[0].lang;
+export const externalServer = (outputPath: string) => {
+  // Ivy i18n doesn't yet work with `ng serve` so we must use a separate server.
+  const app = express();
+  app.use(express.static(resolve(outputPath)));
+
+  // call .close() on the return value to close the server.
+  return app.listen(4200, 'localhost');
+};
+
+export async function setupI18nConfig(useLocalize = true) {
+  // Add component with i18n content, both translations and localeData (plural, dates).
+  await writeFile('src/app/app.component.ts', `
+    import { Component, Inject, LOCALE_ID } from '@angular/core';
+    @Component({
+      selector: 'app-root',
+      templateUrl: './app.component.html'
+    })
+    export class AppComponent {
+      constructor(@Inject(LOCALE_ID) public locale: string) { }
+      title = 'i18n';
+      jan = new Date(2000, 0, 1);
+      minutes = 3;
+    }
+  `);
+  await writeFile(`src/app/app.component.html`, `
+    <p id="hello" i18n="An introduction header for this sample">Hello {{ title }}! </p>
+    <p id="locale">{{ locale }}</p>
+    <p id="date">{{ jan | date : 'LLLL' }}</p>
+    <p id="plural" i18n>Updated {minutes, plural, =0 {just now} =1 {one minute ago} other {{{minutes}} minutes ago}}</p>
+  `);
+
+  // Add e2e specs for each lang.
+  for (const { lang, translation } of langTranslations) {
+    await writeFile(`./src/app.${lang}.e2e-spec.ts`, `
+      import { browser, logging, element, by } from 'protractor';
+
+      describe('workspace-project App', () => {
+        const getParagraph = (name: string) => element(by.css('app-root p#' + name)).getText();
+        beforeEach(() => browser.get(browser.baseUrl););
+        afterEach(async () => {
+          // Assert that there are no errors emitted from the browser
+          const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+          expect(logs).not.toContain(jasmine.objectContaining({
+            level: logging.Level.SEVERE,
+          } as logging.Entry));
+        });
+
+        it('should display welcome message', () =>
+          expect(getParagraph('hello')).toEqual('${translation.hello}'));
+
+        it('should display locale', () =>
+          expect(getParagraph('locale')).toEqual('${lang}'));
+
+        it('should display localized date', () =>
+          expect(getParagraph('date')).toEqual('${translation.plural}'));
+
+        it('should display pluralized message', () =>
+          expect(getParagraph('plural')).toEqual('${translation.plural}'));
+      });
+    `);
+  }
+
+  // Update angular.json to build, serve, and test each locale.
+  await updateJsonFile('angular.json', workspaceJson => {
+    const appProject = workspaceJson.projects['test-project'];
+    const appArchitect = workspaceJson.projects['test-project'].architect;
+    const buildConfigs = appArchitect['build'].configurations;
+    const serveConfigs = appArchitect['serve'].configurations;
+    const e2eConfigs = appArchitect['e2e'].configurations;
+
+    // Make default builds prod.
+    appArchitect['build'].options.optimization = true;
+    appArchitect['build'].options.buildOptimizer = true;
+    appArchitect['build'].options.aot = true;
+    appArchitect['build'].options.fileReplacements = [{
+      replace: 'src/environments/environment.ts',
+      with: 'src/environments/environment.prod.ts',
+    }];
+
+    // Always error on missing translations.
+    appArchitect['build'].options.i18nMissingTranslation = 'error';
+
+    if (useLocalize) {
+      // Enable localization for all locales
+      // TODO: re-enable all locales once localeData support is added.
+      // appArchitect['build'].options.localize = true;
+      appArchitect['build'].options.localize = ['fr'];
+    }
+
+    // Add i18n config items (app, build, serve, e2e).
+    // tslint:disable-next-line: no-any
+    const i18n: Record<string, any> = (appProject.i18n = { locales: {} });
+    for (const { lang, outputPath } of langTranslations) {
+      if (!useLocalize) {
+        if (lang == sourceLocale) {
+          buildConfigs[lang] = { outputPath };
+        } else {
+          buildConfigs[lang] = {
+            outputPath,
+            i18nFile: `src/locale/messages.${lang}.xlf`,
+            i18nFormat: `xlf`,
+            i18nLocale: lang,
+          };
+        }
+      } else {
+        if (lang == sourceLocale) {
+          i18n.sourceLocale = lang;
+        } else {
+          i18n.locales[lang] = `src/locale/messages.${lang}.xlf`;
+        }
+        buildConfigs[lang] = { localize: [lang] };
+      }
+
+      serveConfigs[lang] = { browserTarget: `test-project:build:${lang}` };
+      e2eConfigs[lang] = {
+        specs: [`./src/app.${lang}.e2e-spec.ts`],
+        devServerTarget: `test-project:serve:${lang}`,
+      };
+    }
+  });
+
+  // Extract the translation messages.
+  await ng('xi18n', '--output-path=src/locale');
+  await expectFileToExist('src/locale/messages.xlf');
+  await expectFileToMatch('src/locale/messages.xlf', `source-language="en-US"`);
+  await expectFileToMatch('src/locale/messages.xlf', `An introduction header for this sample`);
+
+  // Make translations for each language.
+  for (const { lang, translationReplacements } of langTranslations) {
+    if (lang != sourceLocale) {
+      await copyFile('src/locale/messages.xlf', `src/locale/messages.${lang}.xlf`);
+      for (const replacements of translationReplacements) {
+        await replaceInFile(`src/locale/messages.${lang}.xlf`, replacements[0], replacements[1]);
+      }
+    }
+  }
+
+  if (useLocalize) {
+    // Install the localize package.
+    let localizeVersion = '@angular/localize@' + readNgVersion();
+    if (getGlobalVariable('argv')['ng-snapshots']) {
+      localizeVersion = require('../../ng-snapshot/package.json').dependencies['@angular/localize'];
+    }
+    await npm('install', `${localizeVersion}`);
+  }
+}
+
+export default async function () {
+  // Setup i18n tests and config.
+  await setupI18nConfig(false);
+
+  // Build each locale and verify the output.
+  for (const { lang, translation, outputPath } of langTranslations) {
+    await ng('build', `--configuration=${lang}`);
+    await expectFileToMatch(`${outputPath}/main-es5.js`, translation.helloPartial);
+    await expectFileToMatch(`${outputPath}/main-es2015.js`, translation.helloPartial);
+
+    // E2E to verify the output runs and is correct.
+    if (getGlobalVariable('argv')['ve']) {
+      await ng('e2e', `--configuration=${lang}`);
+    } else {
+      const server = externalServer(outputPath);
+      try {
+        // Execute without a devserver.
+        await ng('e2e', `--configuration=${lang}`, '--devServerTarget=');
+      } finally {
+        server.close();
+      }
+    }
+  }
+
+  // Verify missing translation behaviour.
+  await appendToFile('src/app/app.component.html', '<p i18n>Other content</p>');
+  await ng('build', '--configuration=fr', '--i18n-missing-translation', 'ignore');
+  await expectFileToMatch(`${baseDir}/fr/main-es5.js`, /Other content/);
+  await expectFileToMatch(`${baseDir}/fr/main-es2015.js`, /Other content/);
+  await expectToFail(() => ng('build', '--configuration=fr'));
+}
