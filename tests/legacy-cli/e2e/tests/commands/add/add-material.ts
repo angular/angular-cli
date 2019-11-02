3f57956c3282361f8909ff8b0d51471d708162eb
Charles Lyding <19598772+clydin@users.noreply.github.com>
2019-11-01 21:39:10 -0400

    fix(@angular/cli): verify package specifier when adding a package
    Fixes #16029

--- tests/legacy-cli/e2e/tests/commands/add/add-material.ts
+++ tests/legacy-cli/e2e/tests/commands/add/add-material.ts
@@ -8,4 +8,24 @@ export default async function () {
 
   await ng('add', '@angular/material');
   await expectFileToMatch('package.json', /@angular\/material/);
+
+  const output1 = await ng('add', '@angular/material');
+  if (!output1.stdout.includes('Skipping installation: Package already installed')) {
+    throw new Error('Installation was not skipped');
+  }
+
+  const output2 = await ng('add', '@angular/material@latest');
+  if (output2.stdout.includes('Skipping installation: Package already installed')) {
+    throw new Error('Installation should not have been skipped');
+  }
+
+  const output3 = await ng('add', '@angular/material@8.0.0');
+  if (output3.stdout.includes('Skipping installation: Package already installed')) {
+    throw new Error('Installation should not have been skipped');
+  }
+
+  const output4 = await ng('add', '@angular/material@8');
+  if (!output4.stdout.includes('Skipping installation: Package already installed')) {
+    throw new Error('Installation was not skipped');
+  }
 }
