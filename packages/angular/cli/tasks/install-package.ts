b7b07d5d7dd0569689311978cef592dd47034a12
Alan Agius <alan.agius4@gmail.com>
2019-11-07 15:50:41 +0100

    fix(@angular/cli): use spawned process error when it's available
    In some cases when an error occurs in the spawned process the `error` property will be populated instead of `stderr` or `stdout`.
    
    See: https://nodejs.org/api/child_process.html#child_process_child_process_spawnsync_command_args_options

--- packages/angular/cli/tasks/install-package.ts
+++ packages/angular/cli/tasks/install-package.ts
@@ -40,7 +40,7 @@ export function installPackage(
 
   logger.info(colors.green(`Installing packages for tooling via ${packageManager}.`));
 
-  const { status, stderr, stdout } = spawnSync(packageManager, [...installArgs, ...extraArgs], {
+  const { status, stderr, stdout, error } = spawnSync(packageManager, [...installArgs, ...extraArgs], {
     stdio: 'pipe',
     encoding: 'utf8',
     shell: true,
@@ -48,11 +48,11 @@ export function installPackage(
   });
 
   if (status !== 0) {
-    let error = (stderr || stdout).trim();
-    if (error) {
-      error += '\n';
+    let errorMessage = ((error && error.message) || stderr || stdout || '').trim();
+    if (errorMessage) {
+      errorMessage += '\n';
     }
-    throw new Error(error + `Package install failed${error ? ', see above' : ''}.`);
+    throw new Error(errorMessage + `Package install failed${errorMessage ? ', see above' : ''}.`);
   }
 
   logger.info(colors.green(`Installed packages for tooling via ${packageManager}.`));
