d41522302bf8951327f04f97ba053dab7ce1df93
Charles Lyding <19598772+clydin@users.noreply.github.com>
2019-11-11 18:04:24 -0500

    fix(@angular-devkit/build-angular): ignore node modules when polling
    The node modules directory contains a massive set of directories and files.  When watching via polling, that set needs to be queried repeatedly to determine if any files have changed.  Changes within node modules are quite rare while using `ng serve` or `ng build --watch`.  As a result, polling the node modules directory is rarely useful.  This change causes CPU usage to drop from a potential high of ~80% to a more manageable ~5-10%.

--- packages/angular_devkit/build_angular/src/dev-server/index.ts
+++ packages/angular_devkit/build_angular/src/dev-server/index.ts
@@ -427,7 +427,11 @@ export function buildServerConfig(
     stats: false,
     compress: styles || scripts,
     watchOptions: {
-      poll: browserOptions.poll,
+      // Using just `--poll` will result in a value of 0 which is very likely not the intention
+      // A value of 0 is falsy and will disable polling rather then enable
+      // 500 ms is a sensible default in this case
+      poll: serverOptions.poll === 0 ? 500 : serverOptions.poll,
+      ignored: serverOptions.poll === undefined ? undefined : /[\\\/]node_modules[\\\/]/,
     },
     https: serverOptions.ssl,
     overlay: {
