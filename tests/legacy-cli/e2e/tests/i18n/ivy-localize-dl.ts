bb403692d1e2d80c1af503665927a4eac5d8f4b6
Charles Lyding <19598772+clydin@users.noreply.github.com>
2019-11-13 20:01:11 -0500

    revert: fix(@angular-devkit/build-angular): temporarily disable global locale data injection
    This reverts commit 00aa5bba5a6cc31a96fef0697da110057d63bca1.

--- tests/legacy-cli/e2e/tests/i18n/ivy-localize-dl.ts
+++ tests/legacy-cli/e2e/tests/i18n/ivy-localize-dl.ts
@@ -33,8 +33,8 @@ export default async function() {
     await expectFileToMatch(`${outputPath}/main-es2015.js`, lang);
 
     // Verify the locale data is registered using the global files
-    // await expectFileToMatch(`${outputPath}/main-es5.js`, '.ng.common.locales');
-    // await expectFileToMatch(`${outputPath}/main-es2015.js`, '.ng.common.locales');
+    await expectFileToMatch(`${outputPath}/main-es5.js`, '.ng.common.locales');
+    await expectFileToMatch(`${outputPath}/main-es2015.js`, '.ng.common.locales');
 
     // Execute Application E2E tests with dev server
     await ng('e2e', `--configuration=${lang}`, '--port=0');
@@ -49,9 +49,9 @@ export default async function() {
   }
 
   // Verify deprecated locale data registration is not present
-  // await ng('build', '--configuration=fr', '--optimization=false');
-  // await expectToFail(() => expectFileToMatch(`${baseDir}/fr/main-es5.js`, 'registerLocaleData('));
-  // await expectToFail(() => expectFileToMatch(`${baseDir}/fr/main-es2015.js`, 'registerLocaleData('));
+  await ng('build', '--configuration=fr', '--optimization=false');
+  await expectToFail(() => expectFileToMatch(`${baseDir}/fr/main-es5.js`, 'registerLocaleData('));
+  await expectToFail(() => expectFileToMatch(`${baseDir}/fr/main-es2015.js`, 'registerLocaleData('));
 
   // Verify missing translation behaviour.
   await appendToFile('src/app/app.component.html', '<p i18n>Other content</p>');
