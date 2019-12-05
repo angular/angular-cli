c643e714f78cfd5828959c2969b430c2252f1382
Alan Agius <alan.agius4@gmail.com>
2019-12-05 15:00:42 +0100

    build: exclude schematics `testing` folder from being shipped (#1361)
    Currently we are including the testing folder in the npm_package
    
    See: https://unpkg.com/browse/@nguniversal/express-engine@9.0.0-next.8/schematics

--- modules/common/schematics/testing/test-app.ts
+++ modules/common/schematics/testing/test-app.ts
@@ -6,27 +6,28 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {SchematicTestRunner, UnitTestTree} from '@angular-devkit/schematics/testing';
-import {switchMap} from 'rxjs/operators';
+import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/testing';
 
 /** Path to the collection file for the NgUniversal schematics */
 export const collectionPath = require.resolve('@schematics/angular/collection.json');
 
 /** Create a base app used for testing. */
-export function createTestApp(appOptions = {}): Promise<UnitTestTree> {
+export async function createTestApp(appOptions = {}): Promise<UnitTestTree> {
   const baseRunner =
-      new SchematicTestRunner('universal-schematics', collectionPath);
+    new SchematicTestRunner('universal-schematics', collectionPath);
 
-  return baseRunner
-      .runExternalSchematicAsync('@schematics/angular', 'workspace', {
-        name: 'workspace',
-        version: '6.0.0',
-        newProjectRoot: 'projects',
-      })
-      .pipe(
-          switchMap(workspaceTree => baseRunner.runExternalSchematicAsync(
-                  '@schematics/angular', 'application',
-                  {...appOptions, name: 'test-app'}, workspaceTree)),
-      )
-      .toPromise();
+  const tree = await baseRunner.runExternalSchematicAsync('@schematics/angular', 'workspace', {
+    name: 'workspace',
+    version: '6.0.0',
+    newProjectRoot: 'projects',
+  }).toPromise();
+
+  return baseRunner.runExternalSchematicAsync(
+    '@schematics/angular', 'application',
+    {
+      ...appOptions,
+      name: 'test-app',
+    },
+    tree
+  ).toPromise();
 }
