3e89d2eac3770bba57e198df3aadb101db4831d7
Filipe Silva <filipematossilva@gmail.com>
2020-01-16 11:15:49 +0000

    build: update latest versions

--- packages/schematics/angular/utility/latest-versions.ts
+++ packages/schematics/angular/utility/latest-versions.ts
@@ -8,16 +8,18 @@
 
 export const latestVersions = {
   // These versions should be kept up to date with latest Angular peer dependencies.
-  Angular: '~9.0.0-next.15',
-  RxJs: '~6.5.3',
+  Angular: '~9.0.0-rc.9',
+  RxJs: '~6.5.4',
   ZoneJs: '~0.10.2',
-  TypeScript: '~3.6.4',
+  TypeScript: '~3.7.4',
   TsLib: '^1.10.0',
 
   // The versions below must be manually updated when making a new devkit release.
+  // For our e2e tests, these versions must match the latest tag present on the branch.
+  // During RC periods they will not match the latest RC though, and should not be updated.
   DevkitBuildAngular: '~0.900.0-next.19',
   DevkitBuildNgPackagr: '~0.900.0-next.19',
   DevkitBuildWebpack: '~0.900.0-next.19',
 
-  ngPackagr: '^9.0.0-rc.0',
+  ngPackagr: '^9.0.0-rc.7',
 };
