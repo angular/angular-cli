525e4aa8d7352d487883e2f797b0812615cc4041
Doug Parker <dgp1130@users.noreply.github.com>
2020-01-21 14:18:32 -0800

    release: v9.0.0-rc.10
    Not updating Angular because this is off-cycle release to include TS 3.7. FW has no new release to include.

--- packages/schematics/angular/utility/latest-versions.ts
+++ packages/schematics/angular/utility/latest-versions.ts
@@ -15,9 +15,9 @@ export const latestVersions = {
   TsLib: '^1.10.0',
 
   // The versions below must be manually updated when making a new devkit release.
-  DevkitBuildAngular: '~0.900.0-rc.9',
-  DevkitBuildNgPackagr: '~0.900.0-rc.9',
-  DevkitBuildWebpack: '~0.900.0-rc.9',
+  DevkitBuildAngular: '~0.900.0-rc.10',
+  DevkitBuildNgPackagr: '~0.900.0-rc.10',
+  DevkitBuildWebpack: '~0.900.0-rc.10',
 
   ngPackagr: '^9.0.0-rc.7',
 };
