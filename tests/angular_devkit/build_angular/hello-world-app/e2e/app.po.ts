f45b94fefa855eea4770327a55379eb696f58e43
Cyrille Tuzi <web@cyrilletuzi.com>
2020-01-27 22:15:20 +0100

    fix(@schematics/angular): add compliance with no-any lint rule

--- tests/angular_devkit/build_angular/hello-world-app/e2e/app.po.ts
+++ tests/angular_devkit/build_angular/hello-world-app/e2e/app.po.ts
@@ -8,11 +8,11 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  navigateTo() {
-    return browser.get(browser.baseUrl);
+  navigateTo(): Promise<unknown> {
+    return browser.get(browser.baseUrl) as Promise<unknown>;
   }
 
-  getTitleText() {
-    return element(by.css('app-root h1')).getText();
+  getTitleText(): Promise<string> {
+    return element(by.css('app-root h1')).getText() as Promise<string>;
   }
 }
