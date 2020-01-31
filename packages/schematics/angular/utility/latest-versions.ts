65b47c841855cc842b742a967b49e095d0cc69d6
mgechev <mgechev@gmail.com>
2020-01-30 17:19:23 -0800

    release: v9.0.0-rc.12

--- packages/schematics/angular/utility/latest-versions.ts
+++ packages/schematics/angular/utility/latest-versions.ts
@@ -8,16 +8,16 @@
 
 export const latestVersions = {
   // These versions should be kept up to date with latest Angular peer dependencies.
-  Angular: '~9.0.0-rc.11',
+  Angular: '~9.0.0-rc.12',
   RxJs: '~6.5.4',
   ZoneJs: '~0.10.2',
   TypeScript: '~3.7.4',
   TsLib: '^1.10.0',
 
   // The versions below must be manually updated when making a new devkit release.
-  DevkitBuildAngular: '~0.900.0-rc.11',
-  DevkitBuildNgPackagr: '~0.900.0-rc.11',
-  DevkitBuildWebpack: '~0.900.0-rc.11',
+  DevkitBuildAngular: '~0.900.0-rc.12',
+  DevkitBuildNgPackagr: '~0.900.0-rc.12',
+  DevkitBuildWebpack: '~0.900.0-rc.12',
 
   ngPackagr: '^9.0.0-rc.7',
 };
