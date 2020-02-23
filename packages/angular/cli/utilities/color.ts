48f1c6a07e68681d8b61b003e74edeb9ae859376
Charles Lyding <19598772+clydin@users.noreply.github.com>
2020-02-23 08:04:10 -0500

    fix(@angular/cli): directly remove ansi color codes when no color support
    Third party libraries can attempt to write color codes to the output even though the CLI has already determined that color should not be used.  The previously implemented color removal code is no longer functional since the update of ansi-colors to 4.1.0.  While this appears to be a defect in the aforementioned package, the new CLI removal method not only bypasses the defect but also unneeded execution logic that the CLI does not need in this case.
    
    Fixes: #17053

--- packages/angular/cli/utilities/color.ts
+++ packages/angular/cli/utilities/color.ts
@@ -5,7 +5,7 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import * as colors from 'ansi-colors';
+import * as ansiColors from 'ansi-colors';
 import { WriteStream } from 'tty';
 
 // Typings do not contain the function call (added in Node.js v9.9.0)
@@ -13,6 +13,12 @@ export const supportsColor =
   process.stdout instanceof WriteStream &&
   ((process.stdout as unknown) as { getColorDepth(): number }).getColorDepth() > 1;
 
-(colors as { enabled: boolean }).enabled = supportsColor;
+export function removeColor(text: string): string {
+  return text.replace(new RegExp(ansiColors.ansiRegex), '');
+}
+
+// tslint:disable-next-line: no-any
+const colors = (ansiColors as any).create() as typeof ansiColors;
+colors.enabled = supportsColor;
 
 export { colors };
