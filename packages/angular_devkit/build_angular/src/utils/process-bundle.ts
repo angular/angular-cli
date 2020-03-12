a5aefa12580ff8970ab2138a70a01da8b7325388
Charles Lyding <19598772+clydin@users.noreply.github.com>
2020-03-12 13:34:00 -0400

    refactor(@angular-devkit/build-angular): temporarily disable plugin based localization

--- packages/angular_devkit/build_angular/src/utils/process-bundle.ts
+++ packages/angular_devkit/build_angular/src/utils/process-bundle.ts
@@ -12,6 +12,7 @@ import {
   parseSync,
   transformAsync,
   transformFromAstSync,
+  traverse,
   types,
 } from '@babel/core';
 import templateBuilder from '@babel/template';
@@ -502,6 +503,8 @@ function createReplacePlugin(replacements: [string, string][]): PluginObj {
   };
 }
 
+const USE_LOCALIZE_PLUGINS = false;
+
 async function createI18nPlugins(
   locale: string,
   translation: unknown | undefined,
@@ -565,6 +568,13 @@ export interface InlineOptions {
   setLocale?: boolean;
 }
 
+interface LocalizePosition {
+  start: number;
+  end: number;
+  messageParts: TemplateStringsArray;
+  expressions: types.Expression[];
+}
+
 const localizeName = '$localize';
 
 export async function inlineLocales(options: InlineOptions) {
@@ -601,6 +611,10 @@ export async function inlineLocales(options: InlineOptions) {
 
   if (!ast) {
     throw new Error(`Unknown error occurred inlining file "${options.filename}"`);
+  }
+
+  if (!USE_LOCALIZE_PLUGINS) {
+    return inlineLocalesDirect(ast, options);
   }
 
   const diagnostics = [];
@@ -667,6 +681,95 @@ export async function inlineLocales(options: InlineOptions) {
   return { file: options.filename, diagnostics };
 }
 
+async function inlineLocalesDirect(ast: ParseResult, options: InlineOptions) {
+  if (!i18n || i18n.inlineLocales.size === 0) {
+    return { file: options.filename, diagnostics: [], count: 0 };
+  }
+
+  const { default: MagicString } = await import('magic-string');
+  const { default: generate } = await import('@babel/generator');
+  const utils = await import(
+    // tslint:disable-next-line: trailing-comma no-implicit-dependencies
+    '@angular/localize/src/tools/src/translate/source_files/source_file_utils'
+  );
+  // tslint:disable-next-line: no-implicit-dependencies
+  const localizeDiag = await import('@angular/localize/src/tools/src/diagnostics');
+
+  const diagnostics = new localizeDiag.Diagnostics();
+
+  const positions = findLocalizePositions(ast, options, utils);
+  if (positions.length === 0 && !options.setLocale) {
+    return inlineCopyOnly(options);
+  }
+
+  // tslint:disable-next-line: no-any
+  let content = new MagicString(options.code, { filename: options.filename } as any);
+  const inputMap = options.map && (JSON.parse(options.map) as RawSourceMap);
+  let contentClone;
+  for (const locale of i18n.inlineLocales) {
+    const isSourceLocale = locale === i18n.sourceLocale;
+    // tslint:disable-next-line: no-any
+    const translations: any = isSourceLocale ? {} : i18n.locales[locale].translation || {};
+    for (const position of positions) {
+      const translated = utils.translate(
+        diagnostics,
+        translations,
+        position.messageParts,
+        position.expressions,
+        isSourceLocale ? 'ignore' : options.missingTranslation || 'warning',
+      );
+
+      const expression = utils.buildLocalizeReplacement(translated[0], translated[1]);
+      const { code } = generate(expression);
+
+      content.overwrite(position.start, position.end, code);
+    }
+
+    if (options.setLocale) {
+      const setLocaleText = `var $localize=Object.assign(void 0===$localize?{}:$localize,{locale:"${locale}"});`;
+      contentClone = content.clone();
+      content.prepend(setLocaleText);
+
+      // If locale data is provided, load it and prepend to file
+      const localeDataPath = i18n.locales[locale] && i18n.locales[locale].dataPath;
+      if (localeDataPath) {
+        const localDataContent = await loadLocaleData(localeDataPath, true);
+        // The semicolon ensures that there is no syntax error between statements
+        content.prepend(localDataContent + ';');
+      }
+    }
+
+    const output = content.toString();
+    const outputPath = path.join(
+      options.outputPath,
+      i18n.flatOutput ? '' : locale,
+      options.filename,
+    );
+    fs.writeFileSync(outputPath, output);
+
+    if (inputMap) {
+      const contentMap = content.generateMap();
+      const outputMap = mergeSourceMaps(
+        options.code,
+        inputMap,
+        output,
+        contentMap,
+        options.filename,
+        options.code.length > FAST_SOURCEMAP_THRESHOLD,
+      );
+
+      fs.writeFileSync(outputPath + '.map', JSON.stringify(outputMap));
+    }
+
+    if (contentClone) {
+      content = contentClone;
+      contentClone = undefined;
+    }
+  }
+
+  return { file: options.filename, diagnostics: diagnostics.messages, count: positions.length };
+}
+
 function inlineCopyOnly(options: InlineOptions) {
   if (!i18n) {
     throw new Error('i18n options are missing');
@@ -685,6 +788,62 @@ function inlineCopyOnly(options: InlineOptions) {
   }
 
   return { file: options.filename, diagnostics: [], count: 0 };
+}
+
+function findLocalizePositions(
+  ast: ParseResult,
+  options: InlineOptions,
+  // tslint:disable-next-line: no-implicit-dependencies
+  utils: typeof import('@angular/localize/src/tools/src/translate/source_files/source_file_utils'),
+): LocalizePosition[] {
+  const positions: LocalizePosition[] = [];
+  if (options.es5) {
+    traverse(ast, {
+      CallExpression(path: NodePath<types.CallExpression>) {
+        const callee = path.get('callee');
+        if (
+          callee.isIdentifier() &&
+          callee.node.name === localizeName &&
+          utils.isGlobalIdentifier(callee)
+        ) {
+          const messageParts = utils.unwrapMessagePartsFromLocalizeCall(path);
+          const expressions = utils.unwrapSubstitutionsFromLocalizeCall(path.node);
+          positions.push({
+            // tslint:disable-next-line: no-non-null-assertion
+            start: path.node.start!,
+            // tslint:disable-next-line: no-non-null-assertion
+            end: path.node.end!,
+            messageParts,
+            expressions,
+          });
+        }
+      },
+    });
+  } else {
+    const traverseFast = ((types as unknown) as {
+      traverseFast: (node: types.Node, enter: (node: types.Node) => void) => void;
+    }).traverseFast;
+
+    traverseFast(ast, node => {
+      if (
+        node.type === 'TaggedTemplateExpression' &&
+        types.isIdentifier(node.tag) &&
+        node.tag.name === localizeName
+      ) {
+        const messageParts = utils.unwrapMessagePartsFromTemplateLiteral(node.quasi.quasis);
+        positions.push({
+          // tslint:disable-next-line: no-non-null-assertion
+          start: node.start!,
+          // tslint:disable-next-line: no-non-null-assertion
+          end: node.end!,
+          messageParts,
+          expressions: node.quasi.expressions,
+        });
+      }
+    });
+  }
+
+  return positions;
 }
 
 async function loadLocaleData(path: string, optimize: boolean): Promise<string> {
