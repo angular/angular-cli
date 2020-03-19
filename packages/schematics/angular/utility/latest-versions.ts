97a97b42ee3f19b6dda60dc6f939be4ed9d3c631
mgechev <mgechev@gmail.com>
2020-03-18 20:10:57 -0700

    release: v9.0.7

--- packages/schematics/angular/utility/latest-versions.ts
+++ packages/schematics/angular/utility/latest-versions.ts
@@ -8,7 +8,7 @@
 
 export const latestVersions = {
   // These versions should be kept up to date with latest Angular peer dependencies.
-  Angular: '~9.0.6',
+  Angular: '~9.0.7',
   RxJs: '~6.5.4',
   ZoneJs: '~0.10.2',
   TypeScript: '~3.7.5',
@@ -17,9 +17,9 @@ export const latestVersions = {
   // The versions below must be manually updated when making a new devkit release.
   // For our e2e tests, these versions must match the latest tag present on the branch.
   // During RC periods they will not match the latest RC though, and should not be updated.
-  DevkitBuildAngular: '~0.900.6',
-  DevkitBuildNgPackagr: '~0.900.6',
-  DevkitBuildWebpack: '~0.900.6',
+  DevkitBuildAngular: '~0.900.7',
+  DevkitBuildNgPackagr: '~0.900.7',
+  DevkitBuildWebpack: '~0.900.7',
 
   ngPackagr: '^9.0.0',
 };
