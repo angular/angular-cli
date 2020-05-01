51549977286904b65c800f44792e19ed671c3b8d
Walter Werner SCHNEIDER <contact@schnwalter.eu>
2020-05-01 18:17:57 +0300

    refactor(@schematics/angular): remove unused code

--- packages/schematics/angular/utility/project-targets.ts
+++ packages/schematics/angular/utility/project-targets.ts
@@ -6,30 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import { SchematicsException, Tree } from '@angular-devkit/schematics';
-import { getProject, isWorkspaceProject } from './project';
-import { WorkspaceProject, WorkspaceSchema, WorkspaceTargets } from './workspace-models';
-
-export function getProjectTargets(project: WorkspaceProject): WorkspaceTargets;
-export function getProjectTargets(
-  workspaceOrHost: WorkspaceSchema | Tree,
-  projectName: string,
-): WorkspaceTargets;
-export function getProjectTargets(
-  projectOrHost: WorkspaceProject | Tree | WorkspaceSchema,
-  projectName = '',
-): WorkspaceTargets {
-  const project = isWorkspaceProject(projectOrHost)
-    ? projectOrHost
-    : getProject(projectOrHost, projectName);
-
-  const projectTargets = project.targets || project.architect;
-  if (!projectTargets) {
-    throw new Error('Project target not found.');
-  }
-
-  return projectTargets;
-}
+import { SchematicsException } from '@angular-devkit/schematics';
 
 export function targetBuildNotFoundError(): SchematicsException {
   return new SchematicsException(`Project target "build" not found.`);
