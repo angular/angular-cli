0aecfaab936512f250209969885729c843c30a56
Charles Lyding <19598772+clydin@users.noreply.github.com>
2020-05-04 12:48:46 -0400

    refactor(@angular-devkit/build-optimizer): removed deprecated/unused passes
    BREAKING CHANGE
    Deprecated and outdated optimization passes class fold and import tslib have been removed.
    
    Note: This change only effects direct @angular-devkit/schematics users and not the application developers.

--- etc/api/angular_devkit/build_optimizer/src/_golden-api.d.ts
+++ etc/api/angular_devkit/build_optimizer/src/_golden-api.d.ts
@@ -8,10 +8,6 @@ export declare class BuildOptimizerWebpackPlugin {
 
 export default function buildOptimizerLoader(this: webpack.loader.LoaderContext, content: string, previousSourceMap: RawSourceMap): void;
 
-export declare function getFoldFileTransformer(program: ts.Program): ts.TransformerFactory<ts.SourceFile>;
-
-export declare function getImportTslibTransformer(): ts.TransformerFactory<ts.SourceFile>;
-
 export declare function getPrefixClassesTransformer(): ts.TransformerFactory<ts.SourceFile>;
 
 export declare function getPrefixFunctionsTransformer(): ts.TransformerFactory<ts.SourceFile>;
