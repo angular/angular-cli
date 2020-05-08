56ffa1c7b2aac7b017f00b8ee859b33a97381356
Alan Agius <alan.agius4@gmail.com>
2020-05-08 13:38:16 +0200

    build: vendor typescript 3.6 in build optimizer
    With this change we "vendor" TypeScript 3.6 in build optimizer instead of using directly the npm package.
    
    Reasons:
    - We cannot update to a more recent version of TypeScript due to https://github.com/microsoft/TypeScript/issues/38412
    - Yarn workspace are not supported under Bazel. This means currently we are running unit testswhich uses a different TypeScript version at runtime.
    - In TypeScript 3.9, the shape of ES2015 classes changed https://github.com/microsoft/TypeScript/pull/32011, this requires some changes in the UT expects, but this again will not be correct to change now.

--- packages/angular_devkit/build_optimizer/src/transforms/prefix-classes.ts
+++ packages/angular_devkit/build_optimizer/src/transforms/prefix-classes.ts
@@ -5,7 +5,7 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import * as ts from 'typescript';
+import * as ts from '../../third_party/github.com/Microsoft/TypeScript/lib/typescript';
 import { addPureComment } from '../helpers/ast-utils';
 
 export function testPrefixClasses(content: string) {
