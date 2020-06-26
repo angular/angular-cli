0ebf8462a431a4bcefdf36bceeca0faf027f486b
Alan Agius <alan.agius4@gmail.com>
2020-06-26 10:45:56 +0200

    fix(express-engine): fix return type for app method

--- modules/express-engine/schematics/install/files/__serverFileName@stripTsExtension__.ts
+++ modules/express-engine/schematics/install/files/__serverFileName@stripTsExtension__.ts
@@ -9,7 +9,7 @@ import { APP_BASE_HREF } from '@angular/common';
 import { existsSync } from 'fs';
 
 // The Express app is exported so that it can be used by serverless Functions.
-export function app(): void {
+export function app(): express.Express {
   const server = express();
   const distFolder = join(process.cwd(), '<%= browserDistDirectory %>');
   const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
