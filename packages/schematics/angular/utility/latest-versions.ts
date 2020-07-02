a6543c4c376de45794637edfd46591d297d71193
Alan Agius <alan.agius4@gmail.com>
2020-07-02 11:41:41 +0200

    build: update rxjs in latest-versions.ts to 6.6.0

--- packages/schematics/angular/utility/latest-versions.ts
+++ packages/schematics/angular/utility/latest-versions.ts
@@ -9,7 +9,7 @@
 export const latestVersions = {
   // These versions should be kept up to date with latest Angular peer dependencies.
   Angular: '~10.0.0-rc.0',
-  RxJs: '~6.5.4',
+  RxJs: '~6.6.0',
   ZoneJs: '~0.10.2',
   TypeScript: '~3.9.5',
   TsLib: '^2.0.0',
