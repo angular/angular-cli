3d9f2a30ecf796a1f5b34566dd8f66a2bd9dd715
Alan Agius <alan.agius4@gmail.com>
2020-07-08 15:33:21 +0200

    fix(@angular-devkit/schematics-cli): only show prompts in TTY terminals

--- packages/angular_devkit/schematics_cli/bin/schematics.ts
+++ packages/angular_devkit/schematics_cli/bin/schematics.ts
@@ -270,8 +270,9 @@ export async function main({
   delete parsedArgs._;
 
   // Add prompts.
-  workflow.registry.usePromptProvider(_createPromptProvider());
-
+  if (argv['interactive'] && isTTY()) {
+    workflow.registry.usePromptProvider(_createPromptProvider());
+  }
 
   /**
    *  Execute the workflow, which will report the dry run events, run the tasks, and complete
@@ -337,6 +338,8 @@ function getUsage(): string {
       --list-schematics   List all schematics from the collection, by name. A collection name
                           should be suffixed by a colon. Example: '@schematics/schematics:'.
 
+      --no-interactive    Disables interactive input prompts.
+
       --verbose           Show more information.
 
       --help              Show this message.
@@ -357,6 +360,7 @@ const booleanArgs = [
   'list-schematics',
   'listSchematics',
   'verbose',
+  'interactive',
 ];
 
 function parseArgs(args: string[] | undefined): minimist.ParsedArgs {
@@ -368,11 +372,27 @@ function parseArgs(args: string[] | undefined): minimist.ParsedArgs {
         'allowPrivate': 'allow-private',
       },
       default: {
+        'interactive': true,
         'debug': null,
         'dryRun': null,
       },
       '--': true,
     });
+}
+
+function isTTY(): boolean {
+  const isTruthy = (value: undefined | string) => {
+    // Returns true if value is a string that is anything but 0 or false.
+    return value !== undefined && value !== '0' && value.toUpperCase() !== 'FALSE';
+  };
+
+  // If we force TTY, we always return true.
+  const force = process.env['NG_FORCE_TTY'];
+  if (force !== undefined) {
+    return isTruthy(force);
+  }
+
+  return !!process.stdout.isTTY && !isTruthy(process.env['CI']);
 }
 
 if (require.main === module) {
