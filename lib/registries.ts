8a785e0815bdc9769046a1597d82d6d07f2d2d4c
Doug Parker <dgp1130@users.noreply.github.com>
2020-07-10 11:15:55 -0700

    refactor: move Wombat registry to reusable constant
    Pure refactor that just pulls the Wombat registry into a separate string constant to be re-used across scripts.

---
+++ lib/registries.ts
@@ -0,0 +1,10 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+/** URL to Wombat NPM registry proxy. */
+export const wombat = 'https://wombat-dressing-room.appspot.com';
