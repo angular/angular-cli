eb4b9fcaf37e765fdc58a1a99969964b3c33da8a
Alan Agius <alan.agius4@gmail.com>
2020-07-21 10:20:11 +0200

    refactor: remove no longer needed workaround for AJV

--- packages/angular_devkit/build_angular/src/index.ts
+++ packages/angular_devkit/build_angular/src/index.ts
@@ -6,13 +6,6 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-// TODO: remove this commented AJV require.
-// We don't actually require AJV, but there is a bug with NPM and peer dependencies that is
-// whose workaround is to depend on AJV.
-// See https://github.com/angular/angular-cli/issues/9691#issuecomment-367322703 for details.
-// We need to add a require here to satisfy the dependency checker.
-// require('ajv');
-
 export * from './transforms';
 
 export {
