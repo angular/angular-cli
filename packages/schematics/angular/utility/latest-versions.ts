441ce5fa3cf7b93d7c5434f0ea5aaa2f5f93b9ee
Keen Yee Liau <kyliau@google.com>
2020-07-22 13:26:54 -0700

    v10.1.0-next.2

--- packages/schematics/angular/utility/latest-versions.ts
+++ packages/schematics/angular/utility/latest-versions.ts
@@ -8,7 +8,7 @@
 
 export const latestVersions = {
   // These versions should be kept up to date with latest Angular peer dependencies.
-  Angular: '~10.1.0-next.1',
+  Angular: '~10.1.0-next.2',
   RxJs: '~6.6.0',
   ZoneJs: '~0.10.2',
   TypeScript: '~3.9.5',
@@ -18,9 +18,9 @@ export const latestVersions = {
   // For our e2e tests, these versions must match the latest tag present on the branch.
   // During RC periods they will not match the latest RC until there's a new git tag, and
   // should not be updated.
-  DevkitBuildAngular: '~0.1001.0-next.1',
-  DevkitBuildNgPackagr: '~0.1001.0-next.1',
-  DevkitBuildWebpack: '~0.1001.0-next.1',
+  DevkitBuildAngular: '~0.1001.0-next.2',
+  DevkitBuildNgPackagr: '~0.1001.0-next.2',
+  DevkitBuildWebpack: '~0.1001.0-next.2',
 
   ngPackagr: '^10.0.0',
 };
