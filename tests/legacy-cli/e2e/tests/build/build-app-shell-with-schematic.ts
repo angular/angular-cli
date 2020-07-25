480f673cbbe5215fa51ec21490b23c7d856f9dae
Charles Lyding <19598772+clydin@users.noreply.github.com>
2020-07-25 10:42:30 -0400

    test: use correct package versions in platform-server E2E tests

--- tests/legacy-cli/e2e/tests/build/build-app-shell-with-schematic.ts
+++ tests/legacy-cli/e2e/tests/build/build-app-shell-with-schematic.ts
@@ -2,20 +2,31 @@ import { getGlobalVariable } from '../../utils/env';
 import { appendToFile, expectFileToMatch } from '../../utils/fs';
 import { ng, silentNpm } from '../../utils/process';
 import { updateJsonFile } from '../../utils/project';
-import { readNgVersion } from '../../utils/version';
 
+const snapshots = require('../../ng-snapshot/package.json');
 
 export default async function () {
   await appendToFile('src/app/app.component.html', '<router-outlet></router-outlet>');
   await ng('generate', 'appShell', '--client-project', 'test-project');
-  await updateJsonFile('package.json', packageJson => {
-    const dependencies = packageJson['dependencies'];
-    dependencies['@angular/platform-server'] = getGlobalVariable('argv')['ng-snapshots']
-      ? require('../../ng-snapshot/package.json').dependencies['@angular/platform-server']
-      : readNgVersion();
-  });
 
-  await silentNpm('install');
+  const isSnapshotBuild = getGlobalVariable('argv')['ng-snapshots'];
+  if (isSnapshotBuild) {
+    const packagesToInstall = [];
+    await updateJsonFile('package.json', (packageJson) => {
+      const dependencies = packageJson['dependencies'];
+      // Iterate over all of the packages to update them to the snapshot version.
+      for (const [name, version] of Object.entries(snapshots.dependencies)) {
+        if (name in dependencies && dependencies[name] !== version) {
+          packagesToInstall.push(version);
+        }
+      }
+    });
+
+    for (const pkg of packagesToInstall) {
+      await silentNpm('install', pkg);
+    }
+  }
+
   await ng('run', 'test-project:app-shell');
   await expectFileToMatch('dist/test-project/browser/index.html', /app-shell works!/);
 
--- tests/legacy-cli/e2e/tests/build/platform-server.ts
+++ tests/legacy-cli/e2e/tests/build/platform-server.ts
@@ -3,22 +3,43 @@ import { getGlobalVariable } from '../../utils/env';
 import { appendToFile, expectFileToMatch, writeFile } from '../../utils/fs';
 import { exec, ng, silentNpm } from '../../utils/process';
 import { updateJsonFile } from '../../utils/project';
-import { readNgVersion } from '../../utils/version';
+
+const snapshots = require('../../ng-snapshot/package.json');
 
 export default async function () {
   const argv = getGlobalVariable('argv');
   const veEnabled = argv['ve'];
 
-  await ng('add', '@nguniversal/express-engine@9.0.0-next.6');
+  // @nguniversal/express-engine currently relies on ^0.1000.0 of @angular-devkit/architect
+  // which is not present in the local package registry and not semver compatible with 0.1001.0+
+  const { stdout: stdout1 } = await silentNpm('pack', '@angular-devkit/architect@0.1000', '--registry=https://registry.npmjs.org');
+  await silentNpm('publish', stdout1.trim(), '--registry=http://localhost:4873', '--tag=minor');
 
-  await updateJsonFile('package.json', packageJson => {
-    const dependencies = packageJson['dependencies'];
-    dependencies['@angular/platform-server'] = getGlobalVariable('argv')['ng-snapshots']
-      ? require('../../ng-snapshot/package.json').dependencies['@angular/platform-server']
-      : readNgVersion();
-  });
+  // @nguniversal/express-engine currently relies on ^10.0.0 of @angular-devkit/core
+  // which is not present in the local package registry and not semver compatible prerelease version of 10.1.0
+  const { stdout: stdout2 } = await silentNpm('pack', '@angular-devkit/core@10.0', '--registry=https://registry.npmjs.org');
+  await silentNpm('publish', stdout2.trim(), '--registry=http://localhost:4873', '--tag=minor');
 
-  await silentNpm('install');
+  await ng('add', '@nguniversal/express-engine');
+
+  const isSnapshotBuild = getGlobalVariable('argv')['ng-snapshots'];
+  if (isSnapshotBuild) {
+    const packagesToInstall = [];
+    await updateJsonFile('package.json', (packageJson) => {
+      const dependencies = packageJson['dependencies'];
+      // Iterate over all of the packages to update them to the snapshot version.
+      for (const [name, version] of Object.entries(snapshots.dependencies)) {
+        if (name in dependencies && dependencies[name] !== version) {
+          packagesToInstall.push(version);
+        }
+      }
+    });
+
+    for (const pkg of packagesToInstall) {
+      await silentNpm('install', pkg);
+    }
+  }
+
   if (veEnabled) {
     // todo: https://github.com/angular/angular-cli/issues/15851
     // We need to fix the 'export_ngfactory' transformer as with the
