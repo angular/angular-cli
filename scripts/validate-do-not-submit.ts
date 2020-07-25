f8e4e66811c3ab82c26689756e4e061f1c75a1ca
Charles Lyding <19598772+clydin@users.noreply.github.com>
2020-07-24 21:10:44 -0400

    ci: prevent validation failure of non-fork PRs

--- scripts/validate-do-not-submit.ts
+++ scripts/validate-do-not-submit.ts
@@ -50,6 +50,10 @@ export default async function (argv: ValidateCommitsOptions, logger: logging.Log
   let errorCount = 0;
 
   for (const fileName of files) {
+    if (!fileName || fileName.trim().length === 0) {
+      continue;
+    }
+
     const diff = execSync(`git diff "${baseSha}..${sha}" -- "${fileName}"`, {
       // Increase the default buffer size by 100. Diffs can be quite large.
       maxBuffer: 100 * 1024 * 1024,
