bce881d8a849a49349a72d1e1aef1eca57e1d620
Alan Agius <alan.agius4@gmail.com>
2020-07-28 12:02:27 +0200

    fix(@angular-devkit/build-angular): improve debugging experience
    With this change we improve the debugging experience with 2 things.
    
    1. Cleaner Karma stack traces in the terminal
    Before
    ```
    Chrome 84.0.4147.105 (Mac OS 10.15.5) AppComponent should have as title 'foo' FAILED
            Error: Expected 'foo' to equal 'fox'.
                at <Jasmine>
                at UserContext.<anonymous> (http://localhost:9876/_karma_webpack_/webpack:/projects/foo/src/app/app.component.spec.ts:22:23)
                at ZoneDelegate.invoke (http://localhost:9876/_karma_webpack_/webpack:/node_modules/zone.js/dist/zone-evergreen.js:364:1)
                at ProxyZoneSpec.push../node_modules/zone.js/dist/zone-testing.js.ProxyZoneSpec.onInvoke (http://localhost:9876/_karma_webpack_/webpack:/node_modules/zone.js/dist/zone-testing.js:292:1)
    Chrome 84.0.4147.105 (Mac OS 10.15.5): Executed 3 of 3 (1 FAILED) (0 secs / 0.214 secs)
    Chrome 84.0.4147.105 (Mac OS 10.15.5) AppComponent should have as title 'foo' FAILED
            Error: Expected 'foo' to equal 'fox'.
                at <Jasmine>
                at UserContext.<anonymous> (http://localhost:9876/_karma_webpack_/webpack:/projects/foo/src/app/app.component.spec.ts:22:23)
                at ZoneDelegate.invoke (http://localhost:9876/_karma_webpack_/webpack:/node_modules/zone.js/dist/zone-evergreen.js:364:1)
                at ProxyZoneSpec.push../node_modules/zone.js/dist/zone-testing.js.ProxyZoneSpec.onInvoke (http://localhost:9876/_karma_webpack_/webpack:/node_modules/zone.js/dist/zChrome 84.0.4147.105 (Mac OS 10.15.5): Executed 3 of 3 (1 FAILED) (0.225 secs / 0.214 secs)
    ```
    
    After
    ```
    Chrome 84.0.4147.105 (Mac OS 10.15.5) AppComponent should have as title 'foo' FAILED
            Error: Expected 'foo' to equal 'fox'.
                at <Jasmine>
                at UserContext.<anonymous> (projects/foo/src/app/app.component.spec.ts:22:23)
                at ZoneDelegate.invoke (node_modules/zone.js/dist/zone-evergreen.js:364:1)
                at ProxyZoneSpec.push../node_modules/zone.js/dist/zone-testing.js.ProxyZoneSpec.onInvoke (node_modules/zone.js/dist/zone-testing.js:292:1)
    Chrome 84.0.4147.105 (Mac OS 10.15.5): Executed 2 of 3 (1 FAILED) (0 secs / 0.225 secs)
    Chrome 84.0.4147.105 (Mac OS 10.15.5) AppComponent should have as title 'foo' FAILED
            Error: Expected 'foo' to equal 'fox'.
                at <Jasmine>
                at UserContext.<anonymous> (projects/foo/src/app/app.component.spec.ts:22:23)
                at ZoneDelegate.invoke (node_modules/zone.js/dist/zone-evergreen.js:364:1)
    Chrome 84.0.4147.105 (Mac OS 10.15.5): Executed 3 of 3 (1 FAILED) (0.281 secs / 0.237 secs)
    ```
    
    2. Reduce VS Code configuration
    Before
    ```
    {
      "name": "ng test",
      "type": "chrome",
      "request": "launch",
      "url": "http://localhost:9876/debug.html",
      "webRoot": "${workspaceFolder}",
      "sourceMaps": true,
      "sourceMapPathOverrides": {
        "webpack:/*": "${webRoot}/*",
        "/./*": "${webRoot}/*",
        "/src/*": "${webRoot}/*",
        "/*": "*",
        "/./~/*": "${webRoot}/node_modules/*"
      }
    },
    ```
    
    After
    ```
    {
      "name": "ng test",
      "type": "chrome",
      "request": "launch",
      "url": "http://localhost:9876/debug.html",
      "webRoot": "${workspaceFolder}",
    },
    ```

--- packages/angular_devkit/build_angular/src/angular-cli-files/models/webpack-configs/utils.ts
+++ packages/angular_devkit/build_angular/src/angular-cli-files/models/webpack-configs/utils.ts
@@ -91,7 +91,7 @@ export function getSourceMapDevTool(
     // inline sourcemaps as otherwise paths to sourcemaps will be broken in browser
     // `webpack:///` is needed for Visual Studio breakpoints to work properly as currently
     // there is no way to set the 'webRoot'
-    sourceRoot: inlineSourceMap ? '' : 'webpack:///',
+    sourceRoot: 'webpack:///',
     moduleFilenameTemplate: '[resource-path]',
     append: hiddenSourceMap ? false : undefined,
     exclude: vendorSourceMap ? undefined : /vendor.*\.js/,
---
+++ tests/legacy-cli/e2e/tests/misc/karma-error-paths.ts
@@ -0,0 +1,24 @@
+import { writeMultipleFiles } from '../../utils/fs';
+import { ng } from '../../utils/process';
+import { expectToFail } from '../../utils/utils';
+
+export default async function () {
+  await writeMultipleFiles({
+    'src/app/app.component.spec.ts': `
+        describe('AppComponent', () => {
+          it('failing test', () => {
+            expect('1').toEqual('2');
+          });
+        });
+      `,
+  });
+
+  const { message } = await expectToFail(() => ng('test', '--no-watch'));
+  if (message.includes('_karma_webpack_')) {
+    throw new Error(`Didn't expect logs to server address and webpack scheme.\n${message}`);
+  }
+
+  if (!message.includes('(src/app/app.component.spec.ts:4:25)')) {
+    throw new Error(`Expected logs to contain relative path to (src/app/app.component.spec.ts:4:25)\n${message}`);
+  }
+}
