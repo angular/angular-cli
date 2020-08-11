e8bbf0d3afc77e9e12cfb1b6c952102cc899d081
Charles Lyding <19598772+clydin@users.noreply.github.com>
2020-08-11 10:26:05 -0400

    refactor(@angular-devkit/build-angular): cleanup server webpack configuration

--- packages/angular_devkit/build_angular/src/angular-cli-files/models/webpack-configs/server.ts
+++ packages/angular_devkit/build_angular/src/angular-cli-files/models/webpack-configs/server.ts
@@ -11,8 +11,8 @@ import { WebpackConfigOptions } from '../build-options';
 import { getSourceMapDevTool } from './utils';
 
 /**
- * Returns a partial specific to creating a bundle for node
- * @param wco Options which are include the build options and app config
+ * Returns a partial Webpack configuration specific to creating a bundle for node
+ * @param wco Options which include the build options and app config
  */
 export function getServerConfig(wco: WebpackConfigOptions): Configuration {
   const {
@@ -22,11 +22,14 @@ export function getServerConfig(wco: WebpackConfigOptions): Configuration {
   } = wco.buildOptions;
 
   const extraPlugins = [];
-  if (sourceMap) {
-    const { scripts, styles, hidden } = sourceMap;
-    if (scripts || styles) {
-      extraPlugins.push(getSourceMapDevTool(scripts, styles, hidden));
-    }
+  const { scripts, styles, hidden } = sourceMap;
+  if (scripts || styles) {
+    extraPlugins.push(getSourceMapDevTool(scripts, styles, hidden));
+  }
+
+  const externals: Configuration['externals'] = [...externalDependencies];
+  if (!bundleDependencies) {
+    externals.push(externalizePackages);
   }
 
   const config: Configuration = {
@@ -44,31 +47,29 @@ export function getServerConfig(wco: WebpackConfigOptions): Configuration {
       ...extraPlugins,
     ],
     node: false,
+    externals,
   };
 
-  if (bundleDependencies) {
-    config.externals = [...externalDependencies];
-  } else {
-    config.externals = [
-      ...externalDependencies,
-      (context: string, request: string, callback: (error?: null, result?: string) => void) => {
-        // Absolute & Relative paths are not externals
-        if (request.startsWith('.') || isAbsolute(request)) {
-          callback();
+  return config;
+}
 
-          return;
-        }
+function externalizePackages(
+  context: string,
+  request: string,
+  callback: (error?: Error, result?: string) => void,
+): void {
+  // Absolute & Relative paths are not externals
+  if (request.startsWith('.') || isAbsolute(request)) {
+    callback();
 
-        try {
-          require.resolve(request);
-          callback(null, request);
-        } catch {
-          // Node couldn't find it, so it must be user-aliased
-          callback();
-        }
-      },
-    ];
+    return;
   }
 
-  return config;
+  try {
+    require.resolve(request, { paths: [context] });
+    callback(undefined, request);
+  } catch {
+    // Node couldn't find it, so it must be user-aliased
+    callback();
+  }
 }
