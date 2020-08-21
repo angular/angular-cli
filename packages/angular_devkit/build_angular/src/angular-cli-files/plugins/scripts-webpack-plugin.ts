0d7e0f5eb5c255efe4908991fca579323beae8f6
Charles Lyding <19598772+clydin@users.noreply.github.com>
2020-08-21 14:53:43 -0400

    style(@angular-devkit/build-angular): remove duplicate license header

--- packages/angular_devkit/build_angular/src/angular-cli-files/plugins/scripts-webpack-plugin.ts
+++ packages/angular_devkit/build_angular/src/angular-cli-files/plugins/scripts-webpack-plugin.ts
@@ -1,20 +1,13 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
 // tslint:disable
 // TODO: cleanup this file, it's copied as is from Angular CLI.
 
-/**
- * @license
- * Copyright Google Inc. All Rights Reserved.
- *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.io/license
- */
-/**
- * @license
- * Copyright Google Inc. All Rights Reserved.
- *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.io/license
- */
 import { Compiler, loader } from 'webpack';
 import { CachedSource, ConcatSource, OriginalSource, RawSource, Source } from 'webpack-sources';
 import { interpolateName } from 'loader-utils';
