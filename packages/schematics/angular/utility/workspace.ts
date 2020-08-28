20bb298d4a6b5befa06f1d73697774e0cf4e5dc0
Charles Lyding <19598772+clydin@users.noreply.github.com>
2020-08-28 15:22:13 -0400

    refactor(@schematics/angular): allow followup rules with workspace helper
    This change allows the `updateWorkspace` helper rule to optionally return a rule that will be executed after the workspace is updated.

--- packages/schematics/angular/utility/workspace.ts
+++ packages/schematics/angular/utility/workspace.ts
@@ -6,7 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 import { virtualFs, workspaces } from '@angular-devkit/core';
-import { Rule, Tree } from '@angular-devkit/schematics';
+import { Rule, Tree, noop } from '@angular-devkit/schematics';
 import { ProjectType } from './workspace-models';
 
 function createHost(tree: Tree): workspaces.WorkspaceHost {
@@ -33,14 +33,14 @@ function createHost(tree: Tree): workspaces.WorkspaceHost {
 }
 
 export function updateWorkspace(
-  updater: (workspace: workspaces.WorkspaceDefinition) => void | PromiseLike<void>,
+  updater: (workspace: workspaces.WorkspaceDefinition) => void | Rule | PromiseLike<void | Rule>,
 ): Rule;
 export function updateWorkspace(
   workspace: workspaces.WorkspaceDefinition,
 ): Rule;
 export function updateWorkspace(
   updaterOrWorkspace: workspaces.WorkspaceDefinition
-    | ((workspace: workspaces.WorkspaceDefinition) => void | PromiseLike<void>),
+    | ((workspace: workspaces.WorkspaceDefinition) => void | Rule | PromiseLike<void | Rule>),
 ): Rule {
   return async (tree: Tree) => {
     const host = createHost(tree);
@@ -49,14 +49,15 @@ export function updateWorkspace(
 
       const { workspace } = await workspaces.readWorkspace('/', host);
 
-      const result = updaterOrWorkspace(workspace);
-      if (result !== undefined) {
-        await result;
-      }
+      const result = await updaterOrWorkspace(workspace);
 
       await workspaces.writeWorkspace(workspace, host);
+
+      return result || noop;
     } else {
       await workspaces.writeWorkspace(updaterOrWorkspace, host);
+
+      return noop;
     }
   };
 }
