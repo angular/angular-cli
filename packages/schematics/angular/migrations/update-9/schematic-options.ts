eef1d311ab12db6cde21eb43464ff28e3b6d1f8d
Charles Lyding <19598772+clydin@users.noreply.github.com>
2020-08-31 09:06:06 -0400

    refactor(@schematics/angular): use new workspace helpers in schematic-options migration

--- packages/schematics/angular/migrations/update-9/schematic-options.ts
+++ packages/schematics/angular/migrations/update-9/schematic-options.ts
@@ -5,62 +5,31 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import { JsonAstObject } from '@angular-devkit/core';
-import { Rule, UpdateRecorder } from '@angular-devkit/schematics';
-import { getWorkspacePath } from '../../utility/config';
-import { findPropertyInAstObject } from '../../utility/json-utils';
-import { getWorkspace } from './utils';
+import { json } from '@angular-devkit/core';
+import { Rule } from '@angular-devkit/schematics';
+import { updateWorkspace } from '../../utility/workspace';
 
 export default function(): Rule {
-  return tree => {
-    const workspacePath = getWorkspacePath(tree);
-    const workspace = getWorkspace(tree);
-    const recorder = tree.beginUpdate(workspacePath);
-
-    const rootSchematics = findSchematicsField(workspace);
-    if (rootSchematics) {
-      updateSchematicsField(rootSchematics, recorder);
+  return updateWorkspace(workspace => {
+    // Update root level schematics options if present
+    const rootSchematics = workspace.extensions.schematics;
+    if (rootSchematics && json.isJsonObject(rootSchematics)) {
+      updateSchematicsField(rootSchematics);
     }
 
-    const projects = findPropertyInAstObject(workspace, 'projects');
-    if (!projects || projects.kind !== 'object' || !projects.properties) {
-      return;
-    }
-
-    for (const { value } of projects.properties) {
-      if (value.kind !== 'object') {
-        continue;
+    // Update project level schematics options if present
+    for (const [, project] of workspace.projects) {
+      const projectSchematics = project.extensions.schematics;
+      if (projectSchematics && json.isJsonObject(projectSchematics)) {
+        updateSchematicsField(projectSchematics);
       }
-
-      const projectSchematics = findSchematicsField(value);
-      if (!projectSchematics) {
-        continue;
-      }
-
-      updateSchematicsField(projectSchematics, recorder);
     }
-
-    tree.commitUpdate(recorder);
-
-    return tree;
-  };
+  });
 }
 
-function findSchematicsField(value: JsonAstObject): JsonAstObject | null {
-  const schematics = findPropertyInAstObject(value, 'schematics');
-  if (schematics && schematics.kind == 'object') {
-    return schematics;
-  }
-
-  return null;
-}
-
-function updateSchematicsField(schematics: JsonAstObject, recorder: UpdateRecorder): void {
-  for (const {
-    key: { value: schematicName },
-    value: schematicValue,
-  } of schematics.properties) {
-    if (schematicValue.kind !== 'object') {
+function updateSchematicsField(schematics: json.JsonObject): void {
+  for (const [schematicName, schematicOptions] of Object.entries(schematics)) {
+    if (!json.isJsonObject(schematicOptions)) {
       continue;
     }
 
@@ -68,23 +37,17 @@ function updateSchematicsField(schematics: JsonAstObject, recorder: UpdateRecord
       continue;
     }
 
-    for (const { key: optionKey, value: optionValue } of schematicValue.properties) {
-      if (optionKey.value === 'styleext') {
-        // Rename `styleext` to `style
-        const offset = optionKey.start.offset + 1;
-        recorder.remove(offset, optionKey.value.length);
-        recorder.insertLeft(offset, 'style');
-      } else if (optionKey.value === 'spec') {
-        // Rename `spec` to `skipTests`
-        const offset = optionKey.start.offset + 1;
-        recorder.remove(offset, optionKey.value.length);
-        recorder.insertLeft(offset, 'skipTests');
+    // Replace `styleext` with `style`
+    if (schematicOptions.styleext !== undefined) {
+      schematicOptions.style = schematicOptions.styleext;
+      delete schematicOptions.styleext;
+    }
 
-        // invert value
-        const { start, end } = optionValue;
-        recorder.remove(start.offset, end.offset - start.offset);
-        recorder.insertLeft(start.offset, `${!optionValue.value}`);
-      }
+    // Replace `spec` with `skipTests`
+    if (schematicOptions.spec !== undefined) {
+      // skipTests value is inverted
+      schematicOptions.skipTests = !schematicOptions.spec;
+      delete schematicOptions.spec;
     }
   }
 }
