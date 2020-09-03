7d621abc1a37d28eee174e5853d3fc711af3d33b
Alan Agius <alan.agius4@gmail.com>
2020-09-03 19:50:01 +0200

    fix(@angular-devkit/build-angular): consider ascii_only terser setting when counting components in analytics
    (cherry picked from commit ceeb6fba34b736d134fa4cd12f8c6d31de0b91b8)

--- packages/angular_devkit/build_angular/plugins/webpack/analytics.ts
+++ packages/angular_devkit/build_angular/plugins/webpack/analytics.ts
@@ -161,6 +161,8 @@ export class NgBuildAnalyticsPlugin {
       this._stats.numberOfComponents += countOccurrences(module._source.source(), 'Component({');
       // For Ivy we just count ɵcmp.
       this._stats.numberOfComponents += countOccurrences(module._source.source(), '.ɵcmp', true);
+      // for ascii_only true
+      this._stats.numberOfComponents += countOccurrences(module._source.source(), '.\u0275cmp', true);
     }
   }
 
@@ -172,6 +174,8 @@ export class NgBuildAnalyticsPlugin {
       // Count the number of `.ɵccf(` strings (case sensitive). They're calls to components
       // factories.
       this._stats.numberOfComponents += countOccurrences(module._source.source(), '.ɵccf(');
+      // for ascii_only true
+      this._stats.numberOfComponents += countOccurrences(module._source.source(), '.\u0275ccf(');
     }
   }
 
