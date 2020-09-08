e5266fdd443a4fc408d6d8fd2133334d50031e8f
Charles Lyding <19598772+clydin@users.noreply.github.com>
2020-09-08 18:21:40 -0400

    refactor(@angular-devkit/build-angular): temporarily disable Webpack 5 license extraction
    The license extraction plugin is currently causing errors when building/serving with Webpack 5.  This change temporarily disables the option when Webpack 5 is used until further investigation can take place and to unblock initial Webpack 5 support.

--- packages/angular_devkit/build_angular/src/webpack/configs/browser.ts
+++ packages/angular_devkit/build_angular/src/webpack/configs/browser.ts
@@ -7,7 +7,7 @@
  */
 import * as webpack from 'webpack';
 import { WebpackConfigOptions } from '../../utils/build-options';
-import { withWebpackFourOrFive } from '../../utils/webpack-version';
+import { isWebpackFiveOrHigher, withWebpackFourOrFive } from '../../utils/webpack-version';
 import { CommonJsUsageWarnPlugin } from '../plugins';
 import { getSourceMapDevTool } from '../utils/helpers';
 
@@ -38,7 +38,13 @@ export function getBrowserConfig(wco: WebpackConfigOptions): webpack.Configurati
     }));
   }
 
-  if (extractLicenses) {
+  // TODO_WEBPACK_5: Investigate build/serve issues with the `license-webpack-plugin` package
+  if (extractLicenses && isWebpackFiveOrHigher()) {
+    wco.logger.warn(
+      'WARNING: License extraction is currently disabled when using Webpack 5. ' +
+        'This is temporary and will be corrected in a future update.',
+    );
+  } else if (extractLicenses) {
     const LicenseWebpackPlugin = require('license-webpack-plugin').LicenseWebpackPlugin;
     extraPlugins.push(new LicenseWebpackPlugin({
       stats: {
