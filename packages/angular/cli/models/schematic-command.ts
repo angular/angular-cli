64cc005f0b737a3e5fde082612cfff9c110748be
Alan Agius <alan.agius4@gmail.com>
2020-09-14 21:57:46 +0200

    refactor: change UPDATE action color to cyan

--- packages/angular/cli/models/schematic-command.ts
+++ packages/angular/cli/models/schematic-command.ts
@@ -501,7 +501,7 @@ export abstract class SchematicCommand<
           break;
         case 'update':
           loggingQueue.push(tags.oneLine`
-            ${colors.white('UPDATE')} ${eventPath} (${event.content.length} bytes)
+            ${colors.cyan('UPDATE')} ${eventPath} (${event.content.length} bytes)
           `);
           break;
         case 'create':
