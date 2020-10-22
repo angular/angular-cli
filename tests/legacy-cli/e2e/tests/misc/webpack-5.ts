ed9163e503e22c7e46fcfcb059efb25e1f1aa6a5
Charles Lyding <19598772+clydin@users.noreply.github.com>
2020-10-22 12:50:10 -0400

    test(@angular-devkit/build-webpack): update webpack 5 E2E to 5.1.3

--- tests/legacy-cli/e2e/tests/misc/webpack-5.ts
+++ tests/legacy-cli/e2e/tests/misc/webpack-5.ts
@@ -6,7 +6,7 @@ export default async function() {
   // Setup project for yarn usage
   await rimraf('node_modules');
   await updateJsonFile('package.json', (json) => {
-    json.resolutions = { webpack: '5.1.0' };
+    json.resolutions = { webpack: '5.1.3' };
   });
   await silentYarn();
   await silentYarn('webdriver-update');
