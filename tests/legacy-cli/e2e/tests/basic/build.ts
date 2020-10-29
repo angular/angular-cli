66abb226a1b6672b6c14a7d2afb0f2dbd0b738e0
Alan Agius <alan.agius4@gmail.com>
2020-10-29 09:04:36 +0100

    test(@angular-devkit/build-angular): add test to check build logs
    (cherry picked from commit 4205452cc962d829c161eda7d52aa0e39aa46291)

--- tests/legacy-cli/e2e/tests/basic/build.ts
+++ tests/legacy-cli/e2e/tests/basic/build.ts
@@ -1,4 +1,4 @@
-import { expectFileToMatch } from '../../utils/fs';
+import { expectFileToMatch, replaceInFile } from '../../utils/fs';
 import { ng } from '../../utils/process';
 
 
@@ -12,8 +12,34 @@ export default async function() {
   await ng('build', 'test-project', '--no-progress');
   await ng('build', '--no-progress', 'test-project');
 
+  // Enable Differential loading to run both size checks
+  await replaceInFile(
+    '.browserslistrc',
+    'not IE 11',
+    'IE 11',
+  );
   // Production build
-  await ng('build', '--prod');
-  await expectFileToMatch('dist/test-project/index.html', /main\.[a-zA-Z0-9]{20}\.js/);
-  await ng('build', '--prod', '--no-progress', 'test-project');
+  const { stderr: stderrProgress } = await ng('build', '--prod', '--progress');
+  await expectFileToMatch('dist/test-project/index.html', /main-es5\.[a-zA-Z0-9]{20}\.js/);
+  await expectFileToMatch('dist/test-project/index.html', /main-es2015\.[a-zA-Z0-9]{20}\.js/);
+
+  const logs: string[] = [
+    'Browser application bundle generation complete',
+    'ES5 bundle generation complete',
+    'Copying assets complete',
+    'Index html generation complete',
+  ];
+
+  for (const log of logs) {
+    if (!stderrProgress.includes(log)) {
+      throw new Error(`Expected stderr to contain '${log}' but didn't.\n${stderrProgress}`);
+    }
+  }
+
+  const { stderr: stderrNoProgress } = await ng('build', '--prod', '--no-progress');
+  for (const log of logs) {
+    if (stderrNoProgress.includes(log)) {
+      throw new Error(`Expected stderr not to contain '${log}' but it did.\n${stderrProgress}`);
+    }
+  }
 }
