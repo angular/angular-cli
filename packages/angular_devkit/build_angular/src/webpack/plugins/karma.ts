0a32f853c890fc1ca349b1258ee2406e8fb694d3
Keen Yee Liau <kyliau@google.com>
2020-11-10 11:52:16 -0800

    fix(@angular-devkit/build-angular): Downgrade Karma to 5.1.x
    There's a bug in Karma 5.2.x that causes unit tests to fail when they
    should not. It was supposed to be fixed in
    https://github.com/karma-runner/karma/pull/3565 but CI still failed,
    blocking the release of Karma.
    
    In order to mitigate this, we temporarily downgrade Karma back to 5.1.x
    
    A side-effect of this is that coverage tests that uses karma-coverage
    and exceeds the threshold will not fail, because karma-coverage requires
    Karma 5.2.x. We expect this to be fixed very soon.

--- packages/angular_devkit/build_angular/src/webpack/plugins/karma.ts
+++ packages/angular_devkit/build_angular/src/webpack/plugins/karma.ts
@@ -99,6 +99,7 @@ const init: any = (config: any, emitter: any, customFileHandlers: any) => {
     const hasIstanbulReporter = reporters.includes('coverage-istanbul');
     if (hasCoveragePlugin && !hasCoverageReporter) {
       reporters.push('coverage');
+      logger.warn(`There is a known issue with Karma and karma-coverage. Tests that exceed the coverage threshold will not fail. This will be fixed in Karma 5.2.x soon.`)
     }
     else if (hasIstanbulPlugin && !hasIstanbulReporter) {
       // coverage-istanbul is deprecated in favor of karma-coverage
