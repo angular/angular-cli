277cda46d4fa927397108bbac75c5da4575eb468
Alan Agius <alan.agius4@gmail.com>
2020-11-30 09:35:18 +0100

    fix(@angular-devkit/build-angular): resolve partial SCSS files in sourcemaps
    Closes #19505
    
    (cherry picked from commit 064d62014cba8abcc5c26aba07d33e4ef03129cd)

--- packages/angular_devkit/build_angular/src/webpack/configs/styles.ts
+++ packages/angular_devkit/build_angular/src/webpack/configs/styles.ts
@@ -221,9 +221,7 @@ export function getStylesConfig(wco: WebpackConfigOptions) {
 
   // load global css as css files
   if (globalStylePaths.length > 0) {
-    const globalSourceMap = !!(
-      cssSourceMap && !buildOptions.extractCss && !buildOptions.sourceMap.hidden
-    );
+    const globalSourceMap = !!cssSourceMap && !buildOptions.sourceMap.hidden;
 
     rules.push(
       ...baseRules.map(({ test, use }) => {
