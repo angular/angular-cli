f3059b995a4b9829992cb2166d6d24f857d066f7
Charles Lyding <19598772+clydin@users.noreply.github.com>
2020-12-08 14:37:34 -0500

    test(@angular-devkit/build-angular): use harness for browser builder works test
    This converts the 'works' spec to use the new builder harness for testing. The change also provides for a before/after comparison of an existing test.

--- packages/angular_devkit/build_angular/src/browser/specs/works_spec.ts
+++ packages/angular_devkit/build_angular/src/browser/specs/works_spec.ts
@@ -5,33 +5,41 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
+import { describeBuilder } from '../../testing';
+import { buildWebpackBrowser } from '../index';
 
-import { Architect } from '@angular-devkit/architect';
-import { normalize } from '@angular-devkit/core';
-import { browserBuild, createArchitect, host } from '../../test-utils';
+const BROWSER_BUILDER_INFO = {
+  name: '@angular-devkit/build-angular:browser',
+  schemaPath: __dirname + '/../schema.json',
+};
 
+describeBuilder(buildWebpackBrowser, BROWSER_BUILDER_INFO, (harness) => {
+  describe('basic test', () => {
+    it('works', async () => {
+      // Provide a target and options for builder execution
+      harness.useTarget('build', {
+        outputPath: 'dist',
+        index: 'src/index.html',
+        main: 'src/main.ts',
+        polyfills: 'src/polyfills.ts',
+        tsConfig: 'src/tsconfig.app.json',
+        progress: false,
+        assets: ['src/favicon.ico', 'src/assets'],
+        styles: ['src/styles.css'],
+        scripts: [],
+      });
 
-describe('Browser Builder basic test', () => {
-  const target = { project: 'app', target: 'build' };
-  let architect: Architect;
+      // Execute builder with above provided project, target, and options
+      await harness.executeOnce();
 
-  beforeEach(async () => {
-    await host.initialize().toPromise();
-    architect = (await createArchitect(host.root())).architect;
-  });
-
-  afterEach(async () => host.restore().toPromise());
-
-  it('works', async () => {
-    await browserBuild(architect, host, target);
-
-    // Default files should be in outputPath.
-    expect(await host.scopedSync().exists(normalize('dist/runtime.js'))).toBe(true);
-    expect(await host.scopedSync().exists(normalize('dist/main.js'))).toBe(true);
-    expect(await host.scopedSync().exists(normalize('dist/polyfills.js'))).toBe(true);
-    expect(await host.scopedSync().exists(normalize('dist/vendor.js'))).toBe(true);
-    expect(await host.scopedSync().exists(normalize('dist/favicon.ico'))).toBe(true);
-    expect(await host.scopedSync().exists(normalize('dist/styles.css'))).toBe(true);
-    expect(await host.scopedSync().exists(normalize('dist/index.html'))).toBe(true);
+      // Default files should be in outputPath.
+      expect(harness.hasFile('dist/runtime.js')).toBe(true);
+      expect(harness.hasFile('dist/main.js')).toBe(true);
+      expect(harness.hasFile('dist/polyfills.js')).toBe(true);
+      expect(harness.hasFile('dist/vendor.js')).toBe(true);
+      expect(harness.hasFile('dist/favicon.ico')).toBe(true);
+      expect(harness.hasFile('dist/styles.css')).toBe(true);
+      expect(harness.hasFile('dist/index.html')).toBe(true);
+    });
   });
 });
