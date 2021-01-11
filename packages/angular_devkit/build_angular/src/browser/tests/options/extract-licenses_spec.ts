232da624792157235bc1bdb5f1b7a732bfb51f9f
Alan Agius <alan.agius4@gmail.com>
2021-01-11 19:14:58 +0100

    test(@angular-devkit/build-angular): change `extractLicenses` unit test to use build harness
    This change adds a unit tests for the browser builder's extractLicenses option using the builder test harness.

---
+++ packages/angular_devkit/build_angular/src/browser/tests/options/extract-licenses_spec.ts
@@ -0,0 +1,45 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { buildWebpackBrowser } from '../../index';
+import { BASE_OPTIONS, BROWSER_BUILDER_INFO, describeBuilder } from '../setup';
+
+describeBuilder(buildWebpackBrowser, BROWSER_BUILDER_INFO, (harness) => {
+  describe('Option: "extractLicenses"', () => {
+    it(`should generate '3rdpartylicenses.txt' when 'extractLicenses' is true`, async () => {
+      harness.useTarget('build', {
+        ...BASE_OPTIONS,
+        extractLicenses: true,
+      });
+
+      const { result } = await harness.executeOnce();
+      expect(result?.success).toBe(true);
+      harness.expectFile('dist/3rdpartylicenses.txt').content.toContain('MIT');
+    });
+
+    it(`should not generate '3rdpartylicenses.txt' when 'extractLicenses' is false`, async () => {
+      harness.useTarget('build', {
+        ...BASE_OPTIONS,
+        extractLicenses: false,
+      });
+
+      const { result } = await harness.executeOnce();
+      expect(result?.success).toBe(true);
+      harness.expectFile('dist/3rdpartylicenses.txt').toNotExist();
+    });
+
+    it(`should not generate '3rdpartylicenses.txt' when 'extractLicenses' is not set`, async () => {
+      harness.useTarget('build', {
+        ...BASE_OPTIONS,
+      });
+
+      const { result } = await harness.executeOnce();
+      expect(result?.success).toBe(true);
+      harness.expectFile('dist/3rdpartylicenses.txt').toNotExist();
+    });
+  });
+});
