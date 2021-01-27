238438e26fbf335a7ee87115df9720f8e1665605
Alan Agius <alan.agius4@gmail.com>
2021-01-27 20:23:51 +0100

    refactor(@angular-devkit/build-angular): remove BundleBudgetPlugin
    Remove BundleBudgetPlugin, and instead use the `checkBudgets` method directly in the browser buillder.

--- packages/angular_devkit/build_angular/src/webpack/plugins/index.ts
+++ packages/angular_devkit/build_angular/src/webpack/plugins/index.ts
@@ -9,7 +9,6 @@
 // Exports the webpack plugins we use internally.
 export { AnyComponentStyleBudgetChecker } from './any-component-style-budget-checker';
 export { OptimizeCssWebpackPlugin, OptimizeCssWebpackPluginOptions } from './optimize-css-webpack-plugin';
-export { BundleBudgetPlugin, BundleBudgetPluginOptions } from './bundle-budget';
 export { ScriptsWebpackPlugin, ScriptsWebpackPluginOptions } from './scripts-webpack-plugin';
 export { SuppressExtractedTextChunksWebpackPlugin } from './suppress-entry-chunks-webpack-plugin';
 export { RemoveHashPlugin, RemoveHashPluginOptions } from './remove-hash-plugin';
