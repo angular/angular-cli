4a7da6e5ffdedeaf1c8d1604e8b1b5bc28b9fc43
Charles Lyding <19598772+clydin@users.noreply.github.com>
2021-02-04 11:14:21 -0500

    test(@angular-devkit/build-angular): add async downlevel source map unit test

--- packages/angular_devkit/build_angular/src/browser/tests/behavior/typescript-target_spec.ts
+++ packages/angular_devkit/build_angular/src/browser/tests/behavior/typescript-target_spec.ts
@@ -55,6 +55,50 @@ describeBuilder(buildWebpackBrowser, BROWSER_BUILDER_INFO, (harness) => {
       harness.expectFile('dist/main.js').content.toContain('"from-async-js-function"');
     });
 
+    it('creates correct sourcemaps when downleveling async functions', async () => {
+      // Set TypeScript configuration target to ES2017 to enable native async
+      await harness.modifyFile('src/tsconfig.app.json', (content) => {
+        const tsconfig = JSON.parse(content);
+        if (!tsconfig.compilerOptions) {
+          tsconfig.compilerOptions = {};
+        }
+        tsconfig.compilerOptions.target = 'es2017';
+
+        return JSON.stringify(tsconfig);
+      });
+
+      // Add a JavaScript file with async code
+      await harness.writeFile(
+        'src/async-test.js',
+        'async function testJs() { console.log("from-async-js-function"); }',
+      );
+
+      // Add an async function to the project as well as JavaScript file
+      // The type `Void123` is used as a unique identifier for the final sourcemap
+      // If sourcemaps are not properly propagated then it will not be in the final sourcemap
+      await harness.modifyFile(
+        'src/main.ts',
+        (content) =>
+          'import "./async-test";\n' +
+          content +
+          '\ntype Void123 = void;' +
+          `\nasync function testApp(): Promise<Void123> { console.log("from-async-app-function"); }`,
+      );
+
+      harness.useTarget('build', {
+        ...BASE_OPTIONS,
+        sourceMap: {
+          scripts: true,
+        },
+      });
+
+      const { result } = await harness.executeOnce();
+
+      expect(result?.success).toBe(true);
+      harness.expectFile('dist/main.js').content.not.toMatch(/\sasync\s/);
+      harness.expectFile('dist/main.js.map').content.toContain('Promise<Void123>');
+    });
+
     it('downlevels async functions when targetting greater than ES2017', async () => {
       // Set TypeScript configuration target greater than ES2017 to enable native async
       await harness.modifyFile('src/tsconfig.app.json', (content) => {
