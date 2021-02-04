40ab557971e958a3ff882ae12a26de8e0fbdb6bc
Keen Yee Liau <kyliau@google.com>
2021-02-03 16:40:03 -0800

    release: v11.1.3

--- packages/schematics/angular/utility/latest-versions.ts
+++ packages/schematics/angular/utility/latest-versions.ts
@@ -8,7 +8,7 @@
 
 export const latestVersions = {
   // These versions should be kept up to date with latest Angular peer dependencies.
-  Angular: '~11.1.1',
+  Angular: '~11.1.2',
   RxJs: '~6.6.0',
   ZoneJs: '~0.11.3',
   TypeScript: '~4.1.2',
@@ -18,9 +18,9 @@ export const latestVersions = {
   // For our e2e tests, these versions must match the latest tag present on the branch.
   // During RC periods they will not match the latest RC until there's a new git tag, and
   // should not be updated.
-  DevkitBuildAngular: '~0.1101.2',
-  DevkitBuildNgPackagr: '~0.1101.2',
-  DevkitBuildWebpack: '~0.1101.2',
+  DevkitBuildAngular: '~0.1101.3',
+  DevkitBuildNgPackagr: '~0.1101.3',
+  DevkitBuildWebpack: '~0.1101.3',
 
   ngPackagr: '^11.0.0',
 };
