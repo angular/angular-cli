e1fa4e5c874b9c81a34896df86786cb34ec84462
Alan Agius <alan.agius4@gmail.com>
2021-02-08 20:13:44 +0100

    test(@nguniversal/common): update e2e test

--- integration/common-engine/e2e/src/app.e2e-spec.ts
+++ integration/common-engine/e2e/src/app.e2e-spec.ts
@@ -8,31 +8,33 @@
 
 import {browser, by, element} from 'protractor';
 
-import {verifyNoBrowserErrors} from './util';
+import {verifyNoBrowserErrors, waitForAppRootElement} from './util';
 
-describe('Hello world E2E Tests', function() {
-  it('should display: Hello world! with static counter', function() {
+describe('Hello world E2E Tests', () => {
+  beforeAll(async () => {
+    await browser.driver.wait(
+      waitForAppRootElement(),
+      6000 * 2,
+      'Server should have started in 2 minutes',
+    );
+  });
+
+  it('should display: Hello world!', async () => {
     // Load the page without waiting for Angular since it is not bootstrapped automatically.
-    browser.driver.get(browser.baseUrl + 'helloworld');
-
+    await browser.driver.get(browser.baseUrl);
 
     const style = browser.driver.findElement(by.css('style[ng-transition="hlw"]'));
     expect(style.getText()).not.toBeNull();
 
     // Test the contents from the server.
-    const serverDiv = browser.driver.findElement(by.css('div#name'));
+    const serverDiv = browser.driver.findElement(by.css('div'));
     expect(serverDiv.getText()).toMatch('Hello world!');
 
-    // Test the contents from the server.
-    const counterDiv = browser.driver.findElement(by.css('div#counter'));
-    expect(counterDiv.getText()).toMatch('Counter: 1');
-
     // Bootstrap the client side app.
-    browser.executeScript('doBootstrap()');
+    await browser.executeScript('doBootstrap()');
 
     // Retest the contents after the client bootstraps.
-    expect(element(by.css('div#name')).getText()).toMatch('Hello world!');
-    expect(element(by.css('div#counter')).getText()).toMatch('Counter: 1');
+    expect(element(by.css('div')).getText()).toMatch('Hello world!');
 
     // Make sure the server styles got replaced by client side ones.
     expect(element(by.css('style[ng-transition="hlw"]')).isPresent()).toBeFalsy();
