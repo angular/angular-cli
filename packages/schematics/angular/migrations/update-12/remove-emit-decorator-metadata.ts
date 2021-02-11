36ba1a3b0970d9d353a66ae63a26aea64893b58e
Alan Agius <alan.agius4@gmail.com>
2021-02-11 11:07:55 +0100

    feat(@schematics/angular): add migration to remove emitDecoratorMetadata
    Add migration to remove 'emitDecoratorMetadata' TypeScript compiler option. Decorator metadata is no longer needed by Angular.
    
    Read more about this option here: https://www.typescriptlang.org/docs/handbook/decorators.html#metadata

---
+++ packages/schematics/angular/migrations/update-12/remove-emit-decorator-metadata.ts
@@ -0,0 +1,40 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { join } from '@angular-devkit/core';
+import { DirEntry, Rule } from '@angular-devkit/schematics';
+import { JSONFile } from '../../utility/json-file';
+
+function* visitJsonFiles(directory: DirEntry): IterableIterator<string> {
+  for (const path of directory.subfiles) {
+    if (!path.endsWith('.json')) {
+      continue;
+    }
+
+    yield join(directory.path, path);
+  }
+
+  for (const path of directory.subdirs) {
+    if (path === 'node_modules' || path.startsWith('.')) {
+      continue;
+    }
+
+    yield* visitJsonFiles(directory.dir(path));
+  }
+}
+
+export default function (): Rule {
+  return tree => {
+    for (const path of visitJsonFiles(tree.root)) {
+      const content = tree.read(path);
+      if (content?.toString().includes('"emitDecoratorMetadata"')) {
+        const json = new JSONFile(tree, path);
+        json.remove(['compilerOptions', 'emitDecoratorMetadata']);
+      }
+    }
+  };
+}
