127de869a26397ef1601fbde8743041c08ac606d
Alan Agius <alan.agius4@gmail.com>
2021-02-11 10:41:54 +0100

    feat(@schematics/angular): strict mode by default
    With this change we workspaces are generated strict by default. To create non-strict workspace the `--no-strict` command line option.

--- tests/legacy-cli/e2e/tests/build/strict-mode.ts
+++
@@ -1,7 +0,0 @@
-import { ng } from '../../utils/process';
-import { createProject } from '../../utils/project';
-
-export default async function() {
-  await createProject('strict-test-project', '--strict');
-  await ng('e2e', '--prod');
-}
