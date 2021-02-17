7d57dd2f3e7d36cc4ed2c356f79139486790cbfa
Alan Agius <alan.agius4@gmail.com>
2021-02-17 19:08:35 +0100

    feat(@schematics/angular): add migration to use new zone.js entry-points

--- packages/schematics/angular/utility/latest-versions.ts
+++ packages/schematics/angular/utility/latest-versions.ts
@@ -10,7 +10,7 @@ export const latestVersions = {
   // These versions should be kept up to date with latest Angular peer dependencies.
   Angular: '~12.0.0-next.0',
   RxJs: '~6.6.0',
-  ZoneJs: '~0.11.3',
+  ZoneJs: '~0.11.4',
   TypeScript: '~4.1.2',
   TsLib: '^2.0.0',
 
