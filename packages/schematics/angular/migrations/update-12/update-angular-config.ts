16dd4eb74318859397e7add314b40c6477e8fe98
Alan Agius <alan.agius4@gmail.com>
2021-03-03 12:02:22 +0100

    feat(@schematics/angular): add migration to update server and browser builder options

--- packages/schematics/angular/migrations/update-12/update-angular-config.ts
+++ packages/schematics/angular/migrations/update-12/update-angular-config.ts
@@ -6,19 +6,68 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
+import { JsonValue, workspaces } from '@angular-devkit/core';
 import { Rule } from '@angular-devkit/schematics';
 import { allTargetOptions, allWorkspaceTargets, updateWorkspace } from '../../utility/workspace';
+
+type BuilderOptionsType = Readonly<[optionName: string, oldDefault: JsonValue | undefined, newDefault: JsonValue | undefined][]>;
+
+const BrowserBuilderOptions: BuilderOptionsType = [
+  ['aot', false, true],
+  ['vendorChunk', true, false],
+  ['extractLicenses', true, false],
+  ['buildOptimizer', false, true],
+  ['sourceMap', true, false],
+  ['optimization', false, true],
+  ['namedChunks', false, true],
+  ['outputHashing', 'none', 'all'],
+];
+
+const ServerBuilderOptions: BuilderOptionsType = [
+  ['sourceMap', true, false],
+  ['optimization', false, true],
+  ['outputHashing', 'none', 'media'],
+];
 
 export default function (): Rule {
   return updateWorkspace(workspace => {
     for (const [, target] of allWorkspaceTargets(workspace)) {
-      if (!target.builder.startsWith('@angular-devkit/build-angular')) {
-        continue;
-      }
+      // Only interested in Angular Devkit broweser and server builders
+      if (target?.builder === '@angular-devkit/build-angular:server') {
+        updateOptions(target, ServerBuilderOptions);
+      } else if (target?.builder === '@angular-devkit/build-angular:browser') {
+        updateOptions(target, BrowserBuilderOptions);
 
-      for (const [, options] of allTargetOptions(target)) {
-        delete options.experimentalRollupPass;
+        for (const [, options] of allTargetOptions(target)) {
+          delete options.experimentalRollupPass;
+        }
       }
     }
   });
+}
+
+function updateOptions(
+  target: workspaces.TargetDefinition, optionsToUpdate: typeof ServerBuilderOptions | typeof BrowserBuilderOptions,
+): void {
+  if (!target.options) {
+    target.options = {};
+  }
+
+  const configurationOptions = target.configurations && Object.values(target.configurations);
+
+  for (const [optionName, oldDefault, newDefault] of optionsToUpdate) {
+    let value = target.options[optionName];
+    if (value === newDefault) {
+      // Value is same as new default
+      delete target.options[optionName];
+    } else if (value === undefined) {
+      // Value is not defined, hence the default in the builder was used.
+      target.options[optionName] = oldDefault;
+      value = oldDefault;
+    }
+
+    // Remove overrides in configurations which are no longer needed.
+    configurationOptions?.filter(o => o && o[optionName] === value)
+      .forEach(o => o && delete o[optionName]);
+  }
 }
