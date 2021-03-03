d48b6299e038b8f702d641fb165a5fa9c2b0fe7b
Charles Lyding <19598772+clydin@users.noreply.github.com>
2021-03-02 20:55:34 -0500

    refactor(@angular-devkit/core): deprecate unused utility functions
    The `clean` and `mapObject` utility functions are unused by the Angular CLI.

--- packages/angular_devkit/core/src/utils/array.ts
+++ packages/angular_devkit/core/src/utils/array.ts
@@ -6,6 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
+/** @deprecated Since v12.0, unused by the Angular tooling */
 export function clean<T>(array: Array<T | undefined>): Array<T> {
   return array.filter(x => x !== undefined) as Array<T>;
 }
