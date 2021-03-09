f4875b967ae9ca5640cb27bfb37166528cab88d8
Alan Agius <alan.agius4@gmail.com>
2021-03-09 11:10:54 +0100

    feat(@schematics/angular): add migration to remove `lazyModules` configuration option

--- packages/schematics/angular/migrations/update-12/update-angular-config.ts
+++ packages/schematics/angular/migrations/update-12/update-angular-config.ts
@@ -18,6 +18,7 @@ export default function (): Rule {
 
       for (const [, options] of allTargetOptions(target)) {
         delete options.experimentalRollupPass;
+        delete options.lazyModules;
       }
     }
   });
