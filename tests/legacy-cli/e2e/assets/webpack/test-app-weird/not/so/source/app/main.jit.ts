af1cc2d6423d1fa5790a5bd613f876ec4685b518
Alan Agius <alan.agius4@gmail.com>
2021-03-09 13:09:28 +0100

    test(@ngtools/webpack): delete disabled test-app-weird and server-app

--- tests/legacy-cli/e2e/assets/webpack/test-app-weird/not/so/source/app/main.jit.ts
+++
@@ -1,5 +0,0 @@
-import 'core-js/proposals/reflect-metadata';
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {AppModule} from './app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule);
