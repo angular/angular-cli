f94a78024642043b483ed8dff2993bf0a07cffb8
Charles Lyding <19598772+clydin@users.noreply.github.com>
2021-03-16 11:31:13 -0400

    fix(@angular-devkit/build-angular): support writing large Webpack stat outputs
    When using the `statsJson` browser builder option, the resulting JSON data is now streamed into a file instead of written in one large block.  This mitigates crashes due to the generated string exceeded the Node.js limit.

--- packages/angular_devkit/build_angular/src/webpack/configs/common.ts
+++ packages/angular_devkit/build_angular/src/webpack/configs/common.ts
@@ -10,7 +10,7 @@ import {
   buildOptimizerLoaderPath,
 } from '@angular-devkit/build-optimizer';
 import * as CopyWebpackPlugin from 'copy-webpack-plugin';
-import { existsSync } from 'fs';
+import { createWriteStream, existsSync } from 'fs';
 import * as path from 'path';
 import { ScriptTarget } from 'typescript';
 import {
@@ -21,7 +21,6 @@ import {
   compilation,
   debug,
 } from 'webpack';
-import { RawSource } from 'webpack-sources';
 import { AssetPatternClass } from '../../browser/schema';
 import { BuildBrowserFeatures, maxWorkers } from '../../utils';
 import { WebpackConfigOptions } from '../../utils/build-options';
@@ -290,9 +289,17 @@ export function getCommonConfig(wco: WebpackConfigOptions): Configuration {
     extraPlugins.push(
       new (class {
         apply(compiler: Compiler) {
-          compiler.hooks.emit.tap('angular-cli-stats', compilation => {
-            const data = JSON.stringify(compilation.getStats().toJson('verbose'), undefined, 2);
-            compilation.assets['stats.json'] = new RawSource(data);
+          compiler.hooks.done.tapPromise('angular-cli-stats', async (stats) => {
+            const { stringifyStream } = await import('@discoveryjs/json-ext');
+            const data = stats.toJson('verbose');
+            const statsOutputPath = path.join(stats.compilation.outputOptions.path, 'stats.json');
+
+            return new Promise<void>((resolve, reject) =>
+              stringifyStream(data)
+                .pipe(createWriteStream(statsOutputPath))
+                .on('close', resolve)
+                .on('error', reject),
+            );
           });
         }
       })(),
