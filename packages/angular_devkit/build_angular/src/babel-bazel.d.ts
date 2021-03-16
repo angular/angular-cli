ae261192842401eeefa6cf079f57b199ce520984
Charles Lyding <19598772+clydin@users.noreply.github.com>
2021-03-16 11:31:13 -0400

    fix(@angular-devkit/build-angular): support writing large Webpack stat outputs
    When using the `statsJson` browser builder option, the resulting JSON data is now streamed into a file instead of written in one large block.  This mitigates crashes due to the generated string exceeded the Node.js limit.
    
    (cherry picked from commit d5645675fd555e7f1afd523d4f2d42095034fc46)

---
+++ packages/angular_devkit/build_angular/src/babel-bazel.d.ts
@@ -0,0 +1,22 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+// Workaround for https://github.com/bazelbuild/rules_nodejs/issues/1033
+// Alternative approach instead of https://github.com/angular/angular/pull/33226
+declare module '@babel/core' {
+  export * from '@types/babel__core';
+}
+declare module '@babel/generator' {
+  export { default } from '@types/babel__generator';
+}
+declare module '@babel/traverse' {
+  export { default } from '@types/babel__traverse';
+}
+declare module '@babel/template' {
+  export { default } from '@types/babel__template';
+}
