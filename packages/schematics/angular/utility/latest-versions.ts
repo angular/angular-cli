a2b39c4ee6f1c3d2e24ed7dfa1987c29b7b2980e
Doug Parker <dgp1130@users.noreply.github.com>
2021-03-17 12:06:37 -0700

    release: v11.2.5
    `latest-versions.ts` updated devkit versions by 2 because the previous release forgot to increment them.

--- packages/schematics/angular/utility/latest-versions.ts
+++ packages/schematics/angular/utility/latest-versions.ts
@@ -8,7 +8,7 @@
 
 export const latestVersions = {
   // These versions should be kept up to date with latest Angular peer dependencies.
-  Angular: '~11.2.5',
+  Angular: '~11.2.6',
   RxJs: '~6.6.0',
   ZoneJs: '~0.11.3',
   TypeScript: '~4.1.5',
@@ -18,9 +18,9 @@ export const latestVersions = {
   // For our e2e tests, these versions must match the latest tag present on the branch.
   // During RC periods they will not match the latest RC until there's a new git tag, and
   // should not be updated.
-  DevkitBuildAngular: '~0.1102.3',
-  DevkitBuildNgPackagr: '~0.1102.3',
-  DevkitBuildWebpack: '~0.1102.3',
+  DevkitBuildAngular: '~0.1102.5',
+  DevkitBuildNgPackagr: '~0.1102.5',
+  DevkitBuildWebpack: '~0.1102.5',
 
   ngPackagr: '^11.0.0',
 };
