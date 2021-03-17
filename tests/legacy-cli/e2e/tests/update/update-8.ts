b406add29e364cf1aed9dcbbaccd41cf4fb1fbba
Alan Agius <alan.agius4@gmail.com>
2021-03-17 18:22:05 +0100

    test(@schematics/angular): update version 8 migration to use NPM packages
    This is needed as otherwise the installation of older packages will fai.

--- tests/legacy-cli/e2e/tests/update/update-8.ts
+++ tests/legacy-cli/e2e/tests/update/update-8.ts
@@ -1,23 +1,47 @@
 import { createProjectFromAsset } from '../../utils/assets';
-import { expectFileMatchToExist } from '../../utils/fs';
-import { installWorkspacePackages } from '../../utils/packages';
-import { ng, noSilentNg, silentNpm } from '../../utils/process';
+import { getGlobalVariable } from '../../utils/env';
+import { expectFileMatchToExist, rimraf, writeFile } from '../../utils/fs';
+import { installWorkspacePackages, setRegistry } from '../../utils/packages';
+import { ng, noSilentNg } from '../../utils/process';
 import { isPrereleaseCli, useBuiltPackages, useCIChrome, useCIDefaults } from '../../utils/project';
 
-export default async function() {
-  await createProjectFromAsset('8.0-project');
-  await ng('update', '@angular/cli', '--migrate-only', '--from=8');
+export default async function () {
+  await createProjectFromAsset('8.0-project', true, true);
+
+  // We need to use the public registry because in the local NPM server we don't have
+  // older versions @angular/cli packages which would cause `npm install` during `ng update` to fail.
+  try {
+    await setRegistry(false);
+
+    await useBuiltPackages();
+    await installWorkspacePackages();
+
+    // Update Angular CLI.
+    await ng('update', '@angular/cli', '--migrate-only', '--from=8');
+  } finally {
+    await setRegistry(true);
+  }
+
+  if (!getGlobalVariable('ci')) {
+    const testRegistry = getGlobalVariable('package-registry');
+    await writeFile('.npmrc', `registry=${testRegistry}`);
+  }
+
+  // Update Angular.
+  const extraUpdateArgs = await isPrereleaseCli() ? ['--next', '--force'] : [];
+  await ng('update', '@angular/core', ...extraUpdateArgs);
 
   // Use the packages we are building in this commit, and CI Chrome.
   await useBuiltPackages();
   await useCIChrome('./');
   await useCIChrome('./e2e/');
   await useCIDefaults('eight-project');
-  await installWorkspacePackages();
 
-  // Update Angular.
-  const extraUpdateArgs = await isPrereleaseCli() ? ['--next', '--force'] : [];
-  await ng('update', '@angular/core', ...extraUpdateArgs);
+  // This is needed as otherwise causes local modules not to override already present modules
+  await rimraf('node_modules/@angular-devkit');
+  await rimraf('node_modules/@angular/cli');
+
+  await installWorkspacePackages();
 
   // Run CLI commands.
   await ng('generate', 'component', 'my-comp');
