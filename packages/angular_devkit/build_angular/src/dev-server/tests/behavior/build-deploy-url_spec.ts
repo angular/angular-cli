2041c70f7693e7a427780948a45693f2e1e6a2d9
Charles Lyding <19598772+clydin@users.noreply.github.com>
2021-03-17 21:45:32 -0400

    test(@angular-devkit/build-angular): add dev-server builder deployUrl behavior tests
    This change adds expanded unit tests for the dev-server builder's build deploy URL behavior using the builder test harness.

---
+++ packages/angular_devkit/build_angular/src/dev-server/tests/behavior/build-deploy-url_spec.ts
@@ -0,0 +1,33 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { serveWebpackBrowser } from '../../index';
+import { executeOnceAndFetch } from '../execute-fetch';
+import { BASE_OPTIONS, DEV_SERVER_BUILDER_INFO, describeBuilder, setupBrowserTarget } from '../setup';
+
+describeBuilder(serveWebpackBrowser, DEV_SERVER_BUILDER_INFO, (harness) => {
+  describe('Behavior: "browser builder deployUrl"', () => {
+    beforeEach(() => {
+      setupBrowserTarget(harness, {
+        deployUrl: 'test/',
+      });
+    });
+
+    it('uses the deploy URL defined in the "browserTarget" options as the serve path', async () => {
+      harness.useTarget('serve', {
+        ...BASE_OPTIONS,
+      });
+
+      const { result, response } = await executeOnceAndFetch(harness, 'runtime.js');
+
+      expect(result?.success).toBeTrue();
+      expect(result?.baseUrl).toMatch(/\/test$/);
+      expect(response?.url).toMatch(/\/test\/runtime.js$/);
+      expect(await response?.text()).toContain('window["webpackJsonp"]');
+    });
+  });
+});
