ae0fdfac321a7863b6fd800cc3fafd1db04ca03c
Alan Agius <alan.agius4@gmail.com>
2021-03-30 10:48:44 +0200

    refactor(@angular/cli): remove `Ivy Workspace` from `ng version`
    In version 12, only Ivy will be allowed to be used to compile an application.

--- tests/legacy-cli/e2e/tests/misc/version.ts
+++ tests/legacy-cli/e2e/tests/misc/version.ts
@@ -1,23 +1,11 @@
-import { getGlobalVariable } from '../../utils/env';
 import { deleteFile } from '../../utils/fs';
 import { ng } from '../../utils/process';
-
 
 export default async function() {
   const { stdout: commandOutput } = await ng('version');
   const { stdout: optionOutput } = await ng('--version');
   if (!optionOutput.includes('Angular CLI:')) {
     throw new Error('version not displayed');
-  }
-
-  const argv = getGlobalVariable('argv');
-  const veProject = argv['ve'];
-
-  const ivyWorkspaceMatch = veProject
-    ? 'Ivy Workspace: No'
-    : 'Ivy Workspace: Yes';
-  if (!optionOutput.includes(ivyWorkspaceMatch)) {
-    throw new Error(`Expected output to contain ${ivyWorkspaceMatch}`);
   }
 
   if (commandOutput !== optionOutput) {
