118ffb17feb76457f7f5417230ebac5af88bb5e6
Charles Lyding <19598772+clydin@users.noreply.github.com>
2021-03-31 12:25:56 -0400

    test(@ngtools/webpack): update webpack test-app E2E to use Webpack 5
    The direct Webpack test for the Webpack plugin is now updated to use Webpack 5 as Webpack 5 is now the default for the Angular tooling.

--- tests/legacy-cli/e2e/tests/packages/webpack/test-app.ts
+++ tests/legacy-cli/e2e/tests/packages/webpack/test-app.ts
@@ -3,17 +3,18 @@ import { createProjectFromAsset } from '../../../utils/assets';
 import { expectFileSizeToBeUnder, expectFileToMatch, replaceInFile } from '../../../utils/fs';
 import { exec } from '../../../utils/process';
 
-
 export default async function (skipCleaning: () => void) {
   const webpackCLIBin = normalize('node_modules/.bin/webpack-cli');
 
   await createProjectFromAsset('webpack/test-app');
+
   await exec(webpackCLIBin);
 
   // Note: these sizes are without Build Optimizer or any advanced optimizations in the CLI.
-  await expectFileSizeToBeUnder('dist/app.main.js', 565 * 1024);
-  await expectFileSizeToBeUnder('dist/1.app.main.js', 1 * 1024);
-  await expectFileSizeToBeUnder('dist/2.app.main.js', 2 * 1024);
+  await expectFileSizeToBeUnder('dist/app.main.js', 656 * 1024);
+  await expectFileSizeToBeUnder('dist/501.app.main.js', 1 * 1024);
+  await expectFileSizeToBeUnder('dist/888.app.main.js', 2 * 1024);
+  await expectFileSizeToBeUnder('dist/972.app.main.js', 2 * 1024);
 
 
   // test resource urls without ./
