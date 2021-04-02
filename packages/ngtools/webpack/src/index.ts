160102ae57d780dded6c9002faf07b601a866d3d
Charles Lyding <19598772+clydin@users.noreply.github.com>
2021-04-02 14:30:05 -0400

    fix(@ngtools/webpack): remove Webpack plugin for deprecated ViewEngine compiler
    BREAKING CHANGE: Removal of View Engine support from application builds
    With the removal of the deprecated View Engine compiler in Angular version 12 for applications, the View Engine Webpack plugin has been removed.
    The Ivy-based Webpack plugin is the default used within the Angular CLI.
    If using a custom standalone Webpack configuration, the removed `AngularCompilerPlugin` should be replaced with the Ivy-based `AngularWebpackPlugin`.

--- packages/ngtools/webpack/src/index.ts
+++ packages/ngtools/webpack/src/index.ts
@@ -5,11 +5,20 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
+import * as ivyInternal from './ivy';
+export {
+  AngularWebpackLoaderPath,
+  AngularWebpackPlugin,
+  AngularWebpackPluginOptions,
+  default,
+} from './ivy';
 
-export * from './angular_compiler_plugin';
-export * from './interfaces';
-export { ngcLoader as default } from './loader';
-
-export const NgToolsLoader = __filename;
-
-export * as ivy from './ivy';
+/** @deprecated Deprecated as of v12, please use the direct exports
+ * (`AngularWebpackPlugin` instead of `ivy.AngularWebpackPlugin`)
+ */
+export namespace ivy {
+  export const AngularWebpackLoaderPath = ivyInternal.AngularWebpackLoaderPath;
+  export const AngularWebpackPlugin = ivyInternal.AngularWebpackPlugin;
+  export type AngularWebpackPlugin = ivyInternal.AngularWebpackPlugin;
+  export type AngularPluginOptions = ivyInternal.AngularWebpackPluginOptions;
+}
--- packages/ngtools/webpack/src/ivy/index.ts
+++ packages/ngtools/webpack/src/ivy/index.ts
@@ -6,6 +6,6 @@
  * found in the LICENSE file at https://angular.io/license
  */
 export { angularWebpackLoader as default } from './loader';
-export { AngularPluginOptions, AngularWebpackPlugin } from './plugin';
+export { AngularWebpackPluginOptions, AngularWebpackPlugin } from './plugin';
 
 export const AngularWebpackLoaderPath = __filename;
