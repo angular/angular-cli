b428a84a7d8c2a3e02ed54bbc0f77103516633f6
Charles Lyding <19598772+clydin@users.noreply.github.com>
2021-04-05 11:43:43 -0400

    test(@angular-devkit/build-angular): add initial dev-server builder hmr and liveReload option tests
    The CommonJS usage warning tests are moved to use the builder harness for both the `hmr` and `liveReload` options.

---
+++ packages/angular_devkit/build_angular/src/dev-server/tests/options/live-reload_spec.ts
@@ -0,0 +1,38 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { serveWebpackBrowser } from '../../index';
+import {
+  BASE_OPTIONS,
+  DEV_SERVER_BUILDER_INFO,
+  describeBuilder,
+  setupBrowserTarget,
+} from '../setup';
+
+describeBuilder(serveWebpackBrowser, DEV_SERVER_BUILDER_INFO, (harness) => {
+  describe('Option: "liveReload"', () => {
+    beforeEach(() => {
+      setupBrowserTarget(harness);
+    });
+
+    it('should not show a CommonJS usage warning when enabled', async () => {
+      harness.useTarget('serve', {
+        ...BASE_OPTIONS,
+        liveReload: true,
+      });
+
+      const { result, logs } = await harness.executeOnce();
+
+      expect(result?.success).toBe(true);
+      expect(logs).not.toContain(
+        jasmine.objectContaining({
+          message: jasmine.stringMatching('CommonJS or AMD dependencies'),
+        }),
+      );
+    });
+  });
+});
