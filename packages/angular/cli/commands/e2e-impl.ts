5197f424efd9a79eeea4e2ab2542a0b0fe276fc6
Keen Yee Liau <kyliau@umich.edu>
2021-04-12 13:33:27 -0700

    refactor(@schematics/angular): Do not include Protractor test in new project
    Do not include Protractor for new projects since we do not have a concrete
    plan for Protractor yet.
    
    (cherry picked from commit 49ba5e20aa3604dcb96c2cca80892270c0ae7aee)

--- packages/angular/cli/commands/e2e-impl.ts
+++ packages/angular/cli/commands/e2e-impl.ts
@@ -14,8 +14,20 @@ import { Schema as E2eCommandSchema } from './e2e';
 export class E2eCommand extends ArchitectCommand<E2eCommandSchema> {
   public readonly target = 'e2e';
   public readonly multiTarget = true;
+  public readonly missingTargetError = `
+Cannot find "e2e" target for the specified project.
 
-  public async run(options: E2eCommandSchema & Arguments) {
-    return this.runArchitectTarget(options);
+You should add a package that implements end-to-end testing capabilities.
+
+For example:
+  WebdriverIO: ng add @wdio/schematics
+
+More options will be added to the list as they become available.
+`;
+
+  async initialize(options: E2eCommandSchema & Arguments) {
+    if (!options.help) {
+      return super.initialize(options);
+    }
   }
 }
