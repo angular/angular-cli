241fa1d341c5cd1c8e27d35ff1900ba369fd0add
Charles Lyding <19598772+clydin@users.noreply.github.com>
2021-04-19 17:12:45 -0400

    refactor(@ngtools/webpack): remove unused transformation helper code
    With the recent refactoring of the package's TypeScript transformers, several internal functions and types are no longer used or needed.

--- packages/ngtools/webpack/src/transformers/index.ts
+++ packages/ngtools/webpack/src/transformers/index.ts
@@ -5,8 +5,5 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-export * from './interfaces';
-export * from './ast_helpers';
-export * from './make_transform';
 export * from './elide_imports';
 export * from './replace_resources';
