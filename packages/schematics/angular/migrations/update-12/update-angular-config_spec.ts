decaef592e11a2d1e4b9a6f10246a406cc4da2ce
Alan Agius <alan.agius4@gmail.com>
2021-04-28 13:33:26 +0200

    fix(@schematics/angular): fix migration for namedChunks and option
    Both values of  `extractLicenses` and `namedChunks` were inverted.

--- packages/schematics/angular/migrations/update-12/update-angular-config_spec.ts
+++ packages/schematics/angular/migrations/update-12/update-angular-config_spec.ts
@@ -32,6 +32,7 @@ function createWorkSpaceConfig(tree: UnitTestTree) {
               optimization: true,
               experimentalRollupPass: false,
               buildOptimizer: false,
+              namedChunks: true,
               // tslint:disable-next-line:no-any
             } as any,
             configurations: {
@@ -105,5 +106,13 @@ describe(`Migration to update 'angular.json'. ${schematicName}`, () => {
     expect(configurations?.one.sourceMap).toBeUndefined();
     expect(configurations?.two.sourceMap).toBeUndefined();
     expect(configurations?.two.optimization).toBeFalse();
+  });
+
+  it(`should not remove value in "options" when value is not the new default`, async () => {
+    const newTree = await schematicRunner.runSchematicAsync(schematicName, {}, tree).toPromise();
+    const { options } = getBuildTarget(newTree);
+
+    expect(options.namedChunks).toBeTrue();
+    expect(options.buildOptimizer).toBeFalse();
   });
 });
