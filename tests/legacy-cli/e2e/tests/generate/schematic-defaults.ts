7319d4e45a4b70d2ef776174d3cf35544dcfe32b
Alan Agius <alan.agius4@gmail.com>
2021-05-04 13:03:33 +0200

    fix(@angular/cli): infer schematic defaults correctly when using `--project`
    Previously, the `--project` flag was ignored when gathering and merging the schematics defaults from the angular workspace configuration file.
    
    Closes #20666

---
+++ tests/legacy-cli/e2e/tests/generate/schematic-defaults.ts
@@ -0,0 +1,42 @@
+import { ng } from '../../utils/process';
+import { updateJsonFile } from '../../utils/project';
+
+export default async function () {
+  await updateJsonFile('angular.json', (config) => {
+    config.projects['test-project'].schematics = {
+      '@schematics/angular:component': {
+        style: 'scss',
+      },
+    };
+  });
+
+  // Generate component in application to verify that it's minimal
+  const { stdout } = await ng('generate', 'component', 'foo');
+  if (!stdout.includes('foo.component.scss')) {
+    console.log(stdout);
+    throw new Error('Expected "foo.component.scss" to exist.');
+  }
+
+  // Generate another project with different settings
+  await ng('generate', 'application', 'test-project-two', '--no-minimal');
+
+  await updateJsonFile('angular.json', (config) => {
+    config.projects['test-project-two'].schematics = {
+      '@schematics/angular:component': {
+        style: 'less',
+      },
+    };
+  });
+
+  const { stdout: stdout2 } = await ng(
+    'generate',
+    'component',
+    'foo',
+    '--project',
+    'test-project-two',
+  );
+  if (!stdout2.includes('foo.component.less')) {
+    console.log(stdout2);
+    throw new Error('Expected "foo.component.less" to exist.');
+  }
+}
