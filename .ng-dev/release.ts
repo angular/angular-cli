71f4173129c35e6dc4ead77081bc4edc7e22efcc
Joey Perrott <josephperrott@gmail.com>
2021-05-17 10:29:00 -0700

    build: set up releasing using ng-dev

--- .ng-dev/release.ts
+++ .ng-dev/release.ts
@@ -1,11 +1,16 @@
+import '../lib/bootstrap-local';
+
 import { ReleaseConfig } from '@angular/dev-infra-private/release/config';
 import { packages } from '../lib/packages';
+import buildPackages from '../scripts/build';
+
+const npmPackages = Object.keys(packages);
 
 /** Configuration for the `ng-dev release` command. */
 export const release: ReleaseConfig = {
-  npmPackages: Object.keys(packages),
-  // TODO: Set up package building.
-  buildPackages: async () => [],
-  // TODO: Set up generating changelogs
-  generateReleaseNotesForHead: async () => {},
+  npmPackages,
+  buildPackages,
+  releaseNotes: {},
+  publishRegistry: 'https://wombat-dressing-room.appspot.com',
+  releasePrLabels: ['action: merge'],
 };
