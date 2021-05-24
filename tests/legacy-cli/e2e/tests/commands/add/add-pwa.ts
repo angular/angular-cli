41f417afb76335e4e8d25828e40a62ef254ef22f
Charles Lyding <19598772+clydin@users.noreply.github.com>
2021-05-24 17:09:50 -0400

    test: use correct snapshot versions in add-pwa E2E test
    When executing the add-pwa E2E test with Angular snapshots, the package versions need to be updated after the `ng add @angular/pwa` call to ensure that the correct version of `@angular/service-worker` is used.

--- tests/legacy-cli/e2e/tests/commands/add/add-pwa.ts
+++ tests/legacy-cli/e2e/tests/commands/add/add-pwa.ts
@@ -1,6 +1,11 @@
 import { join } from 'path';
+import { getGlobalVariable } from '../../../utils/env';
 import { expectFileToExist, readFile, rimraf } from '../../../utils/fs';
+import { installPackage } from '../../../utils/packages';
 import { ng } from '../../../utils/process';
+import { updateJsonFile } from '../../../utils/project';
+
+const snapshots = require('../../../ng-snapshot/package.json');
 
 export default async function () {
   // forcibly remove in case another test doesn't clean itself up
@@ -17,6 +22,24 @@ export default async function () {
   );
   if (hasPWADep) {
     throw new Error(`Expected 'package.json' not to contain a dependency on '@angular/pwa'.`);
+  }
+
+  const isSnapshotBuild = getGlobalVariable('argv')['ng-snapshots'];
+  if (isSnapshotBuild) {
+    const packagesToInstall = [];
+    await updateJsonFile('package.json', (packageJson) => {
+      const dependencies = packageJson['dependencies'];
+      // Iterate over all of the packages to update them to the snapshot version.
+      for (const [name, version] of Object.entries(snapshots.dependencies)) {
+        if (name in dependencies && dependencies[name] !== version) {
+          packagesToInstall.push(version);
+        }
+      }
+    });
+
+    for (const pkg of packagesToInstall) {
+      await installPackage(pkg);
+    }
   }
 
   // It should generate a SW configuration file (`ngsw.json`).
