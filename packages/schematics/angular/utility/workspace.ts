b36c0c96ec55aa93764ae5b46e932267495d9a8b
Alan Agius <alan.agius4@gmail.com>
2021-06-01 10:24:35 +0200

    fix(@schematics/angular): show better error when non existing project is passed to the component schematic
    Closes: #21003
    (cherry picked from commit 7cd801eb066543fe751ac6b7840c279e37b6e74a)

--- packages/schematics/angular/utility/workspace.ts
+++ packages/schematics/angular/utility/workspace.ts
@@ -85,7 +85,7 @@ export async function createDefaultPath(tree: Tree, projectName: string): Promis
   const workspace = await getWorkspace(tree);
   const project = workspace.projects.get(projectName);
   if (!project) {
-    throw new Error('Specified project does not exist.');
+    throw new Error(`Project "${projectName}" does not exist.`);
   }
 
   return buildDefaultPath(project);
