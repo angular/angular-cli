e0ce7a7a6ee04daef3853fd5b011334e7ff36026
Michel Ruffieux <michel.ruffieux@jobcloud.ch>
2021-06-04 23:13:59 +0200

    feat(@angular-devkit/schematics): refactor branch strategy to avoid file collision

--- packages/angular_devkit/schematics/src/rules/move.ts
+++ packages/angular_devkit/schematics/src/rules/move.ts
@@ -8,44 +8,43 @@
 
 import { join, normalize } from '@angular-devkit/core';
 import { Rule } from '../engine/interface';
-import { MergeStrategy } from '../tree/interface';
+import { Tree } from '../tree/interface';
 import { noop } from './base';
 
 export function move(from: string, to?: string | string[]): Rule {
-  if (to === undefined) {
+  if (typeof to === 'undefined') {
     to = from;
     from = '/';
   }
 
-  if (typeof from === 'string' && typeof to === 'string' && from === to) {
-    return noop;
-  }
-
   if (typeof to === 'string') {
-    // For simplicity, we wrap to string into an array
     to = new Array(to);
   }
 
   return (tree) => {
-    const fromPath = normalize('/' + from);
+    const arrayLength = (to as Array<string>).length;
 
-    (to as Array<string>).forEach((toElement: string) => {
+    (to as Array<string>).forEach((toElement: string, index: number) => {
+      const requireToBranch = arrayLength !== ++index;
+      const branchStrategy = requireToBranch ? tree.branch() : tree;
+      const fromPath = normalize('/' + from);
       const toPath = normalize('/' + toElement);
 
-      // we branch on tree to not override former fromPath
-      const branch = tree.branch();
+      if (fromPath === toPath) {
+        return noop;
+      }
 
-      if (tree.exists(fromPath)) {
-        // fromPath is a file
-        branch.rename(fromPath, toPath);
+      if (branchStrategy.exists(fromPath)) {
+        branchStrategy.rename(fromPath, toPath);
       } else {
-        // fromPath is a directory
-        branch.getDir(fromPath).visit((path) => {
-          branch.rename(path, join(toPath, path.substr(fromPath.length)));
+        branchStrategy.getDir(fromPath).visit((path) => {
+          branchStrategy.rename(path, join(toPath, path.substr(fromPath.length)));
         });
       }
 
-      tree.merge(branch, MergeStrategy.Overwrite);
+      if (requireToBranch) {
+        tree.merge(branchStrategy);
+      }
     });
 
     return tree;
