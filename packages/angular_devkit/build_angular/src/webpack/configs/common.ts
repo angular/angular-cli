4c2bd2f5a6de7eb3b1d75998fc80afc9ce8a1143
Alan Agius <alan.agius4@gmail.com>
2021-06-07 13:05:22 +0200

    perf(@angular-devkit/build-angular): reduce memory usage by cleaning output directory before emitting
    Unless `deleteOutputPath` is false, we should clean, this helps reduce assets in tests.
    
    (cherry picked from commit 699802d48853cedbf9468b94d196fd8106d43485)

--- packages/angular_devkit/build_angular/src/webpack/configs/common.ts
+++ packages/angular_devkit/build_angular/src/webpack/configs/common.ts
@@ -413,7 +413,7 @@ export function getCommonConfig(wco: WebpackConfigOptions): Configuration {
     context: root,
     entry: entryPoints,
     output: {
-      clean: buildOptions.deleteOutputPath,
+      clean: buildOptions.deleteOutputPath ?? true,
       path: path.resolve(root, buildOptions.outputPath),
       publicPath: buildOptions.deployUrl ?? '',
       filename: ({ chunk }) => {
