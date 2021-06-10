c9fe66ab7924ebbdddc0b6d1b469ea8753fcbe10
Doug Parker <dgp1130@users.noreply.github.com>
2021-06-10 09:38:33 -0700

    release: v12.1.0-next.5

--- packages/schematics/angular/utility/latest-versions.ts
+++ packages/schematics/angular/utility/latest-versions.ts
@@ -8,7 +8,7 @@
 
 export const latestVersions = {
   // These versions should be kept up to date with latest Angular peer dependencies.
-  Angular: '~12.1.0-next.4',
+  Angular: '~12.1.0-next.5',
   RxJs: '~6.6.0',
   ZoneJs: '~0.11.4',
   TypeScript: '~4.3.2',
