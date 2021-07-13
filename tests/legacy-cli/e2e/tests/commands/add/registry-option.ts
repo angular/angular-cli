acd12ba32f64540db21117e2e726eebce523e3cd
Mike Jancar <michael_jancar@progressive.com>
2021-07-13 13:24:31 -0400

    fix(@angular/cli): handle NPM_CONFIG environment variables during ng update and ng add
    Some organizations are moving away from storing tokens/secrets in an NPM config file in favor
    of environment variables that only exist for the span of a terminal session. This commit will
    make sure those variables are read even when there is no NPM config file present.

--- tests/legacy-cli/e2e/tests/commands/add/registry-option.ts
+++ tests/legacy-cli/e2e/tests/commands/add/registry-option.ts
@@ -10,9 +10,13 @@ export default async function () {
   await writeMultipleFiles({
     '.npmrc': 'registry=http://127.0.0.1:9999',
   });
-  // The environment variable has priority over the .npmrc
-  const originalRegistryVariable = process.env['NPM_CONFIG_REGISTRY'];
-  delete process.env['NPM_CONFIG_REGISTRY'];
+
+  let originalRegistryVariable;
+  if (process.env['NPM_CONFIG_REGISTRY']) {
+    // The environment variable has priority over the .npmrc
+    originalRegistryVariable = process.env['NPM_CONFIG_REGISTRY'];
+    delete process.env['NPM_CONFIG_REGISTRY'];
+  }
 
   try {
     await expectToFail(() => ng('add', '@angular/pwa', '--skip-confirmation'));
@@ -20,6 +24,8 @@ export default async function () {
     await ng('add', `--registry=${testRegistry}`, '@angular/pwa', '--skip-confirmation');
     await expectFileToExist('src/manifest.webmanifest');
   } finally {
-    process.env['NPM_CONFIG_REGISTRY'] = originalRegistryVariable;
+    if (originalRegistryVariable) {
+      process.env['NPM_CONFIG_REGISTRY'] = originalRegistryVariable;
+    }
   }
 }
