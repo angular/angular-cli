6946f68c7c8d590a7766521206fcaac40214d351
Charles Lyding <19598772+clydin@users.noreply.github.com>
2021-07-16 13:22:43 -0400

    build: transition from ts-api-guardian to dev-infra api-golden utility
    The dev-infra tooling now directly provides public API change testing capabilities that leverage the `api-extractor` utility. These new testing capabilities are shared with framework and components.
    `ts-api-guardian` has been removed as a dependency as a result.

--- goldens/public-api/angular_devkit/core/node/testing/index.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class TempScopedNodeJsSyncHost extends virtualFs.ScopedHost<fs.Stats> {
-    protected _root: Path;
-    protected _sync?: virtualFs.SyncDelegateHost<fs.Stats>;
-    get files(): Path[];
-    get root(): Path;
-    get sync(): virtualFs.SyncDelegateHost<fs.Stats>;
-    constructor();
-}
