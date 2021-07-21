99bd478b678a2a539a82cf4cba712e05f4e28836
Alan Agius <alan.agius4@gmail.com>
2021-07-21 11:35:29 +0200

    test(@angular/cli): improve environment variables clean up during E2E's

--- tests/legacy-cli/e2e/tests/update/update-secure-registry.ts
+++ tests/legacy-cli/e2e/tests/update/update-secure-registry.ts
@@ -4,37 +4,26 @@ import { expectToFail } from '../../utils/utils';
 
 export default async function () {
   // The environment variable has priority over the .npmrc
-  let originalRegistryVariable;
-  if (process.env['NPM_CONFIG_REGISTRY']) {
-    originalRegistryVariable = process.env['NPM_CONFIG_REGISTRY'];
-    delete process.env['NPM_CONFIG_REGISTRY'];
-  }
-
+  delete process.env['NPM_CONFIG_REGISTRY'];
   const worksMessage = 'We analyzed your package.json';
 
-  try {
-    // Valid authentication token
-    await createNpmConfigForAuthentication(false);
-    const { stdout: stdout1 } = await ng('update');
-    if (!stdout1.includes(worksMessage)) {
-      throw new Error(`Expected stdout to contain "${worksMessage}"`);
-    }
-
-    await createNpmConfigForAuthentication(true);
-    const { stdout: stdout2 } = await ng('update');
-    if (!stdout2.includes(worksMessage)) {
-      throw new Error(`Expected stdout to contain "${worksMessage}"`);
-    }
-
-    // Invalid authentication token
-    await createNpmConfigForAuthentication(false, true);
-    await expectToFail(() => ng('update'));
-
-    await createNpmConfigForAuthentication(true, true);
-    await expectToFail(() => ng('update'));
-  } finally {
-    if (originalRegistryVariable) {
-      process.env['NPM_CONFIG_REGISTRY'] = originalRegistryVariable;
-    }
+  // Valid authentication token
+  await createNpmConfigForAuthentication(false);
+  const { stdout: stdout1 } = await ng('update');
+  if (!stdout1.includes(worksMessage)) {
+    throw new Error(`Expected stdout to contain "${worksMessage}"`);
   }
+
+  await createNpmConfigForAuthentication(true);
+  const { stdout: stdout2 } = await ng('update');
+  if (!stdout2.includes(worksMessage)) {
+    throw new Error(`Expected stdout to contain "${worksMessage}"`);
+  }
+
+  // Invalid authentication token
+  await createNpmConfigForAuthentication(false, true);
+  await expectToFail(() => ng('update'));
+
+  await createNpmConfigForAuthentication(true, true);
+  await expectToFail(() => ng('update'));
 }
