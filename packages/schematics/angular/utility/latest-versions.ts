cfc267426716e9ecf0c9833720cb35298284f699
Charles Lyding <19598772+clydin@users.noreply.github.com>
2021-07-27 12:47:29 -0400

    fix(@schematics/angular): ensure valid SemVer range for new project Angular packages
    While npm supports a package specifier with a trailing dash, the trailing dash is technically not SemVer compliant and fails with Yarn 2+.
    The 12.1.x branch will not have another prerelease which allows the prelease specifiers to be ignored in this case.

--- packages/schematics/angular/utility/latest-versions.ts
+++ packages/schematics/angular/utility/latest-versions.ts
@@ -26,9 +26,8 @@ export const latestVersions = {
   TsLib: '^2.2.0',
 
   // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
-  // Angular CLI minor version with earliest prerelease (appended with `-`) will match the latest
-  // Angular Framework minor.
-  Angular: `~${getEarliestMinorVersion(require('../package.json')['version'])}-`,
+  // Angular CLI minor version will match the latest Angular Framework minor.
+  Angular: `~${getEarliestMinorVersion(require('../package.json')['version'])}`,
 
   // Since @angular-devkit/build-angular and @schematics/angular are always
   // published together from the same monorepo, and they are both
