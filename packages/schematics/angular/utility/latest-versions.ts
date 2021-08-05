f227e145dfbec2954cb96c92ab3c4cb97cbe0f32
Charles Lyding <19598772+clydin@users.noreply.github.com>
2021-08-05 19:14:48 -0400

    fix(@schematics/angular): updated Angular new project version to v13.0 prerelease

--- packages/schematics/angular/utility/latest-versions.ts
+++ packages/schematics/angular/utility/latest-versions.ts
@@ -15,7 +15,7 @@ export const latestVersions: Record<string, string> & {
   ...require('./latest-versions/package.json')['dependencies'],
 
   // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
-  Angular: '~12.2.0-rc.0',
+  Angular: '~13.0.0-next.0',
 
   // Since @angular-devkit/build-angular and @schematics/angular are always
   // published together from the same monorepo, and they are both
