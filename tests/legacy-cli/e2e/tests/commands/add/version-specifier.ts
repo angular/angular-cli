e47c4a57658438817581bc5e993c10b95419bef4
Alan Agius <alan.agius4@gmail.com>
2021-08-10 20:59:17 +0200

    test(@angular/cli): update @angular/localize version to 12
    This is needed because prior versions used the deprecated id instead of $id in the schema.

--- tests/legacy-cli/e2e/tests/commands/add/version-specifier.ts
+++ tests/legacy-cli/e2e/tests/commands/add/version-specifier.ts
@@ -3,14 +3,13 @@ import { uninstallPackage } from '../../../utils/packages';
 import { ng } from '../../../utils/process';
 import { isPrereleaseCli } from '../../../utils/project';
 
-
 export default async function () {
   // forcibly remove in case another test doesn't clean itself up.
   await rimraf('node_modules/@angular/localize');
 
-  const tag = await isPrereleaseCli() ?  '@next' : '';
+  const tag = (await isPrereleaseCli()) ? '@next' : '';
 
-  await ng('add',  `@angular/localize${tag}`, '--skip-confirmation');
+  await ng('add', `@angular/localize${tag}`, '--skip-confirmation');
   await expectFileToMatch('package.json', /@angular\/localize/);
 
   const output1 = await ng('add', '@angular/localize', '--skip-confirmation');
@@ -23,12 +22,12 @@ export default async function () {
     throw new Error('Installation should not have been skipped');
   }
 
-  const output3 = await ng('add', '@angular/localize@10.0.0', '--skip-confirmation');
+  const output3 = await ng('add', '@angular/localize@12.0.0', '--skip-confirmation');
   if (output3.stdout.includes('Skipping installation: Package already installed')) {
     throw new Error('Installation should not have been skipped');
   }
 
-  const output4 = await ng('add', '@angular/localize@10', '--skip-confirmation');
+  const output4 = await ng('add', '@angular/localize@12', '--skip-confirmation');
   if (!output4.stdout.includes('Skipping installation: Package already installed')) {
     throw new Error('Installation was not skipped');
   }
