6f4bf38e9ca0af03f8f6beff1ec67903727390ad
Charles Lyding <19598772+clydin@users.noreply.github.com>
2021-08-24 12:55:56 -0400

    refactor(@angular/cli): convert `npm-pick-manifest` usage from require to import
    The removal of require usage supports the eventual shift to an ESM output for the CLI package.

---
+++ packages/angular/cli/src/typings.d.ts
@@ -0,0 +1,15 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+declare module 'npm-pick-manifest' {
+  function pickManifest(
+    metadata: import('../utilities/package-metadata').PackageMetadata,
+    selector: string,
+  ): import('../utilities/package-metadata').PackageManifest;
+  export = pickManifest;
+}
