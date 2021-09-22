036384352896d61d53ffdfe7de665d57c8fc79d4
Alan Agius <alan.agius4@gmail.com>
2021-09-22 08:08:06 +0200

    build: replace ng-dev MergeConfig wiith PullRequestConfig
    More info: https://github.com/angular/dev-infra/commit/c3f5729376048af3ce939a505e5389dba11ff7d2

--- .ng-dev/config.ts
+++ .ng-dev/config.ts
@@ -1,5 +1,5 @@
 export { commitMessage } from './commit-message';
 export { github } from './github';
-export { merge } from './merge';
+export { pullRequest } from './pull-request';
 export { format } from './format';
 export { release } from './release';
