1b57ffca338058a414f46da44ef4791b4cfbd681
Alan Agius <alan.agius4@gmail.com>
2021-09-22 08:05:50 +0200

    build: replace ng-dev MergeConfig wiith PullRequestConfig
    More info: https://github.com/angular/dev-infra/commit/c3f5729376048af3ce939a505e5389dba11ff7d2

--- .ng-dev/pull-request.ts
+++ .ng-dev/pull-request.ts
@@ -1,10 +1,10 @@
-import { MergeConfig } from '@angular/dev-infra-private/ng-dev/pr/merge/config';
+import { PullRequestConfig } from '@angular/dev-infra-private/ng-dev/pr/config';
 
 /**
  * Configuration for the merge tool in `ng-dev`. This sets up the labels which
  * are respected by the merge script (e.g. the target labels).
  */
-export const merge: MergeConfig = {
+export const pullRequest: PullRequestConfig = {
   githubApiMerge: {
     default: 'rebase',
     labels: [{ pattern: 'squash commits', method: 'squash' }],
