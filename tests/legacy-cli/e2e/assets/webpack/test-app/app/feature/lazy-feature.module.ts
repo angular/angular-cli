81b49829351814d436edac7355c3d5693e178d71
Charles Lyding <19598772+clydin@users.noreply.github.com>
2021-09-30 18:40:48 -0400

    test: remove last string-form loadChildren usage
    One E2E test was still using the deprecated string-form of the routing `loadChildren` property. The `loadChildren` usage has now been converted to use the suppported dynamic import form. The string-form has been removed from Angular v13.

--- tests/legacy-cli/e2e/assets/webpack/test-app/app/feature/lazy-feature.module.ts
+++ tests/legacy-cli/e2e/assets/webpack/test-app/app/feature/lazy-feature.module.ts
@@ -1,19 +1,22 @@
-import {NgModule, Component} from '@angular/core';
-import {RouterModule} from '@angular/router';
+import { NgModule, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'lazy-feature-comp',
-  template: 'lazy feature!'
+  template: 'lazy feature!',
 })
 export class LazyFeatureComponent {}
 
 @NgModule({
   imports: [
     RouterModule.forChild([
-      {path: '', component: LazyFeatureComponent, pathMatch: 'full'},
-      {path: 'feature', loadChildren: './feature.module#FeatureModule'}
-    ])
+      { path: '', component: LazyFeatureComponent, pathMatch: 'full' },
+      {
+        path: 'feature',
+        loadChildren: () => import('./feature.module').then((m) => m.FeatureModule),
+      },
+    ]),
   ],
-  declarations: [LazyFeatureComponent]
+  declarations: [LazyFeatureComponent],
 })
 export class LazyFeatureModule {}
