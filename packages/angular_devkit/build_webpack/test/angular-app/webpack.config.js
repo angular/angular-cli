75fddc9b1333f517184fe1b85857febaae543968
Alan Agius <alan.agius4@gmail.com>
2021-10-05 14:50:58 +0200

    test(@angular-devkit/build-angular): workaround for rxjs 6 directory imports
    rxjs 6 requires directory imports which are not support in ES modules. Disabling `fullySpecified` allows Webpack to ignore cases such as this until the package can be updated to use package exports.

--- packages/angular_devkit/build_webpack/test/angular-app/webpack.config.js
+++ packages/angular_devkit/build_webpack/test/angular-app/webpack.config.js
@@ -24,6 +24,10 @@ module.exports = {
   ],
   module: {
     rules: [
+      // rxjs 6 requires directory imports which are not support in ES modules.
+      // Disabling `fullySpecified` allows Webpack to ignore this but this is
+      // not ideal because it currently disables ESM behavior import for all JS files.
+      { test: /\.[m]?js$/, resolve: { fullySpecified: false } },
       { test: /\.css$/, type: 'asset/source' },
       { test: /\.html$/, type: 'asset/source' },
       { test: /\.ts$/, loader: '@ngtools/webpack' },
