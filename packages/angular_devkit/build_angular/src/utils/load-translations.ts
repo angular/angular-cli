e11209591adf917221279652497af888389982b6
Alan Agius <alan.agius4@gmail.com>
2021-10-05 14:31:54 +0200

    refactor(@angular-devkit/build-angular): use `@angular/localize/tools` entry-point

--- packages/angular_devkit/build_angular/src/utils/load-translations.ts
+++ packages/angular_devkit/build_angular/src/utils/load-translations.ts
@@ -6,6 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
+import type { Diagnostics } from '@angular/localize/tools';
 import { createHash } from 'crypto';
 import * as fs from 'fs';
 import { loadEsmModule } from './load-esm';
@@ -14,7 +15,7 @@ export type TranslationLoader = (path: string) => {
   translations: Record<string, import('@angular/localize').ɵParsedTranslation>;
   format: string;
   locale?: string;
-  diagnostics: import('@angular/localize/src/tools/src/diagnostics').Diagnostics;
+  diagnostics: Diagnostics;
   integrity: string;
 };
 
@@ -60,64 +61,27 @@ export async function createTranslationLoader(): Promise<TranslationLoader> {
 }
 
 async function importParsers() {
-  // All the localize usages are setup to first try the ESM entry point then fallback to the deep imports.
-  // This provides interim compatibility while the framework is transitioned to bundled ESM packages.
-  // TODO_ESM: Remove all deep imports once `@angular/localize` is published with the `tools` entry point
-  let localizeToolsModule;
   try {
     // Load ESM `@angular/localize/tools` using the TypeScript dynamic import workaround.
     // Once TypeScript provides support for keeping the dynamic import this workaround can be
     // changed to a direct dynamic import.
-    // TODO_ESM: The type needs to be manually constructed until the tools entry point exists
-    localizeToolsModule = await loadEsmModule<{
-      Diagnostics: typeof import('@angular/localize/src/tools/src/diagnostics').Diagnostics;
-      /* eslint-disable max-len */
-      ArbTranslationParser: typeof import('@angular/localize/src/tools/src/translate/translation_files/translation_parsers/arb_translation_parser').ArbTranslationParser;
-      SimpleJsonTranslationParser: typeof import('@angular/localize/src/tools/src/translate/translation_files/translation_parsers/simple_json_translation_parser').SimpleJsonTranslationParser;
-      Xliff1TranslationParser: typeof import('@angular/localize/src/tools/src/translate/translation_files/translation_parsers/xliff1_translation_parser').Xliff1TranslationParser;
-      Xliff2TranslationParser: typeof import('@angular/localize/src/tools/src/translate/translation_files/translation_parsers/xliff2_translation_parser').Xliff2TranslationParser;
-      XtbTranslationParser: typeof import('@angular/localize/src/tools/src/translate/translation_files/translation_parsers/xtb_translation_parser').XtbTranslationParser;
-      /* eslint-enable max-len */
-    }>('@angular/localize/tools');
-  } catch {}
+    const {
+      Diagnostics,
+      ArbTranslationParser,
+      SimpleJsonTranslationParser,
+      Xliff1TranslationParser,
+      Xliff2TranslationParser,
+      XtbTranslationParser,
+    } = await loadEsmModule<typeof import('@angular/localize/tools')>('@angular/localize/tools');
 
-  try {
-    const { Diagnostics } =
-      localizeToolsModule ?? (await import('@angular/localize/src/tools/src/diagnostics'));
     const diagnostics = new Diagnostics();
-
     const parsers = {
-      arb: new (
-        localizeToolsModule ??
-        (await import(
-          '@angular/localize/src/tools/src/translate/translation_files/translation_parsers/arb_translation_parser'
-        ))
-      ).ArbTranslationParser(),
-      json: new (
-        localizeToolsModule ??
-        (await import(
-          '@angular/localize/src/tools/src/translate/translation_files/translation_parsers/simple_json_translation_parser'
-        ))
-      ).SimpleJsonTranslationParser(),
-      xlf: new (
-        localizeToolsModule ??
-        (await import(
-          '@angular/localize/src/tools/src/translate/translation_files/translation_parsers/xliff1_translation_parser'
-        ))
-      ).Xliff1TranslationParser(),
-      xlf2: new (
-        localizeToolsModule ??
-        (await import(
-          '@angular/localize/src/tools/src/translate/translation_files/translation_parsers/xliff2_translation_parser'
-        ))
-      ).Xliff2TranslationParser(),
+      arb: new ArbTranslationParser(),
+      json: new SimpleJsonTranslationParser(),
+      xlf: new Xliff1TranslationParser(),
+      xlf2: new Xliff2TranslationParser(),
       // The name ('xmb') needs to match the AOT compiler option
-      xmb: new (
-        localizeToolsModule ??
-        (await import(
-          '@angular/localize/src/tools/src/translate/translation_files/translation_parsers/xtb_translation_parser'
-        ))
-      ).XtbTranslationParser(),
+      xmb: new XtbTranslationParser(),
     };
 
     return { parsers, diagnostics };
