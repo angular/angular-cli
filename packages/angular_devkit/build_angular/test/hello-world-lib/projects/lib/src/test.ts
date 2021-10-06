585adacd0624ddf32c5c69a755d8e542f3463861
Alan Agius <alan.agius4@gmail.com>
2021-10-06 14:41:10 +0200

    fix(@schematics/angular): don't add `destroyAfterEach` in newly generated spec files
    This is now enabled by default.
    
    See https://github.com/angular/angular/pull/43353 for more context.

--- packages/angular_devkit/build_angular/test/hello-world-lib/projects/lib/src/test.ts
+++ packages/angular_devkit/build_angular/test/hello-world-lib/projects/lib/src/test.ts
@@ -27,7 +27,6 @@ declare const require: {
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting(),
-  { teardown: { destroyAfterEach: true }},
 );
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
