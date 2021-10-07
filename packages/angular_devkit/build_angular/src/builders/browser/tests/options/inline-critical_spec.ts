bbaccfb22e097215a811ffc8f4b3782bcae1a112
Alan Agius <alan.agius4@gmail.com>
2021-10-07 19:19:55 +0200

    Revert "feat(@angular-devkit/build-angular): disable critical CSS inlining by default"
    This reverts commit bf0709b14c1f0d47af77a64e586a5d4564b75766.
    
    (cherry picked from commit f3c0c98fbd291c83e117e6280e9846edad30f945)

--- packages/angular_devkit/build_angular/src/builders/browser/tests/options/inline-critical_spec.ts
+++ packages/angular_devkit/build_angular/src/builders/browser/tests/options/inline-critical_spec.ts
@@ -40,7 +40,7 @@ describeBuilder(buildWebpackBrowser, BROWSER_BUILDER_INFO, (harness) => {
       harness.expectFile('dist/index.html').content.toContain(`body{color:#000;}`);
     });
 
-    it(`should not extract critical css when 'optimization' is unset`, async () => {
+    it(`should extract critical css when 'optimization' is unset`, async () => {
       harness.useTarget('build', {
         ...BASE_OPTIONS,
         styles: ['src/styles.css'],
@@ -50,10 +50,15 @@ describeBuilder(buildWebpackBrowser, BROWSER_BUILDER_INFO, (harness) => {
       const { result } = await harness.executeOnce();
 
       expect(result?.success).toBe(true);
-      harness.expectFile('dist/index.html').content.not.toContain(`<style`);
+      harness
+        .expectFile('dist/index.html')
+        .content.toContain(
+          `<link rel="stylesheet" href="styles.css" media="print" onload="this.media='all'">`,
+        );
+      harness.expectFile('dist/index.html').content.toContain(`body{color:#000;}`);
     });
 
-    it(`should not extract critical css when 'optimization' is true`, async () => {
+    it(`should extract critical css when 'optimization' is true`, async () => {
       harness.useTarget('build', {
         ...BASE_OPTIONS,
         styles: ['src/styles.css'],
@@ -63,7 +68,12 @@ describeBuilder(buildWebpackBrowser, BROWSER_BUILDER_INFO, (harness) => {
       const { result } = await harness.executeOnce();
 
       expect(result?.success).toBe(true);
-      harness.expectFile('dist/index.html').content.not.toContain(`<style`);
+      harness
+        .expectFile('dist/index.html')
+        .content.toContain(
+          `<link rel="stylesheet" href="styles.css" media="print" onload="this.media='all'">`,
+        );
+      harness.expectFile('dist/index.html').content.toContain(`body{color:#000;}`);
     });
 
     it(`should not extract critical css when 'optimization' is false`, async () => {
