d4d89bbf2b28f576784ae107e1e2e1b42895947d
Alan Agius <alan.agius4@gmail.com>
2021-10-12 11:39:13 +0200

    refactor(@angular-devkit/build-angular): replace `Ivy Enabled` analytics dimension with `AOT Enabled`
    The motivation behind this change is that since version 12, application are always built using Ivy, in addition to this, adding AOT as dimension might be  helpful in our decision process if we want to remove JIT.
    
    (cherry picked from commit 079ef070c978b947005fa24bb14d4e5b7318d137)

--- packages/angular_devkit/core/src/analytics/index.ts
+++ packages/angular_devkit/core/src/analytics/index.ts
@@ -27,7 +27,7 @@ export enum NgCliAnalyticsDimensions {
   RamInGigabytes = 3,
   NodeVersion = 4,
   NgAddCollection = 6,
-  NgIvyEnabled = 8,
+  AotEnabled = 8,
   BuildErrors = 20,
 }
 
@@ -57,7 +57,7 @@ export const NgCliAnalyticsDimensionsFlagInfo: { [name: string]: [string, string
   RamInGigabytes: ['RAM (In GB)', 'number'],
   NodeVersion: ['Node Version', 'number'],
   NgAddCollection: ['--collection', 'string'],
-  NgIvyEnabled: ['Ivy Enabled', 'boolean'],
+  AotEnabled: ['AOT Enabled', 'boolean'],
   BuildErrors: ['Build Errors (comma separated)', 'string'],
 };
 
