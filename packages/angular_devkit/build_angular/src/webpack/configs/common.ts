a64e8215958acbaeefd3b588c52d3bc6842941a1
Alan Agius <alan.agius4@gmail.com>
2021-11-09 14:43:47 +0100

    perf(@angular-devkit/build-angular): disable webpack backwards compatible APIs
    See https://github.com/webpack/webpack/releases/tag/v5.62.0 and https://github.com/webpack/webpack/issues/14580 for more context.

--- packages/angular_devkit/build_angular/src/webpack/configs/common.ts
+++ packages/angular_devkit/build_angular/src/webpack/configs/common.ts
@@ -403,6 +403,7 @@ export async function getCommonConfig(wco: WebpackConfigOptions): Promise<Config
       ],
     },
     experiments: {
+      backCompat: false,
       syncWebAssembly: true,
       asyncWebAssembly: true,
     },
