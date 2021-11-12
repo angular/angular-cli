a51afc3952783e6bdec24500fd312a195e7384fa
Charles Lyding <19598772+clydin@users.noreply.github.com>
2021-11-12 17:04:26 -0500

    feat(@angular-devkit/build-angular): support JSON comments in dev-server proxy configuration file
    The `proxyConfig` option for the `dev-server` builder now supports JSON files containing comments and trailing commas.
    Several additional tests regarding the use of ESM and CommonJS JavaScript configuration files were also added to reduce the likelihood of future regressions.
    
    Closes: #21862

--- packages/angular_devkit/build_angular/src/builders/dev-server/tests/options/proxy-config_spec.ts
+++ packages/angular_devkit/build_angular/src/builders/dev-server/tests/options/proxy-config_spec.ts
@@ -25,7 +25,7 @@ describeBuilder(serveWebpackBrowser, DEV_SERVER_BUILDER_INFO, (harness) => {
       await harness.writeFile('src/main.ts', '');
     });
 
-    it('proxies requests based on the JSON proxy configuration file provided in the option', async () => {
+    it('proxies requests based on the `.json` proxy configuration file provided in the option', async () => {
       harness.useTarget('serve', {
         ...BASE_OPTIONS,
         proxyConfig: 'proxy.config.json',
@@ -49,7 +49,34 @@ describeBuilder(serveWebpackBrowser, DEV_SERVER_BUILDER_INFO, (harness) => {
       }
     });
 
-    it('proxies requests based on the JS proxy configuration file provided in the option', async () => {
+    it('proxies requests based on the `.json` (with comments) proxy configuration file provided in the option', async () => {
+      harness.useTarget('serve', {
+        ...BASE_OPTIONS,
+        proxyConfig: 'proxy.config.json',
+      });
+
+      const proxyServer = createProxyServer();
+      try {
+        await new Promise<void>((resolve) => proxyServer.listen(0, '127.0.0.1', resolve));
+        const proxyAddress = proxyServer.address() as import('net').AddressInfo;
+
+        await harness.writeFiles({
+          'proxy.config.json': `
+            // JSON file with comments
+            { "/api/*": { "target": "http://127.0.0.1:${proxyAddress.port}" } }
+          `,
+        });
+
+        const { result, response } = await executeOnceAndFetch(harness, '/api/test');
+
+        expect(result?.success).toBeTrue();
+        expect(await response?.text()).toContain('TEST_API_RETURN');
+      } finally {
+        await new Promise<void>((resolve) => proxyServer.close(() => resolve()));
+      }
+    });
+
+    it('proxies requests based on the `.js` (CommonJS) proxy configuration file provided in the option', async () => {
       harness.useTarget('serve', {
         ...BASE_OPTIONS,
         proxyConfig: 'proxy.config.js',
@@ -73,7 +100,56 @@ describeBuilder(serveWebpackBrowser, DEV_SERVER_BUILDER_INFO, (harness) => {
       }
     });
 
-    it('proxies requests based on the MJS proxy configuration file provided in the option', async () => {
+    it('proxies requests based on the `.js` (ESM) proxy configuration file provided in the option', async () => {
+      harness.useTarget('serve', {
+        ...BASE_OPTIONS,
+        proxyConfig: 'proxy.config.js',
+      });
+
+      const proxyServer = createProxyServer();
+      try {
+        await new Promise<void>((resolve) => proxyServer.listen(0, '127.0.0.1', resolve));
+        const proxyAddress = proxyServer.address() as import('net').AddressInfo;
+
+        await harness.writeFiles({
+          'proxy.config.js': `export default { "/api/*": { "target": "http://127.0.0.1:${proxyAddress.port}" } }`,
+          'package.json': '{ "type": "module" }',
+        });
+
+        const { result, response } = await executeOnceAndFetch(harness, '/api/test');
+
+        expect(result?.success).toBeTrue();
+        expect(await response?.text()).toContain('TEST_API_RETURN');
+      } finally {
+        await new Promise<void>((resolve) => proxyServer.close(() => resolve()));
+      }
+    });
+
+    it('proxies requests based on the `.cjs` proxy configuration file provided in the option', async () => {
+      harness.useTarget('serve', {
+        ...BASE_OPTIONS,
+        proxyConfig: 'proxy.config.cjs',
+      });
+
+      const proxyServer = createProxyServer();
+      try {
+        await new Promise<void>((resolve) => proxyServer.listen(0, '127.0.0.1', resolve));
+        const proxyAddress = proxyServer.address() as import('net').AddressInfo;
+
+        await harness.writeFiles({
+          'proxy.config.cjs': `module.exports = { "/api/*": { "target": "http://127.0.0.1:${proxyAddress.port}" } }`,
+        });
+
+        const { result, response } = await executeOnceAndFetch(harness, '/api/test');
+
+        expect(result?.success).toBeTrue();
+        expect(await response?.text()).toContain('TEST_API_RETURN');
+      } finally {
+        await new Promise<void>((resolve) => proxyServer.close(() => resolve()));
+      }
+    });
+
+    it('proxies requests based on the `.mjs` proxy configuration file provided in the option', async () => {
       harness.useTarget('serve', {
         ...BASE_OPTIONS,
         proxyConfig: 'proxy.config.mjs',
@@ -109,6 +185,30 @@ describeBuilder(serveWebpackBrowser, DEV_SERVER_BUILDER_INFO, (harness) => {
       expect(error).toEqual(
         jasmine.objectContaining({
           message: jasmine.stringMatching('INVALID\\.json does not exist'),
+        }),
+      );
+    });
+
+    it('throws an error when JSON proxy configuration file cannot be parsed', async () => {
+      harness.useTarget('serve', {
+        ...BASE_OPTIONS,
+        proxyConfig: 'proxy.config.json',
+      });
+
+      // Create a JSON file with a parse error (target property has no value)
+      await harness.writeFiles({
+        'proxy.config.json': `
+          // JSON file with comments
+          { "/api/*": { "target": } }
+        `,
+      });
+
+      const { result, error } = await harness.executeOnce({ outputLogsOnException: false });
+
+      expect(result).toBeUndefined();
+      expect(error).toEqual(
+        jasmine.objectContaining({
+          message: jasmine.stringMatching('contains parse errors:\\n\\[3, 35\\] ValueExpected'),
         }),
       );
     });
