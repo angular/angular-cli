d0924da418c8314555393afb809d5f6d2cc34a87
Alan Agius <alan.agius4@gmail.com>
2021-11-23 15:04:58 +0100

    test(@angular/cli): fix failing LTS e2e tests

--- tests/legacy-cli/e2e/tests/commands/add/add-pwa-yarn.ts
+++ tests/legacy-cli/e2e/tests/commands/add/add-pwa-yarn.ts
@@ -3,23 +3,26 @@ import { expectFileToExist, readFile, rimraf } from '../../../utils/fs';
 import { ng, npm } from '../../../utils/process';
 
 export default async function () {
-    // forcibly remove in case another test doesn't clean itself up
-    await rimraf('node_modules/@angular/pwa');
+  // forcibly remove in case another test doesn't clean itself up
+  await rimraf('node_modules/@angular/pwa');
 
-    // set yarn as package manager
-    await ng('config', 'cli.packageManager', 'yarn');
-    await ng('add', '@angular/pwa');
-    await expectFileToExist(join(process.cwd(), 'src/manifest.webmanifest'));
+  // set yarn as package manager
+  await ng('config', 'cli.packageManager', 'yarn');
+  await ng('add', '@angular/pwa', '--registry=http://localhost:4873');
+  await expectFileToExist(join(process.cwd(), 'src/manifest.webmanifest'));
 
-    // Angular PWA doesn't install as a dependency
-    const { dependencies, devDependencies } = JSON.parse(await readFile(join(process.cwd(), 'package.json')));
-    const hasPWADep = Object.keys({ ...dependencies, ...devDependencies })
-        .some(d => d === '@angular/pwa');
-    if (hasPWADep) {
-        throw new Error(`Expected 'package.json' not to contain a dependency on '@angular/pwa'.`);
-    }
+  // Angular PWA doesn't install as a dependency
+  const { dependencies, devDependencies } = JSON.parse(
+    await readFile(join(process.cwd(), 'package.json')),
+  );
+  const hasPWADep = Object.keys({ ...dependencies, ...devDependencies }).some(
+    (d) => d === '@angular/pwa',
+  );
+  if (hasPWADep) {
+    throw new Error(`Expected 'package.json' not to contain a dependency on '@angular/pwa'.`);
+  }
 
-    // 'yarn' will nuke the entire node_modules when it is triggered during the above tests.
-    await rimraf('node_modules');
-    await npm('install');
+  // 'yarn' will nuke the entire node_modules when it is triggered during the above tests.
+  await rimraf('node_modules');
+  await npm('install');
 }
