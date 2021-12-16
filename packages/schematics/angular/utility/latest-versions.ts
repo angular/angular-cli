1badd2179096a400823c1bde9245938283a5d85e
Doug Parker <dgp1130@users.noreply.github.com>
2021-12-16 09:33:11 -0800

    release: v11.2.17

--- packages/schematics/angular/utility/latest-versions.ts
+++ packages/schematics/angular/utility/latest-versions.ts
@@ -18,9 +18,9 @@ export const latestVersions = {
   // For our e2e tests, these versions must match the latest tag present on the branch.
   // During RC periods they will not match the latest RC until there's a new git tag, and
   // should not be updated.
-  DevkitBuildAngular: '~0.1102.13',
-  DevkitBuildNgPackagr: '~0.1102.13',
-  DevkitBuildWebpack: '~0.1102.13',
+  DevkitBuildAngular: '~0.1102.17',
+  DevkitBuildNgPackagr: '~0.1102.17',
+  DevkitBuildWebpack: '~0.1102.17',
 
   ngPackagr: '^11.0.0',
 };
