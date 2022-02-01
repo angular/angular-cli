270860d98dc289295079061c2e69df43a1e1b2f9
Alan Agius <alan.agius4@gmail.com>
2022-02-01 17:17:20 +0100

    wip! test.ts without require.context

--- packages/angular_devkit/build_angular/src/webpack/plugins/single-test-transform.ts
+++ packages/angular_devkit/build_angular/src/webpack/plugins/single-test-transform.ts
@@ -6,12 +6,12 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import { logging, tags } from '@angular-devkit/core';
+import { logging } from '@angular-devkit/core';
 import { extname } from 'path';
 
 export interface SingleTestTransformLoaderOptions {
   /* list of paths relative to the entry-point */
-  files?: string[];
+  files: string[];
   logger?: logging.Logger;
 }
 
@@ -20,38 +20,16 @@ export const SingleTestTransformLoader = __filename;
 /**
  * This loader transforms the default test file to only run tests
  * for some specs instead of all specs.
- * It works by replacing the known content of the auto-generated test file:
- *   const context = require.context('./', true, /\.spec\.ts$/);
- *   context.keys().map(context);
- * with:
- *   const context = { keys: () => ({ map: (_a) => { } }) };
- *   context.keys().map(context);
- * So that it does nothing.
- * Then it adds import statements for each file in the files options
- * array to import them directly, and thus run the tests there.
  */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function loader(
   this: import('webpack').LoaderContext<SingleTestTransformLoaderOptions>,
   source: string,
 ): string {
-  const { files = [], logger = console } = this.getOptions();
-  // signal the user that expected content is not present.
-  if (!source.includes('require.context(')) {
-    logger.error(tags.stripIndent`The 'include' option requires that the 'main' file for tests includes the below line:
-      const context = require.context('./', true, /\.spec\.ts$/);
-      Arguments passed to require.context are not strict and can be changed.`);
+  const { files } = this.getOptions();
 
-    return source;
-  }
-
-  const targettedImports = files
+  const imports = files
     .map((path) => `require('./${path.replace('.' + extname(path), '')}');`)
     .join('\n');
 
-  const mockedRequireContext =
-    'Object.assign(() => { }, { keys: () => [], resolve: () => undefined });\n';
-  source = source.replace(/require\.context\(.*/, mockedRequireContext + targettedImports);
-
-  return source;
+  return `${source}\n${imports}`;
 }
