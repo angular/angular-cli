edf05f2b520a852b1aa76a99fd43906479935e20
Daniele Maltese <daniele.maltese@soluzione1.it>
2022-02-23 17:20:29 +0100

    fix(@angular-devkit/core): typo in string.ts doc Typo in underscore function code documentation.

--- packages/angular_devkit/core/src/utils/strings.ts
+++ packages/angular_devkit/core/src/utils/strings.ts
@@ -93,7 +93,7 @@ export function classify(str: string): string {
 }
 
 /**
- More general than decamelize. Returns the lower\_case\_and\_underscored
+ More general than decamelize. Returns the lower_case_and_underscored
  form of a string.
 
  ```javascript
