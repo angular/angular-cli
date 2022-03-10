a74c0acb3e4275af228606b42551b519906cad7c
Alan Agius <alan.agius4@gmail.com>
2022-03-10 18:53:05 +0100

    docs: replace `app` with `application`

--- packages/schematics/angular/e2e/e2e-long.md
+++ packages/schematics/angular/e2e/e2e-long.md
@@ -1,2 +1,2 @@
-The e2e tests are created in a separate app in the `projects` folder of the workspace,
+The e2e tests are created in a separate application in the `projects` folder of the workspace,
 next to the project being tested.
--- packages/schematics/angular/service-worker/index.ts
+++ packages/schematics/angular/service-worker/index.ts
@@ -95,11 +95,11 @@ function updateAppModule(mainPath: string): Rule {
       }
     }
 
-    // register SW in app module
+    // register SW in application module
     const importText = tags.stripIndent`
       ServiceWorkerModule.register('ngsw-worker.js', {
         enabled: ${importModule}.production,
-        // Register the ServiceWorker as soon as the app is stable
+        // Register the ServiceWorker as soon as the application is stable
         // or after 30 seconds (whichever comes first).
         registrationStrategy: 'registerWhenStable:30000'
       })
@@ -146,7 +146,7 @@ export default function (options: ServiceWorkerOptions): Rule {
     if (!buildTarget) {
       throw targetBuildNotFoundError();
     }
-    const buildOptions = ((buildTarget.options || {}) as unknown) as BrowserBuilderOptions;
+    const buildOptions = (buildTarget.options || {}) as unknown as BrowserBuilderOptions;
     const root = project.root;
     buildOptions.serviceWorker = true;
     buildOptions.ngswConfigPath = join(normalize(root), 'ngsw-config.json');
