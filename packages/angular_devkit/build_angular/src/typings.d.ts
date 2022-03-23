eedfdefa669519c02e725bacbb7414193c677308
Alan Agius <alan.agius4@gmail.com>
2022-03-23 14:53:36 +0100

    refactor(@angular-devkit/build-angular): remove types for `@discoveryjs/json-ext`
    This package now ships TypeScript declaration files.

--- packages/angular_devkit/build_angular/src/typings.d.ts
+++ packages/angular_devkit/build_angular/src/typings.d.ts
@@ -6,10 +6,6 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-declare module '@discoveryjs/json-ext' {
-  export function stringifyStream(value: unknown): import('stream').Readable;
-}
-
 declare module '@babel/helper-annotate-as-pure' {
   export default function annotateAsPure(
     pathOrNode: import('@babel/types').Node | { node: import('@babel/types').Node },
