269fb87ae17120eb4267bc98e96202e10b910f6f
Alan Agius <alan.agius4@gmail.com>
2022-05-03 08:23:33 +0200

    test: reject promise when output doesn't match `waitForMatch`
    Previously, when the process output didn't match, the promise was always resolved.

--- tests/legacy-cli/e2e/tests/misc/completion.ts
+++ tests/legacy-cli/e2e/tests/misc/completion.ts
@@ -2,49 +2,61 @@ import { execAndWaitForOutputToMatch } from '../../utils/process';
 
 export default async function () {
   // ng build
-  await execAndWaitForOutputToMatch('ng', ['--get-yargs-completions', 'b', ''], /test-project/);
-  await execAndWaitForOutputToMatch('ng', ['--get-yargs-completions', 'build', ''], /test-project/);
-  await execAndWaitForOutputToMatch('ng', ['--get-yargs-completions', 'build', '--a'], /--aot/);
   await execAndWaitForOutputToMatch(
     'ng',
-    ['--get-yargs-completions', 'build', '--configuration'],
+    ['--get-yargs-completions', 'ng', 'b', ''],
+    /test-project/,
+  );
+  await execAndWaitForOutputToMatch(
+    'ng',
+    ['--get-yargs-completions', 'ng', 'build', ''],
+    /test-project/,
+  );
+  await execAndWaitForOutputToMatch(
+    'ng',
+    ['--get-yargs-completions', 'ng', 'build', '--a'],
+    /--aot/,
+  );
+  await execAndWaitForOutputToMatch(
+    'ng',
+    ['--get-yargs-completions', 'ng', 'build', '--configuration'],
     /production/,
   );
   await execAndWaitForOutputToMatch(
     'ng',
-    ['--get-yargs-completions', 'b', '--configuration'],
+    ['--get-yargs-completions', 'ng', 'b', '--configuration'],
     /production/,
   );
 
   // ng run
   await execAndWaitForOutputToMatch(
     'ng',
-    ['--get-yargs-completions', 'run', ''],
-    /test-project\:build\:development/,
+    ['--get-yargs-completions', 'ng', 'run', ''],
+    /test-project\\:build\\:development/,
   );
   await execAndWaitForOutputToMatch(
     'ng',
-    ['--get-yargs-completions', 'run', ''],
-    /test-project\:build/,
+    ['--get-yargs-completions', 'ng', 'run', ''],
+    /test-project\\:build/,
   );
   await execAndWaitForOutputToMatch(
     'ng',
-    ['--get-yargs-completions', 'run', ''],
-    /test-project\:test/,
+    ['--get-yargs-completions', 'ng', 'run', ''],
+    /test-project\\:test/,
   );
   await execAndWaitForOutputToMatch(
     'ng',
-    ['--get-yargs-completions', 'run', 'test-project:build'],
-    /test-project\:build\:development/,
+    ['--get-yargs-completions', 'ng', 'run', 'test-project:build'],
+    /test-project\\:build\\:development/,
   );
   await execAndWaitForOutputToMatch(
     'ng',
-    ['--get-yargs-completions', 'run', 'test-project:'],
-    /test-project\:test/,
+    ['--get-yargs-completions', 'ng', 'run', 'test-project:'],
+    /test-project\\:test/,
   );
   await execAndWaitForOutputToMatch(
     'ng',
-    ['--get-yargs-completions', 'run', 'test-project:build'],
+    ['--get-yargs-completions', 'ng', 'run', 'test-project:build'],
     // does not include 'test-project:serve'
     /^((?!:serve).)*$/,
   );
