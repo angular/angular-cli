77ba3613566770a6b58ed469957269eedf69fa45
Charles Lyding <19598772+clydin@users.noreply.github.com>
2022-05-05 14:54:20 -0400

    test: add a minimal standalone component application E2E test
    A new E2E test has been added that updates a newly generated application to use a standalone component that
    is bootstrapped with the newly introduced `bootstrapApplication` API. This test is intended to check that
    the minimal functionality for a standalone-based application functions with the Angular CLI. More expansive
    tests will be added as standalone features and capabilities are introduced within the Angular CLI.
    
    (cherry picked from commit d51e63b26b95d10076589fecb1be749176fcbd13)

---
+++ tests/legacy-cli/e2e/tests/basic/standalone.ts
@@ -0,0 +1,56 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ *
+ * @fileoverview
+ * Tests the minimal conversion of a newly generated application
+ * to use a single standalone component.
+ */
+
+import { writeFile } from '../../utils/fs';
+import { ng } from '../../utils/process';
+
+/**
+ * An application main file that uses a standalone component with
+ * bootstrapApplication to start the application. `ng-template` and
+ * `ngIf` are used to ensure that `CommonModule` and `imports` are
+ * working in standalone mode.
+ */
+const STANDALONE_MAIN_CONTENT = `
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { bootstrapApplication } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  template: \`
+    <ng-template [ngIf]="isVisible">
+      <div class="content">
+        <span>{{name}} app is running!</span>
+      </div>
+    </ng-template>
+  \`,
+  imports: [CommonModule],
+})
+export class AppComponent {
+  name = 'test-project';
+  isVisible = true;
+}
+
+bootstrapApplication(AppComponent);
+`;
+
+export default async function () {
+  // Update to a standalone application
+  await writeFile('src/main.ts', STANDALONE_MAIN_CONTENT);
+
+  // Execute a production build
+  await ng('build');
+
+  // Perform the default E2E tests
+  await ng('e2e', 'test-project');
+}
