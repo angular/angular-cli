5a6f621f86d01ba0182d01f0ba3419864653285e
Charles Lyding <19598772+clydin@users.noreply.github.com>
2022-05-06 11:05:08 -0400

    build: update `@angular/localize` import to use non-private entry
    The `ɵParsedTranslation` type is now exported from the root entry point and can be used without the `private` deep import.

--- packages/angular_devkit/build_angular/src/babel/presets/application.ts
+++ packages/angular_devkit/build_angular/src/babel/presets/application.ts
@@ -6,7 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import type { ɵParsedTranslation } from '@angular/localize/private';
+import type { ɵParsedTranslation } from '@angular/localize';
 import type {
   DiagnosticHandlingStrategy,
   Diagnostics,
