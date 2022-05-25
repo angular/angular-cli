007dd65926675ed6d23589cfcb33fc317c9464f2
Charles Lyding <19598772+clydin@users.noreply.github.com>
2022-05-25 12:38:17 -0400

    test: ensure temporary .npmrc file is removed on win32 in secure-registry E2E

--- tests/legacy-cli/e2e/tests/commands/add/secure-registry.ts
+++ tests/legacy-cli/e2e/tests/commands/add/secure-registry.ts
@@ -1,3 +1,4 @@
+import { rm } from 'fs/promises';
 import { expectFileNotToExist, expectFileToExist } from '../../../utils/fs';
 import { git, ng } from '../../../utils/process';
 import { createNpmConfigForAuthentication } from '../../../utils/registry';
@@ -10,23 +11,28 @@ export default async function () {
   const command = ['add', '@angular/pwa', '--skip-confirmation'];
   await expectFileNotToExist('src/manifest.webmanifest');
 
-  // Works with unscoped registry authentication details
-  await createNpmConfigForAuthentication(false);
-  await ng(...command);
-  await expectFileToExist('src/manifest.webmanifest');
-  await git('clean', '-dxf');
+  try {
+    // Works with unscoped registry authentication details
+    await createNpmConfigForAuthentication(false);
+    await ng(...command);
+    await expectFileToExist('src/manifest.webmanifest');
+    await git('clean', '-dxf');
 
-  // Works with scoped registry authentication details
-  await expectFileNotToExist('src/manifest.webmanifest');
+    // Works with scoped registry authentication details
+    await expectFileNotToExist('src/manifest.webmanifest');
 
-  await createNpmConfigForAuthentication(true);
-  await ng(...command);
-  await expectFileToExist('src/manifest.webmanifest');
+    await createNpmConfigForAuthentication(true);
+    await ng(...command);
+    await expectFileToExist('src/manifest.webmanifest');
 
-  // Invalid authentication token
-  await createNpmConfigForAuthentication(false, true);
-  await expectToFail(() => ng(...command));
+    // Invalid authentication token
+    await createNpmConfigForAuthentication(false, true);
+    await expectToFail(() => ng(...command));
 
-  await createNpmConfigForAuthentication(true, true);
-  await expectToFail(() => ng(...command));
+    await createNpmConfigForAuthentication(true, true);
+    await expectToFail(() => ng(...command));
+  } finally {
+    // On win32, the `.npmrc` file does not get removed properly
+    await rm('.npmrc', { force: true });
+  }
 }
