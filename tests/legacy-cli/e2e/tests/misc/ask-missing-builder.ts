8f42740ce62eb98863805693a48d12e4866382ab
Alan Agius <alanagius@google.com>
2022-05-30 10:13:12 +0000

    test: use `execAndWaitForOutputToMatch` to match prompt output
    Since the prompt will be displayed in the same process on `ng deploy` and `ng lint` we don't need to look for matching outputs in all processes. This also fixes some failures on Windows.

--- tests/legacy-cli/e2e/tests/misc/ask-missing-builder.ts
+++ tests/legacy-cli/e2e/tests/misc/ask-missing-builder.ts
@@ -1,30 +1,27 @@
-import { execWithEnv, killAllProcesses, waitForAnyProcessOutputToMatch } from '../../utils/process';
+import { execAndWaitForOutputToMatch, killAllProcesses } from '../../utils/process';
 
 export default async function () {
   try {
-    // Execute a command with TTY force enabled
-    execWithEnv('ng', ['deploy'], {
-      ...process.env,
-      NG_FORCE_TTY: '1',
-      NG_CLI_ANALYTICS: 'false',
-    });
-
-    // Check if the prompt is shown
-    await waitForAnyProcessOutputToMatch(
+    // Execute a command with TTY force enabled and check that the prompt is shown.
+    await execAndWaitForOutputToMatch(
+      'ng',
+      ['deploy'],
       /Would you like to add a package with "deploy" capabilities/,
+      {
+        ...process.env,
+        NG_FORCE_TTY: '1',
+        NG_CLI_ANALYTICS: 'false',
+      },
     );
 
     await killAllProcesses();
 
-    // Execute a command with TTY force enabled
-    execWithEnv('ng', ['lint'], {
+    // Execute a command with TTY force enabled and check that the prompt is shown.
+    await execAndWaitForOutputToMatch('ng', ['lint'], /Would you like to add ESLint now/, {
       ...process.env,
       NG_FORCE_TTY: '1',
       NG_CLI_ANALYTICS: 'false',
     });
-
-    // Check if the prompt is shown
-    await waitForAnyProcessOutputToMatch(/Would you like to add ESLint now/);
   } finally {
     await killAllProcesses();
   }
