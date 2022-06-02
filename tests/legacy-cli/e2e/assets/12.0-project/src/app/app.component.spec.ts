542d369007553ad5b598b97d151565801bc625b9
Alan Agius <alanagius@google.com>
2022-06-02 15:46:12 +0000

    test: remove update tests for no longer LTS versions
    With this change we remove update tests for version 10 and 11 which are no longer under LTS support.

--- tests/legacy-cli/e2e/assets/12.0-project/src/app/app.component.spec.ts
+++ tests/legacy-cli/e2e/assets/12.0-project/src/app/app.component.spec.ts
@@ -5,8 +5,12 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [AppComponent],
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -16,18 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ten-project'`, () => {
+  it(`should have as title 'twelve-project'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ten-project');
+    expect(app.title).toEqual('twelve-project');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain(
-      'ten-project app is running!',
-    );
+    expect(compiled.querySelector('.content span').textContent).toContain('twelve-project app is running!');
   });
 });
--- tests/legacy-cli/e2e/assets/12.0-project/src/main.ts
+++ tests/legacy-cli/e2e/assets/12.0-project/src/main.ts
@@ -8,6 +8,5 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
