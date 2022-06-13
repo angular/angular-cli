59fe830d185951f6c17cb341b424e3699d9dd95e
Alan Agius <alanagius@google.com>
2022-06-13 17:51:20 +0000

    test: simplify test sourcemaps e2e test

--- tests/legacy-cli/e2e/tests/test/test-sourcemap.ts
+++ tests/legacy-cli/e2e/tests/test/test-sourcemap.ts
@@ -1,6 +1,5 @@
 import { writeFile } from '../../utils/fs';
 import { ng } from '../../utils/process';
-import { updateJsonFile } from '../../utils/project';
 
 export default async function () {
   await writeFile(
@@ -12,46 +11,19 @@ export default async function () {
     `,
   );
 
-  await updateJsonFile('angular.json', (configJson) => {
-    const appArchitect = configJson.projects['test-project'].architect;
-    appArchitect.test.options.sourceMap = {
-      scripts: true,
-    };
-  });
-
   // when sourcemaps are 'on' the stacktrace will point to the spec.ts file.
   try {
-    await ng('test', '--watch', 'false');
+    await ng('test', '--no-watch', '--source-map');
     throw new Error('ng test should have failed.');
   } catch (error) {
     if (!error.message.includes('app.component.spec.ts')) {
       throw error;
     }
   }
-
-  await updateJsonFile('angular.json', (configJson) => {
-    const appArchitect = configJson.projects['test-project'].architect;
-    appArchitect.test.options.sourceMap = true;
-  });
-
-  // when sourcemaps are 'on' the stacktrace will point to the spec.ts file.
-  try {
-    await ng('test', '--watch', 'false');
-    throw new Error('ng test should have failed.');
-  } catch (error) {
-    if (!error.message.includes('app.component.spec.ts')) {
-      throw error;
-    }
-  }
-
-  await updateJsonFile('angular.json', (configJson) => {
-    const appArchitect = configJson.projects['test-project'].architect;
-    appArchitect.test.options.sourceMap = false;
-  });
 
   // when sourcemaps are 'off' the stacktrace won't point to the spec.ts file.
   try {
-    await ng('test', '--watch', 'false');
+    await ng('test', '--no-watch', '--no-source-map');
     throw new Error('ng test should have failed.');
   } catch (error) {
     if (!error.message.includes('main.js')) {
