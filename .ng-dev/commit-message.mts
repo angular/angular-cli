64cd98bc905631566d5ebc4e0fd02366b277fede
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-06-17 13:47:17 +0000

    build: update `ng-dev` setup to work with new ESM version
    The `ng-dev` tool is now strict ESM. Config loading needs to be updated
    to also emit `.mjs` with `ts-node` (this cannot happen dynamically as
    ESM Node loading cannot be patched by ts-node at runtime).

---
+++ .ng-dev/commit-message.mts
@@ -0,0 +1,13 @@
+import { CommitMessageConfig } from '@angular/dev-infra-private/ng-dev';
+import packages from '../lib/packages.js';
+
+/**
+ * The configuration for `ng-dev commit-message` commands.
+ */
+export const commitMessage: CommitMessageConfig = {
+  maxLineLength: Infinity,
+  minBodyLength: 0,
+  minBodyLengthTypeExcludes: ['docs'],
+  // Note: When changing this logic, also change the `contributing.ejs` file.
+  scopes: [...Object.keys(packages.packages)],
+};
--- scripts/templates.ts
+++ scripts/templates.ts
@@ -10,11 +10,16 @@ import { logging } from '@angular-devkit/core';
 import * as fs from 'fs';
 import * as path from 'path';
 
-function _runTemplate(inputPath: string, outputPath: string, logger: logging.Logger) {
+async function _runTemplate(inputPath: string, outputPath: string, logger: logging.Logger) {
   inputPath = path.resolve(__dirname, inputPath);
   outputPath = path.resolve(__dirname, outputPath);
 
   logger.info(`Building ${path.relative(path.dirname(__dirname), outputPath)}...`);
+
+  // TODO(ESM): Consider making this an actual import statement.
+  const { COMMIT_TYPES, ScopeRequirement } = await new Function(
+    `return import('@angular/dev-infra-private/ng-dev');`,
+  )();
 
   const template = require(inputPath).default;
   const content = template({
@@ -22,13 +27,18 @@ function _runTemplate(inputPath: string, outputPath: string, logger: logging.Log
     packages: require('../lib/packages').packages,
     encode: (x: string) => global.encodeURIComponent(x),
     require: (x: string) => require(path.resolve(path.dirname(inputPath), x)),
+
+    // Pass-through `ng-dev` ESM commit message information for the `contributing.ejs`
+    // template. EJS templates using the devkit template cannot use ESM.
+    COMMIT_TYPES: COMMIT_TYPES,
+    ScopeRequirement: ScopeRequirement,
   });
   fs.writeFileSync(outputPath, content, 'utf-8');
 }
 
 export default async function (_options: {}, logger: logging.Logger): Promise<number> {
-  _runTemplate('./templates/readme', '../README.md', logger);
-  _runTemplate('./templates/contributing', '../CONTRIBUTING.md', logger);
+  await _runTemplate('./templates/readme', '../README.md', logger);
+  await _runTemplate('./templates/contributing', '../CONTRIBUTING.md', logger);
 
   return 0;
 }
