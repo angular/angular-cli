6d7dc5b1bc98ba8327b0ccab2d28efe31d19a33a
Jason Bedard <jason+github@jbedard.ca>
2022-06-24 16:16:43 -0700

    test: align yarn and npm prefix config

--- tests/legacy-cli/e2e/setup/002-npm-sandbox.ts
+++ tests/legacy-cli/e2e/setup/002-npm-sandbox.ts
@@ -8,15 +8,21 @@ import { getGlobalVariable, setGlobalVariable } from '../utils/env';
 export default async function () {
   const tempRoot: string = getGlobalVariable('tmp-root');
   const npmModulesPrefix = join(tempRoot, 'npm-global');
+  const yarnModulesPrefix = join(tempRoot, 'yarn-global');
   const npmRegistry: string = getGlobalVariable('package-registry');
   const npmrc = join(tempRoot, '.npmrc');
+  const yarnrc = join(tempRoot, '.yarnrc');
 
-  // Configure npm to use the sandboxed npm globals and rc file
-  // From this point onward all npm transactions use the "global" npm cache
-  // isolated within this e2e test invocation.
+  // Change the npm+yarn userconfig to the sandboxed npmrc to override the default ~
   process.env.NPM_CONFIG_USERCONFIG = npmrc;
-  process.env.NPM_CONFIG_PREFIX = npmModulesPrefix;
+
+  // The npm+yarn registry URL
   process.env.NPM_CONFIG_REGISTRY = npmRegistry;
+
+  // Configure npm+yarn to use a sandboxed bin directory
+  // From this point onward all yarn/npm bin files/symlinks are put into the prefix directories
+  process.env.NPM_CONFIG_PREFIX = npmModulesPrefix;
+  process.env.YARN_CONFIG_PREFIX = yarnModulesPrefix;
 
   // Snapshot builds may contain versions that are not yet released (e.g., RC phase main branch).
   // In this case peer dependency ranges may not resolve causing npm 7+ to fail during tests.
@@ -25,11 +31,16 @@ export default async function () {
     process.env['NPM_CONFIG_legacy_peer_deps'] = 'true';
   }
 
-  // Configure the registry and prefix used within the test sandbox
+  // Configure the registry and prefix used within the test sandbox via rc files
   await writeFile(npmrc, `registry=${npmRegistry}\nprefix=${npmModulesPrefix}`);
+  await writeFile(yarnrc, `registry ${npmRegistry}\nprefix ${yarnModulesPrefix}`);
+
   await mkdir(npmModulesPrefix);
+  await mkdir(yarnModulesPrefix);
 
   setGlobalVariable('npm-global', npmModulesPrefix);
+  setGlobalVariable('yarn-global', yarnModulesPrefix);
 
-  console.log(`  Using "${npmModulesPrefix}" as e2e test global npm cache.`);
+  console.log(`  Using "${npmModulesPrefix}" as e2e test global npm bin dir.`);
+  console.log(`  Using "${yarnModulesPrefix}" as e2e test global yarn bin dir.`);
 }
