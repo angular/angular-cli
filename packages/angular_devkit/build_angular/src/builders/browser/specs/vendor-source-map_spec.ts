f3087dc4343f0dac26ffd7c633e7dcf3618db2b7
Victor Porof <victorporof@chromium.org>
2022-07-11 08:58:35 +0000

    feat(@angular-devkit/build-angular): Identify third-party sources in sourcemaps
    This PR includes a webpack plugin which adds a field to source maps that identifies which sources are vendored or runtime-injected (aka third-party) sources. These will be consumed by Chrome DevTools to automatically ignore-list sources.
    
    When vendor source map processing is enabled, this is interpreted as the developer intending to debug third-party code; in this case, the feature is disabled.
    
    Signed-off-by: Victor Porof <victorporof@chromium.org>

--- packages/angular_devkit/build_angular/src/builders/browser/specs/vendor-source-map_spec.ts
+++ packages/angular_devkit/build_angular/src/builders/browser/specs/vendor-source-map_spec.ts
@@ -10,6 +10,10 @@ import { Architect } from '@angular-devkit/architect';
 import * as path from 'path';
 import { browserBuild, createArchitect, host } from '../../../testing/test-utils';
 
+// Following the naming conventions from
+// https://sourcemaps.info/spec.html#h.ghqpj1ytqjbm
+const IGNORE_LIST = 'x_google_ignoreList';
+
 describe('Browser Builder external source map', () => {
   const target = { project: 'app', target: 'build' };
   let architect: Architect;
@@ -48,5 +52,72 @@ describe('Browser Builder external source map', () => {
     const sourcePaths: string[] = JSON.parse(await files['vendor.js.map']).sources;
     const hasTsSourcePaths = sourcePaths.some((p) => path.extname(p) == '.ts');
     expect(hasTsSourcePaths).toBe(false, `vendor.js.map not should have '.ts' extentions`);
+  });
+});
+
+describe('Identifying third-party code in source maps', () => {
+  interface SourceMap {
+    sources: string[];
+    [IGNORE_LIST]: number[];
+  }
+
+  const target = { project: 'app', target: 'build' };
+  let architect: Architect;
+
+  beforeEach(async () => {
+    await host.initialize().toPromise();
+    architect = (await createArchitect(host.root())).architect;
+  });
+  afterEach(async () => host.restore().toPromise());
+
+  it('specifies which sources are third party when vendor processing is disabled', async () => {
+    const overrides = {
+      sourceMap: {
+        scripts: true,
+        vendor: false,
+      },
+    };
+
+    const { files } = await browserBuild(architect, host, target, overrides);
+    const mainMap: SourceMap = JSON.parse(await files['main.js.map']);
+    const polyfillsMap: SourceMap = JSON.parse(await files['polyfills.js.map']);
+    const runtimeMap: SourceMap = JSON.parse(await files['runtime.js.map']);
+    const vendorMap: SourceMap = JSON.parse(await files['vendor.js.map']);
+
+    expect(mainMap[IGNORE_LIST]).not.toBeUndefined();
+    expect(polyfillsMap[IGNORE_LIST]).not.toBeUndefined();
+    expect(runtimeMap[IGNORE_LIST]).not.toBeUndefined();
+    expect(vendorMap[IGNORE_LIST]).not.toBeUndefined();
+
+    expect(mainMap[IGNORE_LIST].length).toEqual(0);
+    expect(polyfillsMap[IGNORE_LIST].length).not.toEqual(0);
+    expect(runtimeMap[IGNORE_LIST].length).not.toEqual(0);
+    expect(vendorMap[IGNORE_LIST].length).not.toEqual(0);
+
+    const thirdPartyInMain = mainMap.sources.some((s) => s.includes('node_modules'));
+    const thirdPartyInPolyfills = polyfillsMap.sources.some((s) => s.includes('node_modules'));
+    const thirdPartyInRuntime = runtimeMap.sources.some((s) => s.includes('webpack'));
+    const thirdPartyInVendor = vendorMap.sources.some((s) => s.includes('node_modules'));
+    expect(thirdPartyInMain).toBe(false, `main.js.map should not include any node modules`);
+    expect(thirdPartyInPolyfills).toBe(true, `polyfills.js.map should include some node modules`);
+    expect(thirdPartyInRuntime).toBe(true, `runtime.js.map should include some webpack code`);
+    expect(thirdPartyInVendor).toBe(true, `vendor.js.map should include some node modules`);
+
+    // All sources in the main map are first-party.
+    expect(mainMap.sources.filter((_, i) => !mainMap[IGNORE_LIST].includes(i))).toEqual([
+      './src/app/app.component.ts',
+      './src/app/app.module.ts',
+      './src/environments/environment.ts',
+      './src/main.ts',
+    ]);
+
+    // Only some sources in the polyfills map are first-party.
+    expect(polyfillsMap.sources.filter((_, i) => !polyfillsMap[IGNORE_LIST].includes(i))).toEqual([
+      './src/polyfills.ts',
+    ]);
+
+    // None of the sources in the runtime and vendor maps are first-party.
+    expect(runtimeMap.sources.filter((_, i) => !runtimeMap[IGNORE_LIST].includes(i))).toEqual([]);
+    expect(vendorMap.sources.filter((_, i) => !vendorMap[IGNORE_LIST].includes(i))).toEqual([]);
   });
 });
