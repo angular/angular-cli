42eb0595d6b74687e64180f7df994dfadae91fe4
Alan Agius <alanagius@google.com>
2022-07-12 13:51:14 +0000

    test: setup Bazel on Windows

---
+++ .circleci/env.windows.sh
@@ -0,0 +1,9 @@
+####################################################################################################
+# Set bazel configuration for CircleCI runs.
+####################################################################################################
+cp "${PROJECT_ROOT}/.circleci/bazel.windows.rc" "${USERPROFILE}/.bazelrc";
+
+# Expose the Bazelisk version. We need to run Bazelisk globally since Windows has problems launching
+# Bazel from a node modules directoy that might be modified by the Bazel Yarn install then.
+setPublicVar BAZELISK_VERSION \
+    "$(cd ${PROJECT_ROOT}; node -p 'require("./package.json").devDependencies["@bazel/bazelisk"]')"
