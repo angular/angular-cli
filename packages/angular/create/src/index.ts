f93de8071f58a176764bfeab5c63bb208c3a6765
Alan Agius <alanagius@google.com>
2022-07-22 17:18:55 +0000

    fix(@angular/create): use appropriate package manager to install dependencies
    Previously, NPM was always used to install dependencies even when using `yarn create` or `pnpm create`.
    
    Closes #23617

--- packages/angular/create/src/index.ts
+++ packages/angular/create/src/index.ts
@@ -11,9 +11,19 @@ import { spawnSync } from 'child_process';
 import { join } from 'path';
 
 const binPath = join(require.resolve('@angular/cli/package.json'), '../bin/ng.js');
+const args = process.argv.slice(2);
+
+const hasPackageManagerArg = args.some((a) => a.startsWith('--package-manager'));
+if (!hasPackageManagerArg) {
+  // Ex: yarn/1.22.18 npm/? node/v16.15.1 linux x64
+  const packageManager = process.env['npm_config_user_agent']?.split('/')[0];
+  if (packageManager && ['npm', 'pnpm', 'yarn', 'cnpm'].includes(packageManager)) {
+    args.push('--package-manager', packageManager);
+  }
+}
 
 // Invoke ng new with any parameters provided.
-const { error } = spawnSync(process.execPath, [binPath, 'new', ...process.argv.slice(2)], {
+const { error } = spawnSync(process.execPath, [binPath, 'new', ...args], {
   stdio: 'inherit',
 });
 
