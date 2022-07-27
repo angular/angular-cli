eed54b359d2b514156242529ee8a25b51c50dae0
Alan Agius <alanagius@google.com>
2022-07-27 11:38:26 +0000

    fix(@angular-devkit/build-angular): catch clause variable is not an Error instance
    Errors thrown in RxJs are not instanceof Error and therefore the check will always fail.
    
    Closes #23631
    
    (cherry picked from commit 8fd3e9fb3c15c848ac5d5e2ae1ee4b06daa2c957)

--- packages/angular_devkit/build_angular/src/utils/error.ts
+++ packages/angular_devkit/build_angular/src/utils/error.ts
@@ -9,5 +9,9 @@
 import assert from 'assert';
 
 export function assertIsError(value: unknown): asserts value is Error & { code?: string } {
-  assert(value instanceof Error, 'catch clause variable is not an Error instance');
+  const isError =
+    value instanceof Error ||
+    // The following is needing to identify errors coming from RxJs.
+    (typeof value === 'object' && value && 'name' in value && 'message' in value);
+  assert(isError, 'catch clause variable is not an Error instance');
 }
