202313844c0c60d8528fafcf54b393630bf4c5ad
Jason Bedard <jason+github@jbedard.ca>
2022-08-16 17:21:29 -0700

    test: run e2e tests on pre-compiled packages
    The NPM packages being tested must be pre-compiled and the tar packages specified via --package. This way the real packages such as snapshots, release artifacts or cached packages can be tested. Previously the e2e tests compiled and packaged during test execution.
    
    (cherry picked from commit 2624d8941f1ee1453c805d7229c262e7c0af7078)

--- tests/legacy-cli/e2e/setup/010-local-publish.ts
+++ tests/legacy-cli/e2e/setup/010-local-publish.ts
@@ -1,26 +1,30 @@
 import { getGlobalVariable } from '../utils/env';
+import { PkgInfo } from '../utils/packages';
 import { globalNpm, extractNpmEnv } from '../utils/process';
 import { isPrereleaseCli } from '../utils/project';
 
 export default async function () {
   const testRegistry: string = getGlobalVariable('package-registry');
-  await globalNpm(
-    [
-      'run',
-      'admin',
-      '--',
-      'publish',
-      '--no-versionCheck',
-      '--no-branchCheck',
-      `--registry=${testRegistry}`,
-      '--tag',
-      isPrereleaseCli() ? 'next' : 'latest',
-    ],
-    {
-      ...extractNpmEnv(),
-      // Also set an auth token value for the local test registry which is required by npm 7+
-      // even though it is never actually used.
-      'NPM_CONFIG__AUTH': 'e2e-testing',
-    },
+  const packageTars: PkgInfo[] = Object.values(getGlobalVariable('package-tars'));
+
+  // Publish packages specified with --package
+  await Promise.all(
+    packageTars.map(({ path: p }) =>
+      globalNpm(
+        [
+          'publish',
+          `--registry=${testRegistry}`,
+          '--tag',
+          isPrereleaseCli() ? 'next' : 'latest',
+          p,
+        ],
+        {
+          ...extractNpmEnv(),
+          // Also set an auth token value for the local test registry which is required by npm 7+
+          // even though it is never actually used.
+          'NPM_CONFIG__AUTH': 'e2e-testing',
+        },
+      ),
+    ),
   );
 }
