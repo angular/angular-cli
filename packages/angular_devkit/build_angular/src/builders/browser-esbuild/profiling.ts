dabbf1d174847f5018856a617930efa206c0497c
Charles Lyding <19598772+clydin@users.noreply.github.com>
2022-10-21 16:08:21 -0400

    refactor(@angular-devkit/build-angular): show more cumulative metrics for esbuild perf debug output
    When the `NG_BUILD_DEBUG_PERF` environment variable is used to debug performance of the experimental
    esbuild-based browser application builder, additional information will be logged for cumulative
    profiling actions. This includes the count, minimum, maximum, and average.

--- packages/angular_devkit/build_angular/src/builders/browser-esbuild/profiling.ts
+++ packages/angular_devkit/build_angular/src/builders/browser-esbuild/profiling.ts
@@ -8,7 +8,7 @@
 
 import { debugPerformance } from '../../utils/environment-options';
 
-let cumulativeDurations: Map<string, number> | undefined;
+let cumulativeDurations: Map<string, number[]> | undefined;
 
 export function resetCumulativeDurations(): void {
   cumulativeDurations?.clear();
@@ -19,20 +19,39 @@ export function logCumulativeDurations(): void {
     return;
   }
 
-  for (const [name, duration] of cumulativeDurations) {
+  for (const [name, durations] of cumulativeDurations) {
+    let total = 0;
+    let min;
+    let max;
+    for (const duration of durations) {
+      total += duration;
+      if (min === undefined || duration < min) {
+        min = duration;
+      }
+      if (max === undefined || duration > max) {
+        max = duration;
+      }
+    }
+    const average = total / durations.length;
     // eslint-disable-next-line no-console
-    console.log(`DURATION[${name}]: ${duration.toFixed(9)} seconds`);
+    console.log(
+      `DURATION[${name}]: ${total.toFixed(9)}s [count: ${durations.length}; avg: ${average.toFixed(
+        9,
+      )}s; min: ${min?.toFixed(9)}s; max: ${max?.toFixed(9)}s]`,
+    );
   }
 }
 
 function recordDuration(name: string, startTime: bigint, cumulative?: boolean): void {
   const duration = Number(process.hrtime.bigint() - startTime) / 10 ** 9;
   if (cumulative) {
-    cumulativeDurations ??= new Map<string, number>();
-    cumulativeDurations.set(name, (cumulativeDurations.get(name) ?? 0) + duration);
+    cumulativeDurations ??= new Map<string, number[]>();
+    const durations = cumulativeDurations.get(name) ?? [];
+    durations.push(duration);
+    cumulativeDurations.set(name, durations);
   } else {
     // eslint-disable-next-line no-console
-    console.log(`DURATION[${name}]: ${duration.toFixed(9)} seconds`);
+    console.log(`DURATION[${name}]: ${duration.toFixed(9)}s`);
   }
 }
 
