18f31009c198cd41b5196f3decbb9beaf1ec8211
Dominic Elm <elmdominic@gmx.net>
2022-12-02 10:08:38 +0100

    refactor(@angular-devkit/build-angular): convert  into a function to give more control when the readable stream is initialized
    (cherry picked from commit 65f78be26798ca6087f1f27b111f314dc5cfad73)

--- packages/angular_devkit/build_angular/src/utils/index-file/html-rewriting-stream.ts
+++ packages/angular_devkit/build_angular/src/utils/index-file/html-rewriting-stream.ts
@@ -10,36 +10,44 @@ import { Readable, Writable } from 'stream';
 
 export async function htmlRewritingStream(content: string): Promise<{
   rewriter: import('parse5-html-rewriting-stream');
-  transformedContent: Promise<string>;
+  transformedContent: () => Promise<string>;
 }> {
   const chunks: Buffer[] = [];
   const rewriter = new (await import('parse5-html-rewriting-stream')).default();
 
   return {
     rewriter,
-    transformedContent: new Promise((resolve) => {
-      new Readable({
-        encoding: 'utf8',
-        read(): void {
-          this.push(Buffer.from(content));
-          this.push(null);
-        },
-      })
-        .pipe(rewriter)
-        .pipe(
-          new Writable({
-            write(chunk: string | Buffer, encoding: string | undefined, callback: Function): void {
-              chunks.push(
-                typeof chunk === 'string' ? Buffer.from(chunk, encoding as BufferEncoding) : chunk,
-              );
-              callback();
-            },
-            final(callback: (error?: Error) => void): void {
-              callback();
-              resolve(Buffer.concat(chunks).toString());
-            },
-          }),
-        );
-    }),
+    transformedContent: () => {
+      return new Promise((resolve) => {
+        new Readable({
+          encoding: 'utf8',
+          read(): void {
+            this.push(Buffer.from(content));
+            this.push(null);
+          },
+        })
+          .pipe(rewriter)
+          .pipe(
+            new Writable({
+              write(
+                chunk: string | Buffer,
+                encoding: string | undefined,
+                callback: Function,
+              ): void {
+                chunks.push(
+                  typeof chunk === 'string'
+                    ? Buffer.from(chunk, encoding as BufferEncoding)
+                    : chunk,
+                );
+                callback();
+              },
+              final(callback: (error?: Error) => void): void {
+                callback();
+                resolve(Buffer.concat(chunks).toString());
+              },
+            }),
+          );
+      });
+    },
   };
 }
