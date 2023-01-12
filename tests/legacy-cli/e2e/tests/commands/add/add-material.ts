04e7c25aad3644d0502f0ae30308ccabf4db43ea
Alan Agius <alanagius@google.com>
2023-01-12 09:39:36 +0000

    test: use `NPM_CONFIG_legacy_peer_deps` when testing prerelease versions of Angular material.
    `@angular/material` pre-release may not support the current version of `@angular/core` pre-release. In this case peer dependency ranges may not resolve causing npm 7+ to fail during tests.

--- tests/legacy-cli/e2e/tests/commands/add/add-material.ts
+++ tests/legacy-cli/e2e/tests/commands/add/add-material.ts
@@ -1,17 +1,31 @@
 import { assertIsError } from '../../../utils/utils';
 import { expectFileToMatch, rimraf } from '../../../utils/fs';
 import { uninstallPackage } from '../../../utils/packages';
-import { ng } from '../../../utils/process';
+import { execWithEnv } from '../../../utils/process';
 import { isPrereleaseCli } from '../../../utils/project';
 
 export default async function () {
   // forcibly remove in case another test doesn't clean itself up
   await rimraf('node_modules/@angular/material');
 
-  const tag = (await isPrereleaseCli()) ? '@next' : '';
+  const isPrerelease = await isPrereleaseCli();
+  const tag = isPrerelease ? '@next' : '';
+  const processEnv = {
+    ...process.env,
+    // `@angular/material` pre-release may not support the current version of `@angular/core` pre-release.
+    // due to the order of releases FW -> CLI -> Material
+    // In this case peer dependency ranges may not resolve causing npm 7+ to fail during tests.
+    'NPM_CONFIG_legacy_peer_deps': isPrerelease
+      ? 'true'
+      : process.env['NPM_CONFIG_legacy_peer_deps'],
+  };
 
   try {
-    await ng('add', `@angular/material${tag}`, '--unknown', '--skip-confirmation');
+    await execWithEnv(
+      'ng',
+      ['add', `@angular/material${tag}`, '--skip-confirmation', '--unknown'],
+      processEnv,
+    );
   } catch (error) {
     assertIsError(error);
     if (!(error as Error).message.includes(`Unknown option: '--unknown'`)) {
@@ -19,14 +33,12 @@ export default async function () {
     }
   }
 
-  await ng(
-    'add',
-    `@angular/material${tag}`,
-    '--theme',
-    'custom',
-    '--verbose',
-    '--skip-confirmation',
+  await execWithEnv(
+    'ng',
+    ['add', `@angular/material${tag}`, '--theme', 'custom', '--verbose', '--skip-confirmation'],
+    processEnv,
   );
+
   await expectFileToMatch('package.json', /@angular\/material/);
 
   // Clean up existing cdk package
