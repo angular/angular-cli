89ab29901b68ea1a28bbf2ec08ecb5e1dc128fa8
Alan Agius <alanagius@google.com>
2023-01-20 15:54:12 +0000

    docs(@angular/cli): minor updates to `ng run` description
    Minor improvements of `ng run` docs

--- packages/angular/cli/src/commands/run/cli.ts
+++ packages/angular/cli/src/commands/run/cli.ts
@@ -38,7 +38,8 @@ export class RunCommandModule
 
     const localYargs: Argv<RunCommandArgs> = argv
       .positional('target', {
-        describe: 'The Architect target to run.',
+        describe:
+          'The Architect target to run provided in the the following format `project:target[:configuration]`.',
         type: 'string',
         demandOption: true,
         // Show only in when using --help and auto completion because otherwise comma seperated configuration values will be invalid.
