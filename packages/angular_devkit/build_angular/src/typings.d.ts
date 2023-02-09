6b90191d77ac453f653b910acf83adb03b9c13a0
Charles Lyding <19598772+clydin@users.noreply.github.com>
2023-02-09 12:21:36 -0500

    fix(@angular-devkit/build-angular): use babel default export helper in build optimizer
    Within the build optimizer's static member optimization pass, a class that is directly
    default exported must be split into two statements: the class declaration and the
    default export. This is because the pass can wrap classes in a pure annotated IIFE which
    results in a variable declaration replacement and variable declarations can not be directly
    default exported. Previously, the pass did this splitting manually but this was causing
    later babel plugins to fail. In addition to updating the AST in this case, scoping information
    also needed to be updated. To support this, a babel helper package is now used that handles
    the details of the statement split operation.

--- packages/angular_devkit/build_angular/src/typings.d.ts
+++ packages/angular_devkit/build_angular/src/typings.d.ts
@@ -11,3 +11,11 @@ declare module '@babel/helper-annotate-as-pure' {
     pathOrNode: import('@babel/types').Node | { node: import('@babel/types').Node },
   ): void;
 }
+
+declare module '@babel/helper-split-export-declaration' {
+  export default function splitExportDeclaration(
+    exportDeclaration: import('@babel/traverse').NodePath<
+      import('@babel/types').ExportDefaultDeclaration
+    >,
+  ): void;
+}
