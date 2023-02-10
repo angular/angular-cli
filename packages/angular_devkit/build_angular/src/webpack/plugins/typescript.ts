88a33155d4bc00077d32bef42588427fb2ed49f4
Alan Agius <alanagius@google.com>
2023-02-10 16:43:06 +0000

    fix(@angular-devkit/build-angular): update the ECMA output warning message to be more actionable
    Update the `TypeScript compiler options "target" and "useDefineForClassFields" are set to "ES2022"` warning message to be more actionable.
    
    Closes: #24697
    (cherry picked from commit c65b026e2f375f35826d985c8cc558a2729b520e)

--- packages/angular_devkit/build_angular/src/webpack/plugins/typescript.ts
+++ packages/angular_devkit/build_angular/src/webpack/plugins/typescript.ts
@@ -35,7 +35,8 @@ export function createIvyPlugin(
     wco.logger.warn(
       'TypeScript compiler options "target" and "useDefineForClassFields" are set to "ES2022" and ' +
         '"false" respectively by the Angular CLI. To control ECMA version and features use the Browerslist configuration. ' +
-        'For more information, see https://angular.io/guide/build#configuring-browser-compatibility',
+        'For more information, see https://angular.io/guide/build#configuring-browser-compatibility\n' +
+        `NOTE: You can set the "target" to "ES2022" in the project's tsconfig to remove this warning.`,
     );
   }
 
