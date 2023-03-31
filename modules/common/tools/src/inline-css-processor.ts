9fbdb23c528baffa4cb1cda737382b4cab590269
Alan Agius <alanagius@google.com>
2023-03-31 11:35:33 +0000

    feat(@nguniversal/common): add CSP support for inline styles
    Companion change to angular/angular#49444. That adds support to add nonce attributes on inline styles

--- modules/common/tools/src/inline-css-processor.ts
+++ modules/common/tools/src/inline-css-processor.ts
@@ -9,6 +9,37 @@
 import Critters from 'critters';
 import * as fs from 'fs';
 
+/**
+ * Pattern used to extract the media query set by Critters in an `onload` handler.
+ */
+const MEDIA_SET_HANDLER_PATTERN = /^this\.media=["'](.*)["'];?$/;
+
+/**
+ * Name of the attribute used to save the Critters media query so it can be re-assigned on load.
+ */
+const CSP_MEDIA_ATTR = 'ngCspMedia';
+
+/**
+ * Script text used to change the media value of the link tags.
+ */
+const LINK_LOAD_SCRIPT_CONTENT = [
+  `(() => {`,
+  // Save the `children` in a variable since they're a live DOM node collection.
+  // We iterate over the direct descendants, instead of going through a `querySelectorAll`,
+  // because we know that the tags will be directly inside the `head`.
+  `  const children = document.head.children;`,
+  // Declare `onLoad` outside the loop to avoid leaking memory.
+  // Can't be an arrow function, because we need `this` to refer to the DOM node.
+  `  function onLoad() {this.media = this.getAttribute('${CSP_MEDIA_ATTR}');}`,
+  // Has to use a plain for loop, because some browsers don't support
+  // `forEach` on `children` which is a `HTMLCollection`.
+  `  for (let i = 0; i < children.length; i++) {`,
+  `    const child = children[i];`,
+  `    child.hasAttribute('${CSP_MEDIA_ATTR}') && child.addEventListener('load', onLoad);`,
+  `  }`,
+  `})();`,
+].join('\n');
+
 export interface InlineCriticalCssProcessOptions {
   outputPath?: string;
 }
@@ -24,13 +55,45 @@ export interface InlineCriticalCssResult {
   errors?: string[];
 }
 
+/** Partial representation of an `HTMLElement`. */
+interface PartialHTMLElement {
+  getAttribute(name: string): string | null;
+  setAttribute(name: string, value: string): void;
+  hasAttribute(name: string): boolean;
+  removeAttribute(name: string): void;
+  appendChild(child: PartialHTMLElement): void;
+  textContent: string;
+  tagName: string | null;
+  children: PartialHTMLElement[];
+  next: PartialHTMLElement | null;
+  prev: PartialHTMLElement | null;
+}
+
+/** Partial representation of an HTML `Document`. */
+interface PartialDocument {
+  head: PartialHTMLElement;
+  createElement(tagName: string): PartialHTMLElement;
+  querySelector(selector: string): PartialHTMLElement | null;
+}
+
+/** Signature of the `Critters.embedLinkedStylesheet` method. */
+type EmbedLinkedStylesheetFn = (
+  link: PartialHTMLElement,
+  document: PartialDocument,
+) => Promise<unknown>;
+
 class CrittersExtended extends Critters {
   readonly warnings: string[] = [];
   readonly errors: string[] = [];
+  private initialEmbedLinkedStylesheet: EmbedLinkedStylesheetFn;
+  private addedCspScriptsDocuments = new WeakSet<PartialDocument>();
+  private documentNonces = new WeakMap<PartialDocument, string | null>();
+
+  // Inherited from `Critters`, but not exposed in the typings.
+  protected embedLinkedStylesheet!: EmbedLinkedStylesheetFn;
 
   constructor(
-    private readonly optionsExtended: InlineCriticalCssProcessorOptions &
-      InlineCriticalCssProcessOptions,
+    readonly optionsExtended: InlineCriticalCssProcessorOptions & InlineCriticalCssProcessOptions,
     private readonly resourceCache: Map<string, string>,
   ) {
     super({
@@ -46,10 +109,18 @@ class CrittersExtended extends Critters {
       pruneSource: false,
       reduceInlineStyles: false,
       mergeStylesheets: false,
+      // Note: if `preload` changes to anything other than `media`, the logic in
+      // `embedLinkedStylesheetOverride` will have to be updated.
       preload: 'media',
       noscriptFallback: true,
       inlineFonts: true,
     });
+
+    // We can't use inheritance to override `embedLinkedStylesheet`, because it's not declared in
+    // the `Critters` .d.ts which means that we can't call the `super` implementation. TS doesn't
+    // allow for `super` to be cast to a different type.
+    this.initialEmbedLinkedStylesheet = this.embedLinkedStylesheet;
+    this.embedLinkedStylesheet = this.embedLinkedStylesheetOverride;
   }
 
   public override async readFile(path: string): Promise<string> {
@@ -60,6 +131,78 @@ class CrittersExtended extends Critters {
     }
 
     return resourceContent;
+  }
+
+  /**
+   * Override of the Critters `embedLinkedStylesheet` method
+   * that makes it work with Angular's CSP APIs.
+   */
+  private embedLinkedStylesheetOverride: EmbedLinkedStylesheetFn = async (link, document) => {
+    const returnValue = await this.initialEmbedLinkedStylesheet(link, document);
+    const cspNonce = this.findCspNonce(document);
+
+    if (cspNonce) {
+      const crittersMedia = link.getAttribute('onload')?.match(MEDIA_SET_HANDLER_PATTERN);
+
+      if (crittersMedia) {
+        // If there's a Critters-generated `onload` handler and the file has an Angular CSP nonce,
+        // we have to remove the handler, because it's incompatible with CSP. We save the value
+        // in a different attribute and we generate a script tag with the nonce that uses
+        // `addEventListener` to apply the media query instead.
+        link.removeAttribute('onload');
+        link.setAttribute(CSP_MEDIA_ATTR, crittersMedia[1]);
+        this.conditionallyInsertCspLoadingScript(document, cspNonce);
+      }
+
+      link.prev?.setAttribute('nonce', cspNonce);
+    }
+
+    return returnValue;
+  };
+
+  /**
+   * Finds the CSP nonce for a specific document.
+   */
+  private findCspNonce(document: PartialDocument): string | null {
+    if (this.documentNonces.has(document)) {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      return this.documentNonces.get(document)!;
+    }
+
+    // HTML attribute are case-insensitive, but the parser used by Critters is case-sensitive.
+    const nonceElement = document.querySelector('[ngCspNonce], [ngcspnonce]');
+    const cspNonce =
+      nonceElement?.getAttribute('ngCspNonce') || nonceElement?.getAttribute('ngcspnonce') || null;
+
+    this.documentNonces.set(document, cspNonce);
+
+    return cspNonce;
+  }
+
+  /**
+   * Inserts the `script` tag that swaps the critical CSS at runtime,
+   * if one hasn't been inserted into the document already.
+   */
+  private conditionallyInsertCspLoadingScript(document: PartialDocument, nonce: string): void {
+    if (this.addedCspScriptsDocuments.has(document)) {
+      return;
+    }
+
+    if (document.head.textContent.includes(LINK_LOAD_SCRIPT_CONTENT)) {
+      // Script was already added during the build.
+      this.addedCspScriptsDocuments.add(document);
+
+      return;
+    }
+
+    const script = document.createElement('script');
+    script.setAttribute('nonce', nonce);
+    script.textContent = LINK_LOAD_SCRIPT_CONTENT;
+
+    // Append the script to the head since it needs to
+    // run as early as possible, after the `link` tags.
+    document.head.appendChild(script);
+    this.addedCspScriptsDocuments.add(document);
   }
 }
 
