37c645b7e39406a208f1540a4f92e60ca4997ed0
Charles Lyding <19598772+clydin@users.noreply.github.com>
2023-04-25 18:51:29 -0400

    refactor(@angular-devkit/build-angular): separate esbuild compiler plugin diagnostic utilities
    The TypeScript diagnostic to esbuild diagnostic conversion utilities are now contained in a
    separate file. This lowers the line count of the main compiler plugin file and also removes
    an additional direct usage of the `typescript` dependency from the compiler plugin file.

---
+++ packages/angular_devkit/build_angular/src/builders/browser-esbuild/angular/diagnostics.ts
@@ -0,0 +1,99 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import type { PartialMessage, PartialNote } from 'esbuild';
+import { platform } from 'node:os';
+import {
+  Diagnostic,
+  DiagnosticRelatedInformation,
+  flattenDiagnosticMessageText,
+  getLineAndCharacterOfPosition,
+  getPositionOfLineAndCharacter,
+} from 'typescript';
+
+/**
+ * Converts TypeScript Diagnostic related information into an esbuild compatible note object.
+ * Related information is a subset of a full TypeScript Diagnostic and also used for diagnostic
+ * notes associated with the main Diagnostic.
+ * @param info The TypeScript diagnostic relative information to convert.
+ * @returns An esbuild diagnostic message as a PartialMessage object
+ */
+function convertTypeScriptDiagnosticInfo(
+  info: DiagnosticRelatedInformation,
+  textPrefix?: string,
+): PartialNote {
+  const newLine = platform() === 'win32' ? '\r\n' : '\n';
+  let text = flattenDiagnosticMessageText(info.messageText, newLine);
+  if (textPrefix) {
+    text = textPrefix + text;
+  }
+
+  const note: PartialNote = { text };
+
+  if (info.file) {
+    note.location = {
+      file: info.file.fileName,
+      length: info.length,
+    };
+
+    // Calculate the line/column location and extract the full line text that has the diagnostic
+    if (info.start) {
+      const { line, character } = getLineAndCharacterOfPosition(info.file, info.start);
+      note.location.line = line + 1;
+      note.location.column = character;
+
+      // The start position for the slice is the first character of the error line
+      const lineStartPosition = getPositionOfLineAndCharacter(info.file, line, 0);
+
+      // The end position for the slice is the first character of the next line or the length of
+      // the entire file if the line is the last line of the file (getPositionOfLineAndCharacter
+      // will error if a nonexistent line is passed).
+      const { line: lastLineOfFile } = getLineAndCharacterOfPosition(
+        info.file,
+        info.file.text.length - 1,
+      );
+      const lineEndPosition =
+        line < lastLineOfFile
+          ? getPositionOfLineAndCharacter(info.file, line + 1, 0)
+          : info.file.text.length;
+
+      note.location.lineText = info.file.text.slice(lineStartPosition, lineEndPosition).trimEnd();
+    }
+  }
+
+  return note;
+}
+
+/**
+ * Converts a TypeScript Diagnostic message into an esbuild compatible message object.
+ * @param diagnostic The TypeScript diagnostic to convert.
+ * @returns An esbuild diagnostic message as a PartialMessage object
+ */
+export function convertTypeScriptDiagnostic(diagnostic: Diagnostic): PartialMessage {
+  let codePrefix = 'TS';
+  let code = `${diagnostic.code}`;
+  if (diagnostic.source === 'ngtsc') {
+    codePrefix = 'NG';
+    // Remove `-99` Angular prefix from diagnostic code
+    code = code.slice(3);
+  }
+
+  const message: PartialMessage = {
+    ...convertTypeScriptDiagnosticInfo(diagnostic, `${codePrefix}${code}: `),
+    // Store original diagnostic for reference if needed downstream
+    detail: diagnostic,
+  };
+
+  if (diagnostic.relatedInformation?.length) {
+    message.notes = diagnostic.relatedInformation.map((info) =>
+      convertTypeScriptDiagnosticInfo(info),
+    );
+  }
+
+  return message;
+}
