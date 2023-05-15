2d141fe3bc1efb9e254b15ce91ebc885a43c928a
Charles Lyding <19598772+clydin@users.noreply.github.com>
2023-05-15 12:35:16 -0400

    feat(@angular-devkit/build-angular): show estimated transfer size with esbuild builder
    When using the esbuild-based browser application builder, the console build stats output
    will now show the estimated transfer size of JavaScript and CSS files when optimizations
    are enabled. This provides similar behavior to the default Webpack-based builder.

--- tests/legacy-cli/e2e/tests/basic/build.ts
+++ tests/legacy-cli/e2e/tests/basic/build.ts
@@ -4,21 +4,27 @@ import { ng } from '../../utils/process';
 
 export default async function () {
   // Development build
-  const { stdout } = await ng('build', '--configuration=development');
+  const { stdout: stdout1 } = await ng('build', '--configuration=development');
   await expectFileToMatch('dist/test-project/index.html', 'main.js');
 
-  if (stdout.includes('Estimated Transfer Size')) {
+  if (stdout1.includes('Estimated Transfer Size')) {
     throw new Error(
-      `Expected stdout not to contain 'Estimated Transfer Size' but it did.\n${stdout}`,
+      `Expected stdout not to contain 'Estimated Transfer Size' but it did.\n${stdout1}`,
     );
   }
 
   // Production build
-  await ng('build');
+  const { stdout: stdout2 } = await ng('build');
   if (getGlobalVariable('argv')['esbuild']) {
     // esbuild uses an 8 character hash
     await expectFileToMatch('dist/test-project/index.html', /main\.[a-zA-Z0-9]{8}\.js/);
   } else {
     await expectFileToMatch('dist/test-project/index.html', /main\.[a-zA-Z0-9]{16}\.js/);
+  }
+
+  if (!stdout2.includes('Estimated Transfer Size')) {
+    throw new Error(
+      `Expected stdout to contain 'Estimated Transfer Size' but it did not.\n${stdout2}`,
+    );
   }
 }
