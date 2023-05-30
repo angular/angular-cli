eb79d6acd55635c43ba1c11c8f7c6457832d00c8
Charles Lyding <19598772+clydin@users.noreply.github.com>
2023-05-30 12:35:04 -0400

    fix(@angular-devkit/build-angular): correctly emit AOT class metadata in development
    When using the default Webpack-based builder, the AOT-generated class metadata
    was incorrectly removed in all cases. The metadata will now be present in development
    mode which in this case is signified by script optimizations being disabled.

--- packages/angular_devkit/build_angular/src/webpack/plugins/typescript.ts
+++ packages/angular_devkit/build_angular/src/webpack/plugins/typescript.ts
@@ -56,6 +56,7 @@ export function createIvyPlugin(
     compilerOptions,
     fileReplacements,
     jitMode: !aot,
+    emitClassMetadata: !optimize,
     emitNgModuleScope: !optimize,
     inlineStyleFileExtension: buildOptions.inlineStyleLanguage ?? 'css',
   });
