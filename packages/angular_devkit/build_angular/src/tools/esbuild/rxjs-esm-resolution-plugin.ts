24aae8135760035b01e13cf3d85079ff1ec03b82
Alan Agius <alanagius@google.com>
2023-06-28 07:21:25 +0000

    refactor: force ESM resolution of rxjs when in server bundles
    Rxjs conditional export for node points to the CJS version. This commits adds a resolver to replace the resolved path to the ESM version.

---
+++ packages/angular_devkit/build_angular/src/tools/esbuild/rxjs-esm-resolution-plugin.ts
@@ -0,0 +1,48 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import type { Plugin } from 'esbuild';
+
+const RXJS_ESM_RESOLUTION = Symbol('RXJS_ESM_RESOLUTION');
+
+/**
+ * Creates a plugin that forces ESM resolution of rxjs.
+ * This is needed as when targeting node, the CJS version is used to the current package conditional exports.
+ * @see: https://github.com/ReactiveX/rxjs/blob/2947583bb33e97f3db9e6d9f6cea70c62a173060/package.json#L19.
+ *
+ * NOTE: This can be removed when and if rxjs adds an import condition that allows ESM usage on Node.js.
+ *
+ * @returns An esbuild plugin.
+ */
+export function createRxjsEsmResolutionPlugin(): Plugin {
+  return {
+    name: 'angular-rxjs-resolution',
+    setup(build) {
+      build.onResolve({ filter: /^rxjs/ }, async (args) => {
+        if (args.pluginData?.[RXJS_ESM_RESOLUTION]) {
+          return null;
+        }
+
+        const { importer, kind, resolveDir, namespace, pluginData = {} } = args;
+        pluginData[RXJS_ESM_RESOLUTION] = true;
+
+        const result = await build.resolve(args.path, {
+          importer,
+          kind,
+          namespace,
+          pluginData,
+          resolveDir,
+        });
+
+        result.path = result.path.replace('/dist/cjs/', '/dist/esm/');
+
+        return result;
+      });
+    },
+  };
+}
