801b507b22f1fe4af6fe7556a2d6ca2467c64e93
zdev90 <zdev209@hotmail.com>
2023-07-09 07:04:01 -0700

    fixed some files

--- scripts/lint.ts
+++ scripts/lint.ts
@@ -1,11 +1,4 @@
-/**
- * @license
- * Copyright Google Inc. All Rights Reserved.
- *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.io/license
- */
-// tslint:disable:no-implicit-dependencies
+
 import { logging } from '@angular-devkit/core';
 import { ParsedArgs } from 'minimist';
 import * as path from 'path';
