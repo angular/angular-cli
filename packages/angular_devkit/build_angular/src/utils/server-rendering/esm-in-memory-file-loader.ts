96f54072cfa22e06fb1a34038aca8ec1c9520aa0
Alan Agius <alanagius@google.com>
2023-08-09 12:22:09 +0000

    fix(@angular-devkit/build-angular): update vite to be able to serve app-shell and SSG pages
    This commits, update the application builder and vite dev-server to be able to serve the app-shell and prerendered pages.
    
    (cherry picked from commit f3229c44ad58a872734fa965e20ecd9e76722fa1)

--- packages/angular_devkit/build_angular/src/utils/server-rendering/esm-in-memory-file-loader.ts
+++ packages/angular_devkit/build_angular/src/utils/server-rendering/esm-in-memory-file-loader.ts
@@ -6,19 +6,38 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
+import { join } from 'node:path';
 import { workerData } from 'node:worker_threads';
 import { fileURLToPath } from 'url';
+import { JavaScriptTransformer } from '../../tools/esbuild/javascript-transformer';
 
 /**
  * Node.js ESM loader to redirect imports to in memory files.
  * @see: https://nodejs.org/api/esm.html#loaders for more information about loaders.
  */
 
-const { outputFiles } = workerData as {
+const { outputFiles, workspaceRoot } = workerData as {
   outputFiles: Record<string, string>;
+  workspaceRoot: string;
 };
 
-export function resolve(specifier: string, context: {}, nextResolve: Function) {
+const TRANSFORMED_FILES: Record<string, string> = {};
+const CHUNKS_REGEXP = /file:\/\/\/(main\.server|chunk-\w+)\.mjs/;
+const WORKSPACE_ROOT_FILE = new URL(join(workspaceRoot, 'index.mjs'), 'file:').href;
+
+const JAVASCRIPT_TRANSFORMER = new JavaScriptTransformer(
+  // Always enable JIT linking to support applications built with and without AOT.
+  // In a development environment the additional scope information does not
+  // have a negative effect unlike production where final output size is relevant.
+  { sourcemap: true, jit: true },
+  1,
+);
+
+export function resolve(
+  specifier: string,
+  context: { parentURL: undefined | string },
+  nextResolve: Function,
+) {
   if (!isFileProtocol(specifier)) {
     const normalizedSpecifier = specifier.replace(/^\.\//, '');
     if (normalizedSpecifier in outputFiles) {
@@ -32,12 +51,24 @@ export function resolve(specifier: string, context: {}, nextResolve: Function) {
 
   // Defer to the next hook in the chain, which would be the
   // Node.js default resolve if this is the last user-specified loader.
-  return nextResolve(specifier);
+  return nextResolve(
+    specifier,
+    isBundleEntryPointOrChunk(context) ? { ...context, parentURL: WORKSPACE_ROOT_FILE } : context,
+  );
 }
 
-export function load(url: string, context: { format?: string | null }, nextLoad: Function) {
+export async function load(url: string, context: { format?: string | null }, nextLoad: Function) {
   if (isFileProtocol(url)) {
-    const source = outputFiles[fileURLToPath(url).slice(1)]; // Remove leading slash
+    const filePath = fileURLToPath(url);
+    let source =
+      outputFiles[filePath.slice(1)] /* Remove leading slash */ ?? TRANSFORMED_FILES[filePath];
+
+    if (source === undefined) {
+      source = TRANSFORMED_FILES[filePath] = Buffer.from(
+        await JAVASCRIPT_TRANSFORMER.transformFile(filePath),
+      ).toString('utf-8');
+    }
+
     if (source !== undefined) {
       const { format } = context;
 
@@ -56,3 +87,15 @@ export function load(url: string, context: { format?: string | null }, nextLoad:
 function isFileProtocol(url: string): boolean {
   return url.startsWith('file://');
 }
+
+function handleProcessExit(): void {
+  void JAVASCRIPT_TRANSFORMER.close();
+}
+
+function isBundleEntryPointOrChunk(context: { parentURL: undefined | string }): boolean {
+  return !!context.parentURL && CHUNKS_REGEXP.test(context.parentURL);
+}
+
+process.once('exit', handleProcessExit);
+process.once('SIGINT', handleProcessExit);
+process.once('uncaughtException', handleProcessExit);
