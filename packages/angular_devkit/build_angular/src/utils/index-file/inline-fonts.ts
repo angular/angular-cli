ad821d29b756661904c98d94c370253f0353699a
Alan Agius <alanagius@google.com>
2023-08-30 08:52:21 +0000

    refactor(@angular-devkit/build-angular): remove disabling certificate validation when inlining fonts
    Disabling certificate validation is strongly discouraged and is not required for inling of fonts.
    
    Addresses https://github.com/angular/angular-cli/security/code-scanning/29
    Closes #25731
    
    (cherry picked from commit cdcc8586a08fcc2860fe33aaadd860186ed75d05)

--- packages/angular_devkit/build_angular/src/utils/index-file/inline-fonts.ts
+++ packages/angular_devkit/build_angular/src/utils/index-file/inline-fonts.ts
@@ -210,7 +210,6 @@ export class InlineFontsProcessor {
           url,
           {
             agent,
-            rejectUnauthorized: false,
             headers: {
               'user-agent':
                 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
