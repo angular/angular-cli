5a21bb03da9d69110c262d78a77d49526aed4c47
Alan Agius <alanagius@google.com>
2023-09-13 07:50:40 +0000

    refactor(@angular-devkit/build-angular): remove deep import of `zone.js/node`
    This is no longer needed as `zone.js` now has proper package exports.

--- packages/angular_devkit/build_angular/src/tools/esbuild/application-code-bundle.ts
+++ packages/angular_devkit/build_angular/src/tools/esbuild/application-code-bundle.ts
@@ -162,7 +162,7 @@ export function createServerCodeBundleOptions(
   const polyfills = [`import '@angular/platform-server/init';`];
 
   if (options.polyfills?.includes('zone.js')) {
-    polyfills.push(`import 'zone.js/fesm2015/zone-node.js';`);
+    polyfills.push(`import 'zone.js/node';`);
   }
 
   if (jit) {
