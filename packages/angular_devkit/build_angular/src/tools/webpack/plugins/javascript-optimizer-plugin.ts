5c2d0de9ba87db384e8e3f8f5a9cbbedf8321b44
Charles Lyding <19598772+clydin@users.noreply.github.com>
2023-10-19 16:37:20 -0400

    refactor(@angular-devkit/build-angular): workaround for piscina worker destroy recreation
    When destroying a `piscina` package worker pool, the minimum workers must be set to 0 first
    to prevent the recreation of the minimum number of workers. These workers would then be
    cleaned up once the worker pool goes out of scope but the recreation and initialization of
    each worker is unneeded processing that should be avoided.

--- packages/angular_devkit/build_angular/src/tools/webpack/plugins/javascript-optimizer-plugin.ts
+++ packages/angular_devkit/build_angular/src/tools/webpack/plugins/javascript-optimizer-plugin.ts
@@ -231,6 +231,8 @@ export class JavaScriptOptimizerPlugin {
 
             await Promise.all(tasks);
           } finally {
+            // Workaround piscina bug where a worker thread will be recreated after destroy to meet the minimum.
+            workerPool.options.minThreads = 0;
             void workerPool.destroy();
           }
 
