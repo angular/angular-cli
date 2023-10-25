5b3541040bd27f8bff132de623f45db19a3d8ccf
Alan Agius <alanagius@google.com>
2023-10-25 10:11:06 +0000

    fix(@angular-devkit/build-angular): handle HTTP requests to assets during prerendering
    This commit fixes an issue were during prerendering (SSG) http requests to assets causes prerendering to fail.
    
    Closes #25720

---
+++ tests/legacy-cli/e2e/tests/build/prerender/http-requests-assets.ts
@@ -0,0 +1,83 @@
+import { ng } from '../../../utils/process';
+import { getGlobalVariable } from '../../../utils/env';
+import { expectFileToMatch, rimraf, writeMultipleFiles } from '../../../utils/fs';
+import { installWorkspacePackages } from '../../../utils/packages';
+import { useSha } from '../../../utils/project';
+
+export default async function () {
+  // TODO(alanagius): allow this test to run for non snapshots once FW v17.0.0-rc.1 is released on NPM.
+  const isSnapshotBuild = getGlobalVariable('argv')['ng-snapshots'];
+  if (!isSnapshotBuild) {
+    return;
+  }
+
+  const useWebpackBuilder = !getGlobalVariable('argv')['esbuild'];
+  if (useWebpackBuilder) {
+    // Not supported by the webpack based builder.
+    return;
+  }
+
+  // Forcibly remove in case another test doesn't clean itself up.
+  await rimraf('node_modules/@angular/ssr');
+  await ng('add', '@angular/ssr', '--skip-confirmation');
+  await useSha();
+  await installWorkspacePackages();
+
+  await writeMultipleFiles({
+    // Add http client and route
+    'src/app/app.config.ts': `
+      import { ApplicationConfig } from '@angular/core';
+      import { provideRouter } from '@angular/router';
+
+      import {HomeComponent} from './home/home.component';
+      import { provideClientHydration } from '@angular/platform-browser';
+      import { provideHttpClient, withFetch } from '@angular/common/http';
+
+      export const appConfig: ApplicationConfig = {
+        providers: [
+          provideRouter([{
+            path: '',
+            component: HomeComponent,
+          }]),
+          provideClientHydration(),
+          provideHttpClient(withFetch()),
+        ],
+      };
+    `,
+    // Add asset
+    'src/assets/media.json': JSON.stringify({ dataFromAssets: true }),
+    // Update component to do an HTTP call to asset.
+    'src/app/app.component.ts': `
+    import { Component, inject } from '@angular/core';
+    import { CommonModule } from '@angular/common';
+    import { RouterOutlet } from '@angular/router';
+    import { HttpClient } from '@angular/common/http';
+
+    @Component({
+      selector: 'app-root',
+      standalone: true,
+      imports: [CommonModule, RouterOutlet],
+      template: \`
+        <p>{{ data | json }}</p>
+        <router-outlet></router-outlet>
+      \`,
+    })
+    export class AppComponent {
+      data: any;
+      constructor() {
+        const http = inject(HttpClient);
+        http.get('/assets/media.json').subscribe((d) => {
+          this.data = d;
+        });
+      }
+    }
+    `,
+  });
+
+  await ng('generate', 'component', 'home');
+  await ng('build', '--configuration=production', '--prerender');
+  await expectFileToMatch(
+    'dist/test-project/browser/index.html',
+    /<p>{[\S\s]*"dataFromAssets":[\s\S]*true[\S\s]*}<\/p>/,
+  );
+}
