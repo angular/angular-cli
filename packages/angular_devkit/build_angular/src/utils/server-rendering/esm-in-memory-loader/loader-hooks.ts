e4b8b6152c2e4e772848fd2b2cfa4dbf9aa23200
Alan Agius <alanagius@google.com>
2023-10-31 14:59:42 +0100

    fixup! fix(@angular-devkit/build-angular): Windows Node.js 20 prerendering failure
    Co-authored-by: Charles <19598772+clydin@users.noreply.github.com>

--- packages/angular_devkit/build_angular/src/utils/server-rendering/esm-in-memory-loader/loader-hooks.ts
+++ packages/angular_devkit/build_angular/src/utils/server-rendering/esm-in-memory-loader/loader-hooks.ts
@@ -71,7 +71,7 @@ export function resolve(
 export async function load(url: string, context: { format?: string | null }, nextLoad: Function) {
   const { format } = context;
 
-  // CommonJs modules require not transformations and are not in memory.
+  // CommonJs modules require no transformations and are not in memory.
   if (format !== 'commonjs' && isFileProtocol(url)) {
     const filePath = fileURLToPath(url);
     // Remove '/' or drive letter for Windows that was added in the above 'resolve'.
