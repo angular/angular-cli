cb090d69e414e0628d174754a3c668f5de129366
Alan Agius <alanagius@google.com>
2023-11-03 12:03:48 +0000

    test: temporary disable Jest E2E tests
    More context in https://github.com/angular/angular/pull/52505
    
    (cherry picked from commit 5f14bf3441de0ca4f263129eac4a415967eaddd7)

--- tests/legacy-cli/e2e/tests/jest/basic.ts
+++ tests/legacy-cli/e2e/tests/jest/basic.ts
@@ -2,6 +2,9 @@ import { applyJestBuilder } from '../../utils/jest';
 import { ng } from '../../utils/process';
 
 export default async function (): Promise<void> {
+  // TODO(alanagius): enable once https://github.com/angular/angular/pull/52505 is released.
+  return;
+
   await applyJestBuilder();
 
   const { stderr } = await ng('test');
