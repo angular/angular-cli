6a55bafc621eee7841639707f722620f3bee9800
Alan Agius <alanagius@google.com>
2023-11-08 12:28:41 +0000

    build: update Angular packages to version 17 stable
    Update all versions to stable

--- packages/schematics/angular/utility/latest-versions.ts
+++ packages/schematics/angular/utility/latest-versions.ts
@@ -16,7 +16,7 @@ export const latestVersions: Record<string, string> & {
   ...require('./latest-versions/package.json')['dependencies'],
 
   // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
-  Angular: '^17.0.0-next.0',
+  Angular: '^17.0.0',
 
   DevkitBuildAngular: '^0.0.0-PLACEHOLDER',
   AngularSSR: '^0.0.0-PLACEHOLDER',
