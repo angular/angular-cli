72bd0ab9d069ad726c389d860ac5f11f34370214
Alan Agius <alanagius@google.com>
2023-12-12 11:59:53 +0000

    fix(@angular-devkit/build-angular): allow vite to serve JavaScript and TypeScript assets
    This commit fixes an issue which caused vite to transform JavaScript and TypeScript assets.
    
    Closes #26641

--- packages/angular_devkit/build_angular/src/builders/dev-server/tests/behavior/build-assets_spec.ts
+++ packages/angular_devkit/build_angular/src/builders/dev-server/tests/behavior/build-assets_spec.ts
@@ -12,9 +12,11 @@ import { describeServeBuilder } from '../jasmine-helpers';
 import { BASE_OPTIONS, DEV_SERVER_BUILDER_INFO } from '../setup';
 
 describeServeBuilder(executeDevServer, DEV_SERVER_BUILDER_INFO, (harness, setupTarget) => {
+  const javascriptFileContent =
+    "import {foo} from 'unresolved'; /* a comment */const foo = `bar`;\n\n\n";
+
   describe('Behavior: "browser builder assets"', () => {
     it('serves a project JavaScript asset unmodified', async () => {
-      const javascriptFileContent = '/* a comment */const foo = `bar`;\n\n\n';
       await harness.writeFile('src/extra.js', javascriptFileContent);
 
       setupTarget(harness, {
@@ -29,6 +31,26 @@ describeServeBuilder(executeDevServer, DEV_SERVER_BUILDER_INFO, (harness, setupT
       });
 
       const { result, response } = await executeOnceAndFetch(harness, 'extra.js');
+
+      expect(result?.success).toBeTrue();
+      expect(await response?.text()).toContain(javascriptFileContent);
+    });
+
+    it('serves a project TypeScript asset unmodified', async () => {
+      await harness.writeFile('src/extra.ts', javascriptFileContent);
+
+      setupTarget(harness, {
+        assets: ['src/extra.ts'],
+        optimization: {
+          scripts: true,
+        },
+      });
+
+      harness.useTarget('serve', {
+        ...BASE_OPTIONS,
+      });
+
+      const { result, response } = await executeOnceAndFetch(harness, 'extra.ts');
 
       expect(result?.success).toBeTrue();
       expect(await response?.text()).toContain(javascriptFileContent);
