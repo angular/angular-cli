8282599d1d751a95ee30db62cddfc793fb37b625
syntax-by-ai <156721427+syntax-by-ai@users.noreply.github.com>
2024-01-15 22:24:12 +0000

    Refactor execute-build

--- packages/angular_devkit/build_angular/src/builders/application/execute-build.ts
+++ packages/angular_devkit/build_angular/src/builders/application/execute-build.ts
@@ -8,38 +8,55 @@
 
 import { BuilderContext } from '@angular-devkit/architect';
 import { SourceFileCache } from '../../tools/esbuild/angular/source-file-cache';
+import {
+  createBrowserCodeBundleOptions,
+  createBrowserPolyfillBundleOptions,
+  createServerCodeBundleOptions,
+} from '../../tools/esbuild/application-code-bundle';
 import { generateBudgetStats } from '../../tools/esbuild/budget-stats';
 import { BuildOutputFileType, BundlerContext } from '../../tools/esbuild/bundler-context';
 import { ExecutionResult, RebuildState } from '../../tools/esbuild/bundler-execution-result';
 import { checkCommonJSModules } from '../../tools/esbuild/commonjs-checker';
+import { createGlobalScriptsBundleOptions } from '../../tools/esbuild/global-scripts';
+import { createGlobalStylesBundleOptions } from '../../tools/esbuild/global-styles';
 import { extractLicenses } from '../../tools/esbuild/license-extractor';
-import { calculateEstimatedTransferSizes, logBuildStats } from '../../tools/esbuild/utils';
-import { BudgetCalculatorResult, checkBudgets } from '../../utils/bundle-calculator';
-import { colors } from '../../utils/color';
+import {
+  calculateEstimatedTransferSizes,
+  getSupportedNodeTargets,
+  logBuildStats,
+  logMessages,
+  transformSupportedBrowsersToTargets,
+} from '../../tools/esbuild/utils';
+import { checkBudgets } from '../../utils/bundle-calculator';
 import { copyAssets } from '../../utils/copy-assets';
 import { getSupportedBrowsers } from '../../utils/supported-browsers';
 import { executePostBundleSteps } from './execute-post-bundle';
 import { inlineI18n, loadActiveTranslations } from './i18n';
 import { NormalizedApplicationBuildOptions } from './options';
-import { setupBundlerContexts } from './setup-bundling';
 
+// eslint-disable-next-line max-lines-per-function
 export async function executeBuild(
   options: NormalizedApplicationBuildOptions,
   context: BuilderContext,
   rebuildState?: RebuildState,
 ): Promise<ExecutionResult> {
+  const startTime = process.hrtime.bigint();
+
   const {
     projectRoot,
     workspaceRoot,
     i18nOptions,
     optimizationOptions,
+    serverEntryPoint,
     assets,
     cacheOptions,
     prerenderOptions,
+    appShellOptions,
+    ssrOptions,
   } = options;
 
-  // TODO: Consider integrating into watch mode. Would require full rebuild on target changes.
   const browsers = getSupportedBrowsers(projectRoot, context.logger);
+  const target = transformSupportedBrowsersToTargets(browsers);
 
   // Load active translations if inlining
   // TODO: Integrate into watch mode and only load changed translations
@@ -53,75 +70,93 @@ export async function executeBuild(
     rebuildState?.codeBundleCache ??
     new SourceFileCache(cacheOptions.enabled ? cacheOptions.path : undefined);
   if (bundlerContexts === undefined) {
-    bundlerContexts = setupBundlerContexts(options, browsers, codeBundleCache);
+    bundlerContexts = [];
+
+    // Browser application code
+    bundlerContexts.push(
+      new BundlerContext(
+        workspaceRoot,
+        !!options.watch,
+        createBrowserCodeBundleOptions(options, target, codeBundleCache),
+      ),
+    );
+
+    // Browser polyfills code
+    const polyfillBundleOptions = createBrowserPolyfillBundleOptions(
+      options,
+      target,
+      codeBundleCache,
+    );
+    if (polyfillBundleOptions) {
+      bundlerContexts.push(
+        new BundlerContext(workspaceRoot, !!options.watch, polyfillBundleOptions),
+      );
+    }
+
+    // Global Stylesheets
+    if (options.globalStyles.length > 0) {
+      for (const initial of [true, false]) {
+        const bundleOptions = createGlobalStylesBundleOptions(
+          options,
+          target,
+          initial,
+          codeBundleCache?.loadResultCache,
+        );
+        if (bundleOptions) {
+          bundlerContexts.push(
+            new BundlerContext(workspaceRoot, !!options.watch, bundleOptions, () => initial),
+          );
+        }
+      }
+    }
+
+    // Global Scripts
+    if (options.globalScripts.length > 0) {
+      for (const initial of [true, false]) {
+        const bundleOptions = createGlobalScriptsBundleOptions(options, initial);
+        if (bundleOptions) {
+          bundlerContexts.push(
+            new BundlerContext(workspaceRoot, !!options.watch, bundleOptions, () => initial),
+          );
+        }
+      }
+    }
+
+    // Server application code
+    // Skip server build when none of the features are enabled.
+    if (serverEntryPoint && (prerenderOptions || appShellOptions || ssrOptions)) {
+      const nodeTargets = getSupportedNodeTargets();
+      bundlerContexts.push(
+        new BundlerContext(
+          workspaceRoot,
+          !!options.watch,
+          createServerCodeBundleOptions(options, [...target, ...nodeTargets], codeBundleCache),
+          () => false,
+        ),
+      );
+    }
   }
 
-  const bundlingResult = await BundlerContext.bundleAll(
-    bundlerContexts,
-    rebuildState?.fileChanges.all,
-  );
+  const bundlingResult = await BundlerContext.bundleAll(bundlerContexts);
+
+  // Log all warnings and errors generated during bundling
+  await logMessages(context, bundlingResult);
 
   const executionResult = new ExecutionResult(bundlerContexts, codeBundleCache);
-  executionResult.addWarnings(bundlingResult.warnings);
 
   // Return if the bundling has errors
   if (bundlingResult.errors) {
-    executionResult.addErrors(bundlingResult.errors);
-
     return executionResult;
-  }
-
-  // Analyze external imports if external options are enabled
-  if (options.externalPackages || bundlingResult.externalConfiguration) {
-    const {
-      externalConfiguration,
-      externalImports: { browser, server },
-    } = bundlingResult;
-    const implicitBrowser = browser ? [...browser] : [];
-    const implicitServer = server ? [...server] : [];
-    // TODO: Implement wildcard externalConfiguration filtering
-    executionResult.setExternalMetadata(
-      externalConfiguration
-        ? implicitBrowser.filter((value) => !externalConfiguration.includes(value))
-        : implicitBrowser,
-      externalConfiguration
-        ? implicitServer.filter((value) => !externalConfiguration.includes(value))
-        : implicitServer,
-      externalConfiguration,
-    );
   }
 
   const { metafile, initialFiles, outputFiles } = bundlingResult;
 
   executionResult.outputFiles.push(...outputFiles);
 
-  const changedFiles =
-    rebuildState && executionResult.findChangedFiles(rebuildState.previousOutputHashes);
-
-  // Analyze files for bundle budget failures if present
-  let budgetFailures: BudgetCalculatorResult[] | undefined;
-  if (options.budgets) {
-    const compatStats = generateBudgetStats(metafile, initialFiles);
-    budgetFailures = [...checkBudgets(options.budgets, compatStats, true)];
-    for (const { message, severity } of budgetFailures) {
-      if (severity === 'error') {
-        executionResult.addError(message);
-      } else {
-        executionResult.addWarning(message);
-      }
-    }
-  }
-
-  // Calculate estimated transfer size if scripts are optimized
-  let estimatedTransferSizes;
-  if (optimizationOptions.scripts || optimizationOptions.styles.minify) {
-    estimatedTransferSizes = await calculateEstimatedTransferSizes(executionResult.outputFiles);
-  }
-
   // Check metafile for CommonJS module usage if optimizing scripts
   if (optimizationOptions.scripts) {
     const messages = checkCommonJSModules(metafile, options.allowedCommonJsDependencies);
-    executionResult.addWarnings(messages);
+    await logMessages(context, { warnings: messages });
   }
 
   // Copy assets
@@ -140,56 +175,50 @@ export async function executeBuild(
     );
   }
 
-  // Perform i18n translation inlining if enabled
-  let prerenderedRoutes: string[];
-  if (i18nOptions.shouldInline) {
-    const result = await inlineI18n(options, executionResult, initialFiles);
-    executionResult.addErrors(result.errors);
-    executionResult.addWarnings(result.warnings);
-    prerenderedRoutes = result.prerenderedRoutes;
-  } else {
-    const result = await executePostBundleSteps(
-      options,
-      executionResult.outputFiles,
-      executionResult.assetFiles,
-      initialFiles,
-      // Set lang attribute to the defined source locale if present
-      i18nOptions.hasDefinedSourceLocale ? i18nOptions.sourceLocale : undefined,
-    );
-
-    executionResult.addErrors(result.errors);
-    executionResult.addWarnings(result.warnings);
-    prerenderedRoutes = result.prerenderedRoutes;
-    executionResult.outputFiles.push(...result.additionalOutputFiles);
-    executionResult.assetFiles.push(...result.additionalAssets);
-  }
-
-  if (prerenderOptions) {
-    executionResult.addOutputFile(
-      'prerendered-routes.json',
-      JSON.stringify({ routes: prerenderedRoutes.sort((a, b) => a.localeCompare(b)) }, null, 2),
-      BuildOutputFileType.Root,
-    );
-
-    let prerenderMsg = `Prerendered ${prerenderedRoutes.length} static route`;
-    if (prerenderedRoutes.length > 1) {
-      prerenderMsg += 's.';
-    } else {
-      prerenderMsg += '.';
+  // Analyze files for bundle budget failures if present
+  let budgetFailures;
+  if (options.budgets) {
+    const compatStats = generateBudgetStats(metafile, initialFiles);
+    budgetFailures = [...checkBudgets(options.budgets, compatStats, true)];
+    for (const { severity, message } of budgetFailures) {
+      if (severity === 'error') {
+        context.logger.error(message);
+      } else {
+        context.logger.warn(message);
+      }
     }
-
-    context.logger.info(colors.magenta(prerenderMsg) + '\n');
   }
 
-  logBuildStats(
-    context.logger,
-    metafile,
-    initialFiles,
-    budgetFailures,
-    changedFiles,
-    estimatedTransferSizes,
-  );
+  // Calculate estimated transfer size if scripts are optimized
+  let estimatedTransferSizes;
+  if (optimizationOptions.scripts || optimizationOptions.styles.minify) {
+    estimatedTransferSizes = await calculateEstimatedTransferSizes(executionResult.outputFiles);
+  }
 
+  // Perform i18n translation inlining if enabled
+  if (i18nOptions.shouldInline) {
+    const { errors, warnings } = await inlineI18n(options, executionResult, initialFiles);
+    printWarningsAndErrorsToConsole(context, warnings, errors);
+  } else {
+    const { errors, warnings, additionalAssets, additionalOutputFiles } =
+      await executePostBundleSteps(
+        options,
+        executionResult.outputFiles,
+        executionResult.assetFiles,
+        initialFiles,
+        // Set lang attribute to the defined source locale if present
+        i18nOptions.hasDefinedSourceLocale ? i18nOptions.sourceLocale : undefined,
+      );
+
+    executionResult.outputFiles.push(...additionalOutputFiles);
+    executionResult.assetFiles.push(...additionalAssets);
+    printWarningsAndErrorsToConsole(context, warnings, errors);
+  }
+
+  logBuildStats(context, metafile, initialFiles, budgetFailures, estimatedTransferSizes);
+
+  const buildTime = Number(process.hrtime.bigint() - startTime) / 10 ** 9;
+  context.logger.info(`Application bundle generation complete. [${buildTime.toFixed(3)} seconds]`);
   // Write metafile if stats option is enabled
   if (options.stats) {
     executionResult.addOutputFile(
@@ -200,4 +229,17 @@ export async function executeBuild(
   }
 
   return executionResult;
+}
+
+function printWarningsAndErrorsToConsole(
+  context: BuilderContext,
+  warnings: string[],
+  errors: string[],
+): void {
+  for (const error of errors) {
+    context.logger.error(error);
+  }
+  for (const warning of warnings) {
+    context.logger.warn(warning);
+  }
 }
