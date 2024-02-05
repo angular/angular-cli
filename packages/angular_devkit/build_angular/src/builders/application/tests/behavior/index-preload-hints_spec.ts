5e6f1a9f4362e9b12db64c7c2e609a346b17963d
Alan Agius <alanagius@google.com>
2024-02-05 08:24:58 +0000

    fix(@angular-devkit/build-angular): avoid preloading server chunks
    https://github.com/angular/angular-cli/commit/41ea985f9317b11cfa6627a2d3f6b34ff4dbc134#diff-239ad7beb7d8a76046cdc7b4d2263b2e05c300e3e0510916cb907e93efec5af0 introduced a regression which causes server enter-points

--- packages/angular_devkit/build_angular/src/builders/application/tests/behavior/index-preload-hints_spec.ts
+++ packages/angular_devkit/build_angular/src/builders/application/tests/behavior/index-preload-hints_spec.ts
@@ -33,5 +33,32 @@ describeBuilder(buildApplication, APPLICATION_BUILDER_INFO, (harness) => {
           '<link rel="preload" href="https://fonts.googleapis.com/css2?family=Roboto+Mono&family=Roboto:wght@300;400;500;700&display=swap" as="style">',
         );
     });
+
+    it('should not add preload hints for ssr files', async () => {
+      await harness.modifyFile('src/tsconfig.app.json', (content) => {
+        const tsConfig = JSON.parse(content);
+        tsConfig.files ??= [];
+        tsConfig.files.push('main.server.ts', 'server.ts');
+
+        return JSON.stringify(tsConfig);
+      });
+
+      await harness.writeFile('src/server.ts', `console.log('Hello!');`);
+
+      harness.useTarget('build', {
+        ...BASE_OPTIONS,
+        server: 'src/main.server.ts',
+        ssr: true,
+      });
+
+      const { result } = await harness.executeOnce();
+      expect(result?.success).toBeTrue();
+
+      harness.expectFile('dist/server/main.server.mjs').toExist();
+
+      harness
+        .expectFile('dist/browser/index.html')
+        .content.not.toMatch(/<link rel="modulepreload" href="chunk-\.+\.mjs">/);
+    });
   });
 });
