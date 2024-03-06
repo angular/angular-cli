a026803e05415aff552818298608a47e6c7203ab
Alan Agius <alanagius@google.com>
2024-03-06 15:19:34 +0000

    test: fix failing update e2e tests
    The HTML was not matching the expect correctly

--- tests/legacy-cli/e2e/assets/15.0-project/src/app/app.component.spec.ts
+++ tests/legacy-cli/e2e/assets/15.0-project/src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('fifteen-project app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, fifteen-project');
   });
 });
