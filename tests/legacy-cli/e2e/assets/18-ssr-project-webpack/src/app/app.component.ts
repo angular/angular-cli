09dfc37b4db0e3b9ac8e17f6cf6a1f0584366f95
Alan Agius <alanagius@google.com>
2024-03-15 08:56:18 +0000

    test: update e2e test to work with Angular 18
    Updating this test to work with Angular 18.

--- tests/legacy-cli/e2e/assets/18-ssr-project-webpack/src/app/app.component.ts
+++ tests/legacy-cli/e2e/assets/18-ssr-project-webpack/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '17-ssr-project-webpack';
+  title = '18-ssr-project-webpack';
 }
