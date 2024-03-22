d8396e6afd888758856b307130917afe11cc4205
Alan Agius <alanagius@google.com>
2024-03-22 08:21:22 +0000

    test: remove webpack test-app test
    This particular test is no longer useful, hence it has been removed.

--- tests/legacy-cli/e2e/assets/webpack/test-app/app/injectable.ts
+++
@@ -1,8 +0,0 @@
-import {Injectable, Inject, ViewContainerRef} from '@angular/core';
-import {DOCUMENT} from '@angular/common';
-
-
-@Injectable()
-export class MyInjectable {
-  constructor(public viewContainer: ViewContainerRef, @Inject(DOCUMENT) public doc) {}
-}
