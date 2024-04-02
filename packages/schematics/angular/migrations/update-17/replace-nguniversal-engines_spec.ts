6061a0bd470adf5777e9997dd01fda84062edf83
Alan Agius <alan.agius4@gmail.com>
2024-04-02 10:10:33 -0700

    Revert "fix(@schematics/angular): rename SSR port env variable"
    This reverts commit 950a44521fdfb82000f6564ccc4c87d4a2b94680.
    
    (cherry picked from commit a673baf5ce385415ded23641a2dc5cdcae8f3f5c)

--- packages/schematics/angular/migrations/update-17/replace-nguniversal-engines_spec.ts
+++ packages/schematics/angular/migrations/update-17/replace-nguniversal-engines_spec.ts
@@ -128,7 +128,7 @@ export function app(): express.Express {
 }
 
 function run() {
-  const port = process.env.SSR_PORT || 4000;
+  const port = process.env.PORT || 4000;
 
   // Start up the Node server
   const server = app();
