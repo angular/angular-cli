e6582e6931ba22cc3915322e3657df2e3d556794
Charles Lyding <19598772+clydin@users.noreply.github.com>
2024-04-12 10:47:46 -0400

    refactor(@angular-devkit/build-angular): reduce Webpack specific overlap in stats table generation
    Webpack-specific types and imports have been reduced within the logic for the build stats table generation.
    This reduces the amount of unused code that needs to be loaded when not using a Webpack-based bundler.

---
+++ packages/angular_devkit/build_angular/src/utils/format-bytes.ts
@@ -0,0 +1,21 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+export function formatSize(size: number): string {
+  if (size <= 0) {
+    return '0 bytes';
+  }
+
+  const abbreviations = ['bytes', 'kB', 'MB', 'GB'];
+  const index = Math.floor(Math.log(size) / Math.log(1024));
+  const roundedSize = size / Math.pow(1024, index);
+  // bytes don't have a fraction
+  const fractionDigits = index === 0 ? 0 : 2;
+
+  return `${roundedSize.toFixed(fractionDigits)} ${abbreviations[index]}`;
+}
