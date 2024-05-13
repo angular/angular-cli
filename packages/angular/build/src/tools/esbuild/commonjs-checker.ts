02faba6a0cbd3a180aab82cab962926e064349b1
Alan Agius <alanagius@google.com>
2024-05-13 11:16:13 +0000

    refactor: replace angular.io link with angular.dev
    This replaces https://angular.io/guide/build#configuring-commonjs-dependencies with https://angular.dev/tools/cli/build#configuring-commonjs-dependencies

--- packages/angular/build/src/tools/esbuild/commonjs-checker.ts
+++ packages/angular/build/src/tools/esbuild/commonjs-checker.ts
@@ -169,7 +169,7 @@ function createCommonJSModuleError(request: string, importer: string): PartialMe
       {
         text:
           'CommonJS or AMD dependencies can cause optimization bailouts.\n' +
-          'For more information see: https://angular.io/guide/build#configuring-commonjs-dependencies',
+          'For more information see: https://angular.dev/tools/cli/build#configuring-commonjs-dependencies',
       },
     ],
   };
