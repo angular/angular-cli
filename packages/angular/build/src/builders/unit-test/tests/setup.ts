ae9afbe6ac66f730a764ba42ce61958cc4db7881
Charles Lyding <19598772+clydin@users.noreply.github.com>
2024-05-23 13:35:02 -0400

    feat(@angular/build): add experimental vitest unit-testing support
    When using the application build system via the `@angular/build` package
    (default for new projects starting in v20), a new experimental unit-test
    builder is available that initially uses vitest. This experimental system
    is intended to provide support for investigation of future unit testing
    efforts within the Angular CLI. As this is experimental, no SemVer guarantees
    are provided, the API and behavior may change, and there may be unexpected
    behavior. Available test runners may be added or removed as well.
    
    The setup is somewhat different than the previous unit-testing builders.
    It uses a similar mechanism to that of the `dev-server` and requires a
    `buildTarget` option. This allows the code building aspects of the unit-
    testing process to leverage pre-existing option values that are already
    defined for development. If differing option values are required for testing,
    an additional build target configuration specifically for testing can be
    used.
    
    The current vitest support has multiple caveats including but not limited to:
    
    * No watch support
    * `jsdom` based testing only (`jsdom` must be installed in the project)
    * Custom vitest configuration is not supported
    * Code coverage is not yet available
    * Reporter customization is not yet available
    
    An example configuration that would replace the `test` target for a project
    is as follows:
    ```
    "test": {
        "builder": "@angular/build:unit-test",
        "options": {
            "tsConfig": "tsconfig.spec.json",
            "buildTarget": "::development",
            "runner": "vitest"
        }
    }
    ```

---
+++ packages/angular/build/src/builders/unit-test/tests/setup.ts
@@ -0,0 +1,105 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.dev/license
+ */
+
+import { json } from '@angular-devkit/core';
+import { readFileSync } from 'node:fs';
+import path from 'node:path';
+import { BuilderHarness } from '../../../../../../../modules/testing/builder/src';
+import {
+  ApplicationBuilderOptions as AppilicationSchema,
+  buildApplication,
+} from '../../../builders/application';
+import { Schema } from '../schema';
+
+// TODO: Consider using package.json imports field instead of relative path
+//       after the switch to rules_js.
+export * from '../../../../../../../modules/testing/builder/src';
+
+// TODO: Remove and use import after Vite-based dev server is moved to new package
+export const APPLICATION_BUILDER_INFO = Object.freeze({
+  name: '@angular/build:application',
+  schemaPath: path.join(
+    path.dirname(require.resolve('@angular/build/package.json')),
+    'src/builders/application/schema.json',
+  ),
+});
+
+/**
+ * Contains all required application builder fields.
+ * Also disables progress reporting to minimize logging output.
+ */
+export const APPLICATION_BASE_OPTIONS = Object.freeze<AppilicationSchema>({
+  index: 'src/index.html',
+  browser: 'src/main.ts',
+  outputPath: 'dist',
+  tsConfig: 'src/tsconfig.app.json',
+  progress: false,
+
+  // Disable optimizations
+  optimization: false,
+
+  // Enable polling (if a test enables watch mode).
+  // This is a workaround for bazel isolation file watch not triggering in tests.
+  poll: 100,
+});
+
+export const UNIT_TEST_BUILDER_INFO = Object.freeze({
+  name: '@angular/build:unit-test',
+  schemaPath: __dirname + '/../schema.json',
+});
+
+/**
+ * Contains all required dev-server builder fields.
+ * The port is also set to zero to ensure a free port is used for each test which
+ * supports parallel test execution.
+ */
+export const BASE_OPTIONS = Object.freeze<Schema>({
+  buildTarget: 'test:build',
+  tsConfig: 'src/tsconfig.spec.json',
+  runner: 'vitest' as any,
+});
+
+/**
+ * Maximum time for single build/rebuild
+ * This accounts for CI variability.
+ */
+export const BUILD_TIMEOUT = 25_000;
+
+/**
+ * Cached application builder option schema
+ */
+let applicationSchema: json.schema.JsonSchema | undefined;
+
+/**
+ * Adds a `build` target to a builder test harness for the application builder with the base options
+ * used by the application builder tests.
+ *
+ * @param harness The builder harness to use when setting up the application builder target
+ * @param extraOptions The additional options that should be used when executing the target.
+ */
+export function setupApplicationTarget<T>(
+  harness: BuilderHarness<T>,
+  extraOptions?: Partial<AppilicationSchema>,
+): void {
+  applicationSchema ??= JSON.parse(
+    readFileSync(APPLICATION_BUILDER_INFO.schemaPath, 'utf8'),
+  ) as json.schema.JsonSchema;
+
+  harness.withBuilderTarget(
+    'build',
+    buildApplication,
+    {
+      ...APPLICATION_BASE_OPTIONS,
+      ...extraOptions,
+    },
+    {
+      builderName: APPLICATION_BUILDER_INFO.name,
+      optionSchema: applicationSchema,
+    },
+  );
+}
