0e3a21c8a8e36200f5c883f22239810bfa72d482
Charles Lyding <19598772+clydin@users.noreply.github.com>
2024-05-23 13:35:02 -0400

    feat(@angular/build): add experimental vitest unit-testing support
    When using the application build system via the `@angular/build` package
    (default for new projects starting in v20), a new experimental unit-test
    builder is available that initially uses vitest. This experimental system
    is intended to provide support for investigation of future unit testing
    efforts within the Angular CLI. As this is experimental, no SemVer guarantees
    are provided, the API and behavior may change, and there may be unexpected
    behavior. Available test runners may be added or removed as well.
    
    The setup is somewhat different than the previous unit-testing builders.
    It uses a similar mechanism to that of the `dev-server` and requires a
    `buildTarget` option. This allows the code building aspects of the unit-
    testing process to leverage pre-existing option values that are already
    defined for development. If differing option values are required for testing,
    an additional build target configuration specifically for testing can be
    used.
    
    The current vitest support has multiple caveats including but not limited to:
    
    * No watch support
    * `jsdom` based testing only (`jsdom` must be installed in the project)
    * Custom vitest configuration is not supported
    
    An example configuration that would replace the `test` target for a project
    is as follows:
    ```
    "test": {
        "builder": "@angular/build:unit-test",
        "options": {
            "tsConfig": "tsconfig.spec.json",
            "buildTarget": "::development",
            "runner": "vitest"
        }
    }
    ```

---
+++ packages/angular/build/src/builders/unit-test/options.ts
@@ -0,0 +1,74 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.dev/license
+ */
+
+import { type BuilderContext, targetFromTargetString } from '@angular-devkit/architect';
+import path from 'node:path';
+import { normalizeCacheOptions } from '../../utils/normalize-cache';
+import type { Schema as UnitTestOptions } from './schema';
+
+export type NormalizedUnitTestOptions = Awaited<ReturnType<typeof normalizeOptions>>;
+
+export async function normalizeOptions(
+  context: BuilderContext,
+  projectName: string,
+  options: UnitTestOptions,
+) {
+  // Setup base paths based on workspace root and project information
+  const workspaceRoot = context.workspaceRoot;
+  const projectMetadata = await context.getProjectMetadata(projectName);
+  const projectRoot = normalizeDirectoryPath(
+    path.join(workspaceRoot, (projectMetadata.root as string | undefined) ?? ''),
+  );
+  const projectSourceRoot = normalizeDirectoryPath(
+    path.join(workspaceRoot, (projectMetadata.sourceRoot as string | undefined) ?? 'src'),
+  );
+
+  // Gather persistent caching option and provide a project specific cache location
+  const cacheOptions = normalizeCacheOptions(projectMetadata, workspaceRoot);
+  cacheOptions.path = path.join(cacheOptions.path, projectName);
+
+  // Target specifier defaults to the current project's build target using a development configuration
+  const buildTargetSpecifier = options.buildTarget ?? `::development`;
+  const buildTarget = targetFromTargetString(buildTargetSpecifier, projectName, 'build');
+
+  const { codeCoverage, codeCoverageExclude, tsConfig, runner, reporters } = options;
+
+  return {
+    // Project/workspace information
+    workspaceRoot,
+    projectRoot,
+    projectSourceRoot,
+    cacheOptions,
+    // Target/configuration specified options
+    buildTarget,
+    include: options.include ?? ['**/*.spec.ts'],
+    exclude: options.exclude ?? [],
+    runnerName: runner,
+    codeCoverage,
+    codeCoverageExclude,
+    tsConfig,
+    reporters,
+    // TODO: Implement watch support
+    watch: false,
+  };
+}
+
+/**
+ * Normalize a directory path string.
+ * Currently only removes a trailing slash if present.
+ * @param path A path string.
+ * @returns A normalized path string.
+ */
+function normalizeDirectoryPath(path: string): string {
+  const last = path[path.length - 1];
+  if (last === '/' || last === '\\') {
+    return path.slice(0, -1);
+  }
+
+  return path;
+}
