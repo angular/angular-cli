e506a7554ba2c0ae6ac34cad42285044102f23e5
David LJ <mail@davidlj95.com>
2024-05-24 18:30:41 +0200

    docs: update build guide links aio->adev
    (cherry picked from commit 89fbd0f9a2d2ec4cd59e28080064bb19a8668ad3)

--- packages/angular_devkit/build_angular/src/tools/webpack/plugins/typescript.ts
+++ packages/angular_devkit/build_angular/src/tools/webpack/plugins/typescript.ts
@@ -35,7 +35,7 @@ export function createIvyPlugin(
     wco.logger.warn(
       'TypeScript compiler options "target" and "useDefineForClassFields" are set to "ES2022" and ' +
         '"false" respectively by the Angular CLI. To control ECMA version and features use the Browserslist configuration. ' +
-        'For more information, see https://angular.io/guide/build#configuring-browser-compatibility\n' +
+        'For more information, see https://angular.dev/tools/cli/build#configuring-browser-compatibility\n' +
         `NOTE: You can set the "target" to "ES2022" in the project's tsconfig to remove this warning.`,
     );
   }
