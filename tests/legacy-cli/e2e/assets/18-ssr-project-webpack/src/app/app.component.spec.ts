e3b8b780be72d8a0c6247a25e4e1e1ccfbdea67c
Atchyut Pulavarthi <atchyut.pulavarthi@postman.com>
2024-06-14 10:45:14 +0530

    fix(@schematics/angular): update schematics to use RouterModule when --routing flag is present
    update schematics to use RouterModule instead of RouterTestingModule when generating a new project with the --routing flag
    
    Fixes #27691

--- tests/legacy-cli/e2e/assets/18-ssr-project-webpack/src/app/app.component.spec.ts
+++ tests/legacy-cli/e2e/assets/18-ssr-project-webpack/src/app/app.component.spec.ts
@@ -1,10 +1,10 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
+    imports: [RouterModule.forRoot([])],
     declarations: [AppComponent]
   }));
 
