0736a65c93bc5c75d3ba9fbeb430b0f5dac89992
Alan Agius <alanagius@google.com>
2024-06-14 13:50:34 +0000

    test: update NPM to latest version
    This update upgrades the NPM version used for running end-to-end tests to the latest version.
    
    Notable changes include:
    - The `NPM_CONFIG__AUTH` configuration, which is tested here, no longer works for local packages as per [npm/cli#3985](https://github.com/npm/cli/issues/3985).
    - NPM no longer supports unscoped auth. The configuration error "Invalid auth configuration found: `_auth` must be renamed to `//localhost:39521/:_auth` in the project config" will be encountered.

--- tests/legacy-cli/e2e/tests/commands/add/secure-registry.ts
+++ tests/legacy-cli/e2e/tests/commands/add/secure-registry.ts
@@ -1,5 +1,5 @@
 import { expectFileNotToExist, expectFileToExist } from '../../../utils/fs';
-import { installWorkspacePackages } from '../../../utils/packages';
+import { getActivePackageManager, installWorkspacePackages } from '../../../utils/packages';
 import { git, ng } from '../../../utils/process';
 import { createNpmConfigForAuthentication } from '../../../utils/registry';
 import { expectToFail } from '../../../utils/utils';
@@ -7,16 +7,19 @@ import { expectToFail } from '../../../utils/utils';
 export default async function () {
   // The environment variable has priority over the .npmrc
   delete process.env['NPM_CONFIG_REGISTRY'];
+  const isNpm = getActivePackageManager() === 'npm';
 
   const command = ['add', '@angular/pwa', '--skip-confirmation'];
   await expectFileNotToExist('public/manifest.webmanifest');
 
   // Works with unscoped registry authentication details
-  await createNpmConfigForAuthentication(false);
-  await ng(...command);
-  await expectFileToExist('public/manifest.webmanifest');
-  await git('clean', '-dxf');
-
+  if (!isNpm) {
+    // NPM no longer support unscoped.
+    await createNpmConfigForAuthentication(false);
+    await ng(...command);
+    await expectFileToExist('public/manifest.webmanifest');
+    await git('clean', '-dxf');
+  }
   // Works with scoped registry authentication details
   await expectFileNotToExist('public/manifest.webmanifest');
 
@@ -25,8 +28,11 @@ export default async function () {
   await expectFileToExist('public/manifest.webmanifest');
 
   // Invalid authentication token
-  await createNpmConfigForAuthentication(false, true);
-  await expectToFail(() => ng(...command));
+  if (isNpm) {
+    // NPM no longer support unscoped.
+    await createNpmConfigForAuthentication(false, true);
+    await expectToFail(() => ng(...command));
+  }
 
   await createNpmConfigForAuthentication(true, true);
   await expectToFail(() => ng(...command));
