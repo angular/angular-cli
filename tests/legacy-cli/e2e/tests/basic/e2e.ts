73ecd361d51d43309b228939b177d325e0576533
Charles Lyding <19598772+clydin@users.noreply.github.com>
2024-06-21 14:02:56 -0400

    test: add proper assertions to several E2E tests
    The aot, build, command-scope, e2e, and run E2E tests now use assertions from the
    Node.js `assert` builtin module. This reduces the amount of code within
    each test and improves the overall readability.

--- tests/legacy-cli/e2e/tests/basic/e2e.ts
+++ tests/legacy-cli/e2e/tests/basic/e2e.ts
@@ -1,9 +1,9 @@
+import assert from 'node:assert/strict';
 import { setTimeout } from 'node:timers/promises';
 import { silentNg } from '../../utils/process';
-import { expectToFail } from '../../utils/utils';
 
 export default async function () {
-  await expectToFail(() => silentNg('e2e', 'test-project', '--dev-server-target='));
+  assert.rejects(silentNg('e2e', 'test-project', '--dev-server-target='));
 
   // These should work.
   await silentNg('e2e', 'test-project');
