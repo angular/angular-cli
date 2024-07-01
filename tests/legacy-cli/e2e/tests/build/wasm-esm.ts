5440b4cfccbe9cd5a4041f086f08706a1298faae
Charles Lyding <19598772+clydin@users.noreply.github.com>
2024-07-01 14:31:47 -0400

    feat(@angular/build): support WASM/ES Module integration proposal
    Application builds will now support the direct import of WASM files.
    The behavior follows the WebAssembly/ES module integration proposal. The usage
    of this feature requires the ability to use native async/await and top-level
    await. Due to this requirement, applications must be zoneless to use this new
    feature. Applications that use Zone.js are currently incompatible and an error
    will be generated if the feature is used in a Zone.js application. Manual
    setup of a WASM file is, however, possible in a Zone.js application if WASM
    usage is required. Further details for manual setup can be found here:
    https://developer.mozilla.org/en-US/docs/WebAssembly/Loading_and_running
    
    The following is a brief example of using a WASM file in the new feature
    with the integration proposal behavior:
    ```
    import { multiply } from './example.wasm';
    
    console.log(multiply(4, 5));
    ```
    
    NOTE: TypeScript will not automatically understand the types for WASM files.
    Type definition files will need to be created for each WASM file to allow
    for an error-free build. These type definition files are specific to each
    individual WASM file and will either need to be manually created or provided
    by library authors.
    
    The feature relies on an active proposal which may change as it progresses
    through the standardization process. This may result in behavioral differences
    between versions.
    Proposal Details: https://github.com/WebAssembly/esm-integration/tree/main/proposals/esm-integration
    
    For more information regarding zoneless applications, you can visit https://angular.dev/guide/experimental/zoneless

---
+++ tests/legacy-cli/e2e/tests/build/wasm-esm.ts
@@ -0,0 +1,97 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.dev/license
+ */
+import { writeFile } from 'node:fs/promises';
+import { ng } from '../../utils/process';
+import { prependToFile, replaceInFile } from '../../utils/fs';
+import { updateJsonFile } from '../../utils/project';
+
+/**
+ * Compiled and base64 encoded WASM file for the following WAT:
+ * ```
+ * (module
+ *  (import "./values" "getValue" (func $getvalue (result i32)))
+ *  (export "multiply" (func $multiply))
+ *  (export "subtract1" (func $subtract))
+ *  (func $multiply (param i32 i32) (result i32)
+ *    local.get 0
+ *    local.get 1
+ *    i32.mul
+ *  )
+ *  (func $subtract (param i32) (result i32)
+ *    call $getvalue
+ *    local.get 0
+ *    i32.sub
+ *  )
+ * )
+ * ```
+ */
+const importWasmBase64 =
+  'AGFzbQEAAAABEANgAAF/YAJ/fwF/YAF/AX8CFQEILi92YWx1ZXMIZ2V0VmFsdWUAAAMDAgECBxgCCG11bHRpcGx5AAEJc3VidHJhY3QxAAIKEQIHACAAIAFsCwcAEAAgAGsLAC8EbmFtZQEfAwAIZ2V0dmFsdWUBCG11bHRpcGx5AghzdWJ0cmFjdAIHAwAAAQACAA==';
+const importWasmBytes = Buffer.from(importWasmBase64, 'base64');
+
+export default async function () {
+  // Add WASM file to project
+  await writeFile('src/app/multiply.wasm', importWasmBytes);
+  await writeFile(
+    'src/app/multiply.wasm.d.ts',
+    'export declare function multiply(a: number, b: number): number; export declare function subtract1(a: number): number;',
+  );
+
+  // Add requested WASM import file
+  await writeFile('src/app/values.js', 'export function getValue() { return 100; }');
+
+  // Use WASM file in project
+  await prependToFile(
+    'src/app/app.component.ts',
+    `
+      import { multiply, subtract1 } from './multiply.wasm';
+    `,
+  );
+  await replaceInFile(
+    'src/app/app.component.ts',
+    "'test-project'",
+    'multiply(4, 5) + subtract1(88)',
+  );
+
+  // Remove Zone.js from polyfills and make zoneless
+  await updateJsonFile('angular.json', (json) => {
+    // Remove bundle budgets to avoid a build error due to the expected increased output size
+    // of a JIT production build.
+    json.projects['test-project'].architect.build.options.polyfills = [];
+  });
+  await replaceInFile(
+    'src/app/app.config.ts',
+    'provideZoneChangeDetection',
+    'provideExperimentalZonelessChangeDetection',
+  );
+  await replaceInFile(
+    'src/app/app.config.ts',
+    'provideZoneChangeDetection({ eventCoalescing: true })',
+    'provideExperimentalZonelessChangeDetection()',
+  );
+
+  await ng('build');
+
+  // Update E2E test to check for WASM execution
+  await writeFile(
+    'e2e/src/app.e2e-spec.ts',
+    `
+    import { AppPage } from './app.po';
+    import { browser, logging } from 'protractor';
+    describe('WASM execution', () => {
+      it('should log WASM result messages', async () => {
+        const page = new AppPage();
+        await page.navigateTo();
+        expect(await page.getTitleText()).toEqual('Hello, 32');
+      });
+    });
+  `,
+  );
+
+  await ng('e2e');
+}
