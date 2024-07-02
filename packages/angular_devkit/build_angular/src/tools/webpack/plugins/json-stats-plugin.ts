3349952f596351c68aa162a7d6c2721e6e58234c
Charles Lyding <19598772+clydin@users.noreply.github.com>
2024-07-02 09:47:36 -0400

    build: update @discovery/json-ext to v0.6.0

--- packages/angular_devkit/build_angular/src/tools/webpack/plugins/json-stats-plugin.ts
+++ packages/angular_devkit/build_angular/src/tools/webpack/plugins/json-stats-plugin.ts
@@ -6,8 +6,10 @@
  * found in the LICENSE file at https://angular.dev/license
  */
 
-import { createWriteStream, promises as fsPromises } from 'fs';
-import { dirname } from 'path';
+import { createWriteStream } from 'node:fs';
+import { mkdir } from 'node:fs/promises';
+import { dirname } from 'node:path';
+import { pipeline } from 'node:stream/promises';
 import { Compiler } from 'webpack';
 import { assertIsError } from '../../../utils/error';
 import { addError } from '../../../utils/webpack-diagnostics';
@@ -17,17 +19,12 @@ export class JsonStatsPlugin {
 
   apply(compiler: Compiler) {
     compiler.hooks.done.tapPromise('angular-json-stats', async (stats) => {
-      const { stringifyStream } = await import('@discoveryjs/json-ext');
+      const { stringifyChunked } = await import('@discoveryjs/json-ext');
       const data = stats.toJson('verbose');
 
       try {
-        await fsPromises.mkdir(dirname(this.statsOutputPath), { recursive: true });
-        await new Promise<void>((resolve, reject) =>
-          stringifyStream(data)
-            .pipe(createWriteStream(this.statsOutputPath))
-            .on('close', resolve)
-            .on('error', reject),
-        );
+        await mkdir(dirname(this.statsOutputPath), { recursive: true });
+        await pipeline(stringifyChunked(data), createWriteStream(this.statsOutputPath));
       } catch (error) {
         assertIsError(error);
         addError(
