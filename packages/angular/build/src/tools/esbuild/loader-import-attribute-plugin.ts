b87d52b545990da403c7f7e0b4358ecd831c6258
Charles Lyding <19598772+clydin@users.noreply.github.com>
2024-07-15 11:21:50 -0400

    refactor(@angular/build): support image-file loader import attribute type
    An additional `loader` import attribute value specialized for image files
    (`loader: "image-file"`) is now available. This loader type is intended for use
    with the AOT compiler's optimized image instruction. It provides the same
    behavior as the `file` type with the addition of two named exports for the
    width and height of the image. The `image-size` package is used internally
    to determine image dimensions and all image types support by that package
    are also supported here. This loader type is currently not considered part
    of the public API and is only intended for use by the AOT compiler at this
    time.

--- packages/angular/build/src/tools/esbuild/loader-import-attribute-plugin.ts
+++ packages/angular/build/src/tools/esbuild/loader-import-attribute-plugin.ts
@@ -6,8 +6,11 @@
  * found in the LICENSE file at https://angular.dev/license
  */
 
-import type { Loader, Plugin } from 'esbuild';
+import type { Loader, PartialMessage, Plugin } from 'esbuild';
 import { readFile } from 'node:fs/promises';
+import { basename, dirname } from 'node:path';
+import { assertIsError } from '../../utils/error';
+import { MemoryCache } from './cache';
 
 const SUPPORTED_LOADERS: Loader[] = ['binary', 'file', 'text'];
 
@@ -15,10 +18,59 @@ export function createLoaderImportAttributePlugin(): Plugin {
   return {
     name: 'angular-loader-import-attributes',
     setup(build) {
+      let imageSizer: typeof import('image-size') | undefined;
+      const imageDataCache = new MemoryCache<Uint8Array>();
+
+      build.onEnd(() => {
+        imageDataCache.clear();
+      });
+
       build.onLoad({ filter: /./ }, async (args) => {
-        const loader = args.with['loader'] as Loader | undefined;
+        const loader = args.with['loader'] as Loader | 'image-file' | undefined;
         if (!loader) {
           return undefined;
+        }
+
+        // Loader type that provides image metadata as named exports in addition to `file` loader behavior.
+        // This is used by the AOT compiler to support the optimized image functionality.
+        if (loader === 'image-file') {
+          let contents = `import imagePath from ${JSON.stringify('./' + basename(args.path))} with { loader: "file" }\n`;
+          contents += 'export default imagePath;\n';
+
+          // Calculate width and height
+          imageSizer ??= await import('image-size');
+
+          // The image data is required to determine the dimensions and is cached in the plugin to minimize FS access
+          const imageData = await imageDataCache.getOrCreate(
+            args.path,
+            async () => await readFile(args.path),
+          );
+          let warnings: PartialMessage[] | undefined;
+          try {
+            const result = imageSizer.imageSize(imageData);
+            if (typeof result.height === 'number' && typeof result.width === 'number') {
+              contents += `const width = ${result.width};\nconst height = ${result.height};\nexport { width, height }\n`;
+            } else {
+              warnings = [
+                {
+                  text: 'Image dimensions unavailable within file',
+                },
+              ];
+            }
+          } catch (error) {
+            assertIsError(error);
+
+            return {
+              errors: [{ text: 'Unable to analyze image file', notes: [{ text: error.message }] }],
+            };
+          }
+
+          return {
+            contents,
+            resolveDir: dirname(args.path),
+            loader: 'js',
+            warnings,
+          };
         }
 
         if (!SUPPORTED_LOADERS.includes(loader)) {
@@ -34,8 +86,12 @@ export function createLoaderImportAttributePlugin(): Plugin {
           };
         }
 
+        // Use image data cache if present to avoid repeat file access for `image-file` loader types
+        let contents = await imageDataCache.get(args.path);
+        contents ??= await readFile(args.path);
+
         return {
-          contents: await readFile(args.path),
+          contents,
           loader,
         };
       });
