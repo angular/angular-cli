b86bb080e3a58a3320b2f68fb79edcdc98bfa7e9
Charles Lyding <19598772+clydin@users.noreply.github.com>
2024-10-02 11:30:34 -0400

    fix(@angular/build): disable dev-server websocket when live reload is disabled
    When live reload is disabled (`"liveReload": false`/`no-live-reload`) within
    the development server, the Vite websocket server will no longer be initialized.
    Additionally, the client code will not be loaded by the browser. This allows the
    development server to be used in test scenarios that would prefer to more fully
    represent the production fielded configuration such as E2E testing or other forms
    of browser-based testing.
    
    (cherry picked from commit 46335625ebe6476887f3e8797177aa9431d98ee1)

--- packages/angular/build/src/builders/dev-server/vite-server.ts
+++ packages/angular/build/src/builders/dev-server/vite-server.ts
@@ -651,6 +651,8 @@ export async function setupServer(
       host: serverOptions.host,
       open: serverOptions.open,
       headers: serverOptions.headers,
+      // Disable the websocket if live reload is disabled (false/undefined are the only valid values)
+      ws: serverOptions.liveReload === false && serverOptions.hmr === false ? false : undefined,
       proxy,
       cors: {
         // Allow preflight requests to be proxied.
@@ -711,6 +713,7 @@ export async function setupServer(
         virtualProjectRoot,
         outputFiles,
         external: externalMetadata.explicitBrowser,
+        skipViteClient: serverOptions.liveReload === false && serverOptions.hmr === false,
       }),
     ],
     // Browser only optimizeDeps. (This does not run for SSR dependencies).
