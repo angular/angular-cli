143c0dfb5ae4f87e415e761affab15fa58f159e5
Charles Lyding <19598772+clydin@users.noreply.github.com>
2024-10-31 13:57:22 -0400

    refactor(@angular/build): allow caching of TypeScript build contexts for rebuilds
    Now that the TypeScript bundler contexts perform additional checks for validity,
    they can now be cached in memory during watch modes. This allows the TypeScript
    bundler contexts to skip rebundling when not affected by a file changed. This is
    particularly beneficial for style related file changes where no code processing
    is otherwise required.

--- packages/angular/build/src/tools/esbuild/compiler-plugin-options.ts
+++ packages/angular/build/src/tools/esbuild/compiler-plugin-options.ts
@@ -8,14 +8,15 @@
 
 import { NormalizedApplicationBuildOptions } from '../../builders/application/options';
 import type { createCompilerPlugin } from './angular/compiler-plugin';
-import { ComponentStylesheetBundler } from './angular/component-stylesheets';
 import type { SourceFileCache } from './angular/source-file-cache';
+import type { LoadResultCache } from './load-result-cache';
 
 type CreateCompilerPluginParameters = Parameters<typeof createCompilerPlugin>;
 
 export function createCompilerPluginOptions(
   options: NormalizedApplicationBuildOptions,
-  sourceFileCache?: SourceFileCache,
+  sourceFileCache: SourceFileCache,
+  loadResultCache?: LoadResultCache,
 ): CreateCompilerPluginParameters[0] {
   const {
     sourcemapOptions,
@@ -37,7 +38,7 @@ export function createCompilerPluginOptions(
     advancedOptimizations,
     fileReplacements,
     sourceFileCache,
-    loadResultCache: sourceFileCache?.loadResultCache,
+    loadResultCache,
     incremental,
     externalRuntimeStyles,
     instrumentForCoverage,
