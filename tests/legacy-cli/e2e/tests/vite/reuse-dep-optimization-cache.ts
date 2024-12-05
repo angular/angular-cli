64c9e2bc70e0a55f4825cd40742520e95046d9b6
Alan Agius <alanagius@google.com>
2024-12-05 16:55:26 +0000

    fix(@angular/build): show error when Node.js built-ins are used during `ng serve`
    This commit ensures consistent behavior between `ng build` and `ng serve`. Previously, `ng serve` did not display an error message when Node.js built-in modules were included in browser bundles. By default, Vite replaces Node.js built-ins with empty modules, which can lead to unexpected runtime issues. This update addresses the problem by surfacing clear error messages, providing better developer feedback and alignment between the two commands.
    
    Closes: #27425
    (cherry picked from commit 06f478bc18d3e0daa8902d0fef94e55a5d052348)

--- tests/legacy-cli/e2e/tests/vite/reuse-dep-optimization-cache.ts
+++ tests/legacy-cli/e2e/tests/vite/reuse-dep-optimization-cache.ts
@@ -1,11 +1,7 @@
 import assert from 'node:assert';
+import { setTimeout } from 'node:timers/promises';
 import { findFreePort } from '../../utils/network';
-import {
-  execAndWaitForOutputToMatch,
-  killAllProcesses,
-  ng,
-  waitForAnyProcessOutputToMatch,
-} from '../../utils/process';
+import { execAndWaitForOutputToMatch, killAllProcesses, ng } from '../../utils/process';
 
 export default async function () {
   await ng('cache', 'clean');
@@ -13,31 +9,27 @@ export default async function () {
 
   const port = await findFreePort();
 
-  // Make sure serve is consistent with build
-  await execAndWaitForOutputToMatch(
-    'ng',
-    ['serve', '--port', `${port}`],
-    /Dependencies bundled/,
-    // Use CI:0 to force caching
-    { DEBUG: 'vite:deps', CI: '0' },
-  );
+  const [, response] = await Promise.all([
+    execAndWaitForOutputToMatch(
+      'ng',
+      ['serve', '--port', `${port}`],
+      /dependencies optimized/,
+      // Use CI:0 to force caching
+      { DEBUG: 'vite:deps', CI: '0', NO_COLOR: 'true' },
+    ),
+    setTimeout(4_000).then(() => fetch(`http://localhost:${port}/main.js`)),
+  ]);
 
-  // Make request so that vite writes the cache.
-  const response = await fetch(`http://localhost:${port}/main.js`);
   assert(response.ok, `Expected 'response.ok' to be 'true'.`);
-
-  // Wait for vite to write to FS and stablize.
-  await waitForAnyProcessOutputToMatch(/dependencies optimized/, 5000);
 
   // Terminate the dev-server
   await killAllProcesses();
 
-  // The Node.js specific module should not be found
   await execAndWaitForOutputToMatch(
     'ng',
     ['serve', '--port=0'],
     /Hash is consistent\. Skipping/,
     // Use CI:0 to force caching
-    { DEBUG: 'vite:deps', CI: '0' },
+    { DEBUG: 'vite:deps', CI: '0', NO_COLOR: 'true' },
   );
 }
