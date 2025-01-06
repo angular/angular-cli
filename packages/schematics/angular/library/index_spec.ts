02825eec53456384ba5b9c19f25dde3cfc95d796
Charles Lyding <19598772+clydin@users.noreply.github.com>
2025-01-06 12:06:24 -0500

    feat(@schematics/angular): use `@angular/build` package in library generation schematic
    The newly introduced `ng-packagr` builder within the `@angular/build` package
    is now used when generating a new library with `ng generate library`. This
    builder provides the same functionality as the `ng-packagr` builder found
    within the `@angular-devkit/build-angular` package but removes the need for
    projects to install `@angular-devkit/build-angular` if using the `application`
    builder from `@angular/build`.

--- packages/schematics/angular/library/index_spec.ts
+++ packages/schematics/angular/library/index_spec.ts
@@ -388,9 +388,7 @@ describe('Library Schematic', () => {
     const tree = await schematicRunner.runSchematic('library', defaultOptions, workspaceTree);
 
     const workspace = JSON.parse(tree.readContent('/angular.json'));
-    expect(workspace.projects.foo.architect.build.builder).toBe(
-      '@angular-devkit/build-angular:ng-packagr',
-    );
+    expect(workspace.projects.foo.architect.build.builder).toBe('@angular/build:ng-packagr');
   });
 
   describe('standalone=false', () => {
