395562a0f8c129f27d0f6428bb94749e74b0977b
Alan Agius <alanagius@google.com>
2025-01-21 12:47:55 +0000

    fix(@angular/build): include extracted routes in the manifest during prerendering
    This commit ensures that extracted routes are added to the manifest during prerendering. This guarantees a correct ng-server-context value for parameterized routes and provides a performance benefit by preventing route extraction from being repeated for every rendering worker.
    
    Closes #29426

--- packages/angular/build/src/builders/application/execute-post-bundle.ts
+++ packages/angular/build/src/builders/application/execute-post-bundle.ts
@@ -184,7 +184,6 @@ export async function executePostBundleSteps(
     }
 
     const serializableRouteTreeNodeForManifest: WritableSerializableRouteTreeNode = [];
-
     for (const metadata of serializableRouteTreeNode) {
       serializableRouteTreeNodeForManifest.push(metadata);
 
