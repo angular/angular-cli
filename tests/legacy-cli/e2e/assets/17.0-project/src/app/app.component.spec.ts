02df1be430b38e1d4346f9845415ce302da16000
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2025-03-04 12:23:19 +0000

    test: only test ng update for supported versions
    Removes tests for versions 15 and 16 which are no longer in LTS.

--- tests/legacy-cli/e2e/assets/17.0-project/src/app/app.component.spec.ts
+++ tests/legacy-cli/e2e/assets/17.0-project/src/app/app.component.spec.ts
@@ -1,16 +1,10 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterModule.forRoot([])
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'fifteen-project'`, () => {
+  it(`should have the 'seventeen-project' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('fifteen-project');
+    expect(app.title).toEqual('seventeen-project');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, fifteen-project');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, seventeen-project');
   });
 });
