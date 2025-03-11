5802c78c42e46c7effc5a32f95c1236542c450b8
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2025-03-11 13:05:00 +0000

    fix(@angular/build): invalidate `com.chrome.devtools.json` if project is moved
    Ensure that when a project is relocated, the `com.chrome.devtools.json` file is properly invalidated by checking the `root` path.

--- packages/angular/build/src/tools/vite/middlewares/chrome-devtools-middleware.ts
+++ packages/angular/build/src/tools/vite/middlewares/chrome-devtools-middleware.ts
@@ -6,10 +6,18 @@
  * found in the LICENSE file at https://angular.dev/license
  */
 
+import assert from 'node:assert';
 import { randomUUID } from 'node:crypto';
 import { mkdirSync, readFileSync, writeFileSync } from 'node:fs';
 import { join } from 'node:path';
 import type { Connect } from 'vite';
+
+type DevToolsJson = {
+  workspace: {
+    root: string;
+    uuid: string;
+  };
+};
 
 const CHROME_DEVTOOLS_ROUTE = '/.well-known/appspecific/com.chrome.devtools.json';
 
@@ -17,7 +25,7 @@ export function createChromeDevtoolsMiddleware(
   cacheDir: string,
   projectRoot: string,
 ): Connect.NextHandleFunction {
-  let devtoolsConfig: string;
+  let devtoolsConfig: string | undefined;
   const devtoolsConfigPath = join(cacheDir, 'com.chrome.devtools.json');
 
   return function chromeDevtoolsMiddleware(req, res, next) {
@@ -27,22 +35,27 @@ export function createChromeDevtoolsMiddleware(
       return;
     }
 
-    // We store the UUID and re-use it to ensure Chrome does not repeatedly ask for permissions when restarting the dev server.
-    try {
-      devtoolsConfig ??= readFileSync(devtoolsConfigPath, 'utf-8');
-    } catch {
-      const devtoolsConfigJson = {
-        workspace: {
-          root: projectRoot,
-          uuid: randomUUID(),
-        },
-      };
-
-      devtoolsConfig = JSON.stringify(devtoolsConfigJson, undefined, 2);
+    if (!devtoolsConfig) {
+      // We store the UUID and re-use it to ensure Chrome does not repeatedly ask for permissions when restarting the dev server.
       try {
-        mkdirSync(cacheDir, { recursive: true });
-        writeFileSync(devtoolsConfigPath, devtoolsConfig);
-      } catch {}
+        const devtoolsConfig = readFileSync(devtoolsConfigPath, 'utf-8');
+        const devtoolsConfigJson: DevToolsJson = JSON.parse(devtoolsConfig);
+        assert.equal(projectRoot, devtoolsConfigJson?.workspace.root);
+      } catch {
+        const devtoolsConfigJson: DevToolsJson = {
+          workspace: {
+            root: projectRoot,
+            uuid: randomUUID(),
+          },
+        };
+
+        devtoolsConfig = JSON.stringify(devtoolsConfigJson, undefined, 2);
+
+        try {
+          mkdirSync(cacheDir, { recursive: true });
+          writeFileSync(devtoolsConfigPath, devtoolsConfig);
+        } catch {}
+      }
     }
 
     res.setHeader('Content-Type', 'application/json');
