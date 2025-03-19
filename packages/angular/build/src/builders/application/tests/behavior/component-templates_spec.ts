cb2ab43abcf0e3c1a2cc584a326e1eea5eede7a8
Charles Lyding <19598772+clydin@users.noreply.github.com>
2025-03-19 11:36:30 -0400

    fix(@angular/build): ensure errors for missing component resources
    Compile time errors will now always be generated when an HTML template
    and/or CSS component stylesheet are not present on disk. The Angular
    compiler expects a return value of `null` from the `resourceNameToFileName`
    function to indicate that a resource path does not exist. JIT mode previously
    and continues to generate errors as expected.

---
+++ packages/angular/build/src/builders/application/tests/behavior/component-templates_spec.ts
@@ -0,0 +1,33 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.dev/license
+ */
+
+import { buildApplication } from '../../index';
+import { APPLICATION_BUILDER_INFO, BASE_OPTIONS, describeBuilder } from '../setup';
+
+describeBuilder(buildApplication, APPLICATION_BUILDER_INFO, (harness) => {
+  describe('Behavior: "Component Templates"', () => {
+    it('should generater an error for a missing template', async () => {
+      await harness.modifyFile('src/app/app.component.ts', (content) => {
+        return content.replace('./app.component.html', './not-present.html');
+      });
+
+      harness.useTarget('build', {
+        ...BASE_OPTIONS,
+      });
+
+      const { result, logs } = await harness.executeOnce({ outputLogsOnFailure: false });
+      expect(result?.success).toBeFalse();
+      expect(logs).toContain(
+        jasmine.objectContaining({
+          level: 'error',
+          message: jasmine.stringContaining(`Could not find template file './not-present.html'`),
+        }),
+      );
+    });
+  });
+});
