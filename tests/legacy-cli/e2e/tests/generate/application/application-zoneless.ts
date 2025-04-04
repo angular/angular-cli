27a5290140644174dfbcbb29451f46368d34f936
Andrew Scott <atscott01@gmail.com>
2025-04-04 14:07:28 -0700

    feat(@schematics/angular): Add prompt for new apps to be zoneless
    This commit adds a prompt to ask if the user wants to generate the app
    as zoneless.

--- tests/legacy-cli/e2e/tests/generate/application/application-zoneless.ts
+++ tests/legacy-cli/e2e/tests/generate/application/application-zoneless.ts
@@ -2,19 +2,12 @@ import { ng } from '../../../utils/process';
 import { useCIChrome } from '../../../utils/project';
 
 export default async function () {
-  await ng('generate', 'app', 'standalone', '--experimental-zoneless', '--standalone');
+  await ng('generate', 'app', 'standalone', '--zoneless', '--standalone');
   await useCIChrome('standalone', 'projects/standalone');
   await ng('test', 'standalone', '--watch=false');
   await ng('build', 'standalone');
 
-  await ng(
-    'generate',
-    'app',
-    'ngmodules',
-    '--experimental-zoneless',
-    '--no-standalone',
-    '--skip-install',
-  );
+  await ng('generate', 'app', 'ngmodules', '--zoneless', '--no-standalone', '--skip-install');
   await useCIChrome('ngmodules', 'projects/ngmodules');
   await ng('test', 'ngmodules', '--watch=false');
   await ng('build', 'ngmodules');
