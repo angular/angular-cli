74cd4edd5bbf5ae03a910be036f6e7fa7db35642
Dmitriy Stepanenko <stepanenkodmitri@gmail.com>
2025-04-07 12:27:31 +0300

    fix(@angular/build): skip normalization of relative externals
    (cherry picked from commit fc0e05fea89598204a7e5de494da897c396c4e52)

--- packages/angular/build/src/builders/application/options.ts
+++ packages/angular/build/src/builders/application/options.ts
@@ -703,5 +703,12 @@ function normalizeExternals(value: string[] | undefined): string[] | undefined {
     return undefined;
   }
 
-  return [...new Set(value.map((d) => (d.endsWith('/*') ? d.slice(0, -2) : d)))];
+  return [
+    ...new Set(
+      value.map((d) =>
+        // remove "/*" wildcard in the end if provided string is not path-like
+        d.endsWith('/*') && !/^\.{0,2}\//.test(d) ? d.slice(0, -2) : d,
+      ),
+    ),
+  ];
 }
