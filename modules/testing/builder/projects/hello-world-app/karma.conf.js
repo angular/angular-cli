b36034dfd5bc30774b89dce331147ecdc36dea11
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2025-05-16 12:57:07 +0000

    refactor: disable chrome sandbox as it no longer works in CI
    This commit disables the chrome sandbox
    
    (cherry picked from commit 0b4e3f3bfbcaf4a9123e3a544ce80ec7fa91733d)

--- modules/testing/builder/projects/hello-world-app/karma.conf.js
+++ modules/testing/builder/projects/hello-world-app/karma.conf.js
@@ -47,7 +47,7 @@ module.exports = function(config) {
         base: 'ChromeHeadless',
         flags: [
           '--disable-gpu',
-          ...(process.env.CHROME_NO_SANDBOX === '1' ? ['--no-sandbox'] : []),
+          '--no-sandbox'
         ],
       },
     },
