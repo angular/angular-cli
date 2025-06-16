130c650146595f237bc3285302d0075ba0387546
cexbrayat <cedric@ninja-squad.com>
2025-06-16 20:10:37 +0200

    fix(@angular/build): use an empty array as default value for vitest exclude
    Fixes #30543
    
    Avoids the error `resolved.coverage.exclude is not iterable` while running `ng test`.

--- packages/angular/build/src/builders/unit-test/builder.ts
+++ packages/angular/build/src/builders/unit-test/builder.ts
@@ -233,7 +233,7 @@ export async function* execute(
           coverage: {
             enabled: !!normalizedOptions.codeCoverage,
             excludeAfterRemap: true,
-            exclude: normalizedOptions.codeCoverage?.exclude,
+            exclude: normalizedOptions.codeCoverage?.exclude ?? [],
             // Special handling for `reporter` due to an undefined value causing upstream failures
             ...(normalizedOptions.codeCoverage?.reporters
               ? { reporters: normalizedOptions.codeCoverage.reporters }
