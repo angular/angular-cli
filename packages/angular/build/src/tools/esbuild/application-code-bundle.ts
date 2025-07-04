185e5b71dc3bb1e0ab9c4b21ba53340db859079b
Sebastian Barfurth <sbarfurth@google.com>
2025-07-04 09:58:18 +0000

    feat(@angular/build): allow enabling Bazel sandbox plugin with esbuild
    Setting the new `ENABLE_BAZEL_SANDBOX_PLUGIN` environment variable to `true` or `1`
    during a build with the `esbuild` based builder will now inject a special plugin to
    make `esbuild` compatible with Bazel builds in the output tree.
    
    When trying to integrate the `esbuild` based builder into Bazel with `rules_js` we found
    that it will incorrectly follow symlinks out of the sandbox. This is because Node.js based
    tooling runs in the output tree to support canonical JS project directory structures. The
    output tree will contain symlinks outside of the sandbox. Node tooling will generally
    follow these symlinks, which violates the rules of Bazel sandboxing. This can manifest
    in a wide variety of errors. One example we encountered with Angular compilation
    is that the symlinked browser entry point (e.g. `main.ts`) is outside of the range of
    `tsconfig.json` when the compiler follows the symlink.
    
    The plugin itself was originally written in https://github.com/aspect-build/rules_esbuild.
    The version container in this commit is a fork of https://github.com/aspect-build/rules_esbuild/blob/e4e49d3354cbf7087c47ac9c5f2e6fe7f5e398d3/esbuild/private/plugins/bazel-sandbox.js.
    I've adapted the JS file to TypeScript and made no further changes.

--- packages/angular/build/src/tools/esbuild/application-code-bundle.ts
+++ packages/angular/build/src/tools/esbuild/application-code-bundle.ts
@@ -30,6 +30,7 @@ import { createAngularLocaleDataPlugin } from './i18n-locale-plugin';
 import type { LoadResultCache } from './load-result-cache';
 import { createLoaderImportAttributePlugin } from './loader-import-attribute-plugin';
 import { createRxjsEsmResolutionPlugin } from './rxjs-esm-resolution-plugin';
+import { createBazelSandboxPlugin } from './sandbox-plugin-bazel';
 import { createServerBundleMetadata } from './server-bundle-metadata-plugin';
 import { createSourcemapIgnorelistPlugin } from './sourcemap-ignorelist-plugin';
 import { SERVER_GENERATED_EXTERNALS, getFeatureSupport, isZonelessApp } from './utils';
@@ -604,6 +605,31 @@ function getEsBuildCommonOptions(options: NormalizedApplicationBuildOptions): Bu
     } else {
       // Safe to use the packages external option directly
       packages = 'external';
+    }
+  }
+
+  // Inject the Bazel sandbox plugin only when specifically enabled to be fully backward compatible.
+  // Most users will never need this and as such should not have it influence their builds.
+  if (
+    process.env.ENABLE_BAZEL_SANDBOX_PLUGIN === 'true' ||
+    process.env.ENABLE_BAZEL_SANDBOX_PLUGIN === '1'
+  ) {
+    const bindir = process.env.BAZEL_BINDIR;
+    const execroot = process.env.JS_BINARY__EXECROOT;
+
+    let runfiles: string | undefined;
+    // If requested, remap paths to the runfiles tree in the sandbox instead of the bindir
+    // directly. This allows `js_binary` and `js_test` rules to invoke the Angular CLI against
+    // their runfiles.
+    if (
+      process.env.BAZEL_SANDBOX_PLUGIN_REMAP_TO_RUNFILES === '1' ||
+      process.env.BAZEL_SANDBOX_PLUGIN_REMAP_TO_RUNFILES === 'true'
+    ) {
+      runfiles = process.env.JS_BINARY__RUNFILES;
+    }
+
+    if (bindir && execroot) {
+      plugins.push(createBazelSandboxPlugin({ bindir, execroot, runfiles }));
     }
   }
 
