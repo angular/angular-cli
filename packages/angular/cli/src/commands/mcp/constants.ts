c957799c92dde57865dd0127540b4921123231d0
Charles Lyding <19598772+clydin@users.noreply.github.com>
2025-07-09 16:05:07 -0400

    refactor(@angular/cli): add a documentation search tool to MCP server
    An additional MCP tool is now available with the `ng mcp` stdio MCP server
    that supports querying the `angular.dev` documentation. This uses the
    same algolia based search indexing that the documentation website uses.
    Rate limiting has been implemented with the MCP tool that may be adjusted
    based on feedback. The tool returns one or more URLs and titles for relevant
    documentation for a given query. Content of these search results are currently
    not fetched but rather this action is deferred to the host to determine which
    items are most relevant and should be retrieved from the documentation website.

---
+++ packages/angular/cli/src/commands/mcp/constants.ts
@@ -0,0 +1,13 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.dev/license
+ */
+
+export const k1 = '@angular/cli';
+export const at = 'QBHBbOdEO4CmBOC2d7jNmg==';
+export const iv = Buffer.from([
+  0x97, 0xf4, 0x62, 0x95, 0x3e, 0x12, 0x76, 0x84, 0x8a, 0x09, 0x4a, 0xc9, 0xeb, 0xa2, 0x84, 0x69,
+]);
