06a6ddc102f5dc9018ec982f6e4cf56259cc4b52
Charles Lyding <19598772+clydin@users.noreply.github.com>
2025-07-22 17:15:43 -0400

    fix(@angular/build): correct JS/TS file paths when running under Bazel
    The compiler plugin now rewrites Bazel file paths if they were resolved
    outside of the execution root.
    This behavior is only used if Bazel environment variables are present.
    
    (cherry picked from commit 261dbb37cbe01492240c4cedc644663b15a4296a)

--- packages/angular/build/src/tools/esbuild/angular/compiler-plugin.ts
+++ packages/angular/build/src/tools/esbuild/angular/compiler-plugin.ts
@@ -29,6 +29,7 @@ import { SharedTSCompilationState, getSharedCompilationState } from './compilati
 import { ComponentStylesheetBundler } from './component-stylesheets';
 import { FileReferenceTracker } from './file-reference-tracker';
 import { setupJitPluginCallbacks } from './jit-plugin-callbacks';
+import { rewriteForBazel } from './rewrite-bazel-paths';
 import { SourceFileCache } from './source-file-cache';
 
 export interface CompilerPluginOptions {
@@ -411,8 +412,8 @@ export function createCompilerPlugin(
       });
 
       build.onLoad({ filter: /\.[cm]?[jt]sx?$/ }, async (args) => {
-        const request = path.normalize(
-          pluginOptions.fileReplacements?.[path.normalize(args.path)] ?? args.path,
+        const request = rewriteForBazel(
+          path.normalize(pluginOptions.fileReplacements?.[path.normalize(args.path)] ?? args.path),
         );
         const isJS = /\.[cm]?js$/.test(request);
 
@@ -478,13 +479,14 @@ export function createCompilerPlugin(
         return {
           contents,
           loader,
+          resolveDir: path.dirname(request),
         };
       });
 
       build.onLoad(
         { filter: /\.[cm]?js$/ },
         createCachedLoad(pluginOptions.loadResultCache, async (args) => {
-          let request = args.path;
+          let request = rewriteForBazel(args.path);
           if (pluginOptions.fileReplacements) {
             const replacement = pluginOptions.fileReplacements[path.normalize(args.path)];
             if (replacement) {
@@ -505,6 +507,7 @@ export function createCompilerPlugin(
               return {
                 contents,
                 loader: 'js',
+                resolveDir: path.dirname(request),
                 watchFiles: request !== args.path ? [request] : undefined,
               };
             },
