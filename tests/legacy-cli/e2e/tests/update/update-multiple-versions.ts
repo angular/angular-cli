0e26e9c08c94b9c35999252a5b59052b4c03d580
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2025-08-14 15:06:10 +0000

    test: update e2e update test to use angular 18
    Angular 17 is no longer supported.

--- tests/legacy-cli/e2e/tests/update/update-multiple-versions.ts
+++ tests/legacy-cli/e2e/tests/update/update-multiple-versions.ts
@@ -7,7 +7,7 @@ import { expectToFail } from '../../utils/utils';
 export default async function () {
   let restoreRegistry: (() => Promise<void>) | undefined;
   try {
-    restoreRegistry = await createProjectFromAsset('17.0-project', true);
+    restoreRegistry = await createProjectFromAsset('18.0-project', true);
     await setRegistry(true);
 
     const extraArgs = ['--force'];
