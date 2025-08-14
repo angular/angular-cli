6c51602d8249878c91967de0247bf4083473abec
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2025-08-14 15:40:09 +0000

    refactor: delete protractor tests
    protractor builder is private.

--- tests/legacy-cli/e2e/tests/commands/e2e/e2e-and-serve.ts
+++
@@ -1,8 +0,0 @@
-import { silentNg } from '../../../utils/process';
-import { ngServe } from '../../../utils/project';
-
-export default async function () {
-  // Should run side-by-side with `ng serve`
-  await ngServe();
-  await silentNg('e2e');
-}
