6023da588a42b6cf82f5e870d972f8b4ce0e39d8
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2025-08-18 11:55:22 +0000

    fix(@angular/cli): apply default to array types
    This commit fixes an issue where the `default` option was not being applied to `array` type options in yargs.
    
    This seemingly minor change required refactoring in some tests, which revealed that a `.coerce` validation was incorrectly throwing an error on failure. The validation logic was moved to a `.check` to ensure proper error handling and prevent unexpected failures.
    
    (cherry picked from commit c3789e4ebbbdf37a76df3b97a7d842612b52155d)

--- packages/angular/cli/src/command-builder/utilities/json-schema_spec.ts
+++ packages/angular/cli/src/command-builder/utilities/json-schema_spec.ts
@@ -6,95 +6,61 @@
  * found in the LICENSE file at https://angular.dev/license
  */
 
-import { json, schema } from '@angular-devkit/core';
+import { schema } from '@angular-devkit/core';
 import yargs from 'yargs';
 
 import { addSchemaOptionsToCommand, parseJsonSchemaToOptions } from './json-schema';
 
-const YError = (() => {
-  try {
-    const y = yargs().strict().fail(false).exitProcess(false).parse(['--forced-failure']);
-  } catch (e) {
-    if (!(e instanceof Error)) {
-      throw new Error('Unexpected non-Error thrown');
-    }
-
-    return e.constructor as typeof Error;
-  }
-  throw new Error('Expected parse to fail');
-})();
-
-interface ParseFunction {
-  (argv: string[]): unknown;
-}
-
-function withParseForSchema(
-  jsonSchema: json.JsonObject,
-  {
-    interactive = true,
-    includeDefaultValues = true,
-  }: { interactive?: boolean; includeDefaultValues?: boolean } = {},
-): ParseFunction {
-  let actualParse: ParseFunction = () => {
-    throw new Error('Called before init');
-  };
-  const parse: ParseFunction = (args) => {
-    return actualParse(args);
-  };
-
-  beforeEach(async () => {
-    const registry = new schema.CoreSchemaRegistry();
-    const options = await parseJsonSchemaToOptions(registry, jsonSchema, interactive);
-
-    actualParse = async (args: string[]) => {
-      // Create a fresh yargs for each call. The yargs object is stateful and
-      // calling .parse multiple times on the same instance isn't safe.
-      const localYargs = yargs().exitProcess(false).strict().fail(false);
-      addSchemaOptionsToCommand(localYargs, options, includeDefaultValues);
-
+describe('parseJsonSchemaToOptions', () => {
+  describe('without required fields in schema', () => {
+    const parse = async (args: string[]) => {
       // Yargs only exposes the parse errors as proper errors when using the
       // callback syntax. This unwraps that ugly workaround so tests can just
       // use simple .toThrow/.toEqual assertions.
       return localYargs.parseAsync(args);
     };
-  });
 
-  return parse;
-}
-
-describe('parseJsonSchemaToOptions', () => {
-  describe('without required fields in schema', () => {
-    const parse = withParseForSchema({
-      'type': 'object',
-      'properties': {
-        'maxSize': {
-          'type': 'number',
-        },
-        'ssr': {
-          'type': 'string',
-          'enum': ['always', 'surprise-me', 'never'],
-        },
-        'arrayWithChoices': {
-          'type': 'array',
-          'items': {
+    let localYargs: yargs.Argv<unknown>;
+    beforeEach(async () => {
+      // Create a fresh yargs for each call. The yargs object is stateful and
+      // calling .parse multiple times on the same instance isn't safe.
+      localYargs = yargs().exitProcess(false).strict().fail(false).wrap(1_000);
+      const jsonSchema = {
+        'type': 'object',
+        'properties': {
+          'maxSize': {
+            'type': 'number',
+          },
+          'ssr': {
             'type': 'string',
-            'enum': ['always', 'never'],
+            'enum': ['always', 'surprise-me', 'never'],
+          },
+          'arrayWithChoices': {
+            'type': 'array',
+            'default': 'default-array',
+            'items': {
+              'type': 'string',
+              'enum': ['always', 'never', 'default-array'],
+            },
+          },
+          'extendable': {
+            'type': 'object',
+            'properties': {},
+            'additionalProperties': {
+              'type': 'string',
+            },
+          },
+          'someDefine': {
+            'type': 'object',
+            'additionalProperties': {
+              'type': 'string',
+            },
           },
         },
-        'extendable': {
-          'type': 'object',
-          'properties': {},
-          'additionalProperties': {
-            'type': 'string',
-          },
-        },
-        'someDefine': {
-          'type': 'object',
-          'additionalProperties': {
-            'type': 'string',
-          },
-        },
-      },
+      };
+      const registry = new schema.CoreSchemaRegistry();
+      const options = await parseJsonSchemaToOptions(registry, jsonSchema, false);
+      addSchemaOptionsToCommand(localYargs, options, true);
     });
 
     describe('type=number', () => {
@@ -123,6 +89,10 @@ describe('parseJsonSchemaToOptions', () => {
           /Argument: array-with-choices, Given: "yes", Choices:/,
         );
       });
+
+      it('should add default value to help', async () => {
+        expect(await localYargs.getHelp()).toContain('[default: "default-array"]');
+      });
     });
 
     describe('type=string, enum', () => {
@@ -150,11 +120,9 @@ describe('parseJsonSchemaToOptions', () => {
 
       it('rejects invalid values for string maps', async () => {
         await expectAsync(parse(['--some-define', 'foo'])).toBeRejectedWithError(
-          YError,
           /Invalid value for argument: some-define, Given: 'foo', Expected key=value pair/,
         );
         await expectAsync(parse(['--some-define', '42'])).toBeRejectedWithError(
-          YError,
           /Invalid value for argument: some-define, Given: '42', Expected key=value pair/,
         );
       });
@@ -187,43 +155,42 @@ describe('parseJsonSchemaToOptions', () => {
 
   describe('with required positional argument', () => {
     it('marks the required argument as required', async () => {
-      const jsonSchema = JSON.parse(`
-      {
-        "$id": "FakeSchema",
-        "title": "Fake Schema",
-        "type": "object",
-        "required": ["a"],
-        "properties": {
-          "b": {
-            "type": "string",
-            "description": "b.",
-            "$default": {
-              "$source": "argv",
-              "index": 1
-            }
+      const jsonSchema = {
+        '$id': 'FakeSchema',
+        'title': 'Fake Schema',
+        'type': 'object',
+        'required': ['a'],
+        'properties': {
+          'b': {
+            'type': 'string',
+            'description': 'b.',
+            '$default': {
+              '$source': 'argv',
+              'index': 1,
+            },
           },
-          "a": {
-            "type": "string",
-            "description": "a.",
-            "$default": {
-              "$source": "argv",
-              "index": 0
-            }
+          'a': {
+            'type': 'string',
+            'description': 'a.',
+            '$default': {
+              '$source': 'argv',
+              'index': 0,
+            },
           },
-          "optC": {
-            "type": "string",
-            "description": "optC"
+          'optC': {
+            'type': 'string',
+            'description': 'optC',
           },
-          "optA": {
-            "type": "string",
-            "description": "optA"
+          'optA': {
+            'type': 'string',
+            'description': 'optA',
           },
-          "optB": {
-            "type": "string",
-            "description": "optB"
-          }
-        }
-      }`) as json.JsonObject;
+          'optB': {
+            'type': 'string',
+            'description': 'optB',
+          },
+        },
+      };
       const registry = new schema.CoreSchemaRegistry();
       const options = await parseJsonSchemaToOptions(registry, jsonSchema, /* interactive= */ true);
 
