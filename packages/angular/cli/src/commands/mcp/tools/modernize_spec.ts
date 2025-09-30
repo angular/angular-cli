17889c239a784e93d95bae2616289bd7a1fc196f
Alon Mishne <amishne@users.noreply.github.com>
2025-09-30 16:39:59 -0700

    refactor(@angular/cli): Change modernize MCP to invoke schematics directly

--- packages/angular/cli/src/commands/mcp/tools/modernize_spec.ts
+++ packages/angular/cli/src/commands/mcp/tools/modernize_spec.ts
@@ -6,68 +6,213 @@
  * found in the LICENSE file at https://angular.dev/license
  */
 
-import { ModernizeInput, runModernization } from './modernize';
+import { exec } from 'child_process';
+import { mkdir, mkdtemp, readFile, rm, writeFile } from 'fs/promises';
+import { tmpdir } from 'os';
+import { join } from 'path';
+import { promisify } from 'util';
+import { ModernizeOutput, runModernization } from './modernize';
+
+const execAsync = promisify(exec);
 
 describe('Modernize Tool', () => {
-  async function getInstructions(input: ModernizeInput): Promise<string[] | undefined> {
-    const { structuredContent } = await runModernization(input);
+  let projectDir: string;
+  let originalPath: string | undefined;
 
-    if (!structuredContent || !('instructions' in structuredContent)) {
-      fail('Expected instructions to be present in the result');
+  beforeEach(async () => {
+    originalPath = process.env.PATH;
+    projectDir = await mkdtemp(join(tmpdir(), 'angular-modernize-test-'));
 
-      return;
-    }
+    // Create a dummy Angular project structure.
+    await writeFile(
+      join(projectDir, 'angular.json'),
+      JSON.stringify(
+        {
+          version: 1,
+          projects: {
+            app: {
+              root: '',
+              projectType: 'application',
+              architect: {
+                build: {
+                  options: {
+                    tsConfig: 'tsconfig.json',
+                  },
+                },
+              },
+            },
+          },
+        },
+        null,
+        2,
+      ),
+    );
+    await writeFile(
+      join(projectDir, 'package.json'),
+      JSON.stringify(
+        {
+          dependencies: {
+            '@angular/core': 'latest',
+          },
+          devDependencies: {
+            '@angular/cli': 'latest',
+            '@angular-devkit/schematics': 'latest',
+            typescript: 'latest',
+          },
+        },
+        null,
+        2,
+      ),
+    );
+    await writeFile(
+      join(projectDir, 'tsconfig.base.json'),
+      JSON.stringify(
+        {
+          compilerOptions: {
+            strict: true,
+            forceConsistentCasingInFileNames: true,
+            skipLibCheck: true,
+          },
+        },
+        null,
+        2,
+      ),
+    );
+    await writeFile(
+      join(projectDir, 'tsconfig.json'),
+      JSON.stringify(
+        {
+          extends: './tsconfig.base.json',
+          compilerOptions: {
+            outDir: './dist/out-tsc',
+          },
+        },
+        null,
+        2,
+      ),
+    );
 
-    return structuredContent.instructions;
+    // Symlink the node_modules directory from the runfiles to the temporary project.
+    const nodeModulesPath = require
+      .resolve('@angular/core/package.json')
+      .replace(/\/@angular\/core\/package\.json$/, '');
+    await execAsync(`ln -s ${nodeModulesPath} ${join(projectDir, 'node_modules')}`);
+
+    // Prepend the node_modules/.bin path to the PATH environment variable
+    // so that `ng` can be found by `execAsync` calls.
+    process.env.PATH = `${join(nodeModulesPath, '.bin')}:${process.env.PATH}`;
+  });
+
+  afterEach(async () => {
+    process.env.PATH = originalPath;
+    await rm(projectDir, { recursive: true, force: true });
+  });
+
+  async function modernize(
+    dir: string,
+    file: string,
+    transformations: string[],
+  ): Promise<{ structuredContent: ModernizeOutput; newContent: string }> {
+    const structuredContent = (
+      (await runModernization({ directories: [dir], transformations })) as {
+        structuredContent: ModernizeOutput;
+      }
+    ).structuredContent;
+    const newContent = await readFile(file, 'utf8');
+
+    return { structuredContent, newContent };
   }
 
-  it('should return an instruction for a single transformation', async () => {
-    const instructions = await getInstructions({
-      transformations: ['self-closing-tags-migration'],
-    });
+  it('can run a single transformation', async () => {
+    const componentPath = join(projectDir, 'test.component.ts');
+    const componentContent = `
+      import { Component } from '@angular/core';
 
-    expect(instructions).toEqual([
-      'To run the self-closing-tags-migration migration, execute the following command: ' +
-        '`ng generate @angular/core:self-closing-tags-migration`.\nFor more information, ' +
-        'see https://angular.dev/reference/migrations/self-closing-tags.',
+      @Component({
+        selector: 'app-foo',
+        template: '<app-bar></app-bar>',
+      })
+      export class FooComponent {}
+    `;
+    await writeFile(componentPath, componentContent);
+
+    const { structuredContent, newContent } = await modernize(projectDir, componentPath, [
+      'self-closing-tag',
+    ]);
+
+    expect(structuredContent?.stderr).toBe('');
+    expect(newContent).toContain('<app-bar />');
+    expect(structuredContent?.instructions).toEqual([
+      'Migration self-closing-tag on directory . completed successfully.',
     ]);
   });
 
-  it('should return instructions for multiple transformations', async () => {
-    const instructions = await getInstructions({
-      transformations: ['self-closing-tags-migration', 'inject'],
-    });
+  it('can run multiple transformations', async () => {
+    const componentPath = join(projectDir, 'test.component.ts');
+    const componentContent = `
+      import { Component } from '@angular/core';
 
-    const expectedInstructions = [
-      'To run the self-closing-tags-migration migration, execute the following command: ' +
-        '`ng generate @angular/core:self-closing-tags-migration`.\nFor more information, ' +
-        'see https://angular.dev/reference/migrations/self-closing-tags.',
-      'To run the inject migration, execute the following command: ' +
-        '`ng generate @angular/core:inject`.\nFor more information, ' +
-        'see https://angular.dev/reference/migrations/inject-function.',
-    ];
+      @Component({
+        selector: 'app-foo',
+        template: '<app-bar *ngIf="show"></app-bar>',
+      })
+      export class FooComponent {
+        show = true;
+      }
+    `;
+    await writeFile(componentPath, componentContent);
 
-    expect(instructions?.sort()).toEqual(expectedInstructions.sort());
-  });
-
-  it('should return a link to the best practices page when no transformations are requested', async () => {
-    const instructions = await getInstructions({
-      transformations: [],
-    });
-
-    expect(instructions).toEqual([
-      'See https://angular.dev/best-practices for Angular best practices. You can call this ' +
-        'tool if you have specific transformation you want to run.',
+    const { structuredContent, newContent } = await modernize(projectDir, componentPath, [
+      'control-flow',
+      'self-closing-tag',
     ]);
+
+    expect(structuredContent?.stderr).toBe('');
+    expect(newContent).toContain('@if (show) {<app-bar />}');
   });
 
-  it('should return special instructions for standalone migration', async () => {
-    const instructions = await getInstructions({
-      transformations: ['standalone'],
-    });
+  it('can run multiple transformations across multiple directories', async () => {
+    const subfolder1 = join(projectDir, 'subfolder1');
+    await mkdir(subfolder1);
+    const componentPath1 = join(subfolder1, 'test.component.ts');
+    const componentContent1 = `
+    import { Component } from '@angular/core';
 
-    expect(instructions?.[0]).toContain(
-      'Run the commands in the order listed below, verifying that your code builds and runs between each step:',
-    );
+    @Component({
+      selector: 'app-foo',
+      template: '<app-bar *ngIf="show"></app-bar>',
+      })
+      export class FooComponent {
+        show = true;
+        }
+        `;
+    await writeFile(componentPath1, componentContent1);
+
+    const subfolder2 = join(projectDir, 'subfolder2');
+    await mkdir(subfolder2);
+    const componentPath2 = join(subfolder2, 'test.component.ts');
+    const componentContent2 = `
+    import { Component } from '@angular/core';
+
+    @Component({
+      selector: 'app-bar',
+      template: '<app-baz></app-baz>',
+      })
+      export class BarComponent {}
+      `;
+    await writeFile(componentPath2, componentContent2);
+
+    const structuredContent = (
+      (await runModernization({
+        directories: [subfolder1, subfolder2],
+        transformations: ['control-flow', 'self-closing-tag'],
+      })) as { structuredContent: ModernizeOutput }
+    ).structuredContent;
+    const newContent1 = await readFile(componentPath1, 'utf8');
+    const newContent2 = await readFile(componentPath2, 'utf8');
+
+    expect(structuredContent?.stderr).toBe('');
+    expect(newContent1).toContain('@if (show) {<app-bar />}');
+    expect(newContent2).toContain('<app-baz />');
   });
 });
