4d1fc1fd51fde284e6e812be0d3bfbf40a9eaf90
Charles Lyding <19598772+clydin@users.noreply.github.com>
2025-10-01 17:08:57 -0400

    test: directly disable color in cache-info E2E test

--- tests/legacy-cli/e2e/tests/commands/cache/cache-info.ts
+++ tests/legacy-cli/e2e/tests/commands/cache/cache-info.ts
@@ -1,37 +1,62 @@
 import { execAndWaitForOutputToMatch } from '../../../utils/process';
 import { updateJsonFile } from '../../../utils/project';
 
+const ENV_NO_COLOR = {
+  'NO_COLOR': '1',
+};
+
 export default async function () {
   const originalCIValue = process.env['CI'];
 
   try {
     // Should be enabled by default for local builds.
     await configureTest('0' /** envCI */);
-    await execAndWaitForOutputToMatch('ng', ['cache', 'info'], /Effective Status\s*: Enabled/);
+    await execAndWaitForOutputToMatch('ng', ['cache', 'info'], /Effective Status\s*: Enabled/, {
+      ...process.env,
+      ...ENV_NO_COLOR,
+    });
 
     // Should be disabled by default for CI builds.
     await configureTest('1' /** envCI */, { enabled: true });
-    await execAndWaitForOutputToMatch('ng', ['cache', 'info'], /Effective Status\s*: Disabled/);
+    await execAndWaitForOutputToMatch('ng', ['cache', 'info'], /Effective Status\s*: Disabled/, {
+      ...process.env,
+      ...ENV_NO_COLOR,
+    });
 
     // Should be enabled by when environment is local and env is not CI.
     await configureTest('0' /** envCI */, { environment: 'local' });
-    await execAndWaitForOutputToMatch('ng', ['cache', 'info'], /Effective Status\s*: Enabled/);
+    await execAndWaitForOutputToMatch('ng', ['cache', 'info'], /Effective Status\s*: Enabled/, {
+      ...process.env,
+      ...ENV_NO_COLOR,
+    });
 
     // Should be disabled by when environment is local and env is CI.
     await configureTest('1' /** envCI */, { environment: 'local' });
-    await execAndWaitForOutputToMatch('ng', ['cache', 'info'], /Effective Status\s*: Disabled/);
+    await execAndWaitForOutputToMatch('ng', ['cache', 'info'], /Effective Status\s*: Disabled/, {
+      ...process.env,
+      ...ENV_NO_COLOR,
+    });
 
     // Effective status should be enabled when 'environment' is set to 'all' or 'ci'.
     await configureTest('1' /** envCI */, { environment: 'all' });
-    await execAndWaitForOutputToMatch('ng', ['cache', 'info'], /Effective Status\s*: Enabled/);
+    await execAndWaitForOutputToMatch('ng', ['cache', 'info'], /Effective Status\s*: Enabled/, {
+      ...process.env,
+      ...ENV_NO_COLOR,
+    });
 
     // Effective status should be enabled when 'environment' is set to 'ci' and run is in ci
     await configureTest('1' /** envCI */, { environment: 'ci' });
-    await execAndWaitForOutputToMatch('ng', ['cache', 'info'], /Effective Status\s*: Enabled/);
+    await execAndWaitForOutputToMatch('ng', ['cache', 'info'], /Effective Status\s*: Enabled/, {
+      ...process.env,
+      ...ENV_NO_COLOR,
+    });
 
     // Effective status should be disabled when 'enabled' is set to false
     await configureTest('1' /** envCI */, { environment: 'all', enabled: false });
-    await execAndWaitForOutputToMatch('ng', ['cache', 'info'], /Effective Status\s*: Disabled/);
+    await execAndWaitForOutputToMatch('ng', ['cache', 'info'], /Effective Status\s*: Disabled/, {
+      ...process.env,
+      ...ENV_NO_COLOR,
+    });
   } finally {
     process.env['CI'] = originalCIValue;
   }
