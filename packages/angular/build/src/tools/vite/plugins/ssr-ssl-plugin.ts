e1e579f3cff166bcbd2d80bc14b3882a1af85ec7
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2025-11-21 13:36:19 +0000

    fix(@angular/build): force dev-server to use HTTP/1.1 when using SSR with SSL
    When using SSR with SSL, Vite attempts to use HTTP/2 by default. However, the Express server used for SSR does not support HTTP/2, causing requests to fail. This commit forces Vite to use HTTP/1.1 in this scenario by setting the ALPNProtocols to `['http/1.1']`.
    
    This is required as now Vite uses HTTP 2 for SSL: https://github.com/vitejs/vite/commit/fc21af7a42dd559a95f54b6165d34f36883eaa7f
    
    Closes #31894

--- packages/angular/build/src/tools/vite/plugins/ssr-ssl-plugin.ts
+++ packages/angular/build/src/tools/vite/plugins/ssr-ssl-plugin.ts
@@ -23,6 +23,18 @@ export function createAngularServerSideSSLPlugin(): Plugin {
         return;
       }
 
+      if (httpServer && 'ALPNProtocols' in httpServer) {
+        // Force Vite to use HTTP/1.1 when SSR and SSL are enabled.
+        // This is required because the Express server used for SSR does not support HTTP/2.
+        // See: https://github.com/vitejs/vite/blob/46d3077f2b63771cc50230bc907c48f5773c00fb/packages/vite/src/node/http.ts#L126
+
+        // We directly set the `ALPNProtocols` on the HTTP server to override the default behavior.
+        // Passing `ALPNProtocols` in the TLS options would cause Node.js to automatically include `h2`.
+        // Additionally, using `ALPNCallback` is not an option as it is mutually exclusive with `ALPNProtocols`.
+        // See: https://github.com/nodejs/node/blob/b8b4350ed3b73d225eb9e628d69151df56eaf298/lib/internal/http2/core.js#L3351
+        httpServer.ALPNProtocols = ['http/1.1'];
+      }
+
       // TODO(alanagius): Replace `undici` with `tls.setDefaultCACertificates` once we only support Node.js 22.18.0+ and 24.5.0+.
       // See: https://nodejs.org/api/tls.html#tlssetdefaultcacertificatescerts
       const { getGlobalDispatcher, setGlobalDispatcher, Agent } = await import('undici');
