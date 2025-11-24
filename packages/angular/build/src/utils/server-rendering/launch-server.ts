89a6caeb13e84d6531fb0584f8235a1b800411ae
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2025-11-24 13:12:51 +0000

    refactor: simplify dynamic import type assertions and remove resolution mode hints
    Remove `any` castings

--- packages/angular/build/src/utils/server-rendering/launch-server.ts
+++ packages/angular/build/src/utils/server-rendering/launch-server.ts
@@ -21,8 +21,7 @@ export const DEFAULT_URL = new URL('http://ng-localhost/');
 export async function launchServer(): Promise<URL> {
   const { reqHandler } = await loadEsmModuleFromMemory('./server.mjs');
   const { createWebRequestFromNodeRequest, writeResponseToNodeResponse } = (await import(
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    '@angular/ssr/node' as any
+    '@angular/ssr/node' as string
   )) as typeof import('@angular/ssr/node', { with: { 'resolution-mode': 'import' } });
 
   if (!isSsrNodeRequestHandler(reqHandler) && !isSsrRequestHandler(reqHandler)) {
