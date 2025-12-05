9033d9434d61a4969e45b1a943daa41dffeb203e
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2025-12-05 11:07:04 +0000

    ci: force dev server host to IPv4 to resolve RBE resolution issues
    This is needed to get around an RBE issue

--- packages/angular_devkit/build_webpack/test/basic-app/webpack.config.cjs
+++ packages/angular_devkit/build_webpack/test/basic-app/webpack.config.cjs
@@ -3,6 +3,11 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: path.resolve(__dirname, './src/main.js'),
+  devServer: {
+    // Force an IPv4 address as with RBE there are currently resolution issues with IPv6 addresses.
+    // http://localhost is resolved to IPv6 address with vite and webpack but to IPv4 with node fetch.
+    host: '127.0.0.1',
+  },
   module: {
     rules: [
       // rxjs 6 requires directory imports which are not support in ES modules.
