84ef187815c3d2309591433f37bd67d77538ea11
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2025-12-17 12:31:31 +0000

    test: update e2e test assets from Angular 18.0 to 19.0
    Version 18 is no longer supported.

--- tests/legacy-cli/e2e/assets/19.0-project/src/app/app.component.ts
+++ tests/legacy-cli/e2e/assets/19.0-project/src/app/app.component.ts
@@ -3,11 +3,10 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'eighteen-project';
+  title = 'nineteen-project';
 }
