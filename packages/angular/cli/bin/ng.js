7649c66f96cdb60bce17a8e87da08475f1b89579
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2026-05-21 14:23:02 +0000

    build: update minimum Node.js 24 version to 24.15.0
    Updates the minimum supported version of Node.js 24 from 24.13.1 to 24.15.0. This includes upgrading the dev toolchain configured for rules_nodejs in MODULE.bazel to Node.js v24.15.0, along with the corresponding archive checksums across all targeted platforms (Darwin, Linux, and Windows).
    
    Also updates target version range constraints in constants.bzl, package.json, and packages/angular/cli/bin/ng.js.
    
    The MODULE.bazel.lock has been updated by running module resolution.

--- packages/angular/cli/bin/ng.js
+++ packages/angular/cli/bin/ng.js
@@ -57,7 +57,7 @@ if (major === 23 || major === 25) {
 
   require('./bootstrap');
 } else if (!nodeUtils.isNodeVersionSupported()) {
-  // Error and exit if less than 22.22 or 24.13.1
+  // Error and exit if less than 22.22 or 24.15.0
   console.error(
     'Node.js version ' +
       process.version +
