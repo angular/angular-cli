3275b45559bb5c0a82b3f408bb1e6ec034382dab
Charles Lyding <19598772+clydin@users.noreply.github.com>
2026-06-10 07:30:32 -0400

    test(@angular/cli): remove unscoped authentication test cases from registry tests
    Modern package managers do not support unscoped authentication, and Yarn Classic's
    metadata command (yarn info) does not propagate unscoped credentials correctly,
    leading to 403 Forbidden failures on secure registries during ng update and ng add.
    
    This commit removes the unscoped authentication test cases from both add and
    update secure registry E2E tests, and cleans up the
    createNpmConfigForAuthentication helper to default to scoped authentication.
    
    (cherry picked from commit ecda7061f6af9abf9e1793493dc521c79c28404a)

--- tests/e2e/tests/commands/add/secure-registry.ts
+++ tests/e2e/tests/commands/add/secure-registry.ts
@@ -1,5 +1,5 @@
-import { expectFileNotToExist, expectFileToExist, rimraf } from '../../../utils/fs';
-import { getActivePackageManager, installWorkspacePackages } from '../../../utils/packages';
+import { expectFileNotToExist, expectFileToExist } from '../../../utils/fs';
+import { installWorkspacePackages } from '../../../utils/packages';
 import { git, ng } from '../../../utils/process';
 import { createNpmConfigForAuthentication } from '../../../utils/registry';
 import { expectToFail } from '../../../utils/utils';
@@ -9,37 +9,17 @@ export default async function () {
   try {
     // The environment variable has priority over the .npmrc
     delete process.env['NPM_CONFIG_REGISTRY'];
-    const packageManager = getActivePackageManager();
-    const supportsUnscopedAuth = packageManager === 'yarn';
     const command = ['add', '@angular/pwa', '--skip-confirmation'];
-
-    // Works with unscoped registry authentication details
-    if (supportsUnscopedAuth) {
-      // Some package managers such as Bun and NPM do not support unscoped auth.
-      await createNpmConfigForAuthentication(false);
-
-      await expectFileNotToExist('public/manifest.webmanifest');
-
-      await ng(...command);
-      await expectFileToExist('public/manifest.webmanifest');
-      await git('clean', '-dxf');
-    }
 
     // Works with scoped registry authentication details
     await expectFileNotToExist('public/manifest.webmanifest');
 
-    await createNpmConfigForAuthentication(true);
+    await createNpmConfigForAuthentication();
     await ng(...command);
     await expectFileToExist('public/manifest.webmanifest');
     await git('clean', '-dxf');
 
     // Invalid authentication token
-    if (supportsUnscopedAuth) {
-      // Some package managers such as Bun and NPM do not support unscoped auth.
-      await createNpmConfigForAuthentication(false, true);
-      await expectToFail(() => ng(...command));
-    }
-
     await createNpmConfigForAuthentication(true, true);
     await expectToFail(() => ng(...command));
   } finally {
