150361238aa337aad8201659e4d77562cc43869a
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2026-09-09 13:50:48 +0000

    test(@angular/build): fix flakiness in dev-server rebuild error detection test

--- packages/angular/build/src/builders/dev-server/tests/behavior/build-errors_spec.ts
+++ packages/angular/build/src/builders/dev-server/tests/behavior/build-errors_spec.ts
@@ -38,8 +38,8 @@ describeServeBuilder(executeDevServer, DEV_SERVER_BUILDER_INFO, (harness, setupT
             expectNoLog(logs, 'Unexpected character "EOF"');
           },
         ],
-        { outputLogsOnFailure: false },
+        { outputLogsOnFailure: false, timeout: 60_000 },
       );
-    });
+    }, 90_000);
   });
 });
